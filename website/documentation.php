d313d9fadfdfe857f37153961b4b66556c5f51e0
Ceolter <niall.crosby@gmail.com>
2015-03-30 21:49:33 +0100

    updated front page and documentation

---
+++ website/documentation.php
@@ -0,0 +1,74 @@
+<?php
+$key = "index";
+$pageTitle = "Angular Grid Documentation";
+$pageDescription = "Introduction page of documentation for Angular Grid";
+$pageKeyboards = "AngularJS Angular Grid Documentation";
+include 'documentation_header.php';
+?>
+
+<div>
+
+    <h2>Foreword</h2>
+
+    <div style="width: 800px;">
+        <p>
+            I am committed to writing documentation and providing examples. If you notice anything
+            wrong with the documentation, please let me know, so I can expand on it. If you have
+            any examples of the grid you can share, please send them on. If using the grid
+            in the public domain, please pass on your url.
+        </p>
+        <h4>
+            Help Grow the Community
+        </h4>
+        <p>
+            If you are using or thinking of using Angular Grid, then please show your support. You can do this
+            in any of the following ways:
+            <h4>1. Star or Watch this Project on Github
+                    <iframe src="https://ghbtns.com/github-btn.html?user=ceolter&repo=angular-grid&type=star&count=true&size=large"
+                            frameborder="0" scrolling="0" width="160px" height="30px">
+                    </iframe>
+                    <iframe src="https://ghbtns.com/github-btn.html?user=ceolter&repo=angular-grid&type=watch&count=true&size=large&v=2"
+                            frameborder="0" scrolling="0" width="160px" height="30px">
+                    </iframe>
+            </h4>
+
+            <h4>2. Say you use this project in
+                <a href="http://ngmodules.org/modules/angular-grid" style="color: black; border: 1px solid darkgrey; background-color: lightgrey; padding: 2px;">
+                    A<span style="color: darkred">ng</span>ular Modules
+                </a>
+            </h4>
+
+            <h4>3. Spread with Social Media
+
+            <a class='share-link' href="https://www.facebook.com/sharer/sharer.php?u=www.angulargrid.com">
+                <img src="/images/facebook_32.png" alt="Share on Facebook" title="Share on Facebook"/>
+            </a>
+
+            <a class='share-link' href="https://twitter.com/home?status=Check%20out%20AngularGrid,%20a%20new%20way%20to%20show%20grid%20data%20for%20AngularJS">
+                <img src="/images/twitter_32.png" alt="Share on Twitter" title="Share on Twitter"/>
+            </a>
+
+            <a class='share-link' href="https://plus.google.com/share?url=www.angulargrid.com">
+                <img src="/images/googleplus_32.png" alt="Share on Google Plus" title="Share on Google Plus"/>
+            </a>
+
+            <a class='share-link' href="https://www.linkedin.com/shareArticle?mini=true&url=www.angulargrid.com&title=Angular%20Grid&summary=A%20new%20way%20to%20show%20grid%20data%20for%20AngularJS&source=">
+                <img src="/images/linkedin_32.png" alt="Share on LinkedIn" title="Share on LinkedIn"/>
+            </a>
+
+            </h4>
+
+            <h4>4. As a question on the
+                <a href="/forum" style="color: black; border: 1px solid darkgrey; background-color: lightgrey; padding: 2px;">
+                    <span style="color: darkred">A</span>ngular <span style="color: darkred">G</span>rid Forum
+                </a>
+            </h4>
+
+            <h4>5. Link to this page from yours, or ask me to do a link swap!</h4>
+
+        </p>
+    </div>
+
+</div>
+
+<?php include 'documentation_footer.php';?>
