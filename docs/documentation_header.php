4e1b83c44f3f26b57a50b6571bcec56b9a050c73
Niall Crosby <niall.crosby@gmail.com>
2015-04-20 18:37:00 +0100

    iteration of value getters

--- docs/documentation_header.php
+++ docs/documentation_header.php
@@ -129,6 +129,12 @@
                     <a class="sidebarLink" href="/angular-grid-pinning/index.php">Pinning</a>
                 <?php } ?>
 
+                <?php if ($key == "Value Getters") { ?>
+                    <span class="sidebarLinkSelected">Value Getters</span>
+                <?php } else { ?>
+                    <a class="sidebarLink" href="/angular-grid-value-getters/index.php">Value Getters</a>
+                <?php } ?>
+
                 <?php if ($key == "Cell Styling") { ?>
                     <span class="sidebarLinkSelected">Cell Styling</span>
                 <?php } else { ?>
@@ -175,12 +181,6 @@
                     <span class="sidebarLinkSelected">Context</span>
                 <?php } else { ?>
                     <a class="sidebarLink" href="/angular-grid-context/index.php">Context</a>
-                <?php } ?>
-
-                <?php if ($key == "Rules and Expressions") { ?>
-                    <span class="sidebarLinkSelected">Rules and Expressions</span>
-                <?php } else { ?>
-                    <a class="sidebarLink" href="/angular-grid-rules-and-expressions/index.php">Rules and Expressions</a>
                 <?php } ?>
 
                 <?php if ($key == "Datasource") { ?>
