ffdbce522b25a0879c365f21771b8f26d8d30baf
Niall Crosby <niall.crosby@gmail.com>
2015-04-20 15:25:04 +0100

    introducing context

--- docs/angular-grid-context/index.php
+++ docs/angular-grid-context/index.php
@@ -10,9 +10,17 @@ include '../documentation_header.php';
 
     <h2>Context</h2>
 
-    What is the grid context?
+    <p>
+        The context object is passed to most of the callbacks used in Angular Grid, such as the cell renderers.
+        Angular Grid does not place anything into the context, it is not used at all internally by Angular
+        Grid. It's purpose is to allow the client application to pass details to custom callbacks.
+    </p>
 
-    <h4>Sub Heading</h4>
+    <h4>Example</h4>
+
+    In the example below, the drop down selects a reporting currency. This reporting currency selection
+    is placed inside the context object, which is subsequently used by the a) cell renderer to calculate
+    the value to display and b) the header of the last column, to display a particular header value.
 
     <show-example example="example1"></show-example>
 </div>
