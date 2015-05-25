9f694e246233eb9d540b6fff8003693f408578ff
Ceolter <niall.crosby@gmail.com>
2015-05-25 10:44:45 +0100

    added disqus to documentation pages

--- docs/documentation_footer.php
+++ docs/documentation_footer.php
@@ -1,8 +1,24 @@
+            <hr>
+
+            <div id="disqus_thread"></div>
+            <script type="text/javascript">
+                /* * * CONFIGURATION VARIABLES * * */
+                var disqus_shortname = 'angulargrid';
+
+                /* * * DON'T EDIT BELOW THIS LINE * * */
+                (function() {
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                })();
+            </script>
+            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+
             </div>
 
         </div>
 
-        <hr>
+        <hr/>
 
         <footer>
             <div style="float:left;" class="license">
