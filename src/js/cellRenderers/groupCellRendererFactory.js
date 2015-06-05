95b8d57961eaba160e71881244d80e25adbfcc85
Niall Crosby <niall.crosby@gmail.com>
2015-06-05 09:50:57 +0100

    small changes

--- src/js/cellRenderers/groupCellRendererFactory.js
+++ src/js/cellRenderers/groupCellRendererFactory.js
@@ -109,7 +109,7 @@ function groupCellRendererFactory(gridOptionsWrapper, selectionRendererFactory) 
     function getGroupName(params) {
         var cellRenderer = params.colDef.cellRenderer;
         if (cellRenderer && cellRenderer.keyMap
-            && typeof cellRenderer.keyMap === 'object' && colDef.cellRenderer !== null) {
+            && typeof cellRenderer.keyMap === 'object' && params.colDef.cellRenderer !== null) {
             var valueFromMap = cellRenderer.keyMap[params.node.key];
             if (valueFromMap) {
                 return valueFromMap;
