e2c7cb67acbd9d3d7cc3370fb0140ac77dc40451
Ceolter <niall.crosby@gmail.com>
2015-09-06 11:30:33 +0100

    updated 'not used' for release

--- docs/angular-grid-getting-started/index.php
+++ docs/angular-grid-getting-started/index.php
@@ -39,7 +39,7 @@ include '../documentation_header.php';
         changes every time there is a grid release, to trick the browser in getting the latest version rather than using a cached version.
         <br/>
         So eg, the example has this:<br/>
-        <i>&lt;link rel="stylesheet" type="text/css" href="../dist/angular-grid.css?ignore=notused6"><br/></i>
+        <i>&lt;link rel="stylesheet" type="text/css" href="../dist/angular-grid.css?ignore=notused7"><br/></i>
         But all you need is this:<br/>
         <i>&lt;link rel="stylesheet" type="text/css" href="../dist/angular-grid.css"></i>
     </note>
