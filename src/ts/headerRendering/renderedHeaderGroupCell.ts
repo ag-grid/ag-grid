04764ac83f841b2bcb0d71caaad0d760f7f50ad1
Russell Cousineau <russell.miller@rentrakmail.com>
2016-01-06 16:08:02 -0800

    ensure sort icons are updated for sub-headers of a header group

--- src/ts/headerRendering/renderedHeaderGroupCell.ts
+++ src/ts/headerRendering/renderedHeaderGroupCell.ts
@@ -69,6 +69,9 @@ module ag.grid {
             this.children.forEach( (childElement: RenderedHeaderElement)=> {
                 childElement.refreshSortIcon();
             });
+            this.subHeaders.forEach( (childElement: RenderedHeaderGroupCell) => {
+                childElement.refreshSortIcon();
+            });
         }
 
         public onIndividualColumnResized(column: Column) {
