b7b92348ca500438c0d1d5c6b8a60419416a34c6
Ceolter <niall.crosby@gmail.com>
2016-02-09 09:14:08 +0000

    fixes #702, was not exporting everything for CommonJS

--- main.d.ts
+++ main.d.ts
@@ -6,3 +6,6 @@ export * from './dist/lib/components/componentUtil';
 export * from './dist/lib/events';
 export * from './dist/lib/gridApi';
 export * from './dist/lib/columnController/columnController';
+export * from './dist/lib/components/agGridNg1';
+export * from './dist/lib/components/agGridWebComponent';
+export * from './dist/lib/functions';
