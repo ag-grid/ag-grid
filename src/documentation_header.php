0e88bbc0b5a5a4ae18c65240ff7919b32241c28c
Ceolter <niall.crosby@gmail.com>
2016-03-07 20:25:29 +0000

    fixed gulp files

--- src/documentation_header.php
+++ src/documentation_header.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap -->
-    <link inline rel="stylesheet" href="../dist/bootstrap.css">
-    <link inline rel="stylesheet" href="../dist/bootstrap-theme.css">
+    <link inline rel="stylesheet" href="./dist/bootstrap.css">
+    <link inline rel="stylesheet" href="./dist/bootstrap-theme.css">
 
 <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>-->
 <!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>-->
