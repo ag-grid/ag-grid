f60b741e0f87e4fac5545e64f485c6e68b6b5443
Ceolter <niall.crosby@gmail.com>
2016-03-15 07:36:59 +0000

    fixed bug in pagination

---
+++ src/tests/index.php
@@ -0,0 +1,15 @@
+<?php
+$key = "Tests";
+$pageTitle = "Tests";
+$pageDescription = "Tests";
+$pageKeyboards = "Tests";
+include '../documentation-main/documentation_header.php';
+?>
+
+<div>
+
+    <show-example example="itSettingSortBeforeDataSource"></show-example>
+
+</div>
+
+<?php include '../documentation-main/documentation_footer.php';?>
