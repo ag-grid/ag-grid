9dec6fd0fa410eac78bb461dc16a419ea2afd0ae
Ceolter <niall.crosby@gmail.com>
2016-03-23 22:45:01 +0000

    fixes #803

--- src/angular-grid-export/index.php
+++ src/angular-grid-export/index.php
@@ -34,7 +34,7 @@ include '../documentation-main/documentation_header.php';
         <li><b>skipFooters</b>: Set to true to skip footers only if grouping. No impact if not grouping or if not using footers in grouping.</li>
         <li><b>fileName</b>: String to use as the file name. If missing, the file name 'export.csv' will be used.</li>
         <li><b>customHeader</b>: If you want to put some text at the top of the csv file, stick it here.
-            You will need to include '\n' at the end, or many '\n' if you want the header to span lines..</li>
+            You will need to include '\n' at the end, or many '\n' if you want the header to span lines.</li>
         <li><b>customFooter</b>: Same as customHeader, but for the end of the file.</li>
         <li><b>allColumns</b>: If true, all columns will be exported in the order they appear in columnDefs.
             Otherwise only the columns currently showing the in grid, and in that order, are exported.</li>
