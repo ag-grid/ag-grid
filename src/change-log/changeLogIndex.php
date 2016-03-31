40b425b102dc4addc70c2c2bb61b82cb6d29b097
Ceolter <niall.crosby@gmail.com>
2016-03-31 08:43:58 +0100

    fixes #778 - api.setBrowserFocus() now focuses the cell

--- src/change-log/changeLogIndex.php
+++ src/change-log/changeLogIndex.php
@@ -15,6 +15,7 @@ include '../documentation-main/documentation_header.php';
     <li>Editing: Hitting any key started editing.</li>
     <li>Enhancement: api.refreshCell() now takes 'animate' parameter, so cell will flash when changed.</li>
     <li>Enhancement: New api methods: showColumnMenuAfterButtonClick(colKey, buttonElement), showColumnMenuAfterMouseClick(colKey, mouseEvent)</li>
+    <li>Bugfix: api.setFocusedCell() method now sets browser focus (previous was just graphically highlighting the cell)</li>
 
     <h3>Version 4.0.x (ag-Grid 4.0.5, ag-Grid-Enterprise 4.0.7)</h3>
     <li>Bugfix: Status bar was not calculating 'min' correctly.</li>
