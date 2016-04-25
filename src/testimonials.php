acb08adcf2caf8a4c727a3632c080ec45d0cc256
Ceolter <niall.crosby@gmail.com>
2016-04-25 15:10:15 +0100

    update to documentation

--- src/testimonials.php
+++ src/testimonials.php
@@ -70,20 +70,6 @@
             document.write(name + '@' + domain + '</a>');
             // --></script>
             </p>
-<!--            <p>Use the form below to add your own testimonial:</p>-->
-<!--            <div id="disqus_thread"></div>-->
-<!--            <script type="text/javascript">-->
-<!--                /* * * CONFIGURATION VARIABLES * * */-->
-<!--                var disqus_shortname = 'aggrid';-->
-<!---->
-<!--                /* * * DON'T EDIT BELOW THIS LINE * * */-->
-<!--                (function() {-->
-<!--                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;-->
-<!--                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';-->
-<!--                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);-->
-<!--                })();-->
-<!--            </script>-->
-<!--            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>-->
         </div> <!-- end col -->
     </div> <!-- end row -->
 </div>
