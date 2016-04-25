eb3fe1c4e675fcb077aa4b114fa4b4730d9bc6c0
Ceolter <niall.crosby@gmail.com>
2016-04-25 16:56:04 +0100

    release of 4.1.0

--- dist/lib/cellRenderers/groupCellRendererFactory.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for ag-grid v4.0.5
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ceolter/>
-// Definitions: https://github.com/borisyankov/DefinitelyTyped
-import { GridOptionsWrapper } from '../gridOptionsWrapper';
-import { SelectionRendererFactory } from '../selectionRendererFactory';
-import { ExpressionService } from '../expressionService';
-import { EventService } from '../eventService';
-export declare function groupCellRendererFactory(gridOptionsWrapper: GridOptionsWrapper, selectionRendererFactory: SelectionRendererFactory, expressionService: ExpressionService, eventService: EventService): (params: any) => HTMLSpanElement;
