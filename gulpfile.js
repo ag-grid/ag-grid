ca6e35163b6aa2d1c0e67fb40419e1c60cb48533
Sean Landsman <sean@thelandsmans.com>
2016-05-25 16:30:28 +0100

    Licensing work

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+var gulp = require('gulp-param')(require('gulp'), process.argv);
 var gulpTypescript = require('gulp-typescript');
 var typescript = require('typescript');
 var header = require('gulp-header');
