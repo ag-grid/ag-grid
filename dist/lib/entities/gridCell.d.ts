c4d1cb8aef0204d58e73031821c2955ff4a75f1d
ceolter <niall.crosby@gmail.com>
2016-05-27 17:02:35 +0100

    release of 4.2.x

--- dist/lib/entities/gridCell.d.ts
+++ dist/lib/entities/gridCell.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ag-grid v4.2.1
+// Type definitions for ag-grid v4.2.2
 // Project: http://www.ag-grid.com/
 // Definitions by: Niall Crosby <https://github.com/ceolter/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
