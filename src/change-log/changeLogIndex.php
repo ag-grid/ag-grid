c0620e1e01074dae970d3912d2a4f5742bb05856
ceolter <niall.crosby@gmail.com>
2016-06-29 10:15:01 +0100

    isCancelBeforeStart and isCancelAfterEnd wree not working for popup editors

--- src/change-log/changeLogIndex.php
+++ src/change-log/changeLogIndex.php
@@ -28,6 +28,8 @@ include '../documentation-main/documentation_header.php';
     <li>Breaking Change: colDef.suppressAggregation and colDef.suppressRowGroup are gone, replaced with enableRowGroup, enablePivot and enableValue</li>
     <li>Bugfix: AngularJS bindings were not getting cells updated when columns added and removed</li>
     <li>Bugfix: Popups were triggering a scrollbar to flicker on and off when showing (eg show column menu, a scrollbar appeard and then dissappeared in the grid)</li>
+    <li>Bugfix: rowSelectionChanged was not firing on shift select</li>
+    <li>Bugfix: isCancelBeforeStart and isCancelAfterEnd were not working for popup editors</li>
 
 
     <h3>Version 4.2.x (ag-Grid 4.2.7, ag-Grid-Enterprise 4.2.11)</h3>
