2e7158a5adaa275e4f4b3215cac27f131cb3a0bd
ceolter <niall.crosby@gmail.com>
2016-07-10 11:07:24 +0100

    release 5.0.0

--- lib/agGridNg2.d.ts
+++ lib/agGridNg2.d.ts
@@ -1,4 +1,4 @@
-// ag-grid-ng2 v5.0.0-alpha.0
+// ag-grid-ng2 v5.0.0
 import { GridApi, ColumnApi } from 'ag-grid/main';
 import { ElementRef } from '@angular/core';
 export declare class AgGridNg2 {
