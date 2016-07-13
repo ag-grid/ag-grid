ed44ca4c1a6c44870edc15a592c2dcf8f086f0a5
Sean Landsman <sean@thelandsmans.com>
2016-07-13 09:40:11 +0100

    Update release information for 5x

--- src/licenseManager.ts
+++ src/licenseManager.ts
@@ -4,7 +4,7 @@ import {MD5} from './license/md5';
 
 @Bean('licenseManager')
 export class LicenseManager {
-    private static RELEASE_INFORMATION:string = 'MTQ2NDM0OTI1NTM1MQ==';
+    private static RELEASE_INFORMATION:string = 'MTQ2ODM5ODkxNjg4MA==';
     private static licenseKey:string;
 
     @Autowired('md5') private md5:MD5;
