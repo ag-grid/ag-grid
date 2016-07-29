0abc2a5599fc2c1073e0adf7b014d0849e0cd8db
ceolter <niall.crosby@gmail.com>
2016-07-29 13:16:45 +0100

    fixes #1040 - largeTextCellEditor was showing 'undefined' when value was undefined

--- src/change-log/changeLogIndex.php
+++ src/change-log/changeLogIndex.php
@@ -13,6 +13,7 @@ include '../documentation-main/documentation_header.php';
     <li>Bugfix: api.setHeaderHeight() was not working, now fixed.</li>
     <li>Bugfix: fixed issue where hidden grid (it not visible) was not rendering all rows it should.</li>
     <li>Bugfix: columnApi.getColumnState() was setting aggFunc on columns that were aggregated but then later removed.</li>
+    <li>Bugfix: largeTextCellEditor - when value was undefined was displaying 'undefined', now displays blank.</li>
 
     <h3>Version 5.0.x (ag-Grid 5.0.6, ag-Grid-Enterprise 5.0.6)</h3>
     <li>Enhancement: added columnApi.getAllDisplayedVirtualColumns(), so you can check what columns are rendered due to column virtualisation.</li>
