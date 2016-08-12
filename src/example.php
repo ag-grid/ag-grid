a2afc09df227fe862f11591ef35bb41e6dcbda51
ceolter <niall.crosby@gmail.com>
2016-08-12 17:19:30 +0100

    iteration of updatable rowsets

--- src/example.php
+++ src/example.php
@@ -42,8 +42,8 @@
 
         <link rel="shortcut icon" href="https://www.ag-grid.com/favicon.ico" />
 
-<!--        <script src="./dist/ag-grid.js?ignore=notused24"></script>-->
-        <script src="./dist/ag-grid-enterprise.js?ignore=notused24"></script>
+<!--        <script src="./dist/ag-grid.js?ignore=notused25"></script>-->
+        <script src="./dist/ag-grid-enterprise.js?ignore=notused25"></script>
 
         <script src="example.js"></script>
 
