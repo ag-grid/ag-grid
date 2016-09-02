a5ae063a5d7cd61789ef36367a3f4dcc058b2f8b
ceolter <niall.crosby@gmail.com>
2016-09-02 16:40:30 +0100

    release 5.3.2

--- lib/reactCellRendererFactory.d.ts
+++ lib/reactCellRendererFactory.d.ts
@@ -1,2 +1,2 @@
-// ag-grid-react v5.3.1
+// ag-grid-react v5.3.2
 export declare function reactCellRendererFactory(reactComponent: any, parentComponent?: any): ((params: any) => HTMLElement | string);
--- lib/reactFilterFactory.d.ts
+++ lib/reactFilterFactory.d.ts
@@ -1,2 +1,2 @@
-// ag-grid-react v5.3.1
+// ag-grid-react v5.3.2
 export declare function reactFilterFactory(reactComponent: any, parentComponent?: any): Function;
