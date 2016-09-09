c8c17c0e56836e5fe659f651a67fce7d1d5c19fa
ceolter <niall.crosby@gmail.com>
2016-09-09 14:18:10 +0100

    release 5.4.0

--- lib/reactCellRendererFactory.d.ts
+++ lib/reactCellRendererFactory.d.ts
@@ -1,2 +1,2 @@
-// ag-grid-react v5.3.2
+// ag-grid-react v5.4.0
 export declare function reactCellRendererFactory(reactComponent: any, parentComponent?: any): ((params: any) => HTMLElement | string);
