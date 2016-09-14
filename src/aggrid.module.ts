1493db5a3154c50adc489547826ef48a7593e607
seanlandsman <sean@thelandsmans.com>
2016-09-14 19:56:34 +0100

    Allow agmodule to be declared anywhere

--- src/aggrid.module.ts
+++ src/aggrid.module.ts
@@ -6,19 +6,24 @@ import {AgComponentFactory} from './agComponentFactory';
 import {Ng2FrameworkFactory} from './ng2FrameworkFactory';
 
 @NgModule({
-    imports: [
-    ],
+    imports: [],
     declarations: [
         AgGridNg2
     ],
     exports: [
         AgGridNg2
-    ],
-    providers: [
-        AgComponentFactory,
-        Ng2FrameworkFactory,
-        COMPILER_PROVIDERS
     ]
 })
 export class AgGridModule {
+    static forRoot()
+    {
+        return {
+            ngModule: AgGridModule,
+            providers: [ // singletons across the whole app
+                AgComponentFactory,
+                Ng2FrameworkFactory,
+                COMPILER_PROVIDERS
+            ],
+        };
+    }
 }
