0cb17bc4ede673eaf277c495adff8489d3f537e7
seanlandsman <sean@thelandsmans.com>
2016-09-19 07:26:50 +0100

    Correct case of exports

--- main.d.ts
+++ main.d.ts
@@ -2,6 +2,6 @@ export * from './lib/aggrid.module';
 export * from './lib/agGridNg2';
 export * from './lib/agComponentFactory';
 export * from './lib/agAware';                  // deprecated
-export * from './lib/AgFrameworkComponent';
-export * from './lib/AgRendererComponent';
-export * from './lib/AgEditorComponent';
+export * from './lib/agFrameworkComponent';
+export * from './lib/agRendererComponent';
+export * from './lib/agEditorComponent';
