a6d1ae38c801d3a16c512ab94dd41d212b220c84
ceolter <niall.crosby@gmail.com>
2016-10-03 11:31:59 +0100

    iteration of editable rows

--- src/documentation-main/documentation.php
+++ src/documentation-main/documentation.php
@@ -19,6 +19,16 @@ include 'documentation_header.php';
     </style>
 
     <div class="news-box">
+        <div class="news-header">3rs October 2016</div>
+        <div class="news-header">ag-Grid 6.1.x Released</div>
+        <div style="padding-top: 10px;">
+            ag-Grid 6.1.x released with new Feature <a href="../javascript-grid-cell-editing/#fullRowEdit">
+                Full Row Editing</a>, to enable all cells in a row to be editable at the same time.
+            See <a href="../change-log/">Change Log</a> for full release details.
+        </div>
+    </div>
+
+    <div class="news-box">
         <div class="news-header">Angular Connect London 2016</div>
         <div style="text-align: center;">
             <a href="https://www.youtube.com/watch?v=jamr2jmEzh8/">
