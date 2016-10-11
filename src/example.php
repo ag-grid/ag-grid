c9236bf2fcb4afa148ddd7ef3cc71009118ed8a7
ceolter <niall.crosby@gmail.com>
2016-10-11 17:09:39 +0100

    changed not used

--- src/example.php
+++ src/example.php
@@ -42,8 +42,8 @@
 
         <link rel="shortcut icon" href="https://www.ag-grid.com/favicon.ico" />
 
-<!--        <script src="./dist/ag-grid.js?ignore=notused33"></script>-->
-        <script src="./dist/ag-grid-enterprise.js?ignore=notused33"></script>
+<!--        <script src="./dist/ag-grid.js?ignore=notused34"></script>-->
+        <script src="./dist/ag-grid-enterprise.js?ignore=notused34"></script>
 
         <script src="example.js"></script>
 
