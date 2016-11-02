158cc7adf2b6b053dffedd30b2f49938fefbe087
ceolter <niall.crosby@gmail.com>
2016-11-02 08:53:19 +0000

    speed improvements

--- src/example.js
+++ src/example.js
@@ -108,7 +108,7 @@ suppressEnterprise: true,
     pivotPanelShow: 'always', // on of ['always','onlyWhenPivoting']
 //minColWidth: 50,
 //maxColWidth: 300,
-//rowsBuffer: 1,
+//rowBuffer: 0,
 //columnDefs: [],
 //singleClickEdit: true,
 //rowData: [
