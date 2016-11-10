637a4aec1adbbcab56111b312ca68af5616b63cb
Sean Landsman <sean@thelandsmans.com>
2016-11-10 09:44:32 +0000

    move to bundled ng2 examples

--- gulpfile.js
+++ gulpfile.js
@@ -27,8 +27,7 @@ gulp.task('webpackEnterprise', ['tscEnterprise'], webpackEnterprise);
 gulp.task('webpackGrid', ['stylusGrid','tscGrid'], webpackGrid);
 
 
-gulp.task('liveReloadAfterCopyFromDocs', ['copyFromDocsExclNodeModules'], liveReloadTask);
-gulp.task('copyFromDocsExclNodeModules', copyFromDocsExclNodeModules);
+gulp.task('liveReloadAfterCopyFromDocs', ['copyFromDocs'], liveReloadTask);
 gulp.task('copyFromDocs', copyFromDocs);
 gulp.task('copyFromBootstrap', copyFromBootstrap);
 gulp.task('copyFromFontAwesome', copyFromFontAwesome);
@@ -168,16 +167,7 @@ function webpackGrid() {
 }
 
 function copyFromDocs() {
-    return gulp.src(
-        ['../ag-grid-docs/src/**/*'])
-        .pipe(gulp.dest('./web-root'));
-}
-
-function copyFromDocsExclNodeModules() {
-    return gulp.src(
-        ['!../ag-grid-docs/src/ng2-example/node_modules',
-            '../ag-grid-docs/src/**/*'
-        ])
+    return gulp.src(['../ag-grid-docs/src/**/*'])
         .pipe(gulp.dest('./web-root'));
 }
 
