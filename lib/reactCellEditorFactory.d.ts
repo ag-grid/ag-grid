b7dacb72a3d7c89a309f4d076a0f9927f034f74c
Sean Landsman <sean@thelandsmans.com>
2016-11-11 13:37:54 +0000

    6.4.0

--- lib/reactCellEditorFactory.d.ts
+++ lib/reactCellEditorFactory.d.ts
@@ -1,4 +1,4 @@
-// ag-grid-react v6.3.0
+// ag-grid-react v6.4.0
 import { ICellEditor } from 'ag-grid';
 export declare function reactCellEditorFactory(reactComponent: any, parentComponent?: any): {
     new (): ICellEditor;
--- lib/reactCellRendererFactory.d.ts
+++ lib/reactCellRendererFactory.d.ts
@@ -1,4 +1,4 @@
-// ag-grid-react v6.3.0
+// ag-grid-react v6.4.0
 import { ICellRenderer } from 'ag-grid';
 export declare function reactCellRendererFactory(reactComponent: any, parentComponent?: any): {
     new (): ICellRenderer;
