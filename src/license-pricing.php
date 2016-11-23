0f5402aa8469d8da9d19a951718f9136af4a5a23
John Masterson <johnmasterson@johns-mbp.home>
2016-11-23 12:45:48 +0000

    Added Hotjar script for tracking

--- src/license-pricing.php
+++ src/license-pricing.php
@@ -20,7 +20,17 @@
     <link rel="stylesheet" type="text/css" href="/style.css">
 
     <link rel="shortcut icon" href="https://www.ag-grid.com/favicon.ico"/>
-
+    <!-- Hotjar Tracking Code for www.ag-grid.com -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:344028,hjsv:5};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
 </head>
 
 <body ng-app="index" class="big-text">
