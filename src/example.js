6661c087e6793990c469979c378d953edf2c054b
ceolter <niall.crosby@gmail.com>
2016-12-06 14:36:09 +0000

    iteration of RTL

--- src/example.js
+++ src/example.js
@@ -133,7 +133,7 @@ suppressEnterprise: true,
     // suppressDragLeaveHidesColumns: true,
 //unSortIcon: true,
     //rowHeight: 30, // defaults to 25, can be any integer
-    // enableRtl: true,
+    enableRtl: true,
     animateRows: true,
     enableColResize: true, //one of [true, false]
     enableSorting: true, //one of [true, false]
