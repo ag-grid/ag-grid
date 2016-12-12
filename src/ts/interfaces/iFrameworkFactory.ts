4674b3dad70a3ab3eda3841018d341e14bb2fed5
Sean Landsman <sean@thelandsmans.com>
2016-12-12 11:40:31 +0000

    small signature change

--- src/ts/interfaces/iFrameworkFactory.ts
+++ src/ts/interfaces/iFrameworkFactory.ts
@@ -23,5 +23,5 @@ export interface IFrameworkFactory {
 
     /** Because Angular 2 uses Zones, you should not use setTimout(). So to get around this, we allow the framework
      * to specify how to execute setTimeout. The default is to just call the browser setTimeout(). */
-    setTimeout(handler: any, timeout?: any): number;
+    setTimeout(action: any, timeout?: any): void;
 }
