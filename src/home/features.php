d1815d967c98c4054a657b622cf21c847684b864
John Masterson <johnmasterson@Johns-MacBook-Pro.local>
2016-12-15 17:12:01 +0000

    Changes to home page for features

--- src/home/features.php
+++ src/home/features.php
@@ -1,4 +1,4 @@
-<section class="HomeSection HomeBenefits">
+<!-- <section class="HomeSection HomeBenefits">
     <div class="container">
         <div class="row">
             <h3 class="text-center">Extensive Feature List</h3>
@@ -54,4 +54,67 @@
 
         </div>
     </div>
-</section>
+</section> -->
+
+<section class="HomeSection HomeBenefits">
+<div class="container">
+
+
+
+    <div class="container">
+        <div class="row">
+            <h3 class="text-center"><font color="black"> While other grids charge, these features come with ag-Grid-Free:</h3>
+            <ul class="list-unstyled text-center">
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-filtering/">Filtering</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-grouping-headers/">Grouping Columns</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-sorting/">Sorting</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-selection/">Selection</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-resizing/">Resizing</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-pinning/">Pinning</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-tree/">Tree Data</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-row-height/">Row Height</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-floating/">Floating Rows</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-value-getters/">Value Getters</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-cell-expressions/">Cell Expressions</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-cell-styling/">Cell Styling</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-cell-rendering/">Cell Rendering</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-cell-editing/">Cell Editing</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-context/">Context</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-insert-remove/">Insert & Remove</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-refresh/">Refresh</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-animation/">Animation</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-header-rendering/">Header Templates<br/>&nbsp;& Rendering</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-styling/">Layout & Styling including<br/>&nbsp; Multiple Themes</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-master-detail/">Full Width Rows<br/>&nbsp;& Master Detail</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-keyboard-navigation/">Keyboard Navigation</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-master-slave/">Master / Slave</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-touch/">Touch</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-model/">Row Model</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-export/">Data Export</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-internationalisation/">Internationalisation</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-icons/">Icons</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-overlays/">Overlays</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-for-print/">For Print</a></li>
+            </ul>
+        </div>
+<div class="row" style="background-color: #eee">
+            <h3 class="text-center"> <font color="black"> We only charge for ag-Grid-Enterprise when we go above and beyond the competition:</h3>
+            <ul class="list-unstyled text-center">
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-viewport/">Technical Support</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-grouping/">Grouping Rows</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-aggregation/">Aggregation</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-set-filtering/">Set Filtering</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-pivoting/">Pivoting</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-clipboard/">Clipboard</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-viewport/">Viewport</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-data-functions/">Data Functions</a></li>          
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-tool-panel/">Tool Panel</a></li>            
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-column-menu/">Column Menu</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-context-menu/">Context Menu</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-range-selection/">Range Selection</a></li>
+                <li><font color="green"> &#10004; <a href="<?php print($rootFolder) ?>javascript-grid-status-bar/">Status Bar</a></li>
+            </ul>
+        </div>
+
+    </div>
+    </section>
