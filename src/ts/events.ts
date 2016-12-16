ed9fe35f914c363c51ce427978eff059516dfa55
ceolter <niall.crosby@gmail.com>
2016-12-16 10:55:07 +0000

    updated comments on events

--- src/ts/events.ts
+++ src/ts/events.ts
@@ -1,10 +1,11 @@
 export class Events {
 
-    /** A new set of columns has been entered, everything has potentially changed. */
+    /** Everything has changed with the columns. Either complete new set of columns set, or user called setState()*/
     public static EVENT_COLUMN_EVERYTHING_CHANGED = 'columnEverythingChanged';
+    /** User has set in new columns. */
     public static EVENT_NEW_COLUMNS_LOADED = 'newColumnsLoaded';
 
-    /** The reduce flag was changed */
+    /** The pivot mode flag was changed */
     public static EVENT_COLUMN_PIVOT_MODE_CHANGED = 'columnPivotModeChanged';
     
     /** A row group column was added, removed or order changed. */
@@ -43,20 +44,18 @@ export class Events {
     /** A row group was opened / closed */
     public static EVENT_ROW_GROUP_OPENED = 'rowGroupOpened';
 
+    /** The client has set new data into the grid */
     public static EVENT_ROW_DATA_CHANGED = 'rowDataChanged';
 
+    /** The client has set new floating data into the grid */
     public static EVENT_FLOATING_ROW_DATA_CHANGED = 'floatingRowDataChanged';
 
+    /** Range selection has changed */
     public static EVENT_RANGE_SELECTION_CHANGED = 'rangeSelectionChanged';
 
-    public static EVENT_COLUMN_ROW_GROUP_CHANGE_REQUEST = 'columnRowGroupChangeRequest';
-    public static EVENT_COLUMN_PIVOT_CHANGE_REQUEST = 'columnPivotChangeRequest';
-    public static EVENT_COLUMN_VALUE_CHANGE_REQUEST = 'columnValueChangeRequest';
-
-    public static EVENT_COLUMN_AGG_FUNC_CHANGE_REQUEST = 'columnAggFuncChangeRequest';
-
-    public static EVENT_FLASH_CELLS = 'clipboardPaste';
+    /** Model was updated - grid updates the drawn rows when this happens */
     public static EVENT_MODEL_UPDATED = 'modelUpdated';
+
     public static EVENT_CELL_CLICKED = 'cellClicked';
     public static EVENT_CELL_DOUBLE_CLICKED = 'cellDoubleClicked';
     public static EVENT_CELL_CONTEXT_MENU = 'cellContextMenu';
@@ -65,20 +64,38 @@ export class Events {
     public static EVENT_CELL_FOCUSED = 'cellFocused';
     public static EVENT_ROW_SELECTED = 'rowSelected';
     public static EVENT_SELECTION_CHANGED = 'selectionChanged';
+
+    /** 3 events for filtering. The grid LISTENS for filterChanged, and does the filter here. The before and after
+     * are for the client, if it wants to do something before or after the filter getting applied. */
     public static EVENT_BEFORE_FILTER_CHANGED = 'beforeFilterChanged';
     public static EVENT_FILTER_CHANGED = 'filterChanged';
     public static EVENT_AFTER_FILTER_CHANGED = 'afterFilterChanged';
+
+    /** Filter was change but not applied. Only useful if apply buttons are used in filters. */
     public static EVENT_FILTER_MODIFIED = 'filterModified';
+
+    /** 3 events for sorting. The grid LISTENS for sortChanged, and does the filter here. The before and after
+     * are for the client, if it wants to do something before or after the sort getting applied. */
     public static EVENT_BEFORE_SORT_CHANGED = 'beforeSortChanged';
     public static EVENT_SORT_CHANGED = 'sortChanged';
     public static EVENT_AFTER_SORT_CHANGED = 'afterSortChanged';
+
+    /** A row was removed from the dom, for any reason. Use to clean up resources (if any) used by the row. */
     public static EVENT_VIRTUAL_ROW_REMOVED = 'virtualRowRemoved';
+
     public static EVENT_ROW_CLICKED = 'rowClicked';
     public static EVENT_ROW_DOUBLE_CLICKED = 'rowDoubleClicked';
+
+    /** Gets called once after the grid has finished initialising. */
     public static EVENT_GRID_READY = 'gridReady';
+    /** Width of height of the main grid div has changed. Grid listens for this and does layout of grid if it's
+     * changed, so always filling the space it was given.  */
     public static EVENT_GRID_SIZE_CHANGED = 'gridSizeChanged';
+    /** The indexes of the rows rendered has changed, eg user has scrolled to a new vertical position. */
     public static EVENT_VIEWPORT_CHANGED = 'viewportChanged';
+    /** A column drag has started, either resizing a column or moving a column. */
     public static EVENT_DRAG_STARTED = 'dragStarted';
+    /** A column drag has stopped */
     public static EVENT_DRAG_STOPPED = 'dragStopped';
 
     public static EVENT_ROW_EDITING_STARTED = 'rowEditingStarted';
@@ -87,15 +104,31 @@ export class Events {
     public static EVENT_CELL_EDITING_STARTED = 'cellEditingStarted';
     public static EVENT_CELL_EDITING_STOPPED = 'cellEditingStopped';
 
+    /** Client added a new row. */
     public static EVENT_ITEMS_ADDED = 'itemsAdded';
+    /** Client removed a row. */
     public static EVENT_ITEMS_REMOVED = 'itemsRemoved';
 
+    /** Main body of grid has scrolled, either horizontally or vertically */
     public static EVENT_BODY_SCROLL = 'bodyScroll';
+
+    /** All items from here down are used internally by the grid, not intended for external use. */
+    public static EVENT_FLASH_CELLS = 'flashCells';
+
+    /** All the events from here down are experimental, should not be documented or used by ag-Grid customers */
 
     // not documented, as it's experimental, don't want people with dependencies on this
     public static EVENT_DISPLAYED_COLUMNS_WIDTH_CHANGED = 'displayedColumnsWidthChanged';
-
     public static EVENT_SCROLL_VISIBILITY_CHANGED = 'scrollVisibilityChanged';
+
+    // these are used for server side group and agg - only used by CS with Viewport Row Model - intention is
+    // to design these better around server side functions and then release to general public when fully working with
+    // all the row models.
+    public static EVENT_COLUMN_ROW_GROUP_CHANGE_REQUEST = 'columnRowGroupChangeRequest';
+    public static EVENT_COLUMN_PIVOT_CHANGE_REQUEST = 'columnPivotChangeRequest';
+    public static EVENT_COLUMN_VALUE_CHANGE_REQUEST = 'columnValueChangeRequest';
+    public static EVENT_COLUMN_AGG_FUNC_CHANGE_REQUEST = 'columnAggFuncChangeRequest';
+
 }
 
 export interface ModelUpdatedEvent {
