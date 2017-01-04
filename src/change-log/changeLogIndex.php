fbbd56a66a1b7add3c3120c3d5d349d039a786c3
ceolter <niall.crosby@gmail.com>
2017-01-04 11:45:02 +0000

    Bugfix: Column menu was disappearing while doing vertical scroll. Now it only disappears for horizontal scroll. This impacted when filtering changed the v scroll position (eg if everything filtered, scroll set back to top)

--- src/change-log/changeLogIndex.php
+++ src/change-log/changeLogIndex.php
@@ -13,6 +13,8 @@ include '../documentation-main/documentation_header.php';
     <li>Enhancement: richSelect cell editor now works with empty strings.</li>
     <li>Bugfix: callback processCellForClipboard was not getting used when copying just one cell (ie not a range).</li>
     <li>Bugfix: <i>gridOptions.onItemsRemoved</i> was incorrectly called <i>gridOptions.onItemsRemove</i>.</li>
+    <li>Bugfix: Column menu was disappearing while doing vertical scroll. Now it only disappears for horizontal scroll.
+        This impacted when filtering changed the v scroll position (eg if everything filtered, scroll set back to top)</li>
 
     <h2>Version 7.1.x</h2>
 
