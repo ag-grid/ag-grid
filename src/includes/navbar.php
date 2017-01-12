cfbaba4995953a4876941eedee30c639cf2d0ad0
John Masterson <johnmasterson@Johns-MacBook-Pro.local>
2017-01-12 17:26:48 +0000

    Added Try for Free button to header

--- src/includes/navbar.php
+++ src/includes/navbar.php
@@ -20,13 +20,13 @@
                     </iframe>
                 </span>
             </div>
-            <div>
+<!--             <div>
                 <h2>
                     <a class="btn btn-primary btn-large" href="mailto:accounts@ag-grid.com?Subject=ag-Grid%20Free 2 Month Trial">
                         Start Free Trial
                     </a>
                 </h2>
-            </div>
+            </div> -->
 
 
             <div class="navbar-collapse collapse pull-right col-md-5 col-sm-6 col-xs-12">
