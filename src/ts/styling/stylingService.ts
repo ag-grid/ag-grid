7083cb2ac8b5290d3942eea27ac6bcd7ed6f5001
Alberto <alberto.gutierrez.campo@gmail.com>
2017-01-23 16:55:07 +0000

    ag-33 #resolve Adding the cellClass into the excel styles

--- src/ts/styling/stylingService.ts
+++ src/ts/styling/stylingService.ts
@@ -6,14 +6,19 @@ import {ExpressionService} from "../expressionService";
 export class StylingService {
     @Autowired('expressionService') private expressionService: ExpressionService;
 
+    processAllCellClasses(colDef:ColDef, params:any, onApplicableClass:(className:string)=>void, onNotApplicableClass?:(className:string)=>void) {
+        this.processCellClassRules(colDef, params, onApplicableClass, onNotApplicableClass);
+        this.processStaticCellClasses(colDef, params, onApplicableClass)
+    }
+
     processCellClassRules(colDef:ColDef, params:any, onApplicableClass:(className:string)=>void, onNotApplicableClass?:(className:string)=>void) {
-        var classRules = colDef.cellClassRules;
+        let classRules = colDef.cellClassRules;
         if (typeof classRules === 'object' && classRules !== null) {
-            var classNames = Object.keys(classRules);
-            for (var i = 0; i < classNames.length; i++) {
-                var className = classNames[i];
-                var rule = classRules[className];
-                var resultOfRule: any;
+            let classNames = Object.keys(classRules);
+            for (let i = 0; i < classNames.length; i++) {
+                let className = classNames[i];
+                let rule = classRules[className];
+                let resultOfRule: any;
                 if (typeof rule === 'string') {
                     resultOfRule = this.expressionService.evaluate(rule, params);
                 } else if (typeof rule === 'function') {
@@ -24,6 +29,28 @@ export class StylingService {
                 } else if (onNotApplicableClass){
                     onNotApplicableClass(className);
                 }
+            }
+        }
+    }
+
+    processStaticCellClasses (colDef:ColDef, params:any, onApplicableClass:(className:string)=>void){
+        let cellClass = colDef.cellClass;
+        if (cellClass) {
+            var classOrClasses: any;
+
+            if (typeof colDef.cellClass === 'function') {
+                var cellClassFunc = <(cellClassParams: any) => string|string[]> colDef.cellClass;
+                classOrClasses = cellClassFunc(params);
+            } else {
+                classOrClasses = colDef.cellClass;
+            }
+
+            if (typeof classOrClasses === 'string') {
+                onApplicableClass(classOrClasses);
+            } else if (Array.isArray(classOrClasses)) {
+                classOrClasses.forEach( (cssClassItem: string)=> {
+                    onApplicableClass(cssClassItem);
+                });
             }
         }
     }
