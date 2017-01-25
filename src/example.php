4e9dcf27f4a41012a34642df1adead07ed295b44
ceolter <niall.crosby@gmail.com>
2017-01-25 16:45:50 +0000

    AG-168: column hover class

--- src/example.php
+++ src/example.php
@@ -42,7 +42,6 @@
             .ag-dark .bad-score { background-color: rgba(100,0,0,0.4) }
         </style>
 
-
         <!--        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
                 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
         -->
