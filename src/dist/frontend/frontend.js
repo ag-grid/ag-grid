1c68a79c4803d1b54ded7516412dff693c1bce02
Will Halling <will@Wills-MacBook-Pro.local>
2017-02-03 15:25:22 +0000

    Added YouTube video to homepage & demo page.

--- src/dist/frontend/frontend.js
+++ src/dist/frontend/frontend.js
@@ -9,3 +9,16 @@ var toggleClass = function() {
 };
 
 navToggle.addEventListener('click', toggleClass, false);
+
+/*
+* video link on demo
+* if mobile view go to YouTube rather than launch modal
+* */
+var videoLinkTag = document.getElementsByClassName('videoLink');
+videoLinkTag[0].addEventListener('click', function() {
+    if (window.innerWidth <= 768) {
+        videoLinkTag[0].dataset.target = null
+        videoLinkTag[0].dataset.modal = null
+        videoLinkTag[0].dataset.toggle = null
+    }
+}, false);
--- src/home/intro2.php
+++ src/home/intro2.php
@@ -3,22 +3,28 @@
         <div class="row">
             <div class="row text-center">
                 <div class="col-md-4 col-sm-4 col-xs-12">
-                    <h3 class="text-left">For Developers</h3>
-                    <p class="text-left">
-                        Developers love ag-Grid. Once moving to ag-Grid, their lives will be easier - they'll develop applications quicker and spend more time on things that matter.
-                    </p>
+                    <div class="HomeIntro-snippet">
+                        <h3 class="text-left">For Developers</h3>
+                        <p class="text-left">
+                            Developers love ag-Grid. Once moving to ag-Grid, their lives will be easier - they'll develop applications quicker and spend more time on things that matter.
+                        </p>
+                    </div>
+                    <div class="HomeIntro-snippet">
+                        <h3 class="text-left">For Users</h3>
+                        <p class="text-left">
+                            Users will lessen the need to export to Excel and use other reporting tools. The advanced features of ag-Grid will help keep your users on your platform and get more done.
+                        </p>
+                    </div>
+                    <div class="HomeIntro-snippet">
+                        <h3 class="text-left">For Applications</h3>
+                        <p class="text-left">
+                            An application with ag-Grid will perform more smoothly giving your application a professional and expert feel. A great grid is the foundation of a great application.
+                        </p>
+                    </div>
                 </div>
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <h3 class="text-left">For Users</h3>
-                    <p class="text-left">
-                        Users will lessen the need to export to Excel and use other reporting tools. The advanced features of ag-Grid will help keep your users on your platform and get more done.
-                    </p>
-                </div>
-                <div class="col-md-4 col-sm-4 col-xs-12">
-                    <h3 class="text-left">For Applications</h3>
-                    <p class="text-left">
-                        An application with ag-Grid will perform more smoothly giving your application a professional and expert feel. A great grid is the foundation of a great application.
-                    </p>
+                <div class="col-md-8 col-sm-8 col-xs-12 HomeIntro-video">
+                    <h3>Watch a demo of our features</h3>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/tsuhoLiSWmU" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
 
