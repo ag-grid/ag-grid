6e2cdfad38632ff847f6d0cbb713bf445f89576d
Sean Landsman <sean@thelandsmans.com>
2017-02-05 19:06:29 +0000

    AG-216 Add watch gulp tasks to projects for easier dev

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglifyjs');
 const rename = require('gulp-rename');
+const runSequence = require('run-sequence');
+const del = require('del');
 
 gulp.task('default',['src','exports']);
 
@@ -25,3 +27,36 @@ gulp.task('exports', () => {
         .pipe(rename('main.js'))
         .pipe(gulp.dest('./'));
 });
+
+gulp.task('clean-nm-ag-grid', () => {
+    return del(['node_modules/ag-grid', '!node_modules']);
+});
+
+gulp.task('clean-nm-ag-grid-enterprise', () => {
+    return del(['node_modules/ag-grid-enterprise', '!node_modules']);
+});
+
+gulp.task('copy-nm-dirs', (callback) => {
+    return runSequence('copy-from-ag-grid', 'copy-from-ag-grid-enterprise', 'copy-from-ag-grid-ng2', callback);
+});
+
+gulp.task('copy-from-ag-grid', () => {
+    return gulp.src(['../ag-grid/*', '../ag-grid/dist/**/*'], {base: '../ag-grid'})
+        .pipe(gulp.dest('./node_modules/ag-grid'));
+});
+
+gulp.task('copy-from-ag-grid-enterprise', () => {
+    return gulp.src(['../ag-grid-enterprise/*', '../ag-grid-enterprise/dist/**/*'], {base: '../ag-grid-enterprise'})
+        .pipe(gulp.dest('./node_modules/ag-grid-enterprise'));
+});
+
+gulp.task('copy-nm-dirs', (callback) => {
+    return runSequence('copy-from-ag-grid', 'copy-from-ag-grid-enterprise', callback);
+});
+
+gulp.task('watch', ['copy-nm-dirs'], () => {
+    gulp.watch(['../ag-grid/dist/**/*', '../ag-grid/src/**/*'], ['copy-from-ag-grid']);
+    gulp.watch(['../ag-grid-enterprise/dist/**/*', '../ag-grid-enterprise/src/**/*'], ['copy-from-ag-grid-enterprise']);
+    gulp.watch(['./src/**/*','./dist/**/*'], ['default']);
+});
+
