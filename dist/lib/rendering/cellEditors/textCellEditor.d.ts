74776db9d01f673bf887b5253b59ef17274da578
Alberto <alberto.gutierrez.campo@gmail.com>
2017-02-08 16:57:03 +0000

    reverting dist

--- dist/lib/rendering/cellEditors/textCellEditor.d.ts
+++ dist/lib/rendering/cellEditors/textCellEditor.d.ts
@@ -2,8 +2,8 @@
 // Project: http://www.ag-grid.com/
 // Definitions by: Niall Crosby <https://github.com/ceolter/>
 import { Component } from "../../widgets/component";
-import { ICellEditorComp, ICellEditorParams } from "./iCellEditor";
-export declare class TextCellEditor extends Component implements ICellEditorComp {
+import { ICellEditor, ICellEditorParams } from "./iCellEditor";
+export declare class TextCellEditor extends Component implements ICellEditor {
     private static TEMPLATE;
     private highlightAllOnFocus;
     private focusAfterAttached;
