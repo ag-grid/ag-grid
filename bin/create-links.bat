e442149611bb329918020cd8387f51fc6d035d8c
ceolter <niall.crosby@gmail.com>
2017-02-16 15:18:01 +0000

    refactoring of build

--- bin/create-links.bat
+++ bin/create-links.bat
@@ -2,9 +2,9 @@ cd ..\ag-grid-enterprise
 mkdir node_modules
 cd node_modules
 rmdir ag-grid
-mklink /J ag-grid ..\..\ag-grid
+mklink /D ag-grid ..\..\ag-grid
 cd ..\..\ag-grid-docs\src\dist
 rmdir ag-grid
-mklink /J ag-grid ..\..\..\ag-grid\dist
+mklink /D ag-grid ..\..\..\ag-grid\dist
 rmdir ag-grid-enterprise
-mklink /J ag-grid-enterprise ..\..\..\ag-grid-enterprise\dist
+mklink /D ag-grid-enterprise ..\..\..\ag-grid-enterprise\dist
