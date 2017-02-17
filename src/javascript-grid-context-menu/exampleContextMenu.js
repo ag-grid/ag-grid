43df7390bfaffa70932d37f625ad2f1978e672e2
ceolter <niall.crosby@gmail.com>
2017-02-17 14:57:24 +0000

    added more items to demo

--- src/javascript-grid-context-menu/exampleContextMenu.js
+++ src/javascript-grid-context-menu/exampleContextMenu.js
@@ -45,7 +45,12 @@ function getContextMenuItems(params) {
                 {name: 'Niall', action: function() {console.log('Niall was pressed');} },
                 {name: 'Sean', action: function() {console.log('Sean was pressed');} },
                 {name: 'John', action: function() {console.log('John was pressed');} },
-                {name: 'Alberto', action: function() {console.log('Alberto was pressed');} }
+                {name: 'Alberto', action: function() {console.log('Alberto was pressed');} },
+                {name: 'Tony', action: function() {console.log('Tony was pressed');} },
+                {name: 'Andrew', action: function() {console.log('Andrew was pressed');} },
+                {name: 'Kev', action: function() {console.log('Kev was pressed');} },
+                {name: 'Will', action: function() {console.log('Will was pressed');} },
+                {name: 'Armaan', action: function() {console.log('Armaan was pressed');} }
             ]
         }, // built in separator
         'separator',
