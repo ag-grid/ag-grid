ba6f9d4f062cd8076276d780e2c0e68db4e23ec8
Sean Landsman <sean@thelandsmans.com>
2017-02-20 14:09:09 +0000

    Update version numbers

--- src/best-angular-2-data-grid/index.php
+++ src/best-angular-2-data-grid/index.php
@@ -82,8 +82,8 @@ include '../documentation-main/documentation_header.php';
         contains the Angular component.
         <pre>"dependencies": {
     ...
-    "ag-grid": "7.2.x",
-    "ag-grid-angular": "7.2.x"
+    "ag-grid": "8.1.x",
+    "ag-grid-angular": "8.1.x"
 }</pre>
     The major and minor versions should match. Every time a new major or minor
     version of ag-Grid is released, the component will also be released. However
