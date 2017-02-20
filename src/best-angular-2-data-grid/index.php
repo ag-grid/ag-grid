0dff22b4f01043c396c517197983d9ea308fbb9b
Will Halling <will@Wills-MacBook-Pro.local>
2017-02-20 14:20:21 +0000

    Fixed naming issue with dependencies..

--- src/best-angular-2-data-grid/index.php
+++ src/best-angular-2-data-grid/index.php
@@ -81,8 +81,8 @@ include '../documentation-main/documentation_header.php';
         The ag-grid package contains the core ag-grid engine and the ag-grid-angular
         contains the Angular component.
         <pre>"dependencies": {
-    "ag-grid": "8.0.x",
-    "ag-grid-ng2": "8.0.x"
+    "ag-grid": "8.1.x",
+    "ag-grid-angular": "8.1.x"
 }</pre>
     The major and minor versions should match. Every time a new major or minor
     version of ag-Grid is released, the component will also be released. However
