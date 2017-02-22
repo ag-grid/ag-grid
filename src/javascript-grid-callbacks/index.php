ae1f6645255a32f12b2a8055511ec11f727a0e53
ceolter <niall.crosby@gmail.com>
2017-02-22 11:37:40 +0000

    AG-262: refactoring of docs - put the interfacing pieces into different sections

--- src/javascript-grid-callbacks/index.php
+++ src/javascript-grid-callbacks/index.php
@@ -86,11 +86,6 @@ include '../documentation-main/documentation_header.php';
 
     <table class="table">
         <tr>
-            <th>Attribute</th>
-            <th>Description</th>
-        </tr>
-
-        <tr>
             <th>isExternalFilterPresent()</th>
             <td>Grid calls this method to know if external filter is present.</td>
         </tr>
@@ -170,19 +165,6 @@ include '../documentation-main/documentation_header.php';
             <td>Allows overriding what document is used. Currently used by Drag and Drop (may extend to other places
                 in the future). Use this when you want the grid to use a different document than the one available
                 on the global scope. This can happen if docking out components (something which Electron supports).</td>
-        </tr>
-    </table>
-
-    <p>
-        <?php include '../enterprise.php';?>
-        &nbsp;
-        The callbacks below are available in the Enterprise version of ag-Grid.
-    </p>
-
-    <table class="table">
-        <tr>
-            <th>Attribute</th>
-            <th>Description</th>
         </tr>
         <tr>
             <th>getContextMenuItems(params)</th>
