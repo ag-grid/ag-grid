4e72e5d91aeb4eaec00bd517f0ec41605575f23c
Alberto <alberto.gutierrez.campo@gmail.com>
2017-03-01 10:15:50 +0000

    Fixing build

--- bin/release-all-branch.sh
+++ bin/release-all-branch.sh
@@ -10,5 +10,4 @@ fi
 release-just-branch.sh $1 $2 "ag-grid" "ag-grid-enterprise" "ag-grid-dev" "ag-grid-docs" "ag-grid-angular" "ag-grid-angular-example" "ag-grid-react" "ag-grid-react-example" "ag-grid-aurelia" "ag-grid-aurelia-example" "ag-grid-vue" "ag-grid-vue-example"
 release-prepare-examples.sh $1 $2
 release-docs.sh $1
-release-all-merge-locally.sh $1
 
