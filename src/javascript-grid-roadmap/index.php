6b928f6108b66d30ae7d40d940c047710abf5195
Sean Landsman <sean@thelandsmans.com>
2017-03-03 16:08:08 +0000

    doc fixes

--- src/javascript-grid-roadmap/index.php
+++ src/javascript-grid-roadmap/index.php
@@ -19,20 +19,7 @@ include '../documentation-main/documentation_header.php';
         management company in Canada.
     </p>
 
-    <!--<h2>Work in Progress Movie - Pivoting</h2>
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/jCId-Lbg_6k" frameborder="0" allowfullscreen></iframe>
-    -->
-
-<!--    <h4>Aurelia Support</h4>
-
-    <p>
-        Aurelia is getting bigger - we want to add it to our list of supported frameworks. Right now we know people
-        are already using ag-Grid with Aurelia, using either the Web Components or Plain JavaScript versions. We
-        want native support for  Aurelia out of the box. This will be in both free and enterprise versions.
-    </p>
--->
     <h4>Pinnable Filters in Tool Panel (ag-Grid enterprise)</h4>
-
     <p>
         Modern reporting tools show filters in a panel, so you can interact with a set of filters
         concurrently. ag-Grid will allow this option by having a 'pinnable' option on the filters, which
@@ -40,12 +27,25 @@ include '../documentation-main/documentation_header.php';
         column management). This will allow the tool panel to host and display multiple filters concurrently.
     </p>
 
-    <h4>General Cleanup</h4>
+    <h4>Enterprise Row Model (ag-Grid enterprise)</h4>
+    <p>
+        We'll soon be offering an Enterprise Row Model which will allow for lot's of exciting new Server Side capabilities, such as:
 
+        <ul>
+        <li>Server Side Filtering</li>
+        <li>Server Side Grouping</li>
+        <li>Server Side Pivoting</li>
+        <li>Lazy Loading of Groups</li>
+    </ul>
+    </p>
+
+    <h4>General Cleanup</h4>
     <p>
         We are constantly refactoring and making things better. Expect the whole grid to be face lifted on
         a continued basis.
     </p>
+
+    <p>For a full list of up and coming items please see our <a href="../ag-grid-pipeline/">Pipeline</a> page.</p>
 
 </div>
 
