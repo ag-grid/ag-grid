8535ce36234ab64bc11b8711f89706c24d2bdd7a
Sean Landsman <sean@thelandsmans.com>
2017-03-03 14:58:18 +0000

    Add example landing page

---
+++ src/example-landing/index.php
@@ -0,0 +1,32 @@
+<?php
+$key = "ag-Grid Examples";
+$pageTitle = "ag-Grid Examples";
+$pageDescription = "ag-Grid Examples";
+$pageKeyboards = "ag-Grid Examples";
+$pageGroup = "examples";
+include '../documentation-main/documentation_header.php';
+?>
+
+<div>
+
+    <h2><img src="../images/svg/docs/examples.svg" width="50" /> ag-Grid Examples</h2>
+
+    <p>This section lists a sub-set of the examples we offer for ag-Grid usage.</p>
+
+    <p>Please note that most documentation sections have examples that are applicable to those sections, so if you can't find a
+        specific example below, you'll probably find it in the relevant section from the left.</p>
+
+    <p>For example, if you're looking for an example for <a href="../javascript-grid-filtering/">Column Filtering</a> you would find it under Features->Column Filter.</p>
+
+    <div class="row">
+        <div class="col-sm-6">
+            <a href="../example-angular/" class="column-links column-items">Angular Examples</a>
+            <a href="../example-account-report/" class="column-links column-items">Styled Report</a>
+            <a href="../example-file-browser/" class="column-links column-items">File Browser</a>
+            <a href="../example-expressions-and-context/" class="column-links column-items">Expressions and Context</a>
+        </div>
+        <div class="col-sm-6">
+        </div>
+    </div>
+</div>
+<?php include '../documentation-main/documentation_footer.php'; ?>
