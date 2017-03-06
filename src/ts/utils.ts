721110c1025f83d33dc69927d8e420af8d74828f
Alberto <alberto.gutierrez.campo@gmail.com>
2017-03-06 11:49:18 +0100

    AG-284 #fixed Issues with the escaper the value 0 was interpreted as null

--- src/ts/utils.ts
+++ src/ts/utils.ts
@@ -882,7 +882,7 @@ export class Utils {
     }
 
     static escape (toEscape:string):string {
-        if (!toEscape) return null;
+        if (toEscape === null) return null;
         if (!toEscape.replace) return toEscape;
 
         return toEscape.replace(reUnescapedHtml, chr => HTML_ESCAPES[chr])
