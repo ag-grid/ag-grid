7873e2e46e9a8640cbab8ebbfc9b676d6d8876ce
ceolter <niall.crosby@ag-grid.com>
2017-03-06 13:30:51 +0000

    doc changes to floating filters

--- src/javascript-grid-floating-filter-component/index.php
+++ src/javascript-grid-floating-filter-component/index.php
@@ -20,13 +20,16 @@ include '../documentation-main/documentation_header.php';
     in ag-Grid check the <a href="../javascript-grid-filtering/#floatingFilter">docs for floating filters</a>.
 </p>
 
+<h3>Interface IFloatingFilter</h3>
+
 <p>
     To provide a custom floating filter, you have to provide it through the column definition property
-    floatingFilterComponent if using plain JS or floatingFilterComponentFramework if you are using your favourite framework.
+    <i>floatingFilterComponent</i> if using plain JS or <i>floatingFilterComponentFramework</i> if you
+    are using your favourite framework.
 
-    For plain JS (floatingFilterComponent) it needs to be provided in the form of a function. ag-Grid will call 'new' on
-    this function and treat the generated class instance as a floating filter component. A floating filter component
-    class can be any function /class that implements the following interface:
+    For plain JS (<i>floatingFilterComponent</i>) it needs to be provided in the form of a function.
+    ag-Grid will call 'new' on this function and treat the generated class instance as a floating filter
+    component. A floating filter component class can be any function /class that implements the following interface:
 </p>
 
 <pre>interface IFloatingFilter {
@@ -36,8 +39,8 @@ include '../documentation-main/documentation_header.php';
     init(params: IFilterFloatingParams): void;
 
     <span class="codeComment">// This is a method that ag-Grid will call every time the model from the associated rich filter
-    for this floating filter changes. Typically this would be used so that you can refresh your UI and show
-    on it a visual representation of the latest model for the filter as it is being updated somewhere else.</span>
+    // for this floating filter changes. Typically this would be used so that you can refresh your UI and show
+    // on it a visual representation of the latest model for the filter as it is being updated somewhere else.</span>
     onParentModelChanged(parentModel:any)
 
     <span class="codeComment">// Returns the dom html element for this floating filter.</span>
@@ -45,17 +48,17 @@ include '../documentation-main/documentation_header.php';
 
     <span class="codeComment">// optional methods</span>
 
-    <span class="codeComment">// Gets called when the column header section is destroyed.
-    // like column headers, the floating filter life span is only when the column is visible,
+    <span class="codeComment">// Gets called when the floating filter is destroyed.
+    // Like column headers, the floating filter life span is only when the column is visible,
     // so gets destroyed if column is made not visible or when user scrolls column out of
-    // view with horizontal scrolling</span>
+    // view with horizontal scrolling.</span>
     destroy?(): void;
 }</pre>
 
-<h4 id="ifilter-params">IFloatingFilterParams</h4>
+<h3 id="ifilter-params">IFloatingFilterParams</h3>
 
 <p>
-    The method init(params) takes a params object with the items listed below. If the user provides
+    The method <i>init(params)</i> takes a params object with the items listed below. If the user provides
     params via the <i>colDef.floatingFilterComponentParams</i> attribute, these will be additionally added to the
     params object, overriding items of the same name if a name clash exists.
 </p>
@@ -85,7 +88,17 @@ include '../documentation-main/documentation_header.php';
 
 }</pre>
 
+<h3 id="lifecycle">Floating Filter LifeCycle</h3>
 
+<p>
+    Floating filters do not contain filter state. They show the state of the actual filter. Floating
+    filters are only another GUI for the main filter. For this reason, floating filters lifecycle is
+    bound to the visibility of the column. So if you hide a column (either set not visible, or
+    horizontally scroll the column out of view) then the floating filter GUI component is destroyed.
+    If the column comes back into view, it is created again. This is different to column filters,
+    where the column filter will exist as long as the filter column exists, regardless of the columns
+    visibility.
+</p>
 
 <show-example example="exampleCustomFloatingFilter"></show-example>
 
