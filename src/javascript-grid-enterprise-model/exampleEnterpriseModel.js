86eac302c1e5a099e597e677319c77e20ed6ee44
ceolter <niall.crosby@ag-grid.com>
2017-03-07 20:18:44 +0000

    iteration of floating filter

--- src/javascript-grid-enterprise-model/exampleEnterpriseModel.js
+++ src/javascript-grid-enterprise-model/exampleEnterpriseModel.js
@@ -1,7 +1,7 @@
 var columnDefs = [
     {headerName: "Athlete", field: "athlete"},
     {headerName: "Age", field: "age"},
-    {headerName: "Country", field: "country"},
+    {headerName: "Country", field: "country", rowGroupIndex: 0, enableRowGroup: true},
     {headerName: "Year", field: "year"},
     {headerName: "Date", field: "date", width: 110},
     {headerName: "Sport", field: "sport", width: 110},
@@ -15,16 +15,54 @@ var gridOptions = {
     columnDefs: columnDefs,
     enableColResize: true,
     rowModelType: 'enterprise',
+    rowGroupPanelShow: 'always',
     debug: true
 };
 
-function EnterpriseDatasource(allData) {
-    this.allData = allData;
+var allData;
+
+function EnterpriseDatasource() {
 }
 
 EnterpriseDatasource.prototype.getRows = function(params) {
-    params.successCallback(this.allData);
+
+    // console.log('first is ', _.first(this.allData));
+    // console.log('group by is', _.groupBy(this.allData, ['country','age']));
+
+    console.log('EnterpriseDatasource.getRows: params = ', params);
+
+    getRowsFromServer(params);
 };
+
+function getRowsFromServer(params) {
+
+    var result = allData;
+    var rowGroupCols = params.rowGroupCols;
+
+    // if grouping, return the group
+    if (rowGroupCols.length > 0) {
+
+        var field = rowGroupCols[0].field;
+
+        var mappedValues = _.groupBy(this.allData, field);
+
+        var listOfKeys = Object.keys(mappedValues);
+
+        var result = [];
+
+        listOfKeys.forEach(function(key) {
+            var item = {};
+            item[field] = key;
+            result.push(item)
+        });
+
+    }
+
+    setTimeout( function() {
+        params.successCallback(result);
+    }, 1000);
+}
+
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
@@ -35,7 +73,8 @@ document.addEventListener('DOMContentLoaded', function() {
     // you will probably use a framework like JQuery, Angular or something else to do your HTTP calls.
     agGrid.simpleHttpRequest({url: '../olympicWinners.json'})
         .then( function(rows) {
-            var datasource = new EnterpriseDatasource(rows);
+            allData = rows;
+            var datasource = new EnterpriseDatasource();
             gridOptions.api.setEnterpriseDatasource(datasource);
         }
     );
