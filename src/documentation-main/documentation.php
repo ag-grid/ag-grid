0fe37fae47eb01c7b6e8af90940eaf840c84a55c
Sean Landsman <sean@thelandsmans.com>
2017-03-08 11:19:15 +0000

    add getting started links for frameworks

--- src/documentation-main/documentation.php
+++ src/documentation-main/documentation.php
@@ -20,7 +20,8 @@ include 'documentation_header.php';
             into answers, providing a grid worthy of enterprise development.
         </div>
         <div class="list-group">
-            <a href="/javascript-grid-getting-started/" class="list-group-item">
+            <div class="list-group-item">
+                <a href="/javascript-grid-getting-started/" style="color: #555;">
                 <div class="float-parent">
                     <div class="section-icon-container">
                         <img src="../images/svg/docs/getting_started.svg" width="50" />
@@ -33,7 +34,23 @@ include 'documentation_header.php';
                         to further sections to understand how particular features work.
                     </p>
                 </div>
-            </a>
+                <div style="display: flex;justify-content: space-between;padding-top: 10px">
+                    <a href="../javascript-grid-getting-started?framework=javascript" style="margin-left: -15px;color: #555;"><img style="vertical-align: middle" src="/images/javascript.png"
+                                                                                           height="25px"/>JavaScript</a>
+                    <a href="../javascript-grid-getting-started?framework=angular" style="color: #555;"><img style="vertical-align: middle" src="/images/angular2_small.png"
+                                                                                        height="25px"/>Angular</a>
+                    <a href="../javascript-grid-getting-started?framework=react" style="color: #555;"><img style="vertical-align: middle" src="/images/react_small.png"
+                                                                                       height="25px"/>React</a>
+                    <a href="../javascript-grid-getting-started?framework=vue" style="color: #555;"><img style="vertical-align: middle" src="/images/vue_small.png"
+                                                                                     height="25px"/>VueJS</a>
+                    <a href="../javascript-grid-getting-started?framework=aurelia" style="color: #555;"><img style="vertical-align: middle" src="/images/aurelia_small.png"
+                                                                                         height="25px"/>Aurelia</a>
+                    <a href="../javascript-grid-getting-started?framework=webcomponents" style="color: #555;"><img style="vertical-align: middle" src="/images/webComponents_small.png"
+                                                                                               height="25px"/>Web Components</a>
+
+                </div>
+                </a>
+            </div>
             <a href="/javascript-grid-interfacing-overview/" class="list-group-item">
                 <div class="float-parent">
                     <div class="section-icon-container">
@@ -42,9 +59,6 @@ include 'documentation_header.php';
                     <h4 class="list-group-item-heading">Interfacing</h4>
                     <p class="list-group-item-text">
 
-<!--                        Once you have <span style="font-style: italic">The Basics</span> covered, we suggest you take a look at the
-                        <a href="../javascript-grid-interfacing-overview/">Interfacing Overview</a> next.
--->
                         Lists all the configuration options (properties, events, api etc) for ag-Grid.
                         Use this as a quick reference to look all available options.
                     </p>
