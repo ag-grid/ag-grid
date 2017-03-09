d2e143bcbd9a57ba30ed7ff0f45f7dbb89faff6b
ceolter <niall.crosby@gmail.com>
2017-03-09 17:05:46 +0000

    webpack blog

---
+++ src/ag-grid-partners-with-webpack/index.php
@@ -0,0 +1,173 @@
+<?php
+$pageTitle = "ag-Grid Partners with Webpack";
+$pageDescription = "ag-Grid is an open source JavaScript data grid with an optional enterprise paid component. Webpack is an open-source collaborative effort with no revenue stream. ag-Grid is going to help WebPack by generously giving money!";
+$pageKeyboards = "ag-Grid JavaScript ";
+
+include('../includes/mediaHeader.php');
+?>
+
+<div class="row">
+    <div class="col-md-9">
+
+        <h2>ag-Grid Partners With Webpack</h2>
+
+        <div>
+            <i>Niall Crosby, 9th March 2017.</i>
+        </div>
+
+        <h3>The Coders Ultimate Fantasy</h3>
+
+        <p>
+            ag-Grid has two versions, one free and one paid. It's the paid version of ag-Grid that
+            allows ag-Grid to strive as a company and continue it's work creating the best JavaScript
+            data-grid in the world. Since launching (still less than a year ago) we have now over
+            500 paying customers and have 10% of the Fortune 500 listed as clients.
+        </p>
+
+        <p>
+            So we have managed to pull off the coders ultimate fantasy - moving an open-source project
+            into a successful commercial offering. So what do you do when you have a revenue
+            stream and feel obliged to somehow give back? Well that's easy, you continue to do what
+            people paid you to do, continue supporting and plugging away at your project! But from
+            a moral perspective, I had an idea...
+        </p>
+
+        <h3>Donations vs Company Expense</h3>
+
+        <p>
+            I met <a href="https://github.com/TheLarkInn">Sean Larkin</a>, one of the Webpack founders,
+            at ng-Vikings in Copenhagen
+            earlier this year. I have been impressed by Webpack every since coming across a few years
+            back and it was a pleasure to meet Sean and great to realise he was an absolute gentleman.
+            We had a good conversation about our respective projects and comparing how we funded them.
+        </p>
+
+        <p>
+            One thing that stood out with me was the privileged position ag-Grid was in. Because ag-Grid
+            is a data-grid, it is a component that companies can justify spending money on.
+            I think this is because you can show a data-grid to your users and get justification for spending
+            money, however it's difficult to justify a spend on tooling as your users don't care if something
+            makes developers lives easier.
+        </p>
+
+        <p>
+            When ag-Grid started off there was a donation button on the website. In the first year
+            it raised approx £800. It was great to get those donations and motivated me to continue
+            working on the project, however it was small money in comparison to a salary to allow me to
+            continue living in London. With ag-Grid Enterprise, one
+            or two licenses quickly paid more than the whole previous years donations. It was as if
+            there were people waiting in the background for the possibility to expense something
+            against their company. I presume this is
+            because it is far more likely for an ag-Grid user to convince their employers
+            to purchase ag-Grid than it is for them to donate.
+        </p>
+
+        <h3>ag-Grid Giving Back</h3>
+
+        <p>
+            So ag-Grid is in a privileged position, and with that privileged position, what could ag-Grid do
+            towards it's unrelated cousin Webpack apart form just giving them generous amounts of money
+            every month? Well not much, se we have decided to give them generous amounts of money every
+            month. One open-source project gone commercial helping out another open-source project that
+            we believe is very deserving.
+        </p>
+
+        <p>
+            So I got back in touch with Sean and said <i>"hey can our projects be partners and for us to be
+                a major source of funding for you"</i> and Sean was like <i>"well, yeah!"</i>.
+            So we have decided to start sending funds to
+            Webpack, starting at $2,500 per month and see where it takes us. So if you are a user of
+            ag-Grid or Webpack, you can be happy to know some of our revenue is making it's way back
+            to Webpack.
+        </p>
+
+        <h3>ag-Grid Giving Back</h3>
+        <p>
+            So that's it - it's simple. ag-Grid is not a big corporation (6 employees and counting) but
+            we fully appreciate our place in the JavaScript world and want to spread the ethos of what
+            we believe is our Company Social Responsibility, but instead of contributing towards our
+            local communities, we want to contribute to our friends in the online open-source community.
+            We have chosen Webpack as our first as we believe it's a great deserving project. In the
+            future, who knows where this is going, baby steps for now, but wouldn't it be great if we
+            had an ag-Grid Open Source collaborative??!!
+        </p>
+
+        <p>
+            If you are a profitable company, then I urge you to do the same. Great projects like Webpack
+            do need help and companies like us can do our bit. It is just not possible for these projects
+            to continue to survive on donations alone. If the ag-Grid
+            company had a conscience, I'm pretty sure it would be feeling pretty happy about itself right
+            now.
+        </p>
+
+        <div style="margin-top: 20px;">
+            <a href="https://twitter.com/share" class="twitter-share-button" data-url="https://www.ag-grid.com/ag-grid-goes-commercial/" data-text="ag-Grid Goes Commercial" data-via="ceolter" data-size="large">Tweet</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        </div>
+
+    </div>
+    <div class="col-md-3">
+
+        <img src="../images/ag-Grid2-200.png" style="display: inline-block; padding-bottom: 20px;"/>
+
+        <div>
+            <a href="https://twitter.com/share" class="twitter-share-button" data-url="https://www.ag-grid.com/ag-grid-goes-commercial/" data-text="ag-Grid Goes Commercial" data-via="ceolter" data-size="large">Tweet</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+        </div>
+
+        <div style="font-size: 14px; background-color: #dddddd; padding: 15px;">
+
+            <p>
+                <img src="/niall.png"/>
+            </p>
+            <p>
+                About Me
+            </p>
+            <p>
+                I have been writing software all my life! Starting with Assembly, C++ and MFC,
+                moving onto full stack Java / JSP / GWT and now focusing on full stack
+                Java / Javascript.
+            </p>
+            <p>
+                Consultant working in the financial industry, specialising in web based reporting applications.
+            </p>
+
+            <div>
+                <br/>
+                <a href="http://uk.linkedin.com/in/niallcrosby"><img src="/images/linked-in.png"/></a>
+                <br/>
+                <br/>
+                <a href="https://twitter.com/ceolter" class="twitter-follow-button" data-show-count="false" data-size="large">@ceolter</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+<hr/>
+
+<div id="disqus_thread"></div>
+<script type="text/javascript">
+    /* * * CONFIGURATION VARIABLES * * */
+    var disqus_shortname = 'aggrid';
+
+    /* * * DON'T EDIT BELOW THIS LINE * * */
+    (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
+<hr/>
+
+<footer class="license">
+    © ag-Grid Ltd 2015-2016
+</footer>
+
+<?php
+include('../includes/mediaFooter.php');
+?>
