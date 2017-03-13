c958aebcb4f457d6381d7723091a52c2c2ea6eae
Alberto <alberto.gutierrez.campo@gmail.com>
2017-03-13 11:40:09 +0000

    Improving scripts

--- bin/git-merge-branch.sh
+++ bin/git-merge-branch.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ "$#" -ne 1 ]
+if [ "$#" -ne 2 ]
   then
-    echo "You must supply the branch name"
+    echo "You must supply the branch to merge from and the branch to merge into"
     exit 1
 fi
 
 
-echo "Switching to branch" $1
+echo "Merging $1 into $2"
 
 ## for all the modules
 declare -a modules=("ag-grid" "ag-grid-enterprise" "ag-grid-docs" "ag-grid-dev" "ag-grid-angular" "ag-grid-angular-example" "ag-grid-react" "ag-grid-react-example" "ag-grid-aurelia" "ag-grid-aurelia-example" "ag-grid-vue" "ag-grid-vue-example")
@@ -19,9 +19,9 @@ do
     echo =============================================================================================
 
     cd $module
-    git branch $1
-    git push origin $1
-    git branch --set-upstream-to=origin/$1
-
+    git checkout $1
+    git pull
+    git checkout $2
     cd ..
+
 done
