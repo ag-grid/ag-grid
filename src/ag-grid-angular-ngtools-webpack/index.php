e08616171bc804959c26ce5d46d72d437a65ed0a
Sean Landsman <sean@thelandsmans.com>
2017-03-16 16:38:50 +0000

    AG-333 Re-write webpack docs to be from ground up to a running app

--- src/ag-grid-angular-ngtools-webpack/index.php
+++ src/ag-grid-angular-ngtools-webpack/index.php
@@ -215,9 +215,6 @@ import '@angular/platform-browser';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
-import '@angular/http';
-import '@angular/router';
-import '@angular/forms';
 
 // RxJS
 import 'rxjs';
