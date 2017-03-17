9c7146758d92a4444babb09b2618ee773172bca7
ceolter <niall.crosby@gmail.com>
2017-03-17 17:14:33 +0000

    iteration of pagination row model

--- src/ts/interfaces/iEnterpriseDatasource.ts
+++ src/ts/interfaces/iEnterpriseDatasource.ts
@@ -1,3 +1,6 @@
+// we pass a VO of the column and not the column itself,
+// so the data is read to be be converted to JSON and thrown
+// over the wire
 export interface ColumnVO {
     id: string;
     displayName: string;
@@ -6,24 +9,31 @@ export interface ColumnVO {
 }
 
 export interface IEnterpriseGetRowsRequest {
+    // columns that are currently row grouped
     rowGroupCols: ColumnVO[];
+    // columns that have aggregations on them
     valueCols: ColumnVO[];
+    // what groups the user is viewing
     groupKeys: string[];
+    // if filtering, what the filter model is
     filterModel: any;
+    // if sorting, what the sort model is
     sortModel: any;
 }
 
 export interface IEnterpriseGetRowsParams {
-    // startRow: number;
-    // endRow: number;
 
+    // details for the request,
+    request: IEnterpriseGetRowsRequest;
+
+    // success callback, pass the rows back the grid asked for
     successCallback(rowsThisPage: any[]): void;
 
+    // fail callback, tell the grid the call failed so it can adjust it's state
     failCallback(): void;
-
-    request: IEnterpriseGetRowsRequest;
 }
 
+// datasource for Enterprise Row Model
 export interface IEnterpriseDatasource {
     getRows(params: IEnterpriseGetRowsParams): void;
 }
