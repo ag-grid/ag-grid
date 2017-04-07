8ff285eb1fd64f3786d4c957dce184fc8deaaa21
John Masterson <johnmasterson@Johns-MacBook-Pro.local>
2017-04-07 15:19:44 +0100

    Changes to correct typos and add new FAQ

---
+++ src/includes/faqHeader.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+
+    <title><?php echo $pageTitle; ?></title>
+    <meta name="description" content="<?php echo $pageDescription; ?>">
+    <meta name="keywords" content="<?php echo $pageKeyboards; ?>"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="/style.css">
+
+    <link rel="shortcut icon" href="https://www.ag-grid.com/favicon.ico" />
+
+</head>
+
+<body  class="big-text">
+<!--<body ng-app="index" class="big-text">-->
+
+<?php $navKey = "about"; include 'navbar.php'; ?>
+
+<?php $headerTitle = "Frequently Asked Questions"; include 'headerRow.php'; ?>
+
+<div class="container">
