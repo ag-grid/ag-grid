ef205369dba30c0ab7c49b2e4c92b9e534821581
Alberto <alberto.gutierrez.campo@gmail.com>
2017-04-27 15:18:12 +0200

    Release 9.1.0

--- lib/agUtils.d.ts
+++ lib/agUtils.d.ts
@@ -1,2 +1,2 @@
-// ag-grid-aurelia v9.0.0
+// ag-grid-aurelia v9.1.0
 export declare function generateBindables(names: string[], bindingModeToUse?: any): any;
--- lib/editorViewModels.d.ts
+++ lib/editorViewModels.d.ts
@@ -1,4 +1,4 @@
-// ag-grid-aurelia v9.0.0
+// ag-grid-aurelia v9.1.0
 /**
  * Implements all the methods of ICellEditor except for Init
  */
