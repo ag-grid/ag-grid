c93266ead86d1e72ec997f3a0a375d0cc00a8869
Sean Landsman <sean@thelandsmans.com>
2017-04-27 16:29:45 +0100

    AG-436 Improve Docs around Angular

--- bin/dev-update-once.sh
+++ bin/dev-update-once.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd ../ag-grid
+cd ag-grid
 #./node_modules/.bin/gulp stylus
 #./node_modules/.bin/webpack
 gulp webpack
@@ -8,3 +8,5 @@ gulp webpack
 cd ../ag-grid-enterprise
 #./node_modules/.bin/webpack
 gulp webpack
+
+cd ..
