40e6c60c7f1ff9d24781219313bef1897f7feca9
niall <0872393174Aa>
2017-05-04 16:19:59 +0100

    iteration of enterprise row model

--- src/javascript-grid-enterprise-model/exampleEnterpriseSimpleJsDb.js
+++ src/javascript-grid-enterprise-model/exampleEnterpriseSimpleJsDb.js
@@ -23,8 +23,6 @@ var gridOptions = {
     debug: true
 };
 
-
-
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
     var gridDiv = document.querySelector('#myGrid');
