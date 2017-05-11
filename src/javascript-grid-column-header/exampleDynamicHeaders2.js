0e7763964253a1030e5cb1e5c4cea24453679fa1
Alberto <alberto.gutierrez.campo@gmail.com>
2017-05-11 13:01:50 +0100

    Ag-104 [fixed] Big revamp, the header heights now should work very smoothly

--- src/javascript-grid-column-header/exampleDynamicHeaders2.js
+++ src/javascript-grid-column-header/exampleDynamicHeaders2.js
@@ -2,19 +2,19 @@ var columnDefs = [
     {
         headerName: "Athlete Details",
         children: [
-            {headerName: "Athlete", field: "athlete", width: 150, suppressSizeToFit: true, enableRowGroup: true},
+            {headerName: "Athlete", field: "athlete", width: 150, suppressSizeToFit: true, enableRowGroup: true, rowGroupIndex: 0},
             {headerName: "Age", field: "age", width: 90, minwidth: 75, maxWidth: 100, enableRowGroup: true},
             {headerName: "Country", field: "country", width: 120, enableRowGroup: true},
-            {headerName: "Year", field: "year", width: 90, enableRowGroup: true},
+            {headerName: "Year", field: "year", width: 90, enableRowGroup: true, pivotIndex: 0},
             {headerName: "Sport", field: "sport", width: 110, enableRowGroup: true},
             {headerName: "Gold", field: "gold", width: 60, enableValue: true,
-                suppressMenu:true, filter:'number'},
+                suppressMenu:true, filter:'number', aggFunc: 'sum'},
             {headerName: "Silver", field: "silver", width: 60, enableValue: true,
-                suppressMenu:true, filter:'number'},
+                suppressMenu:true, filter:'number', aggFunc: 'sum'},
             {headerName: "Bronze", field: "bronze", width: 60, enableValue: true,
-                suppressMenu:true, filter:'number'},
+                suppressMenu:true, filter:'number', aggFunc: 'sum'},
             {headerName: "Total", field: "total", width: 60, enableValue: true,
-                suppressMenu:true, filter:'number'}
+                suppressMenu:true, filter:'number', aggFunc: 'sum'}
         ]
     }
 ];
@@ -40,8 +40,12 @@ function setFloatingFiltersHeight (value){
     gridOptions.api.setFloatingFiltersHeight(value);
 }
 
-function setPivotFiltersHeight (value){
-    gridOptions.api.setPivotFiltersHeight(value);
+function setPivotGroupHeaderHeight (value){
+    gridOptions.api.setPivotGroupHeaderHeight(value);
+}
+
+function setPivotHeaderHeight (value){
+    gridOptions.api.setPivotHeaderHeight(value);
 }
 
 
--- src/javascript-grid-column-header/headerHeightProperties.php
+++ src/javascript-grid-column-header/headerHeightProperties.php
@@ -9,6 +9,22 @@ $headerHeightProperties = [
     ['floatingFiltersHeight',
     '<p>The height for the row containing the floating filters. If not specified the default is 20px.</p> '],
     ['pivotHeaderHeight',
-        '<p>The height for the row containing the columns when in pivot mode. If not specified, it uses <i>headerHeight</i></p> ']
+    '<p>The height for the row containing the columns when in pivot mode. If not specified, it uses <i>headerHeight</i></p> '],
+    ['pivotGroupHeaderHeight',
+    '<p>The height for the row containing header column groups when in pivot mode. If not specified, it uses <i>groupHeaderHeight</i></p> ']
+];
+
+$headerHeightApi = [
+    ['setHeaderHeight(heightInPx)',
+        '<p>Sets the height for the row containing the column label header.</p>'
+    ],
+    ['setGroupHeaderHeight(heightInPx)',
+        '<p>Sets the height for the rows containing header column groups.</p>'],
+    ['setFloatingFiltersHeight(heightInPx)',
+        '<p>Sets the height for the row containing the floating filters.</p> '],
+    ['setPivotHeaderHeight(heightInPx)',
+        '<p>Sets the height for the row containing the columns when in pivot mode.</p> '],
+    ['setPivotGroupHeaderHeight(heightInPx)',
+        '<p>Sets the height for the row containing header column groups when in pivot mode.</p> ']
 ];
 ?>
