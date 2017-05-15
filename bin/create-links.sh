ba812c9d80636dcd720a7cf6e321894a41dc03ea
Alberto <alberto.gutierrez.campo@gmail.com>
2017-05-15 11:49:43 +0100

    Improving the scripts

--- bin/create-links.sh
+++ bin/create-links.sh
@@ -10,3 +10,8 @@ ln -s "$current_dir/ag-grid/dist" ag-grid-docs/src/dist/ag-grid
 rm -rf ag-grid-docs/src/dist/ag-grid-enterprise
 ln -s "$current_dir/ag-grid-enterprise/dist" ag-grid-docs/src/dist/ag-grid-enterprise
 
+rm -rf ag-grid-angular/node_modules/ag-grid
+rm -rf ag-grid-angular/node_modules/ag-grid-enterprise
+ln -s "$current_dir/ag-grid" ag-grid-angular/node_modules/ag-grid
+ln -s "$current_dir/ag-grid-enterprise" ag-grid-angular/node_modules/ag-grid-enterprise
+
