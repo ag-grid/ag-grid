2b7cfbcb951418038cb2fc88345236d45b7b1c23
Sean Landsman <sean@thelandsmans.com>
2017-05-18 15:42:58 +0100

    AG-472 Improve exports - remove js execution from main.js

--- main-with-styles.js
+++ main-with-styles.js
@@ -1,6 +1,6 @@
 // same as main.js, except also includes the styles, so webpack includes the css in the bundle
+exports.Grid = require('./main').Grid;
 
-require('./main');
 require('./dist/styles/ag-grid.css');
 require('./dist/styles/theme-blue.css');
 require('./dist/styles/theme-dark.css');
