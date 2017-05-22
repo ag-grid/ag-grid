b20297de6e864dc2ba769153ea7ff0b5572c9789
Alberto <alberto.gutierrez.campo@gmail.com>
2017-05-22 10:07:13 +0100

    Polishing v10.0.0 release

--- src/licenseManager.ts
+++ src/licenseManager.ts
@@ -4,7 +4,7 @@ import {MD5} from './license/md5';
 
 @Bean('licenseManager')
 export class LicenseManager {
-    private static RELEASE_INFORMATION:string = 'MTQ5MDc4OTQ1NzYzMQ==';
+    private static RELEASE_INFORMATION:string = 'MTQ5NTQ0Mzc3ODEzNg==';
     private static licenseKey:string;
 
     @Autowired('md5') private md5:MD5;
