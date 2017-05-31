26b499a592387021541b58c63afe0905c960a70d
Alberto <alberto.gutierrez.campo@gmail.com>
2017-05-31 17:13:53 +0100

    AG-496 Allow the provision of a text formatter to let any type of comparisson being made

--- src/javascript-grid-filter-text/exampleFilter.js
+++ src/javascript-grid-filter-text/exampleFilter.js
@@ -1,6 +1,21 @@
 var columnDefs = [
     {headerName: "Athlete", field: "athlete", width: 150, filter: 'text', filterParams:{
-        filterOptions:['contains', 'notContains']
+        filterOptions:['contains', 'notContains'],
+        textFormatter: function(s){
+            var r=s.toLowerCase();
+            r = r.replace(new RegExp("[àáâãäå]", 'g'),"a");
+            r = r.replace(new RegExp("æ", 'g'),"ae");
+            r = r.replace(new RegExp("ç", 'g'),"c");
+            r = r.replace(new RegExp("[èéêë]", 'g'),"e");
+            r = r.replace(new RegExp("[ìíîï]", 'g'),"i");
+            r = r.replace(new RegExp("ñ", 'g'),"n");
+            r = r.replace(new RegExp("[òóôõøö]", 'g'),"o");
+            r = r.replace(new RegExp("œ", 'g'),"oe");
+            r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
+            r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
+            r = r.replace(new RegExp("\\W", 'g'),"");
+            return r;
+        }
     }},
     {headerName: "Country", field: "country", width: 120, filterParams:{
         filterOptions:['contains'],
