99c19444988834d333307615afbd40bfc28eab41
Alberto <alberto.gutierrez.campo@gmail.com>
2017-06-13 09:58:01 +0100

    AG-513 Group sorting is broken, moving it into the model

--- src/example.js
+++ src/example.js
@@ -63,14 +63,6 @@ var groupColumn = {
     headerName: "Group",
     width: 200,
     field: 'name',
-    valueGetter: function(params) {
-        if (params.node.group) {
-            return params.node.key;
-        } else {
-            return params.data[params.colDef.field];
-        }
-    },
-    comparator: agGrid.defaultGroupComparator,
     headerCheckboxSelection: true,
     headerCheckboxSelectionFilteredOnly: true,
     cellRenderer: 'group',
