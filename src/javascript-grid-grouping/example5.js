c82e735159020e328bab0482f6c70e60d1161e3e
Alberto <alberto.gutierrez.campo@gmail.com>
2017-06-16 12:14:10 +0100

    Reverting mistake I introduced in the example

--- src/javascript-grid-grouping/example5.js
+++ src/javascript-grid-grouping/example5.js
@@ -21,7 +21,7 @@ var columnDefs = [
     {headerName: "Bronze", field: "bronze", width: 100, aggFunc: 'sum'},
     {headerName: "Total", field: "total", width: 100, aggFunc: 'sum'},
     {headerName: "Country", field: "country", width: 120, rowGroupIndex: 0},
-    {headerName: "Year", field: "year", width: 90, rowGroupIndex: 1},
+    {headerName: "Year", field: "year", width: 90},
     {headerName: "Date", field: "date", width: 110},
     {headerName: "Sport", field: "sport", width: 110}
 ];
