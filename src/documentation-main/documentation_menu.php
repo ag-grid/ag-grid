7860170194606df45abef7f5cf36c483e9a7ca4a
Sean Landsman <sean@thelandsmans.com>
2017-06-19 16:01:33 +0100

    AG-421 [rxjs] Add RxJS / Async Examples

--- src/documentation-main/documentation_menu.php
+++ src/documentation-main/documentation_menu.php
@@ -320,7 +320,7 @@
         menuItem(2, 'Angular Full Width', 'Full Width Rows', 'example-angular-full-width-rows/');
         menuItem(2, 'Angular Group Row', 'Group Rows', 'example-angular-grouped-row/');
         menuItem(2, 'Angular MasterDetail', 'Master/Detail', 'example-angular-master-detail/');
-        menuItem(2, 'Angular RxJS', 'Angular RxJS', 'example-angular-rxjs/');
+        menuItem(2, 'Angular RxJS', 'RxJS', 'example-angular-rxjs/');
         menuItem(2, 'Angular Third Party', 'Third Party', 'example-angular-third-party/');
     }
     menuItem(1, 'Styled Report', 'Styled Report', 'example-account-report/');
