b9de50891026913c6e1ed7b70658409c0e5c4514
Alberto <alberto.gutierrez.campo@gmail.com>
2017-06-23 13:42:53 +0200

    Fixing properties for release

--- bin/build-dist.sh
+++ bin/build-dist.sh
@@ -16,7 +16,7 @@ do
 
     if [ "$module" != "ag-grid" ]
     then
-        npm install "$current_dir/ag-grid/module.tgz" --no-save
+        npm install "$current_dir/ag-grid/module.tgz"
     fi
 
     cd "$current_dir"
