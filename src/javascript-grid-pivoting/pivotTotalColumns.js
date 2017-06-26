c30cc668017e8d9c730a8501aea4a1d79ed01636
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-06-26 11:05:44 +0100

    AG-551 - simplified pivot total documentation example

--- src/javascript-grid-pivoting/pivotTotalColumns.js
+++ src/javascript-grid-pivoting/pivotTotalColumns.js
@@ -2,63 +2,21 @@ var gridOptions = {
     columnDefs: [
         {headerName: "Athlete", field: "athlete",  minWidth: 150},
         {headerName: "Age", field: "age", minWidth: 50, filter: 'number'},
-        {headerName: "Country", field: "country", width: 120},
-        {headerName: "Year", field: "year", width: 90},
+        {headerName: "Country", field: "country", width: 120, rowGroup: true},
+        {headerName: "Sport", field: "sport", width: 110, pivot: true},
+        {headerName: "Year", field: "year", width: 90, pivot: true},
         {headerName: "Date", field: "date", width: 110},
-        {headerName: "Sport", field: "sport", width: 110},
         {headerName: "Gold", field: "gold", width: 150, aggFunc: 'sum'},
         {headerName: "Silver", field: "silver", width: 150, aggFunc: 'sum'},
         {headerName: "Bronze", field: "bronze", width: 150, aggFunc: 'sum'}
     ],
 
     enableColResize: true,
-    floatingFilter: true,
-    pivotTotals: true, <!-- turns on pivot total columns when pivot mode is enabled -->
-
-    onGridReady: function () {
-        initialise();
-    }
+    pivotMode: true,
+    pivotTotals: true,
+    sizeColumnsToFit: true
 };
 
-var pivotingOn = true;
-var groupOn = true;
-
-function initialise() {
-    gridOptions.api.sizeColumnsToFit();
-    gridOptions.columnApi.setPivotMode(true);
-    gridOptions.columnApi.setRowGroupColumns(['country']);
-    gridOptions.columnApi.setPivotColumns(['sport', 'year']);
-}
-
-function pivot() {
-    var pivotBtn = document.getElementById('pivotBtn');
-
-    if(pivotingOn) {
-        pivotBtn.innerHTML = 'Pivot';
-        gridOptions.columnApi.setPivotMode(false);
-    } else {
-        pivotBtn.innerHTML = 'Unpivot';
-        gridOptions.columnApi.setPivotMode(true);
-    }
-
-    pivotBtn.blur();
-    pivotingOn = !pivotingOn;
-}
-
-function group() {
-    var groupBtn = document.getElementById('groupBtn');
-
-    if(groupOn) {
-        groupBtn.innerHTML = 'Group';
-        gridOptions.columnApi.setRowGroupColumns([]);
-    } else {
-        groupBtn.innerHTML = 'Ungroup';
-        gridOptions.columnApi.setRowGroupColumns(['country']);
-    }
-
-    groupBtn.blur();
-    groupOn = !groupOn;
-}
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function () {
