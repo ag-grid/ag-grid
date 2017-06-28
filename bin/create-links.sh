8514c239e309e89a0e14cfbd821de8f5e322c2fe
Alberto <alberto.gutierrez.campo@gmail.com>
2017-06-28 17:20:35 +0100

    Improving the scripts

--- bin/create-links.sh
+++ bin/create-links.sh
@@ -1,22 +1,16 @@
 #!/usr/bin/env bash
 current_dir=$(pwd)
 
-rm -rf ag-grid-enterprise/node_modules/ag-grid
+remove-dependencies.sh
+
 ln -s "$current_dir/ag-grid" ag-grid-enterprise/node_modules/ag-grid
 
-rm -rf ag-grid-docs/src/dist/ag-grid
 ln -s "$current_dir/ag-grid/dist" ag-grid-docs/src/dist/ag-grid
-
-rm -rf ag-grid-docs/src/dist/ag-grid-enterprise
 ln -s "$current_dir/ag-grid-enterprise/dist" ag-grid-docs/src/dist/ag-grid-enterprise
 
-rm -rf ag-grid-angular/node_modules/ag-grid
-rm -rf ag-grid-angular/node_modules/ag-grid-enterprise
 ln -s "$current_dir/ag-grid" ag-grid-angular/node_modules/ag-grid
 ln -s "$current_dir/ag-grid-enterprise" ag-grid-angular/node_modules/ag-grid-enterprise
 
-rm -rf ag-grid-react/node_modules/ag-grid
-rm -rf ag-grid-react/node_modules/ag-grid-enterprise
 ln -s "$current_dir/ag-grid" ag-grid-react/node_modules/ag-grid
 ln -s "$current_dir/ag-grid-enterprise" ag-grid-react/node_modules/ag-grid-enterprise
 
---
+++ bin/remove-dependencies.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+current_dir=$(pwd)
+
+rm -rf ag-grid-enterprise/node_modules/ag-grid
+
+rm -rf ag-grid-docs/src/dist/ag-grid
+rm -rf ag-grid-docs/src/dist/ag-grid-enterprise
+
+rm -rf ag-grid-angular/node_modules/ag-grid
+rm -rf ag-grid-angular/node_modules/ag-grid-enterprise
+
+rm -rf ag-grid-angular-example/node_modules/ag-grid
+rm -rf ag-grid-angular-example/node_modules/ag-grid-enterprise
+rm -rf ag-grid-angular-example/node_modules/ag-grid-angular
+
+
+rm -rf ag-grid-react/node_modules/ag-grid
+rm -rf ag-grid-react/node_modules/ag-grid-enterprise
+
+rm -rf ag-grid-react-example/node_modules/ag-grid
+rm -rf ag-grid-react-example/node_modules/ag-grid-enterprise
+rm -rf ag-grid-react-example/node_modules/ag-grid-angular
+
