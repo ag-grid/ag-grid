4f560de9fbd5bbe1657938ea0891563034e370bf
Sean Landsman <sean@thelandsmans.com>
2017-07-06 12:25:28 +0100

    Add ng1 documentation around components

--- src/javascript-grid-components/index.php
+++ src/javascript-grid-components/index.php
@@ -7,46 +7,60 @@ $pageGroup = "components";
 include '../documentation-main/documentation_header.php';
 ?>
 
-<div>
+    <div>
 
-    <h2 id="Components">
-        <img src="../images/svg/docs/components.svg" width="50" />
-        Components
-    </h2>
+        <h2 id="Components">
+            <img src="../images/svg/docs/components.svg" width="50"/>
+            Components
+        </h2>
 
-    <p>
-        By default, the grid does not need to you to provide any components.
-        However to allow you to customise the grid, it allows you to plug
-        your own components into the grid.
-    </p>
+        <p>
+            By default, the grid does not need to you to provide any components.
+            However to allow you to customise the grid, it allows you to plug
+            your own components into the grid.
+        </p>
 
-    <p>
-        The following sections show how to use your own components inside the grid.
-        Each component can be done using plain JavaScript or using one of the
-        supported
-    </p>
+        <p>
+            The following sections show how to use your own components inside the grid.
+            Each component can be done using plain JavaScript or using one of the
+            supported
+        </p>
 
-    <h2>JavaScript or Framework</h2>
+        <h2>JavaScript or Framework</h2>
 
-    <p>
-        A component in ag-Grid means an ag-Grid component. It does not necessarily
-        mean using a component from a framework (eg a React or Angular component).
-        If you are using a framework, then you have a choice of the following:
+        <p>
+            A component in ag-Grid means an ag-Grid component. It does not necessarily
+            mean using a component from a framework (eg a React or Angular component).
+            If you are using a framework, then you have a choice of the following:
         <ol>
             <li>Provide an ag-Grid component in JavaScript.</li>
             <li>Provide an ag-Grid component as a framework component (eg React or Angular).</li>
         </ol>
         So if using a framework, you have the choice of using the framework to provide
         the component or just plain JavaScript.
-    </p>
+        </p>
 
-    <p>
-        If using a framework, the the documentation first explains how to build the
-        component using plain JavaScript and then goes onto how to use the framework
-        specific component. This is because the framework specific component builds
-        on what you learn from the JavaScript component.
-    </p>
+        <p>
+            If using a framework, the the documentation first explains how to build the
+            component using plain JavaScript and then goes onto how to use the framework
+            specific component. This is because the framework specific component builds
+            on what you learn from the JavaScript component.
+        </p>
 
-</div>
+        <?php if (isFrameworkAngular1()) { ?>
+            <h2 id="ng1Components">
+                <img src="../images/angularjs.png" style="width: 25px;"/> AngularJS 1.x
+            </h2>
+            <p>When using <a href="../javascript-grid-components">Components</a> within an AngularJS 1.x application you need to manage
+                both the scope yourself - the Grid does not provide the scope to the Components nor will it destroy it.</p>
 
-<?php include '../documentation-main/documentation_footer.php';?>
+            <p>You need to ensure provide an appropriate scope to the Components and ensure you delete it when the component is destroyed.</p>
+
+            <p><code>angularCompileRows</code>, <code>angularCompileFilters</code> and <code>angularCompileHeaders</code> are not
+                supported within Components.</p>
+
+        <?php } ?>
+
+    </div>
+
+<?php include '../documentation-main/documentation_footer.php'; ?>
