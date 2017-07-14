5da4922b7c84d16151c10f4148284988489dc2c7
niall <0872393174Aa>
2017-07-14 16:01:39 +0100

    changes for building angular

--- bin/create-links.bat
+++ bin/create-links.bat
@@ -1,11 +1,26 @@
-cd ..\ag-grid-enterprise
+cd ..
+
+cd ag-grid-enterprise
 mkdir node_modules
 cd node_modules
 rmdir ag-grid
 mklink /J ag-grid ..\..\ag-grid
-cd ..\..\ag-grid-docs\src\dist
+cd ..\..
+
+cd ag-grid-docs\src\dist
 rmdir ag-grid
 mklink /J ag-grid ..\..\..\ag-grid\dist
 rmdir ag-grid-enterprise
 mklink /J ag-grid-enterprise ..\..\..\ag-grid-enterprise\dist
-cd ..\..\..\ag-grid-dev
+cd ..\..\..
+
+cd ag-grid-angular
+mkdir node_modules
+cd node_modules
+rmdir ag-grid
+mklink /J ag-grid ..\..\ag-grid
+rmdir ag-grid-enterprise
+mklink /J ag-grid-enterprise ..\..\ag-grid-enterprise
+cd ..\..
+
+cd ag-grid-dev
