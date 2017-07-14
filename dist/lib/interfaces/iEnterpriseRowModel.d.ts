3480cc7598f17ab2bb4c541fbcd648d0fb4fbf35
Alberto <alberto.gutierrez.campo@gmail.com>
2017-07-14 15:49:48 +0100

    Restoring git-ignores

--- dist/lib/interfaces/iEnterpriseRowModel.d.ts
+++
@@ -1,8 +0,0 @@
-// Type definitions for ag-grid v12.0.0
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ceolter/>
-import { IRowModel } from "./iRowModel";
-export interface IEnterpriseRowModel extends IRowModel {
-    purgeCache(route?: string[]): void;
-    getBlockState(): any;
-}
--- dist/lib/rowModels/inMemory/filterStage.d.ts
+++
@@ -1,9 +0,0 @@
-// Type definitions for ag-grid v12.0.0
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ceolter/>
-import { IRowNodeStage, StageExecuteParams } from "../../interfaces/iRowNodeStage";
-export declare class FilterStage implements IRowNodeStage {
-    private gridOptionsWrapper;
-    private filterService;
-    execute(params: StageExecuteParams): void;
-}
