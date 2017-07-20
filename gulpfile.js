10550a58c207036b0b10b13a034879916abf823f
Alberto <alberto.gutierrez.campo@gmail.com>
2017-07-20 13:59:16 +0200

    AG-618 Fixing floating filter issue. Also allowing for gulp watch of Angular and React

--- gulpfile.js
+++ gulpfile.js
@@ -14,16 +14,23 @@ var gulpIf = require('gulp-if');
 
 gulp.task('default', ['webpack'], watchTask);
 gulp.task('watch-angular', ['webpack-angular'], watchAngularTask);
+gulp.task('watch-react', ['webpack-react'], watchReactTask);
 
 gulp.task('webpack', ['webpackEnterprise','webpackGrid'], liveReloadTask);
-gulp.task('webpack-angular', ['webpackAngular','webpackEnterprise','webpackGrid'], liveReloadTask);
+gulp.task('webpack-angular', ['tscAngularExample','webpackAngular','webpackEnterprise','webpackGrid'], liveReloadTask);
+gulp.task('webpack-react', ['webpackReact','webpackEnterprise','webpackGrid'], liveReloadTask);
+
+gulp.task('webpackReact', ['tscReact'], webpackReact);
 gulp.task('webpackAngular', ['tscAngular'], webpackAngular);
 gulp.task('webpackEnterprise', ['tscEnterprise'], webpackEnterprise);
 gulp.task('webpackGrid', ['stylusGrid','tscGrid'], webpackGrid);
 
 gulp.task('tscGrid', tscGrid);
 gulp.task('tscEnterprise', ['tscGrid'], tscEnterprise);
+gulp.task('tscReact', ['tscEnterprise'], tscAngular);
 gulp.task('tscAngular', ['tscEnterprise'], tscAngular);
+gulp.task('tscAngularExample', ['tscAngular'], tscAngularExample);
+gulp.task('tscReactExample', ['tscReact'], tscReactExample);
 
 gulp.task('stylusGrid', stylusGrid);
 
@@ -39,9 +46,27 @@ function watchTask() {
 function watchAngularTask() {
     // listen for changes with a custom port
     liveReload.listen();
-    gulp.watch(['../ag-grid/src/**/*','../ag-grid-enterprise/src/**/*','../ag-grid-angular/src/**/*'], ['webpack-angular']);
+    gulp.watch([
+        '../ag-grid/src/**/*',
+        '../ag-grid-enterprise/src/**/*',
+        '../ag-grid-angular/src/**/*',
+        '../ag-grid-angular-example/systemjs_aot/app/**/*'
+    ], ['webpack-angular']);
     gulp.watch(['../ag-grid-docs/src/**/*','!../ag-grid-docs/src/dist/**/*'], ['liveReload']);
 }
+
+function watchReactTask() {
+    // listen for changes with a custom port
+    liveReload.listen();
+    gulp.watch([
+        '../ag-grid/src/**/*',
+        '../ag-grid-enterprise/src/**/*',
+        '../ag-grid-react/src/**/*',
+        '../ag-grid-react-example/src/**/*'
+    ], ['webpack-react']);
+    gulp.watch(['../ag-grid-docs/src/**/*','!../ag-grid-docs/src/dist/**/*'], ['liveReload']);
+}
+
 
 function tscGrid() {
     var project = gulpTypescript.createProject('../ag-grid/tsconfig.json', {typescript: typescript});
@@ -92,6 +117,38 @@ function tscAngular() {
             .pipe(gulp.dest('../ag-grid-angular/dist'))
     ]);
 }
+
+
+function tscAngularExample() {
+    var project = gulpTypescript.createProject('../ag-grid-angular-example/systemjs_aot/tsconfig-gulp.json');
+
+    var tsResult = gulp
+        .src('../ag-grid-angular-example/systemjs_aot/app/boot.ts', {typescript: typescript})
+        .pipe(project());
+
+    return merge([
+        tsResult.dts
+            .pipe(gulp.dest('../ag-grid-angular-example/systemjs_aot/dist')),
+        tsResult.js
+            .pipe(gulp.dest('../ag-grid-angular-example/systemjs_aot/dist'))
+    ]);
+}
+
+function tscReactExample() {
+    var project = gulpTypescript.createProject('../ag-grid-react-example/src/tsconfig.json');
+
+    var tsResult = gulp
+        .src('../ag-grid-react-example/src/index.js', {typescript: typescript})
+        .pipe(project());
+
+    return merge([
+        tsResult.dts
+            .pipe(gulp.dest('../ag-grid-react-example/dist')),
+        tsResult.js
+            .pipe(gulp.dest('../ag-grid-react-example/dist'))
+    ]);
+}
+
 function stylusGrid() {
     // Uncompressed
     gulp.src(['../ag-grid/src/styles/*.styl', '!../ag-grid/src/styles/theme-common.styl'])
@@ -185,4 +242,29 @@ function webpackAngular() {
             }
         }))
         .pipe(gulp.dest('../ag-grid-angular/dist'));
+}
+
+function webpackReact() {
+
+    var mainFile = '../ag-grid-react/main.js';
+    var fileName = 'ag-grid-react.js';
+
+    return gulp.src('src/entry.js')
+        .pipe(webpackStream({
+            entry: {
+                main: mainFile
+            },
+            output: {
+                path: path.join(__dirname, "dist"),
+                filename: fileName,
+                library: ["agGrid"],
+                libraryTarget: "umd"
+            },
+            module: {
+                loaders: [
+                    { test: /\.css$/, loader: "style-loader!css-loader" }
+                ]
+            }
+        }))
+        .pipe(gulp.dest('../ag-grid-react/dist'));
 }
