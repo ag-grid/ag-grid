9c2fd36c9f97319479eed324bd9d2a85c3afc35f
Alberto <alberto.gutierrez.campo@gmail.com>
2017-07-26 16:48:59 +0100

    Merging 12.0.1

--- dist/lib/filter/filter.d.ts
+++
@@ -1,6 +0,0 @@
-// Type definitions for ag-grid v12.0.1
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ceolter/>
-import { IFilter } from "../interfaces/iFilter";
-export interface Filter extends IFilter {
-}
