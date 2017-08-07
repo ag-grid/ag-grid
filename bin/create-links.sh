72cce0850af242d430d1da407709ab1e4256d5bb
Petyo Ivanov <underlog@gmail.com>
2017-08-07 18:59:37 +0300

    force link re-creation

--- bin/create-links.sh
+++ bin/create-links.sh
@@ -3,21 +3,21 @@ current_dir=$(pwd)
 
 remove-dependencies.sh
 
-ln -s "$current_dir/ag-grid" ag-grid-enterprise/node_modules/ag-grid
+ln -sf "$current_dir/ag-grid" ag-grid-enterprise/node_modules/ag-grid
 
-ln -s "$current_dir/ag-grid/dist" ag-grid-docs/src/dist/ag-grid
-ln -s "$current_dir/ag-grid-enterprise/dist" ag-grid-docs/src/dist/ag-grid-enterprise
+ln -sf "$current_dir/ag-grid/dist" ag-grid-docs/src/dist/ag-grid
+ln -sf "$current_dir/ag-grid-enterprise/dist" ag-grid-docs/src/dist/ag-grid-enterprise
 
-ln -s "$current_dir/ag-grid" ag-grid-angular/node_modules/ag-grid
-ln -s "$current_dir/ag-grid-enterprise" ag-grid-angular/node_modules/ag-grid-enterprise
+ln -sf "$current_dir/ag-grid" ag-grid-angular/node_modules/ag-grid
+ln -sf "$current_dir/ag-grid-enterprise" ag-grid-angular/node_modules/ag-grid-enterprise
 
-ln -s "$current_dir/ag-grid" ag-grid-react/node_modules/ag-grid
-ln -s "$current_dir/ag-grid-enterprise" ag-grid-react/node_modules/ag-grid-enterprise
+ln -sf "$current_dir/ag-grid" ag-grid-react/node_modules/ag-grid
+ln -sf "$current_dir/ag-grid-enterprise" ag-grid-react/node_modules/ag-grid-enterprise
 
-ln -s "$current_dir/ag-grid" ag-grid-angular-example/systemjs_aot/node_modules/ag-grid
-ln -s "$current_dir/ag-grid-enterprise" ag-grid-angular-example/systemjs_aot/node_modules/ag-grid-enterprise
-ln -s "$current_dir/ag-grid-angular" ag-grid-angular-example/systemjs_aot/node_modules/ag-grid-angular
+ln -sf "$current_dir/ag-grid" ag-grid-angular-example/systemjs_aot/node_modules/ag-grid
+ln -sf "$current_dir/ag-grid-enterprise" ag-grid-angular-example/systemjs_aot/node_modules/ag-grid-enterprise
+ln -sf "$current_dir/ag-grid-angular" ag-grid-angular-example/systemjs_aot/node_modules/ag-grid-angular
 
-ln -s "$current_dir/ag-grid" ag-grid-react-example/node_modules/ag-grid
-ln -s "$current_dir/ag-grid-enterprise" ag-grid-react-example/node_modules/ag-grid-enterprise
-ln -s "$current_dir/ag-grid-react" ag-grid-react-example/node_modules/ag-grid-react
+ln -sf "$current_dir/ag-grid" ag-grid-react-example/node_modules/ag-grid
+ln -sf "$current_dir/ag-grid-enterprise" ag-grid-react-example/node_modules/ag-grid-enterprise
+ln -sf "$current_dir/ag-grid-react" ag-grid-react-example/node_modules/ag-grid-react
