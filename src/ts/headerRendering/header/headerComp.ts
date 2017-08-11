efd5d3c3b6c2fcfa05f9d2aee0bac231a984fe26
Petyo Ivanov <underlog@gmail.com>
2017-08-11 17:57:13 +0300

    a bunch of icons and whitespace tweaks

--- src/ts/headerRendering/header/headerComp.ts
+++ src/ts/headerRendering/header/headerComp.ts
@@ -40,12 +40,12 @@ export class HeaderComp extends Component implements IHeaderComp {
         '<div class="ag-cell-label-container" role="presentation">' +
         '  <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button" aria-hidden="true"></span>' +
         '  <div ref="eLabel" class="ag-header-cell-label" role="presentation">' +
+        '    <span ref="eText" class="ag-header-cell-text" role="columnheader"></span>' +
+        '    <span ref="eFilter" class="ag-header-icon ag-filter-icon" aria-hidden="true"></span>' +
         '    <span ref="eSortOrder" class="ag-header-icon ag-sort-order" aria-hidden="true"></span>' +
         '    <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon" aria-hidden="true"></span>' +
         '    <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon" aria-hidden="true"></span>' +
         '    <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon" aria-hidden="true"></span>' +
-        '    <span ref="eFilter" class="ag-header-icon ag-filter-icon" aria-hidden="true"></span>' +
-        '    <span ref="eText" class="ag-header-cell-text" role="columnheader"></span>' +
         '  </div>' +
         '</div>';
 
