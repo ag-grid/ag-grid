e6e406b2878707b047d2ac48d7ade0ce4c6ac1fc
John Masterson <johnmasterson@Johns-MacBook-Pro.local>
2017-08-15 18:13:33 +0100

    New HTML5 page

---
+++ src/html5-data-grid/index.php
@@ -0,0 +1,423 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+
+    <title>About Us</title>
+    <meta name="description" content="A feature-rich data grid designed for HTML5 Enterprise Applications. Easily integrate with your framework to deliver all the grid features that you need.">
+    <meta name="keywords" content="HTML5 Data Grid"/>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+
+    <link rel="stylesheet" type="text/css" href="/style.css">
+
+    <link rel="shortcut icon" href="https://www.ag-grid.com/favicon.ico" />
+
+</head>
+
+<body ng-app="index" class="big-text">
+
+ 
+
+<style>
+    .weekly-news-paragraph {
+        color: #767676;
+        padding-bottom: 20px;
+    }
+    .weekly-news-paragraph-title {
+        font-weight: bold;
+        color: #767676;
+        padding-bottom: 5px;
+    }
+    .weekly-news-section {
+        overflow: hidden;
+        border: 1px solid darkgrey;
+        background-color: #eee;
+        padding: 10px;
+        margin: 30px 5px 5px 5px;
+    }
+    .weekly-news-title {
+        font-size: 20px;
+        color: #167ac6;
+        float: left;
+        padding-bottom: 10px;
+    }
+    .weekly-news-sub-title {
+        float: right;
+        color: #767676;
+    }
+    .weekly-news-image-right {
+        margin-left: 15px;
+        margin-bottom: 15px;
+        font-size: 14px;
+        font-style: italic;
+        float: right;
+        width: 500px;
+    }
+    h4 {
+        margin-top: 40px;
+    }
+
+    hr {
+    height: 1px;
+    color: #9c3636;
+    background-color: #9c3636;
+    border: none;
+    }
+
+  670: .HomeSection-framework-single-line {
+  671      padding: 0 15px;
+  672      text-align: center;
+  ...
+  675  }
+
+</style>
+
+<div class="Hero">
+    <div class="Hero-grid">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-4">
+                    <h1 style="" class="big-logo">
+                        <?php include '../images/logo-dark-hacked.svg'; ?>
+                    </h1>
+                </div>
+                <div class="col-md-8">
+                    <h1 style="padding-top: 100px;">The Best HTML 5 Grid In The World</h1>
+                </div>
+            </div>
+        </div>
+
+        <!--
+                        <div class="container">
+                            <div class="row text-center">
+
+                                <h1 style="padding-bottom: 10px; padding-top: 10px;">The Best Grid In The World</h1>
+
+                            </div>
+                        </div>
+        -->
+        <div class="container">
+            <div class="row text-center">
+                <div class="col-md-4 info-item">
+                    Over <span class="info-item-big">800 Companies</span><br/>
+                    use <b>ag-Grid Enterprise</b>.
+                </div>
+                <div class="col-md-4 info-item">
+                    Over <span class="info-item-big">15%</span> of the
+                    <span class="info-item-big">Fortune 500</span><br/>
+                    use <b>ag-Grid Enterprise</b>.
+                </div>
+                <div class="col-md-4 info-item">
+                    Over <span class="info-item-big">100,000 Downloads</span>
+                    per month.
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row text-center">
+
+                <h2>
+                    <a class="btn btn-primary btn-large" href="/javascript-grid-getting-started/">
+                        Use Free Version
+                    </a>
+                    <a class="btn btn-primary btn-large" href="/start-trial.php">
+                        Trial Enterprise Version
+                    </a>
+                </h2>
+
+            </div>
+        </div>
+        <div class="container">
+            <div class="row text-center">
+                <div class="Hero-share">
+                    <a class='share-link' href="https://www.facebook.com/sharer/sharer.php?u=www.ag-grid.com"
+                       alt="Share ag-Grid on Facebook" title="Share ag-Grid on Facebook">
+                        <?php include '../images/social-icons/facebook-logo-hacked.svg'; ?>
+                    </a>
+                    <a class='share-link'
+                       href="https://twitter.com/home?status=http://www.ag-Grid.com,%20Enterprise%20Javascript%20Datagrid%20for%20serious%20enterprise%20developers%20%23aggrid%20"
+                       alt="Share ag-Grid on Twitter" title="Share ag-Grid on Twitter">
+                        <?php include '../images/social-icons/twitter-social-logotype-hacked.svg'; ?>
+                    </a>
+                    <a class='share-link' href="https://plus.google.com/share?url=www.ag-grid.com"
+                       alt="Share ag-Grid on Google Plus" title="Share ag-Grid on Google Plus">
+                        <?php include '../images/social-icons/google-plus-social-logotype-hacked.svg'; ?>
+                    </a>
+                    <a class='share-link'
+                       href="https://www.linkedin.com/shareArticle?mini=true&url=www.ag-grid.com&title=Angular%20Grid&summary=%20Enterprise%20Javascript%20Datagrid%20for%20serious%20enterprise%20developers&source="
+                       alt="Share ag-Grid on LinkedIn" title="Share ag-Grid on LinkedIn">
+                        <?php include '../images/social-icons/linkedin-logo-hacked.svg'; ?>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-12" style="padding-top: 40px;">
+            <h2>
+                The Best JavaScript Data Grid in the Word
+            </h2>
+            <hr/>
+            <p>
+                ag-Grid is a JavaScript datagrid designed for Enterprise Applications. It delivers a large feature set combined with Enterprise grade performance. We provide all the standard features of a datagrid as well as advanced features that no other product on the market offers. This means development time is significantly reduced for essential features.
+            <p/>
+            <p>
+                These features are highly extensible and customisable with detailed documentation and examples to support developers. ag-Grid is framework agnostic, we support all the major JavaScript frameworks, eight in total. This provides flexibility within organisation to choose the appropriate framework for a project.
+            <p/>
+            <p>
+                Each license comes with Technical Support from our core development team who are all experienced Enterprise Application developers. In addition, the Customer Experience team are available to assist with any other queries.
+            </p>
+
+        </div> 
+
+    </div>
+
+</div>
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-md-12" style="padding-top: 40px;">
+            <h2>
+                'AG' Stands For AGnostic - Supporting All Major JavaScript Frameworks
+            </h2>
+            <hr/>
+
+        </div> 
+
+    </div>
+        <div class="row HomeSection-framworkContainer">
+            <div class="HomeSection-framework">
+                <a href="/best-angular-2-data-grid/"><img src="../images/angular2_large.png" alt="Angular 2 Datagrid" title="Angular" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-angular-2-data-grid/">Angular</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-react-data-grid/"><img src="../images/react_large_home.png" alt="React Datagrid" title="React" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-react-data-grid/">React</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-polymer-data-grid/"><img src="../images/polymer-large.png" alt="Polymer Datagrid" title="Polymer" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-polymer-data-grid/">Polymer</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-vuejs-data-grid/"><img src="../images/vue_large_home.png" alt="VueJS Datagrid" title="VueJs" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-vuejs-data-grid/">VueJs</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-angularjs-data-grid/"><img src="../images/angularjs_large.png" alt="Angular Datagrid" title="Angular 1" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-angularjs-data-grid/">Angular 1</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-javascript-data-grid/"><img src="../images/javascript_large.png" alt="JavaScript Datagrid" title="Vanilla JavaScript" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-javascript-data-grid/">JavaScript</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-aurelia-data-grid/"><img src="../images/aurelia_large.png" alt="Aurelia Datagrid" title="Aurelia" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-aurelia-data-grid/">Aurelia</a>
+                </div>
+            </div>
+            <div class="HomeSection-framework">
+                <a href="/best-web-component-data-grid/"><img src="../images/webComponents_large.png" width="100" alt="Web Components Datagrid" title="Web Components" width="75%"/></a>
+                <div class="framework-name">
+                    <a href="/best-web-component-data-grid/">Web Components</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-12" style="padding-top: 40px;">
+
+            <h2 id="tech_team">
+                Getting Started
+            </h2>
+            <hr/>
+
+        </div> 
+
+    </div>
+
+    <div class="row" style="margin-top: 50px;">
+
+        <div class="col-md-4">
+
+            <h3>Documentation</h3>
+
+            <p>
+                Niall provides the technical vision for ag-Grid, juggling this with the usual CEO duties. 15 years of experience building Enterprise applications has given Niall unique insight into the challenges while equipping him with the technical skills to deliver the correct solutions. Niall focuses primarily on developing and maintaining the core of ag-Grid and is very much at the heartbeat of the company.
+            </p>
+        </div>
+
+        <div class="col-md-4">
+
+            <h3>Examples</h3>
+            <p>
+                Sean was the first person that Niall asked to join the team. Sean ensures that we can keep the agnostic in ag-Grid... he is responsible for integrating with all of our supported frameworks. Many of customers will be familiar with Sean as he is very active in our user forums supporting the needs of our customers. He has also recently given a number of talks at conferences where his calm manner belies his years of experience.
+            </p>
+        </div>
+
+        <div class="col-md-4">
+
+            <h3>Technical Support</h3>
+
+            <p>
+                Alberto joined the team in early 2017 and further broadens the Enterprise applications experience. With over 15 years across multiple industries, Alberto has been involved in all aspects of the software development lifecycle. He has joined Niall in enhancing and expanding the core features of the grid as well as contributing technical and industry knowledge. 
+            </p>
+        </div>
+
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+
+        <div class="col-md-12" style="padding-top: 40px;">
+
+            <h2 id="features">
+                Features
+            </h2>
+            <hr/>
+
+        </div> 
+
+    </div>
+
+    <div class="row" style="margin-top: 50px;">
+
+        <div class="col-md-3">
+            <ul>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3">
+            <ul>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3">
+            <ul>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+            </ul>
+        </div>
+
+        <div class="col-md-3">
+            <ul>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+                <li><a href="../javascript-grid-column-definitions/">Grid Size</a></li>
+            </ul>
+        </div>
+
+    </div>
+</div>
+
+
+
+    <div class="row">
+
+        <div class="col-md-12" style="padding-top: 40px;">
+
+            <h2 id="contact">
+                ag-Grid in action: Trader Dashboard
+            </h2>
+            <hr/>
+            <?php include '../home/demo-4.php'; ?>
+        </div> 
+
+    </div>
+   
+</div>
+
+</div>
+
+<?php include("../includes/footer.php"); ?>
+
+</body>
+
+<?php include_once("../includes/analytics.php"); ?>
+
+</html>
