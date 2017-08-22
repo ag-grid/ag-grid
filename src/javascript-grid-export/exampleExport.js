e94647e1badcbc28d7ab02e852494fbb8d49f7fd
Alberto <alberto.gutierrez.campo@gmail.com>
2017-08-22 15:06:17 +0100

    AG-697 Fixing issue when exporting to CSV/Excel more than two levels of header groups

--- src/javascript-grid-export/exampleExport.js
+++ src/javascript-grid-export/exampleExport.js
@@ -1,33 +1,58 @@
 var columnDefs = [{
-    headerName: 'Group1',
-    children: [
-        {headerName: "Athlete", field: "athlete", width: 150},
-        {headerName: "Age", field: "age", width: 90, cellClassRules:{
-            lessThan23IsGreen: function(params) { return params.value < 23},
-            lessThan20IsBlue: function(params) { return params.value < 20}
-        }},
-        {headerName: "Country", field: "country", width: 120},
-        {headerName: "Group", valueGetter: "data.country.charAt(0)", width: 75},
-        {headerName: "Year", field: "year", width: 75}
-    ]
-}, {
-    headerName: 'Group2',
-    children: [
-        {headerName: "Date", field: "date", width: 110},
-        {headerName: "Sport", field: "sport", width: 110},
-        {headerName: "Gold", field: "gold", width: 100},
-        {headerName: "Silver", field: "silver", width: 100},
-        {headerName: "Bronze", field: "bronze", width: 100},
-        {headerName: "Total", field: "total", width: 100}
-    ]
+    headerName: 'GroupA',
+    children: [{
+        headerName: 'Group1',
+        children: [
+            {headerName: "Group", valueGetter: "data.country.charAt(0)", width: 75},
+            {headerName: "Year", field: "year", width: 75}
+        ]
+    }, {
+        headerName: 'Group2',
+        children: [
+            {headerName: "Date", field: "date", width: 110},
+            {headerName: "Sport", field: "sport", width: 110},
+            {headerName: "Gold", field: "gold", width: 100}
+        ]
+    }]
+},{
+    headerName: 'GroupB',
+    children: [{
+        headerName: 'Group11',
+        children: [
+            {headerName: "Athlete", field: "athlete", width: 150},
+            {
+                headerName: "Age", field: "age", width: 90, cellClassRules: {
+                lessThan23IsGreen: function (params) {
+                    return params.value < 23
+                },
+                lessThan20IsBlue: function (params) {
+                    return params.value < 20
+                }
+            }
+            },
+            {headerName: "Country", field: "country", width: 120}
+        ]
+    }, {
+        headerName: 'Group2',
+        children: [
+            {headerName: "Silver", field: "silver", width: 100},
+            {headerName: "Bronze", field: "bronze", width: 100},
+            {headerName: "Total", field: "total", width: 100}
+        ]
+    }]
+
 }];
 
 
-var pinnedTopRow = { athlete: 'Floating Top Athlete', age: 999, country: 'Floating Top Country', year: 2020,
-    date: '01-08-2020', sport: 'Floating Top Sport', gold: 22, silver: 33, bronze: 44, total: 55};
+var pinnedTopRow = {
+    athlete: 'Floating Top Athlete', age: 999, country: 'Floating Top Country', year: 2020,
+    date: '01-08-2020', sport: 'Floating Top Sport', gold: 22, silver: 33, bronze: 44, total: 55
+};
 
-var pinnedBottomRow = { athlete: 'Floating Bottom Athlete', age: 888, country: 'Floating Bottom Country', year: 2030,
-    date: '01-08-2030', sport: 'Floating Bottom Sport', gold: 222, silver: 233, bronze: 244, total: 255};
+var pinnedBottomRow = {
+    athlete: 'Floating Bottom Athlete', age: 888, country: 'Floating Bottom Country', year: 2030,
+    date: '01-08-2030', sport: 'Floating Bottom Sport', gold: 222, silver: 233, bronze: 244, total: 255
+};
 
 var gridOptions = {
     columnDefs: columnDefs,
@@ -59,14 +84,14 @@ function onBtExport() {
     };
 
     if (getBooleanValue('#skipGroupR')) {
-        params.shouldRowBeSkipped = function(params) {
+        params.shouldRowBeSkipped = function (params) {
             return params.node.data.country.charAt(0) === 'R'
         };
     }
 
 
     if (getBooleanValue('#useCellCallback')) {
-        params.processCellCallback = function(params) {
+        params.processCellCallback = function (params) {
             if (params.value && params.value.toUpperCase) {
                 return params.value.toUpperCase();
             } else {
@@ -76,11 +101,11 @@ function onBtExport() {
     }
 
     if (getBooleanValue('#useSpecificColumns')) {
-        params.columnKeys = ['country','bronze'];
+        params.columnKeys = ['country', 'bronze'];
     }
 
     if (getBooleanValue('#processHeaders')) {
-        params.processHeaderCallback  = function(params) {
+        params.processHeaderCallback = function (params) {
             return params.column.getColDef().headerName.toUpperCase();
         };
     }
@@ -96,7 +121,7 @@ function onBtExport() {
 }
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 
@@ -105,7 +130,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var httpRequest = new XMLHttpRequest();
     httpRequest.open('GET', '../olympicWinners.json');
     httpRequest.send();
-    httpRequest.onreadystatechange = function() {
+    httpRequest.onreadystatechange = function () {
         if (httpRequest.readyState == 4 && httpRequest.status == 200) {
             var httpResult = JSON.parse(httpRequest.responseText);
             gridOptions.api.setRowData(httpResult);
