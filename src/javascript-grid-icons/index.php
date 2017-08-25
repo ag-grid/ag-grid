8e7be568443d7fea78e5da98017b8be837bfce9e
Petyo Ivanov <underlog@gmail.com>
2017-08-25 13:24:07 +0300

    Document changing icons through CSS/Scss

--- src/javascript-grid-icons/index.php
+++ src/javascript-grid-icons/index.php
@@ -1,7 +1,7 @@
 <?php
 $key = "Icons";
 $pageTitle = "ag-Grid Icons";
-$pageDescription = "ag-Grid comes with default icons that are created using SVG. You can provide your own icons for the grid to use.";
+$pageDescription = "ag-Grid comes with default svg icons. You can provide your own icons for the grid to use.";
 $pageKeyboards = "ag-Grid Pinning";
 $pageGroup = "feature";
 include '../documentation-main/documentation_header.php';
@@ -9,12 +9,57 @@ include '../documentation-main/documentation_header.php';
 
 <div>
 
-    <h2 id="icons">Icons</h2>
+<h2 id="icons">Icons</h2>
 
-    <p>
-        ag-Grid comes with default icons that are created using SVG. You can provide your own icons for the grid to use.
-    </p>
+<p>
+    By default, ag-Grid comes with a set of SVG icons. You can provide your own icons for the grid to use.
+</p>
 
+<h3>Change Individual Icons (CSS)</h3>
+
+<p>You can change individual icons by overriding the background images for the respective CSS selector. 
+The following code snippet overrides the fresh theme pin icon  used in the drag hint when reordering columns:<p>
+
+<pre>
+/* 
+ * The override should be placed after the import of the theme. 
+ * Alternatively, you can aso increase the selector's specifcity.
+ */
+.ag-fresh .ag-icon-pin {
+    background-image: url('path/to/my-pin-icon.svg');
+}
+</pre>
+
+<p>The icon classes follow the <code>.ag-icon-{icon-file-name}</code> convention.</p>
+
+<h3>Replace the icons by changing the icons path (Scss)</h3>
+
+<p>If you are using Sass/Scss in your project, you can include the ag-grid theme source file and customize its properties by overriding the default variables, including the path to the icons. </p>
+
+<p>The easiest way to replace the entire icon set is to change the <code>$ag-icons-path</code> Scss variable to point to a directory with your set of icons.
+The icons should be <strong>14x14px sized SVG</strong> files. You can check the <a href="https://github.com/ag-grid/ag-grid/tree/latest/src/styles/icons">full icon list in the GitHub repository</a>. 
+</p> 
+
+<pre>
+// styles.scss
+// This is an example of the application scss file; 
+// Popular framework project scaffolders like angular-cli support 
+// generating sass enabled projects. 
+// For example, the `ng new` command accepts `--style scss`.
+
+// override the icons path to a custom path
+$ag-icons-path: "./my-icons/";
+
+// import the Sass files from the ag-Grid npm package. //
+// The "~" path prefix below relies on Webpack's sass-loader -
+// https://github.com/webpack-contrib/sass-loader. 
+@import "~ag-grid/src/styles/ag-grid.scss";
+@import "~ag-grid/src/styles/theme-fresh.scss";
+</pre>
+
+<p>A working Sass / Webpack which includes the source theme file is available in the <a href="https://github.com/ag-grid/ag-grid-seed/tree/master/typescript-webpack/src">ag grid seed repository</a>.
+
+<h3>Set the icons through <code>gridOptions</code> (JavaScript)</h3>
     <p>
         The icons can either be set on the grid options (all icons) or on the column definition (all except group).
         If defined in both the grid options and column definitions, the column definition will get used. This
