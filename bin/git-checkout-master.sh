d56cccd7e4aae5c76e3fc3f83a4839ab8be284c6
Alberto <alberto.gutierrez.campo@gmail.com>
2017-09-08 09:39:09 +0100

    Releasing 13.1.1

--- bin/git-checkout-master.sh
+++ bin/git-checkout-master.sh
@@ -12,7 +12,7 @@ mkdir $1
 cd $1
 
 ## for all the modules
-declare -a modules=("ag-grid" "ag-grid-enterprise" "ag-grid-docs" "ag-grid-dev" "ag-grid-angular" "ag-grid-angular-example" "ag-grid-react" "ag-grid-react-example" "ag-grid-aurelia" "ag-grid-aurelia-example" "ag-grid-vue" "ag-grid-vue-example")
+declare -a modules=("ag-grid" "ag-grid-enterprise" "ag-grid-docs" "ag-grid-dev" "ag-grid-angular" "ag-grid-angular-example" "ag-grid-react" "ag-grid-react-example" "ag-grid-aurelia" "ag-grid-aurelia-example" "ag-grid-vue" "ag-grid-vue-example" "ag-grid-polymer" "ag-grid-polymer-example")
 
 for module in "${modules[@]}"
 do
