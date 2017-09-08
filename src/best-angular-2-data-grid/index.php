09cf0c3e06d01dc8d0db1e609cf9a7494fab2cab
Sean Landsman <sean@thelandsmans.com>
2017-09-08 09:34:32 +0100

    Improve angular docs

--- src/best-angular-2-data-grid/index.php
+++ src/best-angular-2-data-grid/index.php
@@ -14,10 +14,7 @@ include '../documentation-main/documentation_header.php';
         Angular 2+ Datagrid
     </h1>
 
-    <p>
-        Here we explain how to use ag-Grid inside an Angular 2 application.
-        It is broken down into the following sections:
-    </p>
+    <?= example('ag-Grid in Angular', 'rich-grid-example', 'angular', array( "exampleHeight" => 525, "showResult" => true, "extras" => array( "fontawesome" ) )); ?>
 
     <div class="list-group">
         <a href="/angular-getting-started/" class="list-group-item">
@@ -56,47 +53,10 @@ include '../documentation-main/documentation_header.php';
             </div>
         </a>
     </div>
+
+    <?php include '../home/features-detail.php'; ?>
 </div>
 
 
 <?php include '../documentation-main/documentation_footer.php'; ?>
 
-
-
-<?php
-/*$key = "Getting Started ng2";
-$pageTitle = "Angular Datagrid";
-$pageDescription = "A feature rich datagrid designed for Enterprise. Easily integrate with Angular 2.x to deliver filtering, grouping, aggregation, pivoting and much more.";
-$pageKeyboards = "Angular Grid";
-$pageGroup = "basics";
-include '../documentation-main/documentation_header.php';
-*/?><!--
-
-<h2>
-    <img src="../images/svg/docs/getting_started.svg" width="50" />
-    <img style="vertical-align: middle" src="../images/angular2_small.png" height="25px"/>
-    Getting Started
-</h2>
-
-<p>
-    We document (and provide full working examples) on how to get up and running with ag-Grid and Angular in the
-    quickest possible manner using any of the following build tools:
-</p>
-
-<ul>
-    <li><a href="../ag-grid-angular-angularcli">Angular CLI</a></li>
-    <li><a href="../ag-grid-angular-webpack">Webpack</a></li>
-    <li><a href="../ag-grid-angular-webpack-2">Webpack 2</a></li>
-    <li><a href="../ag-grid-angular-ngtools-webpack">@ngtools/Webpack 2</a></li>
-    <li><a href="../ag-grid-angular-systemjs">SystemJS</a></li>
-</ul>
-
-<p>You may also want to take a look at the live <a href="../example-angular">Angular Examples</a> we provide, including
-using Third-Party libraries such as <code>nrx-bootstrap</code>, <code>Material Design Components</code> and <code>ng2-typeahead</code>.</p>
-
-<note>Full working examples using all of the above build tools can be found <a href="https://github.com/ag-grid/ag-grid-angular-example">ag-grid-angular-example</a> on GitHub.</note>
-
-<p>Once you've gone through the Getting Started section of your favourite build tool, we suggest taking a look at
-    <a href="../ag-grid-next-steps">Next Steps</a> next for more detailed information on using Angular with ag-Grid.</p>
-
---><?php /*include '../documentation-main/documentation_footer.php'; */?>
