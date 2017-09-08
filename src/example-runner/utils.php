781c27db522e817f5c369b19480db5ae17d114ee
Petyo Ivanov <underlog@gmail.com>
2017-09-08 08:58:50 +0100

    Fix paths for scripts in vanilla

--- src/example-runner/utils.php
+++ src/example-runner/utils.php
@@ -1,21 +1,21 @@
 <?php
-// $$VERSION$$ gets replaced in prod
+// $$VERSION$$ gets replaced by gulp replace in prod
 define('AG_GRID_VERSION', '$$VERSION$$');
 
 if (isset($_ENV['AG_DEV'])) {
     $systemJsMap = array(
         "ag-grid" =>                "http://{$_SERVER['HTTP_HOST']}/dist/ag-grid/ag-grid.js",
-        "ag-grid-enterprise" =>     "http://{$_SERVER['HTTP_HOST']}/dist/ag-grid/ag-grid.js",
+        "ag-grid-enterprise" =>     "http://{$_SERVER['HTTP_HOST']}/dist/ag-grid-enteprise/ag-grid-enteprise.js",
         "ag-grid-react" =>          "http://{$_SERVER['HTTP_HOST']}/dist/ag-grid-react/ag-grid-react.js",
         // I can't make a bundle for angular , load it from NPM for now. This won't pick the local changes 
-        "ag-grid-angular" =>        "npm:ag-grid-angular@13.0.0/main.js"
         // "ag-grid-angular" =>        "http://{$_SERVER['HTTP_HOST']}/dist/ag-grid-angular/ag-grid-angular.js"
+        "ag-grid-angular" =>        "npm:ag-grid-angular@13.0.0/main.js"
     );
 // production mode, return from unpkg
 } else {
     $systemJsMap = array(
-        "ag-grid" =>                "npm:ag-grid@" . AG_GRID_VERSION . "/dist/ag-grid.min.js",
-        "ag-grid-enterprise" =>     "npm:ag-grid-enterprise@" . AG_GRID_VERSION . "/dist/ag-grid-enterprise.min.js",
+        "ag-grid" =>                "https://unpkg.com/ag-grid@" . AG_GRID_VERSION . "/dist/ag-grid.min.js",
+        "ag-grid-enterprise" =>     "https://unpkg.com/ag-grid-enterprise@" . AG_GRID_VERSION . "/dist/ag-grid-enterprise.min.js",
         "ag-grid-react" =>          "npm:ag-grid-react@" . AG_GRID_VERSION . "/main.js",
         "ag-grid-angular" =>        "npm:ag-grid-angular@" . AG_GRID_VERSION . "/main.js"
     );
