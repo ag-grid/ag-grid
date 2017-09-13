5da29e76de56265338ad3697be3af7b2c5619f92
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-09-13 19:33:08 +0100

    AG-518 - added plunker examples

--- src/javascript-grid-tree/index.php
+++ src/javascript-grid-tree/index.php
@@ -45,7 +45,7 @@ include '../documentation-main/documentation_header.php';
         would put aggregation values or group titles if needed.
     </p>
 
-    <show-example example="exampleTree"></show-example>
+    <?= example('Simple Tree Data', 'tree-data', 'vanilla', array("enterprise" => 1)) ?>
 
     <h2 id="file-explorer-example">Example - File Explorer</h2>
 
