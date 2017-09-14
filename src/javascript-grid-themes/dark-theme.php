2c6fcbd58a58bd9f143a9cd179435e4cca9e565d
Alberto <alberto.gutierrez.campo@gmail.com>
2017-09-14 10:02:21 +0100

    AG-518 Theme dark example

--- src/javascript-grid-themes/dark-theme.php
+++ src/javascript-grid-themes/dark-theme.php
@@ -26,7 +26,7 @@ include '../documentation-main/documentation_header.php';
     This grouped example demonstrates some of the different facets of a theme - full, part and no checkbox selection for example, as well as general look and feel
     <p/>
 
-    <show-example example="example-dark" jsfile="common"></show-example>
+    <?= example('Dark Theme', 'theme-dark') ?>
 
 </pre>
 
