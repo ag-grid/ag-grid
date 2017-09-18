e794bb8dcdb1bae62ae5fe1fbcb55b5a94cc98b5
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-09-18 15:46:18 +0100

    AG-518 - added plunker examples

--- src/javascript-grid-grouping/auto-column-group/main.js
+++ src/javascript-grid-grouping/auto-column-group/main.js
@@ -1,32 +1,24 @@
 var columnDefs = [
-    {headerName: "Athlete", field: "athlete", width: 150},
-    {headerName: "Age", field: "age", width: 90},
-    {headerName: "Country", field: "country", width: 120, rowGroup: true},
-    {headerName: "Year", field: "year", width: 90},
-    {headerName: "Date", field: "date", width: 110},
+    {headerName: "Country", field: "country", width: 120, rowGroup:true},
+    {headerName: "Year", field: "year", width: 90, rowGroup:true},
     {headerName: "Sport", field: "sport", width: 110},
+    {headerName: "Athlete", field: "athlete", width: 200},
     {headerName: "Gold", field: "gold", width: 100},
     {headerName: "Silver", field: "silver", width: 100},
     {headerName: "Bronze", field: "bronze", width: 100},
-    {headerName: "Total", field: "total", width: 100}
+    {headerName: "Total", field: "total", width: 100},
+    {headerName: "Age", field: "age", width: 90},
+    {headerName: "Date", field: "date", width: 110}
 ];
 
 var gridOptions = {
     columnDefs: columnDefs,
-    rowData: null,
     animateRows: true,
-    groupUseEntireRow: true,
-    enableSorting:true
+    enableRangeSelection: true,
+    rowData: null,
+    enableSorting:true,
+    enableFilter:true
 };
-
-function onToggleZimbabwe() {
-    gridOptions.api.forEachNode(function(node) {
-        if (node.key==='Zimbabwe') {
-            var oldValue = node.expanded;
-            node.setExpanded(!oldValue);
-        }
-    });
-}
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
@@ -35,10 +27,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // do http request to get our sample data - not using any framework to keep the example self contained.
     // you will probably use a framework like JQuery, Angular or something else to do your HTTP calls.
-    agGrid.simpleHttpRequest({url: 'https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/olympicWinnersSmall.json'})
-        .then( function(rows) {
-                gridOptions.api.setRowData(rows);
-                gridOptions.api.sizeColumnsToFit();
-            }
-        );
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', 'https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/olympicWinnersSmall.json');
+    httpRequest.send();
+    httpRequest.onreadystatechange = function() {
+        if (httpRequest.readyState === 4 && httpRequest.status === 200) {
+            var httpResult = JSON.parse(httpRequest.responseText);
+            gridOptions.api.setRowData(httpResult);
+        }
+    };
 });
