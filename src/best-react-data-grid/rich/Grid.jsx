ebc3dd931e2f44feee2d1f0e870d0b84be168a66
niall <0872393174Aa>
2017-09-18 14:49:39 +0100

    simplified react example

--- src/best-react-data-grid/rich/Grid.jsx
+++ src/best-react-data-grid/rich/Grid.jsx
@@ -15,31 +15,17 @@ export default class Grid extends Component {
             // set the row data to use inside the grid
             rowData: new SampleRowDataFactory().createRowData()
         };
-
-        this.onGridReady = this.onGridReady.bind(this);
-    }
-
-    onGridReady(params) {
-        this.api = params.api;
-        this.columnApi = params.columnApi;
     }
 
     render() {
         return (
             <div style={{height: 525, width: 900}} className="ag-fresh">
                 <AgGridReact
-                    // gridOptions is optional - it's possible to provide
-                    // all values as React props
-                    gridOptions={this.gridOptions}
-
-                    // listening for events
-                    onGridReady={this.onGridReady}
-
                     // binding to array properties
                     columnDefs={this.state.columnDefs}
                     rowData={this.state.rowData}
 
-                    // no binding, just providing hard coded strings for the properties
+                    // no simple properties
                     suppressRowClickSelection="true"
                     rowSelection="multiple"
                     enableColResize="true"
