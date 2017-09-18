f4c06e4ea5b1365f35617fc096434d61331ffbcc
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-09-18 17:24:25 +0100

    AG-518 - added plunker examples

--- src/javascript-grid-rtl/rtl-complex/main.js
+++ src/javascript-grid-rtl/rtl-complex/main.js
@@ -736,7 +736,7 @@ function ratingRendererGeneral(value, forFilter) {
     var result = '<span>';
     for (var i = 0; i < 5; i++) {
         if (value > i) {
-            result += '<img src="../../images/goldStar.png"/>';
+            result += '<img src="https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/images/goldStar.png"/>';
         }
     }
     if (forFilter && value === 0) {
