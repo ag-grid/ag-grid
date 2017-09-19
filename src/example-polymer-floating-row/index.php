f2c73a27ded330b1fbbcbed604d6456727e3c42f
Sean Landsman <sean@thelandsmans.com>
2017-09-19 13:35:01 +0100

    Polymer examples conversion

--- src/example-polymer-floating-row/index.php
+++ src/example-polymer-floating-row/index.php
@@ -1,18 +1,18 @@
 <?php
-$key = "Polymer Floating Row";
-$pageTitle = "ag-Grid Polymer Floating Row";
-$pageDescription = "ag-Grid Polymer Floating Row Example";
-$pageKeyboards = "ag-Grid polymer floating row";
+$key = "Polymer Pinned Row";
+$pageTitle = "ag-Grid Polymer Pinned Row";
+$pageDescription = "ag-Grid Polymer Pinned Row Example";
+$pageKeyboards = "ag-Grid polymer pinned row";
 $pageGroup = "examples";
 include '../documentation-main/documentation_header.php';
 ?>
 
 <div>
-    <h2>Floating Row Renderer</h2>
-    <p>A Floating Row Renderer Example</p>
+    <h2>Pinned Row Renderer</h2>
+    <p>A Pinned Row Renderer Example</p>
 
-<?= example('Floating row component', 'floating-row-component', 'polymer') ?>
+    <?= example('Pinned Row Renderer', 'floating-row-component', 'polymer', array("exampleHeight" => 360)) ?>
 </div>
 
-<?php include '../documentation-main/documentation_footer.php';?>
+<?php include '../documentation-main/documentation_footer.php'; ?>
 
