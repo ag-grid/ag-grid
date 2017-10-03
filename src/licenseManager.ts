377e5faf814aa95628041e87f3688663315738cd
Alberto <alberto.gutierrez.campo@gmail.com>
2017-10-03 15:45:43 +0200

    Release 13.3.0- Updating license

--- src/licenseManager.ts
+++ src/licenseManager.ts
@@ -4,7 +4,7 @@ import {MD5} from './license/md5';
 
 @Bean('licenseManager')
 export class LicenseManager {
-    private static RELEASE_INFORMATION:string = 'MTUwNTgxNDAzMTE4OQ==';
+    private static RELEASE_INFORMATION:string = 'MTUwNzAzODI5Mzk5Nw==';
     private static licenseKey:string;
 
     @Autowired('md5') private md5:MD5;
