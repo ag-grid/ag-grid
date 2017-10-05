0d4f1057112287cf7ec1a1581f1f3bd42a4ea2d0
Sean Landsman <sean@thelandsmans.com>
2017-10-05 15:55:46 +0100

    remove dist files

--- dist/lib/environment.d.ts
+++
@@ -1,7 +0,0 @@
-// Type definitions for ag-grid v13.3.1
-// Project: http://www.ag-grid.com/
-// Definitions by: Niall Crosby <https://github.com/ag-grid/>
-export declare class Environment {
-    private eGridDiv;
-    getTheme(): string;
-}
