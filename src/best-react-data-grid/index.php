6b1814b61a7db262b405f7ad8ad496cc5b9a7ebc
John Masterson <johnmasterson@Johns-MacBook-Pro.local>
2017-10-10 17:25:50 +0100

    Add order form change and SEO change

--- src/best-react-data-grid/index.php
+++ src/best-react-data-grid/index.php
@@ -1,7 +1,7 @@
 <?php
 $key = "Overview React";
-$pageTitle = "React Datagrid";
-$pageDescription = "A feature-rich React datagrid designed for Enterprise. Easily integrate with React and React Components to deliver all the grid features that you need.";
+$pageTitle = "React Datagrid - Excel-like React Component";
+$pageDescription = "Now supporting React 16. A feature-rich React datagrid designed for Enterprise. Easily integrate with React and React Components to deliver all the grid features that you need.";
 $pageKeyboards = "React Data Grid";
 $pageGroup = "basics";
 include '../documentation-main/documentation_header.php';
