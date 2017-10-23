9b1be9094518ff680870cfefbe2d015f0820b5f5
Petyo Ivanov <underlog@gmail.com>
2017-10-23 11:23:23 +0300

    This is not to be included till branch is merged

--- src/_assets/ts/ag-grid-enterprise.ts
+++ src/_assets/ts/ag-grid-enterprise.ts
@@ -5,7 +5,7 @@ import "../../../../ag-grid/src/styles/theme-dark.scss";
 import "../../../../ag-grid/src/styles/theme-bootstrap.scss";
 import "../../../../ag-grid/src/styles/theme-material.scss";
 import "../../../../ag-grid/src/styles/ag-theme-material.scss";
-import "../../../../ag-grid/src/styles/ag-theme-fresh.scss";
+// import "../../../../ag-grid/src/styles/ag-theme-fresh.scss";
 
 
 (<any> global).hot = true;
