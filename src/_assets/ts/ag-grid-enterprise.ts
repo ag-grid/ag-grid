0ceee827796f68e3c1b9b522e3677dedf1157c35
Petyo Ivanov <underlog@gmail.com>
2017-10-23 06:09:14 +0300

    Fix HMR for enterprise

--- src/_assets/ts/ag-grid-enterprise.ts
+++ src/_assets/ts/ag-grid-enterprise.ts
@@ -5,10 +5,11 @@ import "../../../../ag-grid/src/styles/theme-dark.scss";
 import "../../../../ag-grid/src/styles/theme-bootstrap.scss";
 import "../../../../ag-grid/src/styles/theme-material.scss";
 import "../../../../ag-grid/src/styles/ag-theme-material.scss";
+import "../../../../ag-grid/src/styles/ag-theme-fresh.scss";
 
 
 (<any> global).hot = true;
-import 'webpack-hot-middleware/client?path=/dev/ag-grid-enterprise-bundle/__webpack_hmr?reload=true';
+import 'webpack-hot-middleware/client?path=/dev/ag-grid-enterprise-bundle/__webpack_hmr&reload=true';
 
 export * from "../../../../ag-grid/exports";
 
