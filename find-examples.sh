d7847047a239db91540a2e1bf1fa08a9c9903028
Petyo Ivanov <underlog@gmail.com>
2017-10-24 18:50:30 +0300

    Script for finding examples that need fixing of grid options

---
+++ find-examples.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh 
+ag 'gridOptions' src/*/*/main.js \
+    | grep -v new.agGrid \
+    | grep -v 'var gridOptions' \
+    | grep -v 'let gridOptions' \
+    | grep -v 'const gridOptions' \
+    | grep -v gridOptions.api \
+    | grep -v gridOptions.columnApi \
+    | cut -d: -f 1 | uniq \
+    | grep -v angularjs \
+    | grep -v tutorials \
+    > examples-to-check.txt;
+
+cat examples-to-check.txt
