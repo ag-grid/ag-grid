44b1c4c5f574a0e58fd1e4160e54c3c03bb5c918
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-10-26 21:42:11 +0100

    AG-830 - fixes two clipboard issues; 1) can now copy a single empty cell 2) can now correctly copy a range where the first cell in range is empty.

--- src/clipboardService.ts
+++ src/clipboardService.ts
@@ -364,7 +364,9 @@ export class ClipboardService implements IClipboardService {
 
             this.copyDataToClipboard(data);
         } else {
-            this.copyDataToClipboard('');
+            // copy the new line character to clipboard instead of an empty string, as the 'execCommand' will ignore it.
+            // this behaviour is consistent with how Excel works!
+            this.copyDataToClipboard('\n');
         }
 
         let cellId = focusedCell.createId();
@@ -483,9 +485,9 @@ export class ClipboardService implements IClipboardService {
     }
 
     // From http://stackoverflow.com/questions/1293147/javascript-code-to-parse-csv-data
-    // This will parse a delimited string into an array of
-    // arrays. The default delimiter is the comma, but this
-    // can be overriden in the second argument.
+    // This will parse a delimited string into an array of arrays.
+    // Note: this code fixes an issue with the example posted on stack overflow where it doesn't correctly handle
+    // empty values in the first cell.
     private dataToArray(strData: string): string[][] {
         let delimiter = this.gridOptionsWrapper.getClipboardDeliminator();
 
@@ -506,30 +508,32 @@ export class ClipboardService implements IClipboardService {
         // a default empty first row.
         let arrData: string[][] = [[]];
 
-        // Create an array to hold our individual pattern
-        // matching groups.
-        let arrMatches: string[] = null;
+        // Create an array to hold our individual pattern matching groups.
+        let arrMatches: string[];
+
+        // Required for handling edge case on first row copy
+        let atFirstRow = true;
 
         // Keep looping over the regular expression matches
         // until we can no longer find a match.
-        while (arrMatches = objPattern.exec( strData )){
+        while (arrMatches = objPattern.exec( strData )) {
 
             // Get the delimiter that was found.
             let strMatchedDelimiter = arrMatches[ 1 ];
+
+            // Handles case when first row is an empty cell, insert an empty string before delimiter
+            if (atFirstRow && strMatchedDelimiter) {
+                arrData[0].push("");
+            }
 
             // Check to see if the given delimiter has a length
             // (is not the start of string) and if it matches
             // field delimiter. If id does not, then we know
             // that this delimiter is a row delimiter.
-            if (
-                strMatchedDelimiter.length &&
-                strMatchedDelimiter !== delimiter
-            ) {
-
+            if (strMatchedDelimiter.length && strMatchedDelimiter !== delimiter) {
                 // Since we have reached a new row of data,
                 // add an empty row to our data array.
                 arrData.push( [] );
-
             }
 
             let strMatchedValue: string;
@@ -537,26 +541,20 @@ export class ClipboardService implements IClipboardService {
             // Now that we have our delimiter out of the way,
             // let's check to see which kind of value we
             // captured (quoted or unquoted).
-            if (arrMatches[ 2 ]){
-
+            if (arrMatches[ 2 ]) {
                 // We found a quoted value. When we capture
-                // this value, unescape any double quotes.
-                strMatchedValue = arrMatches[ 2 ].replace(
-                    new RegExp( "\"\"", "g" ),
-                    "\""
-                );
-
+                // this value, unescaped any double quotes.
+                strMatchedValue = arrMatches[ 2 ].replace(new RegExp( "\"\"", "g" ), "\"");
             } else {
-
                 // We found a non-quoted value.
                 strMatchedValue = arrMatches[ 3 ];
-
             }
-
 
             // Now that we have our value string, let's add
             // it to the data array.
-            arrData[ arrData.length - 1 ].push( strMatchedValue );
+            arrData[ arrData.length - 1 ].push(strMatchedValue);
+
+            atFirstRow = false;
         }
 
         // Return the parsed data.
