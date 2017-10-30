b114dd59653ed3f622682a3ddcb1f830be485303
Alberto <alberto.gutierrez.campo@gmail.com>
2017-10-30 16:29:27 +0000

    AG-875 Allowing components to be ASYNC

--- src/_assets/ts/ag-grid-react.ts
+++ src/_assets/ts/ag-grid-react.ts
@@ -5,5 +5,3 @@ if (HMR) {
 }
 
 export * from "../../../../ag-grid-react/src/agGridReact";
-export * from "../../../../ag-grid-react/src/reactCellEditorFactory";
-export * from "../../../../ag-grid-react/src/reactFilterFactory";
