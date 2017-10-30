e44cf16771b2e7313d47dc8d1f7187a3f8d58d3f
Petyo Ivanov <underlog@gmail.com>
2017-10-30 10:13:43 +0200

    Fix invalid markup nesting

--- src/javascript-grid-filter-quick/index.php
+++ src/javascript-grid-filter-quick/index.php
@@ -27,6 +27,8 @@ api.setQuickFilter('new filter text');</snippet>
     instead of searchable string values. Or maybe you want to format string values for searching (eg take out
     accent characters, or take out commas from numbers). If you want to do this, then provide a <i>getQuickFilterText</i>
     to the column definition, eg:
+</p>
+
 <snippet>
 colDef = {
     headerName: "D", field: "d",
@@ -34,8 +36,8 @@ colDef = {
         return params.value.name;
     }
 }</snippet>
-Params contains {value, node, data, column, colDef}.
-</p>
+<p>Params contains {value, node, data, column, colDef}.</p>
+
 <note>
     You only need to override the quick filter value if you have a problem. If you don't have a quick filter
     problem, you don't need to use it, quick filter will work 'out of the box' in most cases.
@@ -61,13 +63,13 @@ Params contains {value, node, data, column, colDef}.
 
 <h4 id="reset-quick-filters">Reset Cache Text</h4>
 
-<p>Quick filter cache text can be reset in any of the following ways:
+<p>Quick filter cache text can be reset in any of the following ways: </p>
+
 <ul>
     <li>Each rowNode has a <code>resetQuickFilterAggregateText</code> method on it - call this to reset the quick filter</li>
     <li><code>rowNode.setDataValue(colKey, newValue)</code> will also reset the quick filter</li>
     <li>Lastly, if using the grid editing features, when you update a cell, the quick filter will get reset.</li>
 </ul>
-</p>
 
 <p>
     If you are not using the cache setting, then you can ignore all this.
@@ -78,18 +80,23 @@ Params contains {value, node, data, column, colDef}.
 <p>
     The example below shows the quick filter working on different data types. Each column demonstrates something
     different as follows:
-</p><ul>
-        <li>A - Simple column, nothing complex.</li>
-        <li>B - Complex object with 'dot' in field, quick filter works fine.</li>
-        <li>C - Complex object and value getter used, again quick filter works fine.</li>
-        <li>D - Complex object, quick filter would call 'toString' on the complex object, so getQuickFilterText is provided.</li>
-        <li>E - Complex object, not getQuickFilterText provided, so the quick filter text ends up with '[object,object]' for this column.</li>
-    </ul>
+</p>
 
-    The example also demonstrates using the quick filter cache vs not using the quick filter cache.
-    The grid works very fast even when the cache is turned off - so you probably don't need it.
-    However for those with very large data sets (eg over 10,000 rows), turning the cache on will
-    improve quick filter speed. The cache is demonstrated as follows:
+<ul>
+    <li>A - Simple column, nothing complex.</li>
+    <li>B - Complex object with 'dot' in field, quick filter works fine.</li>
+    <li>C - Complex object and value getter used, again quick filter works fine.</li>
+    <li>D - Complex object, quick filter would call 'toString' on the complex object, so getQuickFilterText is provided.</li>
+    <li>E - Complex object, not getQuickFilterText provided, so the quick filter text ends up with '[object,object]' for this column.</li>
+</ul>
+
+    <p>
+        The example also demonstrates using the quick filter cache vs not using the quick filter cache.
+        The grid works very fast even when the cache is turned off - so you probably don't need it.
+        However for those with very large data sets (eg over 10,000 rows), turning the cache on will
+        improve quick filter speed. The cache is demonstrated as follows:
+    </p>
+
     <ul>
         <li>
             <b>Normal Quick Filter:</b> The cache is not used. Value getters are executed on each node each
@@ -104,7 +111,6 @@ Params contains {value, node, data, column, colDef}.
             for each column except E (which would be fixed by adding an appropriate getQuickFilterText method like D does).
         </li>
     </ul>
-</p>
 
 <?= example('Quick Filter', 'quick-filter', 'vanilla') ?>
 
@@ -114,10 +120,9 @@ Params contains {value, node, data, column, colDef}.
     Quick Filters only make sense with client side data (i.e. when using the In Memory row model).
     For the other row models (<a href="../javascript-grid-pagination/">pagination</a>,
     <a href="../javascript-grid-virtual-paging/">infinite scrolling</a> etc) you would need to implement your own server side sorting to
-    replicate Quick Filter functionality.</p>
-
-
+    replicate Quick Filter functionality.
 </p>
+
 
 
 <?php include '../documentation-main/documentation_footer.php';?>
