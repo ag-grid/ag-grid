6376202bcfcfeb77f220af1b923bebcbc57d55f4
Alberto <alberto.gutierrez.campo@gmail.com>
2017-10-31 17:03:55 +0000

    Release 14.0.0

---
+++ dist/ng2FrameworkFactory.d.ts
@@ -0,0 +1,12 @@
+import { NgZone, ViewContainerRef } from "@angular/core";
+import { IFrameworkFactory } from "ag-grid/main";
+import { BaseComponentFactory } from "./baseComponentFactory";
+export declare class Ng2FrameworkFactory implements IFrameworkFactory {
+    private _componentFactory;
+    private _ngZone;
+    private _viewContainerRef;
+    private _baseFrameworkFactory;
+    constructor(_componentFactory: BaseComponentFactory, _ngZone: NgZone);
+    setViewContainerRef(viewContainerRef: ViewContainerRef): void;
+    setTimeout(action: any, timeout?: any): void;
+}
