d33da5ad411f56dd2985b0786260c887468ac603
niall <0872393174Aa>
2017-11-13 14:58:41 +0000

    refactor of docs for master / detail

--- src/example-gallery/index.php
+++ src/example-gallery/index.php
@@ -9,7 +9,7 @@ include '../documentation-main/documentation_header.php';
 
 <div>
 
-    <h2 id="icons">Gallery</h2>
+    <h1 class="first-h1" id="icons">Gallery</h1>
 
     <p>
         This section of the documentation demonstrates different configurations of the grid.
@@ -17,7 +17,7 @@ include '../documentation-main/documentation_header.php';
         doesn't fit into a particular documentation section.
     </p>
 
-    <h3>Auto Height, Full Width & Pagination</h3>
+    <h2>Auto Height, Full Width & Pagination</h2>
 
     <p>
         Shows the autoHeight feature working with fullWidth and pagination.
@@ -104,6 +104,22 @@ row = {
                               }"
                           exampleheight="500px">
     </show-complex-example>
+
+    <h2>Flower Nodes</h2>
+
+    <p>
+        Version 14.2 of ag-Grid introduced full support for Master / Detail. Before this, users had
+        to implement master / detail using flower nodes. Flower nodes are now deprecated. However to
+        check for backwards compatibility, flower node examples are presented here for regression
+        testing purposes.
+    </p>
+
+    <p>
+        Below shows using flower nodes to provide a master / detail experience.
+    </p>
+
+    <?= example('Flower Nodes', 'flower-nodes', 'generated') ?>
+
 </div>
 
 <?php include '../documentation-main/documentation_footer.php';?>
--- src/javascript-grid-full-width-rows/index.php
+++ src/javascript-grid-full-width-rows/index.php
@@ -7,21 +7,45 @@ $pageGroup = "feature";
 include '../documentation-main/documentation_header.php';
 ?>
 
-
-<h2 id="full-width-rows-master-detail-grids">Full Width Rows</h2>
+<h1 class="first-h1" id="full-width-rows-master-detail-grids">Full Width Rows</h1>
 
 <p>
     Under normal operation, ag-Grid will render each row as a horizontal list of cells. Each cell in the row
     will correspond to one column definition. It is possible to switch this off and allow you to provide
-    one component to span the entire width of the grid. This is useful if you want to embed a complex component
-    inside the grid instead of rendering a list of cells. This technique can be used for displaying panels
-    of information, including forms and embedding other grids. The pattern of having grids inside grids
-    is sometimes referred to as Master Detail grids.
+    one component to span the entire width of the grid and not use columns. This is useful if you want to
+    embed a complex component inside the grid instead of rendering a list of cells. This technique can be
+    used for displaying panels of information.
 </p>
 
-<h3 id="understanding-full-width">
+<h3>Simple Example of Full Width Rows</h3>
+
+<p>
+    Below shows a simple example using full width. The following can be noted:
+    <ul>
+        <li>
+            The rows for countries France, Italy and Peru have full width components
+            instead of cells.
+        </li>
+        <li>
+            Sorting and filtering all work as if the data was displayed as normal.
+        </li>
+    </ul>
+</p>
+
+<?= example('Simple Full Width', 'simple-full-width', 'generated') ?>
+
+<note>
+    You may be wondering, what is full width rows useful for? It's usage is very
+    rare and most applications will not use it. If you cannot think of a use case for it,
+    then don't worry, do not use it. Full width rows were initially introduced into ag-Grid
+    to support <a href="../javascript-grid-master-detail">Master / Detail</a> before the grid
+    was giving direct support for master / detail. Now that master / detail is directly
+    supported, the usefulness of full width is reduced.
+</note>
+
+<h1 id="understanding-full-width">
     Understanding Full Width
-</h3>
+</h1>
 
 <p>
     A fullWidth (full width) component takes up the entire width of the grid. A fullWidth component:
@@ -45,168 +69,73 @@ include '../documentation-main/documentation_header.php';
 </p>
 
 <p>
-    The <i>cellRenderer</i> can be any ag-Grid cellRenderer. Refer to <a href="../javascript-grid-cell-rendering-components/">
-        Cell Rendering</a> on how to build cellRenderers. The cellRenderer for fullWidth has
-    one difference to normal cellRenderers, that is the parameters passed are missing the value and column information
-    as the cellRenderer, by definition, is not tied to a particular column. Instead you should work off the data
-    parameter, which represents the value for the entire row.
+    The cell renderer can be any ag-Grid cell renderer. Refer to
+    <a href="../javascript-grid-cell-rendering-components/">Cell Rendering</a> on how to build cell renderers.
+    The cell renderer for fullWidth has one difference to normal cell renderers, that is the parameters passed
+    are missing the value and column information as the cellRenderer, by definition, is not tied to a particular
+    column. Instead you should work off the data parameter, which represents the value for the entire row.
 </p>
 
 <p>
-    The <i>isFullWidthCell(rowNode)</i> callback takes a rowNode as input and should return a boolean true
+    The <code>isFullWidthCell(rowNode)</code> callback takes a rowNode as input and should return a boolean true
     (use fullWidth) or false (do not use fullWidth and render as normal).
 </p>
 
 <h3 id="sorting-filtering">Sorting and Filtering</h3>
 
 <p>
-    Sorting and Filtering are NOT impacted by fullWidth. fullWidth is a rendering time feature. The sorting
+    Sorting and Filtering are NOT impacted by full width. Full width is a rendering time feature. The sorting
     and filtering applied to the data is done before rendering and is not impacted.
 </p>
 
-<h3 id="basic-fullwidth-example">Basic fullWidth Example</h3>
+<h3 id="basic-fullwidth-example">Detailed Full Width Example</h3>
 
 <p>
-    Below shows a basic fullWidth example. The example's data is minimalistic to focus on how
-    the fullWidth feature is configured. For demonstration, the pinned rows are shaded blue (with
-    fullWidth a darker shade of blue) and body fullWidth rows are green.
+    Below shows a detailed full width example including pinned rows and columns.
+    The example's data is minimalistic to focus on how
+    the full width impacts rows. For demonstration, the pinned rows are shaded blue (with
+    full width a darker shade of blue) and body full width rows are green.
     The following points should be noted:
 <ul>
     <li>
-        fullWidth can be applied to any row, including pinned rows. The example demonstrates fullWidth in
+        Full width can be applied to any row, including pinned rows. The example demonstrates full width in
         floating top, floating bottom and body rows.
     </li>
     <li>
-        fullWidth rows can be of any height, which is specified in the usual way using the <code>getRowHeight()</code>
+        Full width rows can be of any height, which is specified in the usual way using the <code>getRowHeight()</code>
         callback. The example sets body fullWidth rows to 55px.
     </li>
     <li>
-        The floating fullWidth rows are not impacted by either the vertical or horizontal scrolling.
+        The floating full width rows are not impacted by either the vertical or horizontal scrolling.
     </li>
-    <li>The body fullWidth rows are impacted by vertical scrolling only, and not the horizontal scrolling.</li>
-    <li>The fullWidth rows span the entire grid, including the pinned left and pinned right sections.</li>
-    <li>The fullWidth rows are the width of the grid, despite the grid requiring horizontal scrolling to show the cells.</li>
-    <li>The example is showing a flat list of data, there is no grouping or parent / child relationships between
-        the fullWidth and normal rows.</li>
+    <li>The body full width rows are impacted by vertical scrolling only, and not the horizontal scrolling.</li>
+    <li>The full width rows span the entire grid, including the pinned left and pinned right sections.</li>
+    <li>The full width rows are the width of the grid, despite the grid requiring horizontal scrolling to show the cells.</li>
+    <li>The example is showing a flat list of data. There is no grouping or parent / child relationships between
+        the full width and normal rows.</li>
 </ul>
 </p>
 
 <?= example('Basic Full Width', 'basic-full-width', 'generated') ?>
 
-
-<h3 id="parent-child-relationships">Parent / Child Relationships</h3>
-
-<p>
-    ag-Grid's fullWidth concept is not related to the grids expand / collapse feature, however it often makes
-    sense to use the fullWidth with parent / child relationships. You will probably want to use fullWidth either
-    with <a href="../javascript-grid-grouping/">Row Grouping</a>, <a href="../javascript-grid-tree/">Tree Data</a>
-    or Flower Nodes (explained below). It is up to you to decide what the child data is that gets passed to your
-    cellRenderer as the row data item.
-</p>
-
-<note>
-    Some other JavaScript grids cater directly for Master / Detail grids. This is not the pattern of ag-Grid. ag-Grid
-    allows you to embed another ag-Grid if you want, but does not constrain you to this. You can embed whatever
-    you like, even another vendors data grid. This control makes ag-Grid's fullWidth feature more powerful
-    than grids that cater for master / detail directly.
-</note>
-
-<h3 id="flowerNodes">Expand Groups</h3>
+<h1 id="embeddedFullWidth">Embedded Full Width vs Normal Full Width</h1>
 
 <p>
-    To have the functionality to expand a group (ie have plus (+) and minus (-) icons, you need to configure the
-    group cellRenderer on one of the column definitions. This is done as follows:
-<snippet>
-colDef.cellRenderer = 'group'</snippet>
-</p>
-
-<h3 id="flowerNodes">Flower Nodes</h3>
-
-<p>
-    You can mark a row as a 'can flower' node to tell the grid the row can be expanded even though it is not
-    a group (ie it has no children). This is useful if you have data that either a) has no grouping
-    in it but you still want to expand rows to show a master / detail panel or b) the data is already
-    grouped by another mechanism eg by using the grids internal grouping feature.
-</p>
-
-<p>
-    To tell the grid that a row 'can flower' (ie should be expandable) then implement the
-    <code>doesDataFlower(dataItem)</code> callback.
-</p>
-<snippet>
-gridOptions.doesDataFlower = function(dataItem) {
-    var canFlower = dataItem.checkSomeFlagThatYouSetOnTheData;
-    return canFlower;
-}</snippet>
-
-<p>
-    <i>doesDataFlower</i> gets called exactly once for each data item you provide to the grid. It does not
-    get called for groups when using the grids built in grouping or pivoting features.
-</p>
-
-<p>
-    A row that 'can flower' is expandable. The child row is called the 'flower row'.
-    The 'flower row':
-<ul>
-    <li>will share the same data item as its parent.</li>
-    <li>does not participate in filtering - it will show if parent is shown.</li>
-    <li>does not participate in sorting - it will always display below the parent.</li>
-</ul>
-</p>
-
-<note>
-    Flower nodes work with inMemory row model only. They do not work with virtual pagination or viewport.
-</note>
-
-<note>Why call them 'flowers'? Well the groupings are a tree structure. The tree structure contains
-    leaf nodes, that is the bottom nodes of the tree. Leafs can then in turn have flowers.</note>
-
-<h3 id="example-expanding-to-child-panels">Example - Expanding to Child Panels</h3>
-
-<p>
-    Below shows using ag-Grid to show more information about a country when you click 'expand' on a country name.
-    The following should be noted from the example:
-<ul><li><i>isFullWidthCell(rowNode)</i> callback is configured to use <i>fullWidth</i> on all child rows (that is,
-        rowNode.level === 1).</li>
-    <li><i>getRowHeight(params)</i> callback is configured to make each fullWidth panel 100px, ie bigger than
-        the normal rows.</li>
-    <li><i>doesDataFlower(dataItem)</i> is used to tell the grid which rows to expand. All rows are maked
-        expandable except Venezuela</li>
-    <li><i>fullWidthCellRenderer</i> is configured with a cellRenderer component.</li>
-    <li>The <i>consumeMouseWheelOnCenterText</i> method stops mouseWheel events getting processed by the grid
-        when the mouse is over the embedded text area. This stops both the text area AND the grid scrolling when
-        the user uses the mouse wheel.</li>
-</ul>
-</p>
-
-<note>
-    ag-Grid is programmed to have mouse wheel events on the embedded fullWidth component scroll the main body.
-    This is because the component is not part of the main body DOM, however it appears to be as far as the user
-    is concerned, so the user would expect the wheel to scroll the main body. If this is not the case, and
-    you want the wheel to to something inside your component (such as scroll the text area in the example), then
-    you need to stop the mouse wheel events from bubbling up to the grid (as done in the example for the text area).
-</note>
-
-<?= example('Full Width Component', 'full-width-component', 'generated') ?>
-
-<h3 id="embeddedFullWidth">Embedded Full Width vs Normal Full Width</h3>
-
-<p>
-    The grid uses a trick of placing the fullWidth rows in another div, outside of the main rows and cells.
-    This is what allows the fullWidth rows to span across the pinned areas. One downside of this approach
+    The grid uses a trick of placing the full width rows in another div, outside of the main rows and cells.
+    This is what allows the full width rows to span across the pinned areas. One downside of this approach
     is speed in slower browsers (eg Internet Explorer) where vertical scrolling results in a lag, where
-    the fullWidth rows scroll after the main rows scroll.
+    the full width rows scroll after the main rows scroll.
 </p>
 
 <p>
-    If you want to embed the fullWith rows with the rest of the rows, and not be impacted by the scrolling
+    If you want to embed the full with rows with the rest of the rows, and not be impacted by the scrolling
     performance issue, then set <code>embedFullWidthRows=true</code>. The example below demonstrates as follows:
 </p>
 
 <ul>
-    <li>The fullWidth rows are embedded with the main rows.</li>
-    <li>Each fullWidth row is split into the pinned areas.</li>
-    <li>Each fullWidth row horizontally scrolls with the main grid.</li>
+    <li>The full width rows are embedded with the main rows.</li>
+    <li>Each full width row is split into the pinned areas.</li>
+    <li>Each full width row horizontally scrolls with the main grid.</li>
 </ul>
 
 <?= example('Full Width Embedded', 'full-width-embedded', 'generated') ?>
