e825b16e7097fb5f73f803c663cfa05ffa09dda3
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-11-13 22:27:10 +0000

    AG-897 - converted master detail examples to vanilla rather than generated.

--- src/javascript-grid-master-detail/index.php
+++ src/javascript-grid-master-detail/index.php
@@ -43,7 +43,7 @@ include '../documentation-main/documentation_header.php';
     </ul>
 </p>
 
-<?= example('Simple Example', 'simple', 'generated', array("enterprise" => 1)) ?>
+<?= example('Simple Example', 'simple', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Using String Template</h2>
 
@@ -54,7 +54,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('String Template', 'string-template', 'generated', array("enterprise" => 1)) ?>
+<?= example('String Template', 'string-template', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Using Template Callback with Inner Grid</h2>
 
@@ -65,7 +65,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Template Callback with Inner Grid', 'template-callback-inner-grid', 'generated', array("enterprise" => 1)) ?>
+<?= example('Template Callback with Inner Grid', 'template-callback-inner-grid', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Custom Detail Component without Grid</h2>
 
@@ -76,7 +76,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Custom Detail Component without Grid', 'simple-custom-detail-component', 'generated', array("enterprise" => 1)) ?>
+<?= example('Custom Detail Component without Grid', 'simple-custom-detail-component', 'vanilla', array("enterprise" => 1)) ?>
 
 
 <h2>Example - Custom Detail Component with Grid</h2>
@@ -88,7 +88,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Custom Detail Component with Grid', 'custom-detail-with-grid', 'generated', array("enterprise" => 1)) ?>
+<?= example('Custom Detail Component with Grid', 'custom-detail-with-grid', 'vanilla', array("enterprise" => 1)) ?>
 
 
 <h2>Example - Editing Cells</h2>
@@ -100,7 +100,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Cell Editing', 'cell-editing', 'generated', array("enterprise" => 1)) ?>
+<?= example('Cell Editing', 'cell-editing', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Dynamic Master Nodes</h2>
 
@@ -111,7 +111,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Dynamic Master Nodes', 'dynamic-master-nodes', 'generated', array("enterprise" => 1)) ?>
+<?= example('Dynamic Master Nodes', 'dynamic-master-nodes', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Filtering with Sort</h2>
 
@@ -122,7 +122,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Filtering with Sort', 'filtering-with-sort', 'generated', array("enterprise" => 1)) ?>
+<?= example('Filtering with Sort', 'filtering-with-sort', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Lazy Load Rows</h2>
 
@@ -133,7 +133,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Lazy Load Rows', 'lazy-load-rows', 'generated', array("enterprise" => 1)) ?>
+<?= example('Lazy Load Rows', 'lazy-load-rows', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Multiple Levels of Master Detail</h2>
 
@@ -144,7 +144,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Multiple Levels of Master Detail', 'multiple-levels', 'generated', array("enterprise" => 1)) ?>
+<?= example('Multiple Levels of Master Detail', 'multiple-levels', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Detail Row Height</h2>
 
@@ -155,7 +155,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Detail Row Height', 'detail-row-height', 'generated', array("enterprise" => 1)) ?>
+<?= example('Detail Row Height', 'detail-row-height', 'vanilla', array("enterprise" => 1)) ?>
 
 <h2>Example - Dynamic Height</h2>
 
@@ -166,7 +166,7 @@ include '../documentation-main/documentation_header.php';
 </ul>
 </p>
 
-<?= example('Dynamic Height', 'dynamic-row-height', 'generated', array("enterprise" => 1)) ?>
+<?= example('Dynamic Height', 'dynamic-row-height', 'vanilla', array("enterprise" => 1)) ?>
 
 
 <h2>Supported Modes</h2>
@@ -229,82 +229,5 @@ include '../documentation-main/documentation_header.php';
     When using master / detail and <a href="../javascript-grid-width-and-height/#autoHeight">auto-height</a>,
     then all detail grids need to use auto-height.
 </p>
-
-<div style="border-left: 4px solid lightcoral; padding-left: 4px;">
-
-    <h3 id="example-master-detail-grids">Deprecated: Old Master Detail</h3>
-
-    <p>
-        Before v14.1 of ag-Grid, master detail was not a directly supported feature of ag-Grid.
-        Instead users of ag-Grid had to use the <a href="../javascript-grid-full-width-rows/">fullWidth</a>
-        and depreacted tree data feature
-        to achieve master detail. Below shows this depreated way, which still works, but
-        we advise against it.
-    </p>
-
-    <p>
-        The example below shows using fullWidth to provide a master / detail grid setup. The fullWidth concept
-        is used as normal, that fact that another grid instance is used in the fullWidth cellRenderer is
-        independent to the configuration of the fullWidth feature.
-    </p>
-
-    <p>
-        The example uses <i>getNodeChildDetails(dataItem)</i> callback (explained in section
-        <a href="../javascript-grid-tree/">Tree Data</a>). There is no advantage in this example to using the
-        <a href="../javascript-grid-full-width-rows/#flowerNodes">flower technique</a>, however it is presented to demonstrate an alternative.
-    </p>
-
-    <p>
-        The following should be noted from the example:
-        <ul>
-        <li>Two grid types are used, one instance of the master grid and many detail grids. There are as many
-        detail grids shown as required to render all the currently displayed detail rows.</li>
-        <li>The grid types share no configuration.</li>
-        <li>As the user scrolls up and down, the detail grids are continuously destroyed and recreated
-        in correspondence to ag-Grids row virtualisation.</li>
-        <li>The nested grids are fully featured ag-Grid's, with full support of all features. The example
-        demonstrates filtering and sorting in the detail grid.</li>
-        <li>The grids work independently of each other, for example selection in one grid does not impact
-        selection in any other grid.</li>
-        <li>In order to allow for filtering by name and account, we add to all the lear rows their parent
-            name and parent account values.</li>
-    </ul>
-    </p>
-
-    <?= example('Master Detail', 'master-detail', 'vanilla', array("enterprise" => 1)) ?>
-
-    <note>
-        The example is using parent / child relationships and has exactly one child to each parent.
-        This is why the list of phone calls is returned in <i>getNodeChildDetails()</i> and that full
-        list is the data provided to the fullWidth cellRenderer. The master grid thinks each row has
-        one child row (the fullWidth row), it is not concerned with the fact that each child row
-        in fact displays a list of child records.
-    </note>
-
-    <note>
-        ag-Grid has the concept of <a href="../javascript-grid-master-slave/">Master / Slave</a> grids which
-        is a technique for syncing column between two grids. This is not related to Master / Detail grids explained
-        here. They just have similar names.
-    </note>
-
-    <h3 id="server-requests-for-additional-data">Server Requests for Additional Data</h3>
-
-    <p>
-        The examples on this page do not contact the server for more data to display in the fullWidth components.
-        This is to keep the examples simple. On your application, there is nothing stopping you from hitting there
-        server again to get more data to display. How to do this is outside of the context of ag-Grid, however
-        maybe you would consider showing a loading spinner icon while you fetch the data in the fullWidth component.
-    </p>
-
-    <h3 id="using-fullwidth-with-pagination">Using fullWidth with Pagination, Virtual Pagination and Viewport</h3>
-
-    <p>
-        It is possible to use fullWidth with any of the row models. However grouping (and / or parent child)
-        relationships that are required to make fullWidth useful in your circumstance may not be available.
-        In other words, fullWidth works everywhere but parent / child may not. See the sections on the individual
-        row model for details.
-    </p>
-
-</div>
 
 <?php include '../documentation-main/documentation_footer.php';?>
