7db91bba84361788864c15d2c8626a6b8572fe80
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-12-05 23:17:25 +0000

    AG-1189 - refactored overlays for framework componentisation.

--- src/ts/rendering/overlayRenderers/loadingOverlayRenderer.ts
+++ src/ts/rendering/overlayRenderers/loadingOverlayRenderer.ts
@@ -3,44 +3,31 @@ import {Autowired} from "../../context/context";
 import {Component} from "../../widgets/component";
 import {IComponent} from "../../interfaces/iComponent";
 
-export interface ILoadingOverlayRenderer extends IComponent<ILoadingOverlayRendererParams> {
-}
-
 export interface ILoadingOverlayRendererParams {
     loadingRowsTemplate?: string
 }
 
+export interface ILoadingOverlayRenderer extends IComponent<ILoadingOverlayRendererParams> {}
+
 export class LoadingOverlayRenderer extends Component implements ILoadingOverlayRenderer {
-    // wrapping in outer div, and wrapper, is needed to center the loading icon
-    // The idea for centering came from here: http://www.vanseodesign.com/css/vertical-centering/
-    private static OVERLAY_TEMPLATE =
-        '<div class="ag-overlay-panel" role="presentation">' +
-        '<div class="ag-overlay-wrapper ag-overlay-loading-wrapper">[OVERLAY_TEMPLATE]</div>' +
-        '</div>';
 
     private static DEFAULT_LOADING_OVERLAY_TEMPLATE = '<span class="ag-overlay-loading-center">[LOADING...]</span>';
 
     @Autowired('gridOptionsWrapper') gridOptionsWrapper: GridOptionsWrapper;
 
     constructor() {
-        super(LoadingOverlayRenderer.OVERLAY_TEMPLATE);
+        super();
     }
 
     public init(params: ILoadingOverlayRendererParams): void {
-        let userProvidedTemplate =
-            this.gridOptionsWrapper.getOverlayLoadingTemplate() ?
-                this.gridOptionsWrapper.getOverlayLoadingTemplate() : params.loadingRowsTemplate;
-
-        let templateNotLocalised = this.getTemplate(userProvidedTemplate);
+        let template =
+            this.gridOptionsWrapper.getOverlayLoadingTemplate() ? this.gridOptionsWrapper.getOverlayLoadingTemplate() :
+                params.loadingRowsTemplate ? params.loadingRowsTemplate :
+                    LoadingOverlayRenderer.DEFAULT_LOADING_OVERLAY_TEMPLATE;
 
         let localeTextFunc = this.gridOptionsWrapper.getLocaleTextFunc();
-        let localisedTemplate = templateNotLocalised.replace('[LOADING...]', localeTextFunc('loadingOoo', 'Loading...'));
+        let localisedTemplate = template.replace('[LOADING...]', localeTextFunc('loadingOoo', 'Loading...'));
 
         this.setTemplate(localisedTemplate);
-    }
-
-    private getTemplate(userProvidedTemplate: string) {
-        return LoadingOverlayRenderer.OVERLAY_TEMPLATE.replace('[OVERLAY_TEMPLATE]',
-            userProvidedTemplate ? userProvidedTemplate : LoadingOverlayRenderer.DEFAULT_LOADING_OVERLAY_TEMPLATE);
     }
 }
