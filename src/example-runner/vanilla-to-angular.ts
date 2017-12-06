e28aa89c69ed90bd9a0b993068a27d374dba7426
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-12-06 12:20:25 +0000

    AG-1189 - updated example runner to handle both component and componentFramework examples

--- src/example-runner/vanilla-to-angular.ts
+++ src/example-runner/vanilla-to-angular.ts
@@ -74,7 +74,7 @@ function appComponentTemplate(bindings, componentFileNames) {
             return;
         }
 
-        if(property.name === "components") {
+        if(componentFileNames.length > 0 && property.name === "components") {
             property.name = "frameworkComponents";
         }
 
