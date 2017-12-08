b8ef3704cf1c703e18b2653e2af53d07a288da57
Sean Landsman <sean@thelandsmans.com>
2017-12-08 10:12:41 +0000

    Simplify & merge examples & thirdparty

--- src/javascript-grid-examples/index.php
+++ src/javascript-grid-examples/index.php
@@ -9,110 +9,23 @@ include '../documentation-main/documentation_header.php';
 
 <div>
 
-    <h2><img src="../images/svg/docs/examples.svg" width="50" /> Examples</h2>
+    <h2><img src="../images/svg/docs/examples.svg" width="50"/> Examples</h2>
 
-    <p>
-        In this section we show:
-        <ul>
-            <li>
-                Examples using frameworks where the example is significantly different to
-                the plain JavaScript version. For example, how to build a cellRenderer in React or Angular.
-            </li>
-            <li>
-                Complex examples combining different features of the grid.
-            </li>
-        </ul>
-    </p>
+    <p>In this section we show:</p>
+    <ul>
+        <li>
+            Examples using frameworks where the example is significantly different to
+            the plain JavaScript version. For example how to build use Redux with React, or RxJS with Angular.
+        </li>
+        <li>
+            Complex examples combining different features of the grid.
+        </li>
+    </ul>
 
     <p>
         If you are looking for a particular feature, please refer to the features section for details
         on that feature. Use this section for reference on how to configure ag-Grid.
     </p>
-
-    <div class="row">
-        <div class="col-md-6">
-            <div class="list-group">
-
-                <a href="../example-javascript/" class="list-group-item">
-                    <div class="float-parent">
-                        <div class="section-icon-container">
-                            <img src="../images/svg/javascript.svg" width="50" />
-                        </div>
-                        <h2 class="list-group-item-heading">JavaScript</h2>
-                        <p class="list-group-item-text">
-                            ag-Grid working with plain JavaScript combining different ag-Grid features.
-                        </p>
-                    </div>
-                </a>
-
-            </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="list-group">
-
-                <a href="../example-polymer/" class="list-group-item">
-                    <div class="float-parent">
-                        <div class="section-icon-container">
-                            <img src="../images/polymer-large.png" width="50" />
-                        </div>
-                        <h2 class="list-group-item-heading">Polymer</h2>
-                        <p class="list-group-item-text">
-                            ag-Grid working with Polymer, including custom Polymer components
-                            inside ag-Grid.
-                        </p>
-                    </div>
-                </a>
-
-            </div>
-        </div>
-    </div>
-
-
-    <div class="row">
-        <div class="col-md-6">
-            <div class="list-group">
-
-                <a href="../example-react/" class="list-group-item">
-                    <div class="float-parent">
-                        <div class="section-icon-container">
-                            <img src="../images/react_large_home.png" width="50" />
-                        </div>
-                        <h2 class="list-group-item-heading">React</h2>
-                        <p class="list-group-item-text">
-                            ag-Grid working with React, including custom React components
-                            inside ag-Grid and Redux examples.
-                        </p>
-                    </div>
-                </a>
-
-            </div>
-        </div>
-
-        <div class="col-md-6">
-            <div class="list-group">
-
-                <a href="../example-angular/" class="list-group-item">
-                    <div class="float-parent">
-                        <div class="section-icon-container">
-                            <img src="../images/angular2_large.png" width="50" />
-                        </div>
-                        <h2 class="list-group-item-heading">Angular</h2>
-                        <p class="list-group-item-text">
-                            ag-Grid working with Angular, including custom Angular components
-                            inside ag-Grid and Material Design examples.
-                        </p>
-                    </div>
-                </a>
-
-            </div>
-        </div>
-    </div>
-
-
-
-
-
 
 
 </div>
