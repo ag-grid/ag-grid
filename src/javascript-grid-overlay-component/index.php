e8e29c88be713bb97b24fd98064221f4b21c4d96
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-12-11 11:10:36 +0000

    AG-76 - Refactored overlay component names

--- src/javascript-grid-overlay-component/index.php
+++ src/javascript-grid-overlay-component/index.php
@@ -7,21 +7,77 @@ $pageGroup = "components";
 include '../documentation-main/documentation_header.php';
 ?>
 
-<h1 class="first-h1" id="filter-component">Overlay Component</h1>
-
-<h2 id="custom-overlays">Custom Overlay Renderer's</h2>
+<h1 class="first-h1" id="overlay-component">Overlay Component</h1>
 
 <p>
-    In addition to providing custom template it is also possible to supply custom renderer components for the loading
-    and no rows overlays. These custom overlays are implementations of
-    <a href="../javascript-grid-cell-rendering-components/">Cell Renderer Components</a>.
+    Overlay components allow you to add your own overlays to ag-Grid. Use these when the provided overlay's do not meet
+    your requirements.
 </p>
 
+<h2>Loading Rows Overlay Interface</h2>
+
+<snippet>
+interface ILoadingOverlayComp {
+
+    // mandatory methods
+
+    // The init(params) method is called on the overlay once. See below for details on the parameters.
+    init(params: ILoadingOverlayParams): void;
+
+    // Returns the GUI for this overlay. The GUI can be a) a string of html or b) a DOM element or node.
+    getGui(): any;
+}
+</snippet>
+
+<snippet>
+interface IFilterParams {
+
+    // an optional template for the loading rows overlay
+    loadingRowsTemplate?: string
+
+    // The grid API
+    api: any;
+}
+</snippet>
+
+<h2>No Rows Overlay Interface</h2>
+
+<snippet>
+interface INoRowsOverlayComp {
+
+    // mandatory methods
+
+    // The init(params) method is called on the overlay once. See below for details on the parameters.
+    init(params: INoRowsOverlayParams): void;
+
+    // Returns the GUI for this overlay. The GUI can be a) a string of html or b) a DOM element or node.
+    getGui(): any;
+}
+</snippet>
+
+<snippet>
+interface INoRowsOverlayParams {
+
+    // an optional template for the no rows overlay
+    noRowsTemplate?: string
+
+    // The grid API
+    api: any;
+}
+</snippet>
+
+
+<!--<p>-->
+<!--    In addition to providing custom template it is also possible to supply custom renderer components for the loading-->
+<!--    and no rows overlays. These custom overlays are implementations of-->
+<!--    <a href="../javascript-grid-cell-rendering-components/">Cell Renderer Components</a>.-->
+<!--</p>-->
+
 <p>
-    The example below demonstrate how to provide custom overlay renderer components to the grid. Notice the following:
+    The example below demonstrate how to provide custom overlay components to the grid. Notice the following:
 <ul>
-    <li><b>Custom Loading Overlay Renderer</b> is supplied via <code>gridOptions.loadingOverlayRenderer</code>.</li>
-    <li><b>Custom No Rows Overlay Renderer</b> is supplied via <code>gridOptions.noRowsOverlayRenderer</code>.</li>
+    <li><b>Custom Loading Overlay Renderer</b> is supplied via <code>gridOptions.loadingOverlayComponent</code>.</li>
+    <li><b>Custom No Rows Overlay Renderer</b> is supplied via <code>gridOptions.noRowsOverlayComponent</code>.</li>
 </ul>
 
 <?= example('Custom Overlay Components', 'custom-overlay-components', 'generated', array('enterprise' => false, 'extras' => array('fontawesome')) ) ?>
