51ba524f18bc13aa63c28a2ee15b5713056c59ee
Sean Landsman <sean@thelandsmans.com>
2017-12-12 11:02:54 +0000

    AG-661 Correct examples

--- src/javascript-grid-rtl/rtl-complex/main.js
+++ src/javascript-grid-rtl/rtl-complex/main.js
@@ -237,7 +237,7 @@ var defaultCols = [
                 // rowGroupIndex: 1,
                 enableRowGroup: true,
                 enablePivot: true,
-                cellEditor: 'richSelect',
+                cellEditor: 'agRichSelect',
                 cellEditorParams: {
                     cellRenderer: CountryCellRenderer,
                     values: ["Argentina", "Brazil", "Colombia", "France", "Germany", "Greece", "Iceland", "Ireland",
