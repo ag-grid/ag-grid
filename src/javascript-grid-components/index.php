4a6bac9940ac44ef9761d7ba196470bd8c834d62
Robert Clarke <rmc.software.consultancy@gmail.com>
2017-12-12 12:10:13 +0000

    AG-76 - added overlay section to header section

--- src/javascript-grid-components/index.php
+++ src/javascript-grid-components/index.php
@@ -136,6 +136,26 @@ include '../documentation-main/documentation_header.php';
             </div>
         </div>
 
+        <div class="row">
+            <div class="col-md-6">
+                <div class="list-group">
+
+                    <a href="../javascript-grid-overlay-component/" class="list-group-item">
+                        <div class="float-parent">
+                            <div class="section-icon-container">
+                                <img src="../images/components.png" width="50"/>
+                            </div>
+                            <h3 class="list-group-item-heading">Overlay Component</h3>
+                            <p class="list-group-item-text">
+                                To customise loading and no rows overlay components.
+                            </p>
+                        </div>
+                    </a>
+
+                </div>
+            </div>
+        </div>
+
         <h1 id="registering-custom-components">Registering Custom Components</h1>
 
         <p>
