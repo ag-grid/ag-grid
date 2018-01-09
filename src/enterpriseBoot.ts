d7d75271189a016980ac0bbbe24eaaaccaf0a375
Alberto <alberto.gutierrez.campo@gmail.com>
2018-01-09 12:36:35 +0100

    AG-1283 Allowing FW components to override default components

--- src/enterpriseBoot.ts
+++ src/enterpriseBoot.ts
@@ -1,4 +1,4 @@
-import {Bean, CellEditorFactory, Autowired, FilterManager, PostConstruct, PreConstruct, ComponentProvider} from "ag-grid/main";
+import {Autowired, Bean, CellEditorFactory, ComponentProvider, FilterManager, PostConstruct} from "ag-grid/main";
 import {SetFilter} from "./setFilter/setFilter";
 import {RichSelectCellEditor} from "./rendering/richSelect/richSelectCellEditor";
 import {LicenseManager} from "./licenseManager";
@@ -12,12 +12,12 @@ export class EnterpriseBoot {
     @Autowired('licenseManager') private licenseManager: LicenseManager;
     @Autowired('componentProvider') private componentProvider: ComponentProvider;
 
-    @PreConstruct
+    @PostConstruct
     private init(): void {
-        this.componentProvider.registerComponent('agRichSelect', RichSelectCellEditor);
-        this.componentProvider.registerComponent('agRichSelectCellEditor', RichSelectCellEditor);
-        this.componentProvider.registerComponent('agSetColumnFilter', SetFilter);
-        this.componentProvider.registerComponent('agDetailCellRenderer', DetailCellRenderer);
+        this.componentProvider.registerDefaultComponent('agRichSelect', RichSelectCellEditor);
+        this.componentProvider.registerDefaultComponent('agRichSelectCellEditor', RichSelectCellEditor);
+        this.componentProvider.registerDefaultComponent('agSetColumnFilter', SetFilter);
+        this.componentProvider.registerDefaultComponent('agDetailCellRenderer', DetailCellRenderer);
 
         this.licenseManager.validateLicense();
     }
