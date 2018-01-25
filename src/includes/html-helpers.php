99fe988fa7f34dd6ba47ca62359c3c054b50100b
Petyo Ivanov <underlog@gmail.com>
2018-01-25 16:35:47 +0200

    Non-blocking roboto

--- src/includes/html-helpers.php
+++ src/includes/html-helpers.php
@@ -28,7 +28,7 @@ META;
     echo <<<META
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,900" rel="stylesheet">
+    <!-- link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,900" rel="stylesheet" -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
     <title>$title</title>
