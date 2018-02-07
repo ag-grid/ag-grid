e265e637b33e0919e697142b764b9f3049dee739
Sean Landsman <sean@thelandsmans.com>
2018-02-07 14:55:26 +0000

    Commit temp fix as workaround for build issue

--- src/ts/gridOptionsWrapper.ts
+++ src/ts/gridOptionsWrapper.ts
@@ -449,7 +449,7 @@ export class GridOptionsWrapper {
     public getProcessSecondaryColDefFunc(): (colDef: ColDef)=>void { return this.gridOptions.processSecondaryColDef; }
     public getProcessSecondaryColGroupDefFunc(): (colGroupDef: ColGroupDef)=>void { return this.gridOptions.processSecondaryColGroupDef; }
     public getSendToClipboardFunc() { return this.gridOptions.sendToClipboard; }
-    public getProcessRowPostCreateFunc() { return this.gridOptions.processRowPostCreate; }
+    public getProcessRowPostCreateFunc() : any { return this.gridOptions.processRowPostCreate; }
 
     public getProcessCellForClipboardFunc(): (params: ProcessCellForExportParams)=>any { return this.gridOptions.processCellForClipboard; }
     public getProcessCellFromClipboardFunc(): (params: ProcessCellForExportParams)=>any { return this.gridOptions.processCellFromClipboard; }
