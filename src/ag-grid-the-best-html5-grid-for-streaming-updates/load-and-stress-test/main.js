7fa5b26910e9f51b3f7d0e3775950007f17c7388
ceolter <niall.crosby@gmail.com>
2018-02-15 09:05:47 +0000

    AG-xxx small changes to stress test

--- src/ag-grid-the-best-html5-grid-for-streaming-updates/load-and-stress-test/main.js
+++ src/ag-grid-the-best-html5-grid-for-streaming-updates/load-and-stress-test/main.js
@@ -1,24 +1,30 @@
 var columnDefs = [
         // these are the row groups, so they are all hidden (they are showd in the group column)
-        {headerName: 'Product', field: 'product', type: 'dimension', rowGroupIndex: 0, hide: true},
-        {headerName: 'Portfolio', field: 'portfolio', type: 'dimension', rowGroupIndex: 1, hide: true},
-        {headerName: 'Book', field: 'book', type: 'dimension', rowGroupIndex: 2, hide: true},
-        {headerName: 'Trade', field: 'trade', width: 100},
+        {headerName: 'Hierarchy', children: [
+            {headerName: 'Product', field: 'product', type: 'dimension', rowGroupIndex: 0, hide: true},
+            {headerName: 'Portfolio', field: 'portfolio', type: 'dimension', rowGroupIndex: 1, hide: true},
+            {headerName: 'Book', field: 'book', type: 'dimension', rowGroupIndex: 2, hide: true}
+        ]},
 
         // some string values, that do not get aggregated
-        {headerName: 'Deal Type', field: 'dealType', type: 'dimension'},
-        {headerName: 'Bid', field: 'bidFlag', type: 'dimension', width: 100},
+        {headerName: 'Attributes', children: [
+                {headerName: 'Trade', field: 'trade', width: 100},
+                {headerName: 'Deal Type', field: 'dealType', type: 'dimension'},
+                {headerName: 'Bid', field: 'bidFlag', type: 'dimension', width: 100}
+        ]},
 
         // all the other columns (visible and not grouped)
-        {headerName: 'Current', field: 'current', type: 'measure'},
-        {headerName: 'Previous', field: 'previous', type: 'measure'},
-        {headerName: 'PL 1', field: 'pl1', type: 'measure'},
-        {headerName: 'PL 2', field: 'pl2', type: 'measure'},
-        {headerName: 'Gain-DX', field: 'gainDx', type: 'measure'},
-        {headerName: 'SX / PX', field: 'sxPx', type: 'measure'},
-        {headerName: '99 Out', field: '_99Out', type: 'measure'},
-        {headerName: 'Submitter ID', field: 'submitterID', type: 'measure'},
-        {headerName: 'Submitted Deal ID', field: 'submitterDealID', type: 'measure'}
+        {headerName: 'Values', children: [
+                {headerName: 'Current', field: 'current', type: 'measure'},
+                {headerName: 'Previous', field: 'previous', type: 'measure'},
+                {headerName: 'PL 1', field: 'pl1', type: 'measure'},
+                {headerName: 'PL 2', field: 'pl2', type: 'measure'},
+                {headerName: 'Gain-DX', field: 'gainDx', type: 'measure'},
+                {headerName: 'SX / PX', field: 'sxPx', type: 'measure'},
+                {headerName: '99 Out', field: '_99Out', type: 'measure'},
+                {headerName: 'Submitter ID', field: 'submitterID', type: 'measure'},
+                {headerName: 'Submitted Deal ID', field: 'submitterDealID', type: 'measure'}
+        ]}
     ];
 
 function numberCellFormatter(params) {
@@ -92,7 +98,7 @@ function onColumnsPivot() {
 
 function onColumnsFlat() {
     gridOptions.columnApi.setPivotMode(false);
-    gridOptions.columnApi.setColumnState([{"colId":"product","width":120},{"colId":"portfolio","width":120},{"colId":"book","width":120},{"colId":"trade","width":100},{"colId":"dealType","width":120},{"colId":"bidFlag","width":100},{"colId":"current","aggFunc":"sum","width":150},{"colId":"previous","aggFunc":"sum","width":150},{"colId":"pl1","aggFunc":"sum","width":150},{"colId":"pl2","aggFunc":"sum","width":150},{"colId":"gainDx","aggFunc":"sum","width":150},{"colId":"sxPx","aggFunc":"sum","width":150},{"colId":"_99Out","aggFunc":"sum","width":150},{"colId":"submitterID","aggFunc":"sum","width":150},{"colId":"submitterDealID","aggFunc":"sum","width":150}]);
+    gridOptions.columnApi.setColumnState([{"colId": "product", "width": 120}, {"colId": "portfolio", "width": 120}, {"colId": "book", "width": 120}, {"colId": "trade", "width": 100}, {"colId": "dealType", "width": 120}, {"colId": "bidFlag", "width": 100}, {"colId": "current", "width": 150}, {"colId": "previous", "width": 150}, {"colId": "pl1", "width": 150}, {"colId": "pl2", "width": 150}, {"colId": "gainDx", "width": 150}, {"colId": "sxPx", "width": 150}, {"colId": "_99Out", "width": 150}, {"colId": "submitterID", "width": 150}, {"colId": "submitterDealID", "width": 150}]);
 }
 
 var testStartTime;
