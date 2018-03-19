bc49990dc059361769e5237f01bca8ac365e740a
Petyo Ivanov <underlog@gmail.com>
2018-03-19 17:51:26 +0200

    Add more vanilla guides

--- src/javascript-grid-test-with-jasmine/index.php
+++ src/javascript-grid-test-with-jasmine/index.php
@@ -1,43 +1,41 @@
 <?php
-$pageTitle = "ag-Grid Reference: JavaScript Datagrid - Download";
+$pageTitle = "ag-Grid Reference: JavaScript Datagrid - Test With Jasmine";
 $pageDescription = "ag-Grid is a feature-rich datagrid available in Free or Enterprise versions. This page covers downloading the JavaScript packages for ag-Grid and ag-Grid Enterprise.";
 $pageKeyboards = "JavaScript Grid";
 $pageGroup = "basics";
 include '../documentation-main/documentation_header.php';
 ?>
 
+<h1>Testing ag-Grid Applications with Jasmine</h1>
+    <p>In our <a href="https://github.com/ag-grid/ag-grid-seed">Javascript Seed Repo</a> we provide working
+        examples of how to test your project with Jasmine (under the <code>javascript</code> project).</p>
 
+    <p>In order to test your application you need to ensure that the Grid API is available - the best way to do this
+    is to set a flag when the Grid's <code>gridReady</code> event fires, but this requires an application code change.</p>
 
-    <h1>Download ag-Grid</h1>
-<p class="lead">If your project does not use package manager and you don't want to refer ag-Grid from CDN, you can download the ag-Grid's source files and keep them in your project structure. <strong>Disclaimer:</strong> This makes upgrading more complex and prone to errors.</p>
-    <h2>Download ag-Grid</h2>
+    <p>An alternative is to use a utility function that polls until the API has been set on the <code>GridOptions</code>:</p>
 
-    <p>You can download ag-Grid from the <a href="https://github.com/ag-grid/ag-grid/releases">ag-grid/ag-Grid GitHub repository releases page</a>.    There are four bundle files in the distribution:</p>
+    <snippet>
+function waitForGridApiToBeAvailable(gridOptions, success) {
+    // recursive without a terminating condition, 
+    // but jasmines default test timeout will kill it (jasmine.DEFAULT_TIMEOUT_INTERVAL)
+    if(gridOptions.api) {
+        success()
+    } else {
+        setTimeout(function () {
+            waitForGridApiToBeAvailable(gridOptions, success);
+        }, 500);
+    }
+}   </snippet>
 
-    <ul class="content">
-        <li><code>dist/ag-grid.js</code> &mdash; standard bundle containing JavaScript and CSS</li>
-        <li><code>dist/ag-grid.min.js</code> &mdash; minified bundle containing JavaScript and CSS</li>
-        <li><code>dist/ag-grid.noStyle.js</code> &mdash; standard bundle containing JavaScript without CSS</li>
-        <li><code>dist/ag-grid.min.noStyle.js</code> &mdash; minified bundle containing JavaScript without CSS</li>
-    </ul>
+    <p>Once the API is ready, we can then invoke Grid <code>API</code> and <code>ColumnApi</code> methods:</p>
 
-    <p>Should you decide to use the <code>noStyle</code> versions of the bundle (recommended), the stylesheet files are present in the <code>dist/styles</code> directory.</p>
-
-
-<h2>Download ag-Grid Enterprise</h2>
-
-    <p>You can download ag-Grid Enterprise from the <a href="https://github.com/ag-grid/ag-grid-enterprise/releases">ag-grid/ag-Grid-enterprise GitHub repository releases page</a>. The bundle includes</p>
-
-    <ul class="content">
-        <li><code>dist/ag-grid-enterprise.js</code> &mdash; standard bundle containing JavaScript and CSS</li>
-        <li><code>dist/ag-grid-enterprise.min.js</code> &mdash; minified bundle containing JavaScript and CSS</li>
-        <li><code>dist/ag-grid-enterprise.noStyle.js</code> &mdash; standard bundle containing JavaScript without CSS</li>
-        <li><code>dist/ag-grid-enterprise.min.noStyle.js </code> &mdash; minified bundle containing JavaScript without CSS</li>
-    </ul>
-
-    <p>Should you decide to use the <code>noStyle</code> versions of the bundle (recommended), you should use the style files from the ag-Grid bundle.</p>
-
-<p>After downloading the bundles, you can refer to the files in the same way as you would from CDN. Refer <a href="../javascript-getting-started/">the getting started section</a> for step-by-step guide on that.</p>
+<snippet>
+it('select all button selects all rows', () => {
+    selectAllRows();                    // selectAllRows is a global function created in the application code 
+    expect(gridOptionsUnderTest.api.getSelectedNodes().length).toEqual(3);
+});
+</snippet>
 
 
 <?php include '../documentation-main/documentation_footer.php'; ?>
