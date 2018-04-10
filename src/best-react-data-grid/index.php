6fc730c8fa5a9db34e493c88019bef2966838533
John Masterson <johnmasterson@desktop-h09m510.salix.local>
2018-04-10 14:56:55 +0100

    Change React LP

--- src/best-react-data-grid/index.php
+++ src/best-react-data-grid/index.php
@@ -1,6 +1,6 @@
 <?php
 $pageTitle = "A React Datagrid with 63 Features delivered at speed";
-$pageDescription = "ag-Grid is designed for React 16. 63 features with Enterprise grade performance. We ave just lanuched Version 17 and our product is available in two versions. ag-Grid Community is free and open source while ag-Grid Enterprise can be trialled for two months without obligation. We have detailed React tutorials and How To guides and sample code for all of our features.";
+$pageDescription = "ag-Grid is designed for React 16. 63 features with Enterprise grade performance. We have just launched Version 17 and our product is available in two versions. ag-Grid Community is free and open source while ag-Grid Enterprise can be trialled for two months without obligation. We have detailed React tutorials and How To guides and sample code for all of our features.";
 $pageKeyboards = "React Data Grid";
 $pageGroup = "basics";
 include '../landing-pages/header.php';
@@ -8,32 +8,62 @@ include '../landing-pages/header.php';
 
 <div>
 
-    <h1> React Datagrid </h1>
+    <h1> Our React Datagrid </h1>
     
-    <p class="lead">ag-Grid is designed to integrate deeply into the React framework. 
-You can use ag-Grid as a React component to quickly add a datagrid or datatables to your React application. 
-All of the examples throughout our documentation contains sample React code which is also viewable on Plunker. 
-The <a href="../react-getting-started/">Getting Started section</a> contains a step-by step tutorial on how to integrade ag-Grid in your React project.
+    <p class="lead">ag-Grid is designed to integrate deeply into the React framework. Build your React component to quickly add a datagrid or datatables to your application. 
 </p>
 
-<ul>
-    <li>ag-Grid delivers the performance you need in your React application - test it for yourself on <a href="../example.php">our Demo page</a>, select 100,000 rows and watch it fly.</li>
-    <li>You can use ag-Grid as a React component to quickly add a datagrid or datatables to your React application. The grid is designed to be highly configurable so you can have the granular level of control required for modern Enterprise applications.</li>
-    <li>Browse through <a href="../javascript-grid-features/">our extensive features</a> to select what you need and quickly add these to your application. Every feature has sample code so you can have it up and running quickly.</li>
-</ul>
+<h2>Two Versions - Choose the best fit for you:</h2>
+
+<div class="inline-container team">
+    <div class="row">
+
+        <div class="col-md-6">
+            <h3>ag-Grid Community Edition (CE)</h3>
+            <p>
+                Free and Open Source datagrid designed to meet all of the core requirements. Available now on Github or NPM. To keep up to date with our releases, you can join our mailing list or folow us on Medium.
+
+                INSERT GITHUB and NPM links.
+            </p>
+        </div>
+
+        <div class="col-md-6">
+            <h3>ag-Grid Enterprise Edition</h3>
+            <p>
+                Built on ag-Grid CE, our commercial version contains more features designed for Enterprise Applications. This version also includes Technical Support and Feature Requests. Sign up now for a free two month, fully functional trial.
+            </p>
+        </div>
+</div>
+
+<h2>Key Features and Benefits</h2>
+
+<div class="inline-container team">
+    <div class="row">
+
+        <div class="col-md-4">
+            <h3>Core Features</h3>
+			    <li>
+					insert features list in here
+				</li>
+        </div>
+
+        <div class="col-md-4">
+            <h3>Enterprise Features</h3>
+			    <li>
+					insert features list in here
+				</li>
+        </div>
+
+        <div class="col-md-4">
+            <h3>Performance</h3>
+			    <li>
+					insert features list in here
+				</li>
+        </div>
+</div>
 
 <h2>React Example</h2>
     <?= example('ag-Grid in React', 'full-rich-markup', 'react', array( "enterprise" => 1, "exampleHeight" => 525, "showResult" => true, "extras" => array( "fontawesome" ) )); ?>
-
-<h2>Versions and Features</h2>
-
-<p>ag-Grid comes in two versions. ag-Grid Community Edition is Free and Open Source, we believe it covers all of the requirements of an Open Source Datagrid. 
-If this is the right tool for you, we encourage you to use this. Follow <a href="https://medium.com/ag-grid/">our ag-Grid publication in Medium</a> to get updates on our releases and upcoming plans.</p> 
-
-<p>ag-Grid Enterprise builds on ag-Grid Community via a dependency and adds more heavyweight features that we believe separate us from our competitors. 
-This version is available under commercial license and comes with 1 Year of Support, Maintenance and Updates.</p>
-
-<p>You can find a full list of all ag-Grid features in our <a href="../javascript-grid-features/">documentation &rt; features section</a>. We have indicated the Enterprise features with the <span class="enterprise-icon">e</span> icon.</p>
 
 <h2>How Do I Get Started?</h2>
 
