e75e2714afe27ee55669faa02af958518addddfd
ceolter <niall.crosby@gmail.com>
2018-04-18 16:04:18 +0100

    AG-581 Change scrolling for pinned and master/detail to use native scrolling, not the mimicked version.

--- src/ts/gridPanel/scrollVisibleService.ts
+++ src/ts/gridPanel/scrollVisibleService.ts
@@ -1,16 +1,17 @@
-import {Bean, Autowired} from "../context/context";
+import {Bean, Autowired, PostConstruct} from "../context/context";
 import {Utils as _} from "../utils";
 import {EventService} from "../eventService";
 import {Events, ScrollVisibilityChangedEvent} from "../events";
 import {ColumnController} from "../columnController/columnController";
 import {ColumnApi} from "../columnController/columnApi";
 import {GridApi} from "../gridApi";
+import {GridOptionsWrapper} from "../gridOptionsWrapper";
 
 export interface SetScrollsVisibleParams {
     vBody: boolean;
     hBody: boolean;
-    vPinnedLeft: boolean;
-    vPinnedRight: boolean;
+    vLeft: boolean;
+    vRight: boolean;
 }
 
 @Bean('scrollVisibleService')
@@ -20,26 +21,34 @@ export class ScrollVisibleService {
     @Autowired('columnController') private columnController: ColumnController;
     @Autowired('columnApi') private columnApi: ColumnApi;
     @Autowired('gridApi') private gridApi: GridApi;
+    @Autowired('gridOptionsWrapper') private gridOptionsWrapper: GridOptionsWrapper;
+
+    private scrollWidth: number;
 
     private vBody: boolean;
     private hBody: boolean;
 
-    private vPinnedLeft: boolean;
-    private vPinnedRight: boolean;
+    private vLeft: boolean;
+    private vRight: boolean;
+
+    @PostConstruct
+    private postConstruct(): void {
+        this.scrollWidth = this.gridOptionsWrapper.getScrollbarWidth();
+    }
 
     public setScrollsVisible(params: SetScrollsVisibleParams): void {
 
         let atLeastOneDifferent =
             this.vBody !== params.vBody
             || this.hBody !== params.hBody
-            || this.vPinnedLeft !== params.vPinnedLeft
-            || this.vPinnedRight !== params.vPinnedRight;
+            || this.vLeft !== params.vLeft
+            || this.vRight !== params.vRight;
 
         if (atLeastOneDifferent) {
             this.vBody = params.vBody;
             this.hBody = params.hBody;
-            this.vPinnedLeft = params.vPinnedLeft;
-            this.vPinnedRight = params.vPinnedRight;
+            this.vLeft = params.vLeft;
+            this.vRight = params.vRight;
 
             let event: ScrollVisibilityChangedEvent = {
                 type: Events.EVENT_SCROLL_VISIBILITY_CHANGED,
@@ -59,34 +68,18 @@ export class ScrollVisibleService {
         return this.hBody;
     }
 
-    public isVPinnedLeftShowing(): boolean {
-        return this.vPinnedLeft;
-    }
-
-    public isVPinnedRightShowing(): boolean {
-        return this.vPinnedRight;
-    }
-
-    public getPinnedLeftWidth(): number {
-        return this.columnController.getPinnedLeftContainerWidth();
-    }
-
     public getPinnedLeftWithScrollWidth(): number {
-        let result = this.getPinnedLeftWidth();
-        if (this.vPinnedLeft) {
-            result += _.getScrollbarWidth();
+        let result = this.columnController.getPinnedLeftContainerWidth();
+        if (this.vLeft) {
+            result += this.scrollWidth;
         }
         return result;
     }
 
-    public getPinnedRightWidth(): number {
-        return this.columnController.getPinnedRightContainerWidth();
-    }
-
     public getPinnedRightWithScrollWidth(): number {
-        let result = this.getPinnedRightWidth();
-        if (this.vPinnedRight) {
-            result += _.getScrollbarWidth();
+        let result = this.columnController.getPinnedRightContainerWidth();
+        if (this.vRight) {
+            result += this.scrollWidth;
         }
         return result;
     }
