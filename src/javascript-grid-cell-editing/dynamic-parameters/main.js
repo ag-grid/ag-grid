0b8429c3eaafed1e34b4659bb7b78dea4fbfacaf
Sean Landsman <sean@thelandsmans.com>
2018-04-24 15:51:34 +0100

    AG-1744 Add event deprecation warning mechanism

--- src/javascript-grid-cell-editing/dynamic-parameters/main.js
+++ src/javascript-grid-cell-editing/dynamic-parameters/main.js
@@ -1,12 +1,12 @@
 var columnDefs = [
-    { field: 'name', width: 100},
+    {field: 'name', width: 100},
     {
         field: 'gender',
         width: 90,
         cellRenderer: 'genderCellRenderer',
         cellEditor: 'agRichSelectCellEditor',
         cellEditorParams: {
-            values: ['Male','Female'],
+            values: ['Male', 'Female'],
             cellRenderer: 'genderCellRenderer'
         }
     },
@@ -22,23 +22,212 @@ var columnDefs = [
     {
         field: 'city', width: 70,
         cellEditor: 'agRichSelectCellEditor',
-        cellEditorParams: function(params) {
+        cellEditorParams: function (params) {
             var selectedCountry = params.data.country;
             var allowedCities = countyToCityMap[selectedCountry];
             return {
                 values: allowedCities,
-                formatValue: function(value) {
+                formatValue: function (value) {
                     return value + ' (' + selectedCountry + ')';
                 }
             };
         }
     },
-    { field: 'address', width: 200, cellEditor: 'agLargeTextCellEditor'}
+    {field: 'address', width: 200, cellEditor: 'agLargeTextCellEditor'}
+];
+
+var rowData = [
+    {
+        name: 'Bob Harrison',
+        gender: 'Male',
+        address: '1197 Thunder Wagon Common, Cataract, RI, 02987-1016, US, (401) 747-0763',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Mary Wilson',
+        gender: 'Female',
+        age: 11,
+        address: '3685 Rocky Glade, Showtucket, NU, X1E-9I0, CA, (867) 371-4215',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Sadiq Khan',
+        gender: 'Male',
+        age: 12,
+        address: '3235 High Forest, Glen Campbell, MS, 39035-6845, US, (601) 638-8186',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Jerry Mane',
+        gender: 'Male',
+        age: 12,
+        address: '2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Bob Harrison',
+        gender: 'Male',
+        address: '1197 Thunder Wagon Common, Cataract, RI, 02987-1016, US, (401) 747-0763',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Mary Wilson',
+        gender: 'Female',
+        age: 11,
+        address: '3685 Rocky Glade, Showtucket, NU, X1E-9I0, CA, (867) 371-4215',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Sadiq Khan',
+        gender: 'Male',
+        age: 12,
+        address: '3235 High Forest, Glen Campbell, MS, 39035-6845, US, (601) 638-8186',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Jerry Mane',
+        gender: 'Male',
+        age: 12,
+        address: '2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Bob Harrison',
+        gender: 'Male',
+        address: '1197 Thunder Wagon Common, Cataract, RI, 02987-1016, US, (401) 747-0763',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Mary Wilson',
+        gender: 'Female',
+        age: 11,
+        address: '3685 Rocky Glade, Showtucket, NU, X1E-9I0, CA, (867) 371-4215',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Sadiq Khan',
+        gender: 'Male',
+        age: 12,
+        address: '3235 High Forest, Glen Campbell, MS, 39035-6845, US, (601) 638-8186',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Jerry Mane',
+        gender: 'Male',
+        age: 12,
+        address: '2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Bob Harrison',
+        gender: 'Male',
+        address: '1197 Thunder Wagon Common, Cataract, RI, 02987-1016, US, (401) 747-0763',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Mary Wilson',
+        gender: 'Female',
+        age: 11,
+        address: '3685 Rocky Glade, Showtucket, NU, X1E-9I0, CA, (867) 371-4215',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Sadiq Khan',
+        gender: 'Male',
+        age: 12,
+        address: '3235 High Forest, Glen Campbell, MS, 39035-6845, US, (601) 638-8186',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Jerry Mane',
+        gender: 'Male',
+        age: 12,
+        address: '2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Bob Harrison',
+        gender: 'Male',
+        address: '1197 Thunder Wagon Common, Cataract, RI, 02987-1016, US, (401) 747-0763',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Mary Wilson',
+        gender: 'Female',
+        age: 11,
+        address: '3685 Rocky Glade, Showtucket, NU, X1E-9I0, CA, (867) 371-4215',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Sadiq Khan',
+        gender: 'Male',
+        age: 12,
+        address: '3235 High Forest, Glen Campbell, MS, 39035-6845, US, (601) 638-8186',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Jerry Mane',
+        gender: 'Male',
+        age: 12,
+        address: '2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Bob Harrison',
+        gender: 'Male',
+        address: '1197 Thunder Wagon Common, Cataract, RI, 02987-1016, US, (401) 747-0763',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Mary Wilson',
+        gender: 'Female',
+        age: 11,
+        address: '3685 Rocky Glade, Showtucket, NU, X1E-9I0, CA, (867) 371-4215',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Sadiq Khan',
+        gender: 'Male',
+        age: 12,
+        address: '3235 High Forest, Glen Campbell, MS, 39035-6845, US, (601) 638-8186',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
+    {
+        name: 'Jerry Mane',
+        gender: 'Male',
+        age: 12,
+        address: '2234 Sleepy Pony Mall , Drain, DC, 20078-4243, US, (202) 948-3634',
+        city: 'Dublin',
+        country: 'Ireland'
+    },
 ];
 
 var countyToCityMap = {
-    'Ireland': ['Dublin','Cork','Galway'],
-    'USA': ['New York','Los Angeles','Chicago','Houston']
+    'Ireland': ['Dublin', 'Cork', 'Galway'],
+    'USA': ['New York', 'Los Angeles', 'Chicago', 'Houston']
 };
 
 var gridOptions = {
@@ -51,12 +240,12 @@ var gridOptions = {
     defaultColDef: {
         editable: true
     },
-    onGridReady: function(params) {
+    onGridReady: function (params) {
         params.api.sizeColumnsToFit();
     },
-    onCellValueChanged: function(params) {
+    onCellValueChanged: function (params) {
         var colId = params.column.getId();
-        if (colId==='country') {
+        if (colId === 'country') {
 
             var selectedCountry = params.data.country;
             var selectedCity = params.data.city;
@@ -71,20 +260,21 @@ var gridOptions = {
     }
 };
 
-function GenderCellRenderer() {}
+function GenderCellRenderer() {
+}
 
-GenderCellRenderer.prototype.init = function(params) {
+GenderCellRenderer.prototype.init = function (params) {
     this.eGui = document.createElement('span');
     var img = params.value === 'Male' ? 'male.png' : 'female.png';
-    this.eGui.innerHTML = '<img src="../images/'+img+'"/> ' + params.value;
+    this.eGui.innerHTML = '<img src="../images/' + img + '"/> ' + params.value;
 };
 
-GenderCellRenderer.prototype.getGui = function() {
+GenderCellRenderer.prototype.getGui = function () {
     return this.eGui;
 };
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 });
