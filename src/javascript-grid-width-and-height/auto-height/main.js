426e0f449e6ce23864f71fbf0e44b9c652673eb6
ceolter <niall.crosby@gmail.com>
2018-05-02 16:56:14 +0100

    AG-1768 allow switching between autoHeight and normal height

--- src/javascript-grid-width-and-height/auto-height/main.js
+++ src/javascript-grid-width-and-height/auto-height/main.js
@@ -43,6 +43,20 @@ function cbFloatingRows() {
     }
 }
 
+function setAutoHeight() {
+    gridOptions.api.setGridAutoHeight(true);
+    // auto height will get the grid to fill the height of the contents,
+    // so the grid div should have no height set, the height is dynamic.
+    document.querySelector('#myGrid').style.height = '';
+}
+
+function setFixedHeight() {
+    gridOptions.api.setGridAutoHeight(false);
+    // when auto height is off, the grid ahs a fixed height, and then the grid
+    // will provide scrollbars if the data does not fit into it.
+    document.querySelector('#myGrid').style.height = '400px';
+}
+
 var columnDefs = [
     {
         headerName: 'Core',
@@ -81,6 +95,7 @@ var gridOptions = {
     enableSorting: true,
     enableFilter: true,
     gridAutoHeight: true,
+    animateRows: true,
     onGridReady: function() {
         document.querySelector('#currentRowCount').innerHTML = 5;
     }
