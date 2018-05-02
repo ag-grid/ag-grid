21bcdbf03811f577dcf79849d790327379b7a110
Sean Landsman <sean@thelandsmans.com>
2018-05-02 17:10:56 +0100

    New gridAutoHeight property

--- src/agGridNg2.ts
+++ src/agGridNg2.ts
@@ -382,6 +382,7 @@ export class AgGridNg2 implements AfterViewInit {
     @Input() public suppressEnterpriseResetOnNewColumns : any = undefined;
     @Input() public enableOldSetFilterModel : any = undefined;
     @Input() public suppressRowHoverHighlight : any = undefined;
+    @Input() public gridAutoHeight : any = undefined;
 
     @Output() public columnEverythingChanged: EventEmitter<any> = new EventEmitter<any>();
     @Output() public newColumnsLoaded: EventEmitter<any> = new EventEmitter<any>();
