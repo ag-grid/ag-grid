1f2705812848ffbba08874e89bf633a06c665236
Sean Landsman <sean@thelandsmans.com>
2018-05-21 11:36:38 +0100

    AG-1770 Website - Auto Row Height Angular Example doesn't work

--- src/javascript-grid-row-height/index.php
+++ src/javascript-grid-row-height/index.php
@@ -70,7 +70,6 @@ gridOptions.getRowHeight = function(params) {
         It uses the <code>getRowHeight()</code> callback to achieve this.
     </p>
 
-    <?= example('Row Height Simple', 'row-height-simple', 'generated') ?>
 
     <h2>Row Height More Complex Example</h2>
 
@@ -82,7 +81,6 @@ gridOptions.getRowHeight = function(params) {
         the contents of the cell.
     </p>
 
-    <?= example('Row Height Complex', 'row-height-complex', 'generated') ?>
 
     <h2>Auto Row Height</h2>
 
@@ -203,7 +201,6 @@ gridOptions.getRowHeight = function(params) {
     height is an ag-Grid free feature, we just demonstrate it against groups and normal
     rows below.</p>
 
-    <?= example('Changing Row Height', 'row-height-change', 'generated', array("enterprise" => 1)) ?>
 
     <h2>Height for Pinned Rows</h2>
 
