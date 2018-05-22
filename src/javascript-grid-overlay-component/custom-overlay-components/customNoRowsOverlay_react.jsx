d6b2d48f0eec55f6add6e8e50168179916ba8a2c
Alberto <alberto.gutierrez.campo@gmail.com>
2018-05-22 17:05:26 +0100

    Revert "Revert "Merge branch 'latest' of https://github.com/ag-grid/ag-grid-docs into latest""
    This reverts commit c361908

--- src/javascript-grid-overlay-component/custom-overlay-components/customNoRowsOverlay_react.jsx
+++ src/javascript-grid-overlay-component/custom-overlay-components/customNoRowsOverlay_react.jsx
@@ -4,7 +4,7 @@ export default class CustomNoRowsOverlay extends Component {
     render() {
         return (
             <div className="ag-overlay-loading-center" style={{backgroundColor: 'lightcoral', height: '9%'}}>
-                <i className="fa fa-frown-o"> Sorry - no rows!</i>
+                <i className="fa fa-frown-o"> {this.props.noRowsMessageFunc()}</i>
             </div>
         );
     }
--- src/javascript-grid-overlay-component/custom-overlay-components/customNoRowsOverlay_vanilla.js
+++ src/javascript-grid-overlay-component/custom-overlay-components/customNoRowsOverlay_vanilla.js
@@ -4,7 +4,7 @@ CustomNoRowsOverlay.prototype.init = function(params) {
     this.eGui = document.createElement('div');
     this.eGui.innerHTML =
         '<div class="ag-overlay-loading-center" style="background-color: lightcoral; height: 9%">' +
-        '   <i class="fa fa-frown-o"> Sorry - no rows!</i>' +
+        '   <i class="fa fa-frown-o"> ' + params.noRowsMessageFunc() + ' </i>' +
         '</div>';
 };
 
