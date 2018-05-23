3999e90bb6ce172c72cccdc1bbb99d0986adccc8
Robert Clarke <rmc.software.consultancy@gmail.com>
2018-05-23 15:25:14 +0100

    AG-1793 - added Pivot Row Totals

--- src/agGridNg2.ts
+++ src/agGridNg2.ts
@@ -364,6 +364,8 @@ export class AgGridNg2 implements AfterViewInit {
     @Input() public ensureDomOrder : any = undefined;
     @Input() public accentedSort : any = undefined;
     @Input() public pivotTotals : any = undefined;
+    @Input() public pivotColumnGroupTotals : any = undefined;
+    @Input() public pivotRowTotals : any = undefined;
     @Input() public suppressChangeDetection : any = undefined;
     @Input() public valueCache : any = undefined;
     @Input() public valueCacheNeverExpires : any = undefined;
