e6d7fcdbba48e13f9abbf497287e2a048cefab77
Robert Clarke <rmc.software.consultancy@gmail.com>
2018-05-30 12:53:58 +0100

    AG-1569 - renamed In Memory Row Model to Client Side Row Model

--- src/example-expressions-and-context/monthly-sales/main.js
+++ src/example-expressions-and-context/monthly-sales/main.js
@@ -89,7 +89,7 @@ onChangeMonth = function(i) {
 
     gridOptions.context.month = newMonth;
     document.querySelector('#monthName').innerHTML = monthNames[newMonth + 1];
-    gridOptions.api.refreshInMemoryRowModel('aggregate');
+    gridOptions.api.refreshClientSideRowModel('aggregate');
     gridOptions.api.refreshView();
 };
 
