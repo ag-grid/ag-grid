1cf6d306da0f2793a56b5b943d4bac13ee4f833a
Alberto <alberto.gutierrez.campo@gmail.com>
2018-06-11 11:29:34 +0100

    Release 18.0.0 dist files created

---
+++ dist/lib/pivotCompFactory.d.ts
@@ -0,0 +1,6 @@
+// ag-grid-enterprise v18.0.0
+import { ICompFactory, Component } from "ag-grid/main";
+export declare class PivotCompFactory implements ICompFactory {
+    private context;
+    create(): Component;
+}
