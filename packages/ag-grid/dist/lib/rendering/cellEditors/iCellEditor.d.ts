1f82c57f6c0035a0f801a0ef58f05c85d0b3ade3
MrSafferty <sean@thelandsmans.com>
2018-07-25 10:42:33 +0100

    Release 18.1.2 - Dist files added

--- packages/ag-grid/dist/lib/rendering/cellEditors/iCellEditor.d.ts
+++ packages/ag-grid/dist/lib/rendering/cellEditors/iCellEditor.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for ag-grid v18.1.2-beta.1
+// Type definitions for ag-grid v18.1.2
 // Project: http://www.ag-grid.com/
 // Definitions by: Niall Crosby <https://github.com/ag-grid/>
 import { Column } from "../../entities/column";
--- packages/ag-grid/dist/lib/rendering/overlays/noRowsOverlayComponent.js
+++ packages/ag-grid/dist/lib/rendering/overlays/noRowsOverlayComponent.js
@@ -1,6 +1,6 @@
 /**
  * ag-grid - Advanced Data Grid / Data Table supporting Javascript / React / AngularJS / Web Components
- * @version v18.1.2-beta.1
+ * @version v18.1.2
  * @link http://www.ag-grid.com/
  * @license MIT
  */
