08c971b21bec61e91d80ff0a09f6d3248bc367ec
Robert Clarke <robertclarke@Roberts-MacBook-Pro.local>
2018-07-31 18:08:46 +0100

    AG-1918 - adding server-side row model tree data examples

--- packages/ag-grid-docs/src/javascript-grid-server-side-model/index.php
+++ packages/ag-grid-docs/src/javascript-grid-server-side-model/index.php
@@ -725,4 +725,68 @@ gridOptions.api.forEachNode(function(rowNode) {
 
 <?= example('Server-side Row Model & CRUD', 'crud', 'generated', array("enterprise" => 1)) ?>
 
+
+
+<h2>Tree Data</h2>
+<p>
+    Just like in the Client-side Row Model, it is possible to use Tree Data with the Server-side Row Model. Tree Data is defined
+    as data that has parent / child relationships where the parent / child relationships are provided as part of the data.
+</p>
+
+<h2 id="tree-data-mode">Tree Data Mode</h2>
+
+<p>
+    In order to set the grid to work with Tree Data, simply enable Tree Data mode via the Grid Options using:
+    <code>gridOptions.treeData = true</code>.
+</p>
+
+<h2 id="supplying-tree-data">Supplying Tree Data</h2>
+
+<p>
+    Tree Data is supplied via the <a href="../javascript-grid-server-side-model/#server-side-datasource">Server-side Datasource</a>
+    just like flat data, however there two additional gridOptions callbacks; <code>isServerSideGroup(dataItem)</code> and <code>getServerSideGroupKey(dataItem)</code>.
+</p>
+
+<p>
+    The configuration used The code snippet below shows the <code>gridOptions</code> relevant for configuring tree data with the server-side
+    row model.
+</p>
+
+<snippet>
+var gridOptions = {
+
+    // enable tree data
+    treeData: true,
+
+    // indicate if row is a group node
+    isServerSideGroup: function (dataItem) {
+        return dataItem.group;
+    },
+
+    // specify which group key to use
+    getServerSideGroupKey: function (dataItem) {
+        return dataItem.employeeId;
+    }
+    ...
+}
+</snippet>
+
+<note>
+    Be careful not to get mixed up with the <a href="../javascript-grid-tree-data/">Client-side Tree Data</a> configurations by mistake.
+</note>
+
+    <p>
+        The example below shows this in action where the following can be noted:
+    </p>
+
+    <ul class="content">
+        <li>The <b>Add Row</b> will add a row before the currently selected row.</li>
+        <li>The <b>Remove Row</b> will remove the currently selected row.</li>
+        <li>All operations are done outside of the grid and the grid is then told to refresh.</li>
+    </ul>
+
+<?= example('Tree Data', 'tree-data', 'generated', array("enterprise" => 1, "extras" => array('lodash'))) ?>
+
+<?= example('Purging Tree Data', 'purging-tree-data', 'generated', array("enterprise" => 1, "extras" => array('lodash'))) ?>
+
 <?php include '../documentation-main/documentation_footer.php';?>
---
+++ packages/ag-grid-docs/src/javascript-grid-server-side-model/tree-data/main.js
@@ -0,0 +1,55 @@
+var columnDefs = [
+  {field: "employeeId", hide: true},
+  {field: "employeeName", hide: true},
+  {field: "jobTitle"},
+  {field: "employmentType"}
+];
+
+var gridOptions = {
+  defaultColDef: {
+    width: 250,
+    suppressFilter: true
+  },
+  autoGroupColumnDef: {
+    cellRendererParams: {
+      innerRenderer: function(params) {
+        // display employeeName rather than group key (employeeId)
+        return params.data.employeeName;
+      }
+    }
+  },
+  rowModelType: 'serverSide',
+  treeData: true,
+  columnDefs: columnDefs,
+  enableColResize: true,
+  animateRows: true,
+  icons: {
+    groupLoading: '<img src="https://raw.githubusercontent.com/ag-grid/ag-grid-docs/master/src/javascript-grid-server-side-model/spinner.gif" style="width:22px;height:22px;">'
+  },
+  isServerSideGroup: function (dataItem) {
+     // indicate if node is a group
+     return dataItem.group;
+  },
+  getServerSideGroupKey: function (dataItem) {
+    // specify which group key to use
+    return dataItem.employeeId;
+  },
+  onGridReady: function (params) {
+    // initialise with the first group arbitrarily expanded
+    setTimeout(function() {
+      params.api.getDisplayedRowAtIndex(0).setExpanded(true);
+    }, 1000);
+  }
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function() {
+  var gridDiv = document.querySelector('#myGrid');
+  new agGrid.Grid(gridDiv, gridOptions);
+
+  agGrid.simpleHttpRequest({url: 'https://raw.githubusercontent.com/ag-grid/ag-grid/latest/packages/ag-grid-docs/src/javascript-grid-server-side-model/simple-tree-data/data/data.json'}).then(function(data) {
+    var fakeServer = new FakeServer(data);
+    var datasource = new ServerSideDatasource(fakeServer);
+    gridOptions.api.setServerSideDatasource(datasource);
+  });
+});
