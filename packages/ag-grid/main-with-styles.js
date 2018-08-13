6bfb5a1ebd1f81c5e88950ac023796b74282a4f6
MrSafferty <sean@thelandsmans.com>
2018-08-13 10:34:34 +0100

    retiring deprecated themes

--- packages/ag-grid/main-with-styles.js
+++ packages/ag-grid/main-with-styles.js
@@ -6,12 +6,6 @@ Object.keys(agGrid).forEach(function(key) {
 
 require('./dist/styles/ag-grid.css');
 
-require('./dist/styles/theme-blue.css');
-require('./dist/styles/theme-dark.css');
-require('./dist/styles/theme-fresh.css');
-require('./dist/styles/theme-material.css');
-require('./dist/styles/theme-bootstrap.css');
-
 require('./dist/styles/ag-theme-bootstrap.css');
 require('./dist/styles/ag-theme-blue.css');
 require('./dist/styles/ag-theme-dark.css');
