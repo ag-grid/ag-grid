db5e525df2bb1501535a9df087d9a92f50a5405e
Fahad Ahmad <fahad.ahmad@ag-grid.com>
2018-08-17 15:56:55 +0100

    SEO Optimisation

--- packages/ag-grid-docs/src/best-react-data-grid/index.php
+++ packages/ag-grid-docs/src/best-react-data-grid/index.php
@@ -1,7 +1,7 @@
 <?php
-$pageTitle = "React Datagrid | Packed with features and performance
+$pageTitle = "React Data Grid | Packed with features and performance
 .";
-$pageDescription = "Worlds leading, feature rich React Datagrid. Designed to integrate seamlessly with React to deliver filtering, grouping, aggregation, pivoting and much more with the performance that you expect. Version 18 is out now.";
+$pageDescription = "Worlds leading, feature rich React Data Grid. Designed to integrate seamlessly with React to deliver filtering, grouping, aggregation, pivoting and much more with the performance that you expect. Version 18 is out now.";
 $pageGroup = "basics";
 include '../landing-pages/header.php';
 ?>
@@ -11,37 +11,39 @@ include '../landing-pages/header.php';
     <h1>ag-Grid: World's Leading React Datagrid</h1>
     <hr>
     
-    <p class="lead">ag-Grid is designed to integrate deeply into React. Use our grid as a React component to quickly add a react datagrid or a react table to your application. We support the latest version - React 16 - and always quickly release when new versions come out. This page covers the versions of our product, some of the key benefits and outlines the resources available to quickly add a datgrid or datatable to your React application.   
+    <p class="lead">ag-Grid is designed to integrate deeply into React.<br>
+        Use our grid as a React component to quickly add a react datagrid or a react table to your application.<br>
+        Discover the versions, key benefits and resources available to quickly add a data grid or React datatable to your React application.  
 </p>
 <hr>
 <h2>Two Versions - Which is the Best Fit for you?</h2>
 
 <div>
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6" align="text-center">
                 <h2>ag-Grid Community</h2>
                 <h3>Trusted by the Community</h3>
                 <hr>
-                                <p><strong>2M Downloads
+                                <p style="background-color: aliceblue"><strong>2M Downloads
                     <br>
                     150,000 Downloads Per Month</strong>
                 </p>
                 <hr>
                 <p>
-                    Free and Open Source Angular datagrid.
+                    Free and Open Source React data grid.
                     <br>
                     Designed to meet all of your core requirements.
 <br>
-                    Available via Github or NPM.
+                    Available via <a href="https://github.com/ag-grid/ag-grid-react" target="_blank">Github</a> or <a href="https://www.npmjs.com/package/ag-grid" target="_blank">NPM</a>.
                 </p>
-                <a href="https://github.com/ag-grid/ag-grid-angular"><button type="button" class="btn btn-outline-primary btn-lg">Community Edition</button></a>
+                <a href="../react-getting-started" target="_blank"><button type="button" class="btn btn-outline-primary btn-lg">Community Edition</button></a>
             </div>
 
             <div class="col-md-6">
                 <h2>ag-Grid Enterprise </h2>
                 <h3>Built for the Enterprise</h3>
                 <hr>
-                                <p><strong>Over 1,500 Companies use ag-Grid Enterprise
+                                <p style="background-color: aliceblue"><strong>Over 1,500 Companies use ag-Grid Enterprise
                     <br>
                     Over 25% of FT-500 Companies use ag-Grid Enterprise</strong>
                 </p>
@@ -55,18 +57,82 @@ include '../landing-pages/header.php';
             </div>
         </div>
 
-<br><hr style="width: 1000px" style="border-color: blue">
-    <h2>Key Features and Benefits</h2>
+<br><hr>
+<h1 class="text-center" style="background-color: aliceblue;padding-top: 30px;padding-bottom: 30px;margin-top: 30px"><strong>Key Features and Benefits</strong></h1>
+
+<div class="container">
+<div class="row">
+    <div class="col" style="margin-top: -20px">
+<h2><strong>Core</strong></h2>
+<hr>
+<p>
+    Everything you'd expect from an React Data grid.<br>
+    Available in <a href="">ag-Grid Community</a>.
+</p>
+    </div>
+    <div class="col" style="margin-top: 10px">
+        <strong>Sorting</strong>
+<img src="/best-angular-2-data-grid/sorting.gif" alt="Sorting">
+    </div>
+    <div class="col" style="margin-top: 10px">
+        <strong>Filtering</strong>
+<img src="/best-angular-2-data-grid/filtering.gif" alt="filtering">
+    </div>
+  </div>
+<br>    
+  <div class="row">
+    <div class="col" style="margin-top: -20px">
+<h2><strong>Enterprise</strong></h2>
+<hr>
+<p>
+Powerful, advanced features designed for Enterprise Applications.<br>
+Availalbe in ag-Grid Enterprise. 
+</p>
+    </div>
+    <div class="col" style="margin-top: 10px">
+        <strong>Aggregation</strong>
+<img src="/best-angular-2-data-grid/selection.gif" alt="aggregation">
+    </div>
+    <div class="col" style="margin-top: 10px">
+        <strong>Pivoting</strong>
+<img src="/best-angular-2-data-grid/pivoting.gif" alt="pivoting">
+    </div>
+    <div>
+</div>
+
+<div class="container">
+<h3 class="text-center" style="background-color: whitesmoke;padding-top: 10px;padding-bottom: 10px"><strong>Performance</strong></h3>
+
+<div class="container">
+  <div class="row">
+    <div class="col">
+        <strong>Built to deal with large data sets</strong>
+        <br><br><br><br>
+      ag-Grid is built to deal with large data sets.<br> 
+      We find that the browser runs out of steam before the grid, easily handles 100,000 rows.
+    </div>
+    <div class="col">
+        <div>
+<img src="100,000 rows.gif" alt="Performance" style="margin-left: 20px">
+<button style="margin-top: 10px;width: : 480px" type="button" class="btn btn-outline-primary btn-block">ag-Grid Performance Demo</button>
+</div>
+    </div>
+  </div>
+
     <div>
         <div class="row">
             <div style="margin-bottom: : 10px" class="col-md-4">
                 <h2>Core and Enterprise Features</h2>
                     <ul class="content-list">
                         <li>
-                            Everything you'd expect from an Angular datagrid. Sorting, filtering, row grouping. Available in ag-Grid Community.
+                            Everything you'd expect from an React Data grid.<br> 
+                            Sorting, filtering, row grouping.<br> 
+                            Available in ag-Grid Community.
                         </li>
                         <li>
-                            A more powerful set of advanced features designed for Enterprise Applications. Pivoting, Aggregation, Clipboard, Master/Detail, Tree Data
+                            Powerful set of features designed for Enterprise.<br> 
+                            Pivoting, Aggregation,Tree Data.<br>
+                            Available in ag-Grid Enterprise.
                         </li>
                             </li><hr>
                             <button style="margin-top: 25px" type="button" class="btn btn-outline-primary btn-block">Features Overview</button>
@@ -108,26 +174,49 @@ include '../landing-pages/header.php';
             </div>
         </div>
     </div>
-<hr style="width: 1000px" style="border-color: lightskyblue" style="padding-bottom: 100px">
-<h2>React Grid Example</h2>
-    <?= example('ag-Grid in React', 'full-rich-markup', 'react', array( "enterprise" => 1, "exampleHeight" => 525, "showResult" => true, "extras" => array( "fontawesome" ) )); ?>
 
-<h2>How Do I Get Started?</h2>
+<hr>
+<h1 class="text-center" style="background-color: aliceblue;padding-top: 30px;padding-bottom: 30px;margin-top: 30px"><strong>ag-Grid in React</strong></h1>
 
-<p>We have put together a detailed React tutorial on <a href="../react-getting-started/">Getting Started with React and ag-Grid</a>. This covers getting up and running with ag-Grid Community and then adding ag-Grid Enterprise.</p>
+<div class="container">
+  <div class="row">
+    <div class="col" style="max-width: 400px">
+<h3 class="text-center">Test ag-Grid's features and performance level in the React Data Grid Example.</h3>
+<br><br>
+<h6 class="text-center">
+    <strong>Extensive Set of Features</strong>
+</h6>
+<p class="text-center">
+    Designed for Core and Enterprise needs.
+</p>
+<br>
+<h6 class="text-center">
+    <strong>Features Are Useless Without Performance</strong>
+</h6>
+<p class="text-center">
+    Built to deal with large data sets.
+</p>
+    </div>  
+    <div class="col">
+    <?= example('React Grid Example', 'full-rich-markup', 'react', array( "enterprise" => 1, "exampleHeight" => 300 , "showResult" => true, "extras" => array( "fontawesome" ) )); ?>
 
-<h2>What About Detailed Guides and Examples?</h2>
+          <a href="../example.php" target="_blank"><button type="button" class="btn btn-outline-primary btn-sm btn-block">React Grid Demo</button></a>
 
-<p>We have created a number of more detailed guides as follows:</p>
+    </div>
+  </div>
 
-<div class="btn-group btn-group-justified">
-    <div class="btn-group" style="margin-left: 100px">
-  <a href="#" style="width: 320px" class="btn btn-light">View our React Grid Overview</a>
-  <a href="#" style="width: 320px" class="btn btn-light">React Redux integration | P2</a>
-  <a href="#" style="width: 320px" class="btn btn-light">React Redux integration | P2</a>
+<div>
+<h3>How Do I Get Started?</h3>
+<br>
+<p>We have put together a detailed React Data Grid tutorial on <a href="../react-getting-started-getting-started/" target="_blank">Getting Started with React and ag-Grid</a>. This covers getting up and running with ag-Grid Community and then adding ag-Grid Enterprise.</p>
+
+<h3>What About Detailed Guides and Examples?</h3>
+<br>
+  <a href="../react-more-details/" class="btn btn-light" target="_blank">View our React Grid Overview</a>   
+  <a href="../react-redux-integration-p1" class="btn btn-light" target="_blank">React Redux integration | P1</a>
+    <a href="../react-redux-integration-p2" class="btn btn-light" target="_blank">React Redux integration | P2</a>
 </div>
 
-</div>
 <br>
 <div>
   <a href="https://github.com/ag-grid/ag-grid/tree/master/packages/ag-grid-react"><button type="button" class="btn btn-outline-primary btn-lg btn-block">Community Edition</button></a>
