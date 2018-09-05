f96cf1a35d6fa4576aa974d4688dc08aecfc976d
Robert Clarke <robertclarke@Roberts-MacBook-Pro.local>
2018-09-05 13:14:38 +0100

    Release 19.0.0 license key updated

--- packages/ag-grid-enterprise/src/licenseManager.ts
+++ packages/ag-grid-enterprise/src/licenseManager.ts
@@ -4,7 +4,7 @@ import {MD5} from './license/md5';
 
 @Bean('licenseManager')
 export class LicenseManager {
-    private static RELEASE_INFORMATION:string = 'MTUzMDg3ODc3OTgxMA==';
+    private static RELEASE_INFORMATION:string = 'MTUzNjE0OTU5Nzk2OA==';
     private static licenseKey:string;
 
     @Autowired('md5') private md5:MD5;
