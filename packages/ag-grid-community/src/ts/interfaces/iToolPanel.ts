17e06c74ec8289cd4cd32c4d501aa18c4821f364
Robert Clarke <robertclarke@Roberts-MacBook-Pro.local>
2018-09-06 13:25:03 +0100

    AG-1915 - updated tool panel examples

--- packages/ag-grid-community/src/ts/interfaces/iToolPanel.ts
+++ packages/ag-grid-community/src/ts/interfaces/iToolPanel.ts
@@ -1,11 +1,8 @@
 import {IComponent} from "./iComponent";
 import {GridApi} from "../gridApi";
-import {ColumnApi} from "../columnController/columnApi";
 
 export interface IToolPanelParams {
     api: GridApi;
-    columnApi: ColumnApi;
-    context: any;
 }
 
 export interface IToolPanel {
