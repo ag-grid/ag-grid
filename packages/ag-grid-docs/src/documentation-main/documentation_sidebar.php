caf5b3a6513f8da14bc549b6681509d849a06410
rmc-software <rmc-software-consultancy@gmail.com>
2018-10-30 16:20:08 +0000

    release v19.1 side bar changes

--- packages/ag-grid-docs/src/documentation-main/documentation_sidebar.php
+++ packages/ag-grid-docs/src/documentation-main/documentation_sidebar.php
@@ -1,119 +1,154 @@
 <div id="side-bar-announcements">
-    <p>Work at ag-Grid</a>:</p>
-
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title"><a
-                        href="../ag-grid-jobs-board/">JavaScript Developers</a></h5>
-            <h6 class="card-subtitle mb-2 text-muted">London, United Kingdom</h6>
-            <p class="card-text">
-                Would you like to join the ag-Grid team in London<br>
-                <br>
-                Check the <a href="../ag-grid-jobs-board/">jobs board</a>
-            </p>
-
-        </div>
-    </div>
-    <br>
-    <hr>
-    <p>Latest Release:</p>
-
-    <div class="card card-highlight">
-        <div class="card-body">
-            <h5 class="card-title"><a href="../ag-grid-changelog/?fixVersion=19.1.0">Version 19.1.0 Released</a></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Oct 30</h6>
-            <p class="card-text">
-                Minor release with the following feature highlights:
-            <ul>
-                <li>
-                    <a class="learn-more" href="https://www.ag-grid.com/best-polymer-data-grid/">Polymer 3 Datagrid</a>.
-                </li>
-                <li>
-                    <a class="learn-more" href="https://www.ag-grid.com/javascript-grid-excel/">Export to XLSX</a>.
-                </li>
-                <li>
-                    <a class="learn-more"
-                       href="https://www.ag-grid.com/javascript-grid-column-definitions/#column-changes/">Allow Delta
-                        Changes to
-                        Column Definitions</a>.
-                </li>
-            </ul>
-            </p>
-
-            <p class="text-right">
-                <a class="learn-more" href="../ag-grid-changelog/?fixVersion=19.1.0">Change Log</a>
-            </p>
-        </div>
-    </div>
-
-    <br>
-
-    <div class="card card-highlight">
-        <div class="card-body">
-            <h5 class="card-title"><a href="../ag-grid-changelog/?fixVersion=19.0.0">Version 19.0 Released</a></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Sep 7</h6>
-            <p class="card-text">
-                Major release with the following feature highlights:
-            <ul>
-                <li><a class="learn-more" href="../javascript-grid-status-bar/">Customizable Status Bar</a></li>
-            </ul>
-            <ul>
-                <li><a class="learn-more" href="../javascript-grid-tool-panel-filters/">Filters Tool Panel</a></li>
-            </ul>
-            <ul>
-                <li><a class="learn-more" href="../javascript-grid-tool-panel-component/">Custom Tool Panels</a></li>
-            </ul>
-            <ul>
-                <li><a class="learn-more" href="../javascript-grid-server-side-model-tree-data/">Server-side Tree Data</a></li>
-            </ul>
-            </p>
-            <p class="text-right">
-                <a class="learn-more" href="../ag-grid-changelog/?fixVersion=19.0.0">Change Log</a>
-            </p>
-        </div>
-    </div>
-
-    <p>From our <a href="https://medium.com/ag-grid/">blog</a>:</p>
-
-    <div class="card">
-        <div class="card-body">
-            <h5 class="card-title"><a
-                        href="https://medium.com/ag-grid/version-18-of-ag-grid-is-now-available-cc3f39a12441">Introducing
-                    Version 18 of ag-Grid</a></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Jun 15</h6>
-            <p class="card-text">Version 18 consists of a major features changes, so besides bug fixes and enhancing
-                performance, we have added a number of interesting changes. Read our blog below.</p>
-
-            <p class="text-right">
-                <a class="learn-more"
-                   href="https://medium.com/ag-grid/data-visualisation-for-enterprise-with-angular-and-d3-54165a2778a6">Full
-                    Article</a>
-            </p>
-            <h5 class="card-title"><a
-                        href="https://medium.com/ag-grid/a-plain-english-introduction-to-json-web-tokens-jwt-what-it-is-and-what-it-isnt-8076ca679843">A
-                    plain English introduction to JSON web tokens (JWT)</a></h5>
-            <h6 class="card-subtitle mb-2 text-muted">Jun 26</h6>
-            <p class="card-text">A short yet comprehensive overview of the JWT's structure, usages and security
-                features</p>
-
-            <p class="text-right">
-                <a class="learn-more"
-                   href="https://medium.com/ag-grid/a-plain-english-introduction-to-json-web-tokens-jwt-what-it-is-and-what-it-isnt-8076ca679843">Full
-                    Article</a>
-        </div>
-    </div>
-
-
-    <p>Upcoming events we will<br> be attending and sponsoring:</p>
-
-    <div class="card">
-        <div class="card-body">
-            <dl id="upcoming-events">
-                <dt><a href="https://angularconnect.com/" target="_blank" rel="noopener">Angular Connect</a></dt>
-                <dd class="date">November 6-7<br> London, England</dd>
-                <dd>Platinum Sponsor</dd>
-            </dl>
-        </div>
-    </div>
 
 </div>
+<br>
+<p>Latest Release:</p>
+
+<div class="card card-highlight">
+    <div class="card-body">
+        <h5 class="card-title"><a href="../ag-grid-changelog/?fixVersion=19.1.0">Version 19.1.0 Released</a></h5>
+        <h6 class="card-subtitle mb-2 text-muted">Oct 30</h6>
+        <p class="card-text">
+            Minor release with the following feature highlights:
+        <ul>
+            <li>
+                <a class="learn-more" href="https://www.ag-grid.com/best-polymer-data-grid/">Polymer 3 Datagrid</a>.
+            </li>
+            <li>
+                <a class="learn-more" href="https://www.ag-grid.com/javascript-grid-excel/">Export to XLSX</a>.
+            </li>
+            <li>
+                <a class="learn-more"
+                   href="https://www.ag-grid.com/javascript-grid-column-definitions/#column-changes/">Allow Delta
+                    Changes to
+                    Column Definitions</a>.
+            </li>
+        </ul>
+        </p>
+
+        <p class="text-right">
+            <a class="learn-more" href="../ag-grid-changelog/?fixVersion=19.1.0">Change Log</a>
+        </p>
+    </div>
+</div>
+
+<hr>
+<hr>
+
+<!-- Begin Mailchimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+    /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+    <form action="https://ag-grid.us11.list-manage.com/subscribe/post?u=9b44b788c97fa5b498fbbc9b5&amp;id=9353cf87ce" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+        <div id="mc_embed_signup_scroll">
+            <h2>ag-Grid Mailing List</h2>
+            <div class="mc-field-group">
+                <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                </label>
+                <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+            </div>
+            <div class="mc-field-group">
+                <label for="mce-FNAME">First Name </label>
+                <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+            </div>
+            <br>
+            <div class="mc-field-group input-group">
+                <strong>Select your preference</strong>
+                <ul><li><input type="checkbox" value="1" name="group[18365][1]" id="mce-group[18365]-18365-0"><label for="mce-group[18365]-18365-0"> Angular</label></li>
+                    <li><input type="checkbox" value="2" name="group[18365][2]" id="mce-group[18365]-18365-1"><label for="mce-group[18365]-18365-1"> React</label></li>
+                    <li><input type="checkbox" value="4" name="group[18365][4]" id="mce-group[18365]-18365-2"><label for="mce-group[18365]-18365-2"> General</label></li>
+                </ul>
+            </div>
+            <div id="mce-responses" class="clear">
+                <div class="response" id="mce-error-response" style="display:none"></div>
+                <div class="response" id="mce-success-response" style="display:none"></div>
+            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_9b44b788c97fa5b498fbbc9b5_9353cf87ce" tabindex="-1" value=""></div>
+            <div class="clear"><input type="submit" style="background-color: #0084e7" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+        </div>
+    </form>
+</div>
+<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[3]='ADDRESS';ftypes[3]='address';fnames[4]='PHONE';ftypes[4]='phone';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+<!--End mc_embed_signup-->
+
+<div id="side-bar-announcements" style="margin-top: 10px">
+    <br>
+
+    <br>
+    <div id="ag-grid-conf">
+        <p>Upcoming events</p>
+        <div class="card card-highlight">
+            <div class="card-body">
+                <h5 class="card-title"><a href="https://www.ag-grid-conf.com">ag-Grid Conf</a></h5>
+                <h6 class="card-subtitle mb-2 text-muted">London</h6>
+                <p class="card-text">
+                    A half day conference all about ag-Grid in London.<br><br>
+                    <strong>
+                        <img class="card-image" src="../images/largeFlags/gb2.png">
+                        London, Nov 29th
+                    </strong>
+                </p>
+                To register follow the link below<br><br>
+                <p class="text-left">
+                    <a class="learn-more" href="https://www.ag-grid-conf.com" target="_blank"><strong>Register</strong></a>
+                </p>
+                <hr>
+                <h5 class="card-title"><a href="https://angularconnect.com/" target="_blank" rel="noopener">Angular Connect</a></h5>
+                <h6 class="card-subtitle mb-2 text-muted">November 6-7<br>London, England</h6>
+                <dd>Platinum Sponsor</dd>
+                <hr>
+                <h5 class="card-title"><a href="https://finjs.io" target="_blank" rel="noopener">FinJS</a></h5>
+                <dd>ag-Grid is partnering with FinJS, join us for these excellent event.</dd>
+                <br>
+                <img class="card-image" src="../images/largeFlags/gb2.png">
+                London, Nov 13th<br><br>
+                <p class="text-left">
+                    <a class="learn-more" href="https://finjs.io" target="_blank"><strong>Find out more</strong></a>
+                </p>
+            </div>
+        </div>
+
+        <hr>
+
+        <br>
+        <p>From our <a href="https://medium.com/ag-grid/">blog</a>:</p>
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title"><a
+                            href="https://medium.com/ag-grid/learn-to-customize-react-grid-in-less-than-10-minutes-2ce6845646bb">Learn to customize React grid in less than 10 minutes</a></h5>
+                <h6 class="card-subtitle mb-2 text-muted">Oct 16</h6>
+                <p class="card-text">Implement custom filtering logic, cell editor and renderer in this walk through.</p>
+                <p class="text-right">
+                    <a class="learn-more"
+                       href="https://medium.com/ag-grid/learn-to-customize-react-grid-in-less-than-10-minutes-2ce6845646bb">Full
+                        Article</a>
+                </p>
+                <h5 class="card-title"><a
+                            href="https://medium.com/ag-grid/learn-to-customize-angular-grid-in-less-than-10-minutes-88005d53808d?source=collection_home---2------0---------------------">Learn to customize Angular grid in less than 10 minutes</a></h5>
+                <h6 class="card-subtitle mb-2 text-muted">Oct 16</h6>
+                <p class="card-text">Implement custom filtering logic, cell editor and renderer in this walk through.</p>
+                <p class="text-right">
+                    <a class="learn-more"
+                       href="https://medium.com/ag-grid/learn-to-customize-angular-grid-in-less-than-10-minutes-88005d53808d?source=collection_home---2------0---------------------">Full Article
+                    </a>
+                </p>
+            </div>
+        </div>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <p>Work at ag-Grid</a>:</p>
+                <h5 class="card-title"><a
+                            href="../ag-grid-jobs-board/">JavaScript Developers</a></h5>
+                <h6 class="card-subtitle mb-2 text-muted">London, United Kingdom</h6>
+                <p class="card-text">
+                    Would you like to join the ag-Grid team in London<br>
+                    <br>
+                    Check the <a href="../ag-grid-jobs-board/">jobs board</a>
+                </p>
+            </div>
+        </div>
