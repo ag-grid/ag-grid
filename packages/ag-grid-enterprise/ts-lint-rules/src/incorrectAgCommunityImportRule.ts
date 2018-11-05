635a4823b1977cb26167ddd0b92c9942c60446dc
Sean Landsman <sean@thelandsmans.com>
2018-11-05 13:19:40 +0000

    AG-2268 Add custom tslint to catch relative imports of community in enterprise

---
+++ packages/ag-grid-enterprise/ts-lint-rules/src/incorrectAgCommunityImportRule.ts
@@ -0,0 +1,23 @@
+import * as ts from "typescript";
+import * as Lint from "tslint";
+
+export class Rule extends Lint.Rules.AbstractRule {
+    public static FAILURE_STRING = "Relative import of ag-Grid Community";
+
+    public apply(sourceFile: ts.SourceFile): Lint.RuleFailure[] {
+        return this.applyWithWalker(new NoImportsWalker(sourceFile, this.getOptions()));
+    }
+}
+
+// The walker takes care of all the work.
+class NoImportsWalker extends Lint.RuleWalker {
+    public visitImportDeclaration(node: ts.ImportDeclaration) {
+
+        if(node.getText().indexOf('../ag-grid-community') !== -1) {
+            this.addFailureAt(node.getStart(), node.getWidth(), Rule.FAILURE_STRING);
+        }
+
+        // call the base version of this visitor to actually parse this node
+        super.visitImportDeclaration(node);
+    }
+}
