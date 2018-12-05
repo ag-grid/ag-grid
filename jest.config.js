3bdce84ed0b0913e475831bb2ddab88ad2ef2410
Vitaly Kravchenko <vitalyx@gmail.com>
2018-12-05 11:47:24 +0300

    Add unit testing support with Jest.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "roots": [
+        "<rootDir>/packages/ag-grid-enterprise/src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+};
