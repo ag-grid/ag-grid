f7bc0d7872c025b3e09c8b9359ef5da6f775b7bd
Sean Landsman <sean@thelandsmans.com>
2018-12-06 08:56:51 +0000

    AG-2272 Re-enable vue examples in docs

---
+++ packages/ag-grid-vue/gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp');
+const gulpTypescript = require('gulp-typescript');
+const typescript = require('typescript');
+const merge = require('merge2');
+
+gulp.task('default', ['src']);
+
+gulp.task('src', () => {
+    const tsProject = gulpTypescript.createProject('./tsconfig-lib.json');
+
+    const tsResult = gulp
+        .src('./src/AgGridVue.ts', {typescript: typescript})
+        .pipe(tsProject());
+
+    return merge([
+        tsResult.dts
+            .pipe(gulp.dest('./lib')),
+        tsResult.js
+            .pipe(gulp.dest('./lib'))
+    ]);
+});
+
+gulp.task('watch', ['src'], () => {
+    gulp.watch([
+            './src/*',
+            './node_modules/ag-grid-community/dist/lib/**/*'],
+        ['src']);
+});
