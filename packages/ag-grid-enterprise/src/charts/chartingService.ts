fa5d1bd2c5833756dfdbd00000c7d293ddfb4bcc
Vitaly Kravchenko <vitalyx@gmail.com>
2018-12-12 01:37:32 +0300

    AG-2423 - Horizontal bar charts.

--- packages/ag-grid-enterprise/src/charts/chartingService.ts
+++ packages/ag-grid-enterprise/src/charts/chartingService.ts
@@ -26,21 +26,22 @@ export class ChartingService {
             }
         });
 
-        showBasicBarChart(values);
+        showVerticalBarChart(values);
+        showHorizontalBarChart(values);
     }
 }
 
-function showBasicBarChart(data: number[]) {
+function showVerticalBarChart(data: number[]) {
     const padding = {
         top: 20,
-        right: 60,
+        right: 40,
         bottom: 40,
         left: 40
     };
     const n = data.length;
     const xData = data.map((d, i) => i.toString());
     const yData = data;
-    const canvasWidth = 640;
+    const canvasWidth = document.body.getBoundingClientRect().width;
     const canvasHeight = 480;
     const seriesWidth = canvasWidth - padding.left - padding.right;
     const seriesHeight = canvasHeight - padding.top - padding.bottom;
@@ -75,8 +76,8 @@ function showBasicBarChart(data: number[]) {
         ctx.fillStyle = 'black';
 
         const label = value.toString();
-        const w = ctx.measureText(label).width;
-        ctx.fillText(label, x + bandwidth / 2 - w / 2, seriesHeight - 20);
+        const labelWidth = ctx.measureText(label).width;
+        ctx.fillText(label, x + bandwidth / 2 - labelWidth / 2, y + 20);
     }
     ctx.restore();
 
@@ -91,4 +92,68 @@ function showBasicBarChart(data: number[]) {
     xAxis.translation = [padding.left, padding.top + seriesHeight];
     xAxis.flippedLabels = true;
     xAxis.render(ctx);
+}
+
+function showHorizontalBarChart(data: number[]) {
+    const padding = {
+        top: 20,
+        right: 40,
+        bottom: 40,
+        left: 40
+    };
+    const n = data.length;
+    const xData = data.map((d, i) => i.toString());
+    const yData = data;
+    const canvasWidth = document.body.getBoundingClientRect().width;
+    const canvasHeight = 480;
+    const seriesWidth = canvasWidth - padding.left - padding.right;
+    const seriesHeight = canvasHeight - padding.top - padding.bottom;
+
+    const xScale = scaleLinear();
+    xScale.domain = [0, Math.max(...yData)];
+    xScale.range = [0, seriesWidth];
+
+    const yScale = new BandScale<string>();
+    yScale.domain = xData;
+    yScale.range = [seriesHeight, 0];
+    yScale.paddingInner = 0.1;
+    yScale.paddingOuter = 0.3;
+    let bandwidth = yScale.bandwidth;
+
+    const canvas = createHdpiCanvas(canvasWidth, canvasHeight);
+    document.body.appendChild(canvas);
+
+    const ctx = canvas.getContext('2d')!;
+    ctx.font = '14px Verdana';
+    ctx.textBaseline = 'middle';
+
+    // bars
+    ctx.save();
+    ctx.translate(padding.left, padding.top);
+    for (let i = 0; i < n; i++) {
+        const category = xData[i];
+        const value = yData[i];
+        const x = xScale.convert(value);
+        const y = yScale.convert(category);
+        ctx.fillStyle = '#4983B2';
+        ctx.fillRect(0, y, x, bandwidth);
+        ctx.fillStyle = 'black';
+
+        const label = value.toString();
+        const labelWidth = ctx.measureText(label).width;
+        ctx.fillText(label, x - labelWidth - 10, y + bandwidth / 2);
+    }
+    ctx.restore();
+
+    // x-axis
+    const xAxis = new Axis<number>(xScale);
+    xAxis.rotation = -Math.PI / 2;
+    xAxis.translation = [padding.left, padding.top + seriesHeight];
+    xAxis.flippedLabels = true;
+    xAxis.render(ctx);
+
+    // y-axis
+    const yAxis = new Axis<string>(yScale);
+    yAxis.translation = [padding.left, padding.top];
+    yAxis.render(ctx);
 }
