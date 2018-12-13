40e7a6181f4a616a13225cb52b1eba0fc5f48570
Vitaly Kravchenko <vitalyx@gmail.com>
2018-12-13 13:55:53 +0300

    AG-2424 - basic stacked bar chart example.

--- packages/ag-grid-enterprise/src/charts/chartingService.ts
+++ packages/ag-grid-enterprise/src/charts/chartingService.ts
@@ -26,8 +26,9 @@ export class ChartingService {
             }
         });
 
-        showVerticalBarChart(values);
-        showHorizontalBarChart(values);
+        // showVerticalBarChart(values);
+        // showHorizontalBarChart(values);
+        showStackedBarChart();
     }
 }
 
@@ -156,4 +157,155 @@ function showHorizontalBarChart(data: number[]) {
     const yAxis = new Axis<string>(yScale);
     yAxis.translation = [padding.left, padding.top];
     yAxis.render(ctx);
+}
+
+function drawRect(ctx: CanvasRenderingContext2D,
+                  x1: number, y1: number, x2: number, y2: number,
+                  fill?: string, stroke?: string) {
+    if (fill) {
+        ctx.fillStyle = fill;
+    }
+    if (stroke) {
+        ctx.strokeStyle = stroke;
+    }
+    const width = x2 - x1;
+    const height = y2 - y1;
+    ctx.fillRect(x1, y1, width, height);
+    ctx.strokeRect(x1, y1, width, height);
+}
+
+function showStackedBarChart() {
+    const data = [
+        {
+            category: 'Coffee',
+
+            q1Budget: 500,
+            q2Budget: 500,
+            q3Budget: 500,
+            q4Budget: 500,
+
+            q1Actual: 450,
+            q2Actual: 560,
+            q3Actual: 600,
+            q4Actual: 700
+        },
+        {
+            category: 'Tea',
+
+            q1Budget: 350,
+            q2Budget: 400,
+            q3Budget: 450,
+            q4Budget: 500,
+
+            q1Actual: 270,
+            q2Actual: 380,
+            q3Actual: 450,
+            q4Actual: 520
+        },
+        {
+            category: 'Milk',
+
+            q1Budget: 200,
+            q2Budget: 180,
+            q3Budget: 180,
+            q4Budget: 180,
+
+            q1Actual: 180,
+            q2Actual: 170,
+            q3Actual: 190,
+            q4Actual: 200
+        },
+    ];
+
+    const yFields = ['q1Actual', 'q2Actual', 'q3Actual', 'q4Actual'];
+    const yFieldNames = ['Q1', 'Q2', 'Q3', 'Q4'];
+    const colors = [
+        '#94ae0a',
+        '#115fa6',
+        '#a61120',
+        '#ff8809',
+        '#ffd13e',
+        '#a61187',
+        '#24ad9a',
+    ];
+
+    const padding = {
+        top: 20,
+        right: 40,
+        bottom: 40,
+        left: 60
+    };
+    const n = data.length;
+    const xData = data.map(d => d.category);
+    // For each category returns an array of values representing the top
+    // of each bar in the stack, the last value being the height of the stack.
+    const yData = data.map(datum => {
+        const values: number[] = [];
+        let sum = 0;
+        yFields.forEach(field => values.push(sum += (datum as any)[field]));
+        return values;
+    });
+    const canvasWidth = document.body.getBoundingClientRect().width;
+    const canvasHeight = 480;
+    const seriesWidth = canvasWidth - padding.left - padding.right;
+    const seriesHeight = canvasHeight - padding.top - padding.bottom;
+
+    const yScale = scaleLinear();
+    // Get the height of each stack and find the highest one.
+    yScale.domain = [0, Math.max(...yData.map(d => d[d.length - 1]))];
+    yScale.range = [seriesHeight, 0];
+
+    const xScale = new BandScale<string>();
+    xScale.domain = xData;
+    xScale.range = [0, seriesWidth];
+    xScale.paddingInner = 0.1;
+    xScale.paddingOuter = 0.3;
+    let bandwidth = xScale.bandwidth;
+
+    const canvas = createHdpiCanvas(canvasWidth, canvasHeight);
+    document.body.appendChild(canvas);
+
+    const ctx = canvas.getContext('2d')!;
+    ctx.font = '14px Verdana';
+
+    // bars
+    ctx.save();
+    ctx.translate(padding.left, padding.top);
+    for (let i = 0; i < n; i++) {
+        const category = xData[i];
+        const values = yData[i];
+        const x = xScale.convert(category);
+        // Using reduce here simply to get pairs of number in the `values` array:
+        // the bottom and top of each bar.
+        let j = 0;
+        values.reduce((bottom, top) => {
+            const yBottom = yScale.convert(bottom);
+            const yTop = yScale.convert(top);
+
+            ctx.fillStyle = colors[j % colors.length];
+            ctx.fillRect(x, yTop, bandwidth, yBottom - yTop);
+            ctx.strokeRect(x, yTop, bandwidth, yBottom - yTop);
+
+            const label = yFieldNames[j] + ': ' + (data[i] as any)[yFields[j]].toString();
+            const labelWidth = ctx.measureText(label).width;
+            ctx.fillStyle = 'black';
+            ctx.fillText(label, x + bandwidth / 2 - labelWidth / 2, yTop + 20);
+
+            j++;
+            return top;
+        }, 0);
+    }
+    ctx.restore();
+
+    // y-axis
+    const yAxis = new Axis<number>(yScale);
+    yAxis.translation = [padding.left, padding.top];
+    yAxis.render(ctx);
+
+    // x-axis
+    const xAxis = new Axis<string>(xScale);
+    xAxis.rotation = -Math.PI / 2;
+    xAxis.translation = [padding.left, padding.top + seriesHeight];
+    xAxis.flippedLabels = true;
+    xAxis.render(ctx);
 }
