ac2d5d74c3d95517833615d013813147c218286a
Guilherme Lopes <guilherme@gwm-solutions.com>
2018-12-15 19:47:36 -0700

    AG-2235 - Sass themes update (#2831)
    * AG-2235 - moved theme files into structured folders
    * fixed the build process
    * theme docs update

--- packages/ag-grid-docs/src/javascript-grid-themes/balham-theme.php
+++ packages/ag-grid-docs/src/javascript-grid-themes/balham-theme.php
@@ -39,7 +39,7 @@ include '../documentation-main/documentation_header.php';
 // Set the colors to blue and amber
 $active: #E91E63; // pink-500 from https://www.materialui.co/colors
 // Import the ag-Grid balham theme
-@import '~ag-grid/src/styles/ag-theme-balham';</snippet>
+@import '~ag-grid/src/styles/ag-theme-balham/sass/ag-theme-balham';</snippet>
 
 <p>You can examine the full list of the variables that can be changed in the <a href="https://github.com/ag-grid/ag-grid/blob/latest/src/styles/ag-theme-balham.scss">theme source file</a>.</p>
 
