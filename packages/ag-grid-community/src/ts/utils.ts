c6454c4b7017b7b816f6d7149f2b7efbc8607a2c
Guilherme Lopes <guilherme@gwm-solutions.com>
2018-12-18 07:51:51 -0700

    AG-2325 - SVG icon assignment not working on IE11

--- packages/ag-grid-community/src/ts/utils.ts
+++ packages/ag-grid-community/src/ts/utils.ts
@@ -683,10 +683,12 @@ export class Utils {
                 const cssClasses = element.className.split(' ');
                 if (cssClasses.indexOf(className) < 0) {
                     cssClasses.push(className);
-                    element.className = cssClasses.join(' ');
+                    element.setAttribute('class', cssClasses.join(' '));
                 }
             } else {
-                element.className = className;
+                // do not use element.classList = className here, it will cause
+                // a read-only assignment error on some browsers (IE/Edge).
+                element.setAttribute('class', className);
             }
         }
     }
@@ -750,7 +752,7 @@ export class Utils {
                     while (cssClasses.indexOf(className) >= 0) {
                         cssClasses.splice(cssClasses.indexOf(className), 1);
                     }
-                    element.className = cssClasses.join(' ');
+                    element.setAttribute('class', cssClasses.join(' '));
                 }
             }
         }
