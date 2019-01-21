b0d11b03cfa26096a6e1100d5d8fa7d97131b11d
Vitaly Kravchenko <vitalyx@gmail.com>
2019-01-21 14:36:37 +0300

    Node: transformPoint method.

--- packages/ag-grid-enterprise/src/charts/scene/node.ts
+++ packages/ag-grid-enterprise/src/charts/scene/node.ts
@@ -36,6 +36,25 @@ export abstract class Node { // Don't confuse with `window.Node`.
     protected matrix = new Matrix();
     protected inverseMatrix = new Matrix();
 
+    /**
+     * Calculates the combined inverse transformation for this node,
+     * and uses it to convert the given transformed point
+     * to the untransformed one.
+     * @param x
+     * @param y
+     */
+    transformPoint(x: number, y: number) {
+        const matrix = Matrix.flyweight(this.matrix);
+
+        let parent = this.parent;
+        while (parent) {
+            matrix.preMultiplySelf(parent.matrix);
+            parent = parent.parent;
+        }
+
+        return matrix.invertSelf().transformPoint(x, y);
+    }
+
     // TODO: should this be `true` by default as well?
     private _dirtyTransform = false;
     set dirtyTransform(value: boolean) {
