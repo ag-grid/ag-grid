f998e3587728b6a87562282fc7d51606d3b4f1ca
Sean Landsman <sean@thelandsmans.com>
2019-03-18 10:27:44 +0000

    AG-2731 AngularJS, cellRenderer callBack called too many times when deltaRowDataMode enabled AG-2732 AngularJs, on rowClicked, params.data has a property value appended to it.

--- packages/ag-grid-community/protractor.conf.js
+++ packages/ag-grid-community/protractor.conf.js
@@ -3,5 +3,11 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: [
         'e2e/*spec.js'
-    ]
+    ],
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            args: ['--headless', '--no-sandbox', '--disable-extensions', '--disable-dev-shm-usage']
+        }
+    }
 };
