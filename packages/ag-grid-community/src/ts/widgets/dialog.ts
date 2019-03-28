f04ad694be83003228a99303fb4a8c2555e526d9
Guilherme Lopes <guilherme@gwm-solutions.com>
2019-03-28 13:09:12 +0000

    AG-2794 - added resize

--- packages/ag-grid-community/src/ts/widgets/dialog.ts
+++ packages/ag-grid-community/src/ts/widgets/dialog.ts
@@ -1,4 +1,4 @@
-import { DragService } from "../dragAndDrop/dragService";
+import { DragService, DragListenerParams } from "../dragAndDrop/dragService";
 import { RefSelector } from "./componentAnnotations";
 import { Autowired, PostConstruct } from "../context/context";
 import { PopupService } from "./popupService";
@@ -23,6 +23,11 @@ interface DialogOptions {
     resizable?: boolean | ResizableStructure;
     closable?: boolean;
     title?: string;
+    width?: number;
+    height?: number;
+    x?: number;
+    y?: number;
+    centered?: boolean;
 }
 
 export class Dialog extends PopupComponent {
@@ -51,9 +56,16 @@ export class Dialog extends PopupComponent {
     private movable = false;
     private closable = true;
     private isMoving = false;
+    private isResizing = false;
+
     private position = {
         x: 0,
         y: 0
+    };
+
+    private size = {
+        width: 0,
+        height: 0
     };
 
     public static DESTROY_EVENT = 'destroy';
@@ -84,7 +96,8 @@ export class Dialog extends PopupComponent {
 
     @PostConstruct
     protected postConstruct() {
-        const { resizable, movable, closable, title } = this.config;
+        const { resizable, movable, closable, title, width, height } = this.config;
+        const eGui = this.getGui();
 
         if (resizable) {
             this.setResizable(resizable);
@@ -96,6 +109,16 @@ export class Dialog extends PopupComponent {
 
         this.setMovable(!!movable);
         this.setClosable(!!closable);
+
+        if (width) {
+            _.setFixedWidth(eGui, width);
+            this.size.width = width;
+        }
+
+        if (height) {
+            _.setFixedHeight(eGui, height);
+            this.size.height = height;
+        }
 
         this.close = this.popupService.addPopup(
             false,
@@ -136,64 +159,139 @@ export class Dialog extends PopupComponent {
             };
         }
 
-        const eGui = this.getGui();
-
         Object.keys(resizable).forEach(side => {
             const r = resizable as ResizableStructure;
             const s = side as ResizableSides;
             const val = !!r[s];
             const el = this.getResizerElement(s);
 
+            const params: DragListenerParams = {
+                eElement: el,
+                onDragStart: this.onDialogResizeStart.bind(this),
+                onDragging: (e: MouseEvent) => this.onDialogResize(e, s),
+                onDragStop: this.onDialogResizeEnd.bind(this),
+            };
+
             if (!!this.resizable[s] !== val) {
                 if (val) {
-                    console.log(`add ${s} listeners`);
+                    this.dragService.addDragSource(params);
+                    el.style.pointerEvents = 'all';
                 } else {
-                    console.log(`remove ${s} listeners`);
+                    this.dragService.addDragSource(params);
+                    el.style.pointerEvents = 'none';
                 }
             }
         });
+    }
+
+    private onDialogResizeStart() {
+        this.isResizing = true;
+        this.refreshSize();
+    }
+
+    private onDialogResize(e: MouseEvent, side: ResizableSides) {
+        if (!this.isResizing) { return; }
+
+        const eGui = this.getGui();
+        const isLeft = !!side.match(/left/i);
+        const isRight = !!side.match(/right/i);
+        const isTop = !!side.match(/top/i);
+        const isBottom = !!side.match(/bottom/i);
+        const isHorizontal = isLeft || isRight;
+        const isVertical = isTop || isBottom;
+
+        let offsetLeft = 0;
+        let offsetTop = 0;
+
+        if (isHorizontal) {
+            const direction = isLeft ? -1 : 1;
+            const newWidth = Math.max(0, Math.min(this.size.width + (e.movementX * direction), (eGui.offsetParent as HTMLElement).offsetWidth));
+            _.setFixedWidth(eGui, newWidth);
+
+            if (isLeft) {
+                offsetLeft = this.size.width - newWidth;
+            }
+
+            this.size.width = newWidth;
+        }
+
+        if (isVertical) {
+            const direction = isTop ? -1 : 1;
+            const newHeight = Math.max(0, Math.min(this.size.height + (e.movementY * direction), (eGui.offsetParent as HTMLElement).offsetHeight));
+            _.setFixedHeight(eGui, newHeight);
+
+            if (isTop) {
+                offsetTop = this.size.height - newHeight;
+            }
+
+            this.size.height = newHeight;
+        }
+
+        if (offsetLeft || offsetTop) {
+            this.offsetDialog(
+                this.position.x + offsetLeft,
+                this.position.y + offsetTop
+            );
+        }
+    }
+
+    private onDialogResizeEnd() {
+        this.isResizing = false;
+    }
+
+    private refreshSize() {
+        const { width, height } = this.size;
+
+        if (!width) {
+            this.size.width = this.getGui().offsetWidth;
+        }
+
+        if (!height) {
+            this.size.height = this.getGui().offsetHeight;
+        }
     }
 
     public setMovable(movable: boolean) {
         if (movable !== this.movable) {
             this.movable = movable;
 
-            if (movable) {
-                this.dragService.addDragSource({
-                    eElement: this.eTitleBar,
-                    onDragStart: this.onDialogMoveStart.bind(this),
-                    onDragging: this.onDialogMove.bind(this),
-                    onDragStop: this.onDialogMoveEnd.bind(this)
-                });
-            }
+            const params: DragListenerParams = {
+                eElement: this.getGui(),
+                onDragStart: this.onDialogMoveStart.bind(this),
+                onDragging: this.onDialogMove.bind(this),
+                onDragStop: this.onDialogMoveEnd.bind(this)
+            };
+
+            this.dragService[movable ? 'addDragSource' : 'removeDragSource'](params);
         }
-
-        const eGui = this.getGui();
-
-        _.addOrRemoveCssClass(eGui, 'ag-dialog-movable', movable);
     }
 
-    onDialogMoveStart() {
+    private onDialogMoveStart() {
         this.isMoving = true;
     }
 
-    onDialogMove(e: MouseEvent) {
+    private onDialogMove(e: MouseEvent) {
         if (!this.isMoving) { return; }
         const { x, y } = this.position;
-        const eGui = this.getGui();
+
+        this.offsetDialog(x + e.movementX, y + e.movementY);
+    }
+
+    private offsetDialog(x: number, y: number) {
+        const ePopup = this.getGui();
 
         this.popupService.positionPopup({
-            ePopup: this.getGui(),
-            x: x + e.movementX,
-            y: y + e.movementY,
+            ePopup,
+            x,
+            y,
             keepWithinBounds: true
         });
 
-        this.position.x = parseInt(eGui.style.left, 10);
-        this.position.y = parseInt(eGui.style.top, 10);
+        this.position.x = parseInt(ePopup.style.left, 10);
+        this.position.y = parseInt(ePopup.style.top, 10);
     }
 
-    onDialogMoveEnd() {
+    private onDialogMoveEnd() {
         this.isMoving = false;
     }
 
