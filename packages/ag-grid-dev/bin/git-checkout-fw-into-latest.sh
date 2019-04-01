a123f126a3337384bf04f71f1bc183713a804c3a
Guilherme Lopes <guilherme@gwm-solutions.com>
2019-04-01 12:57:19 +0100

    removed ag-grid-dev

--- packages/ag-grid-dev/bin/git-checkout-fw-into-latest.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ "$#" -ne 1 ]
-  then
-    echo "You must supply the framework as the as first parameter  (ie angular, react, polymer etc)"
-    exit 1
-fi
-
-git-checkout-fw-into-existing-branch.sh latest $1
--- packages/ag-grid-dev/gulp-tasks/clean-nm-ag-grid.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var del = require('del');
-
-gulp.task('clean-nm-ag-grid', cleanNmAgGrid);
-
-function cleanNmAgGrid() {
-    del(['node_modules/ag-grid', '!node_modules']);
-}
