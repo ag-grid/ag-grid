7e8b402f48950c2c43908d38cbe1018b8ceb1d87
Guilherme Lopes <guilherme@gwm-solutions.com>
2019-04-09 18:59:11 +0100

    AG-1630 - enhanced fillHandle selection with pinned columns and rows

--- packages/ag-grid-enterprise/src/widgets/selection/fillHandle.ts
+++ packages/ag-grid-enterprise/src/widgets/selection/fillHandle.ts
@@ -1,16 +1,17 @@
 import {
     Autowired,
     CellComp,
+    CellNavigationService,
     CellPosition,
     CellRange,
-    Column,
+    ColumnController,
     Component,
     DragService,
     IFillHandle,
-    GridOptionsWrapper,
     MouseEventService,
     PostConstruct,
     RowPosition,
+    RowPositionUtils,
     RowRenderer,
     _,
 } from 'ag-grid-community';
@@ -22,7 +23,8 @@ export class FillHandle extends Component implements IFillHandle {
     @Autowired("dragService") private dragService: DragService;
     @Autowired("rangeController") private rangeController: RangeController;
     @Autowired("mouseEventService") private mouseEventService: MouseEventService;
-    @Autowired("gridOptionsWrapper") gridOptionsWrapper: GridOptionsWrapper;
+    @Autowired("columnController") private columnController: ColumnController;
+    @Autowired("cellNavigationService") private cellNavigationService: CellNavigationService;
 
     private cellComp: CellComp;
     private cellRange: CellRange;
@@ -85,7 +87,7 @@ export class FillHandle extends Component implements IFillHandle {
 
         if (this.lastCellHovered && this.lastCellHovered !== this.lastCellMarked) {
             this.lastCellMarked = this.lastCellHovered;
-            this.markPathFrom(this.cellComp, this.lastCellHovered);
+            this.markPathFrom(this.cellComp.getCellPosition(), this.lastCellHovered);
         }
     }
 
@@ -95,21 +97,22 @@ export class FillHandle extends Component implements IFillHandle {
             if (!this.isUp && !this.isLeft) {
                 this.rangeController.extendLatestRangeToCell({
                     rowIndex: isX ? this.rangeEndRow.rowIndex : this.lastCellMarked!.rowIndex,
-                    rowPinned: undefined,
+                    rowPinned: isX ? this.rangeEndRow.rowPinned : this.lastCellMarked!.rowPinned,
                     column: isX ? this.lastCellMarked!.column : this.cellRange.columns[this.cellRange.columns.length - 1]
                 });
             }
             else {
+                const startRow = isX ? this.rangeStartRow: this.lastCellMarked!;
                 const startPosition = {
-                    rowIndex: isX ? this.rangeStartRow.rowIndex : this.lastCellMarked!.rowIndex,
-                    rowPinned: undefined,
+                    rowIndex: startRow.rowIndex,
+                    rowPinned: startRow.rowPinned,
                     column: isX ? this.lastCellMarked!.column : this.cellRange.columns[0]
-                };
+                }
                 this.rangeController.setRangeToCell(startPosition);
 
                 this.rangeController.extendLatestRangeToCell({
                     rowIndex: this.rangeEndRow.rowIndex,
-                    rowPinned: undefined,
+                    rowPinned: this.rangeEndRow.rowPinned,
                     column: this.cellRange.columns[this.cellRange.columns.length - 1]
                 });
             }
@@ -125,88 +128,112 @@ export class FillHandle extends Component implements IFillHandle {
         }
     }
 
-    private markPathFrom(initialComp: CellComp, currentPosition: CellPosition) {
+    private markPathFrom(initialPosition: CellPosition, currentPosition: CellPosition) {
         this.clearMarkedPath();
         if (this.dragAxis === 'y') {
-            const rowIndex = initialComp.getRenderedRow().getRowNode().rowIndex;
-            if (rowIndex < currentPosition.rowIndex) {
-                this.extendVertical(rowIndex + 1, currentPosition.rowIndex);
-            }
-            else if (currentPosition.rowIndex < this.rangeStartRow.rowIndex) {
-                this.extendVertical(currentPosition.rowIndex, this.rangeStartRow.rowIndex - 1, true);
-            }
+            if (RowPositionUtils.sameRow(currentPosition, initialPosition)) { return; }
+
+            const isBefore = RowPositionUtils.before(currentPosition, initialPosition);
+
+            if (isBefore &&
+                currentPosition.rowPinned === this.rangeStartRow.rowPinned &&
+                currentPosition.rowIndex >= this.rangeStartRow.rowIndex
+            ) { return; }
+
+            this.extendVertical(initialPosition, currentPosition, isBefore);
         }
         else {
-            const displayedColumns = this.gridOptionsWrapper.getColumnApi()!.getDisplayedCenterColumns();
-            const colIndex = displayedColumns.indexOf(initialComp.getColumn())
-            const endIndex = displayedColumns.indexOf(currentPosition.column);
-            let rangeStartColIdx;
+            const initialColumn = initialPosition.column;
+            const currentColumn = currentPosition.column;
 
-            if (colIndex < endIndex) {
-                this.extendHorizontal(colIndex + 1, endIndex, displayedColumns);
-            }
-            else if (endIndex < (rangeStartColIdx = displayedColumns.indexOf(this.cellRange.columns[0]))) {
-                this.extendHorizontal(endIndex, rangeStartColIdx, displayedColumns, true);
-            }
+            if (initialColumn === currentColumn) { return; }
+
+            const displayedColumns = this.columnController.getAllDisplayedColumns();
+            const initialIndex = displayedColumns.indexOf(initialColumn)
+            const currentIndex = displayedColumns.indexOf(currentColumn);
+
+            if (currentIndex <= initialIndex && currentIndex >= displayedColumns.indexOf(this.cellRange.columns[0])) { return; }
+            
+            this.extendHorizontal(initialPosition, currentPosition, currentIndex < initialIndex);
         }
     }
 
-    private extendVertical(startIdx: number, endIdx: number, isMovingUp?: boolean) {
-        for (let i = startIdx; i <= endIdx; i++) {
-            const colLen = this.cellRange.columns.length;
-            for (let j = 0; j < colLen; j++) {
-                if (j !== 0 && j !== colLen - 1 && i !== (isMovingUp ? startIdx : endIdx)) { continue; }
+    private extendVertical(initialPosition: CellPosition, endPosition: CellPosition, isMovingUp?: boolean) {
+        let row: RowPosition | null = initialPosition;
 
+        while(
+            row = isMovingUp ? 
+                this.cellNavigationService.getRowAbove(row.rowIndex, row.rowPinned as string) : 
+                this.cellNavigationService.getRowBelow(row)
+        ) {
+            const colLen = this.cellRange.columns.length;
+            for (let i = 0; i < colLen; i++) {
                 const cellComp = this.rowRenderer.getComponentForCell({
-                    rowIndex: i,
-                    rowPinned: undefined,
-                    column: this.cellRange.columns[j]
+                    rowIndex: row.rowIndex,
+                    rowPinned: row.rowPinned,
+                    column: this.cellRange.columns[i]
                 });
+
                 if (!cellComp) { continue; }
                 this.markedCellComps.push(cellComp);
 
                 const eGui = cellComp.getGui();
         
-                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-left', j === 0);
-                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-right', j === colLen - 1);
-                if (isMovingUp) {
-                    this.isUp = true;
-                    _.addOrRemoveCssClass(eGui, 'ag-selection-fill-top', i === startIdx);
-                }
-                else {
-                    _.addOrRemoveCssClass(eGui, 'ag-selection-fill-bottom', i === endIdx);
-                }
+                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-left', i === 0);
+                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-right', i === colLen - 1);
+
+                _.addOrRemoveCssClass(
+                    eGui, 
+                    isMovingUp ? 'ag-selection-fill-top' : 'ag-selection-fill-bottom',
+                    RowPositionUtils.sameRow(row, endPosition)
+                );
+
+                if (isMovingUp) { this.isUp = true; }
             }
+
+            if (RowPositionUtils.sameRow(row, endPosition)) { break; }
         }
     }
 
-    private extendHorizontal(startIdx: number, endIdx: number, columns: Column[], isMovingLeft?: boolean) {
-        for (let i = startIdx; i <= endIdx; i++) { 
-            for (let j = this.rangeStartRow.rowIndex; j <= this.rangeEndRow.rowIndex; j++) {
-                if (j !== this.rangeStartRow.rowIndex && j !== this.rangeEndRow.rowIndex && i !== (isMovingLeft ? startIdx : endIdx)) { continue; }
+    private extendHorizontal(initialPosition: CellPosition, endPosition: CellPosition, isMovingLeft?: boolean) {
+        const allCols = this.columnController.getAllDisplayedColumns();
+        const startCol = allCols.indexOf(isMovingLeft ? endPosition.column : initialPosition.column);
+        const endCol = allCols.indexOf(isMovingLeft ? initialPosition.column : endPosition.column);
+        const offset = isMovingLeft ? 0 : 1;
 
+
+        const colsToMark = allCols.slice(startCol + offset, endCol + offset);
+        colsToMark.forEach(column => {
+            let row: RowPosition = this.rangeStartRow;
+            let isLastRow: boolean = false;
+
+            do {
+                isLastRow = RowPositionUtils.sameRow(row, this.rangeEndRow);
                 const cellComp = this.rowRenderer.getComponentForCell({
-                    rowIndex: j,
-                    rowPinned: undefined,
-                    column: columns[i]
+                    rowIndex: row.rowIndex,
+                    rowPinned: row.rowPinned,
+                    column: column
                 });
 
-                if (!cellComp) { continue; }
+                if (!cellComp) { break; }
                 this.markedCellComps.push(cellComp);
 
                 const eGui = cellComp.getGui();
 
-                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-top', j === this.rangeStartRow.rowIndex);
-                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-bottom', j === this.rangeEndRow.rowIndex);
+                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-top', RowPositionUtils.sameRow(row, this.rangeStartRow));
+                _.addOrRemoveCssClass(eGui, 'ag-selection-fill-bottom', RowPositionUtils.sameRow(row, this.rangeEndRow));
                 if (isMovingLeft) {
                     this.isLeft = true;
-                    _.addOrRemoveCssClass(eGui, 'ag-selection-fill-left', i === startIdx);
+                    _.addOrRemoveCssClass(eGui, 'ag-selection-fill-left', column === colsToMark[0]);
                 }
                 else {
-                    _.addOrRemoveCssClass(eGui, 'ag-selection-fill-right', i === endIdx);
+                    _.addOrRemoveCssClass(eGui, 'ag-selection-fill-right', column === colsToMark[colsToMark.length - 1]);
                 }
+
+                row = this.cellNavigationService.getRowBelow(row) as RowPosition;
             }
-        }
+            while(!isLastRow)
+        });
     }
 
     private clearMarkedPath() {
