c60b061d679210c640587555656a19ecb9ca8568
Vitaly Kravchenko <vitalyx@gmail.com>
2019-04-14 17:48:13 +0100

    Group.getBBox - computes a bounding box for a hierarchy of nodes, accounting for node transformations.

--- packages/ag-grid-enterprise/src/charts/scene/group.ts
+++ packages/ag-grid-enterprise/src/charts/scene/group.ts
@@ -1,4 +1,6 @@
 import { Node } from "./node";
+import { BBox, renderBBox } from "./bbox";
+import { Matrix } from "./matrix";
 
 export class Group extends Node {
 
@@ -6,6 +8,51 @@ export class Group extends Node {
     isPointInNode(x: number, y: number): boolean {
         return true;
     }
+
+    readonly getBBox = (): BBox => {
+        let left = Infinity;
+        let right = -Infinity;
+        let top = Infinity;
+        let bottom = -Infinity;
+
+        if (this.dirtyTransform) {
+            this.computeTransformMatrix();
+        }
+
+        this.children.forEach(child => {
+            if (child.getBBox) {
+                if (child.dirtyTransform) {
+                    child.computeTransformMatrix();
+                }
+                const bbox = child.getBBox();
+                const matrix = Matrix.flyweight(child.matrix);
+                matrix.preMultiplySelf(this.matrix);
+                matrix.transformBBox(bbox, 0, bbox);
+                const x = bbox.x;
+                const y = bbox.y;
+
+                if (x < left) {
+                    left = x;
+                }
+                if (y < top) {
+                    top = y;
+                }
+                if (x + bbox.width > right) {
+                    right = x + bbox.width;
+                }
+                if (y + bbox.height > bottom) {
+                    bottom = y + bbox.height;
+                }
+            }
+        });
+
+        return {
+            x: left,
+            y: top,
+            width: right - left,
+            height: bottom - top
+        };
+    };
 
     render(ctx: CanvasRenderingContext2D) {
         // A group can have `scaling`, `rotation`, `translation` properties
@@ -27,5 +74,13 @@ export class Group extends Node {
             }
             ctx.restore();
         }
+
+        // debug
+        // renderBBox({
+        //     ctx,
+        //     bbox: this.getBBox(),
+        //     resetTransform: true,
+        //     strokeStyle: this.parent ? 'red' : 'orange'
+        // });
     }
 }
