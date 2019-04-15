cd969ddcf1b6a3b41d8c833c8d3d946337cb4412
Vitaly Kravchenko <vitalyx@gmail.com>
2019-04-15 16:35:21 +0100

    Padding class and tests.

--- packages/ag-grid-enterprise/src/charts/util/padding.ts
+++ packages/ag-grid-enterprise/src/charts/util/padding.ts
@@ -1,6 +1,13 @@
-export type Padding = {
-    top: number,
-    right: number,
-    bottom: number,
-    left: number
-};
+export class Padding {
+    top: number;
+    right: number;
+    bottom: number;
+    left: number;
+
+    constructor(top: number = 0, right: number = top, bottom: number = top, left: number = right) {
+        this.top = top;
+        this.right = right;
+        this.bottom = bottom;
+        this.left = left;
+    }
+}
