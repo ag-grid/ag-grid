d1b8daed1ebd106ee79d0b50681810cd10796ccb
Sean Landsman <sean@thelandsmans.com>
2019-05-13 17:03:00 +0100

    AG-2844 Docs - Fix statement "Changing the row height is only supported in the Client-side row model." On row height section

--- packages/ag-grid-docs/src/javascript-grid-row-height/index.php
+++ packages/ag-grid-docs/src/javascript-grid-row-height/index.php
@@ -14,12 +14,9 @@ include '../documentation-main/documentation_header.php';
     </p>
 
     <note>
-        Changing the row height is only supported in the <a href="../javascript-grid-client-side-model/">Client-side</a>
-        row model. You cannot use variable row height when using
-        <a href="../javascript-grid-virtual-paging/">virtual paging</a>,
+        You cannot use variable row height when using
         <a href="../javascript-grid-viewport/">viewport</a> or
-        <a href="../javascript-grid-server-side-model/">Server-side</a> row models.
-        This is because these row models need to work out the position of rows that are not loaded and hence need to
+        This is because this row model needs to work out the position of rows that are not loaded and hence needs to
         assume the row height is fixed.
     </note>
 
