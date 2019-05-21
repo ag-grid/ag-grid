b807bd87a8da7358340bd894ab549aa9ef87b712
Vitaly Kravchenko <vitalyx@gmail.com>
2019-05-21 15:30:09 +0100

    Fix ChartBuilder (check against undefined).

--- packages/ag-grid-enterprise/src/chartAdaptor/chartComp/chartProxies/pieChartProxy.ts
+++ packages/ag-grid-enterprise/src/chartAdaptor/chartComp/chartProxies/pieChartProxy.ts
@@ -83,7 +83,7 @@ export class PieChartProxy extends ChartProxy {
                 calloutWidth: 2,
                 calloutLength: 10,
                 calloutPadding: 3,
-                label: false,
+                label: true,
                 labelFont: '12px Verdana, sans-serif',
                 labelColor: this.options.isDarkTheme() ? 'rgb(221, 221, 221)' : 'black',
                 labelMinAngle: 20,
