3cfd1dcfc5b307d3e88fb5144af14d70152f4624
Vitaly Kravchenko <vitalyx@gmail.com>
2019-05-29 15:09:42 +0100

    Donut docs.

--- packages/ag-grid-docs/src/javascript-grid-charts-customisation-doughnut/index.php
+++ packages/ag-grid-docs/src/javascript-grid-charts-customisation-doughnut/index.php
@@ -20,61 +20,93 @@ include '../documentation-main/documentation_header.php';
 
 <snippet>
 interface DoughnutChartOptions {
+    // Container element for the chart.
     parent?: HTMLElement;
+    // The width of the chart.
     width?: number;
+    // The height of the chart.
     height?: number;
-    series?: any[];
-    data?: any;
+    // The padding of contents from the edges of the chart.
     padding?: {
         top: number;
         right: number;
         bottom: number;
         left: number;
     };
+    // The side of the chart to dock the legend to.
     legendPosition?: 'top' | 'right' | 'bottom' | 'left';
+    // The padding amount between the legend and the series.
     legendPadding?: number;
+    // The CSS class name to be used by the tooltip element.
     tooltipClass?: string;
     legend?: {
+        // The line width of a legend marker.
         markerLineWidth?: number;
+        // The size of a legend marker.
         markerSize?: number;
+        // The padding between a legend marker and its label.
         markerPadding?: number;
+        // The amount of horizontal padding between legend items.
         itemPaddingX?: number;
+        // The amount of vertical padding between legend items.
         itemPaddingY?: number;
+        // The font to be used by the legend's labels.
+        // Should use the same format as the shorthand `font` property in CSS.
         labelFont?: string;
+        // The color to be used by the legend's labels.
         labelColor?: string;
     };
     seriesDefaults?: {
-        type?: string;
-        data?: any[];
         title?: string;
         titleEnabled?: boolean;
         titleFont?: string;
-        visible?: boolean;
+        // Whether this series should be represented in the legend. Defaults to `true`.
         showInLegend?: boolean;
+        // Whether to show the tooltip for bars when they are hovered/tapped. Defaults to `false`.
         tooltipEnabled?: boolean;
-        calloutColors?: string[];
-        calloutWidth?: number;
-        calloutLength?: number;
-        calloutPadding?: number;
-        labelFont?: string;
-        labelColor?: string;
-        labelMinAngle?: number;
+        // The field to use to determine the angle of each pie slice.
         angleField?: string;
+        // The field to use to determine the radius of each pie slice. Optional field.
         radiusField?: string;
+        // The field to use to fetch pie slice label text.
         labelField?: string;
+        // Whether to show pie slice labels or not.
         labelEnabled?: boolean;
+        // The font to be used for slice labels.
+        labelFont?: string;
+        // The color to use for slice labels.
+        labelColor?: string;
+        // If the pie slice angle is smaller than this value (in degrees), the label won't be shown.
+        labelMinAngle?: number;
+        // The fill colors of pie slices.
         fills?: string[];
+        // The stroke colors of pie slices. Darker versions of fill colors by default.
         strokes?: string[];
-        rotation?: number;
+        // The stroke width. Defaults to `1`.
+        strokeWidth?: number;
+        // The callout stroke colors. Same as stroke colors by default.
+        calloutColors?: string[];
+        // The thickness of a callout line. Defaults to 2.
+        calloutWidth?: number;
+        // The length of a callout line. Defaults to 10.
+        calloutLength?: number;
+        // The padding between the callouts and the labels. Defaults to 3.
+        calloutPadding?: number;
+        // The amount by which to extend or shorten the calculated outer radius value of a slice. Defaults to 0.
         outerRadiusOffset?: number;
+        // The amount by which to extend or shorten the calculated inner radius value of a slice. Defaults to 0.
         innerRadiusOffset?: number;
-        // strokeStyle?: string // TODO: ???
+        // The shadow type to use for bars. Defaults to no shadow.
+        // Note: shadows can noticeably slow down rendering of charts with a few hundred bars.
         shadow?: {
+            // The shadow color. For example, 'rgba(0, 0, 0, 0.3)'.
             color?: string;
+            // The shadow offset.
             offset?: [number, number];
+            // The blur amount to apply.
             blur?: number;
         };
-        lineWidth?: number;
+        // The custom tooltip render to use for bar tooltips. Should return a valid HTML string.
         tooltipRenderer?: (params: DoughnutTooltipRendererParams) => string;
     };
 }
@@ -116,7 +148,7 @@ interface DoughnutTooltipRendererParams {
         type: 'pie',
         fills: palette.fills,
         strokes: palette.strokes,
-        lineWidth: 1,
+        strokeWidth: 1,
         calloutColors: palette.strokes,
         calloutWidth: 2,
         calloutLength: 10,
