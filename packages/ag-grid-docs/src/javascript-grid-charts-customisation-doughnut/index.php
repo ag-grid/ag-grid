6ed2a0ab26f716731c71249392c2dfd50a48309e
ceolter <0872393174Aa>
2019-05-31 15:49:52 +0100

    AG-3002 updating charting examples

--- packages/ag-grid-docs/src/javascript-grid-charts-customisation-doughnut/index.php
+++ packages/ag-grid-docs/src/javascript-grid-charts-customisation-doughnut/index.php
@@ -112,54 +112,6 @@ interface DoughnutTooltipRendererParams {
 }
 </snippet>
 
-<h3>Default Doughnut Options</h3>
-
-<p>
-    The default values for the pie chart options are shown below:
-</p>
-
-    <snippet>{
-    parent: this.chartProxyParams.parentElement,
-    width: this.chartProxyParams.width,
-    height: this.chartProxyParams.height,
-    padding: {
-        top: 50,
-        right: 50,
-        bottom: 50,
-        left: 50
-    },
-    legend: {
-        labelFont: '12px Verdana, sans-serif',
-        labelColor: this.getLabelColor(),
-        itemPaddingX: 16,
-        itemPaddingY: 8,
-        markerPadding: 4,
-        markerSize: 14,
-        markerLineWidth: 1
-    },
-    seriesDefaults: {
-        type: 'pie',
-        fills: palette.fills,
-        strokes: palette.strokes,
-        strokeWidth: 1,
-        calloutColors: palette.strokes,
-        calloutWidth: 2,
-        calloutLength: 10,
-        calloutPadding: 3,
-        labelEnabled: false,
-        labelFont: '12px Verdana, sans-serif',
-        labelColor: this.getLabelColor(),
-        labelMinAngle: 20,
-        tooltipEnabled: true,
-        tooltipRenderer: undefined,
-        showInLegend: true,
-        title: '',
-        titleEnabled: false,
-        titleFont: 'bold 12px Verdana, sans-serif'
-    }
-}
-</snippet>
-
-    <?= example('Provided Container', 'provided-container', 'generated', array("enterprise" => true)) ?>
+    <?= example('Custom Doughnut Chart', 'custom-doughnut-chart', 'generated', array("enterprise" => true)) ?>
 
 <?php include '../documentation-main/documentation_footer.php'; ?>
