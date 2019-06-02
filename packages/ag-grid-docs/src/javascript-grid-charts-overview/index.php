df5ee9f8b6be93ed212472efbd10d533aa2335db
Robert Clarke <rmc-software-consultancy@gmail.com>
2019-06-02 22:58:44 +0100

    AG-3002 - chart doc updates

--- packages/ag-grid-docs/src/javascript-grid-charts-overview/index.php
+++ packages/ag-grid-docs/src/javascript-grid-charts-overview/index.php
@@ -9,124 +9,102 @@ include '../documentation-main/documentation_header.php';
     <h1 class="heading-enterprise">Charts - BETA</h1>
 
     <p class="lead">
-        Charting is a major component inside the grid. As well as having a standalone charting library that
-        you can use, the grid comes with charting integration allowing users to chart data directly
-        from the grid without requiring additional developer coding.
+       This section provides an overview of ag-Grid's built-in charting functionality.
     </p>
 
-    <? enterprise_feature("Charting"); ?>
-
-    <note>
-        <b>Charting in ag-Grid is currently in beta.</b> If you are an ag-Grid Enterprise customer then you are
-        encouraged to try out the charts in ag-Grid as described here and give feedback to the ag-Grid team
-        via
-        <a href="https://ag-grid.zendesk.com/">Zendesk</a>
-        or
-        <a href="https://github.com/ag-grid/ag-grid/">Github</a>.
-        We do not expect to drop features of the charting integration, however we do foresee
-        changes to the API. If you plan on going into production with grid charting, just be aware breaking changes
-        will probably come in future grid releases.
-    </note>
-
-    <h2>Enable Charting</h2>
+    <p>
+        The charting functionality provided by ag-Grid is deeply integrated with the grid. This integration gives users
+        a seamless charting experience while keeping the coding required by developers to a minimum.
+    </p>
 
     <p>
-        Charting is enabled in the grid with the following two steps:
+        In fact we are not aware of any other datagrid that provides such integration, as other companies may provide
+        a grid library and / or a charting library, but it's up to the developer to tie the two together.
+    </p>
+
+    <p>
+        This section introduces the two ways charts can be created from the data contained in the grid:
     </p>
 
     <ul>
         <li>
-            Include the grid charting module.
+            <a href="../javascript-grid-charts-overview/#user-created-charts">User Created Charts</a>: a user creates a
+            chart from a range of selected cells via the context menu.
         </li>
         <li>
-            Enable charting with the grid option <code>enableCharts=true</code>.
+            <a href="../javascript-grid-charts-overview/#application-created-charts">Application Created Charts</a>:
+            the application requests the grid to create a chart through the chart API.
         </li>
     </ul>
-
-    <h3>Charting Module</h3>
-
-    <p>
-        To minimise bundle sizes for applications that do not require charting, the charting integration is contained
-        within a module. The module is automatically included in bundled versions of ag-Grid (eg if you are including
-        <code>ag-grid-community.js</code> or <code>ag-grid-enterprise.js</code>).
     </p>
 
+    <note>
+        We are very excited to be introducing ag-Grid's new charting capability, however please be advised that there
+        could be some breaking changes around the API while it is still in beta. Please give us your feedback via
+        <a href="https://ag-grid.zendesk.com/">Zendesk</a>, for existing customers, alternatively
+        <a href="https://github.com/ag-grid/ag-grid/">Github</a> can also be used.
+        </br>
+    </note>
+
+    <h2>Charts Module</h2>
+
     <p>
-        Include the charting module by referencing it in your module imports as follows:
+        To minimise bundle sizes for applications that do not require charting, charts are contained in a separate
+        module, which can be imported as follows:
     </p>
 
     <snippet>
-// import the charting module
-import 'ag-grid-enterprise/chartsModule';
+        // order of imports is important!
+        import 'ag-grid-enterprise/chartsModule';
+        import 'ag-grid-enterprise';
     </snippet>
 
+    <p>
+        The charts module has been built from the ground up with zero dependencies on any third party libraries.
+    </p>
+
     <note>
-        If you are using a bundled version of ag-Grid (eg if you are including
-        <code>ag-grid-enterprise.js</code>) then you do not need to reference
-        the module as it is included in the bundled version.
+        The enterprise bundle of ag-Grid automatically includes charts, i.e. <code>ag-grid-enterprise.js</code>
     </note>
 
-    <h2>
-        Charting Components
-    </h2>
-
+    <h2>Enabling Charts</h2>
     <p>
-        There are two major components of the grid's charting strategy as follows:
+        All that is required to enable charting is set the following grid option:
     </p>
 
-    <ol>
-        <li>
-            Charting Library
-        </li>
-        <li>
-            Charting Integration:
-        </li>
-    </ol>
-
-    <h3>Charting Library</h3>
+<snippet>
+gridOptions = {
+    enableCharts: true
+}
+</snippet>
 
     <p>
-        The charting library is a stand alone charting library capable of creating complex charts.
-        This will be similar (but better obviously) to the many libraries already existing such
-        as Highcharts. The interface of the charting library is still in flux hence the
-        charting library is not available as a standalone component right now. However future releases
-        of ag-Grid will have the charting library available.
+        To allow users to create charts from a
+        <a href="../javascript-grid-range-selection/">Range Selection</a> and / or display the
+        <a href="../javascript-grid-charts-chart-ranges/">Chart Ranges</a> in the grid, the following grid option
+        should also be enabled:
     </p>
 
-    <p>
-        Future releases will see the charting library and include charts such as pie, stack, bar and line charts.
-        The charts will be completely reactive, resolution independent, canvas based. The library will enable
-        building beautiful visualisations of your data.
-    </p>
-
-    <h3>Charting Integration</h3>
-
-    <p>
-        The grid combines features of the grid and the charting library to give
-        a seamless charting experience to application users with minimal configuration of coding required
-        by application developers. For example, the grid allows you to chart ranges from inside the grid via the
-        context menu. No other datagrid we are aware of allows this - all the competitors provide a grid
-        library and / or a charting library, but it's up to the developer to tie the two together.
-        The vision of ag-Grid is to provide an Excel style data visualisation experience out of the box.
-    </p>
-
-    <p>
-        Charting integration allows charts to be created from the grid in the following ways:
-    </p>
-
-    <ul>
-        <li>
-            User Created Charts: The user selects a range and then selects to create a chart from the context menu.
-        </li>
-        <li>
-            Charting API: The application requests the grid to create a chart.
-        </li>
-    </ul>
+<snippet>
+gridOptions = {
+    enableRangeSelection: true
+}
+</snippet>
 
     <h2>User Created Charts</h2>
+    <p>
+        User created charts are designed to provide an out-of-the box charting experience, similar to that found in
+        spreadsheet applications such as Excel, but yet more compelling once integrated inside applications.
+    </p>
 
     <p>
-        Once charting is enabled, users can create charts as follows:
+        All that is required for users to create charts, from the data already contained in the grid,
+        is to import the <a href="../javascript-grid-charts-overview/#charts-module">charts module</a>
+        and <a href="../javascript-grid-charts-overview/#enabling-charts">enable charts</a>.
+    </p>
+
+    <p>
+        Try it out on our <a href="../example.php">demo page</a> by doing the following:
     </p>
 
     <ul>
@@ -135,198 +113,98 @@ import 'ag-grid-enterprise/chartsModule';
             the mouse over a range of cells.
         </li>
         <li>
-            Bring up the <a href="../javascript-grid-context-menu">Context Menu</a> and from the Chart Range
-            sub menu, select the chart type you want to display.
+            The bring up the <a href="../javascript-grid-context-menu">Context Menu</a> and select the desired chart type
+            from the 'Chart Range' sub menu.
         </li>
     </ul>
 
     <p>
-        The best place to play with creating charts is on the main grid <a href="../example.php">demo page</a>.
-        Select a range of cells in the month columns (where there is a lot of numeric data) and choose chart
-        from the context menu.
+        <img alt="Charting Toolbar" src="userChartingShowcase.gif" style="margin-bottom: 0px; width: 100%; border: grey solid 1px">
     </p>
 
-    <img alt="User Created Chart" src="./UserCreatedChart.gif" style="margin-bottom: 20px; width: 100%;">
+    <p>
+        The animation above highlights a number of charting features. For more details on each feature follow the links
+        provided below:
+    </p>
+
+    <ul>
+        <li><a href="../javascript-grid-charts-chart-ranges/">Chart Ranges</a>: when a chart is created, corresponding
+            chart ranges appear in the grid and are adjusted via the chart range handle.
+        </li>
+        <li><a href="../javascript-grid-charts-chart-ranges/#category-and-series-ranges/">Categories and Series</a>:
+            columns can be configured as either categories / series for charting or left to the grid to infer based
+            on the data contained in the columns.
+        </li>
+        <li><a href="../javascript-grid-charts-chart-toolbar/">Chart Toolbar</a>:
+            located in the top left area of the chart, the chart toolbar allows users to perform actions after chart has
+            been created, such as change the chart type, colour palette and chart data.
+        </li>
+        <li><a href="../javascript-grid-charts-customisation/">Chart Customisation</a>: each chart type can be
+            customised to suit each applications requirements.
+        </li>
+    </ul>
 
     <p>
-        User created charts are displayed inside the grid's own popup windows. The windows can be moved (by mouse
+        By default, user created charts are displayed inside the grid's own popup windows. The windows can be moved (by mouse
         dragging the windows title bar) and resized (by mouse dragging the windows borders).
     </p>
 
+    <p>
+        It is also possible to display user created charts in an another location or application dialog. For more details
+        see the section on <a href="../javascript-grid-charts-customisation/#providing-a-chart-container/">providing a chart container</a>.
+    </p>
+
     <note>
-        If using the grid's own popup window, you will probably want to se the grid option
+        If using the grid's own popup window, you will probably want to use the grid option
         <code>popupParent</code> so that the popup windows are not constrained to the bounds of the grid.
         Typically users set <code>popupParent=document.body</code> to achieve this.
     </note>
 
-
-    <h3>Provided Chart Container</h3>
+    <h2>Application Created Charts</h2>
 
     <p>
-        Displaying the generated chart within the grid provided popup window will suit most needs. However
-        you may wish to display the chart in a different location. For example
-        your application may already have popup windows and you wish use the same library for consistency.
+        Charts can be pre-defined or dynamically created from within applications, and as with
+        <a href="../javascript-grid-charts-overview/#user-created-charts">user created charts</a>, these charts also
+        benefit from the integration provided with the grid.
     </p>
 
     <p>
-        To provide an alternative container for popup windows use the grid callback
-        <code>createChartContainer(chartRef)</code>. The interface is as follows:
+        The dummy financial application below just touches on what is possible with ag-Grid's integrated grid and
+        charting capabilities. The following should be noted:
     </p>
 
-    <snippet>
-        // grid callback to implement to place chart in an alternative container
-        function createChartContainer(chartRef: ChartRef): void;
-
-        // function gets a Chart Reference
-        interface ChartRef {
-            // the chart DOM element, application responsible for placing into the DOM
-            chartElement: HTMLElement;
-            // application responsible for calling destroyChart when chart is no longer needed
-            destroyChart: () => void;
-        }
-    </snippet>
-
-    <p>
-        The callback is called each time the user elects to create a chart via the grid UI. The callback
-        is provided with a <code>ChartRef</code> containing the following:
-    </p>
-    <ul>
+    <ul class="content">
         <li>
-            <code>chartElement</code>: The chart DOM element, application responsible for placing into the DOM.
+            <b>Pre-Defined Chart</b>: a pre-defined chart is shown in a separate chart container below the grid.
         </li>
         <li>
-            <code>destroyChart</code>: The application is responsible for calling destroyChart() when the
-            chart is no longer needed.
+            <b>Dynamic Charts</b>: buttons positions above the grid dynamically create different chart types.
+        </li>
+        <li>
+            <b>High Performance</b>: 100 rows are randomly updated 10 times a second. Try updating the example via
+            plunker with higher update frequencies and more data.
         </li>
     </ul>
 
-    <p>
-        The example below demonstrates the <code>createChartContainer()</code> callback. The example does not
-        use an alternative popup window, instead it just places the charts into the DOM below the grid. This
-        crude approach is on purpose to minimise the complexity of the example and focus on just the callback
-        and the interactions of the grid.
-    </p>
+    <?= example('Application Created Charts', 'application-created-charts', 'vanilla', array('height' => '150%', 'width' => '50%', 'enterprise' => true)) ?>
 
     <p>
-        From the example, the following can be noted:
-    </p>
-    <ul>
-        <li>
-            Select a range of numbers (medal columns) and create a chart from the context menu.
-        </li>
-        <li>
-            The chart appears below the grid rather than in a popup window. This is because the
-            <code>createChartContainer()</code> is implemented.
-        </li>
-        <li>
-            Each chart is displayed alongside a 'Destroy' button. The logic behind the destroy
-            button calls <code>destroyChart()</code> to destroy the chart instance.
-        </li>
-    </ul>
-
-    <?= example('Provided Container', 'provided-container', 'generated', array("enterprise" => true)) ?>
-
-    <h2>Charting API</h2>
-
-    <p>
-        Charts can be created programmatically via the grid's <code>chartRange()</code> API. The interface is
-        as follows:
-    </p>
-
-    <snippet>
-        function chartRange(params: ChartRangeParams): ChartRef | undefined;
-
-        interface ChartRangeParams {
-            cellRange: CellRangeParams;
-            chartType: string;
-            chartContainer?: HTMLElement;
-            aggFunc?: string | IAggFunc;
-        }
-
-        export interface CellRangeParams {
-
-            // start row
-            rowStartIndex?: number;
-            rowStartPinned?: string;
-
-            // end row
-            rowEndIndex?: number;
-            rowEndPinned?: string;
-
-            // columns
-            columnStart?: string | Column;
-            columnEnd?: string | Column;
-            columns?: (string | Column)[];
-        }
-    </snippet>
-
-    <p>
-        The provided params contains the following attributes:
+        To learn how to create charts in your applications see the following sections for details:
     </p>
 
     <ul>
-        <li>
-            <code>cellRange</code>: Defines the range of cells to be charted.
-            See <a href="./javascript-grid-range-selection/#api-addcellrange-rangeselection">Add Cell Range</a>
-            for details on <code>CellRangeParams</code>.
+        <li><a href="../javascript-grid-charts-chart-range-api/">Chart Range API</a>: used to create charts programmatically
+            inside applications.
         </li>
-        <li>
-            <code>chartType</code>: The type of chart to create. Choose one of 'groupedBar', 'stackedBar', 'pie'
-            and 'line'.
-        </li>
-        <li>
-            <code>chartContainer</code>: If the chart is to be displayed outside of the grid then provide a chart
-            container. If the chart is to be displayed using the grid's popup window mechanism then leave undefined.
-        </li>
-        <li>
-            <code>aggFunc</code>: <span style="background: yellow;">Rob to advise</span>
+        <li><a href="../javascript-grid-charts-customisation/#providing-a-chart-container/">Provide a Chart Container</a>:
+            used to target chart containers inside the application instead of the popup window provided by the grid.
         </li>
     </ul>
 
-    <p>
-        The API can return back a <code>ChartRef</code> object, the same structure that is provided to the
-        <code>createChartContainer()</code> callback (see above). The <code>ChartRef</code> is returned when
-        the <code>chartContainer</code> is provided. This provides the application with the <code>destroyChart()</code>
-        method that is required when the application wants to dispose the chart.
-    </p>
-
-    <h3>API Example 1 - Chart Range in Grid Window</h3>
+    <h2>Next Up</h2>
 
     <p>
-        This example gets the grid to chart data in the grid provided popup window. The following can be noted:
+        Continue to the next section to learn about: <a href="../javascript-grid-charts-chart-ranges/">Chart Ranges</a>.
     </p>
-
-    <ul>
-        <li>
-            Clicking 'Apples & Oranges, 5 Rows' will chart the first five rows of apples and oranges in
-            a grid popup window.
-        </li>
-        <li>
-            Clicking 'Bananas, All Rows' will chart bananas and all rows (the provided cell range does not specify rows).
-        </li>
-    </ul>
-
-    <?= example('Chart API', 'chart-api', 'generated', array("enterprise" => true)) ?>
-
-
-    <h3>API Example 2 - Charts in Dashboard</h3>
-
-    <p>
-        This example passes a <code>chartContainer</code> to the API to place the chart in a location other
-        than the grid's popup window. The following can be noted:
-    </p>
-
-    <ul>
-        <li>The charts are placed in div elements outside of the grid.</li>
-        <li>The two pie charts are showing aggregations rather than charting individual rows.</li>
-        <li>Clicking on a chart highlights the range in the grid for which the chart is based.</li>
-        <li>
-            The bar chart is sensitive to changes in the rows. For example if you sort, the chart updates to
-            always chart the first five rows.
-        </li>
-        <li>All data is editable in the grid. Changes to the grid data is reflected in the charts.</li>
-    </ul>
-
-    <?= example('Dashboard', 'dashboard', 'generated', array("enterprise" => true)) ?>
 
 <?php include '../documentation-main/documentation_footer.php'; ?>
