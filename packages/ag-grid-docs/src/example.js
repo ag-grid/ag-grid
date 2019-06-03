4675f09b8448ad0436cccfb7d297185fc68dfe91
Robert Clarke <rmc-software-consultancy@gmail.com>
2019-06-03 22:55:56 +0100

    AG-3002 - updated demo example for charting

--- packages/ag-grid-docs/src/example.js
+++ packages/ag-grid-docs/src/example.js
@@ -385,10 +385,10 @@ var gridOptions = {
         if (params.type === 'groupedBar' || params.type === 'stackedBar' || params.type === 'line') {
             options.yAxis.labelFormatter = function(n) {
                 if (n < 1e3) return n;
-                if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
-                if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
-                if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
-                if (n >= 1e12) return +(n / 1e12).toFixed(1) + "T";
+                if (n >= 1e3 && n < 1e6) return `$${+(n / 1e3).toFixed(1)}K`;
+                if (n >= 1e6 && n < 1e9) return `$${+(n / 1e6).toFixed(1)}M`;
+                if (n >= 1e9 && n < 1e12) return `$${+(n / 1e9).toFixed(1)}B`;
+                if (n >= 1e12) return `$${+(n / 1e12).toFixed(1)}T`;
             };
 
             options.seriesDefaults.tooltipRenderer = (params) => {
