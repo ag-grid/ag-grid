a6a33849fde9249aa0c58791af021380ce541fe3
Robert Clarke <rmc-software-consultancy@gmail.com>
2019-06-17 15:04:44 +0100

    AG-3067 - refactored the formatting tool panel to follow the template approach.

--- packages/ag-grid-enterprise/src/chartAdaptor/chartComp/menu/dummyFormattingPanel.ts
+++ packages/ag-grid-enterprise/src/chartAdaptor/chartComp/menu/dummyFormattingPanel.ts
@@ -1,87 +1,50 @@
-import {
-    _,
-    AgCheckbox,
-    AgGroupComponent,
-    AgRadioButton,
-    Autowired,
-    Component,
-    GridOptionsWrapper,
-    PostConstruct
-} from "ag-grid-community";
+import {_, AgCheckbox, Autowired, Component, GridOptionsWrapper, PostConstruct, RefSelector} from "ag-grid-community";
 import {ChartController} from "../chartController";
+import {Chart} from "../../../charts/chart/chart";
 
 export class DummyFormattingPanel extends Component {
 
-    public static TEMPLATE = `<div class="ag-chart-data-wrapper"></div>`;
+    public static TEMPLATE =
+        `<div>
+            <span class="ag-column-tool-panel-column-group">
+                <ag-checkbox ref="cbLegendEnabled"></ag-checkbox>
+                <span ref="labelLegendEnabled"></span>                        
+            </span>    
+         </div>`;
 
     @Autowired('gridOptionsWrapper') private gridOptionsWrapper: GridOptionsWrapper;
 
-    private columnComps: { [key: string]: AgRadioButton | AgCheckbox } = {};
+    @RefSelector('cbLegendEnabled') private cbLegendEnabled: AgCheckbox;
+    @RefSelector('labelLegendEnabled') private labelLegendEnabled: HTMLElement;
 
     private readonly chartController: ChartController;
 
     constructor(chartController: ChartController) {
-        super(DummyFormattingPanel.TEMPLATE);
+        super();
         this.chartController = chartController;
     }
 
     @PostConstruct
     private init() {
-        this.createDataGroupElements();
-        this.addDestroyableEventListener(this.chartController, ChartController.EVENT_CHART_MODEL_UPDATED, this.createDataGroupElements.bind(this));
-    }
-
-    private createDataGroupElements() {
-        this.destroyColumnComps();
-
-        const eGui = this.getGui();
-
-        const groupComp = new AgGroupComponent({label: 'Legend'});
-        this.getContext().wireBean(groupComp);
-
-        this.addLegendEnabledCheckbox(groupComp);
-
-        eGui.appendChild(groupComp.getGui());
-    }
-
-    private addLegendEnabledCheckbox(container: AgGroupComponent): void {
-        const comp = new AgCheckbox();
-
-        this.getContext().wireBean(comp);
-        comp.setLabel('Enabled');
+        this.setTemplate(DummyFormattingPanel.TEMPLATE);
 
         const chartProxy = this.chartController.getChartProxy();
         const chart = chartProxy.getChart();
 
-        // this can be simplified when chart api is improved
+        this.initLegendEnabled(chart);
+    }
+
+    private initLegendEnabled(chart: Chart) {
         let enabled = _.every(chart.series, (series) => series.showInLegend && series.visible);
-        comp.setSelected(enabled);
+        this.cbLegendEnabled.setSelected(enabled);
 
-        this.columnComps['legendEnabled'] = comp;
-
-        this.addDestroyableEventListener(comp, 'change', () => {
-
-            // this can be simplified when chart api is improved
+        this.addDestroyableEventListener(this.cbLegendEnabled, 'change', () => {
             chart.series.forEach(s => {
-                s.showInLegend = comp.isSelected();
-                // shouldn't have to toggle series!
-                s.toggleSeriesItem(1, comp.isSelected());
+                s.showInLegend = this.cbLegendEnabled.isSelected();
+                s.toggleSeriesItem(1, this.cbLegendEnabled.isSelected());
             });
         });
 
-        container.addItem(comp);
-    }
-
-    public destroy(): void {
-        super.destroy();
-        this.destroyColumnComps();
-    }
-
-    private destroyColumnComps(): void {
-        _.clearElement(this.getGui());
-        if (this.columnComps) {
-            _.iterateObject(this.columnComps, (key: string, renderedItem: Component) => renderedItem.destroy());
-        }
-        this.columnComps = {};
+        this.labelLegendEnabled.innerHTML = 'Legend Enabled';
     }
 }
