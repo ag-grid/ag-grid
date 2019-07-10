6ba6adf9455c423ca181cbd223dc1d85ff92403d
Robert Clarke <rmc-software-consultancy@gmail.com>
2019-07-10 21:52:03 +0100

    AG-3002 - improved tooltip in charts overview example

--- packages/ag-grid-docs/src/javascript-grid-charts-overview/application-created-charts/main.js
+++ packages/ag-grid-docs/src/javascript-grid-charts-overview/application-created-charts/main.js
@@ -63,8 +63,8 @@ let gridOptions = {
         opts.seriesDefaults.strokes =['#874349', '#718661', '#a48f5f', '#5a7088', '#7f637a', '#5d8692'];
 
         opts.seriesDefaults.tooltipRenderer = (params) => {
-            let value = params.datum[params.yField].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
-            return `<b>${params.title}</b>: ${value}`;
+            let value = '$' + params.datum[params.yField].toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+            return `<div style="padding: 5px"><b>${params.title}</b>: ${value}</div>`;
         };
 
         return opts;
