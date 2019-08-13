4bcf18bfb4a582a10092c5c2322302be29f290d2
ceolter <0872393174Aa>
2019-08-13 12:02:35 +0100

    AG-3190 Docs update for cell editing and navigation keys

--- packages/ag-grid-docs/src/javascript-grid-cell-editing/index.php
+++ packages/ag-grid-docs/src/javascript-grid-cell-editing/index.php
@@ -99,7 +99,7 @@ include '../documentation-main/documentation_header.php';
         The grid will stop editing when any of the following happen:
     </p>
 
-        <ul class="content">
+    <ul class="content">
         <li><b>Callback stopEditing</b>: The callback <code>stopEditing</code> (from the params above) gets called by the
             editor. This is how your cell editor informs the grid to stop editing.</li>
         <li><b>Other Cell Focus</b>: If focus in the grid goes to another cell, the editing will stop.</li>
