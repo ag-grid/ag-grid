b93ade45c9832a487b977730bdccb160b2fc5876
ceolter <0872393174Aa>
2019-08-22 10:42:26 +0100

    AG-3196 changes to security audit page

--- packages/ag-grid-docs/src/javascript-grid-security/index.php
+++ packages/ag-grid-docs/src/javascript-grid-security/index.php
@@ -13,63 +13,90 @@ include '../documentation-main/documentation_header.php';
 
     <h2>Content Security Policy (CSP)</h2>
 
-    <p>The basic information on Content Security Policy can be found on the <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP">MDN web docs</a> website 
-    and will cover the necessary information on the subject. ag-Grid works with CSP, but some basic configuration is necessary to have your application load correctly.
-    You can see below, what the minimum set of CSP rules for ag-Grid is and why: 
+    <p>
+        The basic information on Content Security Policy can be found on the
+        <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP">MDN web docs</a> website and will cover the
+        necessary information on the subject. The grid works with CSP, but some basic configuration is necessary to
+        have your application load correctly. Below is detailed what the minimum set of CSP rules for the grid is and why.
     </p>
 
     <h3>script-src</h3>
 
     <p>
-        The <code>script-src</code> policy will work only with <code>'self'</code> rule, but if you are working with custom parsers, like the 
-        <a href="/javascript-grid-cell-styles/#cell-class-rules">Cell Class Rules</a> using expressions, for example: 
-        <snippet>
-cellClassRules: {
-    'rag-green': 'x < 20',
-    'rag-amber': 'x >= 20 && x < 25',
-    'rag-red': 'x >= 25'
-}
-        </snippet>
-        it will be necessary to add the <code>unsafe-eval</code> rule to your policy.
+        The <code>script-src</code> policy will work only with <code>'self'</code> rule. If you are working with
+        expressions / code parsing inside of the grid instead of functions, it will be necessary to add the <code>unsafe-eval</code>
+        rule to your policy.
     </p>
+    <p>
+        Using expressions instead of functions is an option for many grid properties such as
+        <a href="../javascript-grid-cell-styles/#cell-class-rules">Cell Class Rules</a> and
+        <a href="../javascript-grid-value-getters/">Value Getters</a>. Below demonstrates the difference
+        where expressions are used instead of functions.
+    </p>
+
+        <snippet>
+// this column definition does NOT use expressions.
+// no need for unsafe-eval
+let colDef = {
+    cellClassRules: {
+        'rag-green': function(params) { return params.value < 20; },
+        'rag-amber': function(params) { return params.value >= 20 && params.value < 25; },
+        'rag-red': function(params) { return params.value >= 25; }
+    },
+    valueGetter: function(params) { return params.data.price * params.data.fx; }
+};
+
+// this column definition does use expressions
+// ******** unsafe-eval is needed
+let colDef = {
+    cellClassRules: {
+        'rag-green': 'x < 20',
+        'rag-amber': 'x >= 20 && x < 25',
+        'rag-red': 'x >= 25'
+    },
+    valueGetter: 'data.price * data.fx'
+};
+</snippet>
 
     <h3>style-src</h3>
 
     <p>
-        The <code>style-src</code> policty requires the <code>unsafe-inline</code> due to the nature of the grid itself. As you scroll the grid, or drag/resize 
-        columns, or do anything that changes the initial look of the grid, we apply inline styles to the relevant elements and this requires the <code>unsafe-inline</code>
-        rule to be added here.
+        The <code>style-src</code> policy requires the <code>unsafe-inline</code> due to the
+        <a href="../javascript-grid-dom-virtualisation/">DOM Row and Column Virtualisation</a>.
+        The technique the grid uses to display position rows requires explicit positioning of the
+        rows and columns. This positioning is only possible using CSS style attributes to set
+        explicit X and Y positions. This is a feature that all data grids have. Without it, the data grid
+        would have a very low limit on the amount of data that could be displayed.
     </p>
 
     <h3>img-src</h3>
     
     <p>
-        The <code>img-src</code> policy requires the <code>data:</code> rule because we load some SVG images as data strings to make sure they are pre-loaded by the grid.
-        One example of such an image is the filter menu section divider
+        The <code>img-src</code> policy requires the <code>data:</code> rule. This is because the grid pre-loads SVG
+        images as data strings. One example of such an image is the filter menu section divider.
     </p>
 
     <h3>font-src</h3>
 
     <p>
-        The <code>font-src</code> policy also requires the <code>data:</code> rule because of the same reason we pre-load SVG images as strings, we also load our icon WebFont 
-        as a string to make sure icons will not be initially empty and suddenly appear after the font is downloaded.
+        The <code>font-src</code> policy also requires the <code>data:</code> rule. This is for the same reason
+        the grid pre-loads SVG images as strings, it also pre-loads the icon WebFont
+        as a string. This is to make sure icons will not be initially empty and suddenly appear after the
+        font is downloaded.
     </p>
-    
-    <p>
 
-        So the minimal rule to load the grid is:
+    <h3>Summary</h3>
+
+    <p>
+        In summary, the minimal rule to load the grid is:
         <snippet>
         &lt;meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src data:"&gt;
         </snippet>
-        If you are using code parsing as mentioned above, the rule is:
+        However if you are using expressions / code parsing, the rule is:
         <snippet>
         &lt;meta http-equiv="Content-Security-Policy" content="default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src data:"&gt;
         </snippet>
-</snippet>
     </p>
 
-    <h2>OWASP Benchmark</h2>
-
-    <p>Stay tuned...</p>
 
 <?php include '../documentation-main/documentation_footer.php';?>
