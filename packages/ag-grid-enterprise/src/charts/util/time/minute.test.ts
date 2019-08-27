49e53d89e6ae0626690d07ad688f1f0bff93b052
Vitaly Kravchenko <vitalyx@gmail.com>
2019-08-27 17:02:36 +0100

    Minute interval unit test.

--- packages/ag-grid-enterprise/src/charts/util/time/minute.test.ts
+++ packages/ag-grid-enterprise/src/charts/util/time/minute.test.ts
@@ -1,19 +1,12 @@
 import minute from "./minute";
 
-const date = new Date(Date.UTC(2019, 7, 23, 15, 10, 5, 100));
+const date = new Date(Date.UTC(2019, 7, 23, 15, 17, 5, 100));
 
 test('minute.every', () => {
-    // console.log(minute.every(5));
     const interval = minute.every(5);
-    // if (interval) {
-    //     const d0 = new Date;
-    //     const d1 = new Date(d0.getTime() + 1000 * 60 * 60);
-    //     debugger;
-    //     if (interval.count) {
-    //         console.log(interval.count(d0, d1));
-    //     }
-    // }
     expect(interval).toBeDefined();
     if (interval) {
+        const floored = interval.floor(date);
+        expect(floored.getTime()).toBe(Date.UTC(2019, 7, 23, 15, 15, 0, 0));
     }
 });
