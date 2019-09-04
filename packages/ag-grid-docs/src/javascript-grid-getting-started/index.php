9c214546f84244b335a7fe4649b1e9ce5d8fe87a
ceolter <0872393174Aa>
2019-09-04 16:53:31 +0100

    AG-3268 Improved "Getting Started" flow

--- packages/ag-grid-docs/src/javascript-grid-getting-started/index.php
+++ packages/ag-grid-docs/src/javascript-grid-getting-started/index.php
@@ -23,9 +23,7 @@ include '../documentation-main/documentation_header.php';
 <div><div class="get-started-framework card-javascript">
     <a href="../javascript-getting-started/">JavaScript</a>
     <div>
-        <h2>JavaScript</h2>
-        <p class="card-text">Start with the vanilla JavaScript flavor of the ag-Grid API.</p>
-        <p><a href="../javascript-getting-started/">Start With JavaScript</a></p>
+        <p><a href="../javascript-getting-started/">Get Started</a></p>
     </div>
 </div></div>
 
@@ -33,27 +31,21 @@ include '../documentation-main/documentation_header.php';
 <div><div class="get-started-framework card-angular">
     <a href="../angular-getting-started/">Angular</a>
     <div>
-        <h2>Angular</h2>
-        <p class="card-text">See how ag-Grid works in the Angular context.</p>
-        <p><a href="../angular-getting-started/">Start With Angular</a></p>
+        <p><a href="../angular-getting-started/">Get Started</a></p>
     </div>
 </div></div>
 
 <div><div class="get-started-framework card-react">
     <a href="../react-getting-started/">React</a>
     <div>
-        <h2>React</h2>
-        <p class="card-text">Integrate ag-Grid in your React project.</p>
-        <p><a href="../react-getting-started/">Start With React</a></p>
+        <p><a href="../react-getting-started/">Get Started</a></p>
     </div>
 </div></div>
 
 <div><div class="get-started-framework card-vue">
     <a href="../vue-getting-started/">Vue.js</a>
     <div>
-        <h2>Vue.js</h2>
-        <p class="card-text">Get started with the Vue.js ag-Grid component wrapper.</p>
-        <p><a href="../vue-getting-started/">Start With Vue.js</a></p>
+        <p><a href="../vue-getting-started/">Get Started</a></p>
     </div>
 </div></div>
 
