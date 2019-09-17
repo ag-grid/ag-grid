877af278e575069f858fddfb6d6360c27f9e6336
Guilherme Lopes <guilherme@gwm-solutions.com>
2019-09-17 11:32:35 +0100

    AG-3268 - docs improvements

--- packages/ag-grid-docs/src/index.php
+++ packages/ag-grid-docs/src/index.php
@@ -21,13 +21,12 @@ meta_and_links($title, $keywords, $description, false);
     <header id="nav" class="compact">
         <?php include 'includes/navbar.php' ?>
     </header>
-    <div class="stage-scenarios main" style="background-color: #125082;">
-        <h2 class="heading-scenarios" style="color: white; font-weight: 500 !important;">The Best JavaScript Grid in the World</h2>
+    <div class="stage-scenarios main">
+        <h2 class="heading-scenarios">The Best JavaScript Grid in the World</h2>
 
-        <section style="display: flex;justify-content: center;">
+        <section>
             <div class="demo" id="demo-1" data-load="home/demo-1.php">
                 <div class="loading">
-                    <img src="_assets/doodles/checkbox-selection.svg" alt="Loading..." />
                     <p>Loading Demo...</p>
                 </div>
             </div>
