db1c85950706ca65451267a82ab4a77e163d6f18
Sean Landsman <sean@thelandsmans.com>
2019-09-30 15:28:22 +0100

    AG-3375 Remove web components from docs

--- packages/ag-grid-docs/src/best-angularjs-2-grid/index.php
+++ packages/ag-grid-docs/src/best-angularjs-2-grid/index.php
@@ -1,4 +1,4 @@
 <?php 
-header("HTTP/1.1 301 Moved Permanently"); 
-header("Location: https://www.ag-grid.com/best-angular-2-data-grid"); 
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.ag-grid.com/angular-grid/");
 ?>
