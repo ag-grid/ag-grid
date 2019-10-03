9708e690c26423f1c57ae95f5bd9e00d17916eeb
Bernie Sumption <bernie@berniecode.com>
2019-10-03 10:03:07 +0100

    Tighter typing in IRichCellEditorParams values

--- packages/ag-grid-community/src/ts/interfaces/iRichCellEditorParams.ts
+++ packages/ag-grid-community/src/ts/interfaces/iRichCellEditorParams.ts
@@ -1,8 +1,10 @@
 import { ICellRendererComp, ICellRendererFunc } from "../rendering/cellRenderers/iCellRenderer";
 import { ICellEditorParams } from "./iCellEditor";
 
+type CellValue = object | string | number;
+
 export interface IRichCellEditorParams extends ICellEditorParams {
-    values: string[];
+    values: CellValue[];
     cellHeight: number;
     cellRenderer: {new(): ICellRendererComp} | ICellRendererFunc | string;
 }
