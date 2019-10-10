688092f8a89bfcbe21f95c8c3fa69cdb769efff4
Sean Landsman <sean@thelandsmans.com>
2019-10-10 13:48:43 +0100

    Update windows file

--- packages/ag-grid-docs/create-windows-links.bat
+++ packages/ag-grid-docs/create-windows-links.bat
@@ -4,8 +4,8 @@ rem in windows, the linked folders inside the _dev folder don't work correctly.
 rem to get the build working, delete the contents of the _dev folder and
 rem run this script instead.
 
-mkdir _dev\ag-grid
-mkdir _dev\ag-grid\dist
+mkdir _dev\ag-grid-community
+mkdir _dev\ag-grid-community\dist
 mkdir "_dev\@ag-community"
 
 mklink /J _dev\ag-grid-angular ..\ag-grid-angular
@@ -13,7 +13,7 @@ mklink /J _dev\ag-grid-enterprise ..\ag-grid-enterprise
 mklink /J _dev\ag-grid-react ..\ag-grid-react
 mklink /J _dev\ag-grid-vue ..\ag-grid-vue
 
-mklink _dev\ag-grid\main.ts ..\ag-grid\exports.ts
-mklink /J _dev\ag-grid\dist\lib ..\ag-grid\src\ts
+mklink _dev\ag-grid-community\main.ts ..\ag-grid-community\exports.ts
+mklink /J _dev\ag-grid-community\dist\lib ..\ag-grid-community\src\ts
 
 mklink /J "_dev\@ag-community\client-side-row-model" ..\..\community-modules\client-side-row-model
