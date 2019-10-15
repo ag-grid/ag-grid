ae534f8453f81ca8ecf7a9ab0de136b7393d46ed
Vitaly Kravchenko <vitalyx@gmail.com>
2019-10-15 13:09:14 +0100

    AG-3390 - Update the Arc shape to extend Path. Move docs to the Path.

--- packages/ag-grid-enterprise/src/charts/scene/shape/path.ts
+++ packages/ag-grid-enterprise/src/charts/scene/shape/path.ts
@@ -6,10 +6,18 @@ export class Path extends Shape {
     static className = 'Path';
 
     /**
-     * Make sure to set {@link dirty} to `true` after changing the path.
+     * Declare a path to retain for later rendering and hit testing
+     * using custom Path2D class. Think of it as a TypeScript version
+     * of the native Path2D (with some differences) that works in all browsers.
      */
     readonly path = new Path2D();
 
+     /**
+     * The path only has to be updated when certain attributes change.
+     * For example, if transform attributes (such as `translationX`)
+     * are changed, we don't have to update the path. The `dirtyPath` flag
+     * is how we keep track if the path has to be updated or not.
+     */
     private _dirtyPath = true;
     set dirtyPath(value: boolean) {
         if (this._dirtyPath !== value) {
