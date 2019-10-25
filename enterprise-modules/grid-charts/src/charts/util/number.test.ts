729751ade436bd6e4a82918096a48bc18b68298a
Vitaly Kravchenko <vitalyx@gmail.com>
2019-10-25 12:21:58 +0100

    AG-3454 - default tooltip throws error for 0 values (fix toFixed method).

--- enterprise-modules/grid-charts/src/charts/util/number.test.ts
+++ enterprise-modules/grid-charts/src/charts/util/number.test.ts
@@ -4,7 +4,10 @@ test('toFixed', () => {
     expect(toFixed(0.000347985)).toBe('0.00035');
     expect(toFixed(234.000347985)).toBe('234.00');
     expect(toFixed(234.2343)).toBe('234.23');
+    expect(toFixed(234.2343, 3)).toBe('234.234');
     expect(toFixed(-0.0830894028175203)).toBe('-0.083');
+    expect(toFixed(-0.0830894028175203, 4)).toBe('-0.08309');
+    expect(toFixed(0)).toBe('0.00');
 });
 
 test('toReadableNumber', () => {
