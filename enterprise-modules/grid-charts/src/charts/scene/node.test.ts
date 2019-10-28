d914771410c402e2c2142ab3d63ff463175d42a9
Vitaly Kravchenko <vitalyx@gmail.com>
2019-10-28 10:34:10 +0000

    AG-3468 - Add Node.nextSibling method. Add unit tests for Node and Group classes.

--- enterprise-modules/grid-charts/src/charts/scene/node.test.ts
+++ enterprise-modules/grid-charts/src/charts/scene/node.test.ts
@@ -1,4 +1,5 @@
 import { Rect } from "./shape/rect";
+import { Group } from "./group";
 
 test('isNode', () => {
     const rect = new Rect();
@@ -8,4 +9,17 @@ test('isNode', () => {
     expect(Rect.isNode(5)).toBe(false);
     expect(Rect.isNode(null as any)).toBe(false);
     expect(Rect.isNode(undefined as any)).toBe(false);
+});
+
+test('nextSibling', () => {
+    const group = new Group();
+    const node1 = new Rect();
+    const node2 = new Rect();
+    const node3 = new Rect();
+
+    group.append([node1, node2, node3]);
+
+    expect(node1.nextSibling).toBe(node2);
+    expect(node2.nextSibling).toBe(node3);
+    expect(node1.nextSibling.nextSibling).toBe(node3);
 });
