238643b5e8e5163829db11be9eee2506ed4c9f71
Guilherme Lopes Portela <guilherme@gwm-solutions.com>
2019-10-28 21:20:50 +0000

    removed theme storybook

--- packages/ag-theme-storybook-enterprise/.storybook/theme.js
+++
@@ -1,6 +0,0 @@
-import { create } from "@storybook/theming";
-
-export default create({
-  base: "light",
-  brandImage: "../static/alpine-logo.svg"
-});
--- packages/ag-theme-storybook/.storybook/config.js
+++
@@ -1,4 +0,0 @@
-import { configure, addDecorator } from "@storybook/react";
-import { withKnobs } from "@storybook/addon-knobs";
-addDecorator(withKnobs);
-configure(require.context("../stories", true, /\.stories\.js$/), module);
