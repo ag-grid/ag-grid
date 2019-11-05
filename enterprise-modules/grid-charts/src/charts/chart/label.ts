6a97c61368e637eff30bedf428e56ca9ee2b5b57
Vitaly Kravchenko <vitalyx@gmail.com>
2019-11-05 14:39:53 +0000

    AG-3501 - Update chart Label to use reactive properties. Update all Labels users.

--- enterprise-modules/grid-charts/src/charts/chart/label.ts
+++ enterprise-modules/grid-charts/src/charts/chart/label.ts
@@ -1,77 +1,15 @@
 import { FontStyle, FontWeight } from "../scene/shape/text";
+import { Observable, reactive } from "../util/observable";
 
-export class Label {
-    onChange?: () => void;
+export class Label extends Observable {
+    @reactive(['style', 'data']) enabled = true;
+    @reactive(['style']) fontStyle?: FontStyle;
+    @reactive(['style']) fontWeight?: FontWeight;
+    @reactive(['style']) fontSize = 12;
+    @reactive(['style']) fontFamily = 'Verdana, sans-serif';
+    @reactive(['style']) color = 'black';
 
-    protected _enabled: boolean = true;
-    set enabled(value: boolean) {
-        if (this._enabled !== value) {
-            this._enabled = value;
-            this.update();
-        }
-    }
-    get enabled(): boolean {
-        return this._enabled;
-    }
-
-    private _fontStyle?: FontStyle;
-    set fontStyle(value: FontStyle | undefined) {
-        if (this._fontStyle !== value) {
-            this._fontStyle = value;
-            this.update();
-        }
-    }
-    get fontStyle(): FontStyle | undefined {
-        return this._fontStyle;
-    }
-
-    private _fontWeight?: FontWeight;
-    set fontWeight(value: FontWeight | undefined) {
-        if (this._fontWeight !== value) {
-            this._fontWeight = value;
-            this.update();
-        }
-    }
-    get fontWeight(): FontWeight | undefined {
-        return this._fontWeight;
-    }
-
-    private _fontSize: number = 12;
-    set fontSize(value: number) {
-        if (this._fontSize !== value) {
-            this._fontSize = value;
-            this.update();
-        }
-    }
-    get fontSize(): number {
-        return this._fontSize;
-    }
-
-    private _fontFamily: string = 'Verdana, sans-serif';
-    set fontFamily(value: string) {
-        if (this._fontFamily !== value) {
-            this._fontFamily = value;
-            this.update();
-        }
-    }
-    get fontFamily(): string {
-        return this._fontFamily;
-    }
-
-    private _color: string = 'black';
-    set color(value: string) {
-        if (this._color !== value) {
-            this._color = value;
-            this.update();
-        }
-    }
-    get color(): string {
-        return this._color;
-    }
-
-    protected update() {
-        if (this.onChange) {
-            this.onChange();
-        }
+    constructor() {
+        super();
     }
 }
