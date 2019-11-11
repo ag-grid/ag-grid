c8e8cfd8565dd8151616f0245c545b5e3b2d3711
Robert Clarke <rmc-software-consultancy@gmail.com>
2019-11-11 12:59:18 +0000

    Updated v22 release notes

--- packages/ag-grid-docs/src/ag-grid-changelog/versionReleaseNotes.php
+++ packages/ag-grid-docs/src/ag-grid-changelog/versionReleaseNotes.php
@@ -1,10 +1,42 @@
 <div class="note" style="display: none" fixVersionNote id="fix_version_22_0_0">
-    <p>Release 22.0.0 (TBH 2019)</p>
+    <p>Release 22.0.0 (11th Nov 2019)</p>
 
     <p>Feature Highlights:</p>
 
     <ul>
         <li>
+            AG-1329 - Modularise Grid Features to reduce grid bundle size
+            (see <a href="/javascript-grid-modules/">Modularisation</a>).
+        </li>
+        <li>
+            AG-3269 - A new pivotChart API has been added to charts.
+            (see <a href="/javascript-grid-charts-chart-range-api/#pivot-charts">Pivot Chart API</a>).
+        </li>
+        <li>
+            AG-2200 - Allow filters to be arranged using column groups in the filters tool panel
+            (see <a href="/javascript-grid-tool-panel-filters/">Filters Tool Panel</a>).
+        </li>
+        <li>
+            AG-2363 - Add filter search to the filters tool panel
+            (see <a href="/javascript-grid-tool-panel-filters/">Filters Tool Panel</a>).
+        </li>
+        <li>
+            AG-1862 - Allow custom column layouts in the Columns Tool Panel
+            (see <a href="/javascript-grid-tool-panel-columns/#custom-column-layout">Custom Column Tool Panel Layout</a>).
+        </li>
+        <li>
+            AG-3131 - Allow custom filter layouts in the Filters Tool Panel
+            (see <a href="javascript-grid-tool-panel-filters/#custom-filters-layout">Custom Filters Tool Panel Layout</a>).
+        </li>
+        <li>
+            AG-1991 - Allow filters and columns tool panel to have API calls to expand/collapse column groups/filters
+            (see <a href="/javascript-grid-tool-panel-columns/#expand-collapse-column-groups">Expand / Collapse Column Groups</a>)
+            (see <a href="/javascript-grid-tool-panel-filters/#expand-collapse-filter-groups">Expand / Collapse Filter Groups</a>).
+        </li>
+
+        <li>
+            AG-1026	- Allow sidebar to be placed in the left or right position of the grid
+            (see <a href="/javascript-grid-side-bar/#sidebardef-configuration">Side Bar Configuration</a>).
         </li>
     </ul>
 
@@ -13,6 +45,16 @@
         <li>
             AG-3316 - agGridReact needs to be updated to use the updated react lifecycle hooks.
             React 16.3 is now the minimum version supported by AgGridReact.
+        </li>
+        <li>
+            AG-3383 - Property selectAllOnMiniFilter no longer used, its the default behaviour for Set Filter.
+        </li>
+        <li>
+            AG-3369 - syncValuesLikeExcel is enabled by default.
+        </li>
+        <li>
+            AG-3345 / AG-3347 - tool panels are now kept in sync with the column order in the grid. Enable 'suppressSyncLayoutWithGrid'
+            on the tool panels to revert this behaviour.
         </li>
 </div>
 
