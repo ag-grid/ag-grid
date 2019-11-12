3b1165b795b5b7ef6f50db15ef4ca3b18c2e21d1
Bernie Sumption <bernie@berniecode.com>
2019-11-12 10:52:09 +0000

    AG-2832 - added documentation to icon map

--- community-modules/grid-core/src/ts/utils/general.ts
+++ community-modules/grid-core/src/ts/utils/general.ts
@@ -1296,69 +1296,138 @@ export class Utils {
         }
     }
 
+    //
+    // IMPORTANT NOTE!
+    //
+    // The comments below provide one example of how each icon is used, so that you can find
+    // an instance of it in the UI and see what it looks like in the UI. Many icons are used
+    // in multiple places.
+    //
+
     static iconNameClassMap: { [key: string]: string } = {
+        // header column group shown when expanded (click to contract)
         columnGroupOpened: 'expanded',
+        // header column group shown when contracted (click to expand)
         columnGroupClosed: 'contracted',
+        // tool panel column group contracted (click to expand)
         columnSelectClosed: 'tree-closed',
+        // tool panel column group expanded (click to contract)
         columnSelectOpen: 'tree-open',
+        // column tool panel header expand/collapse all button, shown when some children are expanded and
+        //     others are collapsed
         columnSelectIndeterminate: 'tree-indeterminate',
+        // shown on ghost icon while dragging column to the side of the grid to pin
         columnMovePin: 'pin',
+        // ??? doesn't seem to be used?
         columnMoveAdd: 'plus',
+        // shown on ghost icon while dragging over part of the page that is not a drop zone
         columnMoveHide: 'eye-slash',
+        // shown on ghost icon while dragging columns to reorder
         columnMoveMove: 'arrows',
+        // animating icon shown when dragging a column to the right of the grid causes horizontal scrolling
         columnMoveLeft: 'left',
+        // animating icon shown when dragging a column to the left of the grid causes horizontal scrolling
         columnMoveRight: 'right',
+        // shown on ghost icon while dragging over Row Groups drop zone
         columnMoveGroup: 'group',
+        // shown on ghost icon while dragging over Values drop zone
         columnMoveValue: 'aggregation',
+        // shown on ghost icon while dragging over pivot drop zone
         columnMovePivot: 'pivot',
+        // shown on ghost icon while dragging over drop zone that doesn't support it, e.g.
+        //     string column over aggregation drop zone
         dropNotAllowed: 'not-allowed',
+        // shown on row group when contracted (click to expand)
         groupContracted: 'contracted',
+        // shown on row group when expanded (click to contract)
         groupExpanded: 'expanded',
+        // context menu chart item
         chart: 'chart',
+        // chart window title bar
         close: 'cross',
+        // X (remove) on column 'pill' after adding it to a drop zone list
         cancel: 'cancel',
+        // indicates the currently active pin state in the "Pin column" sub-menu of the column menu
         check: 'tick',
+        // the following checkbox-* items are for checkboxes on themes that don't use a
+        //     native <input type="radio">
         checkboxChecked: 'checkbox-checked',
         checkboxUnchecked: 'checkbox-unchecked',
         checkboxIndeterminate: 'checkbox-indeterminate',
         checkboxCheckedReadOnly: 'checkbox-checked-readonly',
         checkboxUncheckedReadOnly: 'checkbox-unchecked-readonly',
         checkboxIndeterminateReadOnly: 'checkbox-indeterminate-readonly',
+        // "go to first" button in pagination controls
         first: 'first',
+        // "go to previous" button in pagination controls
         previous: 'previous',
+        // "go to next" button in pagination controls
         next: 'next',
+        // "go to last" button in pagination controls
         last: 'last',
+        // shown on top right of chart when chart is linked to range data (click to unlink)
         linked: 'linked',
+        // shown on top right of chart when chart is not linked to range data (click to link)
         unlinked: 'unlinked',
+        // "Choose colour" button on chart settings tab
         colorPicker: 'color-picker',
+        // the following radio-button-* items are for radio buttons on themes that don't use a
+        //     native <input type="radio">
         radioButtonOn: 'radio-button-on',
         radioButtonOff: 'radio-button-off',
+        // rotating spinner shown by the loading cell renderer
         groupLoading: 'loading',
+        // ??? doesn't seem to be used?
         data: 'data',
+        // button to launch enterprise column menu
         menu: 'menu',
+        // filter tool panel tab
         filter: 'filter',
+        // column tool panel tab
         columns: 'columns',
+        // button in chart regular size window title bar (click to maximise)
         maximize: 'maximize',
+        // button in chart maximised window title bar (click to make regular size)
         minimize: 'minimize',
+        // "Pin column" item in column header menu
         menuPin: 'pin',
+        // "Value aggregation" column menu item (shown on numeric columns when grouping is active)"
         menuValue: 'aggregation',
+        // "Group by {column-name}" item in column header menu
         menuAddRowGroup: 'group',
+        // "Un-Group by {column-name}" item in column header menu
         menuRemoveRowGroup: 'group',
+        // context menu copy item
         clipboardCopy: 'copy',
+        // ??? doesn't seem toi be used?
         clipboardCut: 'cut',
+        // context menu paste item
         clipboardPaste: 'paste',
+        // identifies the pivot drop zone
         pivotPanel: 'pivot',
+        // "Row groups" drop zone in column tool panel
         rowGroupPanel: 'group',
+        // columns tool panel Values drop zone
         valuePanel: 'aggregation',
+        // drag handle used to pick up draggable columns
         columnDrag: 'grip',
+        // drag handle used to pick up draggable rows
         rowDrag: 'grip',
+        // context menu export item
         save: 'save',
+        // version of small-right used in RTL mode
         smallLeft: 'small-left',
+        // separater between column 'pills' when you add multiple columns to the header drop zone
         smallRight: 'small-right',
+        // ??? doesn't seem to be used?
         smallUp: 'small-up',
+        // ??? doesn't seem to be used?
         smallDown: 'small-down',
+        // show on column header when column is sorted ascending
         sortAscending: 'asc',
+        // show on column header when column is sorted descending
         sortDescending: 'desc',
+        // show on column header when column has no sort, only when enabled with gridOptions.unSortIcon=true
         sortUnSort: 'none'
     };
 
