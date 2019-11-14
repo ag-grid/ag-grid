82a1235d0db4e77bcd85c54fafc73280e42bf1a3
Sean Landsman <sean@thelandsmans.com>
2019-11-14 17:05:21 +0000

    AG-3543 Automate copying of licenses in modules & legacy packages

---
+++ scripts/release/updateLicenses.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SOURCE_DIR="grid-core"
+
+COMMUNITY_ROOT=./community-modules
+SOURCE_COMMUNITY_LICENSE=$COMMUNITY_ROOT/grid-core/LICENSE.txt
+LEGACY_ROOT=./packages
+LEGACY_COMMUNITY_DIRS=("ag-grid-angular" "ag-grid-community" "ag-grid-polymer" "ag-grid-react" "ag-grid-vue")
+LEGACY_ENTERPRISE_DIRS=(ag-grid-enterprise)
+
+ENTERPRISE_ROOT=./enterprise-modules
+SOURCE_ENTERPRISE_LICENSE=$ENTERPRISE_ROOT/grid-core/LICENSE.html
+
+function copyLicenses {
+  local directory_root="$1"
+  shift
+  local source_license="$1"
+  shift
+  local directories=("$@")
+
+  for directory in "${directories[@]}";
+  do
+    if [ "$directory" != $SOURCE_DIR ]; then
+      cp "$source_license" "$directory_root/$directory/"
+    fi
+  done
+}
+
+copyLicenses $COMMUNITY_ROOT $SOURCE_COMMUNITY_LICENSE $(ls $COMMUNITY_ROOT)
+copyLicenses $LEGACY_ROOT $SOURCE_COMMUNITY_LICENSE "${LEGACY_COMMUNITY_DIRS[@]}"
+
+copyLicenses $ENTERPRISE_ROOT $SOURCE_ENTERPRISE_LICENSE  $(ls $ENTERPRISE_ROOT)
+copyLicenses $LEGACY_ROOT $SOURCE_ENTERPRISE_LICENSE "${LEGACY_ENTERPRISE_DIRS[@]}"
