a5a1317952fbee4033fc768571338095998207ce
Vitaly Kravchenko <vitalyx@gmail.com>
2019-11-22 15:38:10 +0000

    AG-3390 - Fix default axis label rotation in Cartesian charts.

--- enterprise-modules/grid-charts/src/chartAdaptor/chartComp/chartProxies/cartesian/cartesianChartProxy.ts
+++ enterprise-modules/grid-charts/src/chartAdaptor/chartComp/chartProxies/cartesian/cartesianChartProxy.ts
@@ -3,6 +3,8 @@ import { CartesianChartOptions, _, SeriesOptions, AxisOptions } from "@ag-grid-c
 import { CartesianChart } from "../../../../charts/chart/cartesianChart";
 import { ChartDataModel } from "../../chartDataModel";
 import { GroupedCategoryChart } from "../../../../charts/chart/groupedCategoryChart";
+import { ChartAxisPosition } from "../../../../charts/chart/chartAxis";
+import { find } from "../../../../charts/util/array";
 
 export abstract class CartesianChartProxy<T extends SeriesOptions> extends ChartProxy<CartesianChart | GroupedCategoryChart, CartesianChartOptions<T>> {
     protected constructor(params: ChartProxyParams) {
@@ -28,7 +30,7 @@ export abstract class CartesianChartProxy<T extends SeriesOptions> extends Chart
 
     protected updateLabelRotation(categoryId: string, isHorizontalChart = false) {
         let labelRotation = 0;
-        const axisKey = isHorizontalChart ? "yAxis" : "xAxis";
+        const axisKey = isHorizontalChart ? 'yAxis' : 'xAxis';
 
         if (categoryId !== ChartDataModel.DEFAULT_CATEGORY && !this.chartProxyParams.grouping) {
             const { label } = this.chartOptions[axisKey];
@@ -38,13 +40,11 @@ export abstract class CartesianChartProxy<T extends SeriesOptions> extends Chart
             }
         }
 
-        // this.chart.axes.forEach(axis => {
-        //     if (isHorizontalChart && axis.direction === ChartAxisDirection.Y) {
-        //         axis.label.rotation = labelRotation;
-        //     } else if (axis.direction === ChartAxisDirection.X) {
-        //         axis.label.rotation = labelRotation;
-        //     }
-        // });
+        const axisPosition = isHorizontalChart ? ChartAxisPosition.Left : ChartAxisPosition.Bottom;
+        const axis = find(this.chart.axes, axis => axis.position === axisPosition);
+        if (axis) {
+            axis.label.rotation = labelRotation;
+        }
     }
 
     protected getDefaultAxisOptions(): AxisOptions {
