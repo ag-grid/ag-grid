c7f329815df681c5d7c765e0ce1455a5ba80a54c
Sean Landsman <sean@thelandsmans.com>
2019-11-26 09:44:37 +0000

    AG-3484 Docs - Vue custom floating filter example declares buildModel, but it does not seem to be ever invoked

--- packages/ag-grid-docs/src/javascript-grid-floating-filter-component/floating-filter-component/sliderFloatingFilter_vue.js
+++ packages/ag-grid-docs/src/javascript-grid-floating-filter-component/floating-filter-component/sliderFloatingFilter_vue.js
@@ -24,7 +24,7 @@ export default Vue.extend({
         valueChanged(event) {
             this.currentValue = event.target.value;
             let valueToUse = (this.currentValue === 0) ? null : this.currentValue;
-            this.params.parentFilterInstance( function(instance) {
+            this.params.parentFilterInstance(function (instance) {
                 instance.onFloatingFilterChanged('greaterThan', valueToUse);
             });
         },
@@ -33,18 +33,6 @@ export default Vue.extend({
             // note that the filter could be anything here, but our purposes we're assuming a greater than filter only,
             // so just read off the value and use that
             this.currentValue = !parentModel ? 0 : parentModel.filter
-        },
-
-        buildModel() {
-            if (this.currentValue === 0) {
-                return null;
-            }
-            return {
-                filterType: 'number',
-                type: 'greaterThan',
-                filter: this.currentValue,
-                filterTo: null
-            };
         }
     }
 });
