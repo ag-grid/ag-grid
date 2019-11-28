5465532efa03ba0232780b211ec5d9f52a60a680
Vitaly Kravchenko <vitalyx@gmail.com>
2019-11-28 09:38:57 +0000

    AG-3390 - Remove logging from the docsLock.js.

--- scripts/docsLock.js
+++ scripts/docsLock.js
@@ -3,9 +3,6 @@ const fs = require('fs');
 module.exports = {
     lockFile: __dirname + '/../docs_are_running',
     add: function (errorMessage) {
-        console.log('Add lock file:', this.lockFile);
-        console.trace();
-
         if (this.isLocked()) {
             console.error(errorMessage);
             return false;
@@ -14,9 +11,6 @@ module.exports = {
         return true;
     },
     remove: function () {
-        console.log('Remove lock file:', this.lockFile);
-        console.trace();
-
         if (this.isLocked()) {
             fs.unlinkSync(this.lockFile);
         }
