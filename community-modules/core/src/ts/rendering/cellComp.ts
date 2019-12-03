335983ab99e26cf9c911ee3ba1d981c069a8128c
Guilherme Lopes Portela <guilherme@gwm-solutions.com>
2019-12-03 10:53:00 -0700

    AG-1630 - fixed fill handle refresh

--- community-modules/core/src/ts/rendering/cellComp.ts
+++ community-modules/core/src/ts/rendering/cellComp.ts
@@ -475,8 +475,6 @@ export class CellComp extends Component {
         this.updateAngular1ScopeAndCompile();
 
         this.refreshToolTip();
-        this.refreshHandle();
-
         // we do cellClassRules even if the value has not changed, so that users who have rules that
         // look at other parts of the row (where the other part of the row might of changed) will work.
         this.postProcessCellClassRules();
@@ -2042,8 +2040,6 @@ export class CellComp extends Component {
         if (!cellFocused && !fullRowEdit && this.editingCell) {
             this.stopRowOrCellEdit();
         }
-
-        this.refreshHandle();
     }
 
     // pass in 'true' to cancel the editing.
