145501b58cc0b45cee8d5bba74bf1dd572a01906
Robert Clarke <rmc-software-consultancy@gmail.com>
2019-12-11 12:52:11 +0000

    AG-3669 - added 'skeleton' standalone chart docs.

---
+++ packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/index.php
@@ -0,0 +1,15 @@
+<?php
+$pageTitle = "Charts Standalone: Overview";
+$pageDescription = "ag-Grid is a feature-rich data grid that can also chart data out of the box. Learn how to chart data directly from inside ag-Grid.";
+$pageKeyboards = "Javascript Grid Charting";
+$pageGroup = "feature";
+include '../documentation-main/documentation_header.php';
+?>
+
+    <h1 class="heading-enterprise">Charts Standalone - API</h1>
+
+    <p class="lead">
+       This section introduces the standalone charts API.
+    </p>
+
+<?php include '../documentation-main/documentation_footer.php'; ?>
