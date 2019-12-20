e819cc57782cf557055168bd8f56499b5ca16e7a
Sean Landsman <sean@thelandsmans.com>
2019-12-20 11:44:20 +0000

    Add es5 tests back in, fix example

--- packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/javascript-example/main.js
+++ packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/javascript-example/main.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     var parent = document.getElementById('charts');
     agCharts.agChart.create(
         {
