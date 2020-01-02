35d7e63d3ca6708ae6d364d7b4f4d7ce6d673cf4
ceolter <0872393174Aa>
2020-01-02 10:16:15 +0000

    AG-3390 added page for redirecting customers to quote execution

---
+++ packages/ag-grid-docs/src/quote/index.php
@@ -0,0 +1,6 @@
+<?php
+// This page is used by sales team. Customers go here and are subsequently redirected to the eCommerce
+
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: https://www.ag-grid.com/ecommerce/#/ecommerce/quote-lookup");
+?>
