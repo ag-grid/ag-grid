988d42248074e5c1175a9de27bd547c0a701c5af
Sean Landsman <sean@thelandsmans.com>
2020-01-14 07:02:43 +0000

    Utility script

--- scripts/addNoSyncToNodeModules.js
+++ scripts/addNoSyncToNodeModules.js
@@ -6,13 +6,20 @@ const rootPackages = require('../lerna.json').packages;
 rootPackages.forEach(rootPackage => {
     const packageDirectories = glob.sync(`${rootPackage}`);
     packageDirectories.forEach(packageDirectory => {
-        if(fs.existsSync(`${packageDirectory}/package.json`)) {
-            if(!fs.existsSync(`${packageDirectory}/node_modules`)) {
+        if (fs.existsSync(`${packageDirectory}/package.json`)) {
+            if (!fs.existsSync(`${packageDirectory}/node_modules`)) {
                 fs.mkdirSync(`${packageDirectory}/node_modules`);
             }
-            if(!fs.existsSync(`${packageDirectory}/node_modules/.nosync`)) {
+            if (!fs.existsSync(`${packageDirectory}/node_modules/.nosync`)) {
                 fs.openSync(`${packageDirectory}/node_modules/.nosync`, 'a')
             }
         }
-    })
+    });
+
+    if(!fs.existsSync(`../node_modules`)) {
+        fs.mkdirSync(`../node_modules`);
+    }
+    if(!fs.existsSync(`../node_modules/.nosync`)) {
+        fs.openSync(`../node_modules/.nosync`, 'a')
+    }
 });
