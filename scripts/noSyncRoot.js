596e777dfa07088132b52eeaa45378ee8b619aae
Sean Landsman <sean@thelandsmans.com>
2020-01-15 07:55:13 +0000

    Utility scripts

--- scripts/noSyncRoot.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-if(!fs.existsSync(`node_modules.nosync`)) {
-    fs.mkdirSync(`node_modules.nosync`);
-}
-if(!fs.existsSync(`node_modules`)) {
-    fs.symlinkSync('node_modules.nosync', 'node_modules')
-}
--- scripts/printPackageNodeModules.js
+++ scripts/printPackageNodeModules.js
@@ -1,16 +1,14 @@
 const fs = require("fs-extra");
 const glob = require("glob");
 
-const rootPackages = require('../lerna.json').packages;
+console.log('node_modules');
 
+const rootPackages = require('../lerna.json').packages;
 rootPackages.forEach(rootPackage => {
     const packageDirectories = glob.sync(`${rootPackage}`);
     packageDirectories.forEach(packageDirectory => {
         if (fs.existsSync(`${packageDirectory}/package.json`)) {
-            if (fs.existsSync(`${packageDirectory}/node_modules.nosync`)) {
-                fs.removeSync(`${packageDirectory}/node_modules.nosync`);
-                fs.mkdirSync(`${packageDirectory}/node_modules.nosync`);
-            }
+            console.log(`${packageDirectory}/node_modules`);
         }
     });
 });
