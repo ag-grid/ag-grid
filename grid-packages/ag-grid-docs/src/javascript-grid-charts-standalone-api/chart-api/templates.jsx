3cd8d620877ef0efc15f000da23b28a0b2988b55
Adrian Lowdon <adrian@acceleratech.uk>
2020-01-28 13:40:27 +0000

    AG-3110 - added more data for example charts

--- grid-packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/chart-api/templates.jsx
+++ grid-packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/chart-api/templates.jsx
@@ -3,15 +3,27 @@ import { formatJson, deepClone } from "./utils.jsx";
 export const data = [{
     month: 'Jan',
     revenue: 155,
-    profit: 33
+    profit: 33,
 }, {
     month: 'Feb',
     revenue: 123,
-    profit: 35.5
+    profit: 35.5,
 }, {
     month: 'Mar',
     revenue: 172.5,
-    profit: 41
+    profit: 41,
+}, {
+    month: 'Apr',
+    revenue: 94,
+    profit: 29,
+}, {
+    month: 'May',
+    revenue: 112.5,
+    profit: 37,
+}, {
+    month: 'Jun',
+    revenue: 148,
+    profit: 41.5,
 }];
 
 export const series = [{
--- grid-packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/chart-api/utils.jsx
+++ grid-packages/ag-grid-docs/src/javascript-grid-charts-standalone-api/chart-api/utils.jsx
@@ -1,7 +1,7 @@
 export const formatJson = object => {
     const formatters = {
         'number': x => x,
-        'string': x => `'${x}'`,
+        'string': x => `"${x}"`,
     };
 
     const formatArray = array => {
@@ -20,6 +20,8 @@ export const formatJson = object => {
     const formattedJson = JSON.stringify(object, (_, v) => Array.isArray(v) ? formatArray(v) : v, 2);
 
     return formattedJson
+        .replace(/\'/g, '\\\'') // escape single quotes
+        .replace(/\\\"/g, '"') // unescape double quotes
         .replace(/\"([^\"]+)\":/g, '$1:') // format property names
         .replace(/"\[/g, '[').replace(/\]"/g, ']') // finish array formatting
         .replace(/\"([^\"]+)\"/g, '\'$1\''); // force single quotes
