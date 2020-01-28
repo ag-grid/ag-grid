7cc06e022b729120b25e330800e309007a5d905d
Sean Landsman <sean@thelandsmans.com>
2020-01-28 14:58:33 +0000

    AG-3714 Add types ag-grid-enterprise

--- grid-packages/ag-grid-enterprise/gulpfile.js
+++ grid-packages/ag-grid-enterprise/gulpfile.js
@@ -1,5 +1,5 @@
 const gulp = require('gulp');
-const {series, parallel} = gulp;
+const {series} = gulp;
 
 const fs = require('fs');
 const clean = require('gulp-clean');
@@ -7,17 +7,10 @@ const rename = require("gulp-rename");
 const gulpTypescript = require('gulp-typescript');
 const typescript = require('typescript');
 const header = require('gulp-header');
+const concat = require('gulp-concat');
 const merge = require('merge2');
 const pkg = require('./package.json');
 const replace = require('gulp-replace');
-
-const headerTemplate = ['/**',
-    ' * <%= pkg.name %> - <%= pkg.description %>',
-    ' * @version v<%= pkg.version %>',
-    ' * @link <%= pkg.homepage %>',
-    ' * @license <%= pkg.license %>',
-    ' */',
-    ''].join('\n');
 
 const dtsHeaderTemplate =
     '// Type definitions for <%= pkg.name %> v<%= pkg.version %>\n' +
@@ -34,7 +27,8 @@ const tscMainTask = () => {
 
     return merge([
         tsResult.dts
-            .pipe(replace("\"@ag-grid-enterprise/core", "\"./dist/ag-grid-enterprise.cjs.js"))
+            .pipe(replace("\"@ag-grid-enterprise/core", "\"./dist/lib/main"))
+            .pipe(replace("\"@ag-grid-enterprise/set-filter", "\"./dist/lib/main"))
             .pipe(header(dtsHeaderTemplate, {pkg: pkg}))
             .pipe(rename("main.d.ts"))
             .pipe(gulp.dest('./')),
@@ -50,15 +44,42 @@ const cleanDist = () => {
 // End of Typescript related tasks
 
 const copyGridCoreStyles = (done) => {
-    if(!fs.existsSync('./node_modules/ag-grid-community/dist/styles')) {
+    if (!fs.existsSync('./node_modules/ag-grid-community/dist/styles')) {
         done("node_modules/ag-grid-community/dist/styles doesn't exist - exiting")
     }
 
     return gulp.src('./node_modules/ag-grid-community/dist/styles/**/*').pipe(gulp.dest('./dist/styles'));
 };
 
+const copyAndContactMainTypings = () => {
+    return gulp.src([
+        './node_modules/@ag-grid-enterprise/core/typings/main.*',
+        './node_modules/@ag-grid-enterprise/set-filter/typings/main.*'
+    ])
+        .pipe(concat('main.d.ts'))
+        .pipe(gulp.dest('./dist/lib'));
+};
+
+const copyGridCoreTypings = (done) => {
+    if (!fs.existsSync('./node_modules/@ag-grid-enterprise/core/typings')) {
+        done("node_modules/@ag-grid-enterprise/core/typings doesn't exist - exiting")
+    }
+    if (!fs.existsSync('./node_modules/@ag-grid-enterprise/set-filter/typings')) {
+        done("node_modules/@ag-grid-enterprise/set-filter/typings doesn't exist - exiting")
+    }
+
+    return gulp.src([
+        './node_modules/@ag-grid-enterprise/core/typings/**/*',
+        '!./node_modules/@ag-grid-enterprise/core/typings/main.*',
+        './node_modules/@ag-grid-enterprise/set-filter/typings/**/*',
+        '!./node_modules/@ag-grid-enterprise/set-filter/typings/main.*'
+    ])
+        .pipe(replace("@ag-grid-community/core", "ag-grid-community"))
+        .pipe(gulp.dest('./dist/lib'));
+};
+
 const copyGridAllUmdFiles = (done) => {
-    if(!fs.existsSync('./node_modules/@ag-grid-enterprise/all-modules/dist')) {
+    if (!fs.existsSync('./node_modules/@ag-grid-enterprise/all-modules/dist')) {
         done("./node_modules/@ag-grid-enterprise/all-modules/dist doesn't exist - exiting")
     }
 
@@ -70,14 +91,16 @@ const copyGridAllUmdFiles = (done) => {
 // copy from grid-core tasks
 gulp.task('copy-grid-core-styles', copyGridCoreStyles);
 gulp.task('copy-umd-files', copyGridAllUmdFiles);
+gulp.task('copy-core-typings', copyGridCoreTypings);
+gulp.task('copy-and-contact-typings-main', copyAndContactMainTypings);
 
 // Typescript related tasks
 gulp.task('clean', cleanDist);
 gulp.task('tsc-no-clean', tscMainTask);
 gulp.task('tsc', series('clean', 'tsc-no-clean'));
 
-// webpack related tasks
-gulp.task('package', series('tsc', 'copy-grid-core-styles', 'copy-umd-files'));
+// build tasks
+gulp.task('package', series('tsc', 'copy-grid-core-styles', 'copy-umd-files', 'copy-core-typings', 'copy-and-contact-typings-main'));
 
 // default/release task
 gulp.task('default', series('package'));
