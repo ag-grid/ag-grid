0ef31ef44de4d4f25999628a25cbd8eeaa9008cd
Adrian Lowdon <adrian@acceleratech.uk>
2020-01-30 12:01:41 +0000

    AG-3390 - added Jest config to run for whole monorepo

--- community-modules/core/jest.config.js
+++ community-modules/core/jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    "roots": [
+    roots: [
         "<rootDir>/src"
     ],
-    "transform": {
+    transform: {
         "^.+\\.tsx?$": "ts-jest"
     },
-    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-    "moduleFileExtensions": [
+    testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    moduleFileExtensions: [
         "ts",
         "tsx",
         "js",
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    projects: ["<rootDir>/*-modules/*/jest.config.js"],
+};
