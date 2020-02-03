59e20a2464a093e79e0cf3ae7f2fe46cfba2dee0
Adrian Lowdon <adrian@acceleratech.uk>
2020-02-03 09:35:42 +0000

    AG-3810 - expanded lead paragraph for charts API documentation page

--- grid-packages/ag-grid-docs/src/javascript-charts-api/index.php
+++ grid-packages/ag-grid-docs/src/javascript-charts-api/index.php
@@ -9,7 +9,10 @@ include '../documentation-main/documentation_header.php';
 <h1 class="heading">Charts Standalone - API</h1>
 
 <p class="lead">
-    This section details the standalone chart API.
+    This section details the standalone chart API. You can find more details about getting started with the standalone
+    charts library in the <a href="../javascript-charts-getting-started/">Getting Started</a> section. You can also
+    explore the API and see in real time how different options affect charts using the
+    <a href="../javascript-charts-api-explorer">API Explorer</a>.
 </p>
 
 <?php createDocumentationFromFile("../javascript-charts-api-explorer/config.json") ?>
