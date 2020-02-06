a45b21e3cc540da0fc3bdcf8e416d6375a6962d2
Vitaly Kravchenko <vitalyx@gmail.com>
2020-02-06 16:23:31 +0000

    AG-3110 - Minor tweaks.

--- grid-packages/ag-grid-docs/src/javascript-charts-line-series/two-number-axes/main.js
+++ grid-packages/ag-grid-docs/src/javascript-charts-line-series/two-number-axes/main.js
@@ -1,21 +1,3 @@
-function generateSpiralData() {
-    // r = a + bθ
-    // x = r * Math.cos(θ)
-    // y = r * Math.sin(θ)
-    const a = 1;
-    const b = 1;
-    const data = [];
-    const step = 0.1;
-    for (let th = 1; th < 50; th += step) {
-        const r = (a + b * th);
-        data.push({
-            x: r * Math.cos(th),
-            y: r * Math.sin(th)
-        });
-    }
-    return data;
-}
-
 agCharts.AgChart.create({
     container: document.getElementById('myChart'),
     data: generateSpiralData(),
@@ -36,3 +18,21 @@ agCharts.AgChart.create({
         enabled: false
     }
 });
+
+function generateSpiralData() {
+    // r = a + bθ
+    // x = r * Math.cos(θ)
+    // y = r * Math.sin(θ)
+    const a = 1;
+    const b = 1;
+    const data = [];
+    const step = 0.1;
+    for (let th = 1; th < 50; th += step) {
+        const r = (a + b * th);
+        data.push({
+            x: r * Math.cos(th),
+            y: r * Math.sin(th)
+        });
+    }
+    return data;
+}
