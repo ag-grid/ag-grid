4ba370dcad5c9b50b0c3c66a75a206a3d43fc56f
Robert Clarke <rmc-software-consultancy@gmail.com>
2020-02-19 09:49:59 +0000

    AG-3872 - Server-side Row Model doc improvements

--- grid-packages/ag-grid-docs/src/javascript-grid-server-side-model-grouping/preserve-group-state/main.js
+++ grid-packages/ag-grid-docs/src/javascript-grid-server-side-model-grouping/preserve-group-state/main.js
@@ -1,143 +1,134 @@
 var columnDefs = [
-    {field: 'id', hide: true},
-    {field: 'athlete'},
-    {field: 'age'},
-    {field: 'country', rowGroup: true, hide: true},
-    {field: 'year', rowGroup: true, hide: true},
-    {field: 'sport'},
-    {field: "gold"},
-    {field: "silver"},
-    {field: "bronze"}
+    { field: "country", rowGroup: true, hide: true },
+    { field: "year", rowGroup: true, hide: true },
+    { field: "gold", aggFunc: 'sum' },
+    { field: "silver", aggFunc: 'sum' },
+    { field: "bronze", aggFunc: 'sum' }
 ];
 
 var gridOptions = {
-    defaultColDef: {
-        width: 250,
-        resizable: true
-    },
     columnDefs: columnDefs,
-    // use the enterprise row model
+    defaultColDef: {
+        flex: 1,
+        minWidth: 150,
+        resizable: true,
+        sortable: true
+    },
+    autoGroupColumnDef: {
+        flex: 1,
+        minWidth: 280,
+    },
+    // use the server-side row model
     rowModelType: 'serverSide',
-    sideBar: {
-        toolPanels: [{
-            id: 'columns',
-            labelDefault: 'Columns',
-            labelKey: 'columns',
-            iconKey: 'columns',
-            toolPanel: 'agColumnsToolPanel',
-            toolPanelParams: {
-                suppressPivots: true,
-                suppressPivotMode: true,
-                suppressValues: true
-            }
-        }]
+
+    // fetch 100 rows at a time
+    cacheBlockSize: 100,
+
+    // only keep 10 blocks of rows
+    maxBlocksInCache: 10,
+
+    // supply id from data to the grid
+    getRowNodeId: function (data) {
+        return data.id;
     },
-    cacheBlockSize: 75,
-    animateRows: true,
-    getRowNodeId: function (item) {
-        return item.id;
-    },
+
+    // cache expanded group ids when groups opened
     onRowGroupOpened: function (params) {
         var id = params.data.id;
         if (params.node.expanded) {
             expandedGroupIds.push(id);
         } else {
-            expandedGroupIds = expandedGroupIds.filter(function(grpId) { return !grpId.startsWith(id); });
+            expandedGroupIds = expandedGroupIds.filter(function(grpId) {
+                return !grpId.startsWith(id);
+            });
         }
-    }
-};
+    },
 
-// This example is initialised with expanded groups
-var expandedGroupIds = ["Russia", "Russia-2002", "Ireland", "Ireland-2008"];
+    animateRows: true,
+    suppressAggFuncInHeader: true,
+    // debug: true,
+};
 
 function purgeCache() {
     gridOptions.api.purgeServerSideCache([]);
 }
 
-var allData;
+// This example is initialised with these expanded groups
+var expandedGroupIds = ["Russia", "Russia-2002", "Ireland", "Ireland-2008"];
 
-function addLeafNodeIds(data) {
-    allData = data;
+function ServerSideDatasource(server) {
+    return {
+        getRows: function(params) {
+            console.log('[Datasource] - rows requested by grid: ', params.request);
 
-    // add id to data
-    var idSequence = 0;
-    allData.forEach(function (item) {
-        item.id = idSequence++;
-    });
+            var response = server.getData(params.request);
+
+            // adding delay to simulate real sever call
+            setTimeout(function () {
+                if (response.success) {
+
+                    // add ids to data from server so we can set rows expanded
+                    var rowsWithIds = addIds(params.request, response.rows);
+
+                    // call the success callback
+                    params.successCallback(rowsWithIds, response.lastRow);
+
+                    // to preserve group state we expand any previously expanded groups for this block
+                    rowsWithIds.forEach(function(row) {
+                        if (expandedGroupIds.indexOf(row.id) > -1) {
+                            gridOptions.api.getRowNode(row.id).setExpanded(true);
+                        }
+                    });
+                } else {
+                    // inform the grid request failed
+                    params.failCallback();
+                }
+            }, 200);
+        }
+    };
 }
 
+function addIds(request, rows) {
+    var rowGroupIds = request.rowGroupCols.map(function(group) { return group.id });
+    var groupKeys = request.groupKeys;
+
+    if (groupKeys.length === rowGroupIds.length) return rows;
+
+    var groupIds = rowGroupIds.slice(0, groupKeys.length + 1);
+    rows.forEach(function(row) {
+        row.id = createGroupId(groupIds, groupKeys);
+    });
+
+    return rows;
+}
+
+function createGroupId(groupIds, groupKeys) {
+    // id's are created using a simple heuristic based on group keys: i.e. group node ids will
+    // be in the following format: 'Russia', 'Russia-2002'
+    var groupPart = groupIds.map(function(id) { return row[id] }).join('-');
+    return groupKeys.length > 0 ? groupKeys.join('-') + groupPart : groupPart;
+}
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function () {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 
-    agGrid.simpleHttpRequest({url: 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinners.json'}).then(function (data) {
+    agGrid.simpleHttpRequest({url: 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinners.json'}).then(function (data) {
 
-        addLeafNodeIds(data);
+        // add ids to data
+        var idSequence = 0;
+        data.forEach(function (item) {
+            item.id = idSequence++;
+        });
 
-        var dataSource = {
-            getRows: function (getRowParams) {
+        // setup the fake server with entire dataset
+        var fakeServer = new FakeServer(data);
 
-                // To make the demo look real, wait for 200ms before returning
-                setTimeout(function () {
+        // create datasource with a reference to the fake server
+        var datasource = new ServerSideDatasource(fakeServer);
 
-                    var response = getMockServerResponse(getRowParams.request);
-
-                    // call the success callback
-                    getRowParams.successCallback(response.rowsThisBlock, response.lastRow);
-
-                    // to preserve group state we expand any previously expanded groups for this block
-                    response.rowsThisBlock.forEach(function(row) {
-                        if (expandedGroupIds.indexOf(row.id) > -1) {
-                            gridOptions.api.getRowNode(row.id).setExpanded(true);
-                        }
-                    });
-
-                }, 200);
-            }
-        };
-
-        gridOptions.api.setServerSideDatasource(dataSource);
+        // register the datasource with the grid
+        gridOptions.api.setServerSideDatasource(datasource);
     });
 });
-
-// Note this a stripped down mock server implementation which only supports grouping
-function getMockServerResponse(request) {
-    var groupKeys = request.groupKeys;
-    var rowGroupColIds = request.rowGroupCols.map(function(x) { return x.id; });
-    var parentId = groupKeys.length > 0 ? groupKeys.join("") : "";
-
-    var rows = group(allData, rowGroupColIds, groupKeys, parentId);
-
-    var rowsThisBlock = rows.slice(request.startRow, request.endRow);
-    var lastRow = rows.length <= request.endRow ? rows.length : -1;
-
-    return { rowsThisBlock: rowsThisBlock, lastRow: lastRow };
-}
-
-function group(data, rowGroupColIds, groupKeys, parentId) {
-    var groupColId = rowGroupColIds.shift();
-    if (!groupColId) return data;
-
-    var groupedData = _(data).groupBy(function(x) { return x[groupColId]; }).value();
-
-    if (groupKeys.length === 0) {
-        return Object.keys(groupedData).map(function(key) {
-            var res = {};
-
-            // Note: the server provides group id's using a simple heuristic based on group keys:
-            // i.e. group node ids will be in the following format: 'Russia', 'Russia-2002'
-            res['id'] = getGroupId(parentId, key);
-
-            res[groupColId] = key;
-            return res;
-        });
-    }
-
-    return group(groupedData[groupKeys.shift()], rowGroupColIds, groupKeys, parentId);
-}
-
-function getGroupId(parentId, key) {
-    return parentId ? parentId + "-" + key : key;
-}
