bb256e490e03495b2b9c9e992a8f575b3889d98f
Sean Landsman <sean@thelandsmans.com>
2020-02-19 09:33:08 +0000

    AG-3872 - fix vue examples

--- grid-packages/ag-grid-docs/src/javascript-grid-server-side-model-sort-filter/set-filter-complex-object/main.js
+++ grid-packages/ag-grid-docs/src/javascript-grid-server-side-model-sort-filter/set-filter-complex-object/main.js
@@ -1,3 +1,7 @@
+var countryMap = {"United States": "US", "Russia": "RU", "Australia": "AU", "Canada": "CA", "Norway": "NO", "China": "CN", "Zimbabwe": "ZW", "Venezuela": "VE", "Netherlands": "NL", "South Korea": "KR", "Croatia": "HR", "France": "FR", "Japan": "JP", "Hungary": "HU", "Germany": "DE", "Poland": "PL", "South Africa": "ZA", "Sweden": "SE", "Ukraine": "UA", "Italy": "IT", "Czech Republic": "CZ", "Austria": "AT", "Finland": "FI", "Romania": "RO", "Vietnam": "VN", "Great Britain": "GB", "Jamaica": "JM", "Singapore": "SG", "Belarus": "BY", "Chile": "CL", "Spain": "ES", "Tunisia": "TN", "Brazil": "BR", "Slovakia": "SK", "Costa Rica": "CR", "Bulgaria": "BG", "Switzerland": "CH", "New Zealand": "NZ", "Estonia": "EE", "Kenya": "KE", "Ethiopia": "ET", "Trinidad and Tobago": "TT", "Turkey": "TR", "Morocco": "MA", "Bahamas": "BS", "Slovenia": "SI", "Armenia": "AM", "Azerbaijan": "AZ", "India": "IN", "Puerto Rico": "PR", "Egypt": "EG", "Kazakhstan": "KZ", "Chinese Taipei": "ROC", "Georgia": "GE", "Lithuania": "LT", "Cuba": "CU", "Colombia": "CO", "Mongolia": "MN", "Uzbekistan": "UZ", "North Korea": "KP", "Tajikistan": "TJ", "Kyrgyzstan": "KG", "Greece": "GR", "Indonesia": "ID", "Thailand": "TH", "Latvia": "LV", "Mexico": "MX", "Nigeria": "NG", "Qatar": "QA", "Serbia": "RS", "Hong Kong": "HK", "Denmark": "DK", "Portugal": "PT", "Argentina": "AR", "Afghanistan": "AF", "Gabon": "GA", "Dominican Republic": "DO", "Belgium": "BE", "Kuwait": "KW", "United Arab Emirates": "AE", "Cyprus": "CY", "Israel": "IL", "Algeria": "DZ", "Montenegro": "ME", "Iceland": "IS", "Paraguay": "PY", "Cameroon": "CM", "Saudi Arabia": "SA", "Ireland": "IE", "Malaysia": "MY", "Uruguay": "UY", "Togo": "TG", "Mauritius": "MU", "Botswana": "BW", "Guatemala": "GT", "Bahrain": "BH", "Grenada": "GD", "Uganda": "UG", "Sudan": "SD", "Ecuador": "EC", "Panama": "PA", "Eritrea": "ER", "Sri Lanka": "LK", "Mozambique": "MZ", "Barbados": "BB"};
+
+var countryCodeMap = {US: "United States", RU: "Russia", AU: "Australia", CA: "Canada", NO: "Norway", CN: "China", ZW: "Zimbabwe", NL: "Netherlands", KR: "South Korea", HR: "Croatia", FR: "France", JP: "Japan", HU: "Hungary", DE: "Germany", PL: "Poland", ZA: "South Africa", SE: "Sweden", UA: "Ukraine", IT: "Italy", CZ: "Czech Republic", AT: "Austria", FI: "Finland", RO: "Romania", GB: "Great Britain", JM: "Jamaica", SG: "Singapore", BY: "Belarus", CL: "Chile", ES: "Spain", TN: "Tunisia", BR: "Brazil", SK: "Slovakia", CR: "Costa Rica", BG: "Bulgaria", CH: "Switzerland", NZ: "New Zealand", EE: "Estonia", KE: "Kenya", ET: "Ethiopia", TT: "Trinidad and Tobago", TR: "Turkey", MA: "Morocco", BS: "Bahamas", SI: "Slovenia", AM: "Armenia", AZ: "Azerbaijan", IN: "India", PR: "Puerto Rico", EG: "Egypt", KZ: "Kazakhstan", ROC: "Chinese Taipei", GE: "Georgia", LT: "Lithuania", CU: "Cuba", CO: "Colombia", MN: "Mongolia", UZ: "Uzbekistan", KP: "North Korea", TJ: "Tajikistan", KG: "Kyrgyzstan", GR: "Greece", ID: "Indonesia", TH: "Thailand", LV: "Latvia", MX: "Mexico", NG: "Nigeria", QA: "Qatar", RS: "Serbia", HK: "Hong Kong", DK: "Denmark", PT: "Portugal", AR: "Argentina", AF: "Afghanistan", GA: "Gabon", DO: "Dominican Republic", BE: "Belgium", KW: "Kuwait", AE: "United Arab Emirates", CY: "Cyprus", IL: "Israel", DZ: "Algeria", ME: "Montenegro", IS: "Iceland", PY: "Paraguay", CM: "Cameroon", SA: "Saudi Arabia", IE: "Ireland", MY: "Malaysia", UY: "Uruguay", TG: "Togo", MU: "Mauritius", BW: "Botswana", GT: "Guatemala", BH: "Bahrain", GD: "Grenada", UG: "Uganda", SD: "Sudan", EC: "Ecuador", PA: "Panama", ER: "Eritrea", LK: "Sri Lanka", MZ: "Mozambique", BB: "Barbados"};
+
 var columnDefs = [
     {
       field: 'country',
@@ -75,6 +79,74 @@ function ServerSideDatasource(server) {
                     params.failCallback();
                 }
             }, 500);
+        }
+    };
+}
+
+function FakeServer(allData) {
+
+    // patch country data to use complex object
+    allData.forEach(function (d) {
+        d.country = {
+            code: countryMap[d.country],
+            name: d.country
+        };
+    });
+
+    function doFilter(data, filterModel) {
+        var filterPresent = filterModel && Object.keys(filterModel).length > 0;
+        if (!filterPresent) return data;
+        return data.filter(function(d) {
+            return filterModel.country.values.indexOf(d.country.code) > -1;
+        });
+    }
+
+    function doSort(data, sortModel) {
+        var sortPresent = sortModel && sortModel.length > 0;
+        if (!sortPresent) return data;
+
+        var sortedData = data.slice();
+        sortedData.sort(function (a, b) {
+            for (var k = 0; k < sortModel.length; k++) {
+                var sortColModel = sortModel[k];
+
+                var valueA = a[sortColModel.colId];
+                if (valueA instanceof Object) {
+                    valueA = valueA.name;
+                }
+
+                var valueB = b[sortColModel.colId];
+                if (valueB instanceof Object) {
+                    valueB = valueB.name;
+                }
+
+                if (valueA === valueB) {
+                    continue;
+                }
+                var sortDirection = sortColModel.sort === 'asc' ? 1 : -1;
+                return valueA > valueB ? sortDirection : sortDirection * -1;
+            }
+            return 0;
+        });
+        return sortedData;
+    }
+
+    return {
+        getData: function(request) {
+            var filteredData = doFilter(allData, request.filterModel);
+            var filteredAndSortedData = doSort(filteredData, request.sortModel);
+
+            // take a slice of the rows for requested block
+            var rowsForBlock = filteredAndSortedData.slice(request.startRow, request.endRow);
+
+            // if on or after the last page, work out the last row.
+            var lastRow = allData.length <= request.endRow ? allData.length : -1;
+
+            return {
+                success: true,
+                rows: rowsForBlock,
+                lastRow: lastRow
+            };
         }
     };
 }
--- grid-packages/ag-grid-docs/src/javascript-grid-server-side-model-sort-filter/set-filter/main.js
+++ grid-packages/ag-grid-docs/src/javascript-grid-server-side-model-sort-filter/set-filter/main.js
@@ -41,6 +41,108 @@ function getCountryValuesAsync(params) {
     }, 500);
 }
 
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+
+    agGrid.simpleHttpRequest({url: 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinners.json'}).then(function (data) {
+        // setup the fake server with entire dataset
+        fakeServer = new FakeServer(data);
+
+        // create datasource with a reference to the fake server
+        var datasource = new ServerSideDatasource(fakeServer);
+
+        // register the datasource with the grid
+        gridOptions.api.setServerSideDatasource(datasource);
+    });
+});
+
+// This fake server uses http://alasql.org/ to mimic how a real server
+// might generate sql queries from the Server-side Row Model request.
+// To keep things simple it does the bare minimum to support the example.
+function FakeServer(allData) {
+    alasql.options.cache = false;
+
+    return {
+        getData: function (request) {
+            var results = executeQuery(request);
+            return {
+                success: true,
+                rows: results,
+                lastRow: getLastRowIndex(request, results)
+            };
+        },
+        getCountries: function() {
+            var SQL = 'SELECT DISTINCT country FROM ? ORDER BY country asc';
+            var result = alasql(SQL, [allData]);
+            return result.map(Object.values);
+        }
+    };
+
+    function executeQuery(request) {
+        var SQL = buildSql(request);
+
+        console.log('[FakeServer] - about to execute query:', SQL);
+
+        return alasql(SQL, [allData]);
+    }
+
+    function buildSql(request) {
+        var select = 'SELECT * ';
+        var from = 'FROM ? ';
+        var where = whereSql(request);
+        var orderBy = orderBySql(request);
+        var limit = limitSql(request);
+
+        return select + from + where + orderBy + limit;
+    }
+
+    function whereSql(request) {
+        var whereParts = [];
+
+        var filterModel = request.filterModel;
+        if (filterModel) {
+            var columnKeys = Object.keys(filterModel);
+            whereParts = columnKeys.map(function (columnKey) {
+                var filter = filterModel[columnKey];
+                if (filter.filterType === 'set') {
+                    return columnKey + ' IN (\'' + filter.values.join("', '") + '\')';
+                }
+                console.log('unsupported filter type: ' + filter.filterType);
+                return '';
+            });
+        }
+
+        if (whereParts.length > 0) {
+            return ' WHERE ' + whereParts.join(' AND ') + ' ';
+        }
+
+        return '';
+    }
+
+    function orderBySql(request) {
+        var sortModel = request.sortModel;
+        if (sortModel.length === 0) return '';
+
+        var sorts = sortModel.map(function(s) {
+            return s.colId + ' ' + s.sort;
+        });
+
+        return 'ORDER BY ' + sorts.join(', ') + ' ';
+    }
+
+    function limitSql(request) {
+        var blockSize = request.endRow - request.startRow;
+        return ' LIMIT ' + (blockSize + 1) + ' OFFSET ' + request.startRow;
+    }
+
+    function getLastRowIndex(request, results) {
+        if (!results || results.length === 0) return -1;
+        var currentLastRow = request.startRow + results.length;
+        return currentLastRow <= request.endRow ? currentLastRow : -1;
+    }
+}
 
 function ServerSideDatasource(server) {
     return {
@@ -62,20 +164,3 @@ function ServerSideDatasource(server) {
         }
     };
 }
-
-// setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function () {
-    var gridDiv = document.querySelector('#myGrid');
-    new agGrid.Grid(gridDiv, gridOptions);
-
-    agGrid.simpleHttpRequest({url: 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/packages/ag-grid-docs/src/olympicWinners.json'}).then(function (data) {
-        // setup the fake server with entire dataset
-        fakeServer = new FakeServer(data);
-
-        // create datasource with a reference to the fake server
-        var datasource = new ServerSideDatasource(fakeServer);
-
-        // register the datasource with the grid
-        gridOptions.api.setServerSideDatasource(datasource);
-    });
-});
