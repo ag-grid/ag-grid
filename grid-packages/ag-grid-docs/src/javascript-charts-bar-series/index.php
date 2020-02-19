e93d5926211109157e669bf0337a6b107cdd2eae
Vitaly Kravchenko <vitalyx@gmail.com>
2020-02-19 16:31:57 +0000

    AG-3110 - Finish Bar/Column series docs.

--- grid-packages/ag-grid-docs/src/javascript-charts-bar-series/index.php
+++ grid-packages/ag-grid-docs/src/javascript-charts-bar-series/index.php
@@ -146,10 +146,84 @@ label: {
 
 <h3>Column Labels</h3>
 
+<p>
+    It's possible to add labels to columns, by adding the following to the series config:
+</p>
+
+<snippet language="ts">
+label: {}
+</snippet>
+
+<p>
+    That's it. The config can be empty like that. However, you might want to
+    format your labels. For example, by default the values are rounded to two
+    decimal places before they are used as labels. But for the example below
+    even that is too much, so we use a label formatter that returns the integer
+    part of the number only:
+</p>
+
+<snippet language="ts">
+label: {
+    formatter: function (params) {
+        // for series datums that are not valid numbers,
+        // the formatter's `value` will be `undefined`
+        if (params.value !== undefined) {
+            return params.value.toFixed(0);
+        }
+        return '';
+    }
+}
+</snippet>
+
+<p>
+    The above formatter allows to produce a nice looking chart where the labels
+    don't stick out of their columns:
+</p>
+
 <?= chart_example('Column Series with Labels', 'labeled-column'); ?>
+
+<note>
+    It's best to avoid using labels with grouped columns (or bars),
+    because columns in grouped mode tend to be narrow and often won't fit
+    a label.
+</note>
 
 <h2>Bar Series</h2>
 
+<p>
+    <code>'bar'</code> series configuration is exactly the same as <code>'column'</code>
+    series configuration and all the same modes (regular, stacked, grouped, normalized)
+    apply to bars just as they do to columns.
+</p>
+
+<p>
+    To create a bar chart all one needs to do is to use <code>type: 'bar'</code>
+    instead of <code>type: 'column'</code> in the series config and swap the axes
+    &mdash; the <code>'category'</code> axis moves from the bottom to the left
+    of a chart, and the <code>'number'</code> axis takes its place instead, moving
+    from the left to the bottom:
+</p>
+
+<snippet language="ts">
+axes: [{
+    type: 'number',
+    position: 'bottom'
+}, {
+    type: 'category',
+    position: 'left'
+}]
+</snippet>
+
+<p>
+    With these simple changes we go from <a href="#stacked-columns">stacked columns</a>
+    to stacked bars:
+</p>
+
 <?= chart_example('Stacked Bar Series', 'stacked-bar'); ?>
+
+<h2>Bar/Column Labels API Reference</h2>
+
+<?php createDocumentationFromFile("../javascript-charts-api-explorer/config.json", "barSeriesConfig.label") ?>
+
 
 <?php include '../documentation-main/documentation_footer.php'; ?>
