abc12443a0d11a01924e52e41b1f12a9953f516d
Vitaly Kravchenko <vitalyx@gmail.com>
2020-02-27 16:16:45 +0000

    AG-3110 - Axis Title example. Axis documentation improvements.

--- grid-packages/ag-grid-docs/src/javascript-charts-axis/index.php
+++ grid-packages/ag-grid-docs/src/javascript-charts-axis/index.php
@@ -8,7 +8,10 @@ include '../documentation-main/documentation_header.php';
 
 <h1>Axes</h1>
 
-<h2>Overview</h2>
+<p class="lead">
+    This section explains what chart axes are, how to configure and style them, and which axis type to use
+    in which situation.
+</p>
 
 <p>
     A chart uses axes to plot data such as categories and values by converting them to screen coordinates.
@@ -19,29 +22,42 @@ include '../documentation-main/documentation_header.php';
 </p>
 
 <p>
-    The charting library supports three axes types: <code>'category'</code>, <code>'number'</code>,
-    and <code>'time'</code>. Each type is tailored to be used with certain types of data.
+    The charting library supports three axis types:
+    <ul>
+        <li><a href="#category-axis">Category Axis</a></li>
+        <li><a href="#number-axis">Number Axis</a></li>
+        <li><a href="#time-axis">Time Axis</a></li>
+    </ul>
+    Each type is tailored to be used with certain types of data.
     An axis can be positioned to any side of a chart &mdash; <code>'top'</code>, <code>'right'</code>,
-    <code>'bottom'</code>, <code>'left'</code> &mdash; but only a single axis can exist at any given
-    position, meaning that you can have up to four axes in a chart. Just like with series, the axes
-    can be specified by setting the corresponding <code>axes</code> array property of a chart.
+    <code>'bottom'</code>, or <code>'left'</code>. Just like with series, the axes can be specified
+    by setting the corresponding <code>axes</code> array property of a chart.
 </p>
 
 <note>
-    Axes are only supported in cartesian charts, not polar. For example, you can't use axes
-    with pie series.
+    Axes are only supported in <a href="https://en.wikipedia.org/wiki/Cartesian_coordinate_system" target="blank">cartesian</a> charts,
+    not <a href="https://en.wikipedia.org/wiki/Polar_coordinate_system" target="blank">polar</a>.
+    For example, you can't use axes with pie series.
 </note>
 
 <h3>Category Axis</h3>
 
 <p>
-    The <code>'category'</code> axis is meant to be used with relatively small data sets
+    The category axis is meant to be used with relatively small data sets
     of discrete values or categories, such as sales per product, person or quarter,
-    where <em>product</em>, <em>person</em> and <em>quarter</em> are categories. The category axis
-    renders a tick, a label and a grid line for each category, and spaces out all ticks evenly.
-    <code>'category'</code> axis is commonly used as the x-axis, and positioned at the
-    <code>'bottom'</code> of the chart. The simplest config looks like this:
+    where <em>product</em>, <em>person</em> and <em>quarter</em> are categories.
 </p>
+
+<p>
+    The category axix renders a <a href="#ticks">tick</a>, a <a href="#labels">label</a>
+    and a <a href="#grid-lines">grid line</a> for each category, and spaces out all ticks evenly.
+</p>
+
+<p>
+    The category axis is commonly used as the x-axis, and positioned at the bottom of a chart.
+</p>
+
+<p>The simplest category axis config looks like this:</p>
 
 <snippet language="ts">
 {
@@ -53,12 +69,16 @@ include '../documentation-main/documentation_header.php';
 <h3>Number Axis</h3>
 
 <p>
-    The <code>'number'</code> axis is meant to be used as a value axis. While categories
+    The number axis is meant to be used as a value axis. While categories
     are spaced out evenly, the distance between values depends on their magnitude.
+</p>
+<p>
     Intead of using one tick per value, the number axis will determine the range of all
     values, round it up and try to segment the rounded range with 10 evenly spaced ticks.
-    <code>'number'</code> axis is commonly used as the y-axis and positioned to the
-    <code>'left'</code> of the chart. Here's the simplest number axis config:
+</p>
+<p>
+    The number axis is commonly used as the y-axis and positioned to the
+    left a chart. Here's the simplest number axis config:
 </p>
 
 <snippet language="ts">
@@ -76,8 +96,11 @@ include '../documentation-main/documentation_header.php';
     with numeric data (in addition to <code>Date</code> objects), but the numbers will be
     interpreted as Unix timestamps. The <code>'time'</code> axis differs from the
     <code>'number'</code> axis in tick segmentation and label formatting. You can choose
-    to place a tick every 5 minutes, every month or every Friday for example. Time axis also
-    supports specifier strings to control the way time values are presented as labels.
+    to place a tick every 5 minutes, every month or every Friday for example.
+</p>
+
+<p>
+    Time axis also supports specifier strings to control the way time values are presented as labels.
     For example, the <code>%H:%M:%S</code> specifier string will instruct the axis to
     format a time value like <code>new Date('Tue Feb 04 2020 15:08:03')</code> or <code>1580828883000</code>
     as <code>'15:08:03'</code>. Time axes are typically used as x-axes and placed at the bottom
@@ -91,11 +114,27 @@ include '../documentation-main/documentation_header.php';
 }
 </snippet>
 
-<h2>Position and Title</h2>
+<h2>Title</h2>
 
-<h2>Tick Format</h2>
+<p>
+    Sometimes it may not be obvious what a chart's dimension represents. For example, you can see the
+    numbers on a chart's axis and not being able to tell what they are. Are they millimeters, percentages
+    or dollars? And sometimes it's helpful to provide extra information. For example, category axis
+    labels can clearly show people's names, but it might be worth knowing that they are some company's
+    best performing sales people.
+</p>
 
-<h2>Label Format</h2>
+<p>
+    Luckily, an axis can have a title just like a chart. In the example below we can use axes titles
+    to show that the bottom axis shows desktop operating systems and the left one their percentage market
+    share:
+</p>
+
+<?= chart_example('Chart Axis Title', 'axis-title', 'generated'); ?>
+
+<h2>Ticks</h2>
+
+<h2>Labels</h2>
 
 <h2>Grid Lines</h2>
 
