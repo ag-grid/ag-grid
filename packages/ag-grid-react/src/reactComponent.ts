5b3271bdc1212f7613da48fffef60ecc073c38a2
James <csit.intern.james@gmail.com>
2020-02-28 09:26:34 +0800

    Pre-populate parent if autoHeight is enabled

--- packages/ag-grid-react/src/reactComponent.ts
+++ packages/ag-grid-react/src/reactComponent.ts
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {ReactPortal} from 'react';
+import {renderToStaticMarkup} from 'react-dom/server';
 import * as ReactDOM from 'react-dom';
 import {Promise, Utils} from 'ag-grid-community';
 import {AgGridReact} from "./agGridReact";
@@ -11,6 +12,7 @@ export class ReactComponent extends BaseReactComponent {
     static REACT_MEMO_TYPE = ReactComponent.hasSymbol() ? Symbol.for('react.memo') : 0xead3;
 
     private eParentElement!: HTMLElement;
+    private autoHeightMarkForDeath: HTMLElement | null = null;
     private componentInstance: any;
 
     private reactComponent: any;
@@ -38,10 +40,19 @@ export class ReactComponent extends BaseReactComponent {
     public getReactComponentName(): string {
         return this.reactComponent.name;
     }
-
     public init(params: any): Promise<void> {
+        this.eParentElement = this.createParentElement(params);
+
+        // we populate the innerHTML first, so that autoRowHeight can be calulated first
+        if (params.colDef.autoHeight) {
+            const reactComponent = React.createElement(this.reactComponent, params);
+            this.eParentElement.innerHTML = renderToStaticMarkup(reactComponent);
+            if (this.eParentElement.children.length > 0) { // in theory, we should get a new HTML element
+                this.autoHeightMarkForDeath = this.eParentElement.children[0] as HTMLElement;
+            }
+        }
+        
         return new Promise<void>((resolve: any) => {
-            this.eParentElement = this.createParentElement(params);
             this.createReactComponent(params, resolve);
         });
     }
@@ -71,7 +82,19 @@ export class ReactComponent extends BaseReactComponent {
             generateNewKey() // fixed deltaRowModeRefreshCompRenderer
         );
         this.portal = portal;
-        this.parentComponent.mountReactPortal(portal!, this, resolve);
+        if (!params.colDef.autoHeight) {
+            this.parentComponent.mountReactPortal(portal!, this, resolve);
+        } else { // remove the hack that allowed autoHeight to work (note, does not affect display, so it's an visually invisible change to the user)
+            this.parentComponent.mountReactPortal(portal!, this, (value: any) => {
+                resolve(value);
+                setTimeout(() => {
+                    const markForDeath = this.autoHeightMarkForDeath;
+                    if (markForDeath != null) {
+                        markForDeath.remove();
+                    }
+                }, 100);
+            });
+        }
     }
 
     private addParentContainerStyleAndClasses() {
