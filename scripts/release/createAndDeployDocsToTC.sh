93e291b6fb3ee48cd9d9aa1804fe7ed47acdd757
Sean Landsman <sean@thelandsmans.com>
2020-02-28 08:38:33 +0000

    Utility script for deploying docs to TC

--- scripts/release/createAndDeployDocsToTC.sh
+++ scripts/release/createAndDeployDocsToTC.sh
@@ -3,7 +3,7 @@
 ZIP_PREFIX=`date +%Y%m%d`
 
 cd grid-packages/ag-grid-docs
-npx gulp release
+npx gulp release-archive
 cd dist
 
 FILENAME=release_"$ZIP_PREFIX"_v"$ZIP_PREFIX".zip
@@ -18,6 +18,3 @@ unzip /var/www/html/$FILENAME -d /var/www/html/
 touch /var/www/html/robots.txt
 echo "User-agent: *
 Disallow: /" > /var/www/html/robots.txt
-
-#mkdir /var/www/html/lighthouse/
-#npx lighthouse http://teamcity.ag-grid.com  --chrome-flags="--headless" --output json --output-path /var/www/html/lighthouse/report.json
