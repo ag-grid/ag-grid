a2e040dae8fac878780c1b918513b79289fd7813
Adrian Lowdon <adrian@acceleratech.uk>
2020-03-06 09:33:25 +0000

    AG-3390 - updated snippet helper to ensure Angular does not bind the snippet contents

--- grid-packages/ag-grid-docs/src/php-utils/snippets.php
+++ grid-packages/ag-grid-docs/src/php-utils/snippets.php
@@ -2,5 +2,5 @@
     function createSnippet($code, $language = 'js') {
         $formattedCode = preg_replace('/\<(\/?[^\>]+)\>/', '&lt;$1&gt;', $code);
 
-        return "<snippet language=\"$language\">$formattedCode</snippet>";
+        return "<snippet language=\"$language\"><div ng-non-bindable>$formattedCode</div></snippet>";
     }
