b2f57fd106ab8ee852eb77a1756ab159a50e68ed
Sean Landsman <sean@thelandsmans.com>
2020-03-31 13:54:24 +0100

    AG-3937 Webpack + Typescript

--- grid-packages/ag-grid-docs/src/ag-grid-building-typescript/index.php
+++ grid-packages/ag-grid-docs/src/ag-grid-building-typescript/index.php
@@ -1,7 +1,7 @@
 <?php
-$pageTitle = "ag-Grid Reference: Using TypeScript and Webpack 2";
-$pageDescription = "This Getting Started guide demonstrates building ag-Grid with TypeScript and webpack 2. Featuring step by step guide and code examples.";
-$pageKeywords = "TypeScript Grid Webpack 2";
+$pageTitle = "ag-Grid Reference: Using TypeScript and Webpack 4";
+$pageDescription = "This Getting Started guide demonstrates building ag-Grid with TypeScript and Webpack 4. Featuring step by step guide and code examples.";
+$pageKeywords = "TypeScript Grid Webpack 4";
 $pageGroup = "basics";
 
 include '../documentation-main/documentation_header.php';
@@ -10,7 +10,7 @@ include '../documentation-main/documentation_header.php';
 <div>
 
     <h1 id="typescript-building-with-webpack">
-         TypeScript - Building with Webpack 2</h1>
+         TypeScript - Building with Webpack</h1>
 
     <p class="lead">We walk through the main steps required when using ag-Grid, TypeScript and Webpack below, but for more
         information about
@@ -32,8 +32,8 @@ npm i --save @ag-grid-community/all-modules
 # or, if using Enterprise features
 # npm i --save @ag-grid-enterprise/all-modules
 
-npm i --save-dev typescript ts-loader webpack webpack-dev-server extract-text-webpack-plugin
-npm i --save-dev css-loader style-loader html-loader html-webpack-plugin
+npm i --save-dev typescript ts-loader webpack webpack-dev-server webpack-cli
+npm i --save-dev sass-loader node-sass style-loader css-loader html-webpack-plugin
 </snippet>
 
     <h3>Create Application</h3>
@@ -41,18 +41,19 @@ npm i --save-dev css-loader style-loader html-loader html-webpack-plugin
     <p>Our application will be a very simple one, consisting of a single class that will render a simple grid:</p>
 
     <snippet>
-// src/SimpleGrid.ts 
-import {Grid, GridOptions} from "@ag-grid-community/all-modules";
-import "@ag-grid-community/all-modules/dist/styles/ag-grid.css";
-import "@ag-grid-community/all-modules/dist/styles/ag-theme-balham.css";
+import {Grid, GridOptions, ModuleRegistry} from "@ag-grid-community/all-modules";
+import {ClientSideRowModelModule} from "@ag-grid-community/client-side-row-model";
 
 // or, if using Enterprise features
-// import {Grid, GridOptions} from "@ag-grid-enterprise/all-modules";
-// import "@ag-grid-enterprise/all-modules/dist/styles/ag-grid.css";
-// import "@ag-grid-enterprise/all-modules/dist/styles/ag-theme-balham.css";
+// import {Grid, GridOptions, ModuleRegistry} from "@ag-grid-enterprise/all-modules";
+// import {ClientSideRowModelModule} from "@ag-grid-enterprise/all-modules";
+
+ModuleRegistry.register(ClientSideRowModelModule);
+
+import './styles.scss';
 
 class SimpleGrid {
-    private gridOptions: GridOptions = &lt;GridOptions&gt;{};
+    private gridOptions: GridOptions = &lt;GridOptions>{};
 
     constructor() {
         this.gridOptions = {
@@ -60,7 +61,7 @@ class SimpleGrid {
             rowData: this.createRowData()
         };
 
-        let eGridDiv:HTMLElement = &lt;HTMLElement&gt;document.querySelector('#myGrid');
+        let eGridDiv:HTMLElement = &lt;HTMLElement>document.querySelector('#myGrid');
         new Grid(eGridDiv, this.gridOptions);
     }
 
@@ -83,7 +84,9 @@ class SimpleGrid {
     }
 }
 
-new SimpleGrid();</snippet>
+new SimpleGrid();
+
+</snippet>
 
 <snippet language="html">
 &lt;!DOCTYPE html&gt;
@@ -91,80 +94,46 @@ new SimpleGrid();</snippet>
 &lt;head&gt;
 &lt;/head&gt;
 &lt;body&gt;
-&lt;div id="myGrid" style="height: 150px;width: 600px" class="ag-theme-balham"&gt;&lt;/div&gt;
+    &lt;div id="myGrid" style="height: 200px;width: 600px" class="ag-theme-alpine"&gt;&lt;/div&gt;
 &lt;/body&gt;
 &lt;/html&gt;</snippet>
 
-    <h2>TypeScript Configuration</h2>
-
-    <p>Our <code>tsconfig.json</code> is very simple in this example:</p>
-
-    <snippet>
-// tsconfig.json
-{
-  "compilerOptions": {
-    "sourceMap":  true
-  }
-}</snippet>
 
     <h2>Webpack Configuration</h2>
 
-    <p>We have 2 Webpack Configurations in the example project - a dev configuration and a production configuration. In
-        both
-        of these configurations we make use of an html file where our generated bundle(s) will be inserted and will
-        serve as our application
-        starting point, as well as a helper file for within use of the webpack configurations:</p>
-
-    <h3 id="webpack-dev-configuration">Webpack Development Configuration</h3>
-
-    <snippet>
-// config/webpack.dev.js
-var webpack = require('webpack');
-var path = require('path');
-
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+<snippet>
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    devtool: 'cheap-module-eval-source-map',
-
     entry: './src/SimpleGrid.ts',
-
-    output: {
-        path: path.resolve('dist'),
-        publicPath: 'http://localhost:8080/',
-        filename: 'bundle.js'
-    },
-
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-
     module: {
-        loaders: [
+        rules: [
             {
-                test: /\.ts$/,
-                loader: 'ts-loader'
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
             {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader?sourceMap'})
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
             }
-        ]
+        ],
     },
-
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
     plugins: [
-        new ExtractTextPlugin({filename: '[name].css'}),
-
         new HtmlWebpackPlugin({
-            template: 'config/index.html'
+            template: 'src/index.html'
         })
     ]
-};</snippet>
+};
+</snippet>
 
     <h4>
         <code>entry</code>
@@ -179,93 +148,35 @@ module.exports = {
         we're looking at TypeScript and JavaScript files, but you could also add CSS & HTML files too.</p>
 
     <h4>
-        <code>module.loaders</code>
+        <code>module.rules</code>
     </h4>
     <p>Loaders tell Webpack how & what to do with certain types of file - we have specified a few here to deal with
         Typescript, HTML, CSS and Images:</p>
     <ul class="content">
         <li>ts-loader: transpile Typescript to ES5</li>
         <li>html</li>
-        <li>css: extract and bundle imported CSS into chunked files</li>
+        <li>scss: process and bundle imported SCSS</li>
     </ul>
 
     <h4>
         <code>plugins</code>
     </h4>
     <ul class="content">
-        <li>ExtractTextPlugin: processes and extracts the imported CSS</li>
-        <li>HtmlWebpackPlugin: takes our supplied template index.html and inserts the generates JS & CSS files for us
+        <li>HtmlWebpackPlugin: takes our supplied template index.html and inserts the generates JS file for us
         </li>
     </ul>
 
-    <p>The dev configuration doesn't generate any files - it keeps all bundles in memory, so you won't find any
-        artifacts in the dist directory (from this configuration).</p>
+    <h2>Typescript Configuration</h2>
 
-    <h3 id="webpack-production-configuration">Webpack Production Configuration</h3>
-    <snippet>
-// config/webpack.prod.js
-var webpack = require('webpack');
-var path = require('path');
-
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-    devtool: 'source-map',
-
-    entry: './src/SimpleGrid.ts',
-
-    output: {
-        path: path.resolve('dist'),
-        filename: '[name].[hash].js',
-        chunkFilename: '[id].[hash].chunk.js'
-    },
-
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-
-    module: {
-        loaders: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader'
-            },
-            {
-                test: /\.html$/,
-                loader: 'html-loader'
-            },
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader?sourceMap'})
-            }
-        ]
-    },
-
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({}),
-
-        new ExtractTextPlugin({filename: '[name].css'}),
-
-        new webpack.optimize.CommonsChunkPlugin({
-            name: 'app'
-        }),
-
-        new HtmlWebpackPlugin({
-            template: 'config/index.html'
-        })
-    ]
-};</snippet>
-    <p>We don't use a development server with this configuration - we generate the final artifacts in the dist/ folder
-        and expect this to be deploy to a server.</p>
-    <p>We use the plugins to remove duplicates and minify and extract the CSS into cache busting hash named files.</p>
+    <p>We don't need to specify any Typescript configuration here, but <code>ts-loader</code> does expect a <code>tsconfig.json</code> so
+    we need to create an empty file in the root of the project named <code>tsconfig.json</code>.</p>
 
     <p>With all this in place, we can now add the following npm scripts to our package.json:</p>
 
     <snippet>
 "scripts": {
-    "start": "webpack-dev-server --config config/webpack.dev.js --inline --progress --port 8080",
-    "build": "webpack --config config/webpack.prod.js --progress --profile --bail"
+    "start": "webpack-dev-server --inline --progress --port 8080",
+    "build": "webpack --progress --profile --bail"
 },
    </snippet>
 
@@ -275,7 +186,11 @@ module.exports = {
 
     <p>If we now run our application with the above code we will see this:</p>
 
-    <img src="../images/seed.png" style="width: 100%" alt="Datagrid">
+    <img src="./ts-grid.png" alt="Datagrid">
+
+    <h2>Example Code</h2>
+
+    <p>The code for this example can be found on <a href="https://github.com/seanlandsman/ag-grid-typescript-webpack/">GitHub</a>.</p>
 </div>
 
 <?php include '../documentation-main/documentation_footer.php'; ?>
