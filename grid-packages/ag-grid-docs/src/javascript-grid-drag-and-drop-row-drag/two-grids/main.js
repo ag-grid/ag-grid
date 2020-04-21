c46333c9bde1822b9500295c77d68e20809986bf
Guilherme Lopes Portela <guilherme@gwm-solutions.com>
2020-04-21 09:23:51 -0300

    AG-3635 - rowDrag - drag and drop examples

--- grid-packages/ag-grid-docs/src/javascript-grid-drag-and-drop-row-drag/two-grids/main.js
+++ grid-packages/ag-grid-docs/src/javascript-grid-drag-and-drop-row-drag/two-grids/main.js
@@ -166,9 +166,9 @@ function loadGrid(side) {
 document.addEventListener('DOMContentLoaded', function() {
     var buttons = document.querySelectorAll('button.factory');
 
-    buttons.forEach(function(button) {
-        button.addEventListener('click', onFactoryButtonClick);
-    });
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', onFactoryButtonClick);
+    }
 
     loadGrid('Left');
     loadGrid('Right');
