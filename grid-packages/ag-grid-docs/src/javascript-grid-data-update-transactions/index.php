d213479c276b95fb5d821dd634744363015ce20f
ceolter <0872393174Aa>
2020-04-24 15:21:02 +0100

    AG-4070 refactor of "data update" documentation

--- grid-packages/ag-grid-docs/src/javascript-grid-data-update-transactions/index.php
+++ grid-packages/ag-grid-docs/src/javascript-grid-data-update-transactions/index.php
@@ -1,7 +1,7 @@
 <?php
-$pageTitle = "Updating Data: Core Feature of our Datagrid";
-$pageDescription = "Core feature of ag-Grid supporting Angular, React, Javascript and more. One such feature is Updating Data. Data can be updated in real time. The grid can highlight the change by flashing the cells or by animation inside the cell as the cell refreshes. Version 20 is available for download now, take it for a free two month trial.";
-$pageKeywords = "ag-Grid Insert Remove";
+$pageTitle = "Update transactions";
+$pageDescription = "Transaction Updates allow adding, removing or updating large numbers of rows inside the grid in an efficient manner.";
+$pageKeywords = "ag-Grid Update Transaction";
 $pageGroup = "feature";
 include '../documentation-main/documentation_header.php';
 ?>
