db4aabf574f09fb4ab10058b33b65b2add6ee920
Vitaly Kravchenko <vitalyx@gmail.com>
2020-04-29 15:41:47 +0100

    RTI-122 - IE11 - endsWith is not supported.

--- grid-packages/ag-grid-docs/src/javascript-grid-row-dragging/highlighting-drag-tree-data/main.js
+++ grid-packages/ag-grid-docs/src/javascript-grid-row-dragging/highlighting-drag-tree-data/main.js
@@ -212,6 +212,15 @@ function getFileCellRenderer() {
     return FileCellRenderer;
 }
 
+if (!String.prototype.endsWith) {
+	String.prototype.endsWith = function(search, this_len) {
+		if (this_len === undefined || this_len > this.length) {
+			this_len = this.length;
+		}
+		return this.substring(this_len - search.length, this_len) === search;
+	};
+}
+
 function getFileIcon(filename) {
     return filename.endsWith('.mp3') || filename.endsWith('.wav') ? 'far fa-file-audio'
         : filename.endsWith('.xls') ? 'far fa-file-excel'
