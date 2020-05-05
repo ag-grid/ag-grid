1b0dc04c463e414e55bcd03912a5fedaf342346b
Sean Landsman <sean@thelandsmans.com>
2020-05-05 11:40:43 +0100

    AG-4188 Include angular sub-projects in sanity check tests

--- scripts/validateAgPackageAndDeps.js
+++ scripts/validateAgPackageAndDeps.js
@@ -4,7 +4,7 @@ const getPackageInformation = require("./utils/utils").getPackageInformation;
 
 if (process.argv.length < 6) {
     console.log("Usage: node scripts/sanityCheckPackages.js [Grid Version] [Grid Dependency Version] [Chart Version] [Chart Dependency Version]");
-    console.log("For example: node scripts/sanityCheckPackages.js 23.0.0 ~23.0.0 1.0.0 ~1.0.0");
+    console.log("For example: node scripts/validateAgPackageAndDeps.js 23.0.0 ~23.0.0 1.0.0 ~1.0.0");
     console.log("Note: This script should be run from the root of the monorepo");
     process.exit(1);
 }
@@ -14,16 +14,27 @@ const allPackages = getPackageInformation();
 const packageNames = Object.keys(allPackages);
 packageNames
     .forEach(packageName => {
-    const agPackage = allPackages[packageName];
-    const {projectRoot, isGridPackage, version, agGridDeps, agChartDeps} = agPackage;
+        const agPackage = allPackages[packageName];
+        const {projectRoot, isGridPackage, version, agGridDeps, agChartDeps, agSubAngularVersion, agSubAngularGridDeps, agSubAngularChartDeps} = agPackage;
 
-    function checkDependency(dependencyName, currentVersion, expectedVersion) {
-        if (currentVersion !== expectedVersion) {
-            console.log(`${dependencyName} in ${projectRoot}/package.json has ${currentVersion} but we expect it to be ${expectedVersion}`);
+        function checkDependency(dependencyName, currentVersion, expectedVersion) {
+            if (currentVersion !== expectedVersion) {
+                console.log(`${dependencyName} in ${projectRoot}/package.json has ${currentVersion} but we expect it to be ${expectedVersion}`);
+            }
         }
-    }
 
-    checkDependency(packageName, isGridPackage ? gridNewVersion : chartNewVersion, version);
-    Object.keys(agPackage.agGridDeps).forEach(dependencyName => checkDependency(dependencyName, agGridDeps[dependencyName], gridDependencyVersion));
-    Object.keys(agPackage.agChartDeps).forEach(dependencyName => checkDependency(dependencyName, agChartDeps[dependencyName], chartDependencyVersion));
-});
+        checkDependency(packageName, version, isGridPackage ? gridNewVersion : chartNewVersion);
+        Object.keys(agPackage.agGridDeps).forEach(dependencyName => checkDependency(dependencyName, agGridDeps[dependencyName], gridDependencyVersion));
+        Object.keys(agPackage.agChartDeps).forEach(dependencyName => checkDependency(dependencyName, agChartDeps[dependencyName], chartDependencyVersion));
+
+        if (agSubAngularVersion) {
+            checkDependency(packageName, agSubAngularVersion, isGridPackage ? gridNewVersion : chartNewVersion);
+        }
+
+        if (agSubAngularGridDeps) {
+            Object.keys(agSubAngularGridDeps).forEach(dependencyName => checkDependency(dependencyName, agSubAngularGridDeps[dependencyName], gridDependencyVersion));
+        }
+        if (agSubAngularChartDeps) {
+            Object.keys(agSubAngularChartDeps).forEach(dependencyName => checkDependency(dependencyName, agSubAngularChartDeps[dependencyName], chartDependencyVersion));
+        }
+    });
