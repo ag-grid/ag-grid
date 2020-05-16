4c909f8d060d3d69fd0b965d74e8ba8f12cd93f1
Sean Landsman <sean@thelandsmans.com>
2020-05-16 15:30:39 +0100

    AG-4217 Create CI specific version of build - only build changed & dependent modules/packages

--- scripts/hashDirectory.sh
+++ scripts/hashDirectory.sh
@@ -11,4 +11,4 @@ if [[ `uname -s` == MINGW* ]]; then
   SHASUM='/usr/bin/core_perl/shasum'
 fi
 
-find "$working_directory" -type f \( -path $working_directory/.hash \) -prune -o -type d \( -path $working_directory/cypress/videos -path $working_directory/umd -o -path $working_directory/lib -o -path $working_directory/node_modules -o -path $working_directory/.git -o -path $working_directory/dist -o -path $working_directory/.idea \) -prune -o -type f -print0 | sort -z | xargs -0 $SHASUM | $SHASUM
+find "$working_directory" -type f \( -path $working_directory/.hash \) -prune -o -type d \( -path $working_directory/cypress/videos -o -path $working_directory/umd -o -path $working_directory/lib -o -path $working_directory/node_modules -o -path $working_directory/.git -o -path $working_directory/dist -o -path $working_directory/.idea \) -prune -o -type f -print0 | sort -z | xargs -0 $SHASUM | $SHASUM
