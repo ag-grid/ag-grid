7b1875bd2de99b24b9a750a5287c18fe15459a52
Guilherme Lopes <guilherme@gwm-solutions.com>
2020-06-04 11:36:31 -0300

    RTI-156 - fixed redux example on IE11

--- grid-packages/ag-grid-docs/src/react-redux-integration-pt2/redux-file-browser/FileCellRenderer.jsx
+++ grid-packages/ag-grid-docs/src/react-redux-integration-pt2/redux-file-browser/FileCellRenderer.jsx
@@ -11,9 +11,12 @@ export default class FileCellRenderer extends Component {
   }
 
   getFileIcon = (filename) => {
-    return filename.endsWith('.mp3') || filename.endsWith('.wav') ? 'far fa-file-audio' :
-      filename.endsWith('.xls') ? 'far fa-file-excel' :
-        filename.endsWith('.txt') ? 'far fa-file' :
-          filename.endsWith('.pdf') ? 'far fa-file-pdf' : 'far fa-folder';
+    const extension = filename.substr(-4);
+    const isAudio = extension === '.mp3' || extension === '.wav';
+
+    return isAudio ? 'far fa-file-audio' :
+      extension === '.xls' ? 'far fa-file-excel' :
+        extension === '.txt' ? 'far fa-file' :
+          extension === '.pdf' ? 'far fa-file-pdf' : 'far fa-folder';
   }
 }
