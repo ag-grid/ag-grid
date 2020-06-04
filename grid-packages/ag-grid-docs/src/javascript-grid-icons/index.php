e9d8c18c4676f1e0a3c6e9b6f1d2442e6bbffe66
Adrian Lowdon <adrian@acceleratech.uk>
2020-06-04 16:57:42 +0100

    AG-3390 - updated documentation

--- grid-packages/ag-grid-docs/src/javascript-grid-icons/index.php
+++ grid-packages/ag-grid-docs/src/javascript-grid-icons/index.php
@@ -8,37 +8,44 @@ include '../documentation-main/documentation_header.php';
 
 <h1>Icons</h1>
 
-    <p class="lead">
-        This sections details how to provide your own icons for the grid and style grid icons for your application requirements.
-    </p>
+<p class="lead">
+    This sections details how to provide your own icons for the grid and style grid icons for your application requirements.
+</p>
 
-<h2>Change individual icons using CSS</h2>
+<h2>Change Individual Icons Using CSS</h2>
 
-<p>You can change individual icons by overriding the background images for the respective CSS selector.
-The following code snippet overrides the Balham theme pin icon used in the drag hint when reordering columns:<p>
+<p>
+    You can change individual icons by overriding the background images for the respective CSS selector.
+    The following code snippet overrides the Alpine theme pin icon used in the drag hint when reordering columns:
+</p>
 
-<snippet>
+<?= createSnippet(<<<SNIPPET
 /*
- * The override should be placed after the import of the theme.
- * Alternatively, you can aso increase the selector's specificity.
- */
-.ag-theme-balham .ag-icon-pin {
-    font-family: "Font Awesome 5 Free";
-    /* FontAwesome uses font-weight bold */
-    font-weight: bold;
+* The override should be placed after the import of the theme.
+* Alternatively, you can also increase the selector's specificity.
+*/
+.ag-theme-alpine .ag-icon-pin {
+   font-family: "Font Awesome 5 Free";
+   /* FontAwesome uses font-weight bold */
+   font-weight: bold;
 }
-.ag-theme-balham .ag-icon-pin::before {
-    content: '\f08d';
+
+.ag-theme-alpine .ag-icon-pin::before {
+   content: '\\f08d';
 }
-</snippet>
+SNIPPET
+, 'css') ?>
 
-<h2>Replace the icons by changing the icon font</h2>
+<h2>Replace the Icons by Changing the Icon Font</h2>
 
-<p>If you are using a <a href="/javascript-grid-styling/">custom theme</a> in your project, you can include use theme parameters to change the icon font.
-    We <a href="https://github.com/ag-grid/ag-grid-customise-theme/tree/master/src/vanilla">provide an example</a>
+<p>
+    If you are using a <a href="/javascript-grid-styling/">custom theme</a> in your project, you can use theme
+    parameters to change the icon font. We
+    <a href="https://github.com/ag-grid/ag-grid-customise-theme/tree/master/src/vanilla">provide an example</a>
     that does this, and the relevant code looks like this:
+</p>
 
-    <snippet>
+<?= createSnippet(<<<SNIPPET
 @import "~ag-grid-community/src/styles/ag-grid.scss";
 @import "~ag-grid-community/src/styles/ag-theme-alpine-mixin.scss";
 
@@ -49,49 +56,49 @@ The following code snippet overrides the Balham theme pin icon used in the drag 
         // define icon map - not required when changing between two
         // provided theme fonts, see next code sample for more details
         "icons-font-codes": (
-            "aggregation": "\f247",
-            "arrows": "\f0b2",
-            "asc": "\f062",
-            "cancel": "\f057",
-            "chart": "\f080",
-            "color-picker": "\f576",
-            "columns": "\f0db",
-            "contracted": "\f146",
-            "copy": "\f0c5",
-            "cross": "\f00d",
-            "desc": "\f063",
-            "expanded": "\f0fe",
-            "eye-slash": "\f070",
-            "eye": "\f06e",
-            "filter": "\f0b0",
-            "first": "\f100",
-            "grip": "\f58e",
-            "group": "\f5fd",
-            "last": "\f101",
-            "left": "\f060",
-            "linked": "\f0c1",
-            "loading": "\f110",
-            "maximize": "\f2d0",
-            "menu": "\f0c9",
-            "minimize": "\f2d1",
-            "next": "\f105",
-            "none": "\f338",
-            "not-allowed": "\f05e",
-            "paste": "\f0ea",
-            "pin": "\f276",
-            "pivot": "\f074",
-            "previous": "\f104",
-            "right": "\f061",
-            "save": "\f0c7",
-            "small-down": "\f107",
-            "small-left": "\f104",
-            "small-right": "\f105",
-            "small-up": "\f106",
-            "tick": "\f00c",
-            "tree-closed": "\f105",
-            "tree-indeterminate": "\f068",
-            "tree-open": "\f107",
-            "unlinked": "\f127",
+            "aggregation": "\\f247",
+            "arrows": "\\f0b2",
+            "asc": "\\f062",
+            "cancel": "\\f057",
+            "chart": "\\f080",
+            "color-picker": "\\f576",
+            "columns": "\\f0db",
+            "contracted": "\\f146",
+            "copy": "\\f0c5",
+            "cross": "\\f00d",
+            "desc": "\\f063",
+            "expanded": "\\f0fe",
+            "eye-slash": "\\f070",
+            "eye": "\\f06e",
+            "filter": "\\f0b0",
+            "first": "\\f100",
+            "grip": "\\f58e",
+            "group": "\\f5fd",
+            "last": "\\f101",
+            "left": "\\f060",
+            "linked": "\\f0c1",
+            "loading": "\\f110",
+            "maximize": "\\f2d0",
+            "menu": "\\f0c9",
+            "minimize": "\\f2d1",
+            "next": "\\f105",
+            "none": "\\f338",
+            "not-allowed": "\\f05e",
+            "paste": "\\f0ea",
+            "pin": "\\f276",
+            "pivot": "\\f074",
+            "previous": "\\f104",
+            "right": "\\f061",
+            "save": "\\f0c7",
+            "small-down": "\\f107",
+            "small-left": "\\f104",
+            "small-right": "\\f105",
+            "small-up": "\\f106",
+            "tick": "\\f00c",
+            "tree-closed": "\\f105",
+            "tree-indeterminate": "\\f068",
+            "tree-open": "\\f107",
+            "unlinked": "\\f127",
         )
     ));
 
@@ -100,12 +107,15 @@ The following code snippet overrides the Balham theme pin icon used in the drag 
         font-weight: bold;
     }
 }
+SNIPPET
+, 'scss') ?>
 
-</snippet>
+<p>
+    If you are swapping one theme's icon set for another, you do not need to define an icon map because all theme fonts
+    use the same map. This example shows the use of Alpine with the Material font:
+</p>
 
-<p>If you are swapping one theme's icon set for another, you do not need to define an icon map because all theme fonts use the same map. This example shows the use of Alpine with the Material font:</p>
-
-<snippet>
+<?= createSnippet(<<<SNIPPET
 @import "~ag-grid-community/src/styles/ag-grid.scss";
 @import "~ag-grid-community/src/styles/ag-theme-alpine-mixin.scss";
 
@@ -118,12 +128,17 @@ The following code snippet overrides the Balham theme pin icon used in the drag 
         "icons-data": null, // prevent default Alpine font from being embedded
     ));
 }
+SNIPPET
+, 'scss') ?>
 
 </snippet>
 
-<p>A working project with Sass / Webpack set up to customise an icon set is available in the <a href="https://github.com/ag-grid/ag-grid-customise-theme">ag grid customising theme repository</a>.
+<p>
+    A working project with Sass / Webpack set up to customise an icon set is available in the
+    <a href="https://github.com/ag-grid/ag-grid-customise-theme">ag grid customising theme repository</a>.
+</p>
 
-<h2>Set the icons through <code>gridOptions</code> (JavaScript)</h2>
+<h2>Set the Icons Through <code>gridOptions</code> (JavaScript)</h2>
 
 <p>
     The icons can either be set on the grid options (all icons) or on the column definition (all except group).
@@ -138,7 +153,6 @@ The following code snippet overrides the Balham theme pin icon used in the drag 
 </p>
 
 <snippet>
-
 // header column group shown when expanded (click to contract)
 columnGroupOpened
 // header column group shown when contracted (click to expand)
@@ -233,7 +247,7 @@ rowDrag
 save
 // version of small-right used in RTL mode
 smallLeft
-// separater between column 'pills' when you add multiple columns to the header drop zone
+// separator between column 'pills' when you add multiple columns to the header drop zone
 smallRight
 // show on column header when column is sorted ascending
 sortAscending
@@ -250,18 +264,24 @@ sortUnSort
 <p>
     The icon can be any of the following:
 </p>
-    <ul class="content">
-        <li>
-            <b>String:</b> The string will be treated as html. Use to return just text, or HTML tags.
-        </li>
-        <li>
-            <b>Function:</b> A function that returns either a String or a DOM node or element.
-        </li>
-    </ul>
 
-<h2>Changing checkbox and radio button icons</h2>
+<ul class="content">
+    <li>
+        <b>String:</b> The string will be treated as html. Use to return just text, or HTML tags.
+    </li>
+    <li>
+        <b>Function:</b> A function that returns either a String or a DOM node or element.
+    </li>
+</ul>
 
-<p>As of version 23, checkboxes and radio buttons are native browser inputs styled using CSS. This means that you can change the appearance of the checkbox with Sass, but not using the JavaScript <code>gridOptions</code> technique. Using Sass, you can either change the icon font (set the <code>checkbox-*</code> and <code>radio-button-*</code> entries in the icon font codes map) or add CSS rules to override the appearance of the checkbox.</p>
+<h2>Changing Checkbox and Radio Button Icons</h2>
+
+<p>
+    As of version 23, checkboxes and radio buttons are native browser inputs styled using CSS. This means that you can
+    change the appearance of the checkbox with Sass, but not using the JavaScript <code>gridOptions</code> technique.
+    Using Sass, you can either change the icon font (set the <code>checkbox-*</code> and <code>radio-button-*</code>
+    entries in the icon font codes map) or add CSS rules to override the appearance of the checkbox.
+</p>
 
 <h2>Example</h2>
 
@@ -276,20 +296,19 @@ sortUnSort
 
 <p>
     When you create your own theme as described in <a href="../javascript-grid-themes-customising/">Customising Themes</a>,
-    you are also able to replace the WebFont with SVG Icons.
-
-    To do that you will need to override the <code>ag-icon</code> SASS rules and also the rules for each icon.
-    You can see the example <code>styles.scss</code> file in our custom theme with SVG icons example here:
+    you are also able to replace the WebFont with SVG Icons. To do that you will need to override th
+     <code>ag-icon</code> SASS rules and also the rules for each icon. You can see the example <code>styles.scss</code>
+     file in our custom theme with SVG icons example here:
     <a href="https://github.com/ag-grid/ag-grid-customise-theme/tree/master/src/vanilla-svg-icons">SVG Icons Example</a>.
 </p>
 
 <note>
-    <p>
-        The grid uses the CSS <code>color</code> property to change the color of icons. This works for webfont-based icons, but not for SVG. If you are using SVG for icons, you should ensure that the provided SVG image is already the correct color.
-    </p>
+    The grid uses the CSS <code>color</code> property to change the colour of icons. This works for webfont-based icons,
+    but not for SVG. If you are using SVG for icons, you should ensure that the provided SVG image is already the
+    correct colour.
 </note>
 
-<h2>Provided theme icons</h2>
+<h2>Provided Theme Icons</h2>
 
 <p>
     Below you can see a list with all available icons for each theme, their names, and download them.
@@ -393,36 +412,37 @@ sortUnSort
         });
     });
 </script>
-    <ul class="nav nav-tabs bg-primary pl-2 pt-2" id="icon-tabpanel" role="tablist">
-        <li class="nav-item mr-2">
-            <a class="nav-link active" id="alpine-tab" data-toggle="tab" href="#alpine" role="tab" aria-controls="alpine" aria-selected="true">Alpine Icons</a>
-        </li>
-        <li class="nav-item mr-2">
-            <a class="nav-link" id="balham-tab" data-toggle="tab" href="#balham" role="tab" aria-controls="balham" aria-selected="true">Balham Icons</a>
-        </li>
-        <li class="nav-item mr-2">
-            <a class="nav-link" id="material-tab" data-toggle="tab" href="#material" role="tab" aria-controls="material" aria-selected="false">Material Icons</a>
-        </li>
-        <li class="nav-item mr-2">
-            <a class="nav-link" id="base-tab" data-toggle="tab" href="#base" role="tab" aria-controls="base" aria-selected="false">Base Icons</a>
-        </li>
-    </ul>
-    <div class="tab-content border border-top-0" id="icon-content" style="max-height: 34rem; overflow: hidden;">
-        <div class="tab-pane show active container px-0" id="alpine" role="tabpanel" aria-labelledby="alpine-tab" style="max-height: 34rem;position: relative;">
-            <div class="download bg-primary p-2" style="bottom: 0; left: 0;"><a href="./resources/alpine/alpine-icons.zip">Download All</a></div>
-        </div>
-        <div class="tab-pane show active container px-0" id="balham" role="tabpanel" aria-labelledby="balham-tab" style="max-height: 34rem;position: relative;">
-            <div class="download bg-primary p-2" style="bottom: 0; left: 0;"><a href="./resources/balham/balham-icons.zip">Download All</a></div>
-        </div>
-        <div class="tab-pane show active container px-0" id="balham" role="tabpanel" aria-labelledby="balham-tab" style="max-height: 34rem;position: relative;">
-            <div class="download bg-primary p-2" style="bottom: 0; left: 0;"><a href="./resources/balham/balham-icons.zip">Download All</a></div>
-        </div>
-        <div class="tab-pane container px-0" id="material" role="tabpanel" aria-labelledby="material-tab" style="max-height: 34rem;">
-            <div class="download bg-primary p-2"><a href="./resources/material/material-icons.zip">Download All</a></div>
-        </div>
-        <div class="tab-pane container px-0" id="base" role="tabpanel" aria-labelledby="base-tab" style="max-height: 34rem;">
-            <div class="download bg-primary p-2"><a href="./resources/base/base-icons.zip">Download All</a></div>
-        </div>
+
+<ul class="nav nav-tabs bg-primary pl-2 pt-2" id="icon-tabpanel" role="tablist">
+    <li class="nav-item mr-2">
+        <a class="nav-link active" id="alpine-tab" data-toggle="tab" href="#alpine" role="tab" aria-controls="alpine" aria-selected="true">Alpine Icons</a>
+    </li>
+    <li class="nav-item mr-2">
+        <a class="nav-link" id="balham-tab" data-toggle="tab" href="#balham" role="tab" aria-controls="balham" aria-selected="true">Balham Icons</a>
+    </li>
+    <li class="nav-item mr-2">
+        <a class="nav-link" id="material-tab" data-toggle="tab" href="#material" role="tab" aria-controls="material" aria-selected="false">Material Icons</a>
+    </li>
+    <li class="nav-item mr-2">
+        <a class="nav-link" id="base-tab" data-toggle="tab" href="#base" role="tab" aria-controls="base" aria-selected="false">Base Icons</a>
+    </li>
+</ul>
+<div class="tab-content border border-top-0" id="icon-content" style="max-height: 34rem; overflow: hidden;">
+    <div class="tab-pane show active container px-0" id="alpine" role="tabpanel" aria-labelledby="alpine-tab" style="max-height: 34rem;position: relative;">
+        <div class="download bg-primary p-2" style="bottom: 0; left: 0;"><a href="./resources/alpine/alpine-icons.zip">Download All</a></div>
     </div>
+    <div class="tab-pane show active container px-0" id="balham" role="tabpanel" aria-labelledby="balham-tab" style="max-height: 34rem;position: relative;">
+        <div class="download bg-primary p-2" style="bottom: 0; left: 0;"><a href="./resources/balham/balham-icons.zip">Download All</a></div>
+    </div>
+    <div class="tab-pane show active container px-0" id="balham" role="tabpanel" aria-labelledby="balham-tab" style="max-height: 34rem;position: relative;">
+        <div class="download bg-primary p-2" style="bottom: 0; left: 0;"><a href="./resources/balham/balham-icons.zip">Download All</a></div>
+    </div>
+    <div class="tab-pane container px-0" id="material" role="tabpanel" aria-labelledby="material-tab" style="max-height: 34rem;">
+        <div class="download bg-primary p-2"><a href="./resources/material/material-icons.zip">Download All</a></div>
+    </div>
+    <div class="tab-pane container px-0" id="base" role="tabpanel" aria-labelledby="base-tab" style="max-height: 34rem;">
+        <div class="download bg-primary p-2"><a href="./resources/base/base-icons.zip">Download All</a></div>
+    </div>
+</div>
 
 <?php include '../documentation-main/documentation_footer.php';?>
