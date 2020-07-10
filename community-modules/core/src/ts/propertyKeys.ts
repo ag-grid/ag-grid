af447343eef9e78773d144f9a8bc4fd3e0e731d1
ceolter <0872393174Aa>
2020-07-10 17:16:23 +0100

    AG-4291 Reactive Columns

--- community-modules/core/src/ts/propertyKeys.ts
+++ community-modules/core/src/ts/propertyKeys.ts
@@ -58,7 +58,7 @@ export class PropertyKeys {
         'enableCellTextSelection', 'suppressBrowserResizeObserver', 'suppressMaxRenderedRowRestriction',
         'excludeChildrenWhenTreeDataFiltering', 'tooltipMouseTrack', 'keepDetailRows', 'paginateChildRows', 'preventDefaultOnContextMenu',
         'undoRedoCellEditing', 'allowDragFromColumnsToolPanel', 'immutableData', 'immutableColumns', 'pivotSuppressAutoColumn',
-        'columnsSpike', 'suppressExpandablePivotGroups'
+        'suppressExpandablePivotGroups'
     ];
 
     /** You do not need to include event callbacks in this list, as they are generated automatically. */
