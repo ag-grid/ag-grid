f333cea22d2a80f5e2fae2528a0d5a4a52d8266a
ceolter <0872393174Aa>
2020-07-13 12:57:36 +0100

    AG-4291 Reactive Columns

---
+++ grid-packages/ag-grid-docs/src/javascript-grid-column-state/add-remove-columns/main.js
@@ -0,0 +1,47 @@
+var colDefsMedalsIncluded = [
+    {field: 'athlete'},
+    {field: 'age'},
+    {field: 'country'},
+    {field: 'year'},
+    {field: 'date'},
+    {field: 'gold'},
+    {field: 'silver'},
+    {field: 'bronze'},
+    {field: 'total'}
+];
+
+var colDefsMedalsExcluded = [
+    {field: 'athlete'},
+    {field: 'age'},
+    {field: 'country'},
+    {field: 'year'},
+    {field: 'date'}
+];
+
+var gridOptions = {
+    defaultColDef: {
+        defaultWidth: 100,
+        sortable: true,
+        resizable: true
+    },
+    columnDefs: colDefsMedalsIncluded
+};
+
+function onBtExcludeMedalColumns() {
+    gridOptions.api.setColumnDefs(colDefsMedalsExcluded);
+}
+
+function onBtIncludeMedalColumns() {
+    gridOptions.api.setColumnDefs(colDefsMedalsIncluded);
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function() {
+    var gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
+
+    agGrid.simpleHttpRequest({ url: 'https://raw.githubusercontent.com/ag-grid/ag-grid/master/grid-packages/ag-grid-docs/src/olympicWinnersSmall.json' })
+        .then(function(data) {
+            gridOptions.api.setRowData(data);
+        });
+});
