75e5ca211b77d192e06e68f55b3b4b72d0104e12
Sean Landsman <sean@thelandsmans.com>
2020-07-22 10:10:03 +0100

    AG-4380 Allow the ag-Grid angular component to be wrapped

--- grid-packages/ag-grid-angular/projects/ag-grid-angular/src/public-api.ts
+++ grid-packages/ag-grid-angular/projects/ag-grid-angular/src/public-api.ts
@@ -2,3 +2,5 @@ export * from './lib/ag-grid-column.component';
 export * from './lib/ag-grid-angular.component';
 export * from './lib/ag-grid-angular.module';
 export * from './lib/interfaces';
+export * from './lib/angularFrameworkComponentWrapper'
+export * from './lib/angularFrameworkOverrides'
