73f7204cbb1297798102c1988f7b56bafc70d822
ceolter <0872393174Aa>
2020-07-28 13:38:07 +0100

    AG-4291 Reactive Columns

--- community-modules/core/src/ts/columnController/columnFactory.ts
+++ community-modules/core/src/ts/columnController/columnFactory.ts
@@ -289,7 +289,7 @@ export class ColumnFactory extends BeanStub {
         }
 
         // width - we only set width if column is not flexing
-        const noFlexThisCol = column.getFlex() != null && column.getFlex() <= 0;
+        const noFlexThisCol = column.getFlex() <= 0;
         if (noFlexThisCol) {
             // both null and undefined means we skip, as it's not possible to 'clear' width (a column must have a width)
             const width = attrToNumber(colDef.width);
