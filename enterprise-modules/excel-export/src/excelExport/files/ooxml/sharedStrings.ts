271ed023f4e0cd8e989a33f8f5d7eb8f0c0b0517
Guilherme Lopes <guilherme@gwm-solutions.com>
2020-08-26 16:20:05 +0100

    AG-3923 - added instruction to Excel to keep leading / trailing spaces

--- enterprise-modules/excel-export/src/excelExport/files/ooxml/sharedStrings.ts
+++ enterprise-modules/excel-export/src/excelExport/files/ooxml/sharedStrings.ts
@@ -1,12 +1,28 @@
 import { ExcelOOXMLTemplate, XmlElement, _ } from '@ag-grid-community/core';
 
-const buildSharedString = (textNode: string): XmlElement => ({
-    name: 'si',
-    children: [{
+const buildSharedString = (textNode: string): XmlElement => {
+    textNode = textNode.toString();
+ 
+    const child: XmlElement = {
         name: 't',
-        textNode: _.utf8_encode(_.escapeString(textNode.toString()) as any)
-    }]
-});
+        textNode: _.utf8_encode(_.escapeString(textNode))
+    }
+    // if we have leading or trailing spaces, instruct Excel not to trim them
+    const preserveSpaces = textNode.replace(/^\s*|\s*$/g, '').length !== textNode.length;
+    
+    if (preserveSpaces) {
+        child.properties = {
+            rawMap: {
+                "xml:space": "preserve"
+            }
+        }
+    }
+
+    return {
+        name: 'si',
+        children: [child]
+    }
+};
 
 const sharedStrings: ExcelOOXMLTemplate = {
     getTemplate(strings: string[]) {
