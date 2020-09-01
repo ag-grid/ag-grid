482f4aa85a71666c340144d9e94bdeda24f7e6b1
robertclarke <rmc.software.consultancy@gmail.com>
2020-09-01 15:41:24 +0100

    AG-2821 - added palettes to customisation examples.

--- grid-packages/ag-grid-docs/src/javascript-grid-charts-integrated-customisation-area/custom-area-chart-theme/main.js
+++ grid-packages/ag-grid-docs/src/javascript-grid-charts-integrated-customisation-area/custom-area-chart-theme/main.js
@@ -1,32 +1,14 @@
-var columnDefs = [
-    { field: "country", width: 150, chartDataType: 'category' },
-    { field: "gold", chartDataType: 'series' },
-    { field: "silver", chartDataType: 'series' },
-    { field: "bronze", chartDataType: 'series' },
-    { headerName: "A", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' },
-    { headerName: "B", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' },
-    { headerName: "C", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' },
-    { headerName: "D", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' }
-];
-
-function createRowData() {
-    var countries = [
-        "Ireland", "Spain", "United Kingdom", "France", "Germany", "Luxembourg", "Sweden",
-        "Norway", "Italy", "Greece", "Iceland", "Portugal", "Malta", "Brazil", "Argentina",
-        "Colombia", "Peru", "Venezuela", "Uruguay", "Belgium"
-    ];
-
-    return countries.map(function(country, index) {
-        return {
-            country: country,
-            gold: Math.floor(((index + 1 / 7) * 333) % 100),
-            silver: Math.floor(((index + 1 / 3) * 555) % 100),
-            bronze: Math.floor(((index + 1 / 7.3) * 777) % 100),
-        };
-    });
-}
-
 var gridOptions = {
+    columnDefs: [
+        { field: "country", width: 150, chartDataType: 'category' },
+        { field: "gold", chartDataType: 'series' },
+        { field: "silver", chartDataType: 'series' },
+        { field: "bronze", chartDataType: 'series' },
+        { headerName: "A", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' },
+        { headerName: "B", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' },
+        { headerName: "C", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' },
+        { headerName: "D", valueGetter: 'Math.floor(Math.random()*1000)', chartDataType: 'series' }
+    ],
     defaultColDef: {
         editable: true,
         sortable: true,
@@ -36,13 +18,16 @@ var gridOptions = {
         resizable: true
     },
     popupParent: document.body,
-    columnDefs: columnDefs,
     rowData: createRowData(),
     enableRangeSelection: true,
     enableCharts: true,
     onFirstDataRendered: onFirstDataRendered,
     customChartThemes: {
         myCustomTheme: {
+            palette: {
+                fills: ['#e1ba00', 'silver', 'peru'],
+                strokes: ['black', '#ff0000']
+            },
             defaults: {
                 area: {
                     series: {
@@ -90,6 +75,23 @@ function onFirstDataRendered(params) {
     };
 
     params.api.createRangeChart(createRangeChartParams);
+}
+
+function createRowData() {
+    var countries = [
+        "Ireland", "Spain", "United Kingdom", "France", "Germany", "Luxembourg", "Sweden",
+        "Norway", "Italy", "Greece", "Iceland", "Portugal", "Malta", "Brazil", "Argentina",
+        "Colombia", "Peru", "Venezuela", "Uruguay", "Belgium"
+    ];
+
+    return countries.map(function(country, index) {
+        return {
+            country: country,
+            gold: Math.floor(((index + 1 / 7) * 333) % 100),
+            silver: Math.floor(((index + 1 / 3) * 555) % 100),
+            bronze: Math.floor(((index + 1 / 7.3) * 777) % 100),
+        };
+    });
 }
 
 // setup the grid after the page has finished loading
