56fc0346f8b74b6f2906d63a552dbf5a3ee80cb6
Sean Landsman <sean@thelandsmans.com>
2020-09-03 10:56:18 +0100

    AG-4549 Update react docs to better support functional/hook react examples

--- grid-packages/ag-grid-docs/src/react-fine-tuning/index.php
+++ grid-packages/ag-grid-docs/src/react-fine-tuning/index.php
@@ -7,36 +7,132 @@ include '../documentation-main/documentation_header.php';
 ?>
 
 <div>
-<h1 id="react-grid-api">More Control of ag-Grid with React</h1>
+    <h1 id="react-fine-tuning">Fine Tuning</h1>
 
-<h2 id="grid-api">Access the Grid & Column API</h2>
-<p>
-    When the grid is initialised, it will fire the <code>gridReady</code> event. If you want to
-    use the API of the grid, you should put an <code>onGridReady(params)</code> callback onto
-    the grid and grab the api from the params. You can then call this api at a later
-    stage to interact with the grid (on top of the interaction that can be done by
-setting and changing the props).</p>
-<snippet language="jsx">
+    <p class="lead">This section describes some of the finer grain tuning you might want to do with your React & ag-Grid application.</p>
+
+    <h2>Styling React Components in ag-Grid</h2>
+    <p>By default user supplied React components will be wrapped in a <code>div</code> but it is possible to have your component
+        wrapped in a container of your choice (i.e. a <code>span</code> etc), perhaps to override/control a third party component.</p>
+
+    <p>For example, assuming a user component as follows:</p>
+
+    <snippet language="jsx">
+const HelloWorldRenderer = () => &lt;span&gt;Hello World&lt;/span&gt;;
+    </snippet>
+
+    <p>The default behaviour will render the following within the grid:</p>
+
+    <snippet language="html">
+        &lt;div class="ag-react-container"&gt;&lt;span&gt;Hello World&lt;/span&gt;&lt;/div&gt;
+    </snippet>
+
+    <p>In order to override this default behaviour and can specify a <code>componentWrappingElement</code>:</p>
+
+    <snippet lanuage="jsx">
+&lt;AgGridReact
+    onGridReady=<span ng-non-bindable>{</span>this.onGridReady}
+    rowData=<span ng-non-bindable>{</span>this.state.rowData}
+    componentWrappingElement='span'&gt;
+&lt;/AgGridReact&gt;
+    </snippet>
+
+    <p>Doing this would result in the following being rendered:</p>
+    <snippet language="html">
+        &lt;span class="ag-react-container"&gt;&lt;span&gt;Hello World&lt;/span&gt;&lt;/span&gt;
+    </snippet>
+
+    <p>If you wish to override the style of the grid container you can either provide an implementation of the <code>ag-react-container</code> class, or
+        via the <code>getReactContainerStyle</code> or <code>getReactContainerClasses</code> callbacks on your React component.</p>
+
+    <h3>Styling a React Component:</h3>
+
+    <snippet>
+export default class CustomTooltip extends Component {
+    getReactContainerClasses() {
+        return ['custom-tooltip'];
+    }
+
+    getReactContainerStyle() {
+        return {
+            display: 'inline-block',
+            height: '100%'
+        };
+    }
+
+    ...rest of the component
+    </snippet>
+
+    <h3>Styling a React Hook:</h3>
+
+    <snippet>
+export default forwardRef(({ parentFilterInstance }, ref) => {
+    useImperativeHandle(ref, () => ({
+        getReactContainerClasses() {
+            return ['custom-tooltip'];
+        },
+        getReactContainerStyle() {
+            return {
+                display: 'inline-block',
+                height: '100%'
+        };
+    }));
+
+    ...rest of the hook
+});
+    </snippet>
+    
+    <p>See <a href="../react-hooks/">Hooks</a> for more information in using Hooks within the grid.</p>
+
+    <p>In both cases (component or hook) the following would be rendered:</p>
+
+    <snippet language="html">
+&lt;div class="ag-react-container custom-tooltip" style="display: inline-block; height: 100%" &gt;
+    &lt;span&gt;Hello World&lt;/span&gt;
+&lt;/div&gt;
+    </snippet>
+
+    <h2 id="grid-api">Access the Grid & Column API</h2>
+    <p>
+        When the grid is initialised, it will fire the <code>gridReady</code> event. If you want to
+        use the API of the grid, you should put an <code>onGridReady(params)</code> callback onto
+        the grid and grab the api from the params. You can then call this api at a later
+        stage to interact with the grid (on top of the interaction that can be done by
+    setting and changing the props).</p>
+    <snippet language="jsx">
 // provide gridReady callback to the grid
 &lt;AgGridReact
-    onGridReady={this.onGridReady}
-    .../&gt;
+    onGridReady={onGridReady}
+    ...
+/&gt;
 
 // in onGridReady, store the api for later use
 onGridReady = (params) => {
-    this.api = params.api;
-    this.columnApi = params.columnApi;
+    // using hooks - setGridApi/setColumnApi are returned by useState
+    setGridApi(params.api);
+    setColumnApi(params.columnApi);
+
+    // or setState if using components
+    this.setState({
+        gridApi: params.api,
+        columnApi: params.columnApi
+    });
 }
 
 // use the api some point later!
 somePointLater() {
-    this.api.selectAll();
-    this.columnApi.setColumnVisible('country', visible);
+    // hooks
+    gridApi.selectAll();
+    columnApi.setColumnVisible('country', visible);
+
+    // components
+    this.state.gridApi.selectAll();
+    this.state.columnApi.setColumnVisible('country', visible);
 }</snippet>
 <p>
-    The <code>api</code> and <code>columnApi</code> are also stored inside the React backing object
-    of the grid. So you can also look up the backing object via React and access the
-    <code>api</code> and <code>columnApi</code> that way.
+    The <code>api</code> and <code>columnApi</code> are also stored inside the <code>AgGridReact</code> component, so you
+    can also look up the backing object via React and access the <code>api</code> and <code>columnApi</code> that way if
+    you'd prefer.
 </p>
 
     <h2 id="react-cell-rendering">Cell Component Rendering</h2>
