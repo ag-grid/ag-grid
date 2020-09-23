301fc2f4c31ab6bf77164081075efd72225752e2
ceolter <0872393174Aa>
2020-09-23 15:47:39 +0100

    AG-4241 [Frameworks] Memory not freed if api(s) not dereferenced

--- community-modules/core/src/ts/utils/object.ts
+++ community-modules/core/src/ts/utils/object.ts
@@ -213,7 +213,6 @@ export function getValueUsingField(data: any, field: string, fieldContainsDots: 
 // used by ColumnAPI and GridAPI to remove all references, so keeping grid in memory resulting in a
 // memory leak if user is not disposing of the GridAPI or ColumnApi references
 export function removeAllReferences(obj: any, objectName: string): void {
-/*
     Object.keys(obj).forEach( key => {
         const value = obj[key];
         // leave all basic types - this is needed for GridAPI to leave the "destroyed: boolean" attribute alone
@@ -222,19 +221,23 @@ export function removeAllReferences(obj: any, objectName: string): void {
         }
     });
     const proto = Object.getPrototypeOf(obj);
+    const properties: any = {};
+
     Object.keys(proto).forEach( key => {
         const value = proto[key];
         // leave all basic types - this is needed for GridAPI to leave the "destroyed: boolean" attribute alone
         if (typeof value === 'function') {
-            proto[key] = ()=> {
+            const func = ()=> {
                 console.warn(`ag-Grid: ${objectName} function ${key}() cannot be called as the grid has been 
 destroyed. Please don't call grid API functions on destroyed grids - as a matter of fact you 
 shouldn't be keeping the API reference, your application has a memory leak! Remove the API reference 
 when the grid is destroyed.`);
             };
+            properties[key] = {value: func, writable: true};
         }
     });
-*/
+
+    Object.defineProperties(obj, properties);
 }
 
 export function isNonNullObject(value: any): boolean {
