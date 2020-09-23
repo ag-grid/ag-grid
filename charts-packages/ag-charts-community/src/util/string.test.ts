d20a7a37cc005eb6f76aa383f407a9e5110d4f80
Vitaly Kravchenko <vitalyx@gmail.com>
2020-09-23 16:38:59 +0100

    String interpolation with number/date formatting support.

---
+++ charts-packages/ag-charts-community/src/util/string.test.ts
@@ -0,0 +1,53 @@
+import { interpolate } from "./string";
+
+describe('interpolate', () => {
+    it('should substitute #{key} with values from the given object', () => {
+        const name = 'first name';
+        const result = interpolate(`My ${name} is #{name} and I live in #{place}`, {
+            name: 'Vitaly',
+            place: 'London'
+        });
+        expect(result).toBe('My first name is Vitaly and I live in London');
+    });
+
+    it('should strip #{key} if the key is not in the given object', () => {
+        const result = interpolate('#{something} #{this} #{stuff} should be gone', {
+            something: 'Ebola'
+        });
+        expect(result).toBe('Ebola   should be gone');
+    });
+
+    it('should convert numbers and objects with toString method to strings', () => {
+        const result = interpolate('#{caption}: #{value}', {
+            caption: {
+                first: 'My',
+                second: 'favorite',
+                third: 'number',
+                toString: function () {
+                    return this.first + ' ' + this.second + ' ' + this.third;
+                }
+            },
+            value: 42
+        });
+        expect(result).toBe('My favorite number: 42');
+    });
+
+    it('should format numbers (using Intl.NumberFormat) and dates', () => {
+        const result1 = interpolate('I drank #{drink1:liters} of beer and #{drink2:liters} of vodka on #{day:date}', {
+            drink1: 42000000,
+            drink2: 1234,
+            day: new Date('Wed Sep 23 2020')
+        }, {
+            liters: {
+                locales: 'en-GB',
+                options: {
+                    style: 'unit',
+                    unit: 'liter',
+                    unitDisplay: 'long'
+                }
+            },
+            date: '%A, %b %d %Y'
+        });
+        expect(result1).toBe('I drank 42,000,000 litres of beer and 1,234 litres of vodka on Wednesday, Sep 23 2020');
+    });
+});
