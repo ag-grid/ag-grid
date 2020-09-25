c8b693721eee21bbcdc5e31f58608fa0fa8f627c
Adrian Lowdon <adrian@acceleratech.uk>
2020-09-25 12:10:54 +0100

    AG-4632 - added basic start page

--- grid-packages/ag-grid-docs/new/src/pages/index.js
+++ grid-packages/ag-grid-docs/new/src/pages/index.js
@@ -1,7 +1,17 @@
 import React from "react";
+import './index.css';
 
 export default function Home() {
-  return <div>
-    Welcome to the AG-Grid documentation.
+  return <div style={{ textAlign: 'center' }}>
+    <h1>Welcome to the AG-Grid documentation</h1>
+
+    <p>Which framework would you like to learn?</p>
+
+    <div className="getting-started-cards">
+      {['javascript', 'angular', 'react', 'vue'].map(framework => <div className="getting-started-card">
+        <img alt={framework} src={`fw-logos/${framework}.svg`} className="getting-started-card__logo" /><br />
+        <a href={`/${framework}/getting-started`} className="getting-started-card__button">Get started</a>
+      </div>)}
+    </div>
   </div>;
 }
