5ff76b99e215b27a12851bbfd49a01ef5628e636
Sean Landsman <sean@thelandsmans.com>
2020-10-07 10:43:46 +0100

    AG-4650 Vue 3 Support

--- grid-packages/ag-grid-vue-legacy/src/main.ts
+++ grid-packages/ag-grid-vue-legacy/src/main.ts
@@ -4,12 +4,9 @@ import {Vue} from 'vue-property-decorator';
 
 Vue.config.productionTip = false;
 
-/!*
- * This is for testing this component in isolation - not really useful outside of local dev
- * Not deployed/built for downstream consumption
- *!/
+// This is for testing this component in isolation - not really useful outside of local dev
+// Not deployed/built for downstream consumption
 new Vue({
   render: (h) => h(AgGridVue),
 }).$mount('#app');
-
 */
--- grid-packages/ag-grid-vue/src/main.ts
+++ grid-packages/ag-grid-vue/src/main.ts
@@ -2,11 +2,9 @@
 import {AgGridVue} from './AgGridVue';
 import {createApp} from 'vue';
 
-/!*
- * This is for testing this component in isolation - not really useful outside of local dev
- * Not deployed/built for downstream consumption
- *!/
+
+// This is for testing this component in isolation - not really useful outside of local dev
+// Not deployed/built for downstream consumption
 const app = createApp(AgGridVue)
 app.mount('#app');
-
 */
