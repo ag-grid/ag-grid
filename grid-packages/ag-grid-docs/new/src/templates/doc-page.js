ba2b3e8e3f85a7f603eea20952372ec0cfb2207d
Adrian Lowdon <adrian@acceleratech.uk>
2020-10-15 18:03:51 +0100

    AG-4632 - generate HTML with relevant framework-specific sections removed

--- grid-packages/ag-grid-docs/new/src/templates/doc-page.js
+++ grid-packages/ag-grid-docs/new/src/templates/doc-page.js
@@ -3,19 +3,18 @@ import { Helmet } from "react-helmet";
 import { graphql } from "gatsby";
 import rehypeReact from "rehype-react";
 import ExampleRunner from '../components/ExampleRunner';
-import FrameworkSpecificSection from '../components/FrameworkSpecificSection';
 import SideMenu from '../components/SideMenu';
 import './doc-page.scss';
 
 const DocPageTemplate = ({ data, pageContext: { framework }, location }) => {
   const { markdownRemark: page } = data;
   const pageName = location.pathname.replace(`/${framework}/`, '');
+  const ast = processFrameworkSpecificSections(page.htmlAst, framework);
 
   const renderAst = new rehypeReact({
     createElement: React.createElement,
     components: {
-      "example-runner": props => ExampleRunner({ ...props, framework, pageName }),
-      "div": props => FrameworkSpecificSection({ ...props, framework })
+      "example-runner": props => ExampleRunner({ ...props, framework, pageName })
     },
   }).Compiler;
 
@@ -24,7 +23,7 @@ const DocPageTemplate = ({ data, pageContext: { framework }, location }) => {
       <div className="doc-page">
         <Helmet title={`AG-Grid: ${page.frontmatter.title}`} />
         <h1>{page.frontmatter.title}</h1>
-        {renderAst(page.htmlAst)}
+        {renderAst(ast)}
       </div>
       {page.headings && <SideMenu headings={page.headings} />}
     </div>
@@ -46,5 +45,26 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+const processFrameworkSpecificSections = (ast, framework) => {
+  return {
+    ...ast,
+    children: ast.children.reduce((children, child) => {
+      if (child.properties != null && child.tagName === 'div' && child.properties.className[0] === 'custom-block') {
+        const blockCustomClass = child.properties.className[1];
+
+        if (blockCustomClass.endsWith('-only-section')) {
+          if (blockCustomClass === `${framework}-only-section`) {
+            return [...children, ...child.children[0].children];
+          } else {
+            return children;
+          }
+        }
+      }
+
+      return [...children, child];
+    }, [])
+  };
+};
 
 export default DocPageTemplate;
