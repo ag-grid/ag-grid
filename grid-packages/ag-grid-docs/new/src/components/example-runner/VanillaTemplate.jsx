0ec653fc7b534fc9a093e5690ded4c354782aa45
Adrian Lowdon <adrian@acceleratech.uk>
2020-10-23 12:06:12 +0100

    AG-4634 - removed unwanted wrapper around fragment in vanilla examples

--- grid-packages/ag-grid-docs/new/src/components/example-runner/VanillaTemplate.jsx
+++ grid-packages/ag-grid-docs/new/src/components/example-runner/VanillaTemplate.jsx
@@ -1,28 +1,35 @@
 import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 import ExampleStyle from './ExampleStyle';
 import Extras from './Extras';
 import Scripts from './Scripts';
 import Styles from './Styles';
 
-const VanillaTemplate = ({ appLocation, options, scriptFiles, styleFiles, indexFragment }) => {
-    const agGridVersion = '24.1.0';
-    const communityScriptPath = `https://unpkg.com/@ag-grid-community/all-modules@${agGridVersion}/dist/ag-grid-community.min.js`;
-    const enterpriseScriptPath = `https://unpkg.com/@ag-grid-enterprise/all-modules@${agGridVersion}/dist/ag-grid-enterprise.min.js`;
-
-    return <html lang="en">
+const VanillaTemplate = ({ appLocation, options, scriptFiles, styleFiles, indexFragment }) =>
+    <html lang="en">
         <head>
             <ExampleStyle />
             <Styles files={styleFiles} />
             <Extras options={options} />
         </head>
-        <body>
-            <div style={{ height: '100%', boxSizing: 'border-box' }} dangerouslySetInnerHTML={{ __html: indexFragment }}></div>
+        <Body appLocation={appLocation} options={options} scriptFiles={scriptFiles} indexFragment={indexFragment} />
+    </html>;
 
+// we have to use this function to avoid a wrapping div around the fragment
+const Body = ({ appLocation, options, scriptFiles, indexFragment }) => {
+    const agGridVersion = '24.1.0';
+    const communityScriptPath = `https://unpkg.com/@ag-grid-community/all-modules@${agGridVersion}/dist/ag-grid-community.min.js`;
+    const enterpriseScriptPath = `https://unpkg.com/@ag-grid-enterprise/all-modules@${agGridVersion}/dist/ag-grid-enterprise.min.js`;
+
+    const bodySuffix = ReactDOMServer.renderToStaticMarkup(
+        <>
             <script dangerouslySetInnerHTML={{ __html: `var __basePath = '${appLocation}';` }}></script>
             <script src={options.enterprise ? enterpriseScriptPath : communityScriptPath}></script>
             <Scripts files={scriptFiles} />
-        </body>
-    </html>;
+        </>
+    );
+
+    return <body dangerouslySetInnerHTML={{ __html: `${indexFragment}\n${bodySuffix}` }}></body>;
 };
 
 export default VanillaTemplate;
