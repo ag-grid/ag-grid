9714e3e612109e8cbf175d528412c0aa86233bc0
Guilherme Lopes <guilherme@gwm-solutions.com>
2020-10-28 15:43:06 +0000

    AG-4632 - updated prism theme

--- grid-packages/ag-grid-docs/new/gatsby-browser.js
+++ grid-packages/ag-grid-docs/new/gatsby-browser.js
@@ -2,5 +2,6 @@ import 'jquery/dist/jquery.min.js';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/scss/bootstrap.scss';
-import 'prismjs/themes/prism-okaidia.css';
+// import 'prismjs/themes/prism-coy.css';
+import './static/themes/prism-coy-without-shadows.css';
 import 'fontsource-roboto';
