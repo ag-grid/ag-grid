1678d1cbc1aa98fc4f92b2693edf97a2f3b79b14
Guilherme Lopes <guilherme@gwm-solutions.com>
2020-11-04 15:37:36 +0000

    AG-4634 - made new IconsPanel component more legible

--- grid-packages/ag-grid-docs/documentation/src/components/IconsPanel.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/IconsPanel.jsx
@@ -28,7 +28,14 @@ const PanelTabs = ({ activeTheme, setActiveTheme }) => (
     <ul className={styles.iconsPanel__container}>
         {themes.map(theme => (
             <li key={`${theme}-tab`}>
-                <button className={`${styles.iconsPanel__navLink} ${theme === activeTheme ? styles.iconsPanel__navLink__active: ''}`} data-toggle="tab" role="tab" onClick={(e) => onTabClick(e, setActiveTheme, theme)} aria-controls={theme} aria-selected={theme === activeTheme ? 'true' : 'false'}>{`${capitalizeName(theme)} Icons`}</button>
+                <button 
+                    className={`${styles.iconsPanel__navLink} ${theme === activeTheme ? styles.iconsPanel__navLink__active: ''}`}
+                    data-toggle="tab"
+                    role="tab" 
+                    onClick={(e) => onTabClick(e, setActiveTheme, theme)}
+                    aria-controls={theme}
+                    aria-selected={theme === activeTheme ? 'true' : 'false'}>{`${capitalizeName(theme)} Icons`}
+                </button>
             </li>
         ))}
     </ul>
