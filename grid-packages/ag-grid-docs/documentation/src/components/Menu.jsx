29c94c7a83342e58ec2bf2f2b9382b4da9175440
Guilherme Lopes <guilherme@gwm-solutions.com>
2020-11-04 19:57:11 +0000

    AG-4632 - transformed the framework property into an array

--- grid-packages/ag-grid-docs/documentation/src/components/Menu.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/Menu.jsx
@@ -8,17 +8,29 @@ import styles from './menu.module.scss';
 
 const MenuSection = ({ title, items, currentFramework, isActive, toggleActive }) => {
     return <li key={title} className={styles.menu__section}>
-        <div onClick={() => toggleActive()} onKeyDown={() => toggleActive()} role="button" tabIndex="0">
-            <FontAwesomeIcon icon={faChevronRight} fixedWidth rotation={isActive ? 90 : 0} className={styles.menu__arrow} />
+        <div 
+            onClick={ () => toggleActive() }
+            onKeyDown={ () => toggleActive() }
+            role="button"
+            tabIndex="0">
+            <FontAwesomeIcon
+                icon={faChevronRight}
+                fixedWidth
+                rotation={isActive ? 90 : 0}
+                className={styles.menu__arrow} 
+            />
             {title}
         </div>
-        {isActive && <MenuGroup group={{ group: title, items }} currentFramework={currentFramework} />}
+        { isActive && <MenuGroup group={{ group: title, items }} currentFramework={currentFramework} /> }
     </li>;
 };
 
 const MenuGroup = ({ group, currentFramework }) =>
     <ul className={styles.menu__group}>
-        {group.items.filter(item => !item.framework || item.framework === currentFramework).map(item => <MenuItem key={item.title} item={item} currentFramework={currentFramework} />)}
+        { group.items
+            .filter(item => !item.framework || item.framework.indexOf(currentFramework) !== -1)
+            .map(item => <MenuItem key={item.title} item={item} currentFramework={currentFramework} />)
+        }
     </ul>;
 
 const MenuItem = ({ item, currentFramework }) => {
@@ -27,13 +39,13 @@ const MenuItem = ({ item, currentFramework }) => {
 
     return (
         <li key={item.title} className={styles.menu__item}>
-            {item.url
+            { item.url
                 ? <Link
                     to={item.url.replace('../', `/${currentFramework}/`)}
                     activeClassName={styles.menu__itemActive}>{title}</Link>
                 : title
             }
-            {item.items && <MenuGroup group={{ group: item.title, items: item.items }} currentFramework={currentFramework} />}
+            { item.items && <MenuGroup group={{ group: item.title, items: item.items }} currentFramework={currentFramework} /> }
         </li>
     );
 };
@@ -54,21 +66,23 @@ const Menu = ({ currentFramework, currentPage }) => {
     }, [currentPage]); // eslint-disable-line react-hooks/exhaustive-deps
 
     return <div className={styles.menu}>
-        <Search indices={[{ name: `ag-grid_${currentFramework}`, title: "Documentation Pages" }]} />
+        <Search indices={ [{ name: `ag-grid_${currentFramework}`, title: "Documentation Pages" }] } />
 
         <ul className={styles.menu__sections}>
             {combinedMenuItems.map(item => {
                 const { title } = item;
                 const isActive = title === activeSection;
 
-                return <MenuSection
-                    key={title}
-                    title={title}
-                    items={item.items}
-                    currentFramework={currentFramework}
-                    isActive={isActive}
-                    toggleActive={() => setActiveSection(isActive ? null : title)}
-                />;
+                return (
+                    <MenuSection
+                        key={title}
+                        title={title}
+                        items={item.items}
+                        currentFramework={currentFramework}
+                        isActive={isActive}
+                        toggleActive={() => setActiveSection(isActive ? null : title)}
+                    />
+                );
             })}
         </ul>
     </div>;
