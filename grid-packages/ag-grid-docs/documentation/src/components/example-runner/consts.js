7dc56675d479b1a487dfa6a3e17d7912cc091e97
Adrian Lowdon <adrian@acceleratech.uk>
2020-12-03 10:50:09 +0000

    AG-4635 - pull library package versions for documentation website from package.json

--- grid-packages/ag-grid-docs/documentation/src/components/example-runner/consts.js
+++ grid-packages/ag-grid-docs/documentation/src/components/example-runner/consts.js
@@ -1,3 +1,3 @@
-export const agGridVersion = '24.1.0';
-export const agChartsVersion = '2.1.0';
+export const agGridVersion = require('../../../../../../community-modules/core/package.json').version;
+export const agChartsVersion = require('../../../../../../charts-packages/ag-charts-community/package.json').version;
 export const localPrefix = '//localhost:8080/dev';
