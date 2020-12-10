f65a84db61f3a9657260a919500340a018758d70
Sean Landsman <sean@thelandsmans.com>
2020-12-10 08:09:56 +0000

    AG-4886 Create task to deploy docs to archive via TC

---
+++ scripts/release/createAndDeployDocsToTCArchive.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ZIP_PREFIX=`date +%Y%m%d`
+
+cd grid-packages/ag-grid-docs
+npx gulp release-archive
+cd dist
+
+FILENAME=release_"$ZIP_PREFIX"_v"$ZIP_PREFIX".zip
+zip -r ../../../$FILENAME *
+
+cd ../../../
+
+rm -rf /var/www/html/archive
+mv $FILENAME /var/www/html/archive/1.0.0
+unzip /var/www/html/archive/1.0.0/$FILENAME -d /var/www/html/archive/1.0.0/
+
+# not really necessary as we don't delete this file
+touch /var/www/html/robots.txt
+echo "User-agent: *
+Disallow: /" > /var/www/html/robots.txt
