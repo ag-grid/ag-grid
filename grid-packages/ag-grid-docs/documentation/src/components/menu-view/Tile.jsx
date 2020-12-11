2eedeaa3f45e4fadace4b394d7dee72a5ce340b9
Guilherme Lopes <guilherme@gwm-solutions.com>
2020-12-11 22:31:11 +0000

    AG-4632 - fixed docs landing page indentation

--- grid-packages/ag-grid-docs/documentation/src/components/menu-view/Tile.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/menu-view/Tile.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import classnames from 'classnames';
@@ -11,7 +11,7 @@ const recursiveRender = (items, framework, group, collapsed, level = 0, isLast, 
     if (item.frameworks && item.frameworks.indexOf(framework) === - 1) { return null; }
 
     const className = `menu-view-tile__list__${level === 0 || forceTopLevel ? 'block' : 'inline'}`;
-    const hideComma = level === 0 || forceTopLevel || (isLast && items.length - 1 === idx);
+    const hideComma = level === 0 || forceTopLevel;
 
     const title = item.url && (!collapsed || item.showInCollapsed) && (
         <span className={ styles[className]}>
@@ -33,6 +33,13 @@ const recursiveRender = (items, framework, group, collapsed, level = 0, isLast, 
             level === 0 || (isLast && idx === items.length - 1),
             !!item.forceTopLevelSubItems
         )
+        if (level === 0) {
+            content = (
+                <div className={ classnames(styles['menu-view-tile__sub_list'], { [styles['menu-view-tile--force_toplevel']]: !!item.forceTopLevelSubItems }) }>
+                    { content }
+                </div>
+            )
+        }
      }
 
 
@@ -47,27 +54,59 @@ const recursiveRender = (items, framework, group, collapsed, level = 0, isLast, 
 });
 
 const Tile = ({ data, group, framework }) => {
+    const tileEl = useRef(null);
     const [collapsed, setCollapsed] = useState(true);
 
     if (!data.icon) { return null; }
     const iconName = data.icon.replace('icon-','');
     const iconAlt= iconName.replace(/-/g,' ');
 
-    const onClick = () => {
+    const toggleCollapse = (shouldCollapse) => {
+        if (typeof shouldCollapse !== 'boolean') {
+            shouldCollapse = !collapsed;
+        }
+
         if (document.body.clientWidth < 768) {
-            setCollapsed(!collapsed);
+            setCollapsed(shouldCollapse);
+        }
+    }
+
+    const onClick = () => {
+        toggleCollapse();
+
+    }
+
+    const onKeyDown = (e) => {
+        const key = e.key;
+        const wrapperFocused = tileEl.current === document.activeElement;
+        if (key !== 'Enter' && key !== ' ') { return; }
+
+        if (key === ' ' || wrapperFocused) {
+            e.preventDefault();
+            toggleCollapse();
         }
     }
 
     const onMouseOut = () => {
-        setCollapsed(true);
+        toggleCollapse(true);
+    }
+
+    const onBlur = (e) => {
+        if (tileEl.current && tileEl.current.contains(e.relatedTarget)) { return; }
+        toggleCollapse(true);
     }
 
     return (
         <div 
+            ref={ tileEl }
             className={ classnames(styles['menu-view-tile'], { [styles['menu-view-tile--collapsed']]: collapsed }) }
+            role="button"
+            tabIndex={0}
+            aria-expanded={!collapsed}
             onClick={ () => onClick() }
-            onMouseLeave={ () => onMouseOut() }>
+            onKeyDown={ (e) => onKeyDown(e) }
+            onMouseLeave={ () => onMouseOut() }
+            onBlur={(e) => onBlur(e) }>
             <div className={ styles['menu-view-tile__icon'] }><img alt={ iconAlt } src={ icons[iconName] }></img></div>
             <h3 className={ styles['menu-view-tile__title'] }>{ data.title }</h3>
             <div className={ styles['menu-view-tile__list'] }>
