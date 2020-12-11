0e2c91a133803ed6d4a778d540c938bd303bb2dc
Adrian Lowdon <adrian@acceleratech.uk>
2020-12-11 10:53:06 +0000

    AG-4850 - added mailing list signup

--- grid-packages/ag-grid-docs/documentation/src/components/Announcements.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/Announcements.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import styles from './Announcements.module.scss';
 import thinksterPng from '../images/thinkster.png';
 import communitySvg from '../images/community.svg';
@@ -93,6 +94,7 @@ const Announcements = () => <div className={styles['announcements']}>
         <a href="https://halfstackconf.com/online/" target="_blank" rel="noreferrer">Half Stack Online</a>
         <p className="card-subtitle mb-2 text-muted" style={{ marginTop: '1px' }}>May 22</p>
     </Announcement>
+    <MailingListSignup />
 </div>;
 
 const Announcement = ({ title, date, children, version }) => {
@@ -110,6 +112,93 @@ const Announcement = ({ title, date, children, version }) => {
                 </p>}
         </div>
     </div>;
+};
+
+const MailingListSignup = () => {
+    return <>
+        <Helmet>
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" />
+            <style type="text/css">
+                {`#mc_embed_signup {
+        background: #fff;
+        clear: left;
+        font: 14px Helvetica, Arial, sans-serif;
+    }`}
+            </style>
+        </Helmet>
+        <div id="mc_embed_signup">
+            <form action="https://ag-grid.us11.list-manage.com/subscribe/post?u=9b44b788c97fa5b498fbbc9b5&amp;id=9353cf87ce"
+                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+                target="_blank" novalidate>
+                <div id="mc_embed_signup_scroll">
+                    <h2>ag-Grid Mailing List</h2>
+                    <div class="mc-field-group">
+                        <label for="mce-EMAIL">Email Address <span class="asterisk">*</span>
+                            <input type="email" defaultValue="" name="EMAIL" class="required email" id="mce-EMAIL" />
+                        </label>
+                    </div>
+                    <div class="mc-field-group">
+                        <label for="mce-FNAME">First Name
+                            <input type="text" defaultValue="" name="FNAME" class="" id="mce-FNAME" />
+                        </label>
+                    </div>
+                    <br />
+                    <div class="mc-field-group input-group">
+                        <ul>
+                            <li><strong>Select your preference:</strong></li>
+                            <li>
+                                <label for="mce-group[18365]-18365-0">
+                                    <input type="checkbox" value="1" name="group[18365][1]" id="mce-group[18365]-18365-0" /> Angular
+                                </label>
+                            </li>
+                            <li>
+                                <label for="mce-group[18365]-18365-1">
+                                    <input type="checkbox" value="2" name="group[18365][2]" id="mce-group[18365]-18365-1" /> React
+                                </label>
+                            </li>
+                            <li>
+                                <label for="mce-group[18365]-18365-2">
+                                    <input type="checkbox" value="4" name="group[18365][4]" id="mce-group[18365]-18365-2" /> General
+                                </label>
+                            </li>
+                        </ul>
+                    </div>
+                    <div id="mce-responses" class="clear">
+                        <div class="response" id="mce-error-response" style={{ display: 'none' }}></div>
+                        <div class="response" id="mce-success-response" style={{ display: 'none' }}></div>
+                    </div>
+                    {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups */}
+                    <div style={{ position: 'absolute', left: '-5000px' }} aria-hidden="true">
+                        <input type="text" name="b_9b44b788c97fa5b498fbbc9b5_9353cf87ce" tabindex="-1" defaultValue="" />
+                    </div>
+                    <div class="clear">
+                        <input
+                            type="submit"
+                            style={{ backgroundColor: '#0084e7' }}
+                            value="Subscribe"
+                            name="subscribe"
+                            id="mc-embedded-subscribe"
+                            className="button" />
+                    </div>
+                </div>
+            </form>
+        </div>
+        <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+        <script type='text/javascript' dangerouslySetInnerHTML={{
+            __html: `(function ($) {
+            window.fnames = new Array();
+        window.ftypes = new Array();
+        fnames[0] = 'EMAIL';
+        ftypes[0] = 'email';
+        fnames[1] = 'FNAME';
+        ftypes[1] = 'text';
+        fnames[3] = 'ADDRESS';
+        ftypes[3] = 'address';
+        fnames[4] = 'PHONE';
+        ftypes[4] = 'phone';
+    }(jQuery));
+    var $mcj = jQuery.noConflict(true);`}}></script>
+    </>;
 };
 
 export default Announcements;
