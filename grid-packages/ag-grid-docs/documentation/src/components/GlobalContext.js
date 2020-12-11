a218eb6ade3a888c7a7db8ca2cb3ffc73653211c
Adrian Lowdon <adrian@acceleratech.uk>
2020-12-11 14:37:23 +0000

    AG-4870 - preserve context in localstorage

--- grid-packages/ag-grid-docs/documentation/src/components/GlobalContext.js
+++ grid-packages/ag-grid-docs/documentation/src/components/GlobalContext.js
@@ -8,17 +8,42 @@ const defaultContextValue = {
 
 const { Provider, Consumer } = React.createContext(defaultContextValue);
 
+const contextKey = 'documentationContext';
+
 class GlobalContextProvider extends React.PureComponent {
     constructor() {
         super();
 
+        let contextValue = defaultContextValue;
+
+        if (window.localStorage) {
+            const storedContextString = window.localStorage.getItem(contextKey);
+
+            if (storedContextString) {
+                const storedContext = JSON.parse(storedContextString);
+
+                if (storedContext) {
+                    contextValue = {
+                        ...contextValue,
+                        ...storedContext,
+                    };
+                }
+            }
+        }
+
         this.state = {
-            ...defaultContextValue,
+            ...contextValue,
             set: this.setData,
         };
     }
 
-    setData = newData => this.setState(newData);
+    setData = newData => {
+        this.setState(newData, () => {
+            if (window.localStorage) {
+                window.localStorage.setItem(contextKey, JSON.stringify(this.state));
+            }
+        });
+    };
 
     render() {
         return <Provider value={this.state}>{this.props.children}</Provider>;
