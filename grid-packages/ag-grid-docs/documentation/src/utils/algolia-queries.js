8d3f7db95cd90fe7adceecef178893f4e721dd2b
Adrian Lowdon <adrian@acceleratech.uk>
2020-12-14 15:43:37 +0000

    AG-4862 - ensure Algolia results are only generated for frameworks that a page is relevant to

--- grid-packages/ag-grid-docs/documentation/src/utils/algolia-queries.js
+++ grid-packages/ag-grid-docs/documentation/src/utils/algolia-queries.js
@@ -1,32 +1,24 @@
-const escapeStringRegexp = require('escape-string-regexp');
 const stripHtml = require('./strip-html');
 const processFrameworkSpecificSections = require('./framework-specific-sections');
 const supportedFrameworks = require('./supported-frameworks');
 
-const pagePath = 'pages';
-
 const pageQuery = `{
-  pages: allMarkdownRemark(
-    filter: {
-      fileAbsolutePath: { regex: "/${escapeStringRegexp(pagePath)}/" },
-    }
-  ) {
-    edges {
-      node {
-        id
-        frontmatter {
-          title
-        }
-        fields {
-          path
-        }
-        htmlAst
+  pages: allMarkdownRemark {
+    nodes {
+      id
+      frontmatter {
+        title
+        frameworks
       }
+      fields {
+        path
+      }
+      htmlAst
     }
   }
 }`;
 
-function pageToAlgoliaRecord({ node: { id: objectID, frontmatter: { title }, fields: { path }, htmlAst } }, framework) {
+function pageToAlgoliaRecord({ id: objectID, frontmatter: { title }, fields: { path }, htmlAst }, framework) {
   const text = stripHtml(processFrameworkSpecificSections(htmlAst, framework)).substr(0, 10000);
 
   return {
@@ -39,7 +31,9 @@ function pageToAlgoliaRecord({ node: { id: objectID, frontmatter: { title }, fie
 
 const queries = supportedFrameworks.map(framework => ({
   query: pageQuery,
-  transformer: ({ data }) => data.pages.edges.map(node => pageToAlgoliaRecord(node, framework)),
+  transformer: ({ data }) => data.pages.nodes
+    .filter(node => !node.frontmatter.frameworks || node.frontmatter.frameworks.indexOf(framework) >= 0)
+    .map(node => pageToAlgoliaRecord(node, framework)),
   indexName: `ag-grid_${framework}`,
   settings: { attributesToSnippet: [`text:20`] },
 }));
