e7aaf90b50e2198893a78b2ff5cf100624fffa40
Adrian Lowdon <adrian@acceleratech.uk>
2020-12-15 17:07:47 +0000

    RTI-378 - fixed server-side rendering for GIFs

--- grid-packages/ag-grid-docs/documentation/src/components/Gif.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/Gif.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
-import GifPlayer from 'react-gif-player';
 import { useImageFileNodes, getImage } from './use-image-file-nodes';
+import isServerSideRendering from '../utils/is-server-side-rendering';
 import styles from './Gif.module.scss';
 
 const Gif = ({ pageName, src, autoPlay, className, wrapped, children, ...props }) => {
@@ -12,12 +12,15 @@ const Gif = ({ pageName, src, autoPlay, className, wrapped, children, ...props }
         throw new Error(`Could not find requested image: ${src}`);
     }
 
-    return <GifPlayer
-        gif={image.publicURL}
-        className={classnames(styles['gif'], { [styles['gif--wrapped']]: wrapped }, className)}
-        autoplay={autoPlay}
-        {...props}
-    />;
+    const classes = classnames(styles['gif'], { [styles['gif--wrapped']]: wrapped }, className);
+
+    if (isServerSideRendering()) {
+        return <img src={image.publicURL} className={classes} {...props} />;
+    }
+
+    const GifPlayer = require('react-gif-player');
+
+    return <GifPlayer gif={image.publicURL} className={classes} autoplay={autoPlay} {...props} />;
 };
 
 export default Gif;
