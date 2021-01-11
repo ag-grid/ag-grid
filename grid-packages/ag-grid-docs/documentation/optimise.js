95265f6e34f7c63fb11fca796abe285a9ce3c14a
Adrian Lowdon <adrian@acceleratech.uk>
2021-01-11 15:53:21 +0000

    AG-4940 - added comment around optimisation

--- grid-packages/ag-grid-docs/documentation/optimise.js
+++ grid-packages/ag-grid-docs/documentation/optimise.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+
+// Gatsby looks for queries in all JavaScript and TypeScript files inside src by default. This means that it will
+// needlessly inspect all of our example files for queries, which noticeably affects performance. This optimisation
+// will exclude our example folders from Gatsby's query compilation to avoid this.
+
 const queryCompilerLocation = 'node_modules/gatsby/dist/query/query-compiler.js';
 const data = fs.readFileSync(queryCompilerLocation).toString().split('\n');
 
