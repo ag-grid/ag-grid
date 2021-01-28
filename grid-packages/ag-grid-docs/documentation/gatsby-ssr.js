5515ebf829412dba2347c257e142ad2dd8746838
Adrian Lowdon <adrian@acceleratech.uk>
2021-01-28 13:40:35 +0000

    AG-4865 - don't inline generated CSS

--- grid-packages/ag-grid-docs/documentation/gatsby-ssr.js
+++ grid-packages/ag-grid-docs/documentation/gatsby-ssr.js
@@ -47,3 +47,22 @@ export const wrapPageElement = ({ element, props: { location: { pathname } } }) 
         </>
     );
 };
+
+export const onPreRenderHTML = ({ getHeadComponents }) => {
+    if (process.env.NODE_ENV !== 'production') {
+        return;
+    }
+
+    getHeadComponents().forEach(el => {
+        // remove inlined CSS
+        if (el.type === 'style' && el.props['data-href']) {
+            el.type = 'link';
+            el.props['href'] = el.props['data-href'];
+            el.props['rel'] = 'stylesheet';
+            el.props['type'] = 'text/css';
+
+            delete el.props['data-href'];
+            delete el.props['dangerouslySetInnerHTML'];
+        }
+    });
+};
