0d07a16b296c3dd12e8b68530e02ce7169ad67f5
Adrian Lowdon <adrian@acceleratech.uk>
2021-01-28 15:43:10 +0000

    AG-4865 - load external CSS in development mode

--- grid-packages/ag-grid-docs/documentation/gatsby-ssr.js
+++ grid-packages/ag-grid-docs/documentation/gatsby-ssr.js
@@ -36,39 +36,38 @@ export const wrapPageElement = ({ element, props: { location: { pathname } } }) 
 };
 
 export const onPreRenderHTML = ({ getHeadComponents, replaceHeadComponents }) => {
-    if (process.env.NODE_ENV !== 'production') {
-        return;
-    }
-
     const headComponents = getHeadComponents();
 
-    headComponents.forEach(el => {
-        // remove inlined CSS
-        if (el.type === 'style' && el.props['data-href']) {
-            el.type = 'link';
-            el.props['href'] = el.props['data-href'];
-            el.props['rel'] = 'stylesheet';
-            el.props['type'] = 'text/css';
+    if (process.env.NODE_ENV === 'production') {
+        headComponents.forEach(el => {
+            // remove inlined CSS
+            if (el.type === 'style' && el.props['data-href']) {
+                el.type = 'link';
+                el.props['href'] = el.props['data-href'];
+                el.props['rel'] = 'stylesheet';
+                el.props['type'] = 'text/css';
 
-            delete el.props['data-href'];
-            delete el.props['dangerouslySetInnerHTML'];
-        }
-    });
+                delete el.props['data-href'];
+                delete el.props['dangerouslySetInnerHTML'];
+            }
+        });
+    }
 
     // ensure these styles are loaded before ours
-    headComponents.unshift(
-        <link
-            key="fontawesome"
-            rel="stylesheet"
-            href={`https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-svg-core@${dependencies['@fortawesome/fontawesome-svg-core']}/styles.min.css`}
-            crossOrigin="anonymous" />);
-
     headComponents.unshift(<link
         key="roboto"
         rel="stylesheet"
         href="https://cdn.jsdelivr.net/npm/@fontsource/roboto@4.1.0/index.min.css"
         crossOrigin="anonymous" />,
     );
+
+    // we import the Font Awesome CSS here even though it is also imported by the library to avoid a repaint flash
+    headComponents.unshift(
+        <link
+            key="fontawesome"
+            rel="stylesheet"
+            href={`https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-svg-core@${dependencies['@fortawesome/fontawesome-svg-core']}/styles.min.css`}
+            crossOrigin="anonymous" />);
 
     replaceHeadComponents(headComponents);
 };
