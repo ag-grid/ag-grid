f0185a95d19fab6f6248f839b2d54ac67ed66751
Adrian Lowdon <adrian@acceleratech.uk>
2021-02-02 11:00:55 +0000

    AG-4923 - fixed page scrolling for hash URLs

--- grid-packages/ag-grid-docs/documentation/customise.js
+++ grid-packages/ag-grid-docs/documentation/customise.js
@@ -1,3 +1,27 @@
 const fs = require('fs-extra');
 
-fs.copyFileSync('./customisations/gatsby-source-filesystem/index.js', './node_modules/gatsby-source-filesystem/index.js');
+const addMarkdownIncludeSupport = () => {
+    console.log(`-> Adding support for including Markdown files into other Markdown files`);
+    fs.copyFileSync('./customisations/gatsby-source-filesystem/index.js', './node_modules/gatsby-source-filesystem/index.js');
+};
+
+const fixScrollingIssue = () => {
+    console.log(`-> Fixing scrolling issue for hash URLs`);
+
+    const location = './node_modules/gatsby-remark-autolink-headers/gatsby-browser.js';
+    const contents = fs.readFileSync(location, 'utf8');
+    const newContents = contents.replace('exports.onInitialClientRender', 'exports.onInitialClientRender = function() {}; var ignore');
+
+    if (newContents != contents) {
+        fs.writeFileSync(location, newContents);
+    }
+};
+
+console.log(`--------------------------------------------------------------------------------`);
+console.log(`Applying customisations...`);
+
+addMarkdownIncludeSupport();
+fixScrollingIssue();
+
+console.log(`Done.`);
+console.log(`--------------------------------------------------------------------------------`);
