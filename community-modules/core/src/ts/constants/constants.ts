1bf2b3cd4c86cc9722b628356fd4ebd7373b1c1f
ceolter <0872393174Aa>
2021-02-02 11:40:11 +0000

    AG-3572 [Updating Data] Allow suppressing sort and/or grouping after updating data unless the user requests another sort/group ceolter 27/01/2021 16:14

--- community-modules/core/src/ts/constants/constants.ts
+++ community-modules/core/src/ts/constants/constants.ts
@@ -1,12 +1,5 @@
 export class Constants {
 
-    // static STEP_EVERYTHING = 0;
-    // static STEP_FILTER = 1;
-    // static STEP_SORT = 2;
-    // static STEP_MAP = 3;
-    // static STEP_AGGREGATE = 4;
-    // static STEP_PIVOT = 5;
-
     static ROW_BUFFER_SIZE = 10;
     static LAYOUT_INTERVAL = 500;
     static BATCH_WAIT_MILLIS = 50;
