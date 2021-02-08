d76bc75b39007fe476197379e69752f7d9a9bc12
ceolter <0872393174Aa>
2021-02-08 14:03:45 +0000

    AG-5041 Column groups lose expand/collapse state when Column Definitions updated

--- grid-packages/ag-grid-docs/documentation/doc-pages/column-updating-definitions/examples/column-groups/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/column-updating-definitions/examples/column-groups/main.js
@@ -1,81 +1,85 @@
-var columnSetA = [
-    {
-        headerName: 'Group A',
-        groupId: 'groupA',
-        children: [
-            { field: 'athlete' },
-            { field: 'age' },
-            { field: 'country', columnGroupShow: 'open' },
-        ],
-    },
-    {
-        headerName: 'Group B',
-        children: [
-            { field: 'sport' },
-            { field: 'year' },
-            { field: 'date', columnGroupShow: 'open' }
-        ]
-    },
-    {
-        headerName: 'Group C',
-        groupId: 'groupC',
-        children: [
-            { field: 'total' },
-            { field: 'gold', columnGroupShow: 'open' },
-            { field: 'silver', columnGroupShow: 'open' },
-            { field: 'bronze', columnGroupShow: 'open' }
-        ],
-    }
-];
+function createColSetA() {
+    return [
+        {
+            headerName: 'Group A',
+            groupId: 'groupA',
+            children: [
+                { field: 'athlete' },
+                { field: 'age' },
+                { field: 'country', columnGroupShow: 'open' },
+            ],
+        },
+        {
+            headerName: 'Group B',
+            children: [
+                { field: 'sport' },
+                { field: 'year' },
+                { field: 'date', columnGroupShow: 'open' }
+            ]
+        },
+        {
+            headerName: 'Group C',
+            groupId: 'groupC',
+            children: [
+                { field: 'total' },
+                { field: 'gold', columnGroupShow: 'open' },
+                { field: 'silver', columnGroupShow: 'open' },
+                { field: 'bronze', columnGroupShow: 'open' }
+            ],
+        }
+    ];
+}
 
-var columnSetB = [
-    {
-        headerName: 'GROUP A',
-        groupId: 'groupA',
-        children: [
-            { field: 'athlete' },
-            { field: 'age' },
-            { field: 'country', columnGroupShow: 'open' },
-        ],
-    },
-    {
-        headerName: 'Group B',
-        children: [
-            { field: 'sport' },
-            { field: 'year' },
-            { field: 'date', columnGroupShow: 'open' }
-        ]
-    },
-    {
-        headerName: 'Group C',
-        groupId: 'groupC',
-        children: [
-            { field: 'total' },
-            { field: 'gold', columnGroupShow: 'open' },
-            { field: 'silver', columnGroupShow: 'open' },
-            { field: 'bronze', columnGroupShow: 'open' },
-            { field: 'extraA' },
-            { field: 'extraB', columnGroupShow: 'open' }
-        ],
-    }
-];
+function createColSetB() {
+    return [
+        {
+            headerName: 'GROUP A',
+            groupId: 'groupA',
+            children: [
+                { field: 'athlete' },
+                { field: 'age' },
+                { field: 'country', columnGroupShow: 'open' },
+            ],
+        },
+        {
+            headerName: 'Group B',
+            children: [
+                { field: 'sport' },
+                { field: 'year' },
+                { field: 'date', columnGroupShow: 'open' }
+            ]
+        },
+        {
+            headerName: 'Group C',
+            groupId: 'groupC',
+            children: [
+                { field: 'total' },
+                { field: 'gold', columnGroupShow: 'open' },
+                { field: 'silver', columnGroupShow: 'open' },
+                { field: 'bronze', columnGroupShow: 'open' },
+                { field: 'extraA' },
+                { field: 'extraB', columnGroupShow: 'open' }
+            ],
+        }
+    ];
+}
 
 var gridOptions = {
     defaultColDef: {
         initialWidth: 100,
         sortable: true,
         resizable: true
-    }
+    },
+    columnDefs: createColSetA()
 };
 
 function onBtSetA() {
-    gridOptions.api.setColumnDefs(columnSetA);
+    gridOptions.api.setColumnDefs(createColSetA());
 }
 
 function onBtSetB() {
-    gridOptions.api.setColumnDefs(columnSetB);
+    gridOptions.api.setColumnDefs(createColSetB());
 }
-
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
@@ -84,7 +88,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     agGrid.simpleHttpRequest({ url: 'https://www.ag-grid.com/example-assets/olympic-winners.json' })
         .then(function(data) {
-            this.onBtSetA();
             gridOptions.api.setRowData(data);
         });
 });
