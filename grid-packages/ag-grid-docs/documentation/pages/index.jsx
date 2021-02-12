d1ad9849dad56f81c133de30d00fbb7947ee2841
Adrian Lowdon <adrian@acceleratech.uk>
2021-02-12 15:26:39 +0000

    AG-4632 - added padding to documentation interstitial page

--- grid-packages/ag-grid-docs/documentation/pages/index.jsx
+++ grid-packages/ag-grid-docs/documentation/pages/index.jsx
@@ -4,8 +4,10 @@ import { getHeaderTitle } from 'utils/page-header';
 
 const Default = () => <div>
     <Helmet title={getHeaderTitle('Documentation')} />
-    <h1>Documentation</h1>
-    <div>Loading framework-specific content...</div>
+    <div className="p-3">
+        <h1>AG Grid: Documentation</h1>
+        <div>Loading framework-specific content...</div>
+    </div>
 </div>;
 
 export default Default;
