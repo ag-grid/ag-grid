de6a8fabd3c30f6c8a7279b96e47acc33084c14e
Adrian Lowdon <adrian@acceleratech.uk>
2021-02-18 11:38:22 +0000

    AG-4865 - use 'React Table' instead of 'React Grid'

--- grid-packages/ag-grid-docs/documentation/src/utils/page-header.js
+++ grid-packages/ag-grid-docs/documentation/src/utils/page-header.js
@@ -1,10 +1,15 @@
 import { agGridVersion, agChartsVersion } from 'utils/consts';
 
 export const getHeaderTitle = (title, framework = 'javascript', isCharts = false) =>
-    `${isCharts ? 'AG Charts' : 'AG Grid'} ${getFrameworkPart(framework, isCharts)}: ${title}`;
+    `${isCharts ? 'AG Charts' : 'AG Grid'} (${getFrameworkPart(framework, isCharts)}): ${title}`;
 
-const getFrameworkPart = (framework, isCharts = false) =>
-    `(${getFrameworkName(framework)} ${isCharts ? 'Charts' : 'Grid'})`;
+const getFrameworkPart = (framework, isCharts = false) => {
+    if (framework === 'react' && !isCharts) {
+        return 'React Table';
+    }
+
+    return `${getFrameworkName(framework)} ${isCharts ? 'Charts' : 'Grid'}`;
+};
 
 export const getFrameworkName = framework => {
     const mappings = {
@@ -17,5 +22,5 @@ export const getFrameworkName = framework => {
     return mappings[framework];
 };
 
-export const getGridVersionMessage = framework => `Download v${agGridVersion.split('.')[0]} of the best ${getFrameworkName(framework)} Grid in the world now.`;
-export const getChartsVersionMessage = framework => `Download v${agChartsVersion.split('.')[0]} of our ${getFrameworkName(framework)} Charts now.`;
+export const getGridVersionMessage = framework => `Download v${agGridVersion.split('.')[0]} of the best ${getFrameworkPart(framework, false)} in the world now.`;
+export const getChartsVersionMessage = framework => `Download v${agChartsVersion.split('.')[0]} of our ${getFrameworkPart(framework, true)} now.`;
