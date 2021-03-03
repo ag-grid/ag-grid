96c92aaceec05e8a125a775557ff71a5eab0fdee
Sean Landsman <sean@thelandsmans.com>
2021-03-03 09:54:50 +0000

    Update 25.1.0 for click n' deploy

--- scripts/release/createAndUploadDocsReleaseBundle.sh
+++ scripts/release/createAndUploadDocsReleaseBundle.sh
@@ -17,16 +17,15 @@ FILENAME=release_"$TIMESTAMP"_v"$VERSION".zip
 echo "************************************************************************"
 echo "******************** Create Release Bundle *****************************"
 echo "************************************************************************"
-#./scripts/release/createDocsReleaseBundle.sh $RAW_VERSION
+./scripts/release/createDocsReleaseBundle.sh $RAW_VERSION
 echo "************************************************************************"
 echo "******************** Uploading Release Bundle **************************"
 echo "************************************************************************"
-#./scripts/release/uploadReleaseZip.sh $FILENAME
-
+./scripts/release/uploadReleaseZip.sh $FILENAME
 echo "************************************************************************"
 echo "******************** Archiving Current Release *************************"
 echo "************************************************************************"
-#./scripts/release/archiveCurrentRelease.sh
+./scripts/release/archiveCurrentRelease.sh
 echo "************************************************************************"
 echo "******************** Preparing For New Deployment **********************"
 echo "************************************************************************"
@@ -34,5 +33,5 @@ echo "************************************************************************"
 echo "************************************************************************"
 echo "******************** Switch To New Release *****************************"
 echo "************************************************************************"
-#./scripts/release/switchRelease.sh $TIMESTAMP $2
+./scripts/release/switchRelease.sh $TIMESTAMP $2
 
