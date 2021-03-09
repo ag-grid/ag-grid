f132ba6a810a7238e40a2208ac9616ba45861a66
Sean Landsman <sean@thelandsmans.com>
2021-03-09 14:58:26 +0000

    AG-5138 One click deployment

--- scripts/release/htaccesChecker.js
+++ scripts/release/htaccesChecker.js
@@ -1,16 +1,21 @@
 const fs = require('fs');
 
+if(!fs.existsSync("./grid-packages/ag-grid-docs/documentation/public")) {
+    console.error("./grid-packages/ag-grid-docs/documentation/public doesn't exist - has the documentation build & package been run yet?");
+    process.exit(1);
+}
+
 const htaccess = fs.readFileSync('./grid-packages/ag-grid-docs/.htaccess', 'utf8');
 
 const redirectsTargets = htaccess.split('\n')
     .filter(line => line.includes('Redirect 301'))
     .filter(line => !line.includes("https://medium.com"))
-    .filter(line => !line.includes("gallery"))
     .map(line => line.replace(/^.* (.*)$/, "$1", ""))
-    .map(line => line.replace(/^\/(javascript|angular|react|vue)-(grid|charts)\//, ""))
+    .filter(line => line !== 'https://www.ag-grid.com/')
 
 redirectsTargets.forEach(redirectsTarget => {
-    if (!fs.existsSync(`./grid-packages/ag-grid-docs/documentation/doc-pages/${redirectsTarget}`)) {
-        console.error(`Redirect ending with ${redirectsTarget} - this directory doesn't exist under ./grid-packages/ag-grid-docs/documentation/doc-pages/` );
+    if (!fs.existsSync(`./grid-packages/ag-grid-docs/documentation/public/${redirectsTarget}`) &&
+        !fs.existsSync(`grid-packages/ag-grid-docs/src/${redirectsTarget}/index.php`)) {
+        console.error(`Redirect ending with ${redirectsTarget} - this directory doesn't exist under ./grid-packages/ag-grid-docs/documentation/public/ or under /grid-packages/ag-grid-docs/src/` );
     }
 })
