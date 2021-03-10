1d10fd9228b1abf39a032732601c08f64bf8e8a9
Sean Landsman <sean@thelandsmans.com>
2021-03-10 09:55:04 +0000

    AG-5138 One click deployment

--- scripts/release/switchRelease.sh
+++ scripts/release/switchRelease.sh
@@ -3,15 +3,24 @@
 if [ "$#" -lt 1 ]
   then
     echo "You must supply a timestamp"
-    echo "For example: ./scripts/release/prepareAgGridForRelease.sh 20191210"
+    echo "For example: ./scripts/release/switchRelease.sh 20191210"
     exit 1
 fi
 
+TIMESTAMP=$1
+
+CREDENTIALS_LOCATION=$HOME/$CREDENTIALS_FILE
 SSH_LOCATION=$HOME/$SSH_FILE
 
 if [ -z "$SSH_LOCATION" ]
 then
       echo "\$SSH_LOCATION is not set"
+      exit;
+fi
+
+if [ -z "$CREDENTIALS_LOCATION" ]
+then
+      echo "\$CREDENTIALS_LOCATION is not set"
       exit;
 fi
 
@@ -25,8 +34,7 @@ function checkFileExists {
 }
 
 checkFileExists $SSH_LOCATION
-
-TIMESTAMP=$1
+checkFileExists $CREDENTIALS_LOCATION
 
 # $2 is optional skipWarning argument
 if [ "$2" != "skipWarning" ]; then
@@ -44,10 +52,13 @@ if [ "$2" != "skipWarning" ]; then
     done
 fi
 
-# move file from the archives dir to the root
-ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html public_html_$TIMESTAMP"
-ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html_tmp public_html"
+# copy the remote script that will create tmp dirs, unzip the new deployment etc to the upload dir (archives)
+curl --netrc-file $CREDENTIALS_LOCATION --ftp-create-dirs -T "./scripts/release/switchReleaseRemote.sh" ftp://ag-grid.com/
+# move prepareNewDeploymentRemote from the archives dir to the root, and make it executable
+ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html/archive/switchReleaseRemote.sh ./"
+ssh -i $SSH_LOCATION ceolter@ag-grid.com "chmod +x ./switchReleaseRemote.sh"
 
-# we don't copy the archives - it's too big
-ssh -i $SSH_LOCATION ceolter@ag-grid.com "mv public_html_$TIMESTAMP/archive public_html/"
-
+# backup the old public_html, unzip the new release and update permissions etc
+# we do this via a remote script as there are many steps and doing so one by one remotely times out occasionally
+echo "cd /home/ceolter/ && ./switchReleaseRemote.sh $TIMESTAMP"
+ssh -i $SSH_LOCATION ceolter@ag-grid.com "cd /home/ceolter/ && ./switchReleaseRemote.sh $TIMESTAMP"
