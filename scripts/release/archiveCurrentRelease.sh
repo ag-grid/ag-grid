39b2eada0839cdc23a4e0f88437a495ac691b4cf
Sean Landsman <sean@thelandsmans.com>
2021-03-10 11:05:04 +0000

    AG-5138 One click deployment

--- scripts/release/archiveCurrentRelease.sh
+++ scripts/release/archiveCurrentRelease.sh
@@ -20,7 +20,9 @@ fi
 checkFileExists $SSH_LOCATION
 
 TIMESTAMP=`date +%Y%m%d`
+ARCHIVE_FILENAME="agGridCom_$TIMESTAMP.zip"
 
-ssh -i $SSH_LOCATION ceolter@ag-grid.com "zip -r --exclude=*archive* agGridCom_$TIMESTAMP.zip public_html"
+# archive the current public_html if the archive doesn't already exist (to take account of multiple deployments on the same day)
+ssh -i $SSH_LOCATION ceolter@ag-grid.com "if [ -f "$ARCHIVE_FILENAME" ]; then echo \"$ARCHIVE_FILENAME exists - skipping archive\"; else \"zip -r --exclude=*archive* $ARCHIVE_FILENAME public_html\"; fi"
 
 
