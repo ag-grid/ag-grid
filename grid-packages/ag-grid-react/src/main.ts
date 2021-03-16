53bcbe61942a87488fcf99f8085050ffb58b9645
ceolter <0872393174Aa>
2021-03-16 11:34:29 +0000

    react-changes

--- grid-packages/ag-grid-react/src/main.ts
+++ grid-packages/ag-grid-react/src/main.ts
@@ -1,3 +1,4 @@
 export {AgGridReact} from './agGridReact';
 export {AgGridColumn} from './agGridColumn';
+export {AgGridReactNext} from './agGridReactNext';
 export {ChangeDetectionStrategyType} from './changeDetectionService';
