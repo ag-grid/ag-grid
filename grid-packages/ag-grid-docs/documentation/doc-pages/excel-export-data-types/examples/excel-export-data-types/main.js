22f7e36da18b997022759f2ef840af3d51385b27
robertclarke <rmc.software.consultancy@gmail.com>
2021-04-20 17:35:55 +0100

    AG-3390 - improvements to excel export docs

--- grid-packages/ag-grid-docs/documentation/doc-pages/excel-export-data-types/examples/excel-export-data-types/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/excel-export-data-types/examples/excel-export-data-types/main.js
@@ -1,22 +1,58 @@
 const gridOptions = {
     columnDefs: [
-        { headerName: 'As provided', field: "rawValue" },
-        { headerName: 'As boolean', field: "rawValue", cellClass: 'booleanType' },
-        { headerName: 'As string', field: "rawValue", cellClass: 'stringType' },
-        { headerName: 'Date', field: "dateValue", cellClass: 'dateType', minWidth: 220 }
+        { headerName: 'provided', field: 'rawValue' },
+        { headerName: 'number', field: 'rawValue', cellClass: 'numberType' },
+        { headerName: 'currency', field: 'rawValue', cellClass: 'currencyFormat' },
+        { headerName: 'boolean', field: 'rawValue', cellClass: 'booleanType' },
+        { headerName: 'Negative', field: 'negativeValue', cellClass: 'negativeInBrackets' },
+        { headerName: 'string', field: 'rawValue', cellClass: 'stringType' },
+        { headerName: 'Date', field: 'dateValue', cellClass: 'dateType', minWidth: 220 },
     ],
     defaultColDef: {
         flex: 1,
         minWidth: 100,
         resizable: true,
     },
-    excelStyles: [
-        { id: 'booleanType', dataType: 'Boolean' },
-        { id: 'stringType', dataType: 'String' },
-        { id: 'dateType', dataType: 'DateTime' }
+    rowData: [
+        {
+            rawValue: 1,
+            negativeValue: -10,
+            dateValue: '2009-04-20T00:00:00.000',
+        },
     ],
-    rowData: [{rawValue: 1, dateValue: '2009-04-20T00:00:00.000'}],
-    popupParent: document.body,
+    excelStyles: [
+        {
+            id: 'numberType',
+            numberFormat: {
+                format: '0',
+            },
+        },
+        {
+            id: 'currencyFormat',
+            numberFormat: {
+                format: '#,##0.00 €',
+            },
+        },
+        {
+            id: 'negativeInBrackets',
+            numberFormat: {
+                format: '$[blue] #,##0;$ [red](#,##0)',
+            },
+        },
+        {
+            id: 'booleanType',
+            dataType: 'boolean',
+        },
+        {
+            id: 'stringType',
+            dataType: 'String',
+        },
+        {
+            id: 'dateType',
+            dataType: 'DateTime',
+        },
+    ],
+    popupParent: document.body
 };
 
 function onBtExport() {
