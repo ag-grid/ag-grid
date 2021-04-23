bb00811c7ed90b2e80e266fea4b51f6882fbef85
Guilherme Lopes <guilherme@gwm-solutions.com>
2021-04-23 16:46:52 +0100

    AG-5178 - [excel export] export images

--- grid-packages/ag-grid-docs/documentation/doc-pages/excel-export-images/examples/excel-export-row-images/imageUtils.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/excel-export-images/examples/excel-export-row-images/imageUtils.js
@@ -1,72 +1,57 @@
-function createBlob(response) {
-    return response.blob();
-}
+const createBlob = response => response.blob();
 
-function fromBlobToBase64(blob) {
-    return new Promise(function(res) {
-        var reader = new FileReader();
-        reader.onloadend = function() {
-            res(reader.result);
-        };
-        reader.readAsDataURL(blob);
-    });
-}
+const fromBlobToBase64 = blob => new Promise(res => {
+    var reader = new FileReader();
+    reader.onloadend = () => res(reader.result);
+    reader.readAsDataURL(blob);
+});
 
-function createBase64ImageFromURL(url) {
-    return fetch(url).then(createBlob).then(function(blob) {
-        return fromBlobToBase64(blob);
-    });
-}
+const createBase64ImageFromURL = url => fetch(url)
+    .then(createBlob)
+    .then(blob => fromBlobToBase64(blob));
 
-function createBase64ImagesFromURLArray(arr) {
-    var map = new Map();
-    var promiseArray = arr.reduce(function(promises, currentUrl) {
+const createBase64ImagesFromURLArray = arr => {
+    const map = new Map();
+    const promiseArray = arr.reduce((promises, currentUrl) => {
         if (!map.has(currentUrl)) {
             var promise = createBase64ImageFromURL(currentUrl)
-            promise.then(function(base64) {
-                map.set(currentUrl, base64);
-            });
+            promise.then(base64 => map.set(currentUrl, base64));
             promises.push(promise)
         }
 
         return promises;
     }, []);
     
-    return Promise.all(promiseArray).then(function() { return map; });
+    return Promise.all(promiseArray).then(() => map);
 }
 
 // Example specific code
-function createCountryCodeMap(countryCodeObject) {
-    return fetch('https://flagcdn.com/en/codes.json')
-    .then(function(response) { return response.json(); })
-    .then(function(codes) {
-        Object.keys(codes).forEach(function(code) {
-            countryCodeObject[codes[code]] = code;
-        });
+const createCountryCodeMap = countryCodeObject =>fetch('https://flagcdn.com/en/codes.json')
+    .then(response => response.json())
+    .then(codes => {
+        Object.keys(codes).forEach(code => countryCodeObject[codes[code]] = code);
         return countryCodeObject;
-    })
-}
+    });
 
-function createBase64FlagsFromResponse(response, countryCodeObject, base64FlagsObject) {
+const createBase64FlagsFromResponse = (response, countryCodeObject, base64FlagsObject) => {
     var urlPrefix = 'https://flagcdn.com/w20/';
     var extension = '.png';
     return response.json()
-        .then(function(data) {
-            return createCountryCodeMap(countryCodeObject).then(function() {
-                return data.filter(function (rec) { return rec.country != null; });
-            });
-        })
-        .then(function(data) {
-            var urls = data.map(function(rec) { 
-                var countryCode = countryCodeObject[rec.country];
+        .then(data => createCountryCodeMap(countryCodeObject)
+            .then(() => data.filter(rec => rec.country != null))
+        )
+        .then(data => {
+            const urls = data.map(rec => { 
+                const countryCode = countryCodeObject[rec.country];
                 return urlPrefix + countryCode + extension;
-            })
-            return createBase64ImagesFromURLArray(urls).then(function(map) {
-                map.forEach(function(value, key) {
-                    var code = key.replace(urlPrefix, '').replace(extension, '');
+            });
+
+            return createBase64ImagesFromURLArray(urls).then(map => {
+                map.forEach((value, key) => {
+                    const code = key.replace(urlPrefix, '').replace(extension, '');
                     base64FlagsObject[code] = value;
                 });
                 return data;
             });
-        }).then(function(data){ return data; });
+        }).then(data => data)
 }
--- grid-packages/ag-grid-docs/documentation/doc-pages/excel-export-images/examples/excel-export-row-images/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/excel-export-images/examples/excel-export-row-images/main.js
@@ -32,39 +32,40 @@ var gridOptions = {
     },
     defaultExcelExportParams: {
         addImageToCell: function(rowIndex, col, value) {
-            if (col.colId === 'country') {
-                var countryCode = countryCodes[value];
-                return {
-                    image: {
-                        id: countryCode,
-                        base64: base64flags[countryCode],
-                        imageType: 'png',
-                        width: 20,
-                        height: 11,
-                        position: {
-                            offsetX: 30,
-                            offsetY: 5.5
-                        }
+            if (col.colId !== 'country') { return; }
+
+            var countryCode = countryCodes[value];
+            return {
+                image: {
+                    id: countryCode,
+                    base64: base64flags[countryCode],
+                    imageType: 'png',
+                    width: 20,
+                    height: 11,
+                    position: {
+                        offsetX: 30,
+                        offsetY: 5.5
                     }
-                };
-            }
+                }
+            };
         }
-    }
+    },
+    onGridReady: onGridReady
 };
 
-function onBtExport() {
+const onBtExport = () => {
     gridOptions.api.exportDataAsExcel();
+}
+
+const onGridReady = (params) => {
+    fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
+    .then(response => createBase64FlagsFromResponse(response, countryCodes, base64flags))
+    .then(data => params.api.setRowData(data))
 }
 
 // setup the grid after the page has finished loading
 document.addEventListener('DOMContentLoaded', function() {
     var gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
-
-    fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
-        .then(function(response) { return createBase64FlagsFromResponse(response, countryCodes, base64flags)})
-        .then(function(data) {
-            gridOptions.api.setRowData(data);
-        })
 });
 
