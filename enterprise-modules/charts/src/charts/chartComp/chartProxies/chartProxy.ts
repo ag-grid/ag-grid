bb832697fcc0083869d72bfa0ca0735c602bec5c
robertclarke <rmc.software.consultancy@gmail.com>
2021-05-24 13:03:50 +0100

    AG-5099 - fix for setting fills and strokes on pie charts doesn't work for integrated charts

--- enterprise-modules/charts/src/charts/chartComp/chartProxies/chartProxy.ts
+++ enterprise-modules/charts/src/charts/chartComp/chartProxies/chartProxy.ts
@@ -538,6 +538,7 @@ export abstract class ChartProxy<TChart extends Chart, TOptions extends ChartOpt
         });
     }
 
+    // TODO move to standalone
     protected hexToRGBA(hex: string, alpha: string) {
         const r = parseInt(hex.slice(1, 3), 16);
         const g = parseInt(hex.slice(3, 5), 16);
@@ -545,6 +546,7 @@ export abstract class ChartProxy<TChart extends Chart, TOptions extends ChartOpt
         return alpha ? `rgba(${r}, ${g}, ${b}, ${alpha})` : `rgba(${r}, ${g}, ${b})`;
     }
 
+    // TODO move to standalone
     protected changeOpacity(fills: string[], alpha: number) {
         return fills.map(fill => {
             const c = Color.fromString(fill);
