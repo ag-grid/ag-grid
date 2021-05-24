f95b97310284ff3103469a1eedb0cfc650001ad2
Sean Landsman <sean@thelandsmans.com>
2021-05-24 15:35:31 +0100

    AG-5317 [Regression] When filtering using custom React-Hook filter components an exception in thrown

--- grid-packages/ag-grid-docs/documentation/doc-pages/filter-multi/examples/custom-filter/YearFilter_reactFunctional.jsx
+++ grid-packages/ag-grid-docs/documentation/doc-pages/filter-multi/examples/custom-filter/YearFilter_reactFunctional.jsx
@@ -1,12 +1,18 @@
-import React, { forwardRef, useState, useEffect, useImperativeHandle } from 'react';
+import React, {forwardRef, useEffect, useImperativeHandle, useState} from 'react';
 
-export default forwardRef(({ filterChangedCallback }, ref) => {
+export default forwardRef(({filterChangedCallback}, ref) => {
     const [isActive, setIsActive] = useState(false);
+    const [activated, setActivated] = useState(false);
     const toggleFilter = isActive => {
         setIsActive(isActive);
+        setActivated(activated || isActive);
     };
 
-    useEffect(() => filterChangedCallback());
+    useEffect(() => {
+        if(activated) {
+            filterChangedCallback();
+        }
+    }, [isActive, activated]);
 
     const setModel = model => toggleFilter(!!model);
     const isFilterActive = () => isActive;
@@ -32,10 +38,10 @@ export default forwardRef(({ filterChangedCallback }, ref) => {
     return (
         <div class="year-filter">
             <label>
-                <input type="radio" checked={!isActive} onChange={() => toggleFilter(false)} /> All
+                <input type="radio" checked={!isActive} onChange={() => toggleFilter(false)}/> All
             </label>
             <label>
-                <input type="radio" checked={isActive} onChange={() => toggleFilter(true)} /> After 2004
+                <input type="radio" checked={isActive} onChange={() => toggleFilter(true)}/> After 2004
             </label>
         </div>
     );
