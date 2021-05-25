f787d06e36fb9d99d62e881dadd7d8b3b0e7917e
Vitaly Kravchenko <vitalyx@gmail.com>
2021-05-25 20:54:40 +0100

    RTI-555 - Showing bar tooltips can produces an error.

--- charts-packages/ag-charts-community/src/chart/series/cartesian/barSeries.ts
+++ charts-packages/ag-charts-community/src/chart/series/cartesian/barSeries.ts
@@ -753,12 +753,12 @@ export class BarSeries extends CartesianSeries {
         const xAxis = this.getCategoryAxis();
         const yAxis = this.getValueAxis();
         const { yKey } = nodeDatum;
-        const yGroup = yData[nodeDatum.index];
 
-        if (!xKey || !yKey) {
+        if (!xKey || !yKey || !yData.length) {
             return '';
         }
 
+        const yGroup = yData[nodeDatum.index];
         let fillIndex = 0;
         let i = 0;
         let j = 0;
