2f97dd7c103c89afbe727020013ab0f62954f07c
Sean Landsman <sean@thelandsmans.com>
2021-06-14 10:36:31 +0100

    Only call algolia if search term is not empty

--- grid-packages/ag-grid-docs/documentation/src/components/search/Search.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/search/Search.jsx
@@ -16,8 +16,26 @@ const Search = ({ currentFramework }) => {
 
     // It is important to memoise the client, otherwise we end up creating a new one on every re-render, resulting in
     // no caching and multiple repeated queries!
-    const searchClient = useMemo(() =>
+    const algoliaClient = useMemo(() =>
         algoliasearch(process.env.GATSBY_ALGOLIA_APP_ID, process.env.GATSBY_ALGOLIA_SEARCH_KEY), []);
+    const searchClient = useMemo(() => ({
+        ...algoliaClient,
+        search(requests) {
+            if (requests.every(({ params }) => !params.query)) {
+                return Promise.resolve({
+                    results: requests.map(() => ({
+                        hits: [],
+                        nbHits: 0,
+                        nbPages: 0,
+                        page: 0,
+                        processingTimeMS: 0,
+                    })),
+                });
+            }
+
+            return algoliaClient.search(requests);
+        },
+    }), [algoliaClient]);
 
     const indices = [{ name: `ag-grid${isDevelopment() ? '-dev' : ''}_${currentFramework}` }];
 
