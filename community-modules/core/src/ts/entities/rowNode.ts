76f81546e9d7fdb7cfe2927f59ee7d57acf54c88
robertclarke <rmc.software.consultancy@gmail.com>
2021-06-14 18:10:36 +0100

    AG-1153 - code cleanup

--- community-modules/core/src/ts/entities/rowNode.ts
+++ community-modules/core/src/ts/entities/rowNode.ts
@@ -95,7 +95,7 @@ export class RowNode implements IEventEmitter {
     public id: string | undefined;
 
     /** The group data */
-    public groupData: { [key: string]: string; } | null;
+    public groupData: { [key: string]: string | null; } | null;
 
     /** The aggregated data */
     public aggData: any;
