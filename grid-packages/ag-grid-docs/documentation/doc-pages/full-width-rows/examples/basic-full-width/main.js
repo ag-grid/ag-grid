4af6656d57dd939d91acb7c88cb99515530d51ad
Sean Landsman <sean@thelandsmans.com>
2021-06-25 15:23:52 +0100

    AG-5373 Add vue 3 support to docs

--- grid-packages/ag-grid-docs/documentation/doc-pages/full-width-rows/examples/basic-full-width/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/full-width-rows/examples/basic-full-width/main.js
@@ -1,15 +1,15 @@
-var rowData = createData(100, 'body');
-var pinnedTopRowData = createData(3, 'pinned');
-var pinnedBottomRowData = createData(3, 'pinned');
+const rowData = createData(100, 'body');
+const pinnedTopRowData = createData(3, 'pinned');
+const pinnedBottomRowData = createData(3, 'pinned');
 
 function alphabet() {
     return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 }
 
 function createData(count, prefix) {
-    var rowData = [];
-    for (var i = 0; i < count; i++) {
-        var item = {};
+    const rowData = [];
+    for (let i = 0; i < count; i++) {
+        const item = {};
         // mark every third row as full width. how you mark the row is up to you,
         // in this example the example code (not the grid code) looks at the
         // fullWidth attribute in the isFullWidthCell() callback. how you determine
@@ -25,9 +25,9 @@ function createData(count, prefix) {
 }
 
 function getColumnDefs() {
-    var columnDefs = [];
+    const columnDefs = [];
     alphabet().forEach(function(letter) {
-        var colDef = {
+        const colDef = {
             headerName: letter,
             field: letter,
             width: 150
@@ -43,12 +43,12 @@ function getColumnDefs() {
     return columnDefs;
 }
 
-var gridOptions = {
+const gridOptions = {
     columnDefs: getColumnDefs(),
     rowData: rowData,
     pinnedTopRowData: pinnedTopRowData,
     pinnedBottomRowData: pinnedBottomRowData,
-    isFullWidthCell: function(rowNode) {
+    isFullWidthCell: function (rowNode) {
         // in this example, we check the fullWidth attribute that we set
         // while creating the data. what check you do to decide if you
         // want a row full width is up to you, as long as you return a boolean
@@ -57,10 +57,10 @@ var gridOptions = {
     },
     // see AG Grid docs cellRenderer for details on how to build cellRenderers
     // this is a simple function cellRenderer, returns plain HTML, not a component
-    fullWidthCellRenderer: function(params) {
+    fullWidthCellRenderer: function (params) {
         // pinned rows will have node.rowPinned set to either 'top' or 'bottom' - see docs for row pinning
-        var cssClass;
-        var message;
+        let cssClass;
+        let message;
 
         if (params.node.rowPinned) {
             cssClass = 'example-full-width-pinned-row';
@@ -70,20 +70,20 @@ var gridOptions = {
             message = 'Normal full width row at index' + params.rowIndex;
         }
 
-        var eDiv = document.createElement('div');
+        const eDiv = document.createElement('div');
         eDiv.innerHTML = '<div class="' + cssClass + '"><button>Click</button> ' + message + '</div>';
 
-        var eButton = eDiv.querySelector('button');
-        eButton.addEventListener('click', function() {
+        const eButton = eDiv.querySelector('button');
+        eButton.addEventListener('click', function () {
             alert('button clicked');
         });
 
         return eDiv.firstChild;
     },
-    getRowHeight: function(params) {
+    getRowHeight: function (params) {
         // you can have normal rows and full width rows any height that you want
-        var isBodyRow = params.node.rowPinned === undefined;
-        var isFullWidth = params.node.data.fullWidth;
+        const isBodyRow = params.node.rowPinned === undefined;
+        const isFullWidth = params.node.data.fullWidth;
         if (isBodyRow && isFullWidth) {
             return 75;
         }
@@ -91,7 +91,7 @@ var gridOptions = {
 };
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
-    var gridDiv = document.querySelector('#myGrid');
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDiv = document.querySelector('#myGrid');
     new agGrid.Grid(gridDiv, gridOptions);
 });
