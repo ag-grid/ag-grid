6d29f6d6c8515641b946aa482ea1d4e5397b28cb
seanlandsman <sean@thelandsmans.com>
2021-07-13 09:51:33 +0100

    AG-5209 fix build warnings

--- community-modules/vue3/src/VueFrameworkOverrides.ts
+++ community-modules/vue3/src/VueFrameworkOverrides.ts
@@ -12,8 +12,8 @@ export class VueFrameworkOverrides extends VanillaFrameworkOverrides {
     }
 
     /*
-     * vue components are specified in the "components" part of the vue component - as such we need a way to determine if a given component is
-     * within that context - this method provides this
+     * vue components are specified in the "components" part of the vue component - as such we need a way to determine
+     * if a given component is within that context - this method provides this
      * Note: This is only really used/necessary with cellRendererSelectors
      */
     public frameworkComponent(name: string): any {
