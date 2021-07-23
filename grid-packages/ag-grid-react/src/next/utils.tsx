418227b851f11e8e91abcc7bff47abb0b91f34f7
Niall Crosby <niall.crosby@gmail.com>
2021-07-23 13:49:07 +0100

    AG-5156 - core refactor - improved CssClasses

--- grid-packages/ag-grid-react/src/next/utils.tsx
+++ grid-packages/ag-grid-react/src/next/utils.tsx
@@ -9,6 +9,11 @@ export class CssClasses {
     private classesMap: {[name: string]: boolean} = {};
 
     public setClass(className: string, on: boolean): CssClasses {
+        // important to not make a copy if nothing has changed, so react
+        // won't trigger a render cycle on new object instance
+        const nothingHasChanged = this.classesMap[className] == on;
+        if (nothingHasChanged) { return this; }
+
         const res = new CssClasses();
         res.classesMap = {...this.classesMap};
         res.classesMap[className] = on;
