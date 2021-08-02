3b9e5e751e871f31b2fea00dc141186d1169c442
Niall Crosby <niall.crosby@gmail.com>
2021-08-02 14:39:56 +0100

    AG-5156 - reactUI - docs

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/reactui/examples/react-ui/index.jsx
@@ -0,0 +1,110 @@
+'use strict';
+
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from '@ag-grid-community/react';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import { RichSelectModule } from '@ag-grid-enterprise/rich-select';
+import '@ag-grid-community/core/dist/styles/ag-grid.css';
+import '@ag-grid-community/core/dist/styles/ag-theme-alpine.css';
+
+// this is a hook, but we work also with classes
+function MyRenderer(params) {
+    return (
+          <span className="my-renderer">
+            <img src="https://d1yk6z6emsz7qy.cloudfront.net/static/images/loading.gif" className="my-spinner"/>
+              {params.value}
+          </span>
+    );
+}
+
+class GridExample extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            modules: [ClientSideRowModelModule, RangeSelectionModule, RowGroupingModule, RichSelectModule],
+            columnDefs: [
+                {
+                    field: 'athlete'
+                },
+                {
+                    field: 'age',
+                    cellRendererFramework: MyRenderer,
+                },
+                {
+                    field: 'country',
+                },
+                {
+                    field: 'year',
+                },
+                {
+                    field: 'date',
+                },
+                {
+                    field: 'sport',
+                },
+                { field: 'gold' },
+                { field: 'silver' },
+                { field: 'bronze' },
+                { field: 'total' },
+            ],
+            defaultColDef: {
+                resizable: true,
+                sortable: true
+            },
+            rowData: null,
+            frameworkComponents: {
+                'bananas': MyRenderer
+            }
+        };
+    }
+
+    onGridReady = (params) => {
+        this.gridApi = params.api;
+        this.gridColumnApi = params.columnApi;
+
+        const updateData = (data) => {
+            this.setState({ rowData: data });
+        };
+
+        fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+            .then((resp) => resp.json())
+            .then((data) => updateData(data));
+    };
+
+    render() {
+        return (
+            <div style={{ width: '100%', height: '100%' }}>
+                <div
+                    id="myGrid"
+                    style={{
+                        height: '100%',
+                        width: '100%',
+                    }}
+                    className="ag-theme-alpine"
+                >
+                    <AgGridReact
+                        reactUi="true"
+                        animateRows="true"
+                        modules={this.state.modules}
+                        columnDefs={this.state.columnDefs}
+                        defaultColDef={this.state.defaultColDef}
+                        onGridReady={this.onGridReady}
+                        rowGroupPanelShow="always"
+                        enableRangeSelection="true"
+                        rowData={this.state.rowData}
+                        rowSelection="multiple"
+                        suppressRowClickSelection="true"
+                        frameworkComponents={this.state.frameworkComponents}
+                        components={this.state.components}
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+render(<GridExample></GridExample>, document.querySelector('#root'));
--- grid-packages/ag-grid-docs/documentation/doc-pages/reactui/index.md
+++ grid-packages/ag-grid-docs/documentation/doc-pages/reactui/index.md
@@ -19,8 +19,20 @@ our in house rendering engine. AG Grid React UI will have the entire AG Grid UI 
 
 <image-caption src="reactui/resources/before-vs-after.svg" alt="AG Grid React UI Explained" centered="true"></image-caption>
 
-## When will AG Grid React UI be ready?
+## Show Me...
 
-Our React UI is already half done and included in the latest AG Grid release. To enable it, set the property `reactUi=true`. Below is an example AG Grid React using the new React UI.
+A spike of our React UI is included in the latest AG Grid release. To enable it, set the grid property `reactUi=true`. Below is an example of the new React UI.
 
-<grid-example title='Custom Sorting' name='react-ui' type='generated'></grid-example>
+With `reactUi=true`, the grid renders cells using React, and all parent components of the cells are also written in React, all the way back to the client application. This means all the UI from your application down to the cells, including any provided React Cell Renderers, is all now 100% React.
+
+<grid-example title='React UI' name='react-ui' type='react' options=' { "enterprise": true, "showImportsDropdown": false }'></grid-example>
+
+## Do You Use Portals?
+
+No, React UI does not use React Portals. Previous to React UI, when the rendering engine was written in Plain JavaScript, React Portals were used to host instances of provided Cell Renderer's and Cell Editors. This caused issues such as additional `span`'s appear in the DOM to host the portal, and also sometimes flickering or delayed rendering was visible.
+
+<image-caption src="reactui/resources/no-portals.png" alt="React UI - No Portals" centered="true"></image-caption>
+
+## What Does the Spike Cover
+
+
