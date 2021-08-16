d04f59475d5ada1bdddbdbe79831920f6b17eab5
Sean Landsman <sean@thelandsmans.com>
2021-08-16 13:59:14 +0100

    RTI-584 R [Frameworks] - SSRM TX Hierarchy Example broken

--- grid-packages/ag-grid-docs/documentation/doc-pages/server-side-model-transactions/examples/transactions-hierarchy/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/server-side-model-transactions/examples/transactions-hierarchy/main.js
@@ -1,128 +1,132 @@
-var columnDefs = [
-  { field: 'productName', rowGroup: true, hide: true },
-  { field: 'tradeName' },
-  { field: 'value' }
+const columnDefs = [
+    {field: 'productName', rowGroup: true, hide: true},
+    {field: 'tradeName'},
+    {field: 'value'}
 ];
 
-var gridOptions = {
-  defaultColDef: {
-    width: 250,
-    resizable: true
-  },
-  getRowNodeId: function(data) { return data.id; },
-  rowModelType: 'serverSide',
-  serverSideStoreType: 'full',
-  columnDefs: columnDefs,
-  animateRows: true,
-  purgeClosedRowNodes: true,
-  onGridReady: function(params) {
+const gridOptions = {
+    defaultColDef: {
+        width: 250,
+        resizable: true
+    },
+    getRowNodeId: function (data) {
+        return data.id;
+    },
+    rowModelType: 'serverSide',
+    serverSideStoreType: 'full',
+    columnDefs: columnDefs,
+    animateRows: true,
+    purgeClosedRowNodes: true,
+    onGridReady: function (params) {
 
-    setupData();
+        setupData();
 
-    var dataSource = {
-      getRows: function(params2) {
-        // To make the demo look real, wait for 500ms before returning
-        setTimeout(function() {
-          var doingTopLevel = params2.request.groupKeys.length == 0;
+        const dataSource = {
+            getRows: function (params2) {
+                // To make the demo look real, wait for 500ms before returning
+                setTimeout(function () {
+                    const doingTopLevel = params2.request.groupKeys.length == 0;
 
-          if (doingTopLevel) {
-            params2.success({ rowData: products.slice(), rowCount: products.length });
-          } else {
-            var key = params2.request.groupKeys[0];
-            var foundProduct;
-            products.forEach(function(product) {
-              if (product.productName == key) {
-                foundProduct = product;
-              }
-            });
-            params2.success({ rowData: foundProduct.trades });
-          }
+                    if (doingTopLevel) {
+                        params2.success({rowData: products.slice(), rowCount: products.length});
+                    } else {
+                        const key = params2.request.groupKeys[0];
+                        let foundProduct;
+                        products.forEach(function (product) {
+                            if (product.productName == key) {
+                                foundProduct = product;
+                            }
+                        });
+                        params2.success({rowData: foundProduct.trades});
+                    }
 
-        }, 2000);
-      }
-    };
+                }, 2000);
+            }
+        };
 
-    params.api.setServerSideDatasource(dataSource);
-  },
-  getServerSideStoreParams: function(params) {
-    var type = params.level == 0 ? 'partial' : 'full';
-    return {
-      storeType: type
-    };
-  }
+        params.api.setServerSideDatasource(dataSource);
+    },
+    getServerSideStoreParams: function (params) {
+        const type = params.level == 0 ? 'partial' : 'full';
+        return {
+            storeType: type
+        };
+    }
 };
 
-var productsNames = ['Palm Oil', 'Rubber', 'Wool', 'Amber', 'Copper'];
-var products = [];
-var idSequence = 0;
+const productsNames = ['Palm Oil', 'Rubber', 'Wool', 'Amber', 'Copper'];
+let products = [];
+let idSequence = 0;
 
 function createOneTrade() {
-  return { id: idSequence++, tradeName: 'TRD-' + Math.floor(Math.random() * 20000), value: Math.floor(Math.random() * 20000) };
+    return {id: idSequence++, tradeName: 'TRD-' + Math.floor(Math.random() * 20000), value: Math.floor(Math.random() * 20000)};
 }
 
 function setupData() {
-  productsNames.forEach(function(productName) {
-    var product = { id: idSequence++, productName: productName, trades: [] };
-    products.push(product);
-    for (var i = 0; i < 2; i++) {
-      product.trades.push(createOneTrade());
-    }
-  });
+    productsNames.forEach(function (productName) {
+        const product = {id: idSequence++, productName: productName, trades: []};
+        products.push(product);
+        for (let i = 0; i < 2; i++) {
+            product.trades.push(createOneTrade());
+        }
+    });
 }
 
 function onBtNewPalmOil() {
-  var transaction = {
-    route: ['Palm Oil'],
-    add: [createOneTrade()]
-  };
-  var res = gridOptions.api.applyServerSideTransaction(transaction);
-  console.log('New Palm Oil, result = ' + res.status);
+    const transaction = {
+        route: ['Palm Oil'],
+        add: [createOneTrade()]
+    };
+    const res = gridOptions.api.applyServerSideTransaction(transaction);
+    console.log('New Palm Oil, result = ' + res.status);
 }
 
 function onBtNewRubber() {
-  var transaction = {
-    route: ['Rubber'],
-    add: [createOneTrade()]
-  };
-  var res = gridOptions.api.applyServerSideTransaction(transaction);
-  console.log('New Rubber, result = ' + res.status);
+    const transaction = {
+        route: ['Rubber'],
+        add: [createOneTrade()]
+    };
+    const res = gridOptions.api.applyServerSideTransaction(transaction);
+    console.log('New Rubber, result = ' + res.status);
 }
 
 function onBtNewWoolAmber() {
-  var transactions = [];
-  transactions.push({
-    route: ['Wool'],
-    add: [createOneTrade()]
-  });
-  transactions.push({
-    route: ['Amber'],
-    add: [createOneTrade()]
-  });
-  transactions.forEach(function(tx) {
-    var res = gridOptions.api.applyServerSideTransaction(tx);
-    console.log('New ' + tx.route[0] + ', result = ' + res.status);
-  });
+    const transactions = [];
+    transactions.push({
+        route: ['Wool'],
+        add: [createOneTrade()]
+    });
+    transactions.push({
+        route: ['Amber'],
+        add: [createOneTrade()]
+    });
+
+    const api = gridOptions.api;
+    transactions.forEach(function (tx) {
+        const res = api.applyServerSideTransaction(tx);
+        console.log('New ' + tx.route[0] + ', result = ' + res.status);
+    });
 }
 
 function onBtNewProduct() {
-  var transaction = {
-    route: [],
-    add: [{ id: idSequence++, productName: 'Rice', trades: [] }]
-  };
-  var res = gridOptions.api.applyServerSideTransaction(transaction);
-  console.log('New Product, result = ' + res.status);
+    const transaction = {
+        route: [],
+        add: [{id: idSequence++, productName: 'Rice', trades: []}]
+    };
+    const res = gridOptions.api.applyServerSideTransaction(transaction);
+    console.log('New Product, result = ' + res.status);
 }
 
 function onBtStoreState() {
-  var storeState = gridOptions.api.getServerSideStoreState();
-  console.log('Store States:');
-  storeState.forEach(function(state, index) {
-    console.log(index + ' - ' + JSON.stringify(state).replace(/"/g, '').replace(/,/g, ", "));
-  });
+    const storeState = gridOptions.api.getServerSideStoreState();
+    console.log('Store States:');
+    storeState.forEach(function (state, index) {
+        console.log(index + ' - ' + JSON.stringify(state).replace(/"/g, '').replace(/,/g, ", "));
+    });
 }
 
 // setup the grid after the page has finished loading
-document.addEventListener('DOMContentLoaded', function() {
-  var gridDiv = document.querySelector('#myGrid');
-  new agGrid.Grid(gridDiv, gridOptions);
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv = document.querySelector('#myGrid');
+    new agGrid.Grid(gridDiv, gridOptions);
 });
