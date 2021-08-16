5dc4a49f83de02cd23df918ba5807921aaaa3f02
Sean Landsman <sean@thelandsmans.com>
2021-08-16 12:20:25 +0100

    RTI-565 R [Vue3] - Example Don't Load

--- grid-packages/ag-grid-docs/src/example-generation/grid-vanilla-to-vue.ts
+++ grid-packages/ag-grid-docs/src/example-generation/grid-vanilla-to-vue.ts
@@ -394,11 +394,12 @@ function convertDefaultColDef(defaultColDef): string {
 }
 
 const getColumnDefs = (bindings: any, utilFunctions: any[]) => {
+    debugger
     const columnDefs = bindings.parsedColDefs ? convertColumnDefs(JSON5.parse(bindings.parsedColDefs), bindings.components.map(component => component.name)) : null;
-    if(!columnDefs) {
+    if (!columnDefs) {
         const columnDefProperty = bindings.properties.filter(property => property.name === 'columnDefs');
-        if(columnDefProperty && columnDefProperty.length === 1 && columnDefProperty[0].value && utilFunctions.some(func => func.includes(`window.${columnDefProperty[0].value}`.replace("()", "")))) {
-            return `${columnDefProperty[0].value}`;
+        if (columnDefProperty && columnDefProperty.length === 1 && columnDefProperty[0].value) {
+            return columnDefProperty[0].value;
         }
 
         return [];
@@ -411,7 +412,8 @@ export function vanillaToVue(bindings: any, componentFileNames: string[]): (impo
     const onGridReady = getOnGridReadyCode(bindings);
     const eventAttributes = bindings.eventHandlers.filter(event => event.name !== 'onGridReady').map(toOutput);
     const [eventHandlers, externalEventHandlers, instanceMethods, utilFunctions] = getAllMethods(bindings);
-    const columnDefs = getColumnDefs(bindings, utilFunctions);    const defaultColDef = bindings.defaultColDef ? convertDefaultColDef(bindings.defaultColDef) : null;
+    const columnDefs = getColumnDefs(bindings, utilFunctions);
+    const defaultColDef = bindings.defaultColDef ? convertDefaultColDef(bindings.defaultColDef) : null;
 
     return importType => {
         const imports = getImports(bindings, componentFileNames, importType);
--- grid-packages/ag-grid-docs/src/example-generation/grid-vanilla-to-vue3.ts
+++ grid-packages/ag-grid-docs/src/example-generation/grid-vanilla-to-vue3.ts
@@ -383,8 +383,8 @@ function convertColumnDefs(rawColumnDefs, userComponentNames): string[] {
 
 function convertDefaultColDef(defaultColDef): string {
     return GRID_COMPONENTS.reduce((acc, componentName) => {
-        if(componentName === 'filter') {
-            if(defaultColDef.indexOf('filter: true') === -1) {
+        if (componentName === 'filter') {
+            if (defaultColDef.indexOf('filter: true') === -1) {
                 return acc.replace(componentName, `${componentName}Framework`);
             }
         }
@@ -394,11 +394,14 @@ function convertDefaultColDef(defaultColDef): string {
 }
 
 const getColumnDefs = (bindings: any, utilFunctions: any[]) => {
+    debugger
     const columnDefs = bindings.parsedColDefs ? convertColumnDefs(JSON5.parse(bindings.parsedColDefs), bindings.components.map(component => component.name)) : null;
-    if(!columnDefs) {
+    if (!columnDefs) {
         const columnDefProperty = bindings.properties.filter(property => property.name === 'columnDefs');
-        if(columnDefProperty && columnDefProperty.length === 1 && columnDefProperty[0].value && utilFunctions.some(func => func.includes(`window.${columnDefProperty[0].value}`.replace("()", "")))) {
-            return `${columnDefProperty[0].value}`;
+        if (columnDefProperty && columnDefProperty.length === 1) {
+            if (columnDefProperty && columnDefProperty.length === 1 && columnDefProperty[0].value) {
+                return columnDefProperty[0].value;
+            }
         }
 
         return [];
