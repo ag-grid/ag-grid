9c95d349f13599e19ac692313b3fa418d1b4e98b
Sean Landsman <sean@thelandsmans.com>
2021-08-19 10:45:15 +0100

    26.0.1 Prep

--- enterprise-modules/core/src/licenseManager.ts
+++ enterprise-modules/core/src/licenseManager.ts
@@ -3,7 +3,7 @@ import { MD5 } from './license/md5';
 
 @Bean('licenseManager')
 export class LicenseManager extends BeanStub {
-    private static RELEASE_INFORMATION: string = 'MTYyODc2NTk1NDkwMA==';
+    private static RELEASE_INFORMATION: string = 'MTYyOTM2NTAxNDg4Ng==';
     private static licenseKey: string;
     private watermarkMessage: string | undefined = undefined;
 
