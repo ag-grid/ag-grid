a58b30a2ab680609e2ef9fc0bc86b64c68c85b7b
Vitaly Kravchenko <vitalyx@gmail.com>
2021-08-24 15:37:39 +0100

    AG-3390 - Add more number format tests.

--- charts-packages/ag-charts-community/src/util/numberFormat.test.ts
+++ charts-packages/ag-charts-community/src/util/numberFormat.test.ts
@@ -1,5 +1,5 @@
-import { describe, expect, test } from "@jest/globals";
-import { format, formatDecimalParts, formatNumerals } from "./numberFormat";
+import { describe, expect, test, it } from "@jest/globals";
+import { format, formatDecimalParts, formatNumerals, formatPrefix } from "./numberFormat";
 import { LinearScale } from "../scale/linearScale";
 
 describe('formatDecimalParts', () => {
@@ -20,18 +20,64 @@ describe('formatDecimalParts', () => {
     });
 });
 
+describe('formatPrefix', () => {
+    const f = formatPrefix(',.0', 1e-6);
+    expect(f(0.00042)).toBe('420µ');
+    expect(f(0.0042)).toBe('4,200µ');
+});
+
 describe('format', () => {
-    describe('percent', () => {
-        test('multiply by 100 and add %', () => {
-            const f = format('.0%');
-            expect(f(0.3)).toBe('30%');
-            expect(f(40)).toBe('4000%');
+    describe('fixed decimal', () => {
+        it('should have one decimal point', () => {
+            const f = format('.1f');
+            expect(f(0.1 + 0.2)).toBe('0.3');
         });
     });
+    describe('fixed decimal', () => {
+        it('should have one decimal point', () => {
+            const f = format('.1f');
+            expect(f(0.1 + 0.2)).toBe('0.3');
+        });
+    });
+    describe('rounded percentage', () => {
+        const f = format('.0%');
+        expect(f(0.3)).toBe('30%');
+        expect(f(0.123)).toBe('12%');
+        expect(f(40)).toBe('4000%');
+    });
+    describe('localized fixed-point currency', () => {
+        expect(format('$.2f')(3.5)).toBe('$3.50');
+    });
+    describe('localized fixed-point currency', () => {
+        expect(format('$.2f')(3.5)).toBe('$3.50');
+    });
+    describe('space-filled and signed', () => {
+        // TODO: returns '+420000000000'
+        expect(format('+20')(42)).toBe('                 +42');
+    });
+    describe('dot-filled and centered', () => {
+        // TODO: returns '....420000000000....'
+        expect(format('.^20')(42)).toBe('.........42.........');
+    });
+    describe('prefixed lowercase hexadecimal', () => {
+        expect(format('#x')(48879)).toBe('0xbeef');
+    });
+    describe('grouped thousands with two significant digits', () => {
+        expect(format(',.2r')(4223)).toBe('4,200');
+    });
+    describe('trim insignificant trailing zeros across format types', () => {
+        expect(format('s')(1500)).toBe('1.50000k');
+        expect(format('s')(-1500)).toBe('−1.5k'); // TODO: returns "−1.50000k"
+    });
+    describe('empty type is a shorthand for ~g', () => {
+        expect(format('.2')(42)).toBe('42');
+        expect(format('.2')(4.2)).toBe('4.2');
+        expect(format('.1')(42)).toBe('4e+1');
+        expect(format('.1')(4.2)).toBe('4');
+    });
     describe('SI-prefix', () => {
-        test('Formatted value should have 3 significant digits followed by a unit', () => {
+        it('should have 3 digits followed by a unit', () => {
             const f = format('.3s');
-            console.log(f(43e6));
             expect(f(43e6)).toBe('43.0M');
         });
         test('Test scale format', () => {
@@ -39,4 +85,5 @@ describe('format', () => {
             const f = scale.tickFormat(10, '.3s');
         });
     });
+
 });
