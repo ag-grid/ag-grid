e0cbe7f346bfae34c6aff63d2902791c91a54df7
Niall Crosby <niall.crosby@gmail.com>
2021-09-01 13:50:22 +0100

    AG-5706 React UI - Headers

--- community-modules/core/src/ts/headerRendering/gridHeaderComp.ts
+++ community-modules/core/src/ts/headerRendering/gridHeaderComp.ts
@@ -1,39 +1,15 @@
-import { GridOptionsWrapper } from '../gridOptionsWrapper';
-import { ColumnModel } from '../columns/columnModel';
-import { Autowired, PostConstruct } from '../context/context';
-import { HeaderContainer } from './headerContainer';
-import { Events } from '../events';
+import { Constants } from '../constants/constants';
+import { PostConstruct } from '../context/context';
+import { addOrRemoveCssClass } from '../utils/dom';
 import { Component } from '../widgets/component';
 import { RefSelector } from '../widgets/componentAnnotations';
-import { GridApi } from '../gridApi';
-import { AutoWidthCalculator } from '../rendering/autoWidthCalculator';
-import { Constants } from '../constants/constants';
-import { addOrRemoveCssClass, setDisplayed } from '../utils/dom';
-import { ManagedFocusFeature } from '../widgets/managedFocusFeature';
-import { HeaderNavigationService, HeaderNavigationDirection } from './header/headerNavigationService';
-import { exists } from '../utils/generic';
-import { PinnedWidthService } from "../gridBodyComp/pinnedWidthService";
-import { CenterWidthFeature } from "../gridBodyComp/centerWidthFeature";
-import { CtrlsService } from "../ctrlsService";
-import { KeyCode } from '../constants/keyCode';
-import { FocusService } from '../focusService';
 import { GridHeaderCtrl, IGridHeaderComp } from './gridHeaderCtrl';
+import { HeaderContainer } from './headerContainer';
 
 export class GridHeaderComp extends Component {
 
     private static TEMPLATE = /* html */
-        `<div class="ag-header" role="presentation" unselectable="on">
-            <div class="ag-pinned-left-header" ref="ePinnedLeftHeader" role="presentation"></div>
-            <div class="ag-header-viewport" ref="eHeaderViewport" role="presentation">
-                <div class="ag-header-container" ref="eHeaderContainer" role="rowgroup"></div>
-            </div>
-            <div class="ag-pinned-right-header" ref="ePinnedRightHeader" role="presentation"></div>
-        </div>`;
-
-    @RefSelector('ePinnedLeftHeader') private ePinnedLeftHeader: HTMLElement;
-    @RefSelector('ePinnedRightHeader') private ePinnedRightHeader: HTMLElement;
-    @RefSelector('eHeaderContainer') private eHeaderContainer: HTMLElement;
-    @RefSelector('eHeaderViewport') private eHeaderViewport: HTMLElement;
+        `<div class="ag-header" role="presentation" unselectable="on"/>`;
 
     constructor() {
         super(GridHeaderComp.TEMPLATE);
@@ -44,21 +20,22 @@ export class GridHeaderComp extends Component {
 
         const compProxy: IGridHeaderComp = {
             addOrRemoveCssClass: (cssClassName, on) => this.addOrRemoveCssClass(cssClassName, on),
-            addOrRemoveLeftCssClass: (cssClassName, on) => addOrRemoveCssClass(this.ePinnedLeftHeader, cssClassName, on),
-            addOrRemoveRightCssClass: (cssClassName, on) => addOrRemoveCssClass(this.ePinnedRightHeader, cssClassName, on),
-            setCenterWidth: width => this.eHeaderContainer.style.width = width,
             setHeightAndMinHeight: height => {
                 this.getGui().style.height = height;
                 this.getGui().style.minHeight = height;
-            },
-            setCenterTransform: pos => this.eHeaderContainer.style.transform = pos
+            }
         };
 
         const ctrl = this.createManagedBean(new GridHeaderCtrl());
         ctrl.setComp(compProxy, this.getGui(), this.getFocusableElement());
 
-        this.createManagedBean(new HeaderContainer(this.eHeaderContainer, this.eHeaderViewport, null));
-        this.createManagedBean(new HeaderContainer(this.ePinnedLeftHeader, null, Constants.PINNED_LEFT));
-        this.createManagedBean(new HeaderContainer(this.ePinnedRightHeader, null, Constants.PINNED_RIGHT));
+        const addContainer = (container: HeaderContainer) => {
+            this.createManagedBean(container);
+            this.appendChild(container);
+        };
+
+        addContainer(new HeaderContainer(Constants.PINNED_LEFT));
+        addContainer(new HeaderContainer(null));
+        addContainer(new HeaderContainer(Constants.PINNED_RIGHT));
     }
 }
