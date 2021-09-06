ab6a082eadf54e724fd8f4ef018c6576e4f44b4f
Stephen Cooper <scooperdev@gmail.com>
2021-09-06 15:01:56 +0100

    AG-5559 Update transaction updates

--- community-modules/core/src/ts/interfaces/rowNodeTransaction.ts
+++ community-modules/core/src/ts/interfaces/rowNodeTransaction.ts
@@ -1,7 +1,10 @@
 import { RowNode } from "../entities/rowNode";
 
 export interface RowNodeTransaction {
+    /** Row nodes added */
     add: RowNode[];
+    /** Row nodes removed */
     remove: RowNode[];
+    /** Row nodes updated */
     update: RowNode[];
 }
