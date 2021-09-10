86d19b9342128d9e1194d83326445b5b902ce6d7
Sean Landsman <sean@thelandsmans.com>
2021-09-10 08:17:16 +0100

    AG-5518 removed old cookie banner div AG-5517 - amended cookies policy … AG-5507 - removed facebook and linkedin tracking code AG-5506 - use tagmanager throughout

--- grid-packages/ag-grid-docs/src/includes/html-helpers.php
+++ grid-packages/ag-grid-docs/src/includes/html-helpers.php
@@ -7,15 +7,11 @@ $GTM_SCRIPT = <<<SCRIPT
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TW77RR3');</script>
+})(window,document,'script','dataLayer','GTM-T7JG534');</script>
 <!-- End Google Tag Manager -->
 SCRIPT;
 
 $GTM_DATA_LAYER = json_encode(array('default' => true));
-
-$LINKEDIN_SCRIPT = <<<SCRIPT
-<script type="text/javascript"> _linkedin_data_partner_id = "302985"; </script><script type="text/javascript"> (function(){var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript";b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s);})(); </script> <noscript> <img height="1" width="1" style="display:none;" alt="" src="https://dc.ads.linkedin.com/collect/?pid=302985&fmt=gif" /> </noscript>
-SCRIPT;
 
 function gtm_data_layer($pageCategory, $additional = array()) {
     $additional['pageCategory'] = $pageCategory;
@@ -87,7 +83,6 @@ META;
     <link rel="apple-touch-icon" sizes="180x180" href="{$prefix}_assets/favicons/favicon-180-touch.png" />
     <link rel="apple-touch-icon" sizes="167x167" href="{$prefix}_assets/favicons/favicon-167-touch.png" />
     <link rel="apple-touch-icon" sizes="152x152" href="{$prefix}_assets/favicons/favicon-152-touch.png" />
-    ${GLOBALS['LINKEDIN_SCRIPT']}
 META;
 }
 
