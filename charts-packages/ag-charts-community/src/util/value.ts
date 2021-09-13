f1173e80ecae891936d9ffdcb077fbb6e582331d
Vitaly Kravchenko <vitalyx@gmail.com>
2021-09-13 16:01:06 +0100

    AG-3116 - More data validation methods.

--- charts-packages/ag-charts-community/src/util/value.ts
+++ charts-packages/ag-charts-community/src/util/value.ts
@@ -13,18 +13,34 @@ export function isNumber(n: any): boolean {
     return n - n < 1;
 }
 
-export function isContinuous(value: any): boolean {
-    return isNumber(value) || isDate(value) || isNumberValue(value);
+export function isNumberObject(value: any): boolean {
+    return !!value && value.valueOf && isNumber(value.valueOf());
 }
 
-export function isComparable(value: any): boolean {
-    return isContinuous(value) || (typeof value === 'string') && value >= value;
+export function isNumeric(value: any): boolean {
+    return isNumber(value) || isNumberObject(value);
 }
 
 export function isDate(value: any): boolean {
     return value instanceof Date && !isNaN(+value);
 }
 
-export function isNumberValue(value: any): boolean {
-    return !!value && value.valueOf && isNumber(value.valueOf());
+export function isString(value: any): boolean {
+    return typeof value === 'string';
+}
+
+export function isStringObject(value: any): boolean {
+    return !!value && value.toString && isString(value.toString());
+}
+
+export function isDiscrete(value: any): boolean {
+    return isString(value) || isStringObject(value);
+}
+
+export function isContinuous(value: any): boolean {
+    return isNumeric(value) || isDate(value);
+}
+
+export function isComparable(value: any): boolean {
+    return isContinuous(value) || isDiscrete(value) && value >= value;
 }
