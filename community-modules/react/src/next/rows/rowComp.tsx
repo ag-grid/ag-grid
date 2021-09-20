d7daa81fa1f5faed34e4ec13640730fcd03fa77a
Niall Crosby <niall.crosby@gmail.com>
2021-09-20 15:20:58 +0100

    AG-5661 Bug with reactUI and the getRowStyle callback

--- community-modules/react/src/next/rows/rowComp.tsx
+++ community-modules/react/src/next/rows/rowComp.tsx
@@ -117,7 +117,7 @@ const RowComp = (params: {rowCtrl: RowCtrl, pinned: string | null}) => {
             transform,
             display
         };
-        _.assign(rowStyles, userStyles);
+        _.assign(res, userStyles);
         return res;
     }, [height, top, transform, userStyles, display]);
 
