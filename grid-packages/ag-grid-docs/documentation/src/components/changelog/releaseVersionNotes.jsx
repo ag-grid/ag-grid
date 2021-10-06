f3644fc9a858e04f5af7957bd44d9400779e5079
Guilherme Lopes <guilherme@gwm-solutions.com>
2021-10-06 16:37:10 -0300

    AG-3390 - removed unused import

--- grid-packages/ag-grid-docs/documentation/src/components/changelog/releaseVersionNotes.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/changelog/releaseVersionNotes.jsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useEffect } from "react"
+import React, { forwardRef } from "react"
 import styles from "./Changelog.module.scss"
 
 const ReleaseVersionNotes = forwardRef((props, ref) => {
