ba21672a1b39f83b05de239540e39fc799f8312b
Mana <mana.jalali.15@ucl.ac.uk>
2021-10-25 17:10:27 +0100

    AG-5649 - Bar/column sparkline labels

---
+++ enterprise-modules/sparklines/src/sparkline/label/label.ts
@@ -0,0 +1,19 @@
+import { FontStyle, FontWeight, getFont } from "../../scene/shape/text";
+import { Observable, reactive } from "../../util/observable";
+
+export class Label extends Observable {
+    @reactive('change', 'dataChange') enabled = true;
+    @reactive('change') fontSize = 12;
+    @reactive('change') fontFamily = 'Verdana, sans-serif';
+    @reactive('change') fontStyle?: FontStyle;
+    @reactive('change') fontWeight?: FontWeight;
+    @reactive('change') color = 'rgba(70, 70, 70, 1)';
+
+    getFont(): string {
+        return getFont(this.fontSize, this.fontFamily, this.fontStyle, this.fontWeight);
+    }
+
+    constructor() {
+        super();
+    }
+}
