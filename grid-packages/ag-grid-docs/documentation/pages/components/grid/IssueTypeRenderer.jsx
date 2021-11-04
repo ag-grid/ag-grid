73636585e6bca2b1cfc771b4d5cad4732e6b8d60
Louis <louis.moore@ag-grid.com>
2021-11-04 10:47:54 +0000

    AG-5912 & AG-5911 fixed the alignment of the icons

--- grid-packages/ag-grid-docs/documentation/pages/components/grid/IssueTypeRenderer.jsx
+++ grid-packages/ag-grid-docs/documentation/pages/components/grid/IssueTypeRenderer.jsx
@@ -8,9 +8,9 @@ const taskIcon = "/issue-type-icons/task-icon.svg"
 const PaddingCellRenderer = forwardRef((props, ref) => {
     const icon = props.valueFormatted === 'Defect' ? bugIcon : taskIcon 
 
-    return <>{!IS_SSR && <div ref={ref} style={{fontSize: "16px"}}>
-        <img alt={"issue type icon"} src={icon}></img> {' '}
-        <span>
+    return <>{!IS_SSR && <div ref={ref} style={{fontSize: "16px", display: 'flex', alignContent: "center"}}>
+        <img style={{display:'flex'}} alt={"issue type icon"} src={icon}></img> {' '}
+        <span style={{display: 'flex'}}>
         {props.valueFormatted}
         </span>
         </div>}</>
