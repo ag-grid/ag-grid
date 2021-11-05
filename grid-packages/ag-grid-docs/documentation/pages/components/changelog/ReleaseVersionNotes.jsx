1e214fbf444da330fc36bd96078248b756926e96
Louis <louis.moore@ag-grid.com>
2021-11-05 16:30:46 +0000

    AG-5910 AG-5911 AG-5912 added padding and chevron

--- grid-packages/ag-grid-docs/documentation/pages/components/changelog/ReleaseVersionNotes.jsx
+++ grid-packages/ag-grid-docs/documentation/pages/components/changelog/ReleaseVersionNotes.jsx
@@ -1,7 +1,34 @@
-import React from "react"
+import React, { useState } from "react"
 import styles from "./Changelog.module.scss"
+import chevronStyle from "../grid/ChevronButtonRenderer.module.scss"
 
-const ReleaseVersionNotes = props =>
-    props.releaseNotes ? (<div className={styles["note"]} dangerouslySetInnerHTML={{__html: props.releaseNotes}}></div>) : null
+const TreeOpen = "/theme-icons/alpine/tree-open.svg"
+const TreeClosed = "/theme-icons/alpine/tree-closed.svg"
+
+const ReleaseVersionNotes = props =>{
+let [releaseNoteVisibility, setReleaseNoteVisibility] = useState('block')
+let [chevronState, setChevronState] = useState(TreeOpen)
+
+   return( props.releaseNotes ? (
+    <div>
+        <button className={styles['release-notes-collapsible']}
+            onClick={()=>{
+                            if(releaseNoteVisibility === 'block'){
+                                setReleaseNoteVisibility('none')
+                                setChevronState(TreeClosed)
+                            }else{
+                                setReleaseNoteVisibility('block')
+                                setChevronState(TreeOpen)
+                            }
+                        }
+                    }   
+        >Release Notes
+            <div className={chevronStyle['chevron-container']} style={{marginLeft: '5px'}}>
+                <img alt={"release notes show/hide toggle"} src={chevronState} style={{width: '20px', height: '20px'}} className={chevronStyle['chevron-img']}></img>
+            </div>
+        </button>   
+        <div style={{display: releaseNoteVisibility, marginTop: '1px'}} className={styles["note"]} dangerouslySetInnerHTML={{__html: props.releaseNotes}}></div>
+    </div>
+    ) : null)}
 
 export default ReleaseVersionNotes
--- grid-packages/ag-grid-docs/documentation/pages/components/grid/IssueTypeRenderer.jsx
+++ grid-packages/ag-grid-docs/documentation/pages/components/grid/IssueTypeRenderer.jsx
@@ -9,7 +9,7 @@ const PaddingCellRenderer = forwardRef((props, ref) => {
     const icon = props.valueFormatted === 'Defect' ? bugIcon : taskIcon 
 
     return <>{!IS_SSR && <div ref={ref} style={{fontSize: "16px", display: 'flex', alignContent: "center"}}>
-        <img style={{display:'flex'}} alt={"issue type icon"} src={icon}></img> {' '}
+        <img style={{display:'flex', paddingRight: "5px"}} alt={"issue type icon"} src={icon}></img>
         <span style={{display: 'flex'}}>
         {props.valueFormatted}
         </span>
