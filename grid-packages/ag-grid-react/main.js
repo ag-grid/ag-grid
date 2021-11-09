2f75dc829b8a5768989371f47a3458cef5e44809
seanlandsman <sean@thelandsmans.com>
2021-11-09 16:32:29 +0000

    AG-5962 React UI - Filters

--- grid-packages/ag-grid-react/main.js
+++ grid-packages/ag-grid-react/main.js
@@ -1,4 +1,4 @@
 exports.AgGridReact = require('./lib/agGridReact').AgGridReact;
 exports.AgGridColumn = require('./lib/shared/agGridColumn').AgGridColumn;
 exports.ChangeDetectionStrategyType = require('./lib/shared/changeDetectionService').ChangeDetectionStrategyType;
-exports.AgGridReactUi = require('./lib/reactUi/AgGridReactUi').AgGridReactUi;
+exports.AgGridReactUi = require('./lib/reactUi/agGridReactUi').AgGridReactUi;
