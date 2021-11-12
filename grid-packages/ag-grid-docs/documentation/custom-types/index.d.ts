365868c350cacb1b64690ef60b555b1d4b03fe32
Stephen Cooper <scooperdev@gmail.com>
2021-11-12 11:26:06 +0000

    Run tasks in line for checking and conversion

---
+++ grid-packages/ag-grid-docs/documentation/custom-types/index.d.ts
@@ -0,0 +1,8 @@
+// By providing this type we do not have to add a declare to every example file for the new agGrid.Grid method
+
+// See to explain the inline imports in the type definition
+// https://stackoverflow.com/questions/39040108/import-class-in-definition-file-d-ts 
+
+declare const agGrid: {
+    Grid: (eGridDiv: Element | null, gridOptions: import("@ag-grid-community/core").GridOptions, params?: import("@ag-grid-community/core").GridParams) => void
+};
