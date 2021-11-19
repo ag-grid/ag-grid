3b2975b9bb2da0173e839c6c6367cf7737ba4466
Stephen Cooper <scooperdev@gmail.com>
2021-11-19 11:14:35 +0000

    AG-6004 Convert clipboard

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/clipboard/examples/cellTextSelection/main.ts
@@ -0,0 +1,36 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'athlete', minWidth: 200 },
+    { field: 'age' },
+    { field: 'country', minWidth: 150 },
+    { field: 'year' },
+    { field: 'date', minWidth: 150 },
+    { field: 'sport', minWidth: 150 },
+    { field: 'gold' },
+    { field: 'silver', suppressPaste: true },
+    { field: 'bronze' },
+    { field: 'total' },
+  ],
+
+  defaultColDef: {
+    editable: true,
+    flex: 1,
+    minWidth: 100,
+    resizable: true,
+  },
+
+  ensureDomOrder: true,
+  enableCellTextSelection: true,
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/clipboard/examples/custom/main.ts
@@ -0,0 +1,51 @@
+import { GridOptions, SendToClipboardParams } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'athlete', minWidth: 200 },
+    { field: 'age' },
+    { field: 'country', minWidth: 150 },
+    { field: 'year' },
+    { field: 'date', minWidth: 150 },
+    { field: 'sport', minWidth: 150 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+  ],
+
+  defaultColDef: {
+    editable: true,
+    flex: 1,
+    minWidth: 100,
+    resizable: true,
+  },
+
+  enableRangeSelection: true,
+  rowSelection: 'multiple',
+
+  sendToClipboard: sendToClipboard,
+}
+
+function sendToClipboard(params: SendToClipboardParams) {
+  console.log('send to clipboard called with data:')
+  console.log(params.data)
+}
+
+function onBtCopyRows() {
+  gridOptions.api!.copySelectedRowsToClipboard()
+}
+
+function onBtCopyRange() {
+  gridOptions.api!.copySelectedRangeToClipboard()
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
