84f422b8d12f28fd6e9e9e87fc09c89bc41da443
Stephen Cooper <scooperdev@gmail.com>
2021-11-22 16:09:41 +0000

    AG-6004 Convert column-groups, except advanced

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/column-groups/examples/marry-children/main.ts
@@ -0,0 +1,44 @@
+import { ColDef, ColGroupDef, GridOptions } from '@ag-grid-community/core'
+
+const columnDefs: (ColDef | ColGroupDef)[] = [
+  {
+    headerName: 'Athlete Details',
+    marryChildren: true,
+    children: [
+      { field: 'athlete', colId: 'athlete' },
+      { field: 'country', colId: 'country' },
+    ],
+  },
+  { field: 'age', colId: 'age' },
+  {
+    headerName: 'Sports Results',
+    marryChildren: true,
+    children: [
+      { field: 'sport', colId: 'sport' },
+      { field: 'total', colId: 'total' },
+      { field: 'gold', colId: 'gold' },
+      { field: 'silver', colId: 'silver' },
+      { field: 'bronze', colId: 'bronze' },
+    ],
+  },
+]
+
+const gridOptions: GridOptions = {
+  defaultColDef: {
+    resizable: true,
+    width: 160,
+  },
+  debug: true,
+  columnDefs: columnDefs,
+  rowData: null,
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
