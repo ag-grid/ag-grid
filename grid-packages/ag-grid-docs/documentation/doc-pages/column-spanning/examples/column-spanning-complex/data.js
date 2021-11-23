9e2a4a010183b1472d11203ddcf85332f641de77
Stephen Cooper <scooperdev@gmail.com>
2021-11-23 15:02:49 +0000

    AG-6004 Convert column-spanning

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/column-spanning/examples/column-spanning-complex/data.js
@@ -0,0 +1,21 @@
+function getData() {
+    var rowData = [
+        { section: 'big-title', jan: 'Warehouse 1' },
+        { section: 'quarters', jan: 'Q1', apr: 'Q2' },
+        { jan: 534, feb: 612, mar: 243, apr: 231, may: 428, jun: 231 },
+        { jan: 765, feb: 146, mar: 243, apr: 231, may: 428, jun: 231 },
+        { jan: 335, feb: 122, mar: 243, apr: 231, may: 428, jun: 231 },
+        { jan: 35, feb: 342, mar: 243, apr: 231, may: 428, jun: 231 },
+        { jan: 568, feb: 531, mar: 243, apr: 231, may: 428, jun: 231 },
+        { jan: 365, feb: 361, mar: 243, apr: 231, may: 428, jun: 231 },
+        { section: 'big-title', jan: 'Warehouse 2' },
+        { section: 'quarters', jan: 'Q1', apr: 'Q2' },
+        { jan: 21, feb: 12, mar: 24, apr: 31, may: 28, jun: 31 },
+        { jan: 21, feb: 12, mar: 24, apr: 31, may: 28, jun: 31 },
+        { jan: 21, feb: 12, mar: 24, apr: 31, may: 28, jun: 31 },
+        { jan: 21, feb: 12, mar: 24, apr: 31, may: 28, jun: 31 },
+        { jan: 2, feb: 32, mar: 24, apr: 31, may: 48, jun: 21 },
+        { jan: 21, feb: 12, mar: 24, apr: 31, may: 28, jun: 31 },
+    ]
+    return rowData;
+}
---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/column-spanning/examples/column-spanning-complex/main.ts
@@ -0,0 +1,72 @@
+import { CellClassRules, ColDef, ColSpanParams, GridOptions, RowHeightParams } from '@ag-grid-community/core'
+
+var cellClassRules: CellClassRules = {
+  'header-cell': 'data.section === "big-title"',
+  'quarters-cell': 'data.section === "quarters"',
+}
+
+const columnDefs: ColDef[] = [
+  {
+    headerName: 'Jan',
+    field: 'jan',
+    colSpan: function (params: ColSpanParams) {
+      if (isHeaderRow(params)) {
+        return 6
+      } else if (isQuarterRow(params)) {
+        return 3
+      } else {
+        return 1
+      }
+    },
+    cellClassRules: cellClassRules,
+  },
+  { headerName: 'Feb', field: 'feb' },
+  { headerName: 'Mar', field: 'mar' },
+  {
+    headerName: 'Apr',
+    field: 'apr',
+    colSpan: function (params) {
+      if (isQuarterRow(params)) {
+        return 3
+      } else {
+        return 1
+      }
+    },
+    cellClassRules: cellClassRules,
+  },
+  { headerName: 'May', field: 'may' },
+  { headerName: 'Jun', field: 'jun' },
+]
+
+const gridOptions: GridOptions = {
+  getRowHeight: function (params) {
+    if (isHeaderRow(params)) {
+      return 60
+    }
+  },
+  columnDefs: columnDefs,
+  rowData: getData(),
+  defaultColDef: {
+    width: 100,
+  },
+  onGridReady: function (params) {
+    params.api.sizeColumnsToFit()
+  },
+}
+
+
+function isHeaderRow(params: RowHeightParams | ColSpanParams) {
+  return params.data.section === 'big-title'
+}
+
+function isQuarterRow(params: ColSpanParams) {
+  return params.data.section === 'quarters'
+}
+
+
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+})
