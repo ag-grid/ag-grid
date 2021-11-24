b6744bc2f569bba61656480ddb680b0db53a9dd1
Stephen Cooper <scooperdev@gmail.com>
2021-11-24 16:21:17 +0000

    AG-6004 Convert group-*

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grouping-single-group-column/examples/remove-single-children/main.ts
@@ -0,0 +1,62 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'athlete' },
+    { field: 'country', rowGroup: true },
+    { field: 'city', rowGroup: true },
+    { field: 'year' },
+    { field: 'gold', aggFunc: 'sum' },
+    { field: 'silver', aggFunc: 'sum' },
+    { field: 'bronze', aggFunc: 'sum' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 150,
+    resizable: true,
+  },
+  autoGroupColumnDef: {
+    headerName: 'Group',
+    field: 'athlete',
+    minWidth: 220,
+    cellRenderer: 'agGroupCellRenderer',
+  },
+  rowData: getData(),
+
+  // optional as 'singleColumn' is the default group display type
+  groupDisplayType: 'singleColumn',
+
+  // set this to true to remove single children
+  groupRemoveSingleChildren: false,
+
+  // set this to true to remove leaf level single children
+  groupRemoveLowestSingleChildren: false,
+
+  // expand everything by default
+  groupDefaultExpanded: -1,
+
+  suppressAggFuncInHeader: true,
+  animateRows: true,
+}
+
+function changeSelection(type: string) {
+  // normal, single or lowest
+  if (type === 'normal') {
+    gridOptions.api!.setGroupRemoveSingleChildren(false)
+    gridOptions.api!.setGroupRemoveLowestSingleChildren(false)
+  } else if (type === 'single') {
+    gridOptions.api!.setGroupRemoveSingleChildren(true)
+    gridOptions.api!.setGroupRemoveLowestSingleChildren(false)
+  } else if (type === 'lowest') {
+    gridOptions.api!.setGroupRemoveLowestSingleChildren(true)
+    gridOptions.api!.setGroupRemoveSingleChildren(false)
+  } else {
+    console.log('unknown type: ' + type)
+  }
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+})
