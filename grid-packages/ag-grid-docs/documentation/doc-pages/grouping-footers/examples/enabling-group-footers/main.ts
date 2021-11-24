98bb582d177e3f723c1daa84b907c32f3392f733
Stephen Cooper <scooperdev@gmail.com>
2021-11-24 15:21:48 +0000

    AG-6004 Convert group-footers group-rows

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grouping-footers/examples/enabling-group-footers/main.ts
@@ -0,0 +1,30 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'country', rowGroup: true, hide: true },
+    { field: 'year', rowGroup: true, hide: true },
+    { field: 'gold', aggFunc: 'sum' },
+    { field: 'silver', aggFunc: 'sum' },
+    { field: 'bronze', aggFunc: 'sum' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 150,
+    sortable: true,
+    resizable: true,
+  },
+  autoGroupColumnDef: {
+    minWidth: 300,
+  },
+  groupIncludeFooter: true,
+  groupIncludeTotalFooter: true,
+  animateRows: true,
+  rowData: getData(),
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+})
