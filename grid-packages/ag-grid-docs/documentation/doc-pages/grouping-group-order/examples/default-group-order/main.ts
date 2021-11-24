7af49210312f9eab7d7cb4b27e9909d3fc9b422b
Stephen Cooper <scooperdev@gmail.com>
2021-11-24 15:37:51 +0000

    AG-6004 Convert group-*

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grouping-group-order/examples/default-group-order/main.ts
@@ -0,0 +1,42 @@
+import { GridOptions, RowNode } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'country', rowGroup: true, hide: true },
+    { field: 'year' },
+    { field: 'sport', rowGroup: true, hide: true },
+    { field: 'athlete', minWidth: 200 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+    { field: 'age' },
+    { field: 'date', minWidth: 140 },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 100,
+    resizable: true,
+    sortable: true,
+  },
+  autoGroupColumnDef: {
+    minWidth: 200,
+  },
+  groupDisplayType: 'groupRows',
+  defaultGroupOrderComparator: function (nodeA: RowNode, nodeB: RowNode) {
+    const a = nodeA.key || ''
+    const b = nodeB.key || ''
+    return a < b ? -1 : a > b ? 1 : 0
+  },
+  animateRows: true,
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grouping-group-order/examples/row-group-order/main.ts
@@ -0,0 +1,35 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'country', rowGroupIndex: 1, hide: true },
+    { field: 'year', rowGroupIndex: 0, hide: true },
+    { field: 'sport' },
+    { field: 'athlete' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 100,
+    sortable: true,
+    resizable: true,
+  },
+  autoGroupColumnDef: {
+    minWidth: 250,
+  },
+  // optional as 'singleColumn' is the default group display type
+  groupDisplayType: 'singleColumn',
+  animateRows: true,
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
