8496f2804b8fd7606f2db5eece7fdd226c3f5e12
Stephen Cooper <scooperdev@gmail.com>
2021-11-25 10:12:00 +0000

    AG-6004 Convert infinite-scroll

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/infinite-scrolling/examples/made-up-data/main.ts
@@ -0,0 +1,67 @@
+import { ColDef, GridOptions, IDatasource, IGetRowsParams } from '@ag-grid-community/core'
+
+var ALPHABET = 'abcdefghijklmnopqrstuvwxyz'.split('')
+
+function getColumnDefs() {
+  const columnDefs: ColDef[] = [
+    { checkboxSelection: true, headerName: '', width: 60 },
+    { headerName: '#', width: 80, valueGetter: 'node.rowIndex' },
+  ]
+
+  ALPHABET.forEach(function (letter) {
+    columnDefs.push({
+      headerName: letter.toUpperCase(),
+      field: letter,
+      width: 150,
+    })
+  })
+  return columnDefs
+}
+
+const gridOptions: GridOptions = {
+  columnDefs: getColumnDefs(),
+  defaultColDef: {
+    resizable: true,
+  },
+  rowModelType: 'infinite',
+  rowSelection: 'multiple',
+  maxBlocksInCache: 2,
+  suppressRowClickSelection: true,
+  getRowNodeId: function (item) {
+    return item.a
+  },
+  datasource: getDataSource(100),
+}
+
+function getDataSource(count: number) {
+
+  const dataSource: IDatasource = {
+    rowCount: count,
+    getRows: function (params: IGetRowsParams) {
+      var rowsThisPage: any[] = []
+
+      for (var rowIndex = params.startRow; rowIndex < params.endRow; rowIndex++) {
+        var record: Record<string, string> = {}
+        ALPHABET.forEach(function (letter, colIndex) {
+          var randomNumber = 17 + rowIndex + colIndex
+          var cellKey = letter.toUpperCase() + (rowIndex + 1)
+          record[letter] = cellKey + ' = ' + randomNumber
+        })
+        rowsThisPage.push(record)
+      }
+
+      // to mimic server call, we reply after a short delay
+      setTimeout(function () {
+        // no need to pass the second 'rowCount' parameter as we have already provided it
+        params.successCallback(rowsThisPage)
+      }, 100)
+    }
+
+  }
+  return dataSource;
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+})
