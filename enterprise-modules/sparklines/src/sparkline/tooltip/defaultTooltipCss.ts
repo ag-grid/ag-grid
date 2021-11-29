c93cf45e424f3dcade7b7545d5ae8226e615e9e8
Mana <mana.jalali.15@ucl.ac.uk>
2021-11-29 13:11:43 +0000

    AG-5836 - Sparkline tooltip

--- enterprise-modules/sparklines/src/sparkline/tooltip/defaultTooltipCss.ts
+++ enterprise-modules/sparklines/src/sparkline/tooltip/defaultTooltipCss.ts
@@ -1,28 +1,30 @@
 export const defaultTooltipCss = `
-.ag-sparkline-tooltip {
-    display: table;
+.ag-sparkline-tooltip-wrapper {
     position: absolute;
+    display: table;
     user-select: none;
     pointer-events: none;
     white-space: nowrap;
     z-index: 99999;
+}
+
+.ag-sparkline-tooltip {
     font: 12px arial,sans-serif;
     border-radius: 2px;
     box-shadow: 0 1px 3px rgb(0 0 0 / 20%), 0 1px 1px rgb(0 0 0 / 14%);
+    line-height: 1.7em;
+    overflow: hidden;
 }
 
 .ag-sparkline-tooltip-content {
     padding: 0 7px;
-    line-height: 1.7em;
-    border-radius: 2px;
-    overflow: hidden;
 }
 
 .ag-sparkline-tooltip-title {
-    font-weight: 400;
+    padding-left: 7px;
 }
 
-.ag-sparkline-tooltip-hidden {
+.ag-sparkline-tooltip-wrapper-hidden {
     top: -10000px !important;
 }
 
