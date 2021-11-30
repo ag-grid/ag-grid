2e9e07f6ae200e47c3123b5aad6297c92627cf5a
Stephen Cooper <scooperdev@gmail.com>
2021-11-30 14:21:55 +0000

    AG-6004 Convert pivot

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/pivoting/examples/simple/main.ts
@@ -0,0 +1,34 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'country', rowGroup: true, enableRowGroup: true },
+    { field: 'sport', pivot: true },
+    { field: 'year' },
+    { field: 'date' },
+    { field: 'gold', aggFunc: 'sum' },
+    { field: 'silver', aggFunc: 'sum' },
+    { field: 'bronze', aggFunc: 'sum' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 150,
+    sortable: true,
+    resizable: true,
+  },
+  autoGroupColumnDef: {
+    minWidth: 250,
+  },
+  // set rowData to null or undefined to show loading panel by default
+  pivotMode: true,
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
