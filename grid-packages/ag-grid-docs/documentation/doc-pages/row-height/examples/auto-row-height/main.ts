52171bf4a4ea748c092af6e5b1fece801027a9fc
Stephen Cooper <scooperdev@gmail.com>
2021-11-30 12:13:01 +0000

    AG-6004 Convert row-height

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/row-height/examples/auto-row-height/main.ts
@@ -0,0 +1,66 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    {
+      headerName: 'Row #',
+      field: 'rowNumber',
+      width: 120,
+    },
+    {
+      field: 'autoA',
+      width: 300,
+      wrapText: true,
+      autoHeight: true,
+      headerName: 'A) Auto Height',
+    },
+    {
+      width: 300,
+      field: 'autoB',
+      wrapText: true,
+      headerName: 'B) Normal Height',
+    },
+  ],
+  defaultColDef: {
+    sortable: true,
+    resizable: true,
+  },
+  rowHeight: 150,
+  onGridReady: function (params) {
+    // in this example, the CSS styles are loaded AFTER the grid is created,
+    // so we put this in a timeout, so height is calculated after styles are applied.
+    setTimeout(function () {
+      params.api.setRowData(getData())
+    }, 500)
+  },
+  sideBar: {
+    toolPanels: [
+      {
+        id: 'columns',
+        labelDefault: 'Columns',
+        labelKey: 'columns',
+        iconKey: 'columns',
+        toolPanel: 'agColumnsToolPanel',
+        toolPanelParams: {
+          suppressRowGroups: true,
+          suppressValues: true,
+          suppressPivots: true,
+          suppressPivotMode: true,
+          suppressSideButtons: true,
+          suppressColumnFilter: true,
+          suppressColumnSelectAll: true,
+          suppressColumnExpandAll: true,
+        },
+      },
+    ],
+    defaultToolPanel: 'columns',
+  },
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+})
+
+
