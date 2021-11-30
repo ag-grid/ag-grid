bae63156b0febc0cc69d4df54389b070d0c14835
Stephen Cooper <scooperdev@gmail.com>
2021-11-30 15:02:47 +0000

    AG-6004 Convert printing

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/printing/examples/for-print-simple/main.ts
@@ -0,0 +1,41 @@
+import { ColDef, GridOptions } from '@ag-grid-community/core'
+
+const columnDefs: ColDef[] = [
+  { headerName: 'ID', valueGetter: 'node.rowIndex + 1', width: 70 },
+  { field: 'model', width: 150 },
+  { field: 'color' },
+  { field: 'price', valueFormatter: '"$" + value.toLocaleString()' },
+  { field: 'year' },
+  { field: 'country' },
+]
+
+const gridOptions: GridOptions = {
+  columnDefs: columnDefs,
+  rowData: getData(),
+  defaultColDef: {
+    width: 100,
+  },
+}
+
+function onBtPrinterFriendly() {
+  var eGridDiv = document.querySelector('#myGrid') as any;
+  eGridDiv.style.width = ''
+  eGridDiv.style.height = ''
+
+  gridOptions.api!.setDomLayout('print')
+}
+
+function onBtNormal() {
+  var eGridDiv = document.querySelector('#myGrid') as any;
+  eGridDiv.style.width = '400px'
+  eGridDiv.style.height = '200px'
+
+  // Same as setting to 'normal' as it is the default
+  gridOptions.api!.setDomLayout()
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+})
