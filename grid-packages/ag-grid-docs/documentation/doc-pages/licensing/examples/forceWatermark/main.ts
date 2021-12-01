fcd1c822bb2061063ebb9415ab221b13de7d889b
Stephen Cooper <scooperdev@gmail.com>
2021-12-01 15:18:27 +0000

    AG-6004 Convert licensing

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/licensing/examples/forceWatermark/main.ts
@@ -0,0 +1,46 @@
+import { GridOptions } from '@ag-grid-community/core'
+
+const gridOptions: GridOptions = {
+  // define grid columns
+  columnDefs: [
+    // using default ColDef
+    { headerName: 'Athlete', field: 'athlete' },
+    { headerName: 'Sport', field: 'sport' },
+
+    // using number column type
+    { headerName: 'Age', field: 'age', type: 'numberColumn' },
+    { headerName: 'Year', field: 'year', type: 'numberColumn' },
+
+    // using date and non-editable column types
+    { headerName: 'Date', field: 'date', width: 200 },
+  ],
+
+  defaultColDef: {
+    width: 150,
+  },
+
+  // default ColGroupDef, get applied to every column group
+  defaultColGroupDef: {
+    marryChildren: true,
+  },
+
+  columnTypes: {
+    numberColumn: { width: 83 },
+  },
+
+  rowData: null,
+
+  onGridReady: function (params) {
+    params.api.sizeColumnsToFit()
+  },
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
