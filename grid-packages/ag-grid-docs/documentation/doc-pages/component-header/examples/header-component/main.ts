7b059c0785a2a89a4b7608d12fbbd3dfb4a67318
Stephen Cooper <scooperdev@gmail.com>
2021-12-01 10:03:10 +0000

    AG-6004 Convert component-header

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/component-header/examples/header-component/main.ts
@@ -0,0 +1,56 @@
+import { ColDef, GridOptions } from '@ag-grid-community/core'
+
+declare var CustomHeader: any;
+
+const columnDefs: ColDef[] = [
+  { field: 'athlete', suppressMenu: true, minWidth: 120 },
+  {
+    field: 'age',
+    sortable: false,
+    headerComponentParams: { menuIcon: 'fa-external-link-alt' },
+  },
+  { field: 'country', suppressMenu: true, minWidth: 120 },
+  { field: 'year', sortable: false },
+  { field: 'date', suppressMenu: true },
+  { field: 'sport', sortable: false },
+  {
+    field: 'gold',
+    headerComponentParams: { menuIcon: 'fa-cog' },
+    minWidth: 120,
+  },
+  { field: 'silver', sortable: false },
+  { field: 'bronze', suppressMenu: true, minWidth: 120 },
+  { field: 'total', sortable: false },
+]
+
+const gridOptions: GridOptions = {
+  columnDefs: columnDefs,
+  rowData: null,
+  suppressMenuHide: true,
+  components: {
+    agColumnHeader: CustomHeader,
+  },
+  defaultColDef: {
+    editable: true,
+    sortable: true,
+    flex: 1,
+    minWidth: 100,
+    filter: true,
+    resizable: true,
+    headerComponentParams: {
+      menuIcon: 'fa-bars',
+    },
+  },
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => {
+      gridOptions.api!.setRowData(data)
+    })
+})
