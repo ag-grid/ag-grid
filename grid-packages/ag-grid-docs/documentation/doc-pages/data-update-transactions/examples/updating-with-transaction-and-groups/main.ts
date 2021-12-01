23a0062692d795850ed7be90c538c098a3f55043
Stephen Cooper <scooperdev@gmail.com>
2021-12-01 11:36:01 +0000

    AG-6004 Convert data-updates

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/data-update-transactions/examples/updating-with-transaction-and-groups/main.ts
@@ -0,0 +1,98 @@
+import { GridOptions, ValueFormatterParams } from '@ag-grid-community/core'
+
+declare function createNewRowData(category: string): any;
+
+function poundFormatter(params: ValueFormatterParams) {
+  return (
+    '£' +
+    Math.floor(params.value)
+      .toString()
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+  )
+}
+
+const gridOptions: GridOptions = {
+  columnDefs: [
+    { field: 'category', rowGroupIndex: 1, hide: true },
+    { field: 'price', aggFunc: 'sum', valueFormatter: poundFormatter },
+    { field: 'zombies' },
+    { field: 'style' },
+    { field: 'clothes' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    width: 100,
+    sortable: true,
+  },
+  autoGroupColumnDef: {
+    headerName: 'Group',
+    minWidth: 250,
+    field: 'model',
+    rowGroupIndex: 1,
+    cellRenderer: 'agGroupCellRenderer',
+    cellRendererParams: {
+      checkbox: true,
+    },
+  },
+  groupDefaultExpanded: 1,
+  rowData: getData(),
+  suppressRowClickSelection: true,
+  rowSelection: 'multiple',
+  animateRows: true,
+  groupSelectsChildren: true,
+  suppressAggFuncInHeader: true,
+  // this allows the different colors per group, by assigning a different
+  // css class to each group level based on the key
+  getRowClass: function (params) {
+    var rowNode = params.node
+    if (rowNode.group) {
+      switch (rowNode.key) {
+        case 'In Workshop':
+          return 'category-in-workshop'
+        case 'Sold':
+          return 'category-sold'
+        case 'For Sale':
+          return 'category-for-sale'
+        default:
+          return undefined
+      }
+    } else {
+      // no extra classes for leaf rows
+      return undefined
+    }
+  },
+}
+
+function getRowData() {
+  var rowData: any[] = []
+  gridOptions.api!.forEachNode(function (node) {
+    rowData.push(node.data)
+  })
+  console.log('Row Data:')
+  console.log(rowData)
+}
+
+function onAddRow(category: string) {
+  var rowDataItem = createNewRowData(category)
+  gridOptions.api!.applyTransaction({ add: [rowDataItem] })
+}
+
+function onMoveToGroup(category: string) {
+  var selectedRowData = gridOptions.api!.getSelectedRows()
+  selectedRowData.forEach(function (dataItem) {
+    dataItem.category = category
+  })
+  gridOptions.api!.applyTransaction({ update: selectedRowData })
+}
+
+function onRemoveSelected() {
+  var selectedRowData = gridOptions.api!.getSelectedRows()
+  gridOptions.api!.applyTransaction({ remove: selectedRowData })
+}
+
+// wait for the document to be loaded, otherwise
+// AG Grid will not find the div in the document.
+document.addEventListener('DOMContentLoaded', function () {
+  var eGridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(eGridDiv, gridOptions)
+})
---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/data-update-transactions/examples/updating-with-transaction/data.js
@@ -0,0 +1,28 @@
+function getData() {
+    return [
+        {
+            make: 'Toyota',
+            model: 'Celica',
+            price: 35000,
+            zombies: 'Elly',
+            style: 'Smooth',
+            clothes: 'Jeans',
+        },
+        {
+            make: 'Ford',
+            model: 'Mondeo',
+            price: 32000,
+            zombies: 'Shane',
+            style: 'Filthy',
+            clothes: 'Shorts',
+        },
+        {
+            make: 'Porsche',
+            model: 'Boxter',
+            price: 72000,
+            zombies: 'Jack',
+            style: 'Dirty',
+            clothes: 'Padded',
+        },
+    ]
+}
