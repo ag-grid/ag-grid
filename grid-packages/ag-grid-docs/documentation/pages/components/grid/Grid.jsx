ef2e20501f86ecb538653ad0fe44abdc8b9ef6aa
Louis <louis.moore@ag-grid.com>
2021-12-02 11:00:25 +0000

    AG-5939, AG-5910, AG-5938 + removed status style

--- grid-packages/ag-grid-docs/documentation/pages/components/grid/Grid.jsx
+++ grid-packages/ag-grid-docs/documentation/pages/components/grid/Grid.jsx
@@ -1,10 +1,10 @@
-import React, {forwardRef} from "react"
-import {ClientSideRowModelModule} from "@ag-grid-community/client-side-row-model"
-import {MasterDetailModule} from "@ag-grid-enterprise/master-detail"
-import {SetFilterModule} from "@ag-grid-enterprise/set-filter"
-import {ColumnsToolPanelModule} from "@ag-grid-enterprise/column-tool-panel"
-import {StatusBarModule} from "@ag-grid-enterprise/status-bar"
-import {AgGridReact} from "@ag-grid-community/react"
+import React, { forwardRef } from "react"
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model"
+import { MasterDetailModule } from "@ag-grid-enterprise/master-detail"
+import { SetFilterModule } from "@ag-grid-enterprise/set-filter"
+import { ColumnsToolPanelModule } from "@ag-grid-enterprise/column-tool-panel"
+import { StatusBarModule } from "@ag-grid-enterprise/status-bar"
+import { AgGridReact } from "@ag-grid-community/react"
 
 import "@ag-grid-community/core/dist/styles/ag-grid.css"
 import "@ag-grid-community/core/dist/styles/ag-theme-alpine.css"
@@ -13,7 +13,7 @@ const Grid = forwardRef((props, ref) => {
     return (
         <div
             className="ag-theme-alpine"
-            style={{height: props.gridHeight, width: "100%"}}
+            style={{ width: "100%" }}
         >
             <AgGridReact
                 ref={ref}
@@ -35,9 +35,9 @@ const Grid = forwardRef((props, ref) => {
                             statusPanel: "agTotalRowCountComponent",
                             align: "center",
                         },
-                        {statusPanel: "agFilteredRowCountComponent"},
-                        {statusPanel: "agSelectedRowCountComponent"},
-                        {statusPanel: "agAggregationComponent"},
+                        { statusPanel: "agFilteredRowCountComponent" },
+                        { statusPanel: "agSelectedRowCountComponent" },
+                        { statusPanel: "agAggregationComponent" },
                     ],
                 }}
             ></AgGridReact>
