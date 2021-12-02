f3e6e21f192fd1fd628d2d29bbf02f973e585e8e
Stephen Cooper <scooperdev@gmail.com>
2021-12-02 16:17:42 +0000

    AG-6004 Convert column-group advanced

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/column-groups/examples/advanced-grouping/main.ts
@@ -0,0 +1,201 @@
+import { ColDef, ColGroupDef, GridOptions, HeaderClassParams } from '@ag-grid-community/core'
+
+const columnDefs: (ColDef | ColGroupDef)[] = [
+  {
+    headerName: 'Group A',
+    groupId: 'GroupA',
+    children: [
+      {
+        headerName: 'Athlete 1',
+        field: 'athlete',
+        width: 150,
+        filter: 'agTextColumnFilter',
+      },
+      {
+        headerName: 'Group B',
+        groupId: 'GroupB',
+        children: [
+          { headerName: 'Country 1', field: 'country', width: 120 },
+          {
+            headerName: 'Group C',
+            groupId: 'GroupC',
+            children: [
+              { headerName: 'Sport 1', field: 'sport', width: 110 },
+              {
+                headerName: 'Group D',
+                groupId: 'GroupD',
+                children: [
+                  {
+                    headerName: 'Total 1',
+                    field: 'total',
+                    width: 100,
+                    filter: 'agNumberColumnFilter',
+                  },
+                  {
+                    headerName: 'Group E',
+                    groupId: 'GroupE',
+                    openByDefault: true,
+                    children: [
+                      {
+                        headerName: 'Gold 1',
+                        field: 'gold',
+                        width: 100,
+                        filter: 'agNumberColumnFilter',
+                      },
+                      {
+                        headerName: 'Group F',
+                        groupId: 'GroupF',
+                        openByDefault: true,
+                        children: [
+                          {
+                            headerName: 'Silver 1',
+                            field: 'silver',
+                            width: 100,
+                            filter: 'agNumberColumnFilter',
+                          },
+                          {
+                            headerName: 'Group G',
+                            groupId: 'GroupG',
+                            children: [
+                              {
+                                headerName: 'Bronze',
+                                field: 'bronze',
+                                width: 100,
+                                filter: 'agNumberColumnFilter',
+                              },
+                            ],
+                          },
+                          {
+                            headerName: 'Silver 2',
+                            columnGroupShow: 'open',
+                            field: 'silver',
+                            width: 100,
+                            filter: 'agNumberColumnFilter',
+                          },
+                        ],
+                      },
+                      {
+                        headerName: 'Gold 2',
+                        columnGroupShow: 'open',
+                        field: 'gold',
+                        width: 100,
+                        filter: 'agNumberColumnFilter',
+                      },
+                    ],
+                  },
+                  {
+                    headerName: 'Total 2',
+                    columnGroupShow: 'open',
+                    field: 'total',
+                    width: 100,
+                    filter: 'agNumberColumnFilter',
+                  },
+                ],
+              },
+              {
+                headerName: 'Sport 2',
+                columnGroupShow: 'open',
+                field: 'sport',
+                width: 110,
+              },
+            ],
+          },
+          {
+            headerName: 'Country 2',
+            columnGroupShow: 'open',
+            field: 'country',
+            width: 120,
+          },
+        ],
+      },
+      {
+        headerName: 'Age 2',
+        columnGroupShow: 'open',
+        field: 'age',
+        width: 90,
+        filter: 'agNumberColumnFilter',
+      },
+    ],
+  },
+  {
+    headerName: 'Athlete 2',
+    columnGroupShow: 'open',
+    field: 'athlete',
+    width: 150,
+    filter: 'agTextColumnFilter',
+  },
+]
+
+const gridOptions: GridOptions = {
+  debug: true,
+  columnDefs: columnDefs,
+  rowData: null,
+  defaultColGroupDef: { headerClass: headerClassFunc },
+  defaultColDef: {
+    headerClass: headerClassFunc,
+    sortable: true,
+    resizable: true,
+    filter: true,
+  },
+  icons: {
+    columnGroupOpened: '<i class="far fa-minus-square"/>',
+    columnGroupClosed: '<i class="far fa-plus-square"/>',
+  },
+}
+
+function headerClassFunc(params: HeaderClassParams) {
+  let foundC = false
+  let foundG = false
+
+  // for the bottom row of headers, column is present,
+  // otherwise columnGroup is present. we are guaranteed
+  // at least one is always present.
+  let item = params.column ? params.column : params.columnGroup as any;
+
+  // walk up the tree, see if we are in C or F groups
+  while (item) {
+    // if method getColGroupDef exists, then this is a group
+    // console.log(item.getUniqueId());
+    if (item.getDefinition().groupId === 'GroupC') {
+      foundC = true
+    } else if (item.getDefinition().groupId === 'GroupG') {
+      foundG = true
+    }
+    item = item.getParent()
+  }
+
+  if (foundG) {
+    return 'column-group-g'
+  } else if (foundC) {
+    return 'column-group-c'
+  } else {
+    return []
+  }
+}
+
+function expandAll(expand: boolean) {
+  const columnApi = gridOptions.columnApi!
+  const groupNames = [
+    'GroupA',
+    'GroupB',
+    'GroupC',
+    'GroupD',
+    'GroupE',
+    'GroupF',
+    'GroupG',
+  ]
+
+  groupNames.forEach(groupId => {
+    columnApi.setColumnGroupOpened(groupId, expand)
+  })
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+  const gridDiv = document.querySelector('#myGrid')
+  new agGrid.Grid(gridDiv, gridOptions)
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data))
+})
