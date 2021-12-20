7674dadfef9177ee44edff7c7fba67ccdd2f8fc9
Stephen Cooper <scooperdev@gmail.com>
2021-12-20 14:33:18 +0000

    AG-6004 chart area/axes examples to ts

--- grid-packages/ag-grid-docs/documentation/custom-types/index.d.ts
+++ grid-packages/ag-grid-docs/documentation/custom-types/index.d.ts
@@ -10,3 +10,7 @@ declare const agGrid: {
 // Useful for extracting row data in a file called data.js with a function getData()
 // and setting the rowData: getData()
 declare const getData: () => any[]
+
+// Charts data format
+declare const browserData: any[]
+declare const data: any[]
---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-axes/examples/multiple-axes/main.ts
@@ -0,0 +1,83 @@
+import { AgChartOptions } from '@ag-grid-community/core'
+import * as agCharts from 'ag-charts-community'
+
+const options: AgChartOptions = {
+  container: document.querySelector('#myChart') as HTMLElement,
+  autoSize: true,
+  data: data,
+  title: {
+    text: 'Cattle Holdings and Beef Exports (UK)',
+    fontSize: 18,
+  },
+  subtitle: {
+    text: 'Source: Department for Environment, Food & Rural Affairs',
+  },
+  series: [
+    {
+      type: 'column',
+      xKey: 'year',
+      yKeys: ['male', 'female'],
+      yNames: ['Male cattle', 'Female cattle'],
+      grouped: true,
+      fills: ['#c16068', '#a2bf8a'],
+      strokeWidth: 0,
+    },
+    {
+      type: 'line',
+      xKey: 'year',
+      yKey: 'exportedTonnes',
+      yName: 'Beef exports',
+      stroke: '#80a0c3',
+      strokeWidth: 5,
+      marker: {
+        enabled: false,
+        fill: '#80a0c3',
+      },
+    },
+  ],
+  axes: [
+    {
+      type: 'category',
+      position: 'bottom',
+    },
+    {
+      type: 'number',
+      position: 'left',
+      keys: ['male', 'female'],
+      title: {
+        enabled: true,
+        text: 'Number of cattle',
+      },
+      label: {
+        formatter: function (params) {
+          return params.value / 1000 + 'M'
+        },
+      },
+    },
+    {
+      type: 'number',
+      position: 'right',
+      keys: ['exportedTonnes'],
+      title: {
+        enabled: true,
+        text: 'Exports (tonnes)',
+      },
+      label: {
+        formatter: function (params) {
+          return params.value / 1000 + 'k'
+        },
+      },
+    },
+  ],
+  legend: {
+    position: 'bottom',
+    item: {
+      marker: {
+        shape: 'square',
+        strokeWidth: 0,
+      }
+    }
+  },
+}
+
+var chart = agCharts.AgChart.create(options)
