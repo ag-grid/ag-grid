48b14561781acef8a9d43b4455ba05a9bfebc11d
Stephen Cooper <scooperdev@gmail.com>
2021-12-20 16:54:33 +0000

    AG-6004 chart marker examples to ts

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-markers/examples/marker-shape/data.js
@@ -0,0 +1,28 @@
+function getData() {
+    return [
+        {
+            quarter: 'Q1',
+            petrol: 200,
+            diesel: 100,
+            electric: 50,
+        },
+        {
+            quarter: 'Q2',
+            petrol: 300,
+            diesel: 130,
+            electric: 60,
+        },
+        {
+            quarter: 'Q3',
+            petrol: 350,
+            diesel: 160,
+            electric: 70,
+        },
+        {
+            quarter: 'Q4',
+            petrol: 400,
+            diesel: 200,
+            electric: 50,
+        },
+    ]
+}
