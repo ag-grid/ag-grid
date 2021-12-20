9337994ff53ab26931261d180d3245118fd2e26f
Stephen Cooper <scooperdev@gmail.com>
2021-12-20 16:32:39 +0000

    AG-6004 chart layout line examples to ts

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-line-series/examples/legend-info/main.ts
@@ -0,0 +1,46 @@
+import { AgChartOptions } from '@ag-grid-community/core'
+import * as agCharts from 'ag-charts-community'
+
+const options: AgChartOptions = {
+  container: document.getElementById('myChart'),
+  autoSize: true,
+  title: {
+    text: 'Fuel Spending (2019)',
+  },
+  data: [
+    {
+      quarter: 'Q1',
+      petrol: 200,
+      diesel: 100,
+    },
+    {
+      quarter: 'Q2',
+      petrol: 300,
+      diesel: 130,
+    },
+    {
+      quarter: 'Q3',
+      petrol: 350,
+      diesel: 160,
+    },
+    {
+      quarter: 'Q4',
+      petrol: 400,
+      diesel: 200,
+    },
+  ],
+  series: [
+    {
+      xKey: 'quarter',
+      yKey: 'petrol',
+      yName: 'Petrol',
+    },
+    {
+      xKey: 'quarter',
+      yKey: 'diesel',
+      yName: 'Diesel',
+    },
+  ],
+}
+
+agCharts.AgChart.create(options)
---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-line-series/examples/real-time/main.ts
@@ -0,0 +1,76 @@
+import { AgChartOptions } from '@ag-grid-community/core'
+import * as agCharts from 'ag-charts-community'
+
+var lastTime = new Date('07 Jan 2020 13:25:00 GMT').getTime()
+var data: { time: Date, voltage: number }[] = []
+
+function getData() {
+  data.shift()
+
+  while (data.length < 20) {
+    data.push({
+      time: new Date((lastTime += 1000)),
+      voltage: 1.1 + Math.random() / 2,
+    })
+  }
+
+  return data
+}
+
+const options: AgChartOptions = {
+  container: document.getElementById('myChart'),
+  autoSize: true,
+  data: getData(),
+  series: [
+    {
+      xKey: 'time',
+      yKey: 'voltage',
+    },
+  ],
+  axes: [
+    {
+      type: 'time',
+      position: 'bottom',
+      tick: {
+        count: agCharts.time.second.every(5),
+      },
+      label: {
+        format: '%H:%M:%S',
+      },
+    },
+    {
+      type: 'number',
+      position: 'left',
+      label: {
+        format: '#{.2f}V',
+      },
+    },
+  ],
+  title: {
+    text: 'Core Voltage',
+  },
+  legend: {
+    enabled: false,
+  },
+}
+
+var chart = agCharts.AgChart.create(options)
+var updating = false
+
+function startUpdates() {
+  if (updating) {
+    return
+  }
+
+  updating = true
+  //@ts-ignore
+  this.update()
+  //@ts-ignore
+  setInterval(this.update, 500)
+}
+
+// inScope[update]
+function update() {
+  options.data = getData()
+  agCharts.AgChart.update(chart, options)
+}
