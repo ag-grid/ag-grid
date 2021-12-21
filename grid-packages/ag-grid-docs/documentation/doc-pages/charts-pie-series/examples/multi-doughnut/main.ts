99e7fa3fdef7d511660afad8e070d434cc76ad9f
Stephen Cooper <scooperdev@gmail.com>
2021-12-21 09:20:45 +0000

    AG-6004 chart pie examples to ts

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-pie-series/examples/multi-doughnut/main.ts
@@ -0,0 +1,37 @@
+import { AgChartOptions } from '@ag-grid-community/core'
+import * as agCharts from 'ag-charts-community'
+
+const options: AgChartOptions = {
+  container: document.getElementById('myChart'),
+  data: [
+    { os: 'Android', share: 56.9, satisfaction: 10 },
+    { os: 'iOS', share: 22.5, satisfaction: 15 },
+    { os: 'BlackBerry', share: 6.8, satisfaction: 5 },
+    { os: 'Symbian', share: 8.5, satisfaction: 1 },
+    { os: 'Bada', share: 2.6, satisfaction: 2 },
+    { os: 'Windows', share: 1.9, satisfaction: 12 },
+  ],
+  series: [
+    {
+      type: 'pie',
+      title: {
+        text: 'Market Share',
+      },
+      labelKey: 'os',
+      angleKey: 'share',
+      innerRadiusOffset: -40,
+    },
+    {
+      type: 'pie',
+      title: {
+        text: 'Satisfaction',
+      },
+      labelKey: 'os',
+      angleKey: 'satisfaction',
+      outerRadiusOffset: -100,
+      innerRadiusOffset: -140,
+    },
+  ],
+}
+
+agCharts.AgChart.create(options)
