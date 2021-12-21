3115590c782059a807ff822e289b3e1165e0a08a
Stephen Cooper <scooperdev@gmail.com>
2021-12-21 12:14:39 +0000

    AG-6004 chart theme examples to ts

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-themes/examples/stock-themes/main.ts
@@ -0,0 +1,37 @@
+import { AgChartOptions, AgChartTheme } from '@ag-grid-community/core'
+import * as agCharts from 'ag-charts-community'
+
+const options: AgChartOptions = {
+  container: document.getElementById('myChart'),
+  theme: 'dark',
+  autoSize: true,
+  padding: {
+    left: 70,
+    right: 70,
+  },
+  title: {
+    text: 'Chart Theme Example',
+  },
+  data: [
+    { label: 'Android', value: 56.9, other: 7 },
+    { label: 'iOS', value: 22.5, other: 8 },
+    { label: 'BlackBerry', value: 6.8, other: 9 },
+    { label: 'Symbian', value: 8.5, other: 10 },
+    { label: 'Bada', value: 2.6, other: 11 },
+    { label: 'Windows', value: 1.9, other: 12 },
+  ],
+  series: [
+    {
+      type: 'pie',
+      angleKey: 'value',
+      labelKey: 'label',
+    },
+  ],
+}
+
+var chart = agCharts.AgChart.create(options)
+
+function applyTheme(theme: AgChartTheme) {
+  options.theme = theme
+  agCharts.AgChart.update(chart, options)
+}
