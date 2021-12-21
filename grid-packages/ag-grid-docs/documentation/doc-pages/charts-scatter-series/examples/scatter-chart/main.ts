0824497c9ac50c404840d904be4b99f2a0dbe405
Stephen Cooper <scooperdev@gmail.com>
2021-12-21 11:10:20 +0000

    AG-6004 chart scatter examples to ts

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-scatter-series/examples/scatter-chart/main.ts
@@ -0,0 +1,31 @@
+import { AgChartOptions } from '@ag-grid-community/core'
+import * as agCharts from 'ag-charts-community'
+
+const options: AgChartOptions = {
+  autoSize: true,
+  title: {
+    text: 'Mean Sea Level (mm)',
+  },
+  container: document.getElementById('myChart'),
+  data: getData(),
+  series: [
+    {
+      type: 'scatter',
+      xKey: 'time',
+      yKey: 'mm',
+      showInLegend: false,
+    },
+  ],
+  axes: [
+    {
+      type: 'number',
+      position: 'bottom',
+    },
+    {
+      type: 'number',
+      position: 'left',
+    },
+  ],
+}
+
+agCharts.AgChart.create(options)
