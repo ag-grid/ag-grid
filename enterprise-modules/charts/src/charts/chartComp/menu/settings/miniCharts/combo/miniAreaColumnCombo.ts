baf3d53cee162d0f69729f347ead62bb7561878d
Mana <mana.jalali.15@ucl.ac.uk>
2022-01-06 14:53:28 +0000

    AG-6201 - Area & column combination mini chart for settings panel

--- enterprise-modules/charts/src/charts/chartComp/menu/settings/miniCharts/combo/miniAreaColumnCombo.ts
+++ enterprise-modules/charts/src/charts/chartComp/menu/settings/miniCharts/combo/miniAreaColumnCombo.ts
@@ -1,27 +1,31 @@
 import { MiniChartWithAxes } from "../miniChartWithAxes";
-import { Line, Path, Rect } from "ag-charts-community";
+import { BandScale, LinearScale, Path, Rect } from "ag-charts-community";
 import { ChartType } from "@ag-grid-community/core";
-import { createColumnRects, CreateColumnRectsParams, createLinePaths } from "../miniChartHelpers";
+import { createColumnRects, CreateColumnRectsParams } from "../miniChartHelpers";
 
+export interface Coordinate {
+    x: number;
+    y: number;
+}
 export class MiniAreaColumnCombo extends MiniChartWithAxes {
     static chartType: ChartType = 'areaColumnCombo';
 
     private stackedColumns: Rect[][];
-    private lines: Path[];
+    private areas: Path[];
 
     private columnData = [
         [12, 16],
         [6, 9],
     ];
 
-    private lineData = [
-        [5, 4, 6, 5, 4]
+    private areaData = [
+        [5, 4, 6, 5, 4],
     ];
 
     constructor(container: HTMLElement, fills: string[], strokes: string[]) {
         super(container, "areaColumnComboTooltip");
 
-        const { root, columnData, lineData, size, padding } = this;
+        const { root, columnData, areaData, size, padding } = this;
 
         this.stackedColumns = createColumnRects({
             stacked: true,
@@ -34,21 +38,55 @@ export class MiniAreaColumnCombo extends MiniChartWithAxes {
             xScalePadding: 0.5,
         } as CreateColumnRectsParams);
 
+        // scale for area series
+        const xScale = new BandScale<number>();
+        xScale.range = [padding, size - padding];
+        xScale.domain = [0, 1, 2, 3, 4];
+
+        const yScale = new LinearScale();
+        yScale.range = [size - padding, padding];
+        yScale.domain = [0, 6];
+
+        const pathData: Coordinate[][] = [];
+        const yZero = yScale.convert(0);
+        const firstX = xScale.convert(0);
+
+        areaData.forEach((series, i) => {
+            const points = pathData[i] || (pathData[i] = []);
+            series.forEach((data, j) => {
+                const yDatum = data;
+                const xDatum = j;
+
+                const x = xScale.convert(xDatum);
+                const y = yScale.convert(yDatum);
+
+                points[j] = { x, y };
+            });
+
+            const lastX = xScale.convert(series.length - 1);
+
+            pathData[i].push({
+                x: lastX,
+                y: yZero
+            }, {
+                x: firstX,
+                y: yZero
+            });
+        });
+
+        this.areas = pathData.map((points) => {
+            const area = new Path();
+            area.strokeWidth = 1;
+            area.fillOpacity = 0.7;
+
+            const path = area.path;
+            points.forEach((point, i) => path[i > 0 ? 'lineTo' : 'moveTo'](point.x, point.y));
+
+            return area;
+        });
+
+        root.append(this.areas);
         root.append(([] as Rect[]).concat.apply([], this.stackedColumns));
-
-        const axisStroke = 'gray';
-        const axisOvershoot = 3;
-
-        this.lines = createLinePaths(root, lineData, size - axisOvershoot, padding);
-
-        const rightAxis = new Line();
-        rightAxis.x1 = size - padding - axisOvershoot;
-        rightAxis.y1 = padding;
-        rightAxis.x2 = size - padding - axisOvershoot;
-        rightAxis.y2 = size - padding + axisOvershoot;
-        rightAxis.stroke = axisStroke;
-
-        root.append(rightAxis);
 
         this.updateColors(fills, strokes);
     }
@@ -60,9 +98,9 @@ export class MiniAreaColumnCombo extends MiniChartWithAxes {
                 bar.stroke = strokes[i];
             })
         );
-
-        this.lines.forEach((line, i) => {
-            line.stroke = fills[i + 2];
-        });
+        this.areas.forEach((area, i) => {
+            area.fill = fills[i];
+            area.stroke = strokes[i];
+        })
     }
 }
