0d51d50c059393e854521e34a931330bd3ee7003
Stephen Cooper <scooperdev@gmail.com>
2022-01-06 14:16:42 +0000

    fix multi examples

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grid-size/examples/width-and-height/typescript/main.ts
@@ -0,0 +1,48 @@
+import { GridOptions } from "@ag-grid-community/core";
+
+var gridOptions: GridOptions = {
+    columnDefs: [
+        { field: "athlete", width: 150 },
+        { field: "age", width: 90 },
+        { field: "country", width: 150 },
+        { field: "year", width: 90 },
+        { field: "date", width: 150 },
+        { field: "sport", width: 150 },
+        { field: "gold", width: 100 },
+        { field: "silver", width: 100 },
+        { field: "bronze", width: 100 },
+        { field: "total", width: 100 },
+    ],
+};
+
+function fillLarge() {
+    setWidthAndHeight('100%');
+}
+
+function fillMedium() {
+    setWidthAndHeight('60%');
+}
+
+function fillExact() {
+    setWidthAndHeight('400px');
+}
+
+function setWidthAndHeight(size: string) {
+    var eGridDiv = document.querySelector('#myGrid') as any;
+    eGridDiv.style.setProperty('width', size);
+    eGridDiv.style.setProperty('height', size);
+}
+
+var gridDiv = document.querySelector('#myGrid');
+new agGrid.Grid(gridDiv, gridOptions);
+
+fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then(data => gridOptions.api!.setRowData(data));
+
+if (typeof window !== 'undefined') {
+    // Attach external event handlers to window so they can be called from index.html
+    (<any>window).fillLarge = fillLarge;
+    (<any>window).fillMedium = fillMedium;
+    (<any>window).fillExact = fillExact;
+}
