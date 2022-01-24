01e8bec68b3ef269882ce7247b0d3c40165049e6
Niall Crosby <niall.crosby@gmail.com>
2022-01-24 15:50:28 +0000

    AG-6285 Clear Columns Broke

--- community-modules/core/src/ts/columns/columnModel.ts
+++ community-modules/core/src/ts/columns/columnModel.ts
@@ -3283,26 +3283,12 @@ export class ColumnModel extends BeanStub {
 
     private updateGroupsAndDisplayedColumns(source: ColumnEventType) {
 
-        const hashDisplayedCols = () => {
-            const toHash = (cols: Column[]) => cols.map(c => c.getInstanceId()).join('|');
-            const lists = [this.displayedColumnsLeft, this.displayedColumnsRight, this.displayedColumnsCenter];
-            const res = lists.map(toHash).join('$');
-            return res;
-        };
-
-        const hashBefore = hashDisplayedCols();
-
         this.updateOpenClosedVisibilityInColumnGroups();
         this.deriveDisplayedColumns(source);
         this.refreshFlexedColumns();
         this.extractViewport();
         this.updateBodyWidths();
         // this event is picked up by the gui, headerRenderer and rowRenderer, to recalculate what columns to display
-
-        const hashAfter = hashDisplayedCols();
-
-        const nothingChanged = hashAfter == hashBefore;
-        if (nothingChanged) { return; }
 
         const event: DisplayedColumnsChangedEvent = {
             type: Events.EVENT_DISPLAYED_COLUMNS_CHANGED,
