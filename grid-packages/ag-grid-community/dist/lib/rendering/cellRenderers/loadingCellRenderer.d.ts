4c2be853b1b8475a354dc15bcc40c94b4b14f02d
seanlandsman <sean@thelandsmans.com>
2022-02-08 16:15:33 +0000

    Merge from latest

--- grid-packages/ag-grid-community/dist/lib/rendering/cellRenderers/loadingCellRenderer.d.ts
+++ grid-packages/ag-grid-community/dist/lib/rendering/cellRenderers/loadingCellRenderer.d.ts
@@ -4,9 +4,6 @@ import { IComponent } from "../../interfaces/iComponent";
 export interface ILoadingCellRendererParams extends ICellRendererParams {
 }
 export interface ILoadingCellRenderer {
-    /** Refresh the loading renderer. Return true if successful. Return false if not (or you don't have refresh logic),
-     * then the grid will refresh it for you. */
-    refresh?(params: ILoadingCellRendererParams): boolean;
 }
 export interface ILoadingCellRendererComp extends ILoadingCellRenderer, IComponent<ILoadingCellRendererParams> {
 }
