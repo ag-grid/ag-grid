f95c3d37f370e50e878f6903344a5a819b38bd5e
seanlandsman <sean@thelandsmans.com>
2022-02-21 13:56:29 +0000

    AG-6422 Improve build docs times

--- scripts/hashChanged.sh
+++ scripts/hashChanged.sh
@@ -1,19 +1,25 @@
 #!/usr/bin/env bash
 
 module_directory=$(pwd)
-if [ "$#" -eq 1 ]
+if [ ! -z "$1" ]
 then
   module_directory=$1
 fi
 
-if [ ! -f "$module_directory/.hash" ]
+hash_filename="$module_directory/.hash"
+if [ ! -z "$2" ]
+then
+  hash_filename="$module_directory/$2"
+fi
+
+if [ ! -f  $hash_filename ]
 then
   echo 1;
 else
   __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
   hash_now=$("$__dir"/hashDirectory.sh "$module_directory")
-  existing_hash=$(cat "$module_directory/.hash");
+  existing_hash=$(cat $hash_filename);
 
   if [ "$hash_now" = "$existing_hash" ]
   then
