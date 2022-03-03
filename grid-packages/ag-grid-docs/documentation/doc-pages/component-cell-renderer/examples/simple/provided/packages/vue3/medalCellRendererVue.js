aae0a8b9582c8ecb5abbe037246064a68fa1a07e
Stephen Cooper <scooperdev@gmail.com>
2022-03-03 15:59:41 +0000

    AG-6460 Delete hand written provided/packages examples

--- grid-packages/ag-grid-docs/documentation/doc-pages/component-cell-renderer/examples/simple/provided/packages/vue3/medalCellRendererVue.js
+++
@@ -1,9 +0,0 @@
-export default {
-    template: `<span>{{ displayValue }}</span>`,
-    setup(props) {
-        const displayValue = new Array(props.params.value).fill('#').join('');
-        return {
-            displayValue
-        }
-    }
-};
