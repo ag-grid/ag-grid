22d614b482c489b04b1f5fbe798b261d38a5761c
Stephen Cooper <scooperdev@gmail.com>
2022-03-08 10:28:43 +0000

    revert formatting changes

--- grid-packages/ag-grid-docs/dev-server.js
+++ grid-packages/ag-grid-docs/dev-server.js
@@ -14,11 +14,11 @@ const proxy = require('express-http-proxy');
 const webpackMiddleware = require('webpack-dev-middleware');
 const chokidar = require('chokidar');
 const tcpPortUsed = require('tcp-port-used');
-const { generateDocumentationExamples } = require('./example-generator-documentation');
-const { watchValidateExampleTypes } = require('./example-validator');
-const { updateBetweenStrings, getAllModules } = require('./utils');
-const { getFlattenedBuildChainInfo, buildPackages, buildCss, watchCss } = require('./lernaOperations');
-const { EOL } = os;
+const {generateDocumentationExamples} = require('./example-generator-documentation');
+const {watchValidateExampleTypes} = require('./example-validator');
+const {updateBetweenStrings, getAllModules} = require('./utils');
+const {getFlattenedBuildChainInfo, buildPackages, buildCss, watchCss} = require('./lernaOperations');
+const {EOL} = os;
 
 const key = fs.readFileSync(process.env.AG_DOCS_KEY || './selfsigned.key', 'utf8');
 const cert = fs.readFileSync(process.env.AG_DOCS_CRT || './selfsigned.crt', 'utf8');
@@ -37,7 +37,7 @@ const HOST = '127.0.0.1';
 const WINDOWS = /^win/.test(os.platform());
 
 function reporter(middlewareOptions, options) {
-    const { log, state, stats } = options;
+    const {log, state, stats} = options;
 
     if (state) {
         const displayStats = middlewareOptions.stats !== false;
@@ -139,8 +139,8 @@ function symlinkModules(gridCommunityModules, gridEnterpriseModules, chartCommun
         linkType = 'junction';
     }
 
-    lnk('../../community-modules/vue/', '_dev/@ag-grid-community', { force: true, type: linkType, rename: 'vue' });
-    lnk('../../community-modules/vue3/', '_dev/@ag-grid-community', { force: true, type: linkType, rename: 'vue3' });
+    lnk('../../community-modules/vue/', '_dev/@ag-grid-community', {force: true, type: linkType, rename: 'vue'});
+    lnk('../../community-modules/vue3/', '_dev/@ag-grid-community', {force: true, type: linkType, rename: 'vue3'});
     lnk('../../community-modules/angular/', '_dev/@ag-grid-community', {
         force: true,
         type: linkType,
@@ -260,11 +260,11 @@ async function watchAndGenerateExamples() {
     }
 
     chokidar
-        .watch([`./documentation/doc-pages/**/examples/**/*.{html,css,js,jsx,ts}`], { ignored: ['**/_gen/**/*'] })
+        .watch([`./documentation/doc-pages/**/examples/**/*.{html,css,js,jsx,ts}`], {ignored: ['**/_gen/**/*']})
         .on('change', regenerateDocumentationExamplesForFileChange);
 
     chokidar
-        .watch([`./documentation/doc-pages/**/*.md`], { ignoreInitial: true })
+        .watch([`./documentation/doc-pages/**/*.md`], {ignoreInitial: true})
         .on('add', regenerateDocumentationExamplesForFileChange);
 }
 
@@ -358,6 +358,7 @@ function updateUtilsSystemJsMappingsForFrameworks(gridCommunityModules, gridEnte
         gridEnterpriseModules.filter(module => module.moduleDirName !== 'all-modules'),
         module => `        "${module.publishedName}": \`\${localPrefix}/${module.cjsFilename}\`,`,
         module => `        "${module.publishedName}": \`\${localPrefix}/${module.cjsFilename}\`,`);
+
 
     updatedUtilFileContents = updateBetweenStrings(updatedUtilFileContents,
         '        /* START OF GRID COMMUNITY MODULES PATHS PROD - DO NOT DELETE */',
@@ -537,7 +538,7 @@ function updateModuleChangedHash(moduleRoot) {
     const npm = WINDOWS ? 'npm.cmd' : 'npm';
     const resolvedPath = resolve(moduleRoot).replace(/\\/g, '/').replace("C:", "/c");
 
-    cp.spawnSync(npm, ['run', 'hash'], { cwd: resolvedPath });
+    cp.spawnSync(npm, ['run', 'hash'], {cwd: resolvedPath});
 }
 
 function updateSystemJsBoilerplateMappingsForFrameworks(gridCommunityModules, gridEnterpriseModules, chartsCommunityModules) {
@@ -548,8 +549,7 @@ function updateSystemJsBoilerplateMappingsForFrameworks(gridCommunityModules, gr
         './documentation/static/example-runner/grid-angular-boilerplate/systemjs.config.dev.js',
         './documentation/static/example-runner/grid-react-boilerplate/systemjs.config.dev.js',
         './documentation/static/example-runner/grid-vue-boilerplate/systemjs.config.dev.js',
-        './documentation/static/example-runner/grid-vue3-boilerplate/systemjs.config.dev.js'
-    ];
+        './documentation/static/example-runner/grid-vue3-boilerplate/systemjs.config.dev.js'];
 
     const getModuleConfig = module => [
         `            '${module.publishedName}': {`,
@@ -659,13 +659,13 @@ const readModulesState = () => {
     moduleRootNames.forEach(moduleRootName => {
         const moduleRootDirectory = WINDOWS ? `..\\..\\${moduleRootName}\\` : `../../${moduleRootName}/`;
 
-        fs.readdirSync(moduleRootDirectory, { withFileTypes: true })
+        fs.readdirSync(moduleRootDirectory, {withFileTypes: true})
             .filter(d => d.isDirectory())
             .filter(d => !exclusions.includes(d.name))
             .map(d => WINDOWS ? `..\\..\\${moduleRootName}\\${d.name}` : `../../${moduleRootName}/${d.name}`)
             .map(d => {
                 const packageName = require(WINDOWS ? `${d}\\package.json` : `${d}/package.json`).name;
-                modulesState[packageName] = { moduleChanged: moduleChanged(d) };
+                modulesState[packageName] = {moduleChanged: moduleChanged(d)};
             });
     });
 
@@ -697,7 +697,7 @@ module.exports = async (skipFrameworks, skipExampleFormatting, done) => {
                 process.env.AG_EXAMPLE_DISABLE_FORMATTING = 'true';
             }
 
-            const { gridCommunityModules, gridEnterpriseModules, chartCommunityModules } = getAllModules();
+            const {gridCommunityModules, gridEnterpriseModules, chartCommunityModules} = getAllModules();
 
             const app = express();
 
