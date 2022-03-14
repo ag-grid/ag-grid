97ecdc3fd2bee96c5764b334bde60bcdcce0a5d7
seanlandsman <sean@thelandsmans.com>
2022-03-14 10:36:02 +0000

    RTI-813 R - [Filtering] Set Filter API - Case Sensitivity React example missing buttons

--- grid-packages/ag-grid-docs/documentation/doc-pages/filter-set-api/examples/set-filter-api-case-sensitive/provided/modules/react/index.jsx
+++ grid-packages/ag-grid-docs/documentation/doc-pages/filter-set-api/examples/set-filter-api-case-sensitive/provided/modules/react/index.jsx
@@ -1,19 +1,18 @@
 'use strict';
-
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import { AgGridReact } from '@ag-grid-community/react';
-import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { SetFilterModule } from '@ag-grid-enterprise/set-filter';
-import { MenuModule } from '@ag-grid-enterprise/menu';
-import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
-import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+import {AgGridReact} from '@ag-grid-community/react';
+import {ClientSideRowModelModule} from '@ag-grid-community/client-side-row-model';
+import {SetFilterModule} from '@ag-grid-enterprise/set-filter';
+import {MenuModule} from '@ag-grid-enterprise/menu';
+import {FiltersToolPanelModule} from '@ag-grid-enterprise/filter-tool-panel';
 import '@ag-grid-community/core/dist/styles/ag-grid.css';
 import "@ag-grid-community/core/dist/styles/ag-theme-alpine.css";
 
-import { ModuleRegistry } from '@ag-grid-community/core';
+import {ModuleRegistry} from '@ag-grid-community/core';
+
 // Register the required feature modules with the Grid
-ModuleRegistry.registerModules([ClientSideRowModelModule, SetFilterModule, MenuModule, ColumnsToolPanelModule, FiltersToolPanelModule]);
+ModuleRegistry.registerModules([ClientSideRowModelModule, SetFilterModule, MenuModule, FiltersToolPanelModule])
 
 const colourCellRenderer = props => {
     if (!props.value || props.value === '(Select All)') {
@@ -29,8 +28,17 @@ const colourCellRenderer = props => {
         height: 10,
         backgroundColor: props.value.toLowerCase()
     };
-    return <React.Fragment><div style={styles} />{props.value}</React.Fragment>;
+    return <React.Fragment>
+        <div style={styles}/>
+        {props.value}</React.Fragment>;
 }
+
+const FILTER_TYPES = {
+    insensitive: 'colour',
+    sensitive: 'colour_1',
+};
+
+const MANGLED_COLOURS = ['ReD', 'OrAnGe', 'WhItE', 'YeLlOw'];
 
 class GridExample extends Component {
     constructor(props) {
@@ -65,30 +73,10 @@ class GridExample extends Component {
                 floatingFilter: true,
             },
             sideBar: 'filters',
-            rowData: [
-                { colour: 'Black' },
-                { colour: 'BLACK' },
-                { colour: 'black' },
-                { colour: 'Red' },
-                { colour: 'RED' },
-                { colour: 'red' },
-                { colour: 'Orange' },
-                { colour: 'ORANGE' },
-                { colour: 'orange' },
-                { colour: 'White' },
-                { colour: 'WHITE' },
-                { colour: 'white' },
-                { colour: 'Yellow' },
-                { colour: 'YELLOW' },
-                { colour: 'yellow' },
-                { colour: 'Green' },
-                { colour: 'GREEN' },
-                { colour: 'green' },
-                { colour: 'Purple' },
-                { colour: 'PURPLE' },
-                { colour: 'purple' },
-            ]
+            rowData: getData()
         };
+
+
     }
 
     onGridReady = params => {
@@ -98,13 +86,61 @@ class GridExample extends Component {
     }
 
     onFirstDataRendered = (params) => {
-        ((params.api.getToolPanelInstance('filters'))).expandFilters();
+        ((this.gridApi.getToolPanelInstance('filters'))).expandFilters();
+    }
+
+    setModel = (type) => {
+        const instance = this.gridApi.getFilterInstance(FILTER_TYPES[type]);
+        instance.setModel({values: MANGLED_COLOURS});
+        this.gridApi.onFilterChanged();
+    }
+
+    getModel = (type) => {
+        const instance = this.gridApi.getFilterInstance(FILTER_TYPES[type]);
+        alert(JSON.stringify(instance.getModel(), null, 2));
+    }
+
+    setFilterValues = (type) => {
+        const instance = this.gridApi.getFilterInstance(FILTER_TYPES[type]);
+        instance.setFilterValues(MANGLED_COLOURS);
+        instance.applyModel();
+        this.gridApi.onFilterChanged();
+    }
+
+    getValues = (type) => {
+        const instance = this.gridApi.getFilterInstance(FILTER_TYPES[type]);
+        alert(JSON.stringify(instance.getValues(), null, 2));
+    }
+
+    reset = (type) => {
+        const instance = this.gridApi.getFilterInstance(FILTER_TYPES[type]);
+        instance.resetFilterValues();
+        instance.setModel(null);
+        this.gridApi.onFilterChanged();
     }
 
     render() {
         return (
-            <div style={{ width: '100%', height: '100%' }}>
-                <div style={{ "display": "flex", "flexDirection": "column", "height": "100%" }}>
+            <div style={{width: '100%', height: '100%'}}>
+                <div className="example-wrapper">
+                    <div className="example-header">
+                        <div>
+                            Case Insensitive:
+                            <button onClick={() => this.setModel('insensitive')}>API: setModel() - mismatching case</button>
+                            <button onClick={() => this.getModel('insensitive')}>API: getModel()</button>
+                            <button onClick={() => this.setFilterValues('insensitive')}>API: setFilterValues() - mismatching case</button>
+                            <button onClick={() => this.getValues('insensitive')}>API: getValues()</button>
+                            <button onClick={() => this.reset('insensitive')}>Reset</button>
+                        </div>
+                        <div style={{"paddingTop": "10px"}}>
+                            Case Sensitive:
+                            <button onClick={() => this.setModel('sensitive')}>API: setModel() - mismatching case</button>
+                            <button onClick={() => this.getModel('sensitive')}>API: getModel()</button>
+                            <button onClick={() => this.setFilterValues('sensitive')}>API: setFilterValues() - mismatching case</button>
+                            <button onClick={() => this.getValues('sensitive')}>API: getValues()</button>
+                            <button onClick={() => this.reset('sensitive')}>Reset</button>
+                        </div>
+                    </div>
                     <div
 
                         style={{
