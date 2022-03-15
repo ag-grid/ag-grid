85b5295b2748d3b64b9940a6591c8f377233863d
Guilherme Lopes <guilherme@gwm-solutions.com>
2022-03-15 16:28:51 -0500

    AG-6500 - fixed class object being mutated with defaultColGroupDef

--- community-modules/core/src/ts/headerRendering/cells/cssClassApplier.ts
+++ community-modules/core/src/ts/headerRendering/cells/cssClassApplier.ts
@@ -76,7 +76,7 @@ export class CssClassApplier {
         }
 
         if (typeof classToUse === 'string') { return [classToUse]; }
-        if (Array.isArray(classToUse)) { return classToUse; }
+        if (Array.isArray(classToUse)) { return [...classToUse]; }
 
         return [];
     }
