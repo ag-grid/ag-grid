5c5befa890753553114f6a9c32296bbcd2ea4a54
seanlandsman <sean@thelandsmans.com>
2022-03-15 12:48:37 +0000

    AG-6495 [Docs] Full Width Groups Rendering Example missing event handling (Vue Only)

--- grid-packages/ag-grid-docs/documentation/doc-pages/grouping-group-rows/examples/full-width-groups-rendering/groupRowInnerRenderer_vue.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grouping-group-rows/examples/full-width-groups-rendering/groupRowInnerRenderer_vue.js
@@ -2,25 +2,38 @@ export default {
     template: `
       <div style="display: inline-block">
       <img v-if="flagCodeImg" class="flag" border="0" width="20" height="15" :src="flagCodeImg"/>
-      <span class="groupTitle">{{ this.node.key }}</span>
-      <span class="medal gold" v-bind:aria-label="this.node.key + ' - ' + this.node.aggData.gold + ' gold medals'"><i class="fas fa-medal"></i>
-        {{ this.node.aggData.gold }}</span>
-      <span class="medal silver" v-bind:aria-label="this.node.key + ' - ' + this.node.aggData.silver + ' silver medals'"><i class="fas fa-medal"></i>
-        {{ this.node.aggData.silver }}</span>
-      <span class="medal bronze" v-bind:aria-label="this.node.key + ' - ' + this.node.aggData.bronze + ' bronze medals'"><i class="fas fa-medal"></i>
-        {{ this.node.aggData.bronze }}</span>
+      <span class="groupTitle">{{ key }}</span>
+      <span class="medal gold" v-bind:aria-label="key + ' - ' + gold + ' gold medals'"><i class="fas fa-medal"></i>
+        {{ gold }}</span>
+      <span class="medal silver" v-bind:aria-label="key + ' - ' + silver + ' silver medals'"><i class="fas fa-medal"></i>
+        {{ silver }}</span>
+      <span class="medal bronze" v-bind:aria-label="key + ' - ' + bronze + ' bronze medals'"><i class="fas fa-medal"></i>
+        {{ bronze }}</span>
       </div>
     `,
     data: function () {
         return {
             flagCodeImg: '',
-            node: null
+            key: null,
+            gold: null,
+            silver: null,
+            bronze: null,
         };
     },
     beforeMount() {
-        this.node = this.params.node;
+        this.params.api.addEventListener('cellValueChanged', this.updateGui);
+        this.params.api.addEventListener('filterChanged', this.updateGui);
 
-        const flagCode = this.params.flagCodes[this.node.key];
-        this.flagCodeImg = flagCode ? `https://flags.fmcdn.net/data/flags/mini/${flagCode}.png` : null;
+        this.updateGui();
+    }, methods: {
+        updateGui() {
+            const flagCode = this.params.flagCodes[this.params.node.key];
+            this.flagCodeImg = flagCode ? `https://flags.fmcdn.net/data/flags/mini/${flagCode}.png` : null;
+
+            this.key = this.params.node.key;
+            this.gold = this.params.node.aggData.gold;
+            this.silver = this.params.node.aggData.silver;
+            this.bronze = this.params.node.aggData.bronze;
+        }
     }
 };
