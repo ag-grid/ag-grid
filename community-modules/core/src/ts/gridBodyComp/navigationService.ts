2994d0e25678476703308d5f8dd2f91d42ad1ffc
Guilherme Lopes <guilherme@gwm-solutions.com>
2022-04-04 17:28:02 -0500

    logic refactor

--- community-modules/core/src/ts/gridBodyComp/navigationService.ts
+++ community-modules/core/src/ts/gridBodyComp/navigationService.ts
@@ -159,107 +159,61 @@ export class NavigationService extends BeanStub {
 
         const currentPageBottomPixel = scrollPosition.top + pixelsInOnePage;
         const currentPageBottomRow = this.paginationProxy.getRowIndexAtPixel(currentPageBottomPixel + pagingPixelOffset);
-        let scrollIndex = currentPageBottomRow;
-
-        const currentRowNode = this.paginationProxy.getRow(gridCell.rowIndex);
-        const currentCellPixel = currentRowNode?.rowTop || 0;
-
-        let focusIndex: number;
 
         if (this.columnModel.isAutoRowHeightActive()) {
-            this.navigateTo({
-                scrollIndex,
-                scrollType: 'top',
-                scrollColumn: null,
-                focusIndex: scrollIndex,
-                focusColumn: gridCell.column
-            });
-            setTimeout(() => {
-                focusIndex = this.getNextFocusIndexForAutoHeight(gridCell);
-                this.navigateTo({
-                    scrollIndex,
-                    scrollType: 'top',
-                    scrollColumn: null,
-                    focusIndex: focusIndex,
-                    focusColumn: gridCell.column
-                });
-            }, 50);
-            return;
+            this.navigateToNextPageWithAutoHeight(gridCell, currentPageBottomRow);
+        } else {
+            this.navigateToNextPage(gridCell, currentPageBottomRow);
         }
-
-        const nextCellPixel = currentCellPixel! + pixelsInOnePage - pagingPixelOffset;
-        focusIndex = this.paginationProxy.getRowIndexAtPixel(nextCellPixel + pagingPixelOffset);
-
-        const pageLastRow = this.paginationProxy.getPageLastRow();
-
-        if (focusIndex === gridCell.rowIndex) {
-            scrollIndex = focusIndex = gridCell.rowIndex + 1;
-        }
-        if (focusIndex > pageLastRow) { focusIndex = pageLastRow; }
-        if (scrollIndex > pageLastRow) { scrollIndex = pageLastRow; }
-
-        if (this.isRowTallerThanView(focusIndex)) {
-            scrollIndex = focusIndex;
-        }
-
-        this.navigateTo({
-            scrollIndex,
-            scrollType: 'top',
-            scrollColumn: null,
-            focusIndex,
-            focusColumn: gridCell.column
-        });
     }
 
     private onPageUp(gridCell: CellPosition): void {
         const gridBodyCon = this.ctrlsService.getGridBodyCtrl();
         const scrollPosition = gridBodyCon.getScrollFeature().getVScrollPosition();
-        const pixelsInOnePage = this.getViewportHeight();
 
         const pagingPixelOffset = this.paginationProxy.getPixelOffset();
 
         const currentPageTopPixel = scrollPosition.top;
         const currentPageTopRow = this.paginationProxy.getRowIndexAtPixel(currentPageTopPixel + pagingPixelOffset);
-        let scrollIndex = currentPageTopRow;
-
-        const currentRowNode = this.paginationProxy.getRow(gridCell.rowIndex);
-        const nextCellPixel = currentRowNode?.rowTop! + currentRowNode?.rowHeight! - pixelsInOnePage - pagingPixelOffset;
-        let focusIndex: number;
 
         if (this.columnModel.isAutoRowHeightActive()) {
-            this.navigateTo({
-                scrollIndex,
-                scrollType: 'bottom',
-                scrollColumn: null,
-                focusIndex: scrollIndex,
-                focusColumn: gridCell.column
-            });
-            setTimeout(() => {
-                focusIndex = this.getNextFocusIndexForAutoHeight(gridCell, true);
-
-                this.navigateTo({
-                    scrollIndex,
-                    scrollType: 'bottom',
-                    scrollColumn: null,
-                    focusIndex: focusIndex,
-                    focusColumn: gridCell.column
-                });
-            }, 50);
-            return;
+            this.navigateToNextPageWithAutoHeight(gridCell, currentPageTopRow, true);
+        } else {
+            this.navigateToNextPage(gridCell, currentPageTopRow, true);
         }
+    }
 
-        focusIndex = this.paginationProxy.getRowIndexAtPixel(nextCellPixel + pagingPixelOffset);
-
+    private navigateToNextPage(gridCell: CellPosition, scrollIndex: number, up: boolean = false): void {
+        const pixelsInOnePage = this.getViewportHeight();
         const firstRow = this.paginationProxy.getPageFirstRow();
+        const lastRow = this.paginationProxy.getPageLastRow();
+        const pagingPixelOffset = this.paginationProxy.getPixelOffset();
+        const currentRowNode = this.paginationProxy.getRow(gridCell.rowIndex);
+
+        const rowPixelDiff = up 
+            ? (currentRowNode?.rowHeight! - pixelsInOnePage - pagingPixelOffset) 
+            : (pixelsInOnePage - pagingPixelOffset);
+
+        const nextCellPixel = currentRowNode?.rowTop! + rowPixelDiff;
+
+        let focusIndex = this.paginationProxy.getRowIndexAtPixel(nextCellPixel + pagingPixelOffset);
 
         if (focusIndex === gridCell.rowIndex) {
-            scrollIndex = focusIndex = gridCell.rowIndex - 1;
+            const diff = up ? -1 : 1;
+            scrollIndex = focusIndex = gridCell.rowIndex + diff;
         }
 
-        if (focusIndex < firstRow) { focusIndex = firstRow; }
-        if (scrollIndex < firstRow) { scrollIndex = firstRow; }
+        let scrollType: 'top' | 'bottom';
 
-        let scrollType: 'top' | 'bottom' = 'bottom';
+        if (up) {
+            scrollType = 'bottom';
+            if (focusIndex < firstRow) { focusIndex = firstRow; }
+            if (scrollIndex < firstRow) { scrollIndex = firstRow; }
+        } else {
+            scrollType = 'top'
+            if (focusIndex > lastRow) { focusIndex = lastRow; }
+            if (scrollIndex > lastRow) { scrollIndex = lastRow; }
+        }
 
         if (this.isRowTallerThanView(focusIndex)) {
             scrollIndex = focusIndex;
@@ -273,6 +227,34 @@ export class NavigationService extends BeanStub {
             focusIndex,
             focusColumn: gridCell.column
         });
+    }
+
+    private navigateToNextPageWithAutoHeight(gridCell: CellPosition, scrollIndex: number, up: boolean = false): void {
+        // because autoHeight will calculate the height of rows after scroll
+        // first we scroll towards the required point, then we add a small 
+        // delay to allow the height to be recalculated, check which index
+        // should be focused and then finally navigate to that index.
+        // TODO: we should probably have an event fired once to scrollbar has
+        // settled and all rowHeights have been calculated instead of relying
+        // on a setTimeout of 50ms.
+        this.navigateTo({
+            scrollIndex: scrollIndex,
+            scrollType: up ? 'bottom' : 'top',
+            scrollColumn: null,
+            focusIndex: scrollIndex,
+            focusColumn: gridCell.column
+        });
+        setTimeout(() => {
+            const focusIndex = this.getNextFocusIndexForAutoHeight(gridCell, up);
+
+            this.navigateTo({
+                scrollIndex: scrollIndex,
+                scrollType: up ? 'bottom' : 'top',
+                scrollColumn: null,
+                focusIndex: focusIndex,
+                focusColumn: gridCell.column
+            });
+        }, 50);
     }
 
     private getNextFocusIndexForAutoHeight(gridCell: CellPosition, up: boolean = false): number {
