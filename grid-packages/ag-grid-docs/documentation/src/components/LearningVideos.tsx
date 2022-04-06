3c8392dc23e0b02abdc928425cde48fb8a27d476
seanlandsman <sean@thelandsmans.com>
2022-04-06 11:29:40 +0100

    AG-6588 Add ability to dynamically add videos to video page

--- grid-packages/ag-grid-docs/documentation/src/components/LearningVideos.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/LearningVideos.tsx
@@ -3,42 +3,60 @@ import classnames from 'classnames';
 import styles from './LearningVideos.module.scss';
 import {hostPrefix} from '../utils/consts';
 
-const Video = ({title, url, thumbnail, keyPoints}: any) => {
+type VideoData = {
+    title: string;
+    url: string;
+    thumbnail: {
+        image: string;
+        altText: string;
+    }
+    keyPoints?: string[];
+    runningTime: string;
+}
+
+const Video = ({title, url, thumbnail, keyPoints, runningTime, index}: any) => {
     return (
         <div className={classnames(styles["learning-videos__video"])}>
-            <h2>{title}</h2>
-            <div className={classnames(styles["learning-videos__video__body"])}>
-                <div className={classnames(styles["learning-videos__video__body__video-content"])}>
-                    <a href={url} target="_blank">
-                        <img alt={thumbnail.alt} src={`${hostPrefix}/learning-videos/${thumbnail.image}`}/>
-                    </a>
+            <a href={url} target="_blank">
+                <div className={classnames(styles["learning-videos__video__anchor-body"])} >
+                    <div className={classnames(styles['learning-videos__video__anchor-body__index']) }>{index}</div>
+                    <div>
+                        <h2>{title}</h2>
+                        <div className={classnames(styles["learning-videos__video__anchor-body__body"])}>
+                            <div className={classnames(styles["learning-videos__video__anchor-body__body__video-content"])}>
+                                <img style={{height: 100}} alt={thumbnail.alt} src={`${hostPrefix}/videos/${thumbnail.image}`}/>
+                                <h5 className={classnames(styles["learning-videos__video__anchor-body__body__video-content__running-time"])}>{runningTime}</h5>
+                            </div>
+                            <div>
+                                <ul>
+                                    {
+                                        keyPoints.map((keyPoint: string) => <li key={keyPoint}>{keyPoint}</li>)
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <ul>
-                        {
-                            keyPoints.map((keyPoint: string) => <li>{keyPoint}</li>)
-                        }
-                    </ul>
-                </div>
-            </div>
+            </a>
         </div>
     )
 }
-const LearningVideos = (props: any) => {
-    const [videos, setVideos] = useState([]);
+const LearningVideos = () => {
+    const [videos, setVideos] = useState<VideoData[]>([]);
 
     useEffect(() => {
-        fetch(`${hostPrefix}/learning-videos/videos.json`)
+        fetch(`${hostPrefix}/videos/videos.json`)
             .then(response => response.json())
             .then(resultData => {
                 setVideos(resultData);
             })
     }, [])
 
+
     return (
         <div className={classnames(styles["learning-videos"])}>
-            {videos.map(video => {
-                return (<Video {...video} key={video.url}/>)
+            {videos.map((video, index) => {
+                return (<Video {...video} index={index + 1} key={video.url}/>)
             })}
         </div>
     )
