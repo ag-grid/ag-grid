4edec75f562910a8b01f2559ffa4e5c8f3a5e3d1
Alan Treadway <alan.treadway@ag-grid.com>
2022-05-11 10:49:03 +0100

    AG-6761 - Add axis label rotation and skipping example.

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-axes/examples/axis-label-rotation/data.ts
@@ -0,0 +1,35 @@
+const years: number[] = [];
+for (let year = 2005; year <= 2022; year++) {
+    years.push(year);
+}
+
+const countries = [
+    'Ireland',
+    'Spain',
+    'United Kingdom',
+    'France',
+    'Germany',
+    'Luxembourg',
+    'Sweden',
+    'Norway',
+    'Italy',
+    'Greece',
+    'Iceland',
+    'Portugal',
+    'Malta',
+    'Brazil',
+    'Argentina',
+    'Colombia',
+    'Peru',
+    'Venezuela',
+    'Uruguay',
+    'Belgium',
+];
+
+export function getData(): any[] {
+    return years.map((year, idx) => ({
+        year,
+        country: countries[idx],
+        value: Math.round(Math.random() * 1000),
+    }));
+}
