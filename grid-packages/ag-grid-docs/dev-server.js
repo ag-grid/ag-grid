87d5a9802eac5ae59e952bc82ac4815ef6884efa
seanlandsman <sean@thelandsmans.com>
2022-05-23 11:33:13 +0100

    AG-6422 Improve build docs times

--- grid-packages/ag-grid-docs/dev-server.js
+++ grid-packages/ag-grid-docs/dev-server.js
@@ -1,3 +1,4 @@
+const v8 = require('node:v8');
 const os = require('os');
 const fs = require('fs-extra');
 const cp = require('child_process');
@@ -466,7 +467,7 @@ const watchCoreModules = async (skipFrameworks) => {
 };
 
 const updateCoreModuleHashes = () => {
-    const coreModuleRootNames = ['community-modules', 'enterprise-modules'];
+    const coreModuleRootNames = ['community-modules', 'enterprise-modules', 'charts-packages'];
     const exclusions = ['react', 'angular', 'vue', 'vue3', 'polymer'];
 
     coreModuleRootNames.forEach(moduleRootName => {
@@ -666,7 +667,12 @@ const readModulesState = () => {
     return modulesState;
 };
 
+// 2:18
 module.exports = async (skipFrameworks, skipExampleFormatting, done) => {
+    const {total_available_size} = v8.getHeapStatistics();
+    console.log('**************************************');
+    console.log(`** Total Available Heap Size: ~${(total_available_size / Math.pow(1024, 3)).toFixed(0)}GB **`);
+    console.log('**************************************');
     tcpPortUsed.check(EXPRESS_HTTP_PORT)
         .then(async (inUse) => {
             if (inUse) {
