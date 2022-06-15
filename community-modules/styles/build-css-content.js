e9ac4dd01b0844b6328be972723fc798479c39f1
Bernie Sumption <bernie@berniecode.com>
2022-06-15 11:50:13 +0100

    Put compiled CSS at styles module root

--- community-modules/styles/build-css-content.js
+++ community-modules/styles/build-css-content.js
@@ -1,12 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const scriptName = path.basename(__filename);
 
-const distFolderName = "dist";
-const distFolder = path.join(__dirname, distFolderName);
+const distFolder = __dirname;
 const outputFileName = "_css-content.scss"
 const outputFile = path.join(distFolder, outputFileName);
 
 const cssFiles = fs.readdirSync(distFolder).filter(file => file.endsWith(".css"));
+
+// sanity check built files are present
+if (!cssFiles.includes("ag-theme-alpine.css")) {
+    console.error(`ag-theme-alpine.css not present, somehow ${scriptName} has run before the build`);
+    process.exit(1);
+}
+if (!cssFiles.includes("agGridAlpineFont.css")) {
+    console.error(`agGridAlpineFont.css not present, somehow ${scriptName} has run before the build`);
+    process.exit(1);
+}
 
 const mixins = cssFiles
     .map((file, index) => {
@@ -22,7 +32,7 @@ const mixins = cssFiles
 const content = `
 // THIS FILE IS GENERATED, DO NOT EDIT IT!
 
-// Output compiled CSS content for a file in the dist folder, where $file is one of:
+// Output the content of a compiled CSS file, where $file is one of:
 //     - ${cssFiles.join("\n//     - ")}
 @mixin output-css-file($file, $ignore-missing: false) {
     ${mixins}
@@ -33,4 +43,4 @@ const content = `
 `;
 
 fs.writeFileSync(outputFile, content, "utf8");
-console.log(`Built ${cssFiles.length} CSS files into ${distFolderName}/${outputFileName} (${cssFiles.join(", ")})`);
+console.log(`Built ${cssFiles.length} CSS files into ${outputFileName} (${cssFiles.join(", ")})`);
