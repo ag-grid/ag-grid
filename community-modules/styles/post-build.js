28fbb88c0870230522e3754141734abdc05fbe14
Bernie Sumption <bernie@berniecode.com>
2022-06-15 15:14:03 +0100

    Minify CSS files

--- community-modules/styles/post-build.js
+++ community-modules/styles/post-build.js
@@ -1,12 +1,16 @@
 const fs = require('fs');
 const path = require('path');
+const { minify } = require('csso');
+
 const scriptName = path.basename(__filename);
 
 const distFolder = __dirname;
 const outputFileName = "_css-content.scss"
 const outputFile = path.join(distFolder, outputFileName);
 
-const cssFiles = fs.readdirSync(distFolder).filter(file => file.endsWith(".css"));
+const cssFiles = fs.readdirSync(distFolder)
+    .filter(file => !file.endsWith(".min.css"))
+    .filter(file => file.endsWith(".css"));
 
 // sanity check built files are present
 if (!cssFiles.includes("ag-theme-alpine.css")) {
@@ -18,7 +22,7 @@ if (!cssFiles.includes("agGridAlpineFont.css")) {
     process.exit(1);
 }
 
-const mixins = cssFiles
+const fileBlocks = cssFiles
     .map((file, index) => {
         const content = fs.readFileSync(path.join(distFolder, file), "utf8");
         const operator = index === 0 ? "@if" : "@else if";
@@ -29,13 +33,20 @@ const mixins = cssFiles
     })
     .join("");
 
+for (const file of cssFiles) {
+    const content = fs.readFileSync(file, "utf8");
+    const minifiedContent = minify(content).css;
+    const miniffiedName = file.replace(/\.css$/, ".min.css");
+    fs.writeFileSync(miniffiedName, minifiedContent, "utf8");
+}
+
 const content = `
 // THIS FILE IS GENERATED, DO NOT EDIT IT!
 
 // Output the content of a compiled CSS file, where $file is one of:
 //     - ${cssFiles.join("\n//     - ")}
 @mixin output-css-file($file, $ignore-missing: false) {
-    ${mixins}
+    ${fileBlocks}
     @else if not $ignore-missing {
         @error "No such file #{$file}, try one of: ${cssFiles.join(", ")}";
     }
