b57c71a17081c5b87a89fa7b1404be7debb16b40
Mana <mana.jalali.15@ucl.ac.uk>
2022-06-30 12:44:13 +0100

    AG-5105 - Add more crossLine label positions

--- charts-packages/ag-charts-community/src/chart/crossline/crossLineLabelPosition.ts
+++ charts-packages/ag-charts-community/src/chart/crossline/crossLineLabelPosition.ts
@@ -5,6 +5,10 @@ export type CrossLineLabelPosition =
     | 'left'
     | 'right'
     | 'bottom'
+    | 'topLeft'
+    | 'topRight'
+    | 'bottomLeft'
+    | 'bottomRight'
     | 'inside'
     | 'insideLeft'
     | 'insideRight'
@@ -38,6 +42,10 @@ const horizontalCrosslineTranslationDirections: Record<CrossLineLabelPosition, C
     bottom: { xTranslationDirection: 0, yTranslationDirection: 1 },
     left: { xTranslationDirection: -1, yTranslationDirection: 0 },
     right: { xTranslationDirection: 1, yTranslationDirection: 0 },
+    topLeft: { xTranslationDirection: 1, yTranslationDirection: -1  },
+    topRight: { xTranslationDirection: -1, yTranslationDirection: -1 },
+    bottomLeft: { xTranslationDirection: 1, yTranslationDirection: 1 },
+    bottomRight: { xTranslationDirection: -1, yTranslationDirection: 1 },
     inside: { xTranslationDirection: 0, yTranslationDirection: 0 },
     insideLeft: { xTranslationDirection: 1, yTranslationDirection: 0 },
     insideRight: { xTranslationDirection: -1, yTranslationDirection: 0 },
@@ -54,6 +62,10 @@ const verticalCrossLineTranslationDirections: Record<CrossLineLabelPosition, Cro
     bottom: { xTranslationDirection: -1, yTranslationDirection: 0 },
     left: { xTranslationDirection: 0, yTranslationDirection: -1 },
     right: { xTranslationDirection: 0, yTranslationDirection: 1 },
+    topLeft: { xTranslationDirection: -1, yTranslationDirection: -1  },
+    topRight: { xTranslationDirection: -1, yTranslationDirection: 1 },
+    bottomLeft: { xTranslationDirection: 1, yTranslationDirection: -1 },
+    bottomRight: { xTranslationDirection: 1, yTranslationDirection: 1 },
     inside: { xTranslationDirection: 0, yTranslationDirection: 0 },
     insideLeft: { xTranslationDirection: 0, yTranslationDirection: 1 },
     insideRight: { xTranslationDirection: 0, yTranslationDirection: -1 },
@@ -112,7 +124,7 @@ export const POSITION_INSIDE_COORDINATES: CoordinatesFn = ({ xEnd, yStart, yEnd 
     return { x: xEnd / 2, y: !isNaN(yEnd) ? (yStart + yEnd) / 2 : yStart }
 }
 
-export const POSITION_INSIDE_TOP_LEFT_COORDINATES: CoordinatesFn = ({ yDirection, xStart, xEnd, yStart, yEnd }) => {
+export const POSITION_TOP_LEFT_COORDINATES: CoordinatesFn = ({ yDirection, xStart, xEnd, yStart, yEnd }) => {
     if (yDirection) {
         return { x: xStart / 2, y: !isNaN(yEnd) ? yEnd : yStart }
     } else {
@@ -120,15 +132,15 @@ export const POSITION_INSIDE_TOP_LEFT_COORDINATES: CoordinatesFn = ({ yDirection
     }
 }
 
-export const POSITION_INSIDE_BOTTOM_LEFT_COORDINATES: CoordinatesFn = ({ xStart, yStart }) => {
+export const POSITION_BOTTOM_LEFT_COORDINATES: CoordinatesFn = ({ xStart, yStart }) => {
     return { x: xStart, y: yStart }
 }
 
-export const POSITION_INSIDE_TOP_RIGHT_COORDINATES: CoordinatesFn = ({ xEnd, yStart, yEnd }) => {
+export const POSITION_TOP_RIGHT_COORDINATES: CoordinatesFn = ({ xEnd, yStart, yEnd }) => {
     return { x: xEnd, y: !isNaN(yEnd) ? yEnd : yStart }
 }
 
-export const POSITION_INSIDE_BOTTOM_RIGHT_COORDINATES: CoordinatesFn = ({ yDirection, xStart, xEnd, yStart, yEnd }) => {
+export const POSITION_BOTTOM_RIGHT_COORDINATES: CoordinatesFn = ({ yDirection, xStart, xEnd, yStart, yEnd }) => {
     if (yDirection) {
         return { x: xEnd, y: yStart }
     } else {
@@ -141,13 +153,17 @@ export const labeldDirectionHandling: Record<CrossLineLabelPosition, PositionCal
     bottom: { c: POSITION_BOTTOM_COORDINATES },
     left: { c: POSITION_LEFT_COORDINATES },
     right: { c: POSITION_RIGHT_COORDINATES },
+    topLeft: { c: POSITION_TOP_LEFT_COORDINATES  },
+    topRight: { c: POSITION_TOP_RIGHT_COORDINATES },
+    bottomLeft: { c: POSITION_BOTTOM_LEFT_COORDINATES },
+    bottomRight: { c: POSITION_BOTTOM_RIGHT_COORDINATES },
     inside: { c: POSITION_INSIDE_COORDINATES },
     insideLeft: { c: POSITION_LEFT_COORDINATES },
     insideRight: { c: POSITION_RIGHT_COORDINATES },
     insideTop: { c: POSITION_TOP_COORDINATES },
     insideBottom: { c: POSITION_BOTTOM_COORDINATES },
-    insideTopLeft: { c: POSITION_INSIDE_TOP_LEFT_COORDINATES },
-    insideBottomLeft: { c: POSITION_INSIDE_BOTTOM_LEFT_COORDINATES },
-    insideTopRight: { c: POSITION_INSIDE_TOP_RIGHT_COORDINATES },
-    insideBottomRight: { c: POSITION_INSIDE_BOTTOM_RIGHT_COORDINATES },
+    insideTopLeft: { c: POSITION_TOP_LEFT_COORDINATES },
+    insideBottomLeft: { c: POSITION_BOTTOM_LEFT_COORDINATES },
+    insideTopRight: { c: POSITION_TOP_RIGHT_COORDINATES },
+    insideBottomRight: { c: POSITION_BOTTOM_RIGHT_COORDINATES },
 }
