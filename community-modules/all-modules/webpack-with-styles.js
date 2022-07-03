198abaaadcd5c16081f6983f3c2a21805e455c4c
seanlandsman <sean@thelandsmans.com>
2022-07-03 19:55:38 +0100

    Release 28.0.0 prep

--- community-modules/all-modules/webpack-with-styles.js
+++ community-modules/all-modules/webpack-with-styles.js
@@ -13,15 +13,3 @@ Object.keys(agGrid).forEach(function(key) {
 agGrid.ModuleRegistry.register(ClientSideRowModelModule.ClientSideRowModelModule);
 agGrid.ModuleRegistry.register(CsvExportModule.CsvExportModule);
 agGrid.ModuleRegistry.register(InfiniteRowModelModule.InfiniteRowModelModule);
-require('./styles/ag-grid-no-native-widgets.css');
-require('./styles/ag-grid.css');
-require('./styles/ag-theme-alpine-no-font.css');
-require('./styles/ag-theme-alpine.css');
-require('./styles/ag-theme-balham-no-font.css');
-require('./styles/ag-theme-balham.css');
-require('./styles/ag-theme-material-no-font.css');
-require('./styles/ag-theme-material.css');
-require('./styles/agGridAlpineFont.css');
-require('./styles/agGridBalhamFont.css');
-require('./styles/agGridClassicFont.css');
-require('./styles/agGridMaterialFont.css');
