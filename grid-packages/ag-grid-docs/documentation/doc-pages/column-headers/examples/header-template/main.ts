c9d0651ac400c9ecb4d2ab1817ea6c3461e46377
Guilherme Lopes <guilherme@gwm-solutions.com>
2022-07-11 06:46:06 -0300

    AG-6973 - fixed issue with sortIcons not working with custom templates (#5342)
    * AG-6973 - fixed issue with sortIcons not working with custom templates
    
    * AG-6973 - Update column headers template example
    
    Co-authored-by: Andrew Glazier <andrew.glazier@ag-grid.com>

--- grid-packages/ag-grid-docs/documentation/doc-pages/column-headers/examples/header-template/main.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/column-headers/examples/header-template/main.ts
@@ -24,15 +24,16 @@ const gridOptions: GridOptions<IOlympicData> = {
     width: 150,
     headerComponentParams: {
       menuIcon: 'fa-bars',
-      template: `<div class="ag-cell-label-container" role="presentation">  
-                    <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button"></span>  
-                    <div ref="eLabel" class="ag-header-cell-label" role="presentation">    
-                        <span ref="eSortOrder" class="ag-header-icon ag-sort-order" ></span>    
-                        <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon" ></span>    
-                        <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon" ></span>    
-                        <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon" ></span>    
-                        ** <span ref="eText" class="ag-header-cell-text" role="columnheader"></span>    
-                        <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>  
+      template: `<div class="ag-cell-label-container" role="presentation">
+                    <span ref="eMenu" class="ag-header-icon ag-header-cell-menu-button"></span>
+                    <div ref="eLabel" class="ag-header-cell-label" role="presentation">
+                        <span ref="eSortOrder" class="ag-header-icon ag-sort-order ag-hidden"></span>
+                        <span ref="eSortAsc" class="ag-header-icon ag-sort-ascending-icon ag-hidden"></span>
+                        <span ref="eSortDesc" class="ag-header-icon ag-sort-descending-icon ag-hidden"></span>
+                        <span ref="eSortMixed" class="ag-header-icon ag-sort-mixed-icon ag-hidden"></span>
+                        <span ref="eSortNone" class="ag-header-icon ag-sort-none-icon ag-hidden"></span>
+                        ** <span ref="eText" class="ag-header-cell-text" role="columnheader"></span>
+                        <span ref="eFilter" class="ag-header-icon ag-filter-icon"></span>
                     </div>
                 </div>`,
     },
