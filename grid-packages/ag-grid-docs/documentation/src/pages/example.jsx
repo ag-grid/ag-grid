99e99b577b06622d4a01d6f78441cda216d380b2
seanlandsman <sean@thelandsmans.com>
2022-07-25 11:12:55 +0100

    AG-7016 Demo page on the site has the title "Cookie Policy" and description to match

--- grid-packages/ag-grid-docs/documentation/src/pages/example.jsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/example.jsx
@@ -4,8 +4,8 @@ import Example from "../mainPageDemo/Example";
 
 const ExamplePage = () => {
     return (<>
-            <Seo title="AG Grid: Cookies Policy"
-                 description="This page outlines our policy in relation to the cookies that we collect on our website."/>
+            <Seo title="AG Grid: Demo of high performance datagrid"
+                 description="AG Grid is a feature-rich datagrid available in Community or Enterprise versions. This is our fully interactive demo showcasing all of our features and our performance with large datasets."/>
             <Example/>
         </>
     )
