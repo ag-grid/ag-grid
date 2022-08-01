8e5129c5de44e17053504cb378ff2214cdf8bf2a
seanlandsman <sean@thelandsmans.com>
2022-08-01 13:39:36 +0100

    RTI-1009 R - Components Register Example needs fixing

--- grid-packages/ag-grid-docs/documentation/doc-pages/components/examples/register/medal-renderer.component_angular.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/components/examples/register/medal-renderer.component_angular.ts
@@ -6,17 +6,19 @@ import {ICellRendererParams} from "@ag-grid-community/core";
     selector: 'total-value-component',
     template: `
         <span>
-             <span>{{cellValue}}</span>&nbsp;
+             <span>{{country}}</span>&nbsp;
              <button (click)="buttonClicked()">Push For Total</button>
         </span>
     `
 })
 export class MedalRenderer implements ICellRendererAngularComp {
-    public cellValue: string = '';
+    public country: string = '';
+    public total: string = '';
 
     // gets called once before the renderer is used
     agInit(params: ICellRendererParams): void {
-        this.cellValue = params.valueFormatted ? params.valueFormatted : params.value;
+        this.country = params.valueFormatted ? params.valueFormatted : params.value;
+        this.total = params.data.total;
     }
 
     refresh(params: ICellRendererParams) {
@@ -24,6 +26,6 @@ export class MedalRenderer implements ICellRendererAngularComp {
     }
 
     buttonClicked() {
-        alert(`${this.cellValue} medals won!`)
+        alert(`${this.total} medals won!`)
     }
 }
