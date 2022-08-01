c5ee312b3c48a8e6ebe094d7d54bcb4749acfe0b
seanlandsman <sean@thelandsmans.com>
2022-08-01 07:53:03 +0100

    Update from latest

--- grid-packages/ag-grid-enterprise/dist/ag-grid-enterprise.cjs.js
+++ grid-packages/ag-grid-enterprise/dist/ag-grid-enterprise.cjs.js
@@ -51895,7 +51895,7 @@ var GridChartComp = /** @class */ (function (_super) {
 
 // the line below is automatically modified during releases - do not modify
 // (see scripts/release/updateChartModel.js)
-var CURRENT_VERSION = "28.0.1";
+var CURRENT_VERSION = "28.1.0";
 function upgradeChartModel(model) {
     if (model.version == null) {
         // First release with version field.
