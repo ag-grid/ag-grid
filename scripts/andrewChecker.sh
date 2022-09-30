8c0ca96da5006565d48b5a346ab311594656910b
seanlandsman <sean@thelandsmans.com>
2022-09-30 12:22:40 +0100

    Update andrewChecker

--- scripts/andrewChecker.sh
+++ scripts/andrewChecker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INSTANCES=`grep -R --exclude-dir={node_modules,lib,dist,.cache,_dev,_gen} --include=\*.ts --include=\*.js --include=\*.tsx --include=\*.jsx  'debugger' . | awk '{$1=$1};1' | grep -v ./enterprise-modules/sparklines/src/util/observable.ts | wc -l`
+INSTANCES=`grep -R --exclude-dir={node_modules,lib,dist,.cache,_dev,_gen} --include=\*.ts --include=\*.js --include=\*.tsx --include=\*.jsx  'debugger' . | awk '{$1=$1};1' | grep -v ./enterprise-modules/sparklines/src/util/observable.ts | grep -v grid-packages/ag-grid-docs/documentation/public | wc -l`
 
 if [ $INSTANCES -ne 0 ]
 then
