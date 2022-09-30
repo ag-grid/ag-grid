85cf086e49e49eade559e3a74efb90556932d8f6
Alexander Shutau <shutovby@gmail.com>
2022-09-30 14:44:40 +0100

    AG-7292 Update Doughnut example in the Gallery
    Add labels. The thumbnail generation didn't work.

--- grid-packages/ag-grid-docs/documentation/doc-pages/charts-overview/examples/simple-doughnut/main.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-overview/examples/simple-doughnut/main.ts
@@ -2,10 +2,14 @@ import * as agCharts from 'ag-charts-community';
 import { AgChartOptions } from 'ag-charts-community';
 import { getData } from "./data";
 
+const data = getData();
+const numberFormatter = new Intl.NumberFormat('en-US');
+const total = data.reduce((sum, d) => sum + d['2018/19'], 0);
+
 const options: AgChartOptions = {
   container: document.getElementById('myChart'),
   autoSize: true,
-  data: getData(),
+  data,
   title: {
     text: 'Dwelling Fires (UK)',
     fontSize: 18,
@@ -20,13 +24,32 @@ const options: AgChartOptions = {
       fillOpacity: 0.9,
       strokeWidth: 0,
       angleKey: '2018/19',
+      sectorLabelKey: '2018/19',
       label: {
         enabled: false,
+      },
+      sectorLabel: {
+        color: 'white',
+        fontWeight: 'bold',
+        formatter: ({ datum, sectorLabelKey }) => {
+          const value = datum[sectorLabelKey!];
+          return numberFormatter.format(value);
+        }
       },
       title: {
         text: '2018/19',
       },
-      innerRadiusOffset: -100,
+      innerRadiusRatio: 0.5,
+      innerLabels: [
+        {
+          text: numberFormatter.format(total),
+          fontSize: 18,
+          fontWeight: 'bold',
+        },
+        {
+          text: 'Total',
+        },
+      ],
     },
   ],
 }
