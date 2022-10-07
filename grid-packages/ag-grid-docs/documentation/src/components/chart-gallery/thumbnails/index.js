5229688b1fd4153ba535e03318219d4bbd0f40c2
Alan Treadway <alan.treadway@ag-grid.com>
2022-10-07 12:45:36 +0100

    AG-7323 - Extend scope of Chart gallery generation tool.

--- grid-packages/ag-grid-docs/documentation/src/components/chart-gallery/thumbnails/index.js
+++ grid-packages/ag-grid-docs/documentation/src/components/chart-gallery/thumbnails/index.js
@@ -1,77 +1,75 @@
-import thumbnail100StackedArea from './100--stacked-area.png';
+import thumbnailSimpleBar from './simple-bar.png';
+import thumbnailGroupedBar from './grouped-bar.png';
+import thumbnailStackedBar from './stacked-bar.png';
 import thumbnail100StackedBar from './100--stacked-bar.png';
-import thumbnail100StackedColumn from './100--stacked-column.png';
-import thumbnailAreaWithNegativeValues from './area-with-negative-values.png';
 import thumbnailBarWithLabels from './bar-with-labels.png';
-import thumbnailBubbleWithCategories from './bubble-with-categories.png';
-import thumbnailBubbleWithNegativeValues from './bubble-with-negative-values.png';
-import thumbnailChartCustomisation from './chart-customisation.png';
+import thumbnailSimpleColumn from './simple-column.png';
+import thumbnailGroupedColumn from './grouped-column.png';
+import thumbnailStackedColumn from './stacked-column.png';
+import thumbnail100StackedColumn from './100--stacked-column.png';
 import thumbnailColumnWithNegativeValues from './column-with-negative-values.png';
-import thumbnailCombinationofDifferentSeriesTypes from './combination-of-different-series-types.png';
+import thumbnailSimplePie from './simple-pie.png';
+import thumbnailSimpleDoughnut from './simple-doughnut.png';
+import thumbnailSimpleLine from './simple-line.png';
+import thumbnailLineWithGaps from './line-with-gaps.png';
+import thumbnailSimpleScatter from './simple-scatter.png';
+import thumbnailSimpleBubble from './simple-bubble.png';
+import thumbnailBubbleWithNegativeValues from './bubble-with-negative-values.png';
+import thumbnailBubbleWithCategories from './bubble-with-categories.png';
+import thumbnailSimpleArea from './simple-area.png';
+import thumbnailStackedArea from './stacked-area.png';
+import thumbnail100StackedArea from './100--stacked-area.png';
+import thumbnailAreaWithNegativeValues from './area-with-negative-values.png';
+import thumbnailSimpleHistogram from './simple-histogram.png';
+import thumbnailHistogramWithSpecifiedBins from './histogram-with-specified-bins.png';
+import thumbnailXyHistogramWithMeanAggregation from './xy-histogram-with-mean-aggregation.png';
+import thumbnailMarketIndexTreemap from './market-index-treemap.png';
 import thumbnailCrossLines from './cross-lines.png';
+import thumbnailTimeAxisWithIrregularIntervals from './time-axis-with-irregular-intervals.png';
+import thumbnailLogAxis from './log-axis.png';
+import thumbnailRealTimeDataUpdates from './real-time-data-updates.png';
+import thumbnailCombinationOfDifferentSeriesTypes from './combination-of-different-series-types.png';
+import thumbnailChartCustomisation from './chart-customisation.png';
 import thumbnailCustomMarkerShapes from './custom-marker-shapes.png';
 import thumbnailCustomTooltips from './custom-tooltips.png';
-import thumbnailGroupedBar from './grouped-bar.png';
-import thumbnailGroupedColumn from './grouped-column.png';
-import thumbnailHistogramWithSpecifiedBins from './histogram-with-specified-bins.png';
-import thumbnailLargeDatasets from './large-datasets.png';
-import thumbnailLineWithGaps from './line-with-gaps.png';
-import thumbnailLogAxis from './log-axis.png';
-import thumbnailMarketIndexTreemap from './market-index-treemap.png';
 import thumbnailPerMarkerCustomisation from './per-marker-customisation.png';
-import thumbnailRealTimeDataUpdates from './real-time-data-updates.png';
-import thumbnailSimpleArea from './simple-area.png';
-import thumbnailSimpleBar from './simple-bar.png';
-import thumbnailSimpleBubble from './simple-bubble.png';
-import thumbnailSimpleColumn from './simple-column.png';
-import thumbnailSimpleDoughnut from './simple-doughnut.png';
-import thumbnailSimpleHistogram from './simple-histogram.png';
-import thumbnailSimpleLine from './simple-line.png';
-import thumbnailSimplePie from './simple-pie.png';
-import thumbnailSimpleScatter from './simple-scatter.png';
-import thumbnailStackedArea from './stacked-area.png';
-import thumbnailStackedBar from './stacked-bar.png';
-import thumbnailStackedColumn from './stacked-column.png';
-import thumbnailTimeAxisWithIrregularIntervals from './time-axis-with-irregular-intervals.png';
-import thumbnailXYHistogramWithMeanAggregation from './xy-histogram-with-mean-aggregation.png';
 
 const thumbnails = {
-    '100--stacked-area': thumbnail100StackedArea,
+    'simple-bar': thumbnailSimpleBar,
+    'grouped-bar': thumbnailGroupedBar,
+    'stacked-bar': thumbnailStackedBar,
     '100--stacked-bar': thumbnail100StackedBar,
-    '100--stacked-column': thumbnail100StackedColumn,
-    'area-with-negative-values': thumbnailAreaWithNegativeValues,
     'bar-with-labels': thumbnailBarWithLabels,
-    'bubble-with-categories': thumbnailBubbleWithCategories,
-    'bubble-with-negative-values': thumbnailBubbleWithNegativeValues,
-    'chart-customisation': thumbnailChartCustomisation,
+    'simple-column': thumbnailSimpleColumn,
+    'grouped-column': thumbnailGroupedColumn,
+    'stacked-column': thumbnailStackedColumn,
+    '100--stacked-column': thumbnail100StackedColumn,
     'column-with-negative-values': thumbnailColumnWithNegativeValues,
-    'combination-of-different-series-types': thumbnailCombinationofDifferentSeriesTypes,
+    'simple-pie': thumbnailSimplePie,
+    'simple-doughnut': thumbnailSimpleDoughnut,
+    'simple-line': thumbnailSimpleLine,
+    'line-with-gaps': thumbnailLineWithGaps,
+    'simple-scatter': thumbnailSimpleScatter,
+    'simple-bubble': thumbnailSimpleBubble,
+    'bubble-with-negative-values': thumbnailBubbleWithNegativeValues,
+    'bubble-with-categories': thumbnailBubbleWithCategories,
+    'simple-area': thumbnailSimpleArea,
+    'stacked-area': thumbnailStackedArea,
+    '100--stacked-area': thumbnail100StackedArea,
+    'area-with-negative-values': thumbnailAreaWithNegativeValues,
+    'simple-histogram': thumbnailSimpleHistogram,
+    'histogram-with-specified-bins': thumbnailHistogramWithSpecifiedBins,
+    'xy-histogram-with-mean-aggregation': thumbnailXyHistogramWithMeanAggregation,
+    'market-index-treemap': thumbnailMarketIndexTreemap,
     'cross-lines': thumbnailCrossLines,
+    'time-axis-with-irregular-intervals': thumbnailTimeAxisWithIrregularIntervals,
+    'log-axis': thumbnailLogAxis,
+    'real-time-data-updates': thumbnailRealTimeDataUpdates,
+    'combination-of-different-series-types': thumbnailCombinationOfDifferentSeriesTypes,
+    'chart-customisation': thumbnailChartCustomisation,
     'custom-marker-shapes': thumbnailCustomMarkerShapes,
     'custom-tooltips': thumbnailCustomTooltips,
-    'grouped-bar': thumbnailGroupedBar,
-    'grouped-column': thumbnailGroupedColumn,
-    'histogram-with-specified-bins': thumbnailHistogramWithSpecifiedBins,
-    'large-datasets': thumbnailLargeDatasets,
-    'line-with-gaps': thumbnailLineWithGaps,
-    'log-axis': thumbnailLogAxis,
-    'market-index-treemap': thumbnailMarketIndexTreemap,
     'per-marker-customisation': thumbnailPerMarkerCustomisation,
-    'real-time-data-updates': thumbnailRealTimeDataUpdates,
-    'simple-area': thumbnailSimpleArea,
-    'simple-bar': thumbnailSimpleBar,
-    'simple-bubble': thumbnailSimpleBubble,
-    'simple-column': thumbnailSimpleColumn,
-    'simple-doughnut': thumbnailSimpleDoughnut,
-    'simple-histogram': thumbnailSimpleHistogram,
-    'simple-line': thumbnailSimpleLine,
-    'simple-pie': thumbnailSimplePie,
-    'simple-scatter': thumbnailSimpleScatter,
-    'stacked-area': thumbnailStackedArea,
-    'stacked-bar': thumbnailStackedBar,
-    'stacked-column': thumbnailStackedColumn,
-    'time-axis-with-irregular-intervals': thumbnailTimeAxisWithIrregularIntervals,
-    'xy-histogram-with-mean-aggregation': thumbnailXYHistogramWithMeanAggregation,
-}
+};
 
 export default thumbnails;
