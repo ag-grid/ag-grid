775b0d73e951226b1fbe74894050859dc0a67a1c
Stephen Cooper <scooperdev@gmail.com>
2022-10-18 10:03:03 +0100

    Explicitly implement Angular lifecycle methods

--- community-modules/angular-legacy/projects/ag-grid-angular/src/lib/ag-grid-angular.component.ts
+++ community-modules/angular-legacy/projects/ag-grid-angular/src/lib/ag-grid-angular.component.ts
@@ -6,6 +6,8 @@ import {
     ElementRef,
     EventEmitter,
     Input,
+    OnChanges,
+    OnDestroy,
     Output,
     QueryList,
     ViewContainerRef,
@@ -171,7 +173,7 @@ import { AgGridColumn } from "./ag-grid-column.component";
     // tell angular we don't want view encapsulation, we don't want a shadow root
     encapsulation: ViewEncapsulation.None
 })
-export class AgGridAngular<TData = any, TColDef extends ColDef<TData> = ColDef<any>> implements AfterViewInit {
+export class AgGridAngular<TData = any, TColDef extends ColDef<TData> = ColDef<any>> implements AfterViewInit, OnChanges, OnDestroy {
     // not intended for user to interact with. so putting _ in so if user gets reference
     // to this object, they kind'a know it's not part of the agreed interface
     private _nativeElement: any;
