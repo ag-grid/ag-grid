3104598d9f7462ec19076047fe16d4bbe2ab09bd
Stephen Cooper <scooperdev@gmail.com>
2022-10-18 10:47:59 +0100

    AG-7396 Add missing prop key

--- community-modules/core/src/ts/propertyKeys.ts
+++ community-modules/core/src/ts/propertyKeys.ts
@@ -68,7 +68,7 @@ export class PropertyKeys {
         'suppressModelUpdateAfterUpdateTransaction', 'stopEditingWhenCellsLoseFocus', 'maintainColumnOrder', 'groupMaintainOrder',
         'columnHoverHighlight', 'reactUi', 'suppressReactUi', 'readOnlyEdit', 'suppressRowVirtualisation', 'enableCellEditingOnBackspace',
         'resetRowDataOnUpdate', 'removePivotHeaderRowWhenSingleValueColumn', 'suppressCopySingleCellRanges',
-        'groupRowsSticky', 'serverSideInfiniteScroll', 'rowGroupPanelSuppressSort',
+        'groupRowsSticky', 'serverSideInfiniteScroll', 'rowGroupPanelSuppressSort', 'allowShowChangeAfterFilter'
     ];
 
     /** You do not need to include event callbacks in this list, as they are generated automatically. */
