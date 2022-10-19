9cdd8579b64d2a03f2478f04c340b86671a5bd7c
Guilherme Lopes <guilherme@gwm-solutions.com>
2022-10-19 15:56:51 +0100

    AG-7372 - fixed unsafe string rendered as SideBar label

--- enterprise-modules/side-bar/src/sideBar/sideBarButtonComp.ts
+++ enterprise-modules/side-bar/src/sideBar/sideBarButtonComp.ts
@@ -12,6 +12,7 @@ export class SideBarButtonComp extends Component {
 
     @RefSelector('eToggleButton') private eToggleButton: HTMLButtonElement;
     @RefSelector('eIconWrapper') private eIconWrapper: HTMLElement;
+    @RefSelector('eLabel') private eLabel: HTMLElement;
 
     private readonly toolPanelDef: ToolPanelDef;
 
@@ -28,22 +29,32 @@ export class SideBarButtonComp extends Component {
     private postConstruct(): void {
         const template = this.createTemplate();
         this.setTemplate(template);
-        this.eIconWrapper.insertAdjacentElement('afterbegin', _.createIconNoSpan(this.toolPanelDef.iconKey, this.gridOptionsWrapper)!);
+        this.setLabel();
+        this.setIcon();
         this.addManagedListener(this.eToggleButton, 'click', this.onButtonPressed.bind(this));
     }
 
     private createTemplate(): string {
-        const translate = this.gridOptionsWrapper.getLocaleTextFunc();
-        const def = this.toolPanelDef;
-        const label = translate(def.labelKey, def.labelDefault);
         const res = /* html */
             `<div class="ag-side-button" role="presentation">
                 <button type="button" ref="eToggleButton" tabindex="-1" role="tab" class="ag-side-button-button">
                     <div ref="eIconWrapper" class="ag-side-button-icon-wrapper" aria-hidden="true"></div>
-                    <span class="ag-side-button-label">${label}</span>
+                    <span ref ="eLabel" class="ag-side-button-label"></span>
                 </button>
             </div>`;
         return res;
+    }
+    
+    private setLabel(): void {
+        const translate = this.gridOptionsWrapper.getLocaleTextFunc();
+        const def = this.toolPanelDef;
+        const label = translate(def.labelKey, def.labelDefault);
+
+        this.eLabel.innerText = label;
+    }
+
+    private setIcon(): void {
+        this.eIconWrapper.insertAdjacentElement('afterbegin', _.createIconNoSpan(this.toolPanelDef.iconKey, this.gridOptionsWrapper)!);
     }
 
     private onButtonPressed(): void {
