924d12db5f18755701309f5ecf7efd2d86fc8896
Alan Treadway <alan.treadway@ag-grid.com>
2022-10-21 12:26:51 +0100

    Charts - split unit and e2e tests to improve stability.

--- charts-packages/ag-charts-community/jest.config.js
+++ charts-packages/ag-charts-community/jest.config.js
@@ -1,13 +1,48 @@
-module.exports = {
+const glob = require('glob');
+const fs = require('fs');
+
+const pathToGlob = (path) => path.replace('./', '**/');
+
+const tests = glob.sync('./src/**/*.test.ts');
+const e2eTests = tests
+    .filter((path) => {
+        const fileContents = fs.readFileSync(path).toString();
+
+        // 'Heuristic' for finding e2e tests :-P
+        return fileContents.indexOf('setupMockCanvas()') >= 0;
+    })
+    .map(pathToGlob);
+const unitTests = tests.map(pathToGlob).filter((path) => !e2eTests.includes(path));
+
+const commonConfig = {
     globals: {
         'ts-jest': {
             tsConfig: 'tsconfig.test.json',
         },
     },
-    roots: ['<rootDir>/src'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+module.exports = {
+    projects: [
+        {
+            displayName: 'unit',
+            roots: ['<rootDir>/src'],
+            testMatch: unitTests,
+            ...commonConfig,
+        },
+        {
+            displayName: 'e2e',
+            roots: ['<rootDir>/src'],
+            testMatch: e2eTests,
+            runner: 'jest-serial-runner',
+            // maxWorkers: 1,
+            // maxConcurrency: 1,
+            // slowTestThreshold: 10,
+            ...commonConfig,
+        },
+    ],
 };
