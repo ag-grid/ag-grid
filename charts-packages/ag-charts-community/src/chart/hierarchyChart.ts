f0c40e3f16329f818c7b2886c8c525cacaacfef6
Alan Treadway <alan.treadway@ag-grid.com>
2022-11-01 12:19:51 +0000

    RTI-1099 - Fix series nodeDataRefresh not happening on navigator add/remove.

--- charts-packages/ag-charts-community/src/chart/hierarchyChart.ts
+++ charts-packages/ag-charts-community/src/chart/hierarchyChart.ts
@@ -73,7 +73,7 @@ export class HierarchyChart extends Chart {
         this.series.forEach((series) => {
             series.group.translationX = Math.floor(shrinkRect.x);
             series.group.translationY = Math.floor(shrinkRect.y);
-            series.update(); // this has to happen after the `updateAxes` call
+            series.update({ seriesRect: shrinkRect }); // this has to happen after the `updateAxes` call
         });
 
         const { seriesRoot } = this;
