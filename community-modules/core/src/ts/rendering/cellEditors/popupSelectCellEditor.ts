86ee6ef9efbc9f9394cc6f83097d7759c2642949
Stephen Cooper <scooperdev@gmail.com>
2022-11-14 16:14:45 +0000

    AG-4172 Add version to deprecation

--- community-modules/core/src/ts/rendering/cellEditors/popupSelectCellEditor.ts
+++ community-modules/core/src/ts/rendering/cellEditors/popupSelectCellEditor.ts
@@ -1,11 +1,14 @@
 import { doOnce } from "../../utils/function";
 import { SelectCellEditor } from "./selectCellEditor";
 
+/**
+ * @deprecated Since v27.1 The PopupSelectCellEditor (agPopupSelectCellEditor) is deprecated. Instead use {cellEditor: "agSelectCellEditor", cellEditorPopup: true} '), 'PopupSelectCellEditor.deprecated
+ */
 export class PopupSelectCellEditor extends SelectCellEditor {
 
     constructor() {
         super();
-        doOnce( ()=> console.warn('AG Grid: The PopupSelectCellEditor (agPopupSelectCellEditor) is deprecated. Instead use {cellEditor: "agSelectCellEditor", cellEditorPopup: true} '), 'PopupSelectCellEditor.deprecated');
+        doOnce(() => console.warn('AG Grid: The PopupSelectCellEditor (agPopupSelectCellEditor) is deprecated. Instead use {cellEditor: "agSelectCellEditor", cellEditorPopup: true} '), 'PopupSelectCellEditor.deprecated');
     }
 
     public isPopup(): boolean {
