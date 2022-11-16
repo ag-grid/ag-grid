c0302eae0ffda2e286065fa908897b81eee33690
Stephen Cooper <scooperdev@gmail.com>
2022-11-16 12:01:04 +0000

    Add deprecated annotations

--- community-modules/core/src/ts/rendering/cellEditors/popupSelectCellEditor.ts
+++ community-modules/core/src/ts/rendering/cellEditors/popupSelectCellEditor.ts
@@ -2,7 +2,7 @@ import { doOnce } from "../../utils/function";
 import { SelectCellEditor } from "./selectCellEditor";
 
 /**
- * @deprecated Since v27.1 The PopupSelectCellEditor (agPopupSelectCellEditor) is deprecated. Instead use {cellEditor: "agSelectCellEditor", cellEditorPopup: true} '), 'PopupSelectCellEditor.deprecated
+ * @deprecated Since v27.1 The PopupSelectCellEditor (agPopupSelectCellEditor) is deprecated. Instead use {cellEditor: "agSelectCellEditor", cellEditorPopup: true}
  */
 export class PopupSelectCellEditor extends SelectCellEditor {
 
