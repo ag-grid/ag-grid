31d40369617f74000014a5fb33deb122d9220a53
seanlandsman <sean@thelandsmans.com>
2022-11-17 17:16:06 +0000

    AG-7500 - Deal with agGridCoreExtention in ag-grid-enterprise

--- enterprise-modules/charts/src/agGridCoreExtension.ts
+++ enterprise-modules/charts/src/agGridCoreExtension.ts
@@ -2,7 +2,7 @@ import * as agCharts from 'ag-charts-community';
 
 /*
 * grid-packages/ag-grid-enterprise is dependant on this file - if changes are made here (esp to it's path)
-* then be sure to check if grid-packages/ag-grid-enterprise/gulpfile.js#copyGridCoreTypings needs to be updated too
+* then be sure to check if grid-packages/ag-grid-enterprise/gulpfile.js#copyAndConcatMainTypings needs to be updated too
 */
 declare module '@ag-grid-community/core' {
     export interface AgChartThemeOverrides extends agCharts.AgChartThemeOverrides {}
