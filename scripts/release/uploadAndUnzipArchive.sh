93f8a04ac8e078fd6b7419d8a769c02e7f616e2b
seanlandsman <sean@thelandsmans.com>
2022-11-21 14:13:52 +0000

    AG-7542 Update archive & release tasks to work with AWS

--- scripts/release/uploadAndUnzipArchive.sh
+++ scripts/release/uploadAndUnzipArchive.sh
@@ -49,16 +49,21 @@ if [ "$2" != "skipWarning" ]; then
 fi
 
 # delete dir if it exists - can ignore dir not found error
+echo "ssh -i $SSH_LOCATION -p $SSH_PORT $HOST \"cd $PUBLIC_HTML_PATH/archive/ && [[ -d $VERSION ]] && rm -r $VERSION\""
 ssh -i $SSH_LOCATION -p $SSH_PORT $HOST "cd $PUBLIC_HTML_PATH/archive/ && [[ -d $VERSION ]] && rm -r $VERSION"
 
 # upload file
+echo "ssh -i $SSH_LOCATION -p $SSH_PORT $HOST \"mkdir -p $PUBLIC_HTML_PATH/archive/$VERSION\""
 ssh -i $SSH_LOCATION -p $SSH_PORT $HOST "mkdir -p $PUBLIC_HTML_PATH/archive/$VERSION"
+echo "scp -i $SSH_LOCATION -p $SSH_PORT $ARCHIVE $HOST:$PUBLIC_HTML_PATH/archive/$VERSION/"
 scp -i $SSH_LOCATION -p $SSH_PORT $ARCHIVE $HOST:$PUBLIC_HTML_PATH/archive/$VERSION/
 
 # unzip archive
+echo "ssh -i $SSH_LOCATION -p $SSH_PORT $HOST \"cd $PUBLIC_HTML_PATH/archive/$VERSION && tar -m -xf $ARCHIVE\""
 ssh -i $SSH_LOCATION -p $SSH_PORT $HOST "cd $PUBLIC_HTML_PATH/archive/$VERSION && tar -m -xf $ARCHIVE"
 
 #update folder permissions (default is 777 - change to 755)
+echo "ssh -i $SSH_LOCATION -p $SSH_PORT $HOST \"chmod -R 755 $PUBLIC_HTML_PATH/archive/$VERSION\""
 ssh -i $SSH_LOCATION -p $SSH_PORT $HOST "chmod -R 755 $PUBLIC_HTML_PATH/archive/$VERSION"
 
 
