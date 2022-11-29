3bfd535753022938cc6807f124095adef1352eb2
Alan Treadway <alan.treadway@ag-grid.com>
2022-11-29 10:39:07 +0000

    AG-7500 - Reinstate iAgChartOptions.ts

---
+++ community-modules/core/src/ts/interfaces/iAgChartOptions.ts
@@ -0,0 +1,4 @@
+export interface AgChartThemeOverrides {}
+export interface AgChartThemePalette {}
+export interface AgChartThemeDefinition {}
+export type AgChartTheme = string | AgChartThemeDefinition;
