bc4a0b54a3b0c34e3e129c4f38bab9b17bda78a9
seanlandsman <sean@thelandsmans.com>
2022-12-01 12:09:25 +0000

    AG-7604 Remove deprecated legacy styles

--- community-modules/core/gulpfile.js
+++ community-modules/core/gulpfile.js
@@ -2,19 +2,12 @@ const gulp = require('gulp');
 const {series, parallel} = gulp;
 const sourcemaps = require('gulp-sourcemaps');
 const clean = require('gulp-clean');
-const rename = require("gulp-rename");
-const autoprefixer = require('autoprefixer');
 const gulpTypescript = require('gulp-typescript');
 const typescript = require('typescript');
 const gulpif = require('gulp-if');
 const header = require('gulp-header');
 const merge = require('merge2');
 const pkg = require('./package.json');
-const webpackStream = require('webpack-stream');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const cssnano = require('gulp-cssnano');
-const filter = require('gulp-filter');
-const named = require('vinyl-named');
 
 const headerTemplate = ['/**',
     ' * <%= pkg.name %> - <%= pkg.description %>',
@@ -98,106 +91,6 @@ const watchAndBuildBoth = () => {
 
 // End of Typescript related tasks
 
-// Start of scss/css related tasks
-const scssTask = () => {
-    var f = filter(["**/*.css", '*Font*.css'], {restore: true});
-
-    return gulp.src([
-        'src/styles/**/*.scss',
-        '!src/styles/**/_*.scss',
-        '!**/node_modules/**'])
-        .pipe(named())
-        .pipe(webpackStream({
-            mode: 'none',
-            module: {
-                rules: [
-                    {
-                        test: /\.scss$/,
-                        use: [
-                            MiniCssExtractPlugin.loader,
-                            "css-loader",
-                            {
-                                loader: 'postcss-loader',
-                                options: {
-                                    syntax: 'postcss-scss',
-                                    plugins: [autoprefixer({
-                                        flexbox: true
-                                    })]
-                                }
-                            },
-                            {
-                                loader: 'sass-loader',
-                                options: {
-                                    additionalData: '$ag-compatibility-mode: false;\n$ag-suppress-all-theme-deprecation-warnings: true;'
-                                }
-                            }
-                        ]
-                    },
-                    {
-                        test: /\.(svg)$/,
-                        use: [
-                            'cache-loader',
-                            {
-                                loader: 'url-loader',
-                                options: {
-                                    limit: 8192
-                                }
-                            },
-                            {
-                                loader: 'image-webpack-loader',
-                                options: {
-                                    svgo: {
-                                        cleanupAttrs: true,
-                                        removeDoctype: true,
-                                        removeComments: true,
-                                        removeMetadata: true,
-                                        removeTitle: true,
-                                        removeDesc: true,
-                                        removeEditorsNSData: true,
-                                        removeUselessStrokeAndFill: true,
-                                        cleanupIDs: true,
-                                        collapseGroups: true,
-                                        convertShapeToPath: true
-                                    }
-                                }
-                            }
-                        ]
-                    }
-                ]
-            },
-            plugins: [
-                new MiniCssExtractPlugin({filename: '[name].css'})
-            ]
-        }))
-        .pipe(f)
-        .pipe(gulp.dest('dist/styles/'))
-        .pipe(f.restore)
-        .pipe(filter('*Font*.css'))
-        .pipe(gulp.dest('dist/styles/webfont/'));
-};
-
-const minifyCss = () => {
-    return gulp.src(['./dist/styles/*.css', '!./dist/styles/*.min.css'])
-        .pipe(cssnano({
-            preset: ['default', {
-                discardComments: {
-                    removeAll: true,
-                },
-            }]
-        }))
-        .pipe(rename(path => path.basename = `${path.basename}.min`))
-        .pipe(gulp.dest('./dist/styles'));
-};
-
-const copyGridCoreStyles = () => {
-    return gulp.src(['./src/styles/**/*',
-        '!./src/styles/**/package.json',
-        '!./src/styles/**/generate-web-fonts.js'
-    ]).pipe(gulp.dest('./dist/styles'));
-};
-
-// End of scss/css related tasks
-
 // Typescript related tasks
 gulp.task('clean', cleanDist);
 gulp.task('tsc-no-clean-cjs', parallel(tscSrcCjsEs5Task, tscSrcCjsEs6Task));
@@ -209,21 +102,15 @@ gulp.task('tsc-no-clean-prod', parallel('tsc-no-clean-cjs-prod', 'tsc-no-clean-e
 gulp.task('tsc', series('clean', 'tsc-no-clean'));
 gulp.task('test', tscSrcCjsEs5ProdTask)
 
-// scss/css related tasks
-gulp.task('scss-no-clean', scssTask);
-gulp.task('minify-css', minifyCss);
-gulp.task('scss', series('clean', 'scss-no-clean'));
-gulp.task('copy-styles-for-dist', copyGridCoreStyles);
-
-// tsc & scss/css related tasks
+// tsc related tasks
 gulp.task('tsc-es6-watch', series('tsc-no-clean-esm', watch));
 gulp.task('tsc-watch', series('tsc-no-clean', watchAndBuildBoth));
-gulp.task('tsc-scss-clean', parallel('tsc-no-clean', series('scss-no-clean', 'minify-css')));
-gulp.task('tsc-scss-clean-prod', parallel('tsc-no-clean-prod', series('scss-no-clean', 'minify-css')));
-gulp.task('tsc-scss-no-clean', parallel('tsc-no-clean', series('scss-no-clean', 'minify-css')));
+gulp.task('tsc-clean', series('clean', 'tsc-no-clean'));
+gulp.task('tsc-clean-prod', parallel('clean', 'tsc-no-clean-prod'));
+gulp.task('tsc-no-clean', parallel('tsc-no-clean'));
 
 // default/release task
-gulp.task('default', series('tsc-scss-clean', 'copy-styles-for-dist'));
-gulp.task('prod', series('tsc-scss-clean-prod', 'copy-styles-for-dist'));
+gulp.task('default', series('tsc-clean'));
+gulp.task('prod', series('tsc-clean-prod'));
 
 
