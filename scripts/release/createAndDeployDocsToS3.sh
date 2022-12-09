3ca1c1e7123154449a3fd16cb925a6f363202ea2
seanlandsman <sean@thelandsmans.com>
2022-12-09 17:17:06 +0000

    Update createAndDeployDocsToS3.sh

--- scripts/release/createAndDeployDocsToS3.sh
+++ scripts/release/createAndDeployDocsToS3.sh
@@ -2,14 +2,12 @@
 
 TARGET_DIRECTORY=$1
 
-ZIP_PREFIX=`date +%Y%m%d`
+#cd grid-packages/ag-grid-docs
+#npx gulp release-archive
 
-cd grid-packages/ag-grid-docs
-npx gulp release-archive
+#cd ../../
+#node scripts/release/patchDocs.js
 
-cd ../../
-node scripts/release/patchDocs.js
+#cd grid-packages/ag-grid-docs
 
-cd grid-packages/ag-grid-docs
-
-s3 cp dist s3://testing.ag-grid.com/$TARGET_DIRECTORY --recursive
+aws s3 cp dist s3://testing.ag-grid.com/$TARGET_DIRECTORY --recursive
