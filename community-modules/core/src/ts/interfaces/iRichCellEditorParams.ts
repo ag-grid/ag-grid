101a28f55d89f66faa99bec75fdca3031a3ceaab
Stephen Cooper <scooperdev@gmail.com>
2022-12-12 10:35:55 +0000

    AG-7659 Make it easier to work with IRichCellEditorParams

--- community-modules/core/src/ts/interfaces/iRichCellEditorParams.ts
+++ community-modules/core/src/ts/interfaces/iRichCellEditorParams.ts
@@ -9,7 +9,7 @@ export interface IRichCellEditorParams extends ICellEditorParams {
 /** The row height, in pixels, of each value. */
     cellHeight: number;
 /** The cell renderer to use to render each value. Cell renderers are useful for rendering rich HTML values, or when processing complex data. */
-    cellRenderer: { new(): ICellRendererComp } | ICellRendererFunc | string;
+    cellRenderer: any;
 /** The value in `ms` for the fuzzy search debounce delay. Default: `300` */
     searchDebounceDelay?: number;
     /** A callback function that allows you to change the displayed value for simple data. */
