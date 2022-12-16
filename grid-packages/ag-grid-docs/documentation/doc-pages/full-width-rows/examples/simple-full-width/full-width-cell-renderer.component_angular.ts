50cba1eb15b1821ff38a6947943eb19bc6f63367
Tak Tran <contact@tutaktran.com>
2022-12-16 14:30:00 +0000

    AG-7643 - Clean up simple full width cell render docs code
    * Remove unnecessary mouse wheel handling - can scroll within the renderer text area without it
    * Show latin text as html, rather than text

--- grid-packages/ag-grid-docs/documentation/doc-pages/full-width-rows/examples/simple-full-width/full-width-cell-renderer.component_angular.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/full-width-rows/examples/simple-full-width/full-width-cell-renderer.component_angular.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild, ViewContainerRef } from "@angular/core";
+import { Component } from "@angular/core";
 
-import { ICellRendererParams, IFilterParams } from "@ag-grid-community/core";
-import { ICellRendererAngularComp, IFilterAngularComp } from "@ag-grid-community/angular";
+import { ICellRendererParams } from "@ag-grid-community/core";
+import { ICellRendererAngularComp } from "@ag-grid-community/angular";
 
 @Component({
     selector: 'full-width-cell',
@@ -24,52 +24,9 @@ import { ICellRendererAngularComp, IFilterAngularComp } from "@ag-grid-community
                 </label>
                 <br/>
             </div>
-            <div class="full-width-center" (mousewheel)="mouseWheelListener($event)" (DOMMouseScroll)="mouseWheelListener($event)">
-                {{latinText()}}
-            </div>
+            <div class="full-width-center" [innerHTML]="latinText()"></div>
         </div>
-    `, styles: [
-        `
-            .full-width-panel {
-                /* undo the white-space setting Fresh puts in */
-                white-space: normal;
-                height: 100%;
-                width: 100%;
-                border: 2px solid grey;
-                border-style: ridge;
-                padding: 5px;
-                background-color: darkgray;
-            }
-            .full-width-flag {
-                float: left;
-                padding: 6px;
-            }
-            .full-width-summary {
-                float: left;
-                /*margin-left: 10px;*/
-                margin-right: 10px;
-            }
-            .full-width-panel label {
-                padding-top: 3px;
-                display: inline-block;
-                font-size: 12px;
-            }
-            .full-width-center {
-                overflow-y: scroll;
-                border: 1px solid grey;
-                padding: 2px;
-                height: 100%;
-                font-family: cursive;
-                background-color: #fafafa;
-            }
-            .full-width-center p {
-                margin-top: 0px;
-            }
-            .full-width-title {
-                font-size: 20px;
-            }
-        `
-    ]
+    `
 })
 export class FullWidthCellRenderer implements ICellRendererAngularComp {
     public data: any;
@@ -80,13 +37,10 @@ export class FullWidthCellRenderer implements ICellRendererAngularComp {
         this.flag = `https://www.ag-grid.com/example-assets/large-flags/${this.data.code}.png`;
     }
 
-    mouseWheelListener(event: Event) {
-        event.stopPropagation();
-    }
-
     latinText() {
         return '<p>Sample Text in a Paragraph</p><p>Lorem ipsum dolor sit amet, his mazim necessitatibus te, mea volutpat intellegebat at. Ea nec perpetua liberavisse, et modo rebum persius pri. Velit recteque reprimique quo at. Vis ex persius oporteat, esse voluptatum moderatius te vis. Ex agam suscipit aliquando eum. Mediocrem molestiae id pri, ei cibo facilisis mel. Ne sale nonumy sea. Et vel lorem omittam vulputate. Ne prima impedit percipitur vis, erat summo an pro. Id urbanitas deterruisset cum, at legere oportere has. No saperet lobortis elaboraret qui, alii zril at vix, nulla soluta ornatus per ad. Feugiat consequuntur vis ad, te sit quodsi persequeris, labore perpetua mei ad. Ex sea affert ullamcorper disputationi, sit nisl elit elaboraret te, quodsi doctus verear ut eam. Eu vel malis nominati, per ex melius delenit incorrupte. Partem complectitur sed in. Vix dicta tincidunt ea. Id nec urbanitas voluptaria, pri no nostro disputationi. Falli graeco salutatus pri ea.</p><p>Quo ad omnesque phaedrum principes, tale urbanitas constituam et ius, pericula consequat ad est. Ius tractatos referrentur deterruisset an, odio consequuntur sed ad. Ea molestie adipiscing adversarium eos, tale veniam sea no. Mutat nullam philosophia sed ad. Pri eu dicta consulatu, te mollis quaerendum sea. Ei doming commodo euismod vis. Cu modus aliquip inermis his, eos et eirmod regione delicata, at odio definiebas vis.</p><p>Lorem ipsum dolor sit amet, his mazim necessitatibus te, mea volutpat intellegebat at. Ea nec perpetua liberavisse, et modo rebum persius pri. Velit recteque reprimique quo at. Vis ex persius oporteat, esse voluptatum moderatius te vis. Ex agam suscipit aliquando eum. Mediocrem molestiae id pri, ei cibo facilisis mel. Ne sale nonumy sea. Et vel lorem omittam vulputate. Ne prima impedit percipitur vis, erat summo an pro. Id urbanitas deterruisset cum, at legere oportere has. No saperet lobortis elaboraret qui, alii zril at vix, nulla soluta ornatus per ad. Feugiat consequuntur vis ad, te sit quodsi persequeris, labore perpetua mei ad. Ex sea affert ullamcorper disputationi, sit nisl elit elaboraret te, quodsi doctus verear ut eam. Eu vel malis nominati, per ex melius delenit incorrupte. Partem complectitur sed in. Vix dicta tincidunt ea. Id nec urbanitas voluptaria, pri no nostro disputationi. Falli graeco salutatus pri ea.</p><p>Quo ad omnesque phaedrum principes, tale urbanitas constituam et ius, pericula consequat ad est. Ius tractatos referrentur deterruisset an, odio consequuntur sed ad. Ea molestie adipiscing adversarium eos, tale veniam sea no. Mutat nullam philosophia sed ad. Pri eu dicta consulatu, te mollis quaerendum sea. Ei doming commodo euismod vis. Cu modus aliquip inermis his, eos et eirmod regione delicata, at odio definiebas vis.</p>';
     }
+
     refresh(params: ICellRendererParams): boolean {
         return false;
     }
