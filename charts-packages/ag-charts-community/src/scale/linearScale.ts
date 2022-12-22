ca54039c57e5f10e15f6f014a7e152b4d1f63768
Alexander Shutau <shutovby@gmail.com>
2022-12-22 11:00:32 +0000

    AG-7460 Revert fix for narrow numeric axis

--- charts-packages/ag-charts-community/src/scale/linearScale.ts
+++ charts-packages/ag-charts-community/src/scale/linearScale.ts
@@ -16,15 +16,6 @@ export class LinearScale extends ContinuousScale {
         const [d0, d1] = this.getDomain();
         const count = this.tickCount ?? 10;
 
-        if (this.nice) {
-            if (count === 2) {
-                return this.niceDomain;
-            }
-            if (count === 1) {
-                return this.niceDomain.slice(0, 1);
-            }
-        }
-
         return ticks(d0, d1, count);
     }
 
