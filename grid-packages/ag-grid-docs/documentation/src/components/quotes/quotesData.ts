37bbe0c0c20710b4317c39ddf39fa05ed2a3e3a4
Mark Durrant <mark.j.durrant@gmail.com>
2023-01-05 14:14:22 +0000

    AG-7687 - Reorder developer quotes

--- grid-packages/ag-grid-docs/documentation/src/components/quotes/quotesData.ts
+++ grid-packages/ag-grid-docs/documentation/src/components/quotes/quotesData.ts
@@ -15,13 +15,13 @@ export interface QuotesDataItem {
 export type QuotesData = Record<string, QuotesDataItem>;
 
 export const quotesData: QuotesData = {
-  brianLove: {
-    name: "Brian Love",
-    avatarUrl: "images/developer-quotes/portraits/brian-love-portrait.png",
-    orgName: "Google Developers",
-    orgIconUrl: "images/developer-quotes/orgIcons/google-developer-expert-logo.png",
-    orgRole: "Expert at",
-    text: "If your application needs to display large amounts of data, we recommend AG Grid. Not only is it highly customizable and extensible, it’s also the fastest JavaScript grid on the planet.",
+  tannerLinsley: {
+    name: "Tanner Linsley",
+    avatarUrl: "images/developer-quotes/portraits/tanner-linsley-portrait.png",
+    orgName: "TanStack",
+    orgIconUrl: "images/developer-quotes/orgIcons/tanstack-logo.png",
+    orgRole: "Creator",
+    text: "There are a lot of component-based table libraries out there, but I believe AG Grid is the gold standard and is by far my favourite. AG Grid is perfect for building Enterprise Applications.",
     order: 10,
   },
   ryanCarniato: {
@@ -33,13 +33,13 @@ export const quotesData: QuotesData = {
     text: "I've been impressed with AG Grid. Not only is it incredibly feature-rich, but it also leverages your framework of choice to do its rendering. This means seamless extensibility and a real way to leverage the framework's strengths. And for SolidJS that's a game changer.",
     order: 20,
   },
-  tannerLinsley: {
-    name: "Tanner Linsley",
-    avatarUrl: "images/developer-quotes/portraits/tanner-linsley-portrait.png",
-    orgName: "TanStack",
-    orgIconUrl: "images/developer-quotes/orgIcons/tanstack-logo.png",
-    orgRole: "Creator",
-    text: "There are a lot of component-based table libraries out there, but I believe AG Grid is the gold standard and is by far my favourite. AG Grid is perfect for building Enterprise Applications.",
+  brianLove: {
+    name: "Brian Love",
+    avatarUrl: "images/developer-quotes/portraits/brian-love-portrait.png",
+    orgName: "Google Developers",
+    orgIconUrl: "images/developer-quotes/orgIcons/google-developer-expert-logo.png",
+    orgRole: "Expert at",
+    text: "If your application needs to display large amounts of data, we recommend AG Grid. Not only is it highly customizable and extensible, it’s also the fastest JavaScript grid on the planet.",
     order: 30,
-  },
+  },  
 };
