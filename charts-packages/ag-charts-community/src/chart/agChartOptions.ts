c3fc1d0c95600fa5cf73fecd90e108e60f1ef6cb
Alan Treadway <alan.treadway@ag-grid.com>
2023-01-09 13:14:10 +0000

    AG-7750 - Update chart layout documentation.

--- charts-packages/ag-charts-community/src/chart/agChartOptions.ts
+++ charts-packages/ag-charts-community/src/chart/agChartOptions.ts
@@ -246,6 +246,25 @@ export interface AgChartCaptionOptions {
     fontFamily?: FontFamily;
     /** The colour to use for the title. */
     color?: CssColor;
+    /** Spacing added to help position the caption. Default: 20 */
+    spacing?: number;
+}
+
+export interface AgAxisCaptionOptions {
+    /** Whether or not the title should be shown. */
+    enabled?: boolean;
+    /** The text to show in the title. */
+    text?: string;
+    /** The font style to use for the title. */
+    fontStyle?: FontStyle;
+    /** The font weight to use for the title. */
+    fontWeight?: FontWeight;
+    /** The font size in pixels to use for the title. */
+    fontSize?: FontSize;
+    /** The font family to use for the title. */
+    fontFamily?: FontFamily;
+    /** The colour to use for the title. */
+    color?: CssColor;
 }
 
 export interface AgNavigatorMaskOptions {
@@ -615,7 +634,7 @@ export interface AgBaseCartesianAxisOptions extends AgBaseAxisOptions {
     /** The position on the chart where the axis should be rendered. */
     position?: AgCartesianAxisPosition;
     /** Configuration for the title shown next to the axis. */
-    title?: AgChartCaptionOptions;
+    title?: AgAxisCaptionOptions;
     /** Configuration for the axis line. */
     line?: AgAxisLineOptions;
     /** Configuration for the axis labels, shown next to the ticks. */
