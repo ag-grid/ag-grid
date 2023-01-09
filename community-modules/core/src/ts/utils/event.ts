1c7bca886c910a91c61aaff34a5671016fcce102
Stephen Cooper <scooperdev@gmail.com>
2023-01-09 13:03:02 +0000

    AG-7778 Use browser standard event.composedPath

--- community-modules/core/src/ts/utils/event.ts
+++ community-modules/core/src/ts/utils/event.ts
@@ -64,77 +64,12 @@ export function getCtrlForEvent<T>(gridOptionsService: GridOptionsService, event
     return null;
 }
 
-/**
- * @deprecated
- * Adds all type of change listeners to an element, intended to be a text field
- * @param {HTMLElement} element
- * @param {EventListener} listener
- */
-export function addChangeListener(element: HTMLElement, listener: EventListener) {
-    element.addEventListener('changed', listener);
-    element.addEventListener('paste', listener);
-    element.addEventListener('input', listener);
-}
-
 export function isElementInEventPath(element: HTMLElement, event: Event): boolean {
     if (!event || !element) {
         return false;
     }
 
-    return getEventPath(event).indexOf(element) >= 0;
-}
-
-export function createEventPath(event: Event): EventTarget[] {
-    const res: EventTarget[] = [];
-    let pointer: any = event.target;
-
-    while (pointer) {
-        res.push(pointer);
-        pointer = pointer.parentElement;
-    }
-
-    return res;
-}
-
-/**
- * firefox doesn't have event.path set, or any alternative to it, so we hack
- * it in. this is needed as it's to late to work out the path when the item is
- * removed from the dom. used by MouseEventService, where it works out if a click
- * was from the current grid, or a detail grid (master / detail).
- * @param {Event} event
- */
-export function addAgGridEventPath(event: Event): void {
-    (event as any).__agGridEventPath = getEventPath(event);
-}
-
-/**
- * Gets the path for an Event.
- * https://stackoverflow.com/questions/39245488/event-path-undefined-with-firefox-and-vue-js
- * https://developer.mozilla.org/en-US/docs/Web/API/Event
- * @param {Event} event
- * @returns {EventTarget[]}
- */
-export function getEventPath(event: Event): EventTarget[] {
-    const eventNoType = event as any;
-
-    if (eventNoType.path) {
-        // Chrome supports path
-        return eventNoType.path;
-    }
-
-    if (eventNoType.composedPath) {
-        // Firefox supports composePath
-        return eventNoType.composedPath();
-    }
-
-    if (eventNoType.__agGridEventPath) {
-        // Firefox supports composePath
-        return eventNoType.__agGridEventPath;
-    }
-
-    // and finally, if none of the above worked,
-    // we create the path ourselves
-    return createEventPath(event);
+    return event.composedPath().indexOf(element) >= 0;
 }
 
 export function addSafePassiveEventListener(
