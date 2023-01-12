b79f983c7718cccfba316cb83f2b4d966208e6da
seanlandsman <sean@thelandsmans.com>
2023-01-12 08:52:51 +0000

    Merge branch 'latest' into b29.0.0

--- scripts/release/patchDocs.js
+++ scripts/release/patchDocs.js
@@ -1,8 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-const LATEST_HASH = require('child_process').execSync('grep origin/latest .git/packed-refs | cut -d " " -f1').toString().trim();
-const LATEST_COMMIT = require('child_process').execSync(`git log ${LATEST_HASH} -n 1`).toString().trim();
+// const LATEST_HASH = require('child_process').execSync('grep origin/latest .git/packed-refs | cut -d " " -f1').toString().trim();
+// const LATEST_COMMIT = require('child_process').execSync(`git log ${LATEST_HASH} -n 1`).toString().trim();
+const LATEST_COMMIT = require('child_process').execSync(`git log  HEAD^..HEAD -n 1`).toString().trim();
 
 const DIST_PATH = 'grid-packages/ag-grid-docs/dist';
 
