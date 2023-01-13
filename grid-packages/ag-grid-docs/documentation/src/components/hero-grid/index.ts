02123ce744086549d937bc55564c7e3109c15775
Tak Tran <contact@tutaktran.com>
2023-01-13 17:44:57 +0000

    AG-7801 - Remove hasInitialised code
    Not necessary for hot module reloading, and made module reloading worse.

--- grid-packages/ag-grid-docs/documentation/src/components/hero-grid/index.ts
+++ grid-packages/ag-grid-docs/documentation/src/components/hero-grid/index.ts
@@ -10,8 +10,33 @@ import { createGenerator } from './generator-utils';
 
 const UPDATE_INTERVAL = 100;
 
-let hasInitialised = false;
 const rowData = generateStocks();
+const generator = createGenerator({
+    interval: UPDATE_INTERVAL,
+    callback: () => {
+        if (!gridOptions.api) {
+            return;
+        }
+
+        const randomIndex = Math.floor(Math.random() * rowData.length);
+        const stockToUpdate = rowData[randomIndex];
+        const newStock = generateStockUpdate(stockToUpdate);
+
+        rowData[randomIndex] = newStock;
+        gridOptions.api.applyTransactionAsync(
+            {
+                update: [newStock],
+            },
+            () => {
+                const rowNode = gridOptions.api!.getRowNode(newStock.stock)!;
+                gridOptions?.api?.flashCells({
+                    rowNodes: [rowNode],
+                    columns: ['current'],
+                });
+            }
+        );
+    },
+});
 
 const gridOptions: GridOptions = {
     columnDefs,
@@ -26,28 +51,6 @@ const gridOptions: GridOptions = {
         return data.stock;
     },
     onGridReady() {
-        const generator = createGenerator({
-            interval: UPDATE_INTERVAL,
-            callback: () => {
-                const randomIndex = Math.floor(Math.random() * rowData.length);
-                const stockToUpdate = rowData[randomIndex];
-                const newStock = generateStockUpdate(stockToUpdate);
-        
-                rowData[randomIndex] = newStock;
-                gridOptions.api!.applyTransactionAsync(
-                    {
-                        update: [newStock],
-                    },
-                    () => {
-                        const rowNode = gridOptions.api!.getRowNode(newStock.stock)!;
-                        gridOptions?.api?.flashCells({
-                            rowNodes: [rowNode],
-                            columns: ['current'],
-                        });
-                    }
-                );
-            },
-        });
         generator.start();
     },
 };
@@ -56,15 +59,9 @@ const gridOptions: GridOptions = {
  * Initialise the grid using plain JavaScript, so grid can be dynamically loaded.
 */
 export function initGrid(selector: string) {
-    if (hasInitialised) {
-        return;
-    }
-
     const init = () => {
         const gridDiv = document.querySelector(selector);
         new agGrid.Grid(gridDiv, gridOptions);
-
-        hasInitialised = true;
     };
 
     const loadGrid = function () {
