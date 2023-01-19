c4723fb11d3836c8ed77cd333643026d92ad088f
Stephen Cooper <scooperdev@gmail.com>
2023-01-19 14:07:52 +0000

    Only look for safari version

--- community-modules/core/src/ts/utils/browser.ts
+++ community-modules/core/src/ts/utils/browser.ts
@@ -1,73 +1,42 @@
 /**
  * These variables are lazy loaded, as otherwise they try and get initialised when we are loading
  * unit tests and we don't have references to window or document in the unit tests
- * from http://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser
  */
 let isSafari: boolean;
+let safariVersion: number;
 let isChrome: boolean;
 let isFirefox: boolean;
 let isMacOs: boolean;
 let isIOS: boolean;
 let invisibleScrollbar: boolean;
 let browserScrollbarWidth: number;
-let browserInfo: { name: string, version: number };
-
-/**
- * from https://stackoverflow.com/a/16938481/1388233
- */
-function getBrowserInfo(): { name: string, version: number } {
-    if (browserInfo) {
-        return browserInfo;
-    }
-    const userAgent = navigator.userAgent;
-    let match = userAgent.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
-    let tem;
-    let version: number;
-
-    if (/trident/i.test(match[1])) {
-        tem = /\brv[ :]+(\d+)/g.exec(userAgent) || [];
-        version = tem[1] != null ? parseFloat(tem[1]) : 0;
-        return {
-            name:'IE',
-            version
-        };
-    }
-
-    if (match[1] === 'Chrome') {
-        tem = userAgent.match(/\bOPR|Edge\/(\d+)/);
-        if (tem != null) {
-            version = tem[1] != null ? parseFloat(tem[1]) : 0;
-            return {
-                name:'Opera',
-                version
-            };
-        }
-    }
-
-    match = match[2] ? [match[1], match[2]] : [navigator.appName, navigator.appVersion, '-?'];
-    tem = userAgent.match(/version\/(\d+)/i);
-
-    if (tem != null) {
-        match.splice(1, 1, tem[1]);
-    }
-
-    const name = match[0];
-    version = match[1] != null ? parseFloat(match[1]) : 0;
-
-    browserInfo = { name, version };
-
-    return browserInfo;
- }
 
 export function isBrowserSafari(): boolean {
     if (isSafari === undefined) {
-        // taken from https://stackoverflow.com/a/23522755/1388233
         isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     }
-
     return isSafari;
 }
 
+function getSafariVersion(): number {
+    if (safariVersion === undefined) {
+        if (isBrowserSafari()) {
+            const versionMatch = navigator.userAgent.match(/version\/(\d+)/i);
+            if (versionMatch) {
+                safariVersion = versionMatch[1] != null ? parseFloat(versionMatch[1]) : 0;
+            }
+        } else {
+            safariVersion = 0;
+        }
+    }
+
+    return safariVersion;
+}
+
+
+/**
+ * Returns true for Chrome and also for Edge (Chromium)
+ */
 export function isBrowserChrome(): boolean {
     if (isChrome === undefined) {
         const win = window as any;
@@ -98,7 +67,6 @@ export function isMacOsUserAgent(): boolean {
 
 export function isIOSUserAgent(): boolean {
     if (isIOS === undefined) {
-        // taken from https://stackoverflow.com/a/58064481/1388233
         isIOS = (/iPad|iPhone|iPod/.test(navigator.platform) ||
             // eslint-disable-next-line
             (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
@@ -112,7 +80,7 @@ export function isIOSUserAgent(): boolean {
 export function browserSupportsPreventScroll(): boolean {
     // all browsers except safari support focus({ preventScroll: true }).
     // this feature was added on Safari 15+
-    return !isBrowserSafari() || getBrowserInfo().version >= 15;
+    return !isBrowserSafari() || getSafariVersion() >= 15;
 }
 
 export function getTabIndex(el: HTMLElement | null): string | null {
