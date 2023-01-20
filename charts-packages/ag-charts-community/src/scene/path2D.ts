1ba1c3dcb18c229b02f6be0e2b2f104a9393b7d0
Alan Treadway <alan.treadway@ag-grid.com>
2023-01-20 12:32:07 +0000

    AG-7775 - Remove redundant Path2d.cubicArc().

--- charts-packages/ag-charts-community/src/scene/path2D.ts
+++ charts-packages/ag-charts-community/src/scene/path2D.ts
@@ -55,6 +55,9 @@ export class Path2D {
                 case 'C':
                     ctx.bezierCurveTo(params[j++], params[j++], params[j++], params[j++], params[j++], params[j++]);
                     break;
+                case 'A':
+                    ctx.arc(params[j++], params[j++], params[j++], params[j++], params[j++], params[j++] === 1);
+                    break;
                 case 'Z':
                     ctx.closePath();
                     break;
@@ -97,174 +100,19 @@ export class Path2D {
         this.closePath();
     }
 
-    /**
-     * Approximates an elliptical arc with up to four cubic Bézier curves.
-     * @param commands The string array to write SVG command letters to.
-     * @param params The number array to write SVG command parameters (cubic control points) to.
-     * @param cx The x-axis coordinate for the ellipse's center.
-     * @param cy The y-axis coordinate for the ellipse's center.
-     * @param rx The ellipse's major-axis radius.
-     * @param ry The ellipse's minor-axis radius.
-     * @param phi The rotation for this ellipse, expressed in radians.
-     * @param theta1 The starting angle, measured clockwise from the positive x-axis and expressed in radians.
-     * @param theta2 The ending angle, measured clockwise from the positive x-axis and expressed in radians.
-     * @param anticlockwise The arc control points are always placed clockwise from `theta1` to `theta2`,
-     * even when `theta1 > theta2`, unless this flag is set to `1`.
-     */
-    static cubicArc(
-        commands: string[],
-        params: number[],
-        cx: number,
-        cy: number,
-        rx: number,
-        ry: number,
-        phi: number,
-        theta1: number,
-        theta2: number,
-        anticlockwise: number
-    ) {
-        if (anticlockwise) {
-            const temp = theta1;
-            theta1 = theta2;
-            theta2 = temp;
-        }
-        const start = params.length;
-        // See https://pomax.github.io/bezierinfo/#circles_cubic
-        // Arc of unit circle (start angle = 0, end angle <= π/2) in cubic Bézier coordinates:
-        // S = [1, 0]
-        // C1 = [1, f]
-        // C2 = [cos(θ) + f * sin(θ), sin(θ) - f * cos(θ)]
-        // E = [cos(θ), sin(θ)]
-        // f = 4/3 * tan(θ/4)
-        const f90 = 0.5522847498307935; // f for θ = π/2 is 4/3 * (Math.sqrt(2) - 1)
-        const sinTheta1 = Math.sin(theta1);
-        const cosTheta1 = Math.cos(theta1);
-        const sinPhi = Math.sin(phi);
-        const cosPhi = Math.cos(phi);
-        const rightAngle = Math.PI / 2;
-        // Since we know how to draw an arc of a unit circle with a cubic Bézier,
-        // to draw an elliptical arc with arbitrary rotation and radii we:
-        // 1) rotate the Bézier coordinates that represent a circular arc by θ
-        // 2) scale the circular arc separately along the x/y axes, making it elliptical
-        // 3) rotate elliptical arc by φ
-        // |cos(φ) -sin(φ)| |sx  0| |cos(θ) -sin(θ)| -> |xx xy|
-        // |sin(φ)  cos(φ)| | 0 sy| |sin(θ)  cos(θ)| -> |yx yy|
-        let xx = cosPhi * cosTheta1 * rx - sinPhi * sinTheta1 * ry;
-        let yx = sinPhi * cosTheta1 * rx + cosPhi * sinTheta1 * ry;
-        let xy = -cosPhi * sinTheta1 * rx - sinPhi * cosTheta1 * ry;
-        let yy = -sinPhi * sinTheta1 * rx + cosPhi * cosTheta1 * ry;
+    arc(x: number, y: number, r: number, sAngle: number, eAngle: number, antiClockwise = false) {
+        const endX = x + r * Math.cos(eAngle);
+        const endY = y + r * Math.sin(eAngle);
 
-        // Always draw clockwise from θ1 to θ2.
-        theta2 -= theta1;
-        if (theta2 < 0) {
-            theta2 += Math.PI * 2;
-        }
-
-        // Multiplying each point [x, y] by:
-        // |xx xy cx| |x|
-        // |yx yy cy| |y|
-        // | 0  0  1| |1|
-
-        commands.push('M');
-        params.push(xx + cx, yx + cy);
-        while (theta2 >= rightAngle) {
-            theta2 -= rightAngle;
-            commands.push('C');
-            // Temp workaround for https://bugs.chromium.org/p/chromium/issues/detail?id=993330
-            // Revert this commit when fixed ^^.
-            const lastX = xy + cx;
-            params.push(
-                xx + xy * f90 + cx,
-                yx + yy * f90 + cy,
-                xx * f90 + xy + cx,
-                yx * f90 + yy + cy,
-                Math.abs(lastX) < 1e-8 ? 0 : lastX,
-                yy + cy
-            );
-            // Prepend π/2 rotation matrix.
-            // |xx xy| | 0 1| -> | xy -xx|
-            // |yx yy| |-1 0| -> | yy -yx|
-            // [xx, yx, xy, yy] = [xy, yy, -xx, -yx];
-            // Compared to swapping with a temp variable, destructuring is:
-            // - 10% faster in Chrome 70
-            // - 99% slower in Firefox 63
-            // Temp variable solution is 45% faster in FF than Chrome.
-            // https://jsperf.com/multi-swap
-            // https://bugzilla.mozilla.org/show_bug.cgi?id=1165569
-            let temp = xx;
-            xx = xy;
-            xy = -temp;
-
-            temp = yx;
-            yx = yy;
-            yy = -temp;
-        }
-        if (theta2) {
-            const f = (4 / 3) * Math.tan(theta2 / 4);
-            const sinPhi2 = Math.sin(theta2);
-            const cosPhi2 = Math.cos(theta2);
-            const C2x = cosPhi2 + f * sinPhi2;
-            const C2y = sinPhi2 - f * cosPhi2;
-            commands.push('C');
-            // Temp workaround for https://bugs.chromium.org/p/chromium/issues/detail?id=993330
-            // Revert this commit when fixed ^^.
-            const lastX = xx * cosPhi2 + xy * sinPhi2 + cx;
-            params.push(
-                xx + xy * f + cx,
-                yx + yy * f + cy,
-                xx * C2x + xy * C2y + cx,
-                yx * C2x + yy * C2y + cy,
-                Math.abs(lastX) < 1e-8 ? 0 : lastX,
-                yx * cosPhi2 + yy * sinPhi2 + cy
-            );
-        }
-        if (anticlockwise) {
-            for (let i = start, j = params.length - 2; i < j; i += 2, j -= 2) {
-                let temp = params[i];
-                params[i] = params[j];
-                params[j] = temp;
-                temp = params[i + 1];
-                params[i + 1] = params[j + 1];
-                params[j + 1] = temp;
-            }
-        }
-    }
-
-    cubicArc(
-        cx: number,
-        cy: number,
-        rx: number,
-        ry: number,
-        phi: number,
-        theta1: number,
-        theta2: number,
-        anticlockwise: number
-    ) {
-        const commands = this.commands;
-        const params = this.params;
-        const start = commands.length;
-
-        Path2D.cubicArc(commands, params, cx, cy, rx, ry, phi, theta1, theta2, anticlockwise);
-
-        const x = params[params.length - 2];
-        const y = params[params.length - 1];
         if (this.xy) {
-            commands[start] = 'L';
-            this.xy[0] = x;
-            this.xy[1] = y;
+            this.xy[0] = endX;
+            this.xy[1] = endY;
         } else {
-            this.xy = [x, y];
+            this.xy = [endX, endY];
         }
-    }
 
-    cubicCurveTo(cx1: number, cy1: number, cx2: number, cy2: number, x: number, y: number) {
-        if (!this.xy) {
-            this.moveTo(cx1, cy1);
-        }
-        this.commands.push('C');
-        this.params.push(cx1, cy1, cx2, cy2, x, y);
-        this.xy![0] = x;
-        this.xy![1] = y;
+        this.commands.push('A');
+        this.params.push(x, y, r, sAngle, eAngle, antiClockwise ? 1 : 0);
     }
 
     private _closedPath: boolean = false;
