86d37167b783f39f54f75baba4b1ea60e2afdce2
Tak Tran <contact@tutaktran.com>
2023-01-24 16:28:53 +0000

    AG-7851 - Add semantic colors to style guide

--- grid-packages/ag-grid-docs/documentation/src/components/style-guide/color.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/style-guide/color.tsx
@@ -2,75 +2,15 @@ import classnames from 'classnames';
 import React from 'react';
 import designSystemColors from '../../design-system/color.module.scss';
 import styles from './color.module.scss';
+import { formatName, hexToHSL, isLight } from './utils';
 
-const formatName = (name) => {
-    return name.replace('ag-grid', 'AG Grid').replaceAll('-', ' ');
-};
+const getAbstractColorId = (key) => `abstract-color-${key}`;
+const getSemanticColorId = (key) => `semantic-color-${key}`;
 
-function hexToRGB(hex) {
-    let r = 0,
-        g = 0,
-        b = 0;
-    if (hex.length === 4) {
-        r = parseInt('0x' + hex[1] + hex[1]);
-        g = parseInt('0x' + hex[2] + hex[2]);
-        b = parseInt('0x' + hex[3] + hex[3]);
-    } else if (hex.length === 7) {
-        r = parseInt('0x' + hex[1] + hex[2]);
-        g = parseInt('0x' + hex[3] + hex[4]);
-        b = parseInt('0x' + hex[5] + hex[6]);
-    }
+const getAbstractColor = (key) => designSystemColors[getAbstractColorId(key)];
+const getAbstractColorOfSemanticColor = (key) => designSystemColors[getSemanticColorId(key)];
 
-    return [r, g, b];
-}
-
-function isLight(hex) {
-    let [r, g, b] = hexToRGB(hex);
-
-    return (r + g + b) / 3 > 150;
-}
-
-function hexToHSL(hex) {
-    // Convert hex to RGB first
-    let [r, g, b] = hexToRGB(hex);
-
-    // Then to HSL
-    r /= 255;
-    g /= 255;
-    b /= 255;
-
-    let cmin = Math.min(r, g, b),
-        cmax = Math.max(r, g, b),
-        delta = cmax - cmin,
-        h = 0,
-        s = 0,
-        l = 0;
-
-    if (delta == 0) {
-        h = 0;
-    } else if (cmax == r) {
-        h = ((g - b) / delta) % 6;
-    } else if (cmax == g) {
-        h = (b - r) / delta + 2;
-    } else {
-        h = (r - g) / delta + 4;
-    }
-
-    h = Math.round(h * 60);
-
-    if (h < 0) {
-        h += 360;
-    }
-
-    l = (cmax + cmin) / 2;
-    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
-    s = +(s * 100).toFixed(1);
-    l = +(l * 100).toFixed(1);
-
-    return `hsl(${h}, ${s}%, ${l}%)`;
-}
-
-const colorGroups = {
+const ABSTRACT_COLOR_GROUPS = {
     Brand: ['ag-grid-dark-blue', 'ag-grid-aqua', 'ag-grid-orange', 'ag-grid-red'],
     Blues: ['dark-cerulean-blue', 'medium-electric-blue', 'azure-blue', 'sky-blue', 'water-blue', 'ghost-blue'],
     Grays: [
@@ -87,9 +27,32 @@ const colorGroups = {
     Other: ['cerise-pink'],
 };
 
-const ColorSwatch = ({ id, hexColor }) => {
+/**
+ * Semantic colors as CSS Variable names
+ *
+ * NOTE: **Without** the `--` prefix
+ */
+const SEMANTIC_COLOR_GROUPS = {
+    Text: ['text-color', 'secondary-text-color'],
+    Border: ['border-color'],
+    Link: ['link-color', 'link-hover-color'],
+    Button: ['button-text-color', 'button-color', 'button-hover-color', 'button-active-color'],
+    Inline: ['inline-element-background-color'],
+    Code: ['code-text-color'],
+    Input: [
+        'input-background-color',
+        'input-secondary-background-color',
+        'input-secondary-border-color',
+        'input-primary-color',
+        'input-focus-border-color',
+        'input-focus-box-shadow-color',
+    ],
+    Table: ['table-odd-row-background-color'],
+};
+
+const AbstractColorSwatch = ({ id, hexColor }) => {
     return (
-        <li className={styles.swatch}>
+        <li id={getAbstractColorId(id)} className={styles.swatch}>
             <div
                 className={classnames(styles.color, isLight(hexColor) ? styles['color--light'] : '')}
                 style={{
@@ -107,25 +70,79 @@ const ColorSwatch = ({ id, hexColor }) => {
     );
 };
 
-export const Color = () => {
-    const colors = Object.keys(designSystemColors);
+const SemanticColorSwatch = ({ cssVarName }) => {
+    const abstractColor = getAbstractColorOfSemanticColor(cssVarName);
+    const hexColor = getAbstractColor(abstractColor);
 
     return (
+        <li className={styles.swatch}>
+            <div
+                className={classnames(styles.color, isLight(hexColor) ? styles['color--light'] : '')}
+                style={{
+                    backgroundColor: `var(--${cssVarName})`,
+                }}
+            ></div>
+            <p className={styles.name}>{formatName(cssVarName)}</p>
+            <p className={styles.cssName}>
+                <code>
+                    --{cssVarName}: <a href={`#${getAbstractColorId(abstractColor)}`}>--{abstractColor}</a>
+                </code>
+            </p>
+        </li>
+    );
+};
+
+const AbstractColors = () => {
+    const colors = ABSTRACT_COLOR_GROUPS;
+    return (
         <div className={styles.swatches}>
-            {Object.keys(colorGroups).map((groupName) => {
-                const groupColors = colorGroups[groupName];
+            <h3>Abstract Colors</h3>
+            {Object.keys(colors).map((groupName) => {
+                const groupColors = colors[groupName];
 
                 return (
-                    <>
-                        <h3>{groupName}</h3>
+                    <React.Fragment key={groupName}>
+                        <h4>{groupName}</h4>
                         <ul className={classnames('list-style-none', styles.colorGroup)}>
                             {groupColors.map((key) => {
-                                return <ColorSwatch id={key} hexColor={designSystemColors[key]} />;
+                                return <AbstractColorSwatch key={key} id={key} hexColor={getAbstractColor(key)} />;
                             })}
                         </ul>
-                    </>
+                    </React.Fragment>
                 );
             })}
+        </div>
+    );
+};
+
+const SemanticColors = () => {
+    const colors = SEMANTIC_COLOR_GROUPS;
+    return (
+        <div className={styles.swatches}>
+            <h3>Semantic Colors</h3>
+            {Object.keys(colors).map((groupName) => {
+                const groupColors = colors[groupName];
+
+                return (
+                    <React.Fragment key={groupName}>
+                        <h4>{groupName}</h4>
+                        <ul className={classnames('list-style-none', styles.colorGroup)}>
+                            {groupColors.map((cssVarName) => {
+                                return <SemanticColorSwatch key={cssVarName} cssVarName={cssVarName} />;
+                            })}
+                        </ul>
+                    </React.Fragment>
+                );
+            })}
+        </div>
+    );
+};
+
+export const Color = () => {
+    return (
+        <div className={styles.colorSubsections}>
+            <AbstractColors />
+            <SemanticColors />
         </div>
     );
 };
