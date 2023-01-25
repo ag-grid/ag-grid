5ead0b66c11d32fdb42d00d7ed70ecd623b4a0e4
Tak Tran <contact@tutaktran.com>
2023-01-25 10:52:49 +0000

    AG-7902 - Prettier updates

--- grid-packages/ag-grid-docs/documentation/src/components/HeaderNav.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/HeaderNav.jsx
@@ -1,62 +1,63 @@
-import React, {useState} from 'react';
-import {withPrefix} from 'gatsby';
-import MenuIcon from '../images/inline-svgs/menu-icon.svg';
-import GithubLogo from '../images/inline-svgs/github-logo.svg';
 import classnames from 'classnames';
+import { withPrefix } from 'gatsby';
+import React, { useState } from 'react';
+import GithubLogo from '../images/inline-svgs/github-logo.svg';
+import MenuIcon from '../images/inline-svgs/menu-icon.svg';
 import styles from './HeaderNav.module.scss';
 
 const links = [
     {
         name: 'Demo',
-        url: '/example'
+        url: '/example',
     },
     {
         name: 'Documentation',
-        url: withPrefix('/documentation/')
+        url: withPrefix('/documentation/'),
     },
     {
         name: 'Pricing',
-        url: '/license-pricing'
+        url: '/license-pricing',
     },
     {
         name: 'Blog',
-        url: 'https://blog.ag-grid.com/'
+        url: 'https://blog.ag-grid.com/',
     },
     {
         name: 'Github',
         url: 'https://github.com/ag-grid/ag-grid',
         icon: <GithubLogo />,
-        cssClass: 'github-item'
-    }
+        cssClass: 'github-item',
+    },
 ];
 
 const getActiveLink = (path) => {
     const rawPath = path.replaceAll('/', '');
-    const match = links.filter(link => link.url.includes(rawPath));
+    const match = links.filter((link) => link.url.includes(rawPath));
     if (match && match.length === 1) {
-        return match[0].name
+        return match[0].name;
     }
 
     return 'Documentation';
-}
+};
 
-
-const HeaderLinks = ({path}) => {
-    const [active, setActive] = useState(getActiveLink(path))
+const HeaderLinks = ({ path }) => {
+    const [active, setActive] = useState(getActiveLink(path));
 
     return (
         <ul className={styles['header-nav__navbar']}>
-            {links.map(link => {
-                const linkClasses = classnames(
-                    styles['header-nav__link'], {
-                        [styles['header-nav__link--active']]: link.name === active && path !== '/',
-                        [styles[link.cssClass]]: link.cssClass
-                    }
-                );
+            {links.map((link) => {
+                const linkClasses = classnames(styles['header-nav__link'], {
+                    [styles['header-nav__link--active']]: link.name === active && path !== '/',
+                    [styles[link.cssClass]]: link.cssClass,
+                });
 
                 return (
                     <li key={link.name.toLocaleLowerCase()} className={linkClasses}>
-                        <a className={styles['header-nav__link-anchor']} href={link.url} onClick={() => setActive(link.name)}>
+                        <a
+                            className={styles['header-nav__link-anchor']}
+                            href={link.url}
+                            onClick={() => setActive(link.name)}
+                        >
                             {link.icon}
                             <span>{link.name}</span>
                         </a>
@@ -65,7 +66,7 @@ const HeaderLinks = ({path}) => {
             })}
         </ul>
     );
-}
+};
 
 const HeaderExpandButton = () => (
     <button
@@ -75,24 +76,24 @@ const HeaderExpandButton = () => (
         data-target="#main-nav"
         aria-controls="main-nav"
         aria-expanded="false"
-        aria-label="Toggle navigation">
-
+        aria-label="Toggle navigation"
+    >
         <MenuIcon className={styles['header-nav__menu-icon']} />
     </button>
 );
 
-const HeaderNav = ({path}) => (
+const HeaderNav = ({ path }) => (
     <>
-      <div className={styles['header-nav__widgets']}>
-          <HeaderExpandButton/>
-      </div>
-      <nav className={styles['header-nav']}>
-          <div className={styles['header-nav__wrapper']}>
-              <div className={styles['header-nav__navbar-collapse']} id="main-nav">
-                  <HeaderLinks path={path}/>
-              </div>
-          </div>
-      </nav>
+        <div className={styles['header-nav__widgets']}>
+            <HeaderExpandButton />
+        </div>
+        <nav className={styles['header-nav']}>
+            <div className={styles['header-nav__wrapper']}>
+                <div className={styles['header-nav__navbar-collapse']} id="main-nav">
+                    <HeaderLinks path={path} />
+                </div>
+            </div>
+        </nav>
     </>
 );
 
