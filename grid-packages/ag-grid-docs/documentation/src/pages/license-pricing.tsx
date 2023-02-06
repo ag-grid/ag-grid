589ae8278646ff97a8b25ab821759afbdcc7b951
Mark Durrant <mark.j.durrant@gmail.com>
2023-02-06 17:49:56 +0000

    AG-7956 - Add pricing page content, initial layout & styles

--- grid-packages/ag-grid-docs/documentation/src/pages/license-pricing.tsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/license-pricing.tsx
@@ -26,6 +26,235 @@ const LicensePricing = () => {
 
                 <Licenses />
 
+                <div className={styles.bulkLicences}>
+                    <p className="text-secondary">
+                        Bulk pricing discounts available. Use the pay with card buttons above to see pricing for up to
+                        10 developers. For more than 10 developers or any questions with regards your purchase...
+                    </p>
+
+                    <div>
+                        <a className="button font-size-large" href="#">
+                            Request a quote
+                        </a>
+                        <a className="button secondary-button font-size-large" href="#">
+                            Contact sales
+                        </a>
+                    </div>
+                </div>
+
+                <div className={styles.communityEnterprise}>
+                    <div className={styles.community}>
+                        <h3>AG Grid Community</h3>
+                        <p>
+                            <b>AG Grid Community</b> and <b>AG Charts Community</b> (a.k.a.{' '}
+                            <a href="/javascript-charts/overview/">Standalone Charts</a>) are free and open source
+                            products distributed under the{' '}
+                            <a href="/eula/AG-Grid-Community-License.html">MIT License</a>. These versions are free to
+                            use in production environments.
+                        </p>
+
+                        <a href="" className="button secondary-button">
+                            Get AG Grid Community at NPM
+                        </a>
+                        <br />
+                        <a href="" className="button secondary-button">
+                            Get AG Grid Charts at NPM
+                        </a>
+                    </div>
+
+                    <div className={styles.enterprise}>
+                        <h3>AG Grid Enterprise</h3>
+                        <p>
+                            <b>AG Grid Enterprise</b> offers advanced functionality like{' '}
+                            <a href="/javascript-data-grid/grouping/">Row Grouping</a>,{' '}
+                            <a href="/javascript-data-grid/range-selection/">Range Selection</a>,{' '}
+                            <a href="/javascript-data-grid/master-detail/">Master / Detail</a>,{' '}
+                            <a href="/javascript-data-grid/server-side-model/">Server Side Row Model</a> and{' '}
+                            <a href="/javascript-data-grid/licensing/#feature-comparison">much much more</a>.{' '}
+                            <b>AG Grid Enterprise</b> also comes with{' '}
+                            <a href="/javascript-data-grid/integrated-charts/">Integrated Charts</a>, allowing your
+                            users to create charts using the grid's UI.
+                        </p>
+
+                        <p>
+                            <b>AG Grid Enterprise</b> is a commercial product distributed under our{' '}
+                            <a href="/eula/AG-Grid-Enterprise-License-Latest.html">EULA</a> and supported by our
+                            technical staff. To evaluate <b>AG Grid Enterprise</b> you don't need our permission – all
+                            features are unlocked. To temporarily hide the watermark and browser console errors e-mail
+                            us to <a href="">get a temporary evaluation key</a>.
+                        </p>
+
+                        <p>
+                            Once you're ready to begin development, please purchase an appropriate license key from the
+                            options above.
+                        </p>
+
+                        <p>
+                            Expanded definitions are available further down the page. For any other questions please{' '}
+                            <a href="">get in contact</a>.
+                        </p>
+
+                        <a
+                            href="/javascript-data-grid/licensing/#feature-comparison"
+                            className="button secondary-button"
+                        >
+                            See all AG Grid Enterprise features
+                        </a>
+                    </div>
+                </div>
+
+                <div className={styles.videoExplainer}>
+                    <iframe
+                        src="https://www.youtube.com/embed/20SLdu4wLtI"
+                        title="YouTube video player"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    ></iframe>
+
+                    <div>
+                        <h3 className="font-size-extra-large">Questions about our licences? </h3>
+                        <p>
+                            Watch our short video for an in-depth look at exactly how each AG Grid licence works. Learn
+                            which licence is right for you, how many licences you need for you team, and exactly when
+                            you need a deployment licence.
+                        </p>
+                        <p>
+                            If you have any other questions, or want to investigate volume pricing please{' '}
+                            <a href="mailto:info@ag-grid.com">get in contact</a>.
+                        </p>
+                    </div>
+                </div>
+
+                <div className={styles.licencesInDetail}>
+                    <h3 className="font-size-extra-large">Our Licenses in Depth</h3>
+
+                    <div className={classnames(styles.singleApplicationLicense, styles.card)}>
+                        <div className={styles.primaryContent}>
+                            <h3>Single Application Development License</h3>
+                        </div>
+                        <div className={styles.secondaryContent}>
+                            <p>
+                                Licenses one application, developed for internal use, to embed AG Grid Enterprise in
+                                perpetuity.
+                            </p>
+                            <ul>
+                                <li>Includes a 1-year subscription to new versions, support and maintenance.</li>
+                                <li>
+                                    For customer-facing applications you will also need a Deployment License add-on.
+                                </li>
+                                <li>
+                                    All concurrent, front-end, JavaScript developers working on the Application would
+                                    need to be added to the license count, not just the ones working with AG Grid.
+                                </li>
+                                <li>
+                                    Developers within the Single Application Development License count are unnamed, so
+                                    long as the total licensed count isn’t exceeded.
+                                </li>
+                                <li>
+                                    Single Application Development Licenses are bound to an application name and can’t
+                                    be reused on other applications.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={classnames(styles.multipleApplicationsLicense, styles.card)}>
+                        <div className={styles.primaryContent}>
+                            <h3>Multiple Application Development License</h3>
+                        </div>
+                        <div className={styles.secondaryContent}>
+                            <p>
+                                Licenses unlimited number of applications, developed for internal use, to embed AG Grid
+                                Enterprise in perpetuity.
+                            </p>
+                            <ul>
+                                <li>Includes a 1-year subscription to new versions, support and maintenance.</li>
+                                <li>
+                                    For customer-facing applications you will also need a Deployment License add-on.
+                                </li>
+                                <li>
+                                    All concurrent, front-end, JavaScript developers working across the licensed
+                                    Applications would need to be added to the license count, not just the ones working
+                                    with AG Grid.
+                                </li>
+                                <li>
+                                    Developers within the Multiple Application Development License count are unnamed, so
+                                    long as the total licensed count isn’t exceeded.
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className={classnames(styles.deploymentLicence, styles.card)}>
+                        <div className={styles.primaryContent}>
+                            <h3>Deployment License Add-on</h3>
+                        </div>
+                        <div className={styles.secondaryContent}>
+                            <p>
+                                Allows licensed developers to sub-license AG Grid for one application on one production
+                                environment in perpetuity. Includes a 1-year subscription to new versions, support and
+                                maintenance. Only available with a Developer License.
+                            </p>
+                            <ul>
+                                <li>
+                                    A Deployment License Add-on allows making a project available to individuals (eg
+                                    your customers) outside of your organisation (sub-license).
+                                </li>
+                                <li>
+                                    One Deployment License Add-on covers one production environment for one project.
+                                </li>
+                                <li>
+                                    Only production environments require licensing. All other environments (eg
+                                    development, test, pre-production) do not require a license.
+                                </li>
+                                <li>
+                                    We do not charge per server. A cluster of many servers part of one application
+                                    installation is considered one deployment and requires one Deployment License. This
+                                    is true so long as the application instances within the cluster are replicas of each
+                                    other and server to provide load balancing and fail over only.
+                                </li>
+                                <li>
+                                    Production failover deployments do not need to be licensed separately. They are
+                                    considered part of the overall application production deployment.
+                                </li>
+                                <li>
+                                    Multi-tenant deployments, where one application instance is serving many customers
+                                    over different URLs, is considered one deployment, as each tenant is getting
+                                    serviced by the same application instance.
+                                </li>
+                                <li>
+                                    Different instances of the same application, where the instances are not part of a
+                                    cluster for fail over or load balancing, are considered independent deployments and
+                                    need a Deployment License for each individual application instance.
+                                </li>
+                                <li>
+                                    Deploying an application to a cloud service (eg AWS or Docker) requires one
+                                    Deployment License, regardless of how many virtual containers or servers the cloud
+                                    application spawns for that one single instance of the application.
+                                </li>
+                            </ul>
+
+                            <p>
+                                If you have a deployment that doesn't fit within our licensing model, please{' '}
+                                <a href="mailto:info@ag-grid.com">start a conversation with us</a> and we will do our
+                                best to get to something that works.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className={styles.contactSales}>
+                    <p className="text-secondary">
+                        For any enquires about bulk pricing, questions on which licence is right for you, or any other
+                        licence related questions please contact our friendly sales team.{' '}
+                    </p>
+                    <div>
+                        <a className="button secondary-button font-size-large" href="#">
+                            Contact sales
+                        </a>
+                    </div>
+                </div>
+
                 {/* <section className={styles.info}>
                     <div className={styles.infoItem}>
                         <div className={styles.ukraineFlag}>
