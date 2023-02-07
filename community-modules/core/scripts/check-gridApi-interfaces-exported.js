497cdb0c0efc1af7abef576b0601ee10966bcc8b
Stephen Cooper <scooperdev@gmail.com>
2023-02-07 10:23:23 +0000

    AG-7959 Add quick check for missing api types

--- community-modules/core/scripts/check-gridApi-interfaces-exported.js
+++ community-modules/core/scripts/check-gridApi-interfaces-exported.js
@@ -24,9 +24,11 @@ function checkGridOptionPropertyKeys() {
     })
 
     if (missingPropertyKeys.size > 0) {
-        console.error('check-grid-api-exports - GridApi / ColumnApi are using types that are not publicly exported. Missing the following types:', [...missingPropertyKeys].join(', '))
+        console.error('check-grid-api-exports - GridApi / ColumnApi are using types that are not publicly exported. Missing the following types:', [...missingPropertyKeys].join(', '));
+        console.error('If running locally and you have added the missing type be sure to run build in "community-modules/core", "grid-packages/ag-grid-community", "grid-packages/ag-grid-enterprise".')
         return 1;
     }
+    console.log('check-grid-api-exports - GridApi / ColumnApi Passed sanity check for missing types.')
     return 0;
 }
 
@@ -36,7 +38,10 @@ process.exit(result)
 function getPublicTypes(fileContents) {
     const matchesPublicMethods = [...fileContents.matchAll(/public (\w*)\((.*)\)(:?) (\w*)/g)];
     let publicTypes = [];
-    const toIgnore = ['TData', 'Blob', '', 'Document', 'Function', 'HTMLElement', 'KeyboardEvent', 'MouseEvent', 'Touch', 'OverlayWrapperComponent', 'AgEvent'];
+    const toIgnore = [
+    'TData', 'Blob', '', 'Document', 'Function', 'HTMLElement', 'KeyboardEvent', 'MouseEvent', 'Touch', 
+    // Some just missed by this script due to use of import * from ./events
+    'OverlayWrapperComponent', 'AgEvent', 'ColumnEventType', 'SelectionEventSourceType'];
     matchesPublicMethods.forEach(m => {
         const params = m[2].split(/\W/).map(i => i.trim());
         const returnType = m[4].split(/\W/).map(i => i.trim());
