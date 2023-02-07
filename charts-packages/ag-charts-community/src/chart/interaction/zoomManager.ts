e229be59d276052e22e1e650202a47703e6d9031
Alan Treadway <alan.treadway@ag-grid.com>
2023-02-07 08:27:35 +0000

    AG-7879 - Add base Chart module support.

---
+++ charts-packages/ag-charts-community/src/chart/interaction/zoomManager.ts
@@ -0,0 +1,74 @@
+import { BaseManager } from './baseManager';
+
+interface ZoomState {
+    min: number;
+    max: number;
+}
+
+interface AxisZoomState {
+    x?: ZoomState;
+    y?: ZoomState;
+}
+
+export interface ZoomChangeEvent extends AxisZoomState {
+    type: 'zoom-change';
+}
+
+function isEqual(a?: AxisZoomState, b?: AxisZoomState) {
+    if (a === b) return true;
+    if (a?.x?.min !== b?.x?.min) return false;
+    if (a?.x?.max !== b?.x?.max) return false;
+    if (a?.y?.max !== b?.y?.max) return false;
+    if (a?.y?.min !== b?.y?.min) return false;
+
+    return true;
+}
+
+/**
+ * Manages the current zoom state for a chart. Tracks the requested zoom from distinct dependents
+ * and handles conflicting zoom requests.
+ */
+export class ZoomManager extends BaseManager<'zoom-change', ZoomChangeEvent> {
+    private readonly states: Record<string, AxisZoomState> = {};
+    private currentZoom?: AxisZoomState = undefined;
+
+    public constructor() {
+        super();
+    }
+
+    public updateZoom(callerId: string, newZoom?: AxisZoomState) {
+        delete this.states[callerId];
+
+        if (newZoom != null) {
+            this.states[callerId] = { ...newZoom };
+        }
+
+        this.applyStates();
+    }
+
+    public getZoom(): AxisZoomState | undefined {
+        return this.currentZoom;
+    }
+
+    private applyStates() {
+        const currentZoom = this.currentZoom;
+        let zoomToApply: AxisZoomState = {};
+
+        // Last added entry wins.
+        for (const [_, { x, y }] of Object.entries(this.states)) {
+            zoomToApply.x = x ?? zoomToApply.x;
+            zoomToApply.y = y ?? zoomToApply.y;
+        }
+        this.currentZoom = zoomToApply.x != null || zoomToApply.y != null ? zoomToApply : undefined;
+
+        const changed = !isEqual(currentZoom, this.currentZoom);
+        if (!changed) {
+            return;
+        }
+        const event: ZoomChangeEvent = {
+            type: 'zoom-change',
+            ...(currentZoom ?? {}),
+        };
+        this.listeners.dispatch('zoom-change', event);
+    }
+}
