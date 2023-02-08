b9aeb4ad4dfe9fb0d69b57a3d76218699ff0d25e
Stephen Cooper <scooperdev@gmail.com>
2023-02-08 10:08:39 +0000

    AG-7977 Remove optional chaining from examples

--- grid-packages/ag-grid-docs/documentation/doc-pages/master-detail-refresh/examples/refresh-nothing/main.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/master-detail-refresh/examples/refresh-nothing/main.ts
@@ -23,9 +23,9 @@ const gridOptions: GridOptions<IAccount> = {
       return (
         '<div class="ag-details-row ag-details-row-fixed-height">' +
           '<div style="padding: 4px; font-weight: bold;">' +
-          params.data ? params.data!.name : '' +
+          (params.data ? params.data!.name : '') +
             ' ' +
-            params.data ? params.data!.calls : '' +
+            (params.data ? params.data!.calls : '') +
             ' calls</div>' +
             '<div ref="eDetailGrid" class="ag-details-grid ag-details-grid-fixed-height"/>' +
             '</div>'
