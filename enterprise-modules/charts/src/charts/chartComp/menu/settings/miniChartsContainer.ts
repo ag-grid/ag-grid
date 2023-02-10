d8353fdffcd48ea85770fe1cad95330a6bd71de7
Stephen Cooper <scooperdev@gmail.com>
2023-02-10 10:10:46 +0000

    AG-7949 Deduplicate errors

--- enterprise-modules/charts/src/charts/chartComp/menu/settings/miniChartsContainer.ts
+++ enterprise-modules/charts/src/charts/chartComp/menu/settings/miniChartsContainer.ts
@@ -104,7 +104,12 @@ export class MiniChartsContainer extends Component {
             chartGroupValues!.forEach((chartType: keyof ChartGroupsDef[typeof group]) => {
                 const MiniClass = miniChartMapping[group]?.[chartType] as any;
                 if (!MiniClass) {
-                    _.doOnce(() => console.warn(`AG Grid - invalid chartGroupsDef config '${group}.${chartType}'`), `invalidMiniChart_${chartType}_${group}`);
+                    if (miniChartMapping[group]) {
+                        _.doOnce(() => console.warn(`AG Grid - invalid chartGroupsDef config '${group}.${chartType}'`), `invalid_chartGroupsDef${chartType}_${group}`);
+                    } else {
+                        _.doOnce(() => console.warn(`AG Grid - invalid chartGroupsDef config '${group}'`), `invalid_chartGroupsDef${group}`);
+
+                    }
                     return;
                 }
 
