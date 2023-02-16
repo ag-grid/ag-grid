50492c8b8ba113e27386e5cea071b1398db0e5a7
Mana <mana.jalali.15@ucl.ac.uk>
2023-02-16 17:09:05 +0000

    AG-7746 - Re-enable cartesian charts log axis invalid data domain tests

--- charts-packages/ag-charts-community/src/chart/series/cartesian/barSeries.test.ts
+++ charts-packages/ag-charts-community/src/chart/series/cartesian/barSeries.test.ts
@@ -5,10 +5,10 @@ import { AgChart } from '../../agChartV2';
 import { Chart } from '../../chart';
 import {
     DATA_FRACTIONAL_LOG_AXIS,
-    // DATA_INVALID_DOMAIN_LOG_AXIS,
+    DATA_INVALID_DOMAIN_LOG_AXIS,
     DATA_NEGATIVE_LOG_AXIS,
     DATA_POSITIVE_LOG_AXIS,
-    // DATA_ZERO_EXTENT_LOG_AXIS,
+    DATA_ZERO_EXTENT_LOG_AXIS,
 } from '../../test/data';
 import * as examples from '../../test/examples';
 import { CARTESIAN_CATEGORY_X_AXIS_LOG_Y_AXIS } from '../../test/examples';
@@ -68,8 +68,11 @@ const EXAMPLES: Record<string, TestCase> = {
     COLUMN_CATEGORY_X_AXIS_POSITIVE_LOG_Y_AXIS: buildLogAxisTestCase(DATA_POSITIVE_LOG_AXIS),
     COLUMN_CATEGORY_X_AXIS_NEGATIVE_LOG_Y_AXIS: buildLogAxisTestCase(DATA_NEGATIVE_LOG_AXIS),
     COLUMN_CATEGORY_X_AXIS_FRACTIONAL_LOG_Y_AXIS: buildLogAxisTestCase(DATA_FRACTIONAL_LOG_AXIS),
-    // COLUMN_CATEGORY_X_AXIS_ZERO_EXTENT_LOG_Y_AXIS: buildLogAxisTestCase(DATA_ZERO_EXTENT_LOG_AXIS),
-    // COLUMN_CATEGORY_X_AXIS_INVALID_DOMAIN_LOG_Y_AXIS: buildLogAxisTestCase(DATA_INVALID_DOMAIN_LOG_AXIS),
+    COLUMN_CATEGORY_X_AXIS_ZERO_EXTENT_LOG_Y_AXIS: buildLogAxisTestCase(DATA_ZERO_EXTENT_LOG_AXIS),
+};
+
+const INVALID_DATA_EXAMPLES: Record<string, TestCase> = {
+    COLUMN_CATEGORY_X_AXIS_INVALID_DOMAIN_LOG_Y_AXIS: buildLogAxisTestCase(DATA_INVALID_DOMAIN_LOG_AXIS),
 };
 
 describe('BarSeries', () => {
@@ -125,6 +128,49 @@ describe('BarSeries', () => {
                     await example.extraScreenshotActions(chart);
                     await compare();
                 }
+            });
+        }
+    });
+
+    describe('invalid data domain', () => {
+        beforeEach(() => {
+            console.warn = jest.fn();
+        });
+
+        for (const [exampleName, example] of Object.entries(INVALID_DATA_EXAMPLES)) {
+            it(`for ${exampleName} it should create chart instance as expected`, async () => {
+                const options: AgChartOptions = { ...example.options };
+                options.autoSize = false;
+                options.width = CANVAS_WIDTH;
+                options.height = CANVAS_HEIGHT;
+
+                chart = AgChart.create(options) as Chart;
+                await waitForChartStability(chart);
+                await example.assertions(chart);
+            });
+
+            it(`for ${exampleName} it should render to canvas as expected`, async () => {
+                const compare = async () => {
+                    await waitForChartStability(chart);
+
+                    const imageData = extractImageData(ctx);
+                    (expect(imageData) as any).toMatchImageSnapshot(IMAGE_SNAPSHOT_DEFAULTS);
+                };
+
+                const options: AgChartOptions = { ...example.options };
+                options.autoSize = false;
+                options.width = CANVAS_WIDTH;
+                options.height = CANVAS_HEIGHT;
+
+                chart = AgChart.create(options) as Chart;
+                await compare();
+
+                if (example.extraScreenshotActions) {
+                    await example.extraScreenshotActions(chart);
+                    await compare();
+                }
+
+                expect(console.warn).toBeCalled();
             });
         }
     });
