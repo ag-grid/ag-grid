56e5d70e52f271e2d46be656f0013fd540244246
Alan Treadway <alan.treadway@ag-grid.com>
2023-03-03 12:05:16 +0000

    Tweak CI failure message for ag-grid-enterprise tests.

--- grid-packages/ag-grid-enterprise/test-modifications.sh
+++ grid-packages/ag-grid-enterprise/test-modifications.sh
@@ -3,8 +3,8 @@
 set -eu
 
 if (! git diff-index --quiet HEAD -- .) ; then 
-    echo "Local file modifications found." >&2
+    echo "Local file modifications found for ag-grid-enterprise." >&2
     exit 1
 fi
 
-echo "No local file modifications found."
+echo "No local file modifications found for ag-grid-enterprise."
