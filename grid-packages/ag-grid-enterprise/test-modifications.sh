ec7568a86d708d14f362759ebd008b156102a8f2
Alan Treadway <alan.treadway@ag-grid.com>
2023-03-03 14:22:39 +0000

    Add more debugging for CI check of ag-grid-enterprise modifications.

--- grid-packages/ag-grid-enterprise/test-modifications.sh
+++ grid-packages/ag-grid-enterprise/test-modifications.sh
@@ -2,8 +2,9 @@
 
 set -eu
 
-if (! git diff-index HEAD --quiet -- .) ; then
+if (! git diff HEAD --quiet -- .) ; then
     echo "Local file modifications found for ag-grid-enterprise." # >&2
+    git diff-index HEAD -- . # >&2
     git diff HEAD -- . # >&2
     exit 0 # TODO: Re-enable failure for this case in the future.
 fi
