32b15eab4cc2f0ff83be8b283bff30ba0a961de9
Mana <mana.jalali.15@ucl.ac.uk>
2023-03-10 12:19:29 +0000

    AG-4371 - Export everything from angle util file for crosshair module

--- charts-community-modules/ag-charts-community/src/sparklines-util.ts
+++ charts-community-modules/ag-charts-community/src/sparklines-util.ts
@@ -2,6 +2,7 @@ export * from './util/value';
 export * from './util/id';
 export * from './util/padding';
 export * from './util/json';
+export * from './util/angle';
 
 export { extent } from './util/array';
 export { tickFormat } from './util/numberFormat';
