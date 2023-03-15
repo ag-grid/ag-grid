7315bca5578280e377407748428c10060c74adde
Andrew Glazier <andrew.glazier@ag-grid.com>
2023-03-15 13:45:57 +0000

    AG-2223 - Doc changes

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/server-side-model-selection/examples/group-selects-children-transactions/main.ts
@@ -0,0 +1,163 @@
+import { Grid, ColDef, GridOptions, GetRowIdParams, GridReadyEvent, IServerSideGetRowsParams, IsServerSideGroupOpenByDefaultParams, ServerSideTransaction, ServerSideTransactionResult } from '@ag-grid-community/core'
+
+declare var FakeServer: any;
+declare var deletePortfolioOnServer: any;
+declare var changePortfolioOnServer: any;
+declare var createRowOnServer: any;
+
+const columnDefs: ColDef[] = [
+    { field: 'portfolio', hide: true, rowGroup: true },
+    { field: 'book' },    
+    { field: 'previous' },
+    { field: 'current' },
+];
+
+const gridOptions: GridOptions = {
+  columnDefs,
+  defaultColDef: {
+    flex: 1,
+    minWidth: 100,
+    resizable: true,
+  },
+  autoGroupColumnDef: {
+    minWidth: 220,
+    field: 'tradeId',
+    cellRendererParams: {
+      checkbox: true,
+    },
+  },
+  enableCellChangeFlash: true,
+  isServerSideGroupOpenByDefault: (params: IsServerSideGroupOpenByDefaultParams) => {
+    return params.rowNode.key === 'Aggressive' || params.rowNode.key === 'Hybrid';
+  },
+  getRowId: (params: GetRowIdParams) => {
+    if (params.level === 0) {
+      return params.data.portfolio;
+    }
+    return params.data.tradeId;
+  },
+  onGridReady: (params: GridReadyEvent) => {
+    // setup the fake server
+    const server = new FakeServer();
+
+    // create datasource with a reference to the fake server
+    const datasource = getServerSideDatasource(server);
+
+    // register the datasource with the grid
+    params.api.setServerSideDatasource(datasource);
+  },
+
+  rowModelType: 'serverSide',
+  groupSelectsChildren: true,
+};
+
+function getServerSideDatasource(server: any) {
+  return {
+    getRows: (params: IServerSideGetRowsParams) => {
+      const response = server.getData(params.request);
+
+      // adding delay to simulate real server call
+      setTimeout(function () {
+        if (response.success) {
+          // call the success callback
+          params.success({
+            rowData: response.rows,
+            rowCount: response.lastRow,
+          });
+        } else {
+          // inform the grid request failed
+          params.fail();
+        }
+      }, 300);
+    },
+  };
+}
+
+function logResults(transaction: ServerSideTransaction, result?: ServerSideTransactionResult) {
+  console.log('[Example] - Applied transaction:', transaction, 'Result:', result);
+}
+
+function deleteAllHybrid() {
+  // NOTE: real applications would be better served listening to a stream of changes from the server instead
+  const serverResponse: any = deletePortfolioOnServer('Hybrid');
+  if (!serverResponse.success) {
+    console.warn('Nothing has changed on the server');
+    return;
+  }
+
+  if (serverResponse) {
+    // apply tranaction to keep grid in sync
+    const transaction = {
+      remove: [{ portfolio: 'Hybrid' }],
+    };
+    const result = gridOptions.api!.applyServerSideTransaction(transaction);
+    logResults(transaction, result);
+  }
+}
+
+function createOneAggressive() {
+  // NOTE: real applications would be better served listening to a stream of changes from the server instead
+  const serverResponse: any = createRowOnServer('Aggressive', 'Aluminium', 'GL-1');
+  if (!serverResponse.success) {
+    console.warn('Nothing has changed on the server');
+    return;
+  }
+
+  if (serverResponse.newGroupCreated) {
+    // if a new group had to be created, reflect in the grid
+    const transaction = {
+      route: [],
+      add: [{ portfolio: 'Aggressive' }],
+    };
+    const result = gridOptions.api!.applyServerSideTransaction(transaction);
+    logResults(transaction, result);
+  } else {
+    // if the group already existed, add rows to it
+    const transaction = {
+      route: ['Aggressive'],
+      add: [serverResponse.newRecord],
+    };
+    const result = gridOptions.api!.applyServerSideTransaction(transaction);
+    logResults(transaction, result);
+  }
+}
+
+function updateAggressiveToHybrid() {
+  // NOTE: real applications would be better served listening to a stream of changes from the server instead
+  const serverResponse: any = changePortfolioOnServer('Aggressive', 'Hybrid');
+  if (!serverResponse.success) {
+    console.warn('Nothing has changed on the server');
+    return;
+  }
+
+  const transaction = {
+    remove: [{ portfolio: 'Aggressive' }],
+  };
+  // aggressive group no longer exists, so delete the group
+  const result = gridOptions.api!.applyServerSideTransaction(transaction);
+  logResults(transaction, result);
+
+  if (serverResponse.newGroupCreated) {
+    // hybrid group didn't exist, so just create the new group
+    const t = {
+      route: [],
+      add: [{ portfolio: 'Hybrid' }],
+    };
+    const r = gridOptions.api!.applyServerSideTransaction(t);
+    logResults(t, r);
+  } else {
+    // hybrid group already existed, add rows to it
+    const t = {
+      route: ['Hybrid'],
+      add: serverResponse.updatedRecords,
+    };
+    const r = gridOptions.api!.applyServerSideTransaction(t);
+    logResults(t, r);
+  }
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+  new Grid(gridDiv, gridOptions);
+});
