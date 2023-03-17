1b49498213ef5cf82b0234ed620a14a27d4ac5c5
Alan Treadway <alan.treadway@ag-grid.com>
2023-03-17 14:19:18 +0000

    Fix docs API defaults for seriesAreaPadding, subtitle and footnote.

--- charts-community-modules/ag-charts-community/src/chart/agChartOptions.ts
+++ charts-community-modules/ag-charts-community/src/chart/agChartOptions.ts
@@ -211,25 +211,13 @@ export interface AgChartPaddingOptions {
 }
 
 export interface AgSeriesAreaPaddingOptions {
-    /**
-     * The number of pixels of padding at the top of the series area.
-     * Default: `0`
-     */
+    /** The number of pixels of padding at the top of the series area. */
     top?: PixelSize;
-    /**
-     * The number of pixels of padding at the right of the series area.
-     * Default: `0`
-     */
+    /** The number of pixels of padding at the right of the series area. */
     right?: PixelSize;
-    /**
-     * The number of pixels of padding at the bottom of the series area.
-     * Default: `0`
-     */
+    /** The number of pixels of padding at the bottom of the series area. */
     bottom?: PixelSize;
-    /**
-     * The number of pixels of padding at the left of the series area.
-     * Default: `0`
-     */
+    /** The number of pixels of padding at the left of the series area. */
     left?: PixelSize;
 }
 
@@ -291,6 +279,8 @@ export interface AgChartCaptionOptions {
     /** Spacing added to help position the text. */
     spacing?: number;
 }
+export interface AgChartSubtitleOptions extends AgChartCaptionOptions {}
+export interface AgChartFooterOptions extends AgChartCaptionOptions {}
 
 export interface AgAxisCaptionOptions {
     /** Whether or not the title should be shown. */
@@ -600,9 +590,9 @@ export interface AgBaseChartOptions {
     /** Configuration for the title shown at the top of the chart. */
     title?: AgChartCaptionOptions;
     /** Configuration for the subtitle shown beneath the chart title. Note: a subtitle will only be shown if a title is also present. */
-    subtitle?: AgChartCaptionOptions;
+    subtitle?: AgChartSubtitleOptions;
     /** Configuration for the footnote shown at the bottom of the chart. */
-    footnote?: AgChartCaptionOptions;
+    footnote?: AgChartFooterOptions;
     /** Global configuration that applies to all tooltips in the chart. */
     tooltip?: AgChartTooltipOptions;
     /** Configuration for the chart legend. */
