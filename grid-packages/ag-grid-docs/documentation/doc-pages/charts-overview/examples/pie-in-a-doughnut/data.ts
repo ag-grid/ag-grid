65f97563795d394168112c754cb6da2234094e6e
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-03-22 13:34:47 +0000

    Fix pie in donut example tooltip titles

--- grid-packages/ag-grid-docs/documentation/doc-pages/charts-overview/examples/pie-in-a-doughnut/data.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/charts-overview/examples/pie-in-a-doughnut/data.ts
@@ -1,21 +1,21 @@
 // https://gs.statcounter.com/browser-market-share/desktop/worldwide/#quarterly-202001-202001-bar
 export function getData2020(): any[] {
-    return [
-        { browser: 'Chrome', share: 0.6869 },
-        { browser: 'Edge', share: 0.0463 },
-        { browser: 'Safari', share: 0.087 },
-        { browser: 'Firefox', share: 0.0955 },
-        { browser: 'Other', share: 0.0843 },
-    ];
+  return [
+    { browser: "Chrome", share: 0.6869, year: 2020 },
+    { browser: "Edge", share: 0.0463, year: 2020 },
+    { browser: "Safari", share: 0.087, year: 2020 },
+    { browser: "Firefox", share: 0.0955, year: 2020 },
+    { browser: "Other", share: 0.0843, year: 2020 },
+  ]
 }
 
 // https://gs.statcounter.com/browser-market-share/desktop/worldwide/#quarterly-202203-202203-bar
 export function getData2022(): any[] {
-    return [
-        { browser: 'Chrome', share: 0.6695 },
-        { browser: 'Edge', share: 0.1086 },
-        { browser: 'Safari', share: 0.0891 },
-        { browser: 'Firefox', share: 0.0757 },
-        { browser: 'Other', share: 0.0681 },
-    ];
+  return [
+    { browser: "Chrome", share: 0.6695, year: 2022 },
+    { browser: "Edge", share: 0.1086, year: 2022 },
+    { browser: "Safari", share: 0.0891, year: 2022 },
+    { browser: "Firefox", share: 0.0757, year: 2022 },
+    { browser: "Other", share: 0.0681, year: 2022 },
+  ]
 }
