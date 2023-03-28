844900971e38b131c842e6c199ce1a80d63a2cab
Alan Treadway <alan.treadway@ag-grid.com>
2023-03-28 10:00:02 +0100

    Fix Charts Docker run.sh for core case.

--- core/jest.config.js
+++ core/jest.config.js
@@ -1,52 +1,14 @@
-const glob = require('glob');
-const fs = require('fs');
-
-const pathToGlob = (path) => path.replace('./', '**/');
-
-const tests = glob.sync('./src/**/*.test.ts');
-const e2eTests = tests
-    .filter((path) => {
-        const fileContents = fs.readFileSync(path).toString();
-
-        // 'Heuristic' for finding e2e tests :-P
-        return fileContents.indexOf('setupMockCanvas()') >= 0;
-    })
-    .map(pathToGlob);
-const unitTests = tests.map(pathToGlob).filter((path) => !e2eTests.includes(path));
-
-const commonConfig = {
+module.exports = {
     globals: {
         'ts-jest': {
             tsconfig: 'tsconfig.test.json',
         },
     },
+    roots: ['<rootDir>/src'],
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
     },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
     testEnvironment: 'jsdom',
-    setupFiles: ['jest-canvas-mock'],
-};
-
-module.exports = {
-    globalSetup: './jest.setup.js',
-    projects: [
-        {
-            displayName: 'unit',
-            roots: ['<rootDir>/src'],
-            testMatch: unitTests,
-            ...commonConfig,
-        },
-        {
-            displayName: 'e2e',
-            roots: ['<rootDir>/src'],
-            testMatch: e2eTests,
-            runner: 'jest-serial-runner',
-            // WIP discussion: https://github.com/facebook/jest/issues/10936
-            // maxWorkers: 1,
-            // WIP discussion: https://github.com/facebook/jest/pull/10912
-            // maxConcurrency: 1,
-            ...commonConfig,
-        },
-    ],
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
