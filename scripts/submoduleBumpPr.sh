718bf9720885d0d93658b3df27b2c8a546e3298c
Alan Treadway <alan.treadway@ag-grid.com>
2023-03-30 10:46:34 +0100

    Fix submodule PR script.

--- scripts/submoduleBumpPr.sh
+++ scripts/submoduleBumpPr.sh
@@ -4,17 +4,18 @@ set -eu
 
 START_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 REMOTE_BRANCH=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
+REMOTE=$(git rev-parse --abbrev-ref --symbolic-full-name @{u} | sed 's:/: :' | awk '{print $1}')
 HEAD=$(git rev-parse HEAD)
 BRANCH=bump-submodules-${HEAD}
 
-git fetch
+git fetch ${REMOTE}
 git stash -u
 git checkout -b ${BRANCH}
-git pull
-git submodule foreach git checkout latest
-git submodule foreach git pull origin
+git merge ${REMOTE_BRANCH}
+# git submodule foreach git checkout latest
+# git submodule foreach git pull origin
 git commit -a -m "Submodule bump."
-git push --set-upstream origin ${BRANCH}
+git push --set-upstream ${REMOTE} ${BRANCH}
 open https://github.com/ag-grid/ag-grid/pull/new/${BRANCH}
 git checkout ${START_BRANCH}
 git stash pop
