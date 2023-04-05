f07d7e940a382444b96d7bb7c0a240a354768b20
Mana <mana.jalali.15@ucl.ac.uk>
2023-04-05 10:18:28 +0100

    AG-4371 - more crosshair tests

--- core/src/crosshair/crosshair.test.ts
+++ core/src/crosshair/crosshair.test.ts
@@ -4,6 +4,7 @@ import {
     AgCartesianAxisPosition,
     AgCartesianChartOptions,
     AgChartOptions,
+    AgCartesianAxisOptions,
     AgEnterpriseCharts,
     _ModuleSupport,
 } from '../main';
@@ -97,82 +98,121 @@ const BASE_OPTIONS: AgChartOptions = {
     ],
 };
 
-const SIMPLE_AXIS_OPTIONS: AgCartesianChartOptions = {
+const SIMPLE_AXIS_OPTIONS: AgCartesianAxisOptions[] = [
+    {
+        position: 'left',
+        type: 'number',
+        crosshair: CROSSHAIR_OPTIONS,
+    },
+    {
+        position: 'bottom',
+        type: 'number',
+        crosshair: CROSSHAIR_OPTIONS,
+    },
+];
+
+const CATEGORY_AXIS_OPTIONS: AgCartesianAxisOptions[] = [
+    {
+        position: 'left',
+        type: 'number',
+        crosshair: CROSSHAIR_OPTIONS,
+    },
+    {
+        position: 'bottom',
+        type: 'category',
+        crosshair: CROSSHAIR_OPTIONS,
+    },
+];
+
+const SECONDARY_AXIS_OPTIONS: AgCartesianAxisOptions[] = [
+    {
+        position: 'left',
+        type: 'number',
+        keys: ['y1', 'y3'],
+        crosshair: CROSSHAIR_OPTIONS,
+    },
+    {
+        position: 'left',
+        type: 'number',
+        keys: ['y2'],
+        crosshair: {
+            ...CROSSHAIR_OPTIONS,
+            stroke: '#73C0DE',
+        },
+    },
+    {
+        position: 'bottom',
+        type: 'number',
+        crosshair: CROSSHAIR_OPTIONS,
+    },
+];
+
+const SIMPLE_LINE_OPTIONS: AgCartesianChartOptions = {
     ...BASE_OPTIONS,
-    axes: [
-        {
-            position: 'left',
-            type: 'number',
-            crosshair: CROSSHAIR_OPTIONS,
-        },
-        {
-            position: 'bottom',
-            type: 'number',
-            crosshair: CROSSHAIR_OPTIONS,
-        },
-    ],
+    axes: SIMPLE_AXIS_OPTIONS,
 };
 
-const CATEGORY_AXIS_OPTIONS: AgCartesianChartOptions = {
+const SIMPLE_COLUMN_OPTIONS: AgCartesianChartOptions = {
     ...BASE_OPTIONS,
-    series: SIMPLE_AXIS_OPTIONS.series?.map((s) => ({ ...s, type: 'column' })),
+    axes: CATEGORY_AXIS_OPTIONS,
+    series: BASE_OPTIONS.series?.map((s) => ({ ...s, type: 'column' })),
+};
+
+const LINE_SECONDARY_AXIS_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    axes: SECONDARY_AXIS_OPTIONS,
+};
+
+const STACKED_COLUMN_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    axes: SIMPLE_AXIS_OPTIONS,
+    series: BASE_OPTIONS.series?.map((s) => ({ ...s, type: 'column', stacked: true })),
+};
+
+const GROUPED_COLUMN_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    axes: CATEGORY_AXIS_OPTIONS,
+    series: BASE_OPTIONS.series?.map((s) => ({ ...s, type: 'column', stacked: false })),
+};
+
+const STACKED_BAR_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    axes: SIMPLE_AXIS_OPTIONS,
+    series: BASE_OPTIONS.series?.map((s) => ({ ...s, type: 'bar', stacked: true })),
+};
+
+const GROUPED_BAR_OPTIONS: AgCartesianChartOptions = {
+    ...BASE_OPTIONS,
+    data: [
+        { x: 0, y1: 100, y2: 2, y3: 1700 },
+        { x: 1, y1: 50, y2: 20, y3: 55 },
+        { x: 2, y1: 25, y2: 200, y3: 48 },
+        { x: 3, y1: 75, y2: 2000, y3: 72 },
+    ],
     axes: [
         {
-            position: 'left',
+            position: 'bottom',
             type: 'number',
             crosshair: CROSSHAIR_OPTIONS,
         },
         {
-            position: 'bottom',
+            position: 'left',
             type: 'category',
             crosshair: CROSSHAIR_OPTIONS,
         },
     ],
-};
-
-const SECONDARY_AXIS_OPTIONS: AgCartesianChartOptions = {
-    ...BASE_OPTIONS,
-    axes: [
-        {
-            position: 'left',
-            type: 'number',
-            keys: ['y1', 'y3'],
-            crosshair: CROSSHAIR_OPTIONS,
-        },
-        {
-            position: 'left',
-            type: 'number',
-            keys: ['y2'],
-            crosshair: {
-                ...CROSSHAIR_OPTIONS,
-                stroke: '#73C0DE',
-            },
-        },
-        {
-            position: 'bottom',
-            type: 'number',
-            crosshair: CROSSHAIR_OPTIONS,
-        },
-    ],
-};
-
-const STACKED_COLUMN_OPTIONS: AgCartesianChartOptions = {
-    ...SIMPLE_AXIS_OPTIONS,
-    series: SIMPLE_AXIS_OPTIONS.series?.map((s) => ({ ...s, type: 'column', stacked: true })),
-};
-
-const STACKED_BAR_OPTIONS: AgCartesianChartOptions = {
-    ...SIMPLE_AXIS_OPTIONS,
-    series: SIMPLE_AXIS_OPTIONS.series?.map((s) => ({ ...s, type: 'bar', stacked: true })),
+    series: BASE_OPTIONS.series?.map((s) => ({ ...s, type: 'bar', stacked: false })),
 };
 
 const STACKED_AREA_OPTIONS: AgCartesianChartOptions = {
-    ...SIMPLE_AXIS_OPTIONS,
-    series: SIMPLE_AXIS_OPTIONS.series?.map((s) => ({ ...s, type: 'area', stacked: true })),
+    ...BASE_OPTIONS,
+    axes: SIMPLE_AXIS_OPTIONS,
+    series: BASE_OPTIONS.series?.map((s) => ({ ...s, type: 'area', stacked: true })),
 };
 
 const HISTOGRAM_OPTIONS: AgCartesianChartOptions = {
-    ...SIMPLE_AXIS_OPTIONS,
+    ...BASE_OPTIONS,
+    axes: SIMPLE_AXIS_OPTIONS,
     data: [
         { x: 0, y1: 100 },
         { x: 1, y1: 500 },
@@ -227,16 +267,18 @@ describe('Crosshair', () => {
     };
 
     const CASES = [
-        SIMPLE_AXIS_OPTIONS,
-        applyAxesFlip(SIMPLE_AXIS_OPTIONS),
-        CATEGORY_AXIS_OPTIONS,
-        SECONDARY_AXIS_OPTIONS,
-        applyAxesFlip(SECONDARY_AXIS_OPTIONS),
+        SIMPLE_LINE_OPTIONS,
+        applyAxesFlip(SIMPLE_LINE_OPTIONS),
+        SIMPLE_COLUMN_OPTIONS,
+        LINE_SECONDARY_AXIS_OPTIONS,
+        applyAxesFlip(LINE_SECONDARY_AXIS_OPTIONS),
         STACKED_COLUMN_OPTIONS,
         STACKED_BAR_OPTIONS,
         STACKED_AREA_OPTIONS,
         applyAxesFlip(STACKED_AREA_OPTIONS),
         HISTOGRAM_OPTIONS,
+        GROUPED_COLUMN_OPTIONS,
+        GROUPED_BAR_OPTIONS,
     ];
 
     for (const TEST_CASE of CASES) {
@@ -256,7 +298,7 @@ describe('Crosshair', () => {
 
             chart = AgEnterpriseCharts.create(options);
             await waitForChartStability(chart);
-            await hoverAction(605, 120)(chart);
+            await hoverAction(605, 125)(chart);
             await compare();
         });
 
