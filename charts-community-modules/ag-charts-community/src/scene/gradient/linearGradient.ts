c748640c4c460179b16af257075ab37cad47482c
Alexander Shutau <shutovby@gmail.com>
2023-04-13 10:41:35 +0100

    AG-7600 Make angle calculations readable

--- charts-community-modules/ag-charts-community/src/scene/gradient/linearGradient.ts
+++ charts-community-modules/ag-charts-community/src/scene/gradient/linearGradient.ts
@@ -1,34 +1,39 @@
 import { Gradient } from './gradient';
 import { BBox } from '../bbox';
-import { toRadians } from '../../util/angle';
+import { toRadians, normalizeAngle360 } from '../../util/angle';
 
 export class LinearGradient extends Gradient {
     angle = 0;
 
     createGradient(ctx: CanvasRenderingContext2D, bbox: BBox): CanvasGradient | string {
-        const { stops } = this;
-        let { angle } = this;
-        angle = (((angle + 90) % 360) + 360) % 360;
-        const radians = toRadians(angle);
+        // Gradient 0° angle starts at top according to CSS spec
+        const angleOffset = 90;
+        const { stops, angle } = this;
+        const radians = normalizeAngle360(toRadians(angle + angleOffset));
         const cos = Math.cos(radians);
         const sin = Math.sin(radians);
+
         const w = bbox.width;
         const h = bbox.height;
         const cx = bbox.x + w * 0.5;
         const cy = bbox.y + h * 0.5;
 
         if (w > 0 && h > 0) {
-            const d = Math.sqrt(h * h + w * w) / 2;
-            const a = Math.atan2(h, w);
-            const r =
-                angle <= 90
-                    ? radians
-                    : angle <= 180
-                    ? Math.PI - radians
-                    : angle <= 270
-                    ? radians - Math.PI
-                    : 2 * Math.PI - radians;
-            const l = d * Math.abs(Math.cos(r - a));
+            const diagonal = Math.sqrt(h * h + w * w) / 2;
+            const diagonalAngle = Math.atan2(h, w);
+
+            let quarteredAngle: number;
+            if (radians < Math.PI / 2) {
+                quarteredAngle = radians;
+            } else if (radians < Math.PI) {
+                quarteredAngle = Math.PI - radians;
+            } else if (radians < (3 * Math.PI) / 2) {
+                quarteredAngle = radians - Math.PI;
+            } else {
+                quarteredAngle = 2 * Math.PI - radians;
+            }
+
+            const l = diagonal * Math.abs(Math.cos(quarteredAngle - diagonalAngle));
             const gradient = ctx.createLinearGradient(cx + cos * l, cy + sin * l, cx - cos * l, cy - sin * l);
 
             stops.forEach((stop) => {
