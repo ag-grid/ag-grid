091755a52a60fd5ad9add5eea333fadd9162de1c
Stephen Cooper <scooperdev@gmail.com>
2023-04-18 11:42:32 +0100

    AG-6846 add isCellEditable to col docs

--- grid-community-modules/core/src/ts/entities/column.ts
+++ grid-community-modules/core/src/ts/entities/column.ts
@@ -437,6 +437,9 @@ export class Column implements IHeaderColumn, IProvidedColumn, IEventEmitter {
         return false;
     }
 
+    /**
+     * Returns `true` if the cell for this column is editable for the given `rowNode`, otherwise `false`.
+     */
     public isCellEditable(rowNode: IRowNode): boolean {
         // only allow editing of groups if the user has this option enabled
         if (rowNode.group && !this.gridOptionsService.is('enableGroupEdit')) {
