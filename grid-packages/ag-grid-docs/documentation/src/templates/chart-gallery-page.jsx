d6a83b42aac8694a4070cae2d477c761532ae12d
Mark Durrant <mark.j.durrant@gmail.com>
2023-04-18 09:15:31 +0100

    AG-8094 - Make docs charts gallery pages full width

--- grid-packages/ag-grid-docs/documentation/src/templates/chart-gallery-page.jsx
+++ grid-packages/ag-grid-docs/documentation/src/templates/chart-gallery-page.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import toKebabCase from 'utils/to-kebab-case';
+import classnames from 'classnames';
 import ExampleRunner from 'components/example-runner/ExampleRunner';
 import { SEO } from 'components/SEO';
+import React from 'react';
 import { getHeaderTitle } from 'utils/page-header';
-import pageStyles from './doc-page.module.scss';
+import toKebabCase from 'utils/to-kebab-case';
 import styles from './chart-gallery-page.module.scss';
+import pageStyles from './doc-page.module.scss';
 
 /**
  * This template is used for individual chart gallery pages.
@@ -12,18 +13,31 @@ import styles from './chart-gallery-page.module.scss';
 const ChartGalleryPageTemplate = ({ pageContext: { framework, name, description, previous, next, pageName } }) => {
     return (
         <div id="doc-page-wrapper" className={pageStyles['doc-page__wrapper']}>
-            <div id="doc-content" className={pageStyles['doc-page']}>
+            <div id="doc-content" className={classnames(pageStyles['doc-page'], pageStyles.noSideMenu)}>
                 {/*eslint-disable-next-line react/jsx-pascal-case*/}
                 <SEO
                     title={`Gallery - ${name}`}
                     description={description.replace(/<[^>]+>/g, '')}
                     framework={framework}
-                    pageName={pageName} />
+                    pageName={pageName}
+                />
 
                 <div className={styles['chart-navigation']}>
                     {/* eslint-disable jsx-a11y/control-has-associated-label */}
-                    {previous && <a className={styles['chart-navigation__left']} href={`../${toKebabCase(previous)}/`} dangerouslySetInnerHTML={{ __html: `\u276e&nbsp;&nbsp;${previous}` }}></a>}
-                    {next && <a className={styles['chart-navigation__right']} href={`../${toKebabCase(next)}/`} dangerouslySetInnerHTML={{ __html: `${next}&nbsp;&nbsp;\u276f` }}></a>}
+                    {previous && (
+                        <a
+                            className={styles['chart-navigation__left']}
+                            href={`../${toKebabCase(previous)}/`}
+                            dangerouslySetInnerHTML={{ __html: `\u276e&nbsp;&nbsp;${previous}` }}
+                        ></a>
+                    )}
+                    {next && (
+                        <a
+                            className={styles['chart-navigation__right']}
+                            href={`../${toKebabCase(next)}/`}
+                            dangerouslySetInnerHTML={{ __html: `${next}&nbsp;&nbsp;\u276f` }}
+                        ></a>
+                    )}
                     {/* eslint-enable jsx-a11y/control-has-associated-label */}
                 </div>
 
@@ -37,7 +51,8 @@ const ChartGalleryPageTemplate = ({ pageContext: { framework, name, description,
                     framework={framework}
                     pageName={pageName}
                     library="charts"
-                    options={{ exampleHeight: '60vh' }} />
+                    options={{ exampleHeight: '60vh' }}
+                />
             </div>
         </div>
     );
