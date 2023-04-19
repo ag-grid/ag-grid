a5ca5e8d0f3d183d03c32fbf2d6653be560da697
Mark Durrant <mark.j.durrant@gmail.com>
2023-04-19 10:32:00 +0100

    AG-8094 - Update homepage features list icons

--- grid-packages/ag-grid-docs/documentation/src/pages/components/home-page-demos/FeaturesList.jsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/components/home-page-demos/FeaturesList.jsx
@@ -4,27 +4,30 @@ import AccessoriesIcon from '../../../images/inline-svgs/docs-category-icons/ico
 import DataIcon from '../../../images/inline-svgs/docs-category-icons/icon-data.svg';
 import EditCellsIcon from '../../../images/inline-svgs/docs-category-icons/icon-edit-cells.svg';
 import FilterIcon from '../../../images/inline-svgs/docs-category-icons/icon-filter.svg';
-import GroupIcon from '../../../images/inline-svgs/docs-category-icons/icon-group.svg';
 import ImportExportIcon from '../../../images/inline-svgs/docs-category-icons/icon-import-export.svg';
 import MiscIcon from '../../../images/inline-svgs/docs-category-icons/icon-misc.svg';
+import PivotIcon from '../../../images/inline-svgs/docs-category-icons/icon-pivot.svg';
+import SigmaIcon from '../../../images/inline-svgs/docs-category-icons/icon-sigma.svg';
+import TableIcon from '../../../images/inline-svgs/docs-category-icons/icon-table.svg';
+import TreeIcon from '../../../images/inline-svgs/docs-category-icons/icon-tree.svg';
 import styles from './FeaturesList.module.scss';
 
 const featuresListItems = [
     { name: 'Cell Editing', url: '/javascript-data-grid/cell-editing/', icon: <EditCellsIcon /> },
     { name: 'Transactions', url: '/javascript-data-grid/data-update-transactions/', icon: <DataIcon /> },
-    { name: 'Aggregation', url: '/javascript-data-grid/aggregation/', icon: <GroupIcon /> },
+    { name: 'Aggregation', url: '/javascript-data-grid/aggregation/', icon: <SigmaIcon /> },
 
-    { name: 'Row Grouping', url: '/javascript-data-grid/grouping/', icon: <GroupIcon /> },
+    { name: 'Row Grouping', url: '/javascript-data-grid/grouping/', icon: <TableIcon /> },
     { name: 'Master Detail', url: '/javascript-data-grid/master-detail/', icon: <MiscIcon /> },
     { name: 'Clipboard', url: '/javascript-data-grid/clipboard/', icon: <ImportExportIcon /> },
 
     { name: 'Server-Side Row Model', url: '/javascript-data-grid/server-side-model/', icon: <DataIcon /> },
-    { name: 'Pivoting', url: '/javascript-data-grid/pivoting/', icon: <GroupIcon /> },
+    { name: 'Pivoting', url: '/javascript-data-grid/pivoting/', icon: <PivotIcon /> },
     { name: 'Column Filtering', url: '/javascript-data-grid/filtering-overview/', icon: <FilterIcon /> },
 
     { name: 'Export to Excel', url: '/javascript-data-grid/excel-export/', icon: <ImportExportIcon /> },
     { name: 'Context Menu', url: '/javascript-data-grid/context-menu/', icon: <AccessoriesIcon /> },
-    { name: 'Tree Data', url: '/javascript-data-grid/tree-data/', icon: <GroupIcon /> },
+    { name: 'Tree Data', url: '/javascript-data-grid/tree-data/', icon: <TreeIcon /> },
 ];
 
 const ItemGroup = ({ items }) => {
