1c4a4138dd724d2a0260ec27e6327578a0a52344
Stephen Cooper <scooperdev@gmail.com>
2023-05-02 14:40:55 +0100

    AG-8430 Clean up React examples

--- grid-packages/ag-grid-docs/documentation/doc-pages/full-width-rows/examples/full-width-keyboard-navigation/fullWidthCellRenderer_reactFunctional.tsx
+++ grid-packages/ag-grid-docs/documentation/doc-pages/full-width-rows/examples/full-width-keyboard-navigation/fullWidthCellRenderer_reactFunctional.tsx
@@ -13,7 +13,7 @@ export default (props: ICellRendererParams) => {
                 src={`https://www.ag-grid.com/example-assets/flags/${code}.png`}
                 />
             </button>
-            <input value={name} />
+            <input defaultValue={name} />
             <a href={`https://www.google.com/search?q=${language}`} target="_blank">
                 {language}
             </a>
