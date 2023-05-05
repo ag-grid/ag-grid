d35d5dd1c10381daf827a8a7930e580503f43a92
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-05-05 12:27:38 +0100

    AG-3479 Change window event handling to only be active for wheel

--- charts-community-modules/ag-charts-community/src/chart/interaction/interactionManager.ts
+++ charts-community-modules/ag-charts-community/src/chart/interaction/interactionManager.ts
@@ -101,7 +101,11 @@ export class InteractionManager extends BaseManager<InteractionTypes, Interactio
         }
 
         for (const type of WINDOW_EVENT_HANDLERS) {
-            window.addEventListener(type, this.eventHandler, { passive: false });
+            if (type === 'wheel') {
+                window.addEventListener(type, this.eventHandler, { passive: false });
+            } else {
+                window.addEventListener(type, this.eventHandler);
+            }
         }
 
         if (InteractionManager.interactionDocuments.indexOf(doc) < 0) {
