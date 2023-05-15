5e0a03e777ec5979de266dc0b29e762dd63bfe9d
Mark Durrant <mark.j.durrant@gmail.com>
2023-05-15 11:20:58 +0100

    AG-8094 - Refactor note & warning custom blocks to use <Alert/> component

--- grid-packages/ag-grid-docs/documentation/src/components/alert/Alert.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/alert/Alert.tsx
@@ -12,7 +12,7 @@ export const Alert: FunctionComponent<Props> = ({ type = 'default', children }) 
     const icon = type !== 'default' ? type : null;
 
     return (
-        <div className={classNames(styles.alert, styles[type])}>
+        <div className={classNames('alert', styles.alert, styles[type])}>
             {icon && <Icon name={icon} />}
 
             <div className={styles.content}>{children}</div>
