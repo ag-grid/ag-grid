1a71985918b4a9ad5965edaf3378a11604cfbd61
Mark Durrant <mark.j.durrant@gmail.com>
2023-05-17 09:45:46 +0100

    Ag 8094 design updates (#6611)
    * AG-8094 - Charts API Explorer layout tweaks
    
    * AG-8094 - Tweak Charts API Explorer code selction <select/> styling
    
    * AG-8094 - Fix bug with code zebra stripes on Firefox
    
    * AG-8094 - Make active item styling more consistent between framework selector and tabbed sections
    
    * AG-8094 - Copy change 'Code style:' >> 'Language:'
    
    * AG-8094 - Fix code/snippet white space bug. Make all snipets consistent font size
    
    * AG-8094 - Add loading spinner for React, Angular, & Vue examples runners
    
    * AG-8094 - Minor loading spinner refactor
    
    * AG-8094 - Minor tweak to code zebra stripe position

--- grid-packages/ag-grid-docs/documentation/src/components/example-runner/AngularTemplate.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/example-runner/AngularTemplate.jsx
@@ -1,26 +1,47 @@
 import React from 'react';
 import ExampleStyle from './ExampleStyle';
 import Extras from './Extras';
+import { LoadingSpinner } from './LoadingSpinner';
+import MetaData from './MetaData';
 import Scripts from './Scripts';
 import Styles from './Styles';
 import SystemJs from './SystemJs';
-import MetaData from './MetaData';
 
 /**
  * This is the template for executing Angular examples in the example runner.
  */
-const AngularTemplate = ({ isExecuting, modifiedTimeMs, library, boilerplatePath, appLocation, options, scriptFiles, styleFiles, type, importType }) =>
+const AngularTemplate = ({
+    isExecuting,
+    modifiedTimeMs,
+    library,
+    boilerplatePath,
+    appLocation,
+    options,
+    scriptFiles,
+    styleFiles,
+    type,
+    importType,
+}) => (
     <html lang="en">
         <head>
-            <MetaData title="Angular example" modifiedTimeMs={modifiedTimeMs} isExecuting={isExecuting} options={options} />
+            <MetaData
+                title="Angular example"
+                modifiedTimeMs={modifiedTimeMs}
+                isExecuting={isExecuting}
+                options={options}
+            />
             <ExampleStyle rootId="app" />
-            {(type !== "generated" || library !== "grid") && <Styles files={styleFiles}/>}
+            {(type !== 'generated' || library !== 'grid') && <Styles files={styleFiles} />}
             <Extras options={options} />
         </head>
         <body>
-            <div id="app" dangerouslySetInnerHTML={{ __html: `<my-app>Loading Angular example&hellip;</my-app>` }}></div>
+            <div id="app" dangerouslySetInnerHTML={{ __html: `<my-app></my-app>` }}></div>
 
-            <script dangerouslySetInnerHTML={{ __html: `document.write('<base href="' + document.location + '" />');` }}></script>
+            <LoadingSpinner />
+
+            <script
+                dangerouslySetInnerHTML={{ __html: `document.write('<base href="' + document.location + '" />');` }}
+            ></script>
             <script src="https://cdn.jsdelivr.net/npm/core-js-bundle@3.6.5/minified.js"></script>
             <script src="https://cdn.jsdelivr.net/npm/zone.js@0.11.2/dist/zone.min.js"></script>
             <Scripts files={scriptFiles} />
@@ -31,8 +52,10 @@ const AngularTemplate = ({ isExecuting, modifiedTimeMs, library, boilerplatePath
                 startFile={boilerplatePath + 'main.ts'}
                 framework={'angular'}
                 importType={importType}
-                options={options} />
+                options={options}
+            />
         </body>
-    </html>;
+    </html>
+);
 
 export default AngularTemplate;
