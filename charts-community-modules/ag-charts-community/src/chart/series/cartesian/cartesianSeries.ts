d941f8b6f5af16e62bcf322cec38752d9413b903
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-05-18 13:07:20 +0100

    Remove random function from rebase issue

--- charts-community-modules/ag-charts-community/src/chart/series/cartesian/cartesianSeries.ts
+++ charts-community-modules/ag-charts-community/src/chart/series/cartesian/cartesianSeries.ts
@@ -658,47 +658,6 @@ export abstract class CartesianSeries<
         return false;
     }
 
-    protected validateXYData(
-        xKey: string,
-        yKey: string,
-        data: any[],
-        xAxis: ChartAxis,
-        yAxis: ChartAxis,
-        xData: number[],
-        yData: any[],
-        yDepth = 1
-    ) {
-        if (this.chart?.mode === 'integrated') {
-            // Integrated Charts use-cases do not require this validation.
-            return true;
-        }
-
-        if (!xAxis || !yAxis || data.length === 0 || (this.seriesItemEnabled.size > 0 && !this.isAnySeriesVisible())) {
-            return true;
-        }
-
-        const hasNumber = (items: any[], depth = 0, maxDepth = 0): boolean => {
-            return items.some(
-                depth === maxDepth ? (y) => isContinuous(y) : (arr) => hasNumber(arr, depth + 1, maxDepth)
-            );
-        };
-
-        const isContinuousX = xAxis.scale instanceof ContinuousScale;
-        const isContinuousY = yAxis.scale instanceof ContinuousScale;
-
-        let validationResult = true;
-        if (isContinuousX && !hasNumber(xData)) {
-            Logger.warnOnce(`the number axis has no numeric data supplied for xKey: [${xKey}].`);
-            validationResult = false;
-        }
-        if (isContinuousY && !hasNumber(yData, 0, yDepth - 1)) {
-            Logger.warnOnce(`the number axis has no numeric data supplied for yKey: [${yKey}].`);
-            validationResult = false;
-        }
-
-        return validationResult;
-    }
-
     protected async updateHighlightSelectionItem(opts: {
         item?: C['nodeData'][number];
         highlightSelection: NodeDataSelection<N, C>;
