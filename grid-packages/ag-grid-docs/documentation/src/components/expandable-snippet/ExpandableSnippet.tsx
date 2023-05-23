36c46286ae02eb35655e8a560e223d20cfab9a05
Mark Durrant <mark.j.durrant@gmail.com>
2023-05-23 14:49:48 +0100

    AG-8094 - Remove font awesome icons and replace with <Icon/> component

--- grid-packages/ag-grid-docs/documentation/src/components/expandable-snippet/ExpandableSnippet.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/expandable-snippet/ExpandableSnippet.tsx
@@ -1,7 +1,6 @@
 import React, { Fragment, useState } from "react";
 import classnames from 'classnames';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { Icon } from "../Icon";
 
 import {convertMarkdown, formatJsDocString} from "../documentation-helpers";
 import styles from "./ExpandableSnippet.module.scss";
@@ -72,7 +71,6 @@ const BuildSnippet: React.FC<BuildSnippetParams> = ({
     config = {},
 }) => {
     return renderObjectBreadcrumb(breadcrumbs, () => <Fragment>
-        <FontAwesomeIcon icon={faChevronRight} className={styles['node-expander']} symbol="node-expander" />
         <div className={styles['json-object']} role="presentation">
             <ModelSnippet model={model} config={config} path={[]}></ModelSnippet>
         </div>
@@ -370,11 +368,7 @@ function maybeRenderModelDocumentation(
 }
 
 function renderJsonNodeExpander(isExpanded: boolean) {
-    return (
-        <Fragment>
-            <svg className={classnames(styles['expander'], { 'fa-rotate-90': isExpanded })}><use href="#node-expander" role="button"/></svg>
-        </Fragment>
-    );
+    return <Icon name="chevronRight" svgClasses={classnames(styles['expander'], { 'fa-rotate-90': isExpanded })} />
 }
 
 function renderPropertyDeclaration(
