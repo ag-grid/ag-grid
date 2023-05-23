fa2a4fa08cfcfc9e73bf5e64de7da253a4b632e7
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-05-23 11:56:32 +0100

    AG-8463 Fix line series animation to sort data for smooth drawing

--- charts-community-modules/ag-charts-community/src/chart/series/cartesian/lineSeries.ts
+++ charts-community-modules/ag-charts-community/src/chart/series/cartesian/lineSeries.ts
@@ -560,12 +560,19 @@ export class LineSeries extends CartesianSeries<LineContext> {
                 repeat: 0,
             };
 
+            // Clone and sort the nodes by their x-values to ensure the line is drawn smoothly between each point
+            const sortedNodes = [...nodeData];
+            sortedNodes.sort((a, b) => {
+                if (a.point.x === b.point.x) return 0;
+                return a.point.x < b.point.x ? -1 : 1;
+            });
+
             this.animationManager?.animate<number>(`${this.id}_empty-update-ready`, {
                 ...animationOptions,
                 onUpdate(xValue) {
                     linePath.clear({ trackChanges: true });
 
-                    nodeData.forEach((datum, index) => {
+                    sortedNodes.forEach((datum, index) => {
                         if (datum.point.x <= xValue) {
                             // Draw/move the full segment if past the end of this segment
                             if (datum.point.moveTo) {
@@ -573,9 +580,9 @@ export class LineSeries extends CartesianSeries<LineContext> {
                             } else {
                                 linePath.lineTo(datum.point.x, datum.point.y);
                             }
-                        } else if (index > 0 && nodeData[index - 1].point.x < xValue) {
+                        } else if (index > 0 && sortedNodes[index - 1].point.x < xValue) {
                             // Draw/move partial line if in between the start and end of this segment
-                            const start = nodeData[index - 1].point;
+                            const start = sortedNodes[index - 1].point;
                             const end = datum.point;
 
                             const x = xValue;
