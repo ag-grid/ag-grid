b13bd02960c523fc519488763fece969d71e3a10
Guilherme Lopes <guilherme@gwm-solutions.com>
2023-05-25 14:36:20 -0300

    Revert AG-8588 - allow quotes to be pasted into the grid

--- grid-enterprise-modules/clipboard/src/clipboard/clipboardService.ts
+++ grid-enterprise-modules/clipboard/src/clipboard/clipboardService.ts
@@ -222,6 +222,8 @@ export class ClipboardService extends BeanStub implements IClipboardService {
         const data: any[][] = [];
         const isNewline = (char: string) => char === '\r' || char === '\n';
 
+        let insideQuotedField = false;
+
         if (strData === '') { return [['']]; }
 
         // iterate over each character, keep track of current row and column (of the returned array)
@@ -243,24 +245,45 @@ export class ClipboardService extends BeanStub implements IClipboardService {
 
             ensureDataExists();
 
-            if (currentChar === delimiter) {
-                // move to next column
-                column++;
-                ensureDataExists();
+            if (currentChar === '"') {
+                if (insideQuotedField) {
+                    if (nextChar === '"') {
+                        // unescape double quote
+                        data[row][column] += '"';
+                        position++;
+                    } else {
+                        // exit quoted field
+                        insideQuotedField = false;
+                    }
 
-                continue;
-            } else if (isNewline(currentChar)) {
-                // move to next row
-                column = 0;
-                row++;
-                ensureDataExists();
-
-                if (currentChar === '\r' && nextChar === '\n') {
-                    // skip over second newline character if it exists
-                    position++;
+                    continue;
+                } else if (previousChar === undefined || previousChar === delimiter || isNewline(previousChar)) {
+                    // enter quoted field
+                    insideQuotedField = true;
+                    continue;
                 }
+            }
 
-                continue;
+            if (!insideQuotedField) {
+                if (currentChar === delimiter) {
+                    // move to next column
+                    column++;
+                    ensureDataExists();
+
+                    continue;
+                } else if (isNewline(currentChar)) {
+                    // move to next row
+                    column = 0;
+                    row++;
+                    ensureDataExists();
+
+                    if (currentChar === '\r' && nextChar === '\n') {
+                        // skip over second newline character if it exists
+                        position++;
+                    }
+
+                    continue;
+                }
             }
 
             // add current character to current column
