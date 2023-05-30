53c082095a11d3d44d798e71cc87e1ac1cb369ca
Tak Tran <contact@tutaktran.com>
2023-05-30 12:03:35 +0100

    AG-8094 - Fix loading design system scss file

--- grid-packages/ag-grid-docs/documentation/src/layouts/index.js
+++ grid-packages/ag-grid-docs/documentation/src/layouts/index.js
@@ -6,12 +6,9 @@ import { SiteHeader } from 'components/site-header/SiteHeader';
 import { TopBar } from 'components/TopBar';
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import designSystem from '../design-system/design-system.scss';
+import '../design-system/design-system.scss';
 import favIcons from '../images/favicons';
 import styles from './index.module.scss';
-
-// Required so that VS Code dosen't automatically remove the import with format on save
-designSystem;
 
 /**
  * This controls the layout template for all pages.
