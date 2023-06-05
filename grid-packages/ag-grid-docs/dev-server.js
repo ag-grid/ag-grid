254aac02714fc2f7020e164b4180c2b3b4968627
Stephen Cooper <scooperdev@gmail.com>
2023-06-05 11:07:44 +0100

    potentially reduce CORS issues

--- grid-packages/ag-grid-docs/dev-server.js
+++ grid-packages/ag-grid-docs/dev-server.js
@@ -813,9 +813,17 @@ module.exports = async (skipFrameworks, skipExampleFormatting, chartsOnly, skipE
 
             const app = express();
 
-            // necessary for plunkers
+            // necessary for plunkers and localhost
+            app.options("/*", function (req, res, next) {
+                res.header('Access-Control-Allow-Origin', '*');
+                res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+                res.send(200);
+            });
             app.use(function (req, res, next) {
                 res.setHeader('Access-Control-Allow-Origin', '*');
+                res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+                res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
                 return next();
             });
 
