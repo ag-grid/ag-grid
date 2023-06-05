c42fb928f7c70a82c7c36200177c4f7977784207
Tak Tran <contact@tutaktran.com>
2023-06-05 14:29:30 +0100

    AG-8589 - Add 404 page tracking + abstract tracking plausible once
    AG-8589 - Abstract tracking plausible once
    AG-8589 - Add 404 page tracking

--- grid-packages/ag-grid-docs/documentation/src/pages/404.jsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/404.jsx
@@ -1,22 +1,31 @@
 import { Link } from 'gatsby';
-import React from 'react';
+import React, { useEffect } from 'react';
 import docsStyles from '../templates/doc-page.module.scss';
+import { track404 } from '../utils/analytics';
 import Seo from './components/SEO';
 
-const ErrorPage = () => (
-    <>
-        <Seo title="AG Grid: Page Not Found" />
-        <div className={docsStyles['doc-page-wrapper']}>
-            <div className={docsStyles['doc-page']}>
-                <h1>AG Grid: Page Not Found</h1>
-                <p style={{ height: 600 }}>
-                    Sorry, but it looks like you've ended up in the wrong place. Please go to the{' '}
-                    <Link to="/">homepage</Link> or&nbsp;
-                    <Link to="/documentation">documentation</Link> to try to find what you're looking for.
-                </p>
+const ErrorPage = () => {
+    useEffect(() => {
+        track404({
+            path: document.location.pathname,
+        });
+    }, []);
+
+    return (
+        <>
+            <Seo title="AG Grid: Page Not Found" />
+            <div className={docsStyles['doc-page-wrapper']}>
+                <div className={docsStyles['doc-page']}>
+                    <h1>AG Grid: Page Not Found</h1>
+                    <p style={{ height: 600 }}>
+                        Sorry, but it looks like you've ended up in the wrong place. Please go to the{' '}
+                        <Link to="/">homepage</Link> or&nbsp;
+                        <Link to="/documentation">documentation</Link> to try to find what you're looking for.
+                    </p>
+                </div>
             </div>
-        </div>
-    </>
-);
+        </>
+    );
+};
 
 export default ErrorPage;
