c9c62dc5178b9389119ca2c3b88a597e76b71755
Alan Treadway <alan.treadway@ag-grid.com>
2023-06-06 12:48:06 +0100

    AG-8476 - Fix handling of non-serialisable cache parameters.

--- charts-community-modules/ag-charts-community/src/util/callbackCache.ts
+++ charts-community-modules/ag-charts-community/src/util/callbackCache.ts
@@ -4,23 +4,38 @@ export class CallbackCache {
     private cache: Map<Function, Map<string, any>> = new Map();
 
     call<F extends (...args: any) => any>(f: F, ...params: Parameters<F>): ReturnType<F> | undefined {
-        const serialisedParams = JSON.stringify(params);
-
+        let serialisedParams: string;
         let paramCache = this.cache.get(f);
+
+        const invoke = () => {
+            try {
+                const result = f(...(params as any[]));
+                if (paramCache && serialisedParams != null) {
+                    paramCache.set(serialisedParams, result);
+                }
+                return result;
+            } catch (e) {
+                Logger.warnOnce(`User callback errored, ignoring`, e);
+                return undefined;
+            }
+        };
+
+        try {
+            serialisedParams = JSON.stringify(params);
+        } catch (e) {
+            // Unable to serialise params!
+            // No caching possible.
+
+            return invoke();
+        }
+
         if (paramCache == null) {
             paramCache = new Map();
             this.cache.set(f, paramCache);
         }
 
         if (!paramCache.has(serialisedParams)) {
-            try {
-                const result = f(...(params as any[]));
-                paramCache.set(serialisedParams, result);
-                return result;
-            } catch (e) {
-                Logger.warnOnce(`User callback errored, ignoring`, e);
-                return undefined;
-            }
+            return invoke();
         }
 
         return paramCache.get(serialisedParams);
