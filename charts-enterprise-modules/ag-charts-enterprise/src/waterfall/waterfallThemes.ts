8ff35a02ad9907dbd2c828df7de94d5d0e7a7de6
Mana <mana.jalali.15@ucl.ac.uk>
2023-06-06 16:46:40 +0100

    AG-8616 - Update waterfall theme

--- charts-enterprise-modules/ag-charts-enterprise/src/waterfall/waterfallThemes.ts
+++ charts-enterprise-modules/ag-charts-enterprise/src/waterfall/waterfallThemes.ts
@@ -4,9 +4,25 @@ export const WATERFALL_SERIES_THEME = {
     __extends__: _Theme.EXTENDS_SERIES_DEFAULTS,
     label: {
         enabled: false,
+        placement: 'end',
         fontSize: 12,
         fontFamily: _Theme.DEFAULT_FONT_FAMILY,
         color: 'rgb(70, 70, 70)',
         __overrides__: _Theme.OVERRIDE_SERIES_LABEL_DEFAULTS,
+    },
+    positiveItem: {
+        fill: '#233e6f',
+        stroke: '#233e6f',
+    },
+    negativeItem: {
+        fill: '#9FB7E1',
+        stroke: '#9FB7E1',
+    },
+    line: {
+        stroke: 'black',
+        strokeOpacity: 1,
+        lineDash: [0],
+        lineDashOffset: 0,
+        strokeWidth: 2,
     },
 };
