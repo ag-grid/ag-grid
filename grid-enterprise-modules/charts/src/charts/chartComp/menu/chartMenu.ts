76e4fd7ec551f01987198b5b9354e9dc4ea3778c
rmc-software <rmc.software.consultancy@gmail.com>
2023-06-06 15:03:26 +0100

    Revert "AG-7513 - refresh link / unlink chart toolbar icon on chart api updates"
    This reverts commit 033cb720a0161c75f61e5502bf2d3bf630131f00.

--- grid-enterprise-modules/charts/src/charts/chartComp/menu/chartMenu.ts
+++ grid-enterprise-modules/charts/src/charts/chartComp/menu/chartMenu.ts
@@ -87,16 +87,6 @@ export class ChartMenu extends Component {
             this.getGui().classList.add('ag-chart-tool-panel-button-enable');
             this.addManagedListener(this.eHideButton, 'click', this.toggleMenu.bind(this));
         }
-
-        this.addManagedListener(this.chartController, ChartController.EVENT_CHART_API_UPDATE, this.refreshMenu.bind(this));
-    }
-
-    public refreshMenu() {
-        // primarily used to refresh link / unlink chart toolbar icon on chart api updates
-        if (this.eMenu) {
-            this.destroyBean(this.eMenu);
-        }
-        this.createButtons();
     }
 
     public isVisible(): boolean {
