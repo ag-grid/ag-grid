d98384389efb4d85bc7e71cd9b8759481c485fe0
Peter Reynolds <peterjrreynolds@gmail.com>
2023-06-08 17:42:33 +0100

    RTI-1548 - updated date/number cell editor step docs

--- grid-community-modules/core/src/ts/rendering/cellEditors/dateStringCellEditor.ts
+++ grid-community-modules/core/src/ts/rendering/cellEditors/dateStringCellEditor.ts
@@ -13,6 +13,8 @@ export interface IDateStringCellEditorParams<TData = any, TContext = any> extend
     max?: string | Date;
     /**
      * Size of the value change when stepping up/down, starting from `min` or the initial value if provided.
+     * Step is also the difference between valid values.
+     * If the user-provided value isn't a multiple of the step value from the starting value, it will be considered invalid.
      * Defaults to any value allowed.
      */
     step?: number;
