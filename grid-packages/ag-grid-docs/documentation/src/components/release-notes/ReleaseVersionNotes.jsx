29d39b28e3ecfe10c106ae7ccdaabecd9a0e7d64
Mark Durrant <mark.j.durrant@gmail.com>
2023-06-08 15:12:38 +0100

    RTI-1448 - Format changelog page release notes

--- grid-packages/ag-grid-docs/documentation/src/components/release-notes/ReleaseVersionNotes.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/release-notes/ReleaseVersionNotes.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Collapsible from './Collapsible';
+import styles from './ReleaseVersionNotes.module.scss';
 
 const ReleaseVersionNotes = ({ title, releaseNotes, markdownContent }) => {
-
-    return <Collapsible title={'Release Notes ' + title}>
-        {!!markdownContent ? <ReactMarkdown>{markdownContent}</ReactMarkdown> : releaseNotes ? <div dangerouslySetInnerHTML={{ __html: releaseNotes }}></div> : null}
-    </Collapsible>
+    return (
+        <Collapsible title={'Release Notes ' + title}>
+            {!!markdownContent ? (
+                <div className={styles.markdown}>
+                    <ReactMarkdown>{markdownContent}</ReactMarkdown>
+                </div>
+            ) : releaseNotes ? (
+                <div dangerouslySetInnerHTML={{ __html: releaseNotes }}></div>
+            ) : null}
+        </Collapsible>
+    );
 };
 
 export default ReleaseVersionNotes;
