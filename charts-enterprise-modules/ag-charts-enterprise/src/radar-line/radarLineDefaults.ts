c2d8d0e151c74d4c7265f245ee7a0bdd6a9a493d
Alexander Shutau <shutovby@gmail.com>
2023-06-14 11:10:48 +0100

    AG-8531 AG-8532 Fix Radar without axes provided

--- charts-enterprise-modules/ag-charts-enterprise/src/radar-line/radarLineDefaults.ts
+++ charts-enterprise-modules/ag-charts-enterprise/src/radar-line/radarLineDefaults.ts
@@ -1,3 +1,14 @@
 import { AgPolarChartOptions } from 'ag-charts-community';
+import { AngleCategoryAxis } from './polar-axes/angle-category/angleCategoryAxis';
+import { RadiusNumberAxis } from './polar-axes/radius-number/radiusNumberAxis';
 
-export const RADAR_LINE_DEFAULTS: AgPolarChartOptions = {};
+export const RADAR_LINE_DEFAULTS: AgPolarChartOptions = {
+    axes: [
+        {
+            type: AngleCategoryAxis.type,
+        },
+        {
+            type: RadiusNumberAxis.type,
+        },
+    ],
+};
