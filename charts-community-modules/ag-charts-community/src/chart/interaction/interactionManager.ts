1d5f0274c59e12185af923d14299c02a5638d654
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-06-20 16:54:06 +0100

    Revert "Revert "Revert "Fix wheel event to trigger on element not window"""
    This reverts commit d50eb72c0ec3792c0104def00f01e3fc1bdb8b2a.

--- charts-community-modules/ag-charts-community/src/chart/interaction/interactionManager.ts
+++ charts-community-modules/ag-charts-community/src/chart/interaction/interactionManager.ts
@@ -29,7 +29,7 @@ type SUPPORTED_EVENTS =
     | 'touchcancel'
     | 'pagehide'
     | 'wheel';
-const WINDOW_EVENT_HANDLERS: SUPPORTED_EVENTS[] = ['pagehide', 'mousemove', 'mouseup'];
+const WINDOW_EVENT_HANDLERS: SUPPORTED_EVENTS[] = ['pagehide', 'mousemove', 'mouseup', 'wheel'];
 const EVENT_HANDLERS: SUPPORTED_EVENTS[] = [
     'click',
     'dblclick',
@@ -41,7 +41,6 @@ const EVENT_HANDLERS: SUPPORTED_EVENTS[] = [
     'touchmove',
     'touchend',
     'touchcancel',
-    'wheel',
 ];
 
 export type InteractionEvent<T extends InteractionTypes> = {
