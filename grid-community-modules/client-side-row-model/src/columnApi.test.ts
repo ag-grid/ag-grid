14060101c67f622af9c1b04a2fb7df2383b68fce
Stephen Cooper <scooperdev@gmail.com>
2023-06-22 15:54:55 +0100

    AG-8813 Add tests for columnApi getColumnDefs

---
+++ grid-community-modules/client-side-row-model/src/columnApi.test.ts
@@ -0,0 +1,73 @@
+import { describe, expect, it, xit, xtest, test, jest, beforeEach, afterEach } from '@jest/globals';
+import { ClientSideRowModelModule } from './clientSideRowModelModule';
+import { GridOptions, GridReadyEvent, Grid, ColDef, ColGroupDef } from '@ag-grid-community/core';
+
+const getColNames = (cols: any[] | undefined) => cols?.map(c => c.field ?? c.colId) ?? [];
+
+const onlyFields = [{ field: 'field' }, { field: 'field2' }];
+const withGroups: (ColDef | ColGroupDef)[] = [
+    { field: 'field' },
+    {
+        children: [
+            { field: 'child1' },
+            {
+                children: [
+                    { field: 'grandchild' }]
+            }]
+    },
+    { field: 'field2' }
+];
+
+describe('ColumnApi', () => {
+    test('simple columns', (done) => {
+
+        const options = {
+            columnDefs: onlyFields,
+            onGridReady: (params: GridReadyEvent) => {
+                const defs1 = params.api.getColumnDefs();
+                expect(getColNames(defs1)).toStrictEqual(getColNames(onlyFields));
+                done();
+            }
+        };
+        new Grid(document.createElement('div'), options, {
+            modules: [ClientSideRowModelModule]
+        });
+
+    });
+
+    test('with column groups', (done) => {
+
+        const options = {
+            columnDefs: withGroups,
+            onGridReady: (params: GridReadyEvent) => {
+                const defs1 = params.api.getColumnDefs();
+                expect(getColNames(defs1)).toStrictEqual(getColNames(withGroups));
+                done();
+            }
+        };
+        new Grid(document.createElement('div'), options, {
+            modules: [ClientSideRowModelModule]
+        });
+
+    });
+
+    test('crashes after destroy', (done) => {
+
+        const options = {
+            columnDefs: withGroups,
+            onGridReady: (params: GridReadyEvent) => {
+
+                params.api.destroy();
+
+                // If not broken this will just finish.
+                params.api.getColumnDefs();
+
+                done();
+            }
+        };
+        new Grid(document.createElement('div'), options, {
+            modules: [ClientSideRowModelModule]
+        });
+
+    });
+});
