5e786995325028c47e3f1d993602fc59384791da
Alexander Shutau <shutovby@gmail.com>
2023-07-04 10:35:18 +0100

    AG-8620 Refactor polar crosslines

--- charts-enterprise-modules/ag-charts-enterprise/src/polar-axes/angle-category/angleCrossLine.ts
+++ charts-enterprise-modules/ag-charts-enterprise/src/polar-axes/angle-category/angleCrossLine.ts
@@ -1,156 +1,21 @@
-import { _ModuleSupport, _Scale, _Scene, _Util, FontStyle, FontWeight } from 'ag-charts-community';
+import { _ModuleSupport, _Scale, _Scene, _Util } from 'ag-charts-community';
+import { PolarCrossLine } from '../polarCrossLine';
 
-const {
-    predicateWithMessage,
-    Validate,
-    NUMBER,
-    OPT_BOOLEAN,
-    OPT_STRING,
-    OPT_FONT_STYLE,
-    OPT_FONT_WEIGHT,
-    OPTIONAL,
-    STRING,
-    OPT_COLOR_STRING,
-    Layers,
-    OPT_ARRAY,
-    OPT_NUMBER,
-    OPT_LINE_DASH,
-    ChartAxisDirection,
-} = _ModuleSupport;
+const { ChartAxisDirection } = _ModuleSupport;
+const { Path, Sector } = _Scene;
 
-const { Group, Path, Sector } = _Scene;
-
-const { createId } = _Util;
-
-type CrossLineLabelPosition = string;
-
-const CROSSLINE_LABEL_POSITIONS = [
-    'top',
-    'left',
-    'right',
-    'bottom',
-    'topLeft',
-    'topRight',
-    'bottomLeft',
-    'bottomRight',
-    'inside',
-    'insideLeft',
-    'insideRight',
-    'insideTop',
-    'insideBottom',
-    'insideTopLeft',
-    'insideBottomLeft',
-    'insideTopRight',
-    'insideBottomRight',
-];
-
-const OPT_CROSSLINE_LABEL_POSITION = predicateWithMessage(
-    (v: any, ctx) => OPTIONAL(v, ctx, (v: any) => CROSSLINE_LABEL_POSITIONS.includes(v)),
-    `expecting an optional crossLine label position keyword such as 'topLeft', 'topRight' or 'inside'`
-);
-
-const OPT_CROSSLINE_TYPE = predicateWithMessage(
-    (v: any, ctx) => OPTIONAL(v, ctx, (v: any) => v === 'range' || v === 'line'),
-    `expecting a crossLine type keyword such as 'range' or 'line'`
-);
-
-class CrossLineLabel {
-    @Validate(OPT_BOOLEAN)
-    enabled?: boolean = undefined;
-
-    @Validate(OPT_STRING)
-    text?: string = undefined;
-
-    @Validate(OPT_FONT_STYLE)
-    fontStyle?: FontStyle = undefined;
-
-    @Validate(OPT_FONT_WEIGHT)
-    fontWeight?: FontWeight = undefined;
-
-    @Validate(NUMBER(0))
-    fontSize: number = 14;
-
-    @Validate(STRING)
-    fontFamily: string = 'Verdana, sans-serif';
-
-    /**
-     * The padding between the label and the line.
-     */
-    @Validate(NUMBER(0))
-    padding: number = 5;
-
-    /**
-     * The color of the labels.
-     */
-    @Validate(OPT_COLOR_STRING)
-    color?: string = 'rgba(87, 87, 87, 1)';
-
-    @Validate(OPT_CROSSLINE_LABEL_POSITION)
-    position?: CrossLineLabelPosition = undefined;
-
-    @Validate(OPT_NUMBER(-360, 360))
-    rotation?: number = undefined;
-
-    @Validate(OPT_BOOLEAN)
-    parallel?: boolean = undefined;
-}
-
-type CrossLineType = 'line' | 'range';
-
-export class AngleCrossLine implements _ModuleSupport.CrossLine {
-    protected static readonly LINE_LAYER_ZINDEX = Layers.SERIES_CROSSLINE_LINE_ZINDEX;
-    protected static readonly RANGE_LAYER_ZINDEX = Layers.SERIES_CROSSLINE_RANGE_ZINDEX;
-
+export class AngleCrossLine extends PolarCrossLine {
     static className = 'AngleCrossLine';
-    readonly id = createId(this);
 
-    @Validate(OPT_BOOLEAN)
-    enabled?: boolean = undefined;
-
-    @Validate(OPT_CROSSLINE_TYPE)
-    type?: CrossLineType = undefined;
-
-    @Validate(OPT_ARRAY(2))
-    range?: [any, any] = undefined;
-    value?: any = undefined;
-
-    @Validate(OPT_COLOR_STRING)
-    fill?: string = undefined;
-
-    @Validate(OPT_NUMBER(0, 1))
-    fillOpacity?: number = undefined;
-
-    @Validate(OPT_COLOR_STRING)
-    stroke?: string = undefined;
-
-    @Validate(OPT_NUMBER())
-    strokeWidth?: number = undefined;
-
-    @Validate(OPT_NUMBER(0, 1))
-    strokeOpacity?: number = undefined;
-
-    @Validate(OPT_LINE_DASH)
-    lineDash?: [] = undefined;
-
-    shape: 'polygon' | 'circle' = 'polygon';
-
-    label: any = new CrossLineLabel();
-
-    scale?: _Scale.Scale<any, number> = undefined;
-    clippedRange: [number, number] = [-Infinity, Infinity];
-    gridLength: number = 0;
-    sideFlag: 1 | -1 = -1;
-    parallelFlipRotation: number = 0;
-    regularFlipRotation: number = 0;
     direction: _ModuleSupport.ChartAxisDirection = ChartAxisDirection.X;
-
-    readonly group = new Group({ name: `${this.id}`, layer: true, zIndex: AngleCrossLine.LINE_LAYER_ZINDEX });
 
     private polygonNode = new Path();
     private sectorNode = new Sector();
     private lineNode = new Path();
 
     constructor() {
+        super();
+
         this.group.append(this.polygonNode);
         this.group.append(this.sectorNode);
         this.group.append(this.lineNode);
@@ -250,6 +115,4 @@ export class AngleCrossLine implements _ModuleSupport.CrossLine {
 
         this.group.zIndex = AngleCrossLine.RANGE_LAYER_ZINDEX;
     }
-
-    calculatePadding() {}
 }
--- charts-enterprise-modules/ag-charts-enterprise/src/polar-axes/radius-number/radiusCrossLine.ts
+++ charts-enterprise-modules/ag-charts-enterprise/src/polar-axes/radius-number/radiusCrossLine.ts
@@ -1,163 +1,21 @@
-import {
-    _ModuleSupport,
-    _Scale,
-    _Scene,
-    _Util,
-    AgCrossLineLabelPosition,
-    FontStyle,
-    FontWeight,
-} from 'ag-charts-community';
+import { _ModuleSupport, _Scale, _Scene, _Util } from 'ag-charts-community';
+import { PolarCrossLine } from '../polarCrossLine';
 
-const {
-    predicateWithMessage,
-    Validate,
-    NUMBER,
-    OPT_BOOLEAN,
-    OPT_STRING,
-    OPT_FONT_STYLE,
-    OPT_FONT_WEIGHT,
-    OPTIONAL,
-    STRING,
-    OPT_COLOR_STRING,
-    Layers,
-    OPT_ARRAY,
-    OPT_NUMBER,
-    OPT_LINE_DASH,
-    ChartAxisDirection,
-} = _ModuleSupport;
+const { ChartAxisDirection } = _ModuleSupport;
+const { Path, Sector } = _Scene;
 
-const { Group, Path, Sector } = _Scene;
+export class RadiusCrossLine extends PolarCrossLine {
+    static className = 'RadiusCrossLine';
 
-const { createId } = _Util;
-
-const CROSSLINE_LABEL_POSITIONS = [
-    'top',
-    'left',
-    'right',
-    'bottom',
-    'topLeft',
-    'topRight',
-    'bottomLeft',
-    'bottomRight',
-    'inside',
-    'insideLeft',
-    'insideRight',
-    'insideTop',
-    'insideBottom',
-    'insideTopLeft',
-    'insideBottomLeft',
-    'insideTopRight',
-    'insideBottomRight',
-];
-
-const OPT_CROSSLINE_LABEL_POSITION = predicateWithMessage(
-    (v: any, ctx) => OPTIONAL(v, ctx, (v: any) => CROSSLINE_LABEL_POSITIONS.includes(v)),
-    `expecting an optional crossLine label position keyword such as 'topLeft', 'topRight' or 'inside'`
-);
-
-const OPT_CROSSLINE_TYPE = predicateWithMessage(
-    (v: any, ctx) => OPTIONAL(v, ctx, (v: any) => v === 'range' || v === 'line'),
-    `expecting a crossLine type keyword such as 'range' or 'line'`
-);
-
-class CrossLineLabel {
-    @Validate(OPT_BOOLEAN)
-    enabled?: boolean = undefined;
-
-    @Validate(OPT_STRING)
-    text?: string = undefined;
-
-    @Validate(OPT_FONT_STYLE)
-    fontStyle?: FontStyle = undefined;
-
-    @Validate(OPT_FONT_WEIGHT)
-    fontWeight?: FontWeight = undefined;
-
-    @Validate(NUMBER(0))
-    fontSize: number = 14;
-
-    @Validate(STRING)
-    fontFamily: string = 'Verdana, sans-serif';
-
-    /**
-     * The padding between the label and the line.
-     */
-    @Validate(NUMBER(0))
-    padding: number = 5;
-
-    /**
-     * The color of the labels.
-     */
-    @Validate(OPT_COLOR_STRING)
-    color?: string = 'rgba(87, 87, 87, 1)';
-
-    @Validate(OPT_CROSSLINE_LABEL_POSITION)
-    position?: AgCrossLineLabelPosition = undefined;
-
-    @Validate(OPT_NUMBER(-360, 360))
-    rotation?: number = undefined;
-
-    @Validate(OPT_BOOLEAN)
-    parallel?: boolean = undefined;
-}
-
-type CrossLineType = 'line' | 'range';
-
-export class RadiusCrossLine implements _ModuleSupport.CrossLine {
-    protected static readonly LINE_LAYER_ZINDEX = Layers.SERIES_CROSSLINE_LINE_ZINDEX;
-    protected static readonly RANGE_LAYER_ZINDEX = Layers.SERIES_CROSSLINE_RANGE_ZINDEX;
-
-    static className = 'AngleCrossLine';
-    readonly id = createId(this);
-
-    @Validate(OPT_BOOLEAN)
-    enabled?: boolean = undefined;
-
-    @Validate(OPT_CROSSLINE_TYPE)
-    type?: CrossLineType = undefined;
-
-    @Validate(OPT_ARRAY(2))
-    range?: [any, any] = undefined;
-    value?: any = undefined;
-
-    @Validate(OPT_COLOR_STRING)
-    fill?: string = undefined;
-
-    @Validate(OPT_NUMBER(0, 1))
-    fillOpacity?: number = undefined;
-
-    @Validate(OPT_COLOR_STRING)
-    stroke?: string = undefined;
-
-    @Validate(OPT_NUMBER())
-    strokeWidth?: number = undefined;
-
-    @Validate(OPT_NUMBER(0, 1))
-    strokeOpacity?: number = undefined;
-
-    @Validate(OPT_LINE_DASH)
-    lineDash?: [] = undefined;
-
-    shape: 'polygon' | 'circle' = 'polygon';
-
-    gridAngles?: number[];
-
-    label: any = new CrossLineLabel();
-
-    scale?: _Scale.Scale<any, number> = undefined;
-    clippedRange: [number, number] = [-Infinity, Infinity];
-    gridLength: number = 0;
-    sideFlag: 1 | -1 = -1;
-    parallelFlipRotation: number = 0;
-    regularFlipRotation: number = 0;
     direction: _ModuleSupport.ChartAxisDirection = ChartAxisDirection.Y;
-
-    readonly group = new Group({ name: `${this.id}`, layer: true, zIndex: RadiusCrossLine.LINE_LAYER_ZINDEX });
+    gridAngles?: number[];
 
     private polygonNode = new Path();
     private sectorNode = new Sector();
 
     constructor() {
+        super();
+
         this.group.append(this.polygonNode);
         this.group.append(this.sectorNode);
     }
@@ -248,6 +106,4 @@ export class RadiusCrossLine implements _ModuleSupport.CrossLine {
 
         this.colorizeNode(sector);
     }
-
-    calculatePadding() {}
 }
