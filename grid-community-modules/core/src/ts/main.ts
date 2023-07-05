5ed909c2bed48a58136f7bf93ab63783f270b7da
Stephen Cooper <scooperdev@gmail.com>
2023-07-05 15:32:46 +0100

    AG-8023 Type location properties

--- grid-community-modules/core/src/ts/main.ts
+++ grid-community-modules/core/src/ts/main.ts
@@ -484,7 +484,7 @@ export { ColumnApi } from "./columns/columnApi";
 export { WrappableInterface, BaseComponentWrapper, FrameworkComponentWrapper } from "./components/framework/frameworkComponentWrapper";
 export { IFrameworkOverrides } from "./interfaces/iFrameworkOverrides";
 export { Environment } from "./environment";
-export { ITooltipComp, ITooltipParams } from "./rendering/tooltipComponent";
+export { ITooltipComp, ITooltipParams, TooltipLocation } from "./rendering/tooltipComponent";
 export { CustomTooltipFeature } from "./widgets/customTooltipFeature";
 export { IAggregationStage } from "./interfaces/iAggregationStage";
 
