41bf46ecd2d291f4dd135b264f9e73970e62dd29
Laurence Roberts <laurence.roberts@ag-grid.com>
2023-07-07 13:26:16 +0100

    AG-8561 Change fix animation to ignore invalid durations

--- charts-enterprise-modules/ag-charts-enterprise/src/animation/animation.ts
+++ charts-enterprise-modules/ag-charts-enterprise/src/animation/animation.ts
@@ -3,23 +3,23 @@ import { _ModuleSupport, _Scene } from 'ag-charts-community';
 const { BOOLEAN, NUMBER, ActionOnSet, Validate } = _ModuleSupport;
 
 export class Animation extends _ModuleSupport.BaseModuleInstance implements _ModuleSupport.ModuleInstance {
-    @Validate(BOOLEAN)
     @ActionOnSet<Animation>({
         newValue(value: boolean) {
             if (!this.animationManager) return;
             this.animationManager.skipAnimations = !value;
         },
     })
+    @Validate(BOOLEAN)
     public enabled = true;
 
-    @Validate(NUMBER(0))
     @ActionOnSet<Animation>({
         newValue(value: number | undefined) {
-            if (!this.animationManager || typeof value !== 'number' || isNaN(value) || value < 0) return;
+            if (!this.animationManager) return;
             this.animationManager.defaultOptions.duration = value;
             this.animationManager.skipAnimations = value === 0;
         },
     })
+    @Validate(NUMBER(0))
     public duration?: number;
 
     animationManager: _ModuleSupport.AnimationManager;
