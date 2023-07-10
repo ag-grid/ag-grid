268b184bf59b1931fbd014e3c76877ac88dd1a9b
Alan Treadway <alan.treadway@ag-grid.com>
2023-07-10 10:39:29 +0100

    AG-7933 - Fix SeriesLayerManagement handling of series updates.

--- charts-community-modules/ag-charts-community/src/chart/series/seriesLayerManager.ts
+++ charts-community-modules/ag-charts-community/src/chart/series/seriesLayerManager.ts
@@ -8,11 +8,15 @@ export type SeriesConfig = {
     seriesGrouping?: SeriesGrouping;
     rootGroup: Group;
     type: string;
-    _declarationOrder: number;
     getGroupZIndexSubOrder(
         type: 'data' | 'labels' | 'highlight' | 'path' | 'marker' | 'paths',
         subIndex?: number
     ): ZIndexSubOrder;
+};
+
+type LayerState = {
+    seriesIds: string[];
+    group: Group;
 };
 
 export class SeriesLayerManager {
@@ -20,20 +24,22 @@ export class SeriesLayerManager {
 
     private readonly groups: {
         [type: string]: {
-            [id: string]: {
-                seriesIds: string[];
-                group: Group;
-            };
+            [id: string]: LayerState;
         };
     } = {};
+    private readonly series: { [id: string]: LayerState } = {};
 
     constructor(rootGroup: Group) {
         this.rootGroup = rootGroup;
     }
 
-    public requestGroup(opts: SeriesConfig) {
-        const { id, seriesGrouping, type, rootGroup } = opts;
+    public requestGroup(opts: SeriesConfig, seriesGrouping?: SeriesGrouping) {
+        const { id, type, rootGroup: seriesRootGroup } = opts;
         const { groupIndex = id } = seriesGrouping ?? {};
+
+        if (this.series[id] != null) {
+            throw new Error(`AG Charts - series already has an allocated layer: ${this.series[id]}`);
+        }
 
         this.groups[type] ??= {};
         let groupInfo = this.groups[type][groupIndex];
@@ -51,20 +57,15 @@ export class SeriesLayerManager {
             };
         }
 
+        this.series[id] = groupInfo;
+
         groupInfo.seriesIds.push(id);
-        groupInfo.group.appendChild(rootGroup);
+        groupInfo.group.appendChild(seriesRootGroup);
         return groupInfo.group;
     }
 
-    public changeGroup({
-        id,
-        seriesGrouping,
-        type,
-        rootGroup,
-        oldGrouping,
-        _declarationOrder,
-        getGroupZIndexSubOrder,
-    }: SeriesConfig & { oldGrouping?: SeriesGrouping }) {
+    public changeGroup(opts: SeriesConfig & { oldGrouping?: SeriesGrouping }) {
+        const { id, seriesGrouping, type, rootGroup, oldGrouping } = opts;
         const { groupIndex = id } = seriesGrouping ?? {};
 
         if (this.groups[type]?.[groupIndex]?.seriesIds.includes(id)) {
@@ -72,24 +73,21 @@ export class SeriesLayerManager {
             return;
         }
 
-        this.releaseGroup({ id, seriesGrouping: oldGrouping, type, rootGroup });
-        this.requestGroup({ id, seriesGrouping, type, rootGroup, _declarationOrder, getGroupZIndexSubOrder });
+        if (this.series[id] != null) {
+            this.releaseGroup({ id, seriesGrouping: oldGrouping, type, rootGroup });
+        }
+        this.requestGroup(opts);
     }
 
-    public releaseGroup({
-        id,
-        seriesGrouping,
-        rootGroup,
-        type,
-    }: {
-        id: string;
-        seriesGrouping?: SeriesGrouping;
-        rootGroup: Group;
-        type: string;
-    }) {
+    public releaseGroup(opts: { id: string; seriesGrouping?: SeriesGrouping; rootGroup: Group; type: string }) {
+        const { id, seriesGrouping, rootGroup, type } = opts;
         const { groupIndex = id } = seriesGrouping ?? {};
 
-        const groupInfo = this.groups[type]?.[groupIndex];
+        if (this.series[id] == null) {
+            throw new Error(`AG Charts - series doesn't have an allocated layer: ${id}`);
+        }
+
+        const groupInfo = this.groups[type]?.[groupIndex] ?? this.series[id];
         if (groupInfo) {
             groupInfo.seriesIds = groupInfo.seriesIds.filter((v) => v !== id);
             groupInfo.group.removeChild(rootGroup);
@@ -97,7 +95,10 @@ export class SeriesLayerManager {
         if (groupInfo?.seriesIds.length === 0) {
             this.rootGroup.removeChild(groupInfo.group);
             delete this.groups[type][groupIndex];
+            delete this.groups[type][id];
         }
+
+        delete this.series[id];
     }
 
     public destroy() {
@@ -107,5 +108,6 @@ export class SeriesLayerManager {
             }
         }
         (this as any).groups = {};
+        (this as any).series = {};
     }
 }
