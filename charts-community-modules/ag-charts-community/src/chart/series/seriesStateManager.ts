97a0455898d35d92f3161ce49d4acbdff6a44aea
Alan Treadway <alan.treadway@ag-grid.com>
2023-07-17 11:40:48 +0100

    Fix Charts Sonarlint issues.

--- charts-community-modules/ag-charts-community/src/chart/series/seriesStateManager.ts
+++ charts-community-modules/ag-charts-community/src/chart/series/seriesStateManager.ts
@@ -59,7 +59,7 @@ export class SeriesStateManager {
                 .values(),
         ];
 
-        visibleGroups.sort();
+        visibleGroups.sort((a, b) => a - b);
 
         return { visibleGroupCount: visibleGroups.length, index: visibleGroups.indexOf(seriesGrouping.groupIndex) };
     }
