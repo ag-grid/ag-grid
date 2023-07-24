ae31f6b47a0b16e99d4c62bd3e5f613ae0b558e0
Stephen Cooper <scooperdev@gmail.com>
2023-07-24 15:01:33 +0100

    update demos

--- grid-community-modules/react/src/indexApiTest.tsx
+++ grid-community-modules/react/src/indexApiTest.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
 import { AgGridReact } from './agGridReact';
 import useGridApis from "./useGridApi";
@@ -39,7 +40,5 @@ const App = () => {
     );
 };
 
-ReactDOM.render(
-    <App/>,
-    document.getElementById('root')
-);
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
