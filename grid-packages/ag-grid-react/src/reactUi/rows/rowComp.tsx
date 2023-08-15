630d95ca7fce6289333be70272e0724aaff90922
Stephen Cooper <scooperdev@gmail.com>
2023-08-15 17:44:59 +0100

    AG-9112 Resolve filter flicker of rows

--- grid-packages/ag-grid-react/src/reactUi/rows/rowComp.tsx
+++ grid-packages/ag-grid-react/src/reactUi/rows/rowComp.tsx
@@ -14,18 +14,14 @@ const RowComp = (params: { rowCtrl: RowCtrl, containerType: RowContainerType }) 
     const domOrderRef = useRef<boolean>(rowCtrl.getDomOrder());
     const isFullWidth = rowCtrl.isFullWidth();
 
-    const [rowIndex, setRowIndex] = useState<string>(() => rowCtrl.getRowIndex());
-    const [rowId, setRowId] = useState<string | null>(() => rowCtrl.getRowId());
-    const [rowBusinessKey, setRowBusinessKey] = useState<string | null>(() => rowCtrl.getBusinessKey());
-
     const [userStyles, setUserStyles] = useState<RowStyle | undefined>(() => rowCtrl.getRowStyles());
     const [cellCtrls, setCellCtrls] = useState<CellCtrl[] | null>(() => isFullWidth ? null : rowCtrl.getCellCtrlsForContainer(containerType)); //rowCtrl.getCellCtrlsForContainer(containerType)
     const [fullWidthCompDetails, setFullWidthCompDetails] = useState<UserCompDetails>();
 
     // these styles have initial values, so element is placed into the DOM with them,
     // rather than an transition getting applied.
-    const [top, setTop] = useState<string | undefined>(() => rowCtrl.getInitialRowTop(containerType));
-    const [transform, setTransform] = useState<string | undefined>(() => rowCtrl.getInitialTransform(containerType));
+    const topRef = useRef<string | undefined>(rowCtrl.getInitialRowTop(containerType));
+    const transformRef = useRef<string | undefined>(rowCtrl.getInitialTransform(containerType));
 
     const eGui = useRef<HTMLDivElement | null>(null);
     const fullWidthCompRef = useRef<ICellRenderer>();
@@ -74,17 +70,23 @@ const RowComp = (params: { rowCtrl: RowCtrl, containerType: RowContainerType }) 
         const compProxy: IRowComp = {
             // the rowTop is managed by state, instead of direct style manipulation by rowCtrl (like all the other styles)
             // as we need to have an initial value when it's placed into he DOM for the first time, for animation to work.
-            setTop: value => setTop(value),
-            setTransform: value => setTransform(value),
+            setTop: top => {
+                topRef.current = top;
+                eGui.current!.style.top = top;
+            },
+            setTransform: transform => {
+                eGui.current!.style.transform = transform;
+                transformRef.current = transform;
+            },
 
             // i found using React for managing classes at the row level was to slow, as modifying classes caused a lot of
             // React code to execute, so avoiding React for managing CSS Classes made the grid go much faster.
             addOrRemoveCssClass: (name, on) => cssClassManager.current!.addOrRemoveCssClass(name, on),
 
             setDomOrder: domOrder => domOrderRef.current = domOrder,
-            setRowIndex: value => setRowIndex(value),
-            setRowId: value => setRowId(value),
-            setRowBusinessKey: value => setRowBusinessKey(value),
+            setRowIndex: rowIndex => eGui.current!.setAttribute('row-index', rowIndex),
+            setRowId: rowId => eGui.current!.setAttribute('row-id', rowId),
+            setRowBusinessKey: businessKey => eGui.current!.setAttribute('row-business-key', businessKey),
             setUserStyles: (styles: RowStyle | undefined) => setUserStyles(styles),
             // if we don't maintain the order, then cols will be ripped out and into the dom
             // when cols reordered, which would stop the CSS transitions from working
@@ -101,11 +103,13 @@ const RowComp = (params: { rowCtrl: RowCtrl, containerType: RowContainerType }) 
     useLayoutEffect(() => showJsComp(fullWidthCompDetails, context, eGui.current!, fullWidthCompRef), [fullWidthCompDetails]);
 
     const rowStyles = useMemo(() => {
-        const res = { top, transform };
+        // Will use the current values of top / transform so that these are not overridden when userStyles change
+        // We don't need this to update when top / transform are updated as these are directly updated on the DOM
+        const res = { top: topRef.current, transform: transformRef.current };
 
         Object.assign(res, userStyles);
         return res;
-    }, [top, transform, userStyles]);
+    }, [userStyles]);
 
     const showFullWidthFramework = isFullWidth && fullWidthCompDetails && fullWidthCompDetails.componentFromFramework;
     const showCells = !isFullWidth && cellCtrls != null;
@@ -145,9 +149,6 @@ const RowComp = (params: { rowCtrl: RowCtrl, containerType: RowContainerType }) 
             ref={setRef}
             role={'row'}
             style={rowStyles}
-            row-index={rowIndex}
-            row-id={rowId}
-            row-business-key={rowBusinessKey}
             tabIndex={tabIndex}
         >
             {showCells && showCellsJsx()}
