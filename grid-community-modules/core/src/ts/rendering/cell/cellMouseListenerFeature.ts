d7e7d2e852e914cef21b92d9d4ac967bf799c7c3
Peter Reynolds <peterjrreynolds@gmail.com>
2023-09-14 16:31:16 +0100

    AG-9309 - tidy up range selection focus fixes

--- grid-community-modules/core/src/ts/rendering/cell/cellMouseListenerFeature.ts
+++ grid-community-modules/core/src/ts/rendering/cell/cellMouseListenerFeature.ts
@@ -116,7 +116,8 @@ export class CellMouseListenerFeature extends Beans {
     private onMouseDown(mouseEvent: MouseEvent): void {
         const { ctrlKey, metaKey, shiftKey } = mouseEvent;
         const target = mouseEvent.target as HTMLElement;
-        const { eventService, rangeService } = this.beans;
+        const { cellCtrl, beans } = this;
+        const { eventService, rangeService, focusService } = beans;
 
         // do not change the range for right-clicks inside an existing range
         if (this.isRightClickInExistingRange(mouseEvent)) {
@@ -129,38 +130,36 @@ export class CellMouseListenerFeature extends Beans {
             // We only need to pass true to focusCell when the browser is Safari and we are trying
             // to focus the cell itself. This should never be true if the mousedown was triggered
             // due to a click on a cell editor for example.
-            const forceBrowserFocus = (isBrowserSafari()) && !this.cellCtrl.isEditing() && !isFocusableFormField(target);
+            const forceBrowserFocus = (isBrowserSafari()) && !cellCtrl.isEditing() && !isFocusableFormField(target);
 
-            this.cellCtrl.focusCell(forceBrowserFocus);
+            cellCtrl.focusCell(forceBrowserFocus);
         }
 
         // if shift clicking, and a range exists, we keep the focus on the cell that started the
         // range as the user then changes the range selection.
-        if (shiftKey && ranges) {
-            if (!this.beans.focusService.isCellFocused(this.cellCtrl.getCellPosition())) {
-                // this stops the cell from getting focused
-                mouseEvent.preventDefault();
+        if (shiftKey && ranges && !focusService.isCellFocused(cellCtrl.getCellPosition())) {
+            // this stops the cell from getting focused
+            mouseEvent.preventDefault();
 
-                const focusedCellPosition = this.beans.focusService.getFocusedCell();
-                if (focusedCellPosition) {
-                    const { column, rowIndex, rowPinned } = focusedCellPosition;
-                    const rowCtrl = this.beans.rowRenderer.getRowByPosition({ rowIndex, rowPinned });
-                    const cellCtrl = rowCtrl?.getCellCtrl(column);
+            const focusedCellPosition = focusService.getFocusedCell();
+            if (focusedCellPosition) {
+                const { column, rowIndex, rowPinned } = focusedCellPosition;
+                const focusedRowCtrl = beans.rowRenderer.getRowByPosition({ rowIndex, rowPinned });
+                const focusedCellCtrl = focusedRowCtrl?.getCellCtrl(column);
 
-                    // if the focused cell is editing, need to stop editing first
-                    if (cellCtrl?.isEditing()) {
-                        cellCtrl.stopEditing();
-                    }
-
-                    // focus could have been lost, so restore it to the starting cell in the range if needed
-                    this.beans.focusService.setFocusedCell({
-                        column,
-                        rowIndex,
-                        rowPinned,
-                        forceBrowserFocus: true,
-                        preventScrollOnBrowserFocus: true,
-                    });
+                // if the focused cell is editing, need to stop editing first
+                if (focusedCellCtrl?.isEditing()) {
+                    focusedCellCtrl.stopEditing();
                 }
+
+                // focus could have been lost, so restore it to the starting cell in the range if needed
+                focusService.setFocusedCell({
+                    column,
+                    rowIndex,
+                    rowPinned,
+                    forceBrowserFocus: true,
+                    preventScrollOnBrowserFocus: true,
+                });
             }
         }
 
