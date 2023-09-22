14a1e20db594267c990245359482ddf818741dec
Salmen Hichri <salmen.hichri@gmail.com>
2023-09-22 16:23:08 +0100

    AG-9313 Added grid-lifecycle pre-destroyed doc example for vue/vue3

--- grid-packages/ag-grid-docs/documentation/doc-pages/grid-lifecycle/examples/first-data-rendered/provided/modules/vue3/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/grid-lifecycle/examples/first-data-rendered/provided/modules/vue3/main.js
@@ -106,26 +106,4 @@ const VueExample = {
     }
 }
 
-window._optionalChain = function _optionalChain(ops) {
-    let lastAccessLHS = undefined;
-    let value = ops[0];
-    let i = 1;
-    while (i < ops.length) {
-        const op = ops[i];
-        const fn = ops[i + 1];
-        i += 2;
-        if ((op === 'optionalAccess' || op === 'optionalCall') && value == null) {
-            return undefined;
-        }
-        if (op === 'access' || op === 'optionalAccess') {
-            lastAccessLHS = value;
-            value = fn(value);
-        } else if (op === 'call' || op === 'optionalCall') {
-            value = fn((...args) => value.call(lastAccessLHS, ...args));
-            lastAccessLHS = undefined;
-        }
-    }
-    return value;
-}
-
 createApp(VueExample).mount("#app")
