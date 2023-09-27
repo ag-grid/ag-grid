fa15eb3d25a3e2bd41851d76cf8e4880a7eab4bc
rmc-software <rmc.software.consultancy@gmail.com>
2023-09-27 15:49:40 +0100

    AG-3390 - partially removed standalone charts docs

--- grid-packages/ag-grid-docs/documentation/doc-pages/charts-radar-series/examples/polar-axes/data.ts
+++
@@ -1,9 +0,0 @@
-export function getData() {
-    return [
-        { subject: 'Maths', mike: 7, tony: 4 },
-        { subject: 'Physics', mike: 4, tony: 9 },
-        { subject: 'Biology', mike: 3, tony: 4 },
-        { subject: 'History', mike: 7, tony: 5 },
-        { subject: 'P.E.', mike: 9, tony: 6 },
-    ];
-}
