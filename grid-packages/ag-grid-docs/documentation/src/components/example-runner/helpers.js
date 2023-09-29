7b0f6c48f044d0c1618be0a754f6d888354254bf
seanlandsman <sean@thelandsmans.com>
2023-09-29 13:30:18 +0100

    AG-9444 When opening IRM, SSRM and other docs examples in CodeSandbox, there's an exception

--- grid-packages/ag-grid-docs/documentation/src/components/example-runner/helpers.js
+++ grid-packages/ag-grid-docs/documentation/src/components/example-runner/helpers.js
@@ -313,7 +313,10 @@ export const openCodeSandbox = (exampleInfo) => {
                 if(file === 'index.jsx') {
                     return `src/index.js`
                 }
-                return `src/${file}`;
+                if(file === 'styles.css') {
+                    return `src/styles.css`
+                }
+                return `public/${file}`;
             }
 
             return file;
