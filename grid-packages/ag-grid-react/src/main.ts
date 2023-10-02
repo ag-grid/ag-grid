76acf4dcdde9449667174de7c7c4b4c62b34a8ed
Guilherme Lopes <guilherme@gwm-solutions.com>
2023-10-02 17:26:15 -0300

    AG-9415 React useApi hooks

--- grid-packages/ag-grid-react/src/main.ts
+++ grid-packages/ag-grid-react/src/main.ts
@@ -1,4 +1,5 @@
 export { AgGridReact } from './agGridReact';
+export { useGridApiRefs, useGridApis } from './useGridApi';
 export { AgGridReactLegacy } from './legacy/agGridReactLegacy'
 export { AgGridReactUi as AgGridReactFire } from './reactUi/agGridReactUi'
 export * from './shared/interfaces';
