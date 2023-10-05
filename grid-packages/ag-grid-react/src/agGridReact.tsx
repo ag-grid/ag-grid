847abef20bda47ee6e481baf77ba0285895d31ed
Stephen Cooper <scooperdev@gmail.com>
2023-10-05 14:48:21 +0100

    Start moving columnApi code

--- grid-packages/ag-grid-react/src/agGridReact.tsx
+++ grid-packages/ag-grid-react/src/agGridReact.tsx
@@ -5,7 +5,12 @@ import { ColumnApi, GridApi } from 'ag-grid-community';
 
 
 export class AgGridReact<TData = any> extends Component<AgGridReactProps<TData> | AgReactUiProps<TData>, {}> {
+    /** Grid Api available after onGridReady event has fired. */
     public api!: GridApi<TData>;
+    /**
+     * @deprecated v31 - The `columnApi` has been deprecated and all the methods are now present of the `api`.
+     * Please use the `api` instead.
+     */
     public columnApi!: ColumnApi;
     private apiListeners: Array<(params: any) => void> = [];
 
