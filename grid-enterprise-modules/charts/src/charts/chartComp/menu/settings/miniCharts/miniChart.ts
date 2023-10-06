24e6786ff1bd69b81d40073dcf65ca17cf0a2406
rmc-software <rmc.software.consultancy@gmail.com>
2023-10-06 17:28:39 +0100

    AG-9505 - fixed integrated tool panel issue

--- grid-enterprise-modules/charts/src/charts/chartComp/menu/settings/miniCharts/miniChart.ts
+++ grid-enterprise-modules/charts/src/charts/chartComp/menu/settings/miniCharts/miniChart.ts
@@ -15,7 +15,7 @@ export abstract class MiniChart extends Component {
     constructor(container: HTMLElement, tooltipName: string) {
         super();
 
-        const scene = new _Scene.Scene({ document: window.document, width: this.size, height: this.size } as any);
+        const scene = new _Scene.Scene({ document: window.document, window: window, width: this.size, height: this.size});
         scene.canvas.element.classList.add('ag-chart-mini-thumbnail-canvas');
         scene.root = this.root;
         scene.container = container;
