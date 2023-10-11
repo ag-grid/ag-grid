6cbf5bd7cf1a3223dbc2c383ab24bbbe20c6045b
Stephen Cooper <scooperdev@gmail.com>
2023-10-11 12:29:59 +0100

    revert

--- grid-packages/ag-grid-docs/src/example-generation/grid-vanilla-src-parser.ts
+++ grid-packages/ag-grid-docs/src/example-generation/grid-vanilla-src-parser.ts
@@ -264,7 +264,7 @@ function internalParser(examplePath, {
         matches: tsNodeIsHttpOpen,
         apply: (bindings, node) => {
             const url = node.expression.arguments[1].raw;
-            const callback = '{ params.api.setRowData(data); }';
+            const callback = '{ gridApi.setRowData(data); }';
 
             bindings.data = {url, callback};
         }
