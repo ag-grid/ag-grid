9b614c7b47d50f78c7a2fd12972e8e4484bf511e
Stephen Cooper <scooperdev@gmail.com>
2023-10-13 09:47:46 +0100

    todo

--- grid-community-modules/core/src/ts/interfaces/iAlignedGrid.ts
+++ grid-community-modules/core/src/ts/interfaces/iAlignedGrid.ts
@@ -4,4 +4,6 @@ import { GridApi } from "../gridApi";
 /**
  * Alias for the grid API or an object containing the grid API for linking Aligned Grids.
  */
-export type AlignedGrid = { api?: GridApi | null, columnApi?: ColumnApi | null } | GridApi;
+export type AlignedGrid = { api?: GridApi | null, columnApi?: ColumnApi | null } | GridApi | null;
+
+//TODO Support React ref out the box???
