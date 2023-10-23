f867e0220e383db2dc13d8645923f52fd06565b3
Kyler Phillips <kylerphillips@hotmail.co.uk>
2023-10-23 16:48:09 +0100

    AG-9397 - add border back to tables in dark mode

--- grid-packages/ag-grid-docs/documentation/src/components/example-runner/ExampleStyle.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/example-runner/ExampleStyle.jsx
@@ -14,11 +14,13 @@ const ExampleStyle = ({ rootId }) => <style media="only screen">
             }
 
             #myGrid.ag-theme-alpine-dark {
-                --ag-background-color: var(--background-200);
+                --ag-background-color: #0A161F;
                 --ag-odd-row-background-color: #151F28
                 --ag-foreground-color: #e6f1fc; 
                 --ag-header-background-color: #1B2938;
-                --ag-header-foreground-color: #fff
+                --ag-header-foreground-color: #fff;
+                --ag-border-color: color-mix(in srgb, var(--ag-background-color), var(--ag-foreground-color) 15%);
+                --ag-header-column-resize-handle-color: color-mix(in srgb, var(--ag-background-color), var(--ag-foreground-color) 15%);
             }
 
             html {
@@ -30,11 +32,8 @@ const ExampleStyle = ({ rootId }) => <style media="only screen">
             }
 
             body {
-                padding: 1rem;
                 overflow: auto;
-                
             }
-
         `}
 </style>;
 
