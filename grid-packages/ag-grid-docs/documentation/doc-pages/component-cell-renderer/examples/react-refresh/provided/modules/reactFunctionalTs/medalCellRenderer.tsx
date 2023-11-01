edd4de754c6d1517b0cb89a38d39530efeaa28e7
Peter Reynolds <peterjrreynolds@gmail.com>
2023-11-01 14:53:30 +0000

    AG-9697 - add react cell renderer refresh example

---
+++ grid-packages/ag-grid-docs/documentation/doc-pages/component-cell-renderer/examples/react-refresh/provided/modules/reactFunctionalTs/medalCellRenderer.tsx
@@ -0,0 +1,22 @@
+import React, { forwardRef, useEffect, useImperativeHandle, useState } from 'react';
+import { ICellRendererParams } from '@ag-grid-community/core';
+
+export default forwardRef((props: ICellRendererParams<IOlympicData, number>, ref) => {
+    const [value, setValue] = useState(props.value);
+
+    useEffect(() => {
+        console.log('renderer created');
+    }, []);
+
+    useImperativeHandle(ref, () => {
+        return {
+            refresh(params: ICellRendererParams<IOlympicData, number>) {
+                console.log('renderer refreshed');
+                setValue(params.value);
+                return true;
+            }
+        };
+    });
+
+    return <span>{new Array(value).fill('#').join('')}</span>;
+});
