d03e7e6ced518281086b6d274da8cb68a7497284
seanlandsman <sean@thelandsmans.com>
2023-11-08 08:10:23 +0000

    Merge branch 'latest' into charts-integration

--- scripts/messageSlackNewChartDep.sh
+++ scripts/messageSlackNewChartDep.sh
@@ -3,4 +3,4 @@
 WEBHOOK_URL=$1
 LATEST_CHART_DEP=`node ./scripts/getLatestChartDep.js`
 
-./scripts/messageSlack.sh "New Charts Version Available ($LATEST_CHART_DEP). Please run bootstrap." "$WEBHOOK_URL"
+./scripts/messageSlack.sh "New Charts Version Available ($LATEST_CHART_DEP). You'll need to run bootstrap after you update." "$WEBHOOK_URL"
