0949860443cbf5ee500e67419e6efb4f1f3f20a3
seanlandsman <sean@thelandsmans.com>
2023-11-21 11:53:33 +0000

    AG-9829 Add pricing page for charts

--- grid-packages/ag-grid-docs/documentation/src/pages/license-pricing-bare.tsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/license-pricing-bare.tsx
@@ -1,4 +1,20 @@
 import React from 'react';
 import LicensePricing from './license-pricing';
 
-export default LicensePricing;
+const LicensePricingBare = ({location}) => {
+    const params = new URLSearchParams(location.search);
+    const darkMode = params.get("darkMode") === "true";
+
+    const htmlEl = document.querySelector('html') as any;
+    // Using .no-transitions class so that there are no animations between light/dark modes
+    htmlEl.classList.add('no-transitions');
+    htmlEl.dataset.darkMode = darkMode ? 'true' : 'false';
+    htmlEl.offsetHeight; // Trigger a reflow, flushing the CSS changes
+    htmlEl.classList.remove('no-transitions');
+
+    return (
+            <LicensePricing></LicensePricing>
+    )
+}
+
+export default LicensePricingBare;
