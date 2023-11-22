8989cf51816125c5827a6cca1611976611425444
Guilherme Lopes <guilherme@gwm-solutions.com>
2023-11-22 00:15:40 -0600

    AG-9383 - provided editors examples

--- grid-packages/ag-grid-docs/documentation/doc-pages/provided-cell-editors-select/examples/select-editor/main.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/provided-cell-editors-select/examples/select-editor/main.ts
@@ -3,58 +3,20 @@ import {
   createGrid,
   ColDef,
   GridOptions,
-  ITextCellEditorParams,
-  ILargeTextEditorParams,
   ISelectCellEditorParams,
-  IRichCellEditorParams,
 } from '@ag-grid-community/core';
 import { ColourCellRenderer } from './colourCellRenderer_typescript'
 import { colors } from './colors';
 
 const columnDefs: ColDef[] = [
   { 
-    headerName: 'Text Editor', 
-    field: 'color1', 
-    cellRenderer: ColourCellRenderer,
-    cellEditor: 'agTextCellEditor',
-    cellEditorParams: {
-      maxLength: 20
-    } as ITextCellEditorParams
-  },
-  { 
     headerName: 'Select Editor', 
-    field: 'color2', 
+    field: 'color', 
     cellRenderer: ColourCellRenderer,
     cellEditor: 'agSelectCellEditor',
     cellEditorParams: {
       values: colors
     } as ISelectCellEditorParams
-  },
-  { 
-    headerName: 'Rich Select Editor', 
-    field: 'color3', 
-    cellRenderer: ColourCellRenderer,
-    cellEditor: 'agRichSelectCellEditor',
-    cellEditorParams: {
-      values: colors, 
-      cellRenderer: ColourCellRenderer,
-      filterList: true,
-      searchType: 'match',
-      allowTyping: true,
-      valueListMaxHeight: 220
-    } as IRichCellEditorParams
-  },
-  { 
-    headerName: 'Large Text Editor', 
-    field: 'description', 
-    cellEditorPopup: true,
-    cellEditor: 'agLargeTextCellEditor', 
-    cellEditorParams: {
-      maxLength: 250,
-      rows: 10,
-      cols: 50
-    } as ILargeTextEditorParams,
-    flex: 2 
   }
 ];
 
@@ -64,12 +26,7 @@ function getRandomNumber(min: number, max: number) { // min and max included
 
 const data = Array.from(Array(20).keys()).map(() => {
   const color = colors[getRandomNumber(0, colors.length - 1)];
-  return ({
-    color1: color,
-    color2: color,
-    color3: color,
-    description:  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-  });
+  return ({ color });
 });
 
 let gridApi: GridApi;
