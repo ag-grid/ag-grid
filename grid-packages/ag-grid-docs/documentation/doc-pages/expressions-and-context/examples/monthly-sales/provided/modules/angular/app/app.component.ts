0c911d4162f213c0a6d17dd5569e1dfe61b90433
Stephen Cooper <scooperdev@gmail.com>
2023-11-22 09:39:27 +0000

    AG-9598 Fix types in an example

--- grid-packages/ag-grid-docs/documentation/doc-pages/expressions-and-context/examples/monthly-sales/provided/modules/angular/app/app.component.ts
+++ grid-packages/ag-grid-docs/documentation/doc-pages/expressions-and-context/examples/monthly-sales/provided/modules/angular/app/app.component.ts
@@ -1,14 +1,7 @@
 import { HttpClient } from "@angular/common/http"
 import { Component } from "@angular/core"
 // NOTE: Angular CLI does not support component CSS imports: angular-cli/issues/23273
-import {
-  ColDef,
-  ColGroupDef,
-  GridApi,
-  GridReadyEvent,
-  ICellRendererParams,
-  IGroupCellRendererParams,
-} from "@ag-grid-community/core"
+import { ColDef, ColGroupDef, GridApi, GridReadyEvent, ICellRendererParams, IGroupCellRendererParams } from "@ag-grid-community/core"
 import "@ag-grid-community/styles/ag-grid.css"
 import "@ag-grid-community/styles/ag-theme-quartz.css"
 import "../styles.css"
@@ -152,6 +145,8 @@ export class AppComponent {
   public defaultColDef: ColDef = {
     flex: 1,
     minWidth: 120,
+    sortable: true,
+    resizable: true,
   }
   public autoGroupColumnDef: ColDef = {
     headerName: "Location",
