5590ca50a4094ec9eb4aecbd55980a835774a622
Kyler Phillips <kylerphillips@hotmail.co.uk>
2023-11-23 16:17:28 +0000

    AG-3390 Add logic for close storage

--- grid-packages/ag-grid-docs/documentation/src/components/promo-banner/PromoBanner.jsx
+++ grid-packages/ag-grid-docs/documentation/src/components/promo-banner/PromoBanner.jsx
@@ -1,30 +1,45 @@
-// PromoBanner.js
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './PromoBanner.module.scss';
 import LogoMarkTransparent from '../LogoMarkTransparent';
 import CloseIcon from '../../images/inline-svgs/cross-banner.svg';
 
-
 const PromoBanner = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    // Check local storage to see if the banner should be hidden
+    const isBannerHidden = localStorage.getItem('isPromoBannerHidden');
+    if (isBannerHidden) {
+      setIsVisible(false);
+    }
+  }, []);
+
+  const handleCloseClick = () => {
+    // Hide the banner and set the flag in local storage
+    setIsVisible(false);
+    localStorage.setItem('isPromoBannerHidden', 'true');
+  };
+
+  if (!isVisible) {
+    return null; // Don't render the banner if it's hidden
+  }
+
   return (
     <div className={styles.promoBanner}>
-
-<LogoMarkTransparent />
-<div className={styles.promoText}>
-      Introducing our new AG Charts library 
+      <LogoMarkTransparent />
+      <div className={styles.promoText}>
+        Introducing our new AG Charts library
+      </div>
+      <a href="https://ag-grid.com/ag-charts" className={styles.visitButton}>
+        <div>
+          Visit →
+        </div>
+      </a>
+      <CloseIcon
+        className={styles.closeIcon}
+        onClick={handleCloseClick}
+      />
     </div>
-
-    <a href="https://ag-grid.com/ag-charts" className={styles.visitButton}>
-  <div>
-    Visit →
-  </div>
-</a>
-<CloseIcon className={styles.closeIcon} />
-
-    </div>
-
-
   );
 };
 
