b28f1f2f2d5b4b222868fc4f52e4c4b561e6c890
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2023-11-24 12:06:37 +0000

    AG-9489: Update dataset

--- grid-packages/ag-grid-docs/documentation/doc-pages/getting-started/examples/quick-start-example/provided/modules/vue/main.js
+++ grid-packages/ag-grid-docs/documentation/doc-pages/getting-started/examples/quick-start-example/provided/modules/vue/main.js
@@ -21,18 +21,19 @@ const App = {
     return {
       // Row Data: The data to be displayed.
       rowData: [        
-        {company: "CASC", country: "China", date: "2022-07-24", mission: "Wentian", price: 2150000, successful: true},
-        {company: "SpaceX", country: "USA", date: "2022-07-24", mission: "Starlink Group 4-25", price: 3230000, successful: true},
-        {company: "SpaceX", country: "USA", date: "2022-07-22", mission: "Starlink Group 3-2", price: 8060000, successful: true}
+        {mission: "CRS SpX-25", company: "SpaceX", location: "LC-39A, Kennedy Space Center, Florida, USA", date: "2022-07-15", time: "0:44:00", rocket: "Falcon 9 Block 5", price: 12480000, successful: true},
+        {mission: "LARES 2 & Cubesats", company: "ESA", location: "ELV-1, Guiana Space Centre, French Guiana, France", date: "2022-07-13", time: "13:13:00", rocket: "Vega C", price: 4470000, successful: true},
+        {mission: "Wise One Looks Ahead (NROL-162)", company: "Rocket Lab", location: "Rocket Lab LC-1A, Māhia Peninsula, New Zealand", date: "2022-07-13", time: "6:30:00", rocket: "Electron/Curie", price: 9750000, successful: true}    
       ],
       // Column Definitions: Defines & controls grid columns.
       columnDefs: [
-          { field: "mission" },
-          { field: "country" },
-          { field: "successful" },
-          { field: "date" },
-          { field: "price" },
-          { field: "company" }
+        { field: "mission" },
+        { field: "company" },
+        { field: "location" },
+        { field: "date" },
+        { field: "price" },
+        { field: "successful" },
+        { field: "rocket" }
       ],
       themeClass: /** DARK MODE START **/document.documentElement.dataset.defaultTheme || 'ag-theme-quartz'/** DARK MODE END **/,
     };
