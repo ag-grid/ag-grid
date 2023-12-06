fb3d8d7213b2d06834dea9f4ae720e4df08ebfef
Pete Reynolds <40919976+peterjrreynolds@users.noreply.github.com>
2023-12-06 09:34:21 +0000

    AG-10054 - fix setting of pivot mode via api and ui together (#7322)
    * AG-10054 - fix setting of pivot mode via api and ui together
    
    * AG-10054 - pass source when updating pivot mode

--- grid-enterprise-modules/column-tool-panel/src/columnToolPanel/pivotModePanel.ts
+++ grid-enterprise-modules/column-tool-panel/src/columnToolPanel/pivotModePanel.ts
@@ -38,7 +38,7 @@ export class PivotModePanel extends Component {
     private onBtPivotMode(): void {
         const newValue = !!this.cbPivotMode.getValue();
         if (newValue !== this.columnModel.isPivotMode()) {
-            this.columnModel.setPivotMode(newValue, "toolPanelUi");
+            this.gridOptionsService.updateGridOptions({ options: { pivotMode: newValue}, source: 'toolPanelUi' as any });
             const { api } = this;
             if (api) {
                 api.refreshHeader();
