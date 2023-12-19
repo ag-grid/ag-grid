54273159850ad2ed4e7e13b3fd4455684a29a585
Mark Durrant <mark.j.durrant@gmail.com>
2023-12-19 12:24:35 +0000

    AG-10044 - Update About & Home pages to new DS

--- grid-packages/ag-grid-docs/documentation/src/pages/index.jsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/index.jsx
@@ -8,7 +8,7 @@ import { quotesData } from '../components/quotes/quotesData';
 import { agGridVersion } from '../utils/consts';
 import Seo from './components/SEO';
 // @ts-ignore
-import styles from './homepage.module.scss';
+import styles from '@design-system/modules/GridHomepage.module.scss';
 import LogoMark from '../components/LogoMark';
 import GlobalContextConsumer from '../components/GlobalContext';
 import ChartsLogo from '../images/inline-svgs/ag-charts-logo.svg';
@@ -69,7 +69,7 @@ const Default = () => {
                 description={`AG Grid is a feature rich datagrid designed for the major JavaScript Frameworks. Version ${agGridVersion} is out now. Easily integrate into your application to deliver filtering, grouping, aggregation, pivoting and much more with the performance that your users expect. Our Community version is free and open source, or take a 2 month trial of AG Grid Enterprise.`}
             />
             <div className={styles.homepageHero}>
-                <section className={classNames(styles.heroInner, 'page-margin')}>
+                <section className={classNames(styles.heroInner, 'layout-page-max-width')}>
                     <section className={styles.heroHeadings}>
                         <h1 className="font-size-extra-large">
                             The&nbsp;Best&nbsp;JavaScript Grid&nbsp;in&nbsp;the&nbsp;World
@@ -95,7 +95,7 @@ const Default = () => {
             </div>
 
             <div className={styles.homepageFrameworks}>
-                <div className={classNames(styles.frameworksInner, 'page-margin')}>
+                <div className={classNames(styles.frameworksInner, 'layout-page-max-width')}>
                     <span className={classNames(styles.frameworksLabel, 'text-secondary')}>Get&nbsp;started</span>
 
                     <FrameworkSelector data={frameworksData} isFullWidth />
@@ -103,13 +103,13 @@ const Default = () => {
             </div>
 
             <div className={styles.homepageQuotes}>
-                <div className="page-margin">
+                <div className="layout-page-max-width">
                     <Quotes data={quotesData} />
                 </div>
             </div>
 
             <div className={styles.homepageCustomers}>
-                <div className={classNames(styles.customersInner, 'page-margin')}>
+                <div className={classNames(styles.customersInner, 'layout-page-max-width')}>
                     <p className="font-size-responsive font-size-large text-secondary">
                         Trusted by developers at nine out of ten Fortune 500 companies
                     </p>
@@ -128,7 +128,7 @@ const Default = () => {
             </div>
 
             <div className={styles.homepageCharts}>
-                <div className={classNames(styles.chartsInner, "page-margin")}>
+                <div className={classNames(styles.chartsInner, "layout-page-max-width")}>
                     <div className={styles.chartsCopy}>
                         <ChartsLogo className={styles.chartsLogo}/>
                         <p className="font-size-large">Check out the all new <a href="https://charts.ag-grid.com"><b>AG Charts</b></a>. Experience the power of <b>AG Grid</b> <a href="/javascript-data-grid/integrated-charts/">Integrated Charts</a> in a standalone library.</p>
@@ -144,7 +144,7 @@ const Default = () => {
                 {({ darkMode }) => {
                     return <>
                         <section className={styles.automatedRowGroupingOuter}>
-                            <div className={classNames('page-margin', styles.homepageExample)}>
+                            <div className={classNames('layout-page-max-width', styles.homepageExample)}>
                                 <div className={styles.automatedRowGrouping}>
                                     {!IS_SSR && (
                                         <React.Suspense fallback={<></>}>
@@ -163,7 +163,7 @@ const Default = () => {
                         </section>
 
                         <section className={styles.automatedIntegratedChartsOuter}>
-                            <div className={classNames('page-margin', styles.homepageExample)}>
+                            <div className={classNames('layout-page-max-width', styles.homepageExample)}>
                                 <div className={styles.automatedIntegratedCharts}>
                                     {!IS_SSR && (
                                         <React.Suspense fallback={<></>}>
@@ -184,7 +184,7 @@ const Default = () => {
             </GlobalContextConsumer>
 
             <div className={styles.homepageSponsorship}>
-                <section className={classNames(styles.sponsorshipInner, 'page-margin')}>
+                <section className={classNames(styles.sponsorshipInner, 'layout-page-max-width')}>
                     <div>
                         <h2>Supporting Open&nbsp;Source</h2>
                         <h3 className="thin-text">We are proud to sponsor the tools we use and love.</h3>
