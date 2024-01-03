99ffdcd08f5afe5f3c1aa84ad0f0be1d07099185
Mark Durrant <mark.j.durrant@gmail.com>
2024-01-03 11:31:15 +0000

    AG-10044 - Fix homepage automated examples buttons issues

--- grid-packages/ag-grid-docs/documentation/src/components/automated-examples/ToggleAutomatedExampleButton.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/automated-examples/ToggleAutomatedExampleButton.tsx
@@ -13,7 +13,7 @@ export const ToggleAutomatedExampleButton: FunctionComponent<Props> = ({ isHover
     return (
         <span className={styles.buttonOuter}>
             <button
-                className={classnames(styles.button, {
+                className={classnames(styles.button, "text-xl", {
                     hover: isHoveredOver,
                     [styles.isActive]: scriptIsActive,
                 })}
