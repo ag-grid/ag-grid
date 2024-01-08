2e10d18724b21446da76d98bcc84c795455ff87c
Tak Tran <contact@tutaktran.com>
2024-01-08 12:53:27 +0000

    AG-10257 - Add react astro plugin

--- grid-packages/ag-grid-docs-new/astro.config.mjs
+++ grid-packages/ag-grid-docs-new/astro.config.mjs
@@ -1,4 +1,8 @@
 import { defineConfig } from 'astro/config';
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()]
+});
