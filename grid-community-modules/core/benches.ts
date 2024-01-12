c8845e267cfd8e0eae5bba7f51730d4d0188f6bd
Stephen Cooper <scooperdev@gmail.com>
2024-01-12 17:07:11 +0000

    Clean test file

--- grid-community-modules/core/benches.ts
+++ grid-community-modules/core/benches.ts
@@ -21,61 +21,43 @@ export type NestedFieldPaths<TData = any, TValue = any, TDepth extends any[] = [
 export interface ColDef<TData = any, TValue = any> {
     field: ColDefField<TData, TValue>;
 }
+interface IRowData {
+    a: number;
+    c: string;
+    d: Date;
+    d2: Date;
+    b: IRowData;
+    b2: IRowData;
+}
 
 bench('number', () => {
-    interface IRowData {
-        a: number;
-        c: string;
-        d: Date;
-        d2: Date;
-        b: IRowData;
-        b2: IRowData;
-    }
     return { field: 'a' } as ColDef<IRowData>;
 })
-.mean([2,'ns'])
-.types([662, 'instantiations']);
-
-bench('number[]', () => {
-    interface IRowData {
-        a: number[];
-        c: string[];
-        d: Date[];
-        d2: Date[];
-        b: IRowData;
-        b2: IRowData;
-    }
-    return { field: 'a' } as ColDef<IRowData>;
-})
-.mean([2,'ns'])
-.types([679, 'instantiations']);
+//.mean([2,'ns'])
+.types([2072, 'instantiations']);
 
 bench('OG: number', () => {
-    interface IRowData {
-        a: number;
-        c: string;
-        d: Date;
-        d2: Date;
-        b: IRowData;
-        b2: IRowData;
-
-    }
     return { field: 'a' } as ColDefOriginal<IRowData>;
 })
-.mean([2,'ns'])
-.types([662, 'instantiations']);
+//.mean([2,'ns'])
+.types([2072, 'instantiations']);
+
+interface IRowData2 {
+    a: number[];
+    c: string[];
+    d: Date[];
+    d2: Date[];
+    b: IRowData2;
+    b2: IRowData2;
+}
+bench('number[]', () => {
+    return { field: 'a' } as ColDef<IRowData2>;
+})
+//.mean([2,'ns'])
+.types([865, 'instantiations']);
 
 bench('OG: number[]', () => {
-    interface IRowData {
-        a: number[];
-        c: string[];
-        d: Date[];
-        d2: Date[];
-        b: IRowData;
-        b2: IRowData;
-
-    }
-    return { field: 'a' } as ColDefOriginal<IRowData>;
+    return { field: 'a' } as ColDefOriginal<IRowData2>;
 })
-.mean([2,'ns'])
-.types([679, 'instantiations']);
+//.mean([2,'ns'])
+.types([865, 'instantiations']);
