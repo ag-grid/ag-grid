1a40a8f1d18923f1d73446f82a7b24e940282526
Stephen Cooper <scooperdev@gmail.com>
2024-01-12 17:03:05 +0000

    add timings

--- grid-community-modules/core/benches.ts
+++ grid-community-modules/core/benches.ts
@@ -7,21 +7,12 @@ type NestedPath<TValue, Prefix extends string, TValueNestedChild, TDepth extends
         ? `${Prefix}.${ TDepth['length'] extends 7 ? any : NestedFieldPaths<TValue, TValueNestedChild, TDepth>}`
         : never;
 
-// This type wrapper is needed for correct handling of union types in ColDefField
-// If a user provides a union type for TData = {a: string} | { b: string} then ColDefField<TData> will be "a" | "b"
-// Without the ColDefField wrapper NestedFieldPaths<TData> would return never as there is no overlap between the two types
-/**
- * Returns a union of all possible paths to nested fields in `TData`.
- */
 export type ColDefField<TData = any, TValue = any> = TData extends any ? NestedFieldPaths<TData, TValue, []> : never;
 
-/**
- * Returns a union of all possible paths to nested fields in `TData`.
- */
 export type NestedFieldPaths<TData = any, TValue = any, TDepth extends any[] = []> = {
     [TKey in StringOrNumKeys<TData>]:
         TData[TKey] extends Function | undefined ? never // ignore functions
-        : TData[TKey] extends Date | undefined ? (TData[TKey] extends TValue ? `${TKey}` : never) // dont recurse into dates
+        : TData[TKey] extends Date | undefined ? (TData[TKey] extends TValue ? `${TKey}` : never) // don't recurse into dates
             : TData[TKey] extends any[] | undefined 
                 ? (TData[TKey] extends TValue ? `${TKey}` : never) | `${TKey}.${number}` // arrays support index access
                 : (TData[TKey] extends TValue ? `${TKey}` : never) |  NestedPath<TData[TKey], `${TKey}`, TValue, [...TDepth, any]>;
@@ -41,7 +32,9 @@ bench('number', () => {
         b2: IRowData;
     }
     return { field: 'a' } as ColDef<IRowData>;
-}).types([662, 'instantiations']);
+})
+.mean([2,'ns'])
+.types([662, 'instantiations']);
 
 bench('number[]', () => {
     interface IRowData {
@@ -53,7 +46,9 @@ bench('number[]', () => {
         b2: IRowData;
     }
     return { field: 'a' } as ColDef<IRowData>;
-}).types([679, 'instantiations']);
+})
+.mean([2,'ns'])
+.types([679, 'instantiations']);
 
 bench('OG: number', () => {
     interface IRowData {
@@ -66,7 +61,9 @@ bench('OG: number', () => {
 
     }
     return { field: 'a' } as ColDefOriginal<IRowData>;
-}).types([662, 'instantiations']);
+})
+.mean([2,'ns'])
+.types([662, 'instantiations']);
 
 bench('OG: number[]', () => {
     interface IRowData {
@@ -79,20 +76,6 @@ bench('OG: number[]', () => {
 
     }
     return { field: 'a' } as ColDefOriginal<IRowData>;
-}).types([679, 'instantiations']);
-
-/* bench('bench type', () => {
-    return {field: 'b'} as ColDef<IRowData2, unknown>;
-}).types([2831, 'instantiations']); */
-/* bench('bench field2', () => {
-    
-    return '' as NestedPath<{a: {b:Function}},'',number, []>;
-}).types([3, 'instantiations']); */
-
-/* bench('bench runtime and type', () => {
-    return { field: 'a' } as ColDef<IRowData>;
-}).types([112, 'instantiations']);
-
-bench('bench runtime and Recusvie type', () => {
-    return { field: 'child.a' } as ColDef<IRowData2>;
-}).types([2161, 'instantiations']); */
+})
+.mean([2,'ns'])
+.types([679, 'instantiations']);
