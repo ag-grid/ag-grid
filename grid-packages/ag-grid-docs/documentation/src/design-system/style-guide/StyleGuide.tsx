787df8cb1b15e6b6acca48411d2d1f8573d8fd89
Mark Durrant <mark.j.durrant@gmail.com>
2024-01-16 11:33:43 +0000

    AG-3390 - Sync Grid & Charts design systems

--- grid-packages/ag-grid-docs/documentation/src/design-system/style-guide/StyleGuide.tsx
+++ grid-packages/ag-grid-docs/documentation/src/design-system/style-guide/StyleGuide.tsx
@@ -18,7 +18,7 @@ import { TextSizes } from './TextSizes';
 export const StyleGuide: FunctionComponent = () => {
     return (
         <>
-            <div className={classnames(styles.styleGuide, 'layout-page-max-width')}>
+            <div className={classnames(styles.styleGuide, 'layout-max-width-small')}>
                 <h1>STYLE GUIDE</h1>
                 <Layout />
                 <Spacing />
