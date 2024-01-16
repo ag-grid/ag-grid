0cac5698f7274648b34d738435d2c7bf97d60863
Stephen Cooper <scooperdev@gmail.com>
2024-01-16 12:32:03 +0000

    stash

--- grid-community-modules/core/benches.ts
+++ grid-community-modules/core/benches.ts
@@ -2,39 +2,37 @@ import { bench } from '@arktype/attest';
 import { IItem, IRowData, IRowData2 } from './benchesOG';
 
 type StringOrNumKeys<TObj> = keyof TObj & (string | number);
-type FilteredKeys<TObj, TFilter> = {
-    [TKey in StringOrNumKeys<TObj>]: TObj[TKey] extends TFilter | undefined ? TKey : never;
-}[ StringOrNumKeys<TObj>];
-
 type NestedPath<TValue, Prefix extends string, TValueNestedChild, TDepth extends any[]> = 
-TValue extends object
-?  TValue extends any[] | undefined ? never :
-          TValue extends Date | undefined ? never: `${Prefix}.${ TDepth['length'] extends 5 ? any : NestedFieldPaths<TValue, TValueNestedChild, TDepth>}`
+    TValue extends object
+        ? TValue extends Date | undefined | null ? never // Don't recurse into Date internals
+        : `${Prefix}.${ TDepth['length'] extends 5 ? any : NestedFieldPaths<TValue, TValueNestedChild, TDepth>}`
         : never;
 
 export type ColDefField<TData = any, TValue = any> = TData extends any ? NestedFieldPaths<TData, TValue, []> : never;
 
 export type NestedFieldPaths<TData = any, TValue = any, TDepth extends any[] = []> = {
-    [TKey in FilteredKeys<TData, TValue>]:
+    [TKey in StringOrNumKeys<TData>]:
         TData[TKey] extends Function | undefined ? never // ignore functions
-            : TKey | NestedPath<TData[TKey], `${TKey}`, TValue, [...TDepth, any]>;
-}[FilteredKeys<TData, TValue>];
+            : TData[TKey] extends any[] | undefined 
+                ? (TData[TKey] extends TValue ? `${TKey}` : never) | `${TKey}.${number}` // arrays support index access
+                : (TData[TKey] extends TValue ? `${TKey}` : never) | NestedPath<TData[TKey], `${TKey}`, TValue, [...TDepth, any]>;
+}[StringOrNumKeys<TData>];
 
 export interface ColDef<TData = any, TValue = any> {
     field: ColDefField<TData, TValue>;
 }
 
 bench('IRowData', () => {
-    return { field: 'a' } as ColDef<IRowData, number>;
+    return { field: 'b.d' } as ColDef<IRowData>;
 })
-.types([2055, 'instantiations']);
+.types([2101, 'instantiations']);
 
 bench('IRowData2', () => {
-    return { field: 'c' } as ColDef<IRowData2>;
+    return { field: 'a' } as ColDef<IRowData2>;
 })
-.types([811, 'instantiations']);
+.types([888, 'instantiations']);
 
 bench('ITem[]', () => {
     return [{ field: 'A' }, {field: 'A.B'}] as ColDef<IItem>[];
 })
-.types([10921, 'instantiations']);
+.types([10998, 'instantiations']);
