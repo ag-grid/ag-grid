f67e56f144abdd2336fc35330c4b42b5471e0ee4
Stephen Cooper <scooperdev@gmail.com>
2024-01-24 09:29:16 +0000

    AG-10453 Fix Overlay test for Jsdom

--- grid-community-modules/core/src/ts/rendering/overlays/noRowsOverlayComponent.ts
+++ grid-community-modules/core/src/ts/rendering/overlays/noRowsOverlayComponent.ts
@@ -34,7 +34,7 @@ export class NoRowsOverlayComponent extends Component implements INoRowsOverlayC
             // setTimeout is used because some screen readers only announce `aria-live` text when
             // there is a "text change", so we force a change from empty.
             setTimeout(() => {
-                this.getGui().innerText = localeTextFunc('noRowsToShow', 'No Rows To Show');
+                this.getGui().textContent = localeTextFunc('noRowsToShow', 'No Rows To Show');
             });
         }
     }
