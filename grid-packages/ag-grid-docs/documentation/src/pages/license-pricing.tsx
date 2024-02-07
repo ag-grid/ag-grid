159e5b19af2df460f9db2e9d9c23c934faed27fd
Mark Durrant <mark.j.durrant@gmail.com>
2024-02-07 12:23:53 +0000

    AG-10469 - Code formatting

--- grid-packages/ag-grid-docs/documentation/src/pages/license-pricing.tsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/license-pricing.tsx
@@ -1,23 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import styles from '@design-system/modules/license-pricing.module.scss';
 import classnames from 'classnames';
-import { Icon } from '../components/Icon';
+import React, { useEffect, useState } from 'react';
 import { InfoEmailLink } from '../components/InfoEmailLink';
 import { Licenses } from '../components/licenses/Licenses';
-import NPMIcon from '../images/inline-svgs/npm.svg';
-import { trackOnceInfoEmail } from '../utils/analytics';
-import { hostPrefix } from '../utils/consts';
-import SEO from './components/SEO';
-import styles from '@design-system/modules/license-pricing.module.scss';
-import GridActive from '../images/inline-svgs/pricing/grid-active.svg';
 import ChartsActive from '../images/inline-svgs/pricing/charts-active.svg';
-import GridInactive from '../images/inline-svgs/pricing/grid-inactive.svg';
 import ChartsInactive from '../images/inline-svgs/pricing/charts-inactive.svg';
+import GridActive from '../images/inline-svgs/pricing/grid-active.svg';
+import GridInactive from '../images/inline-svgs/pricing/grid-inactive.svg';
+import { trackOnceInfoEmail } from '../utils/analytics';
+import SEO from './components/SEO';
 
 import ToggleBackground from '../images/inline-svgs/pricing/toggle-background.svg';
 
-
 import ComparisonTable from '../components/licenses/ComparisonTable';
-
 
 export const LicensePricing = () => {
     const [showFullWidthBar, setShowFullWidthBar] = useState(false);
@@ -59,7 +54,7 @@ export const LicensePricing = () => {
     const [isChecked, setIsChecked] = useState(false);
 
     const handleToggle = () => {
-      setIsChecked(!isChecked);
+        setIsChecked(!isChecked);
     };
 
     return (
@@ -67,16 +62,12 @@ export const LicensePricing = () => {
             {showFullWidthBar && (
                 <div className={styles.fullWidthBar}>
                     <div className={classnames('layout-max-width-small', styles.fullWidthBarContainer)}>
-                        <div className={styles.fullWidthBarLeft}>
-                            Pricing
-                        </div>
+                        <div className={styles.fullWidthBarLeft}>Pricing</div>
                         <div className={styles.fullWidthBarItem}>Community</div>
-                            <div className={styles.fullWidthBarItem}>Enterprise</div>
-                            <div className={styles.fullWidthBarItem}>Grid + Charts</div>
+                        <div className={styles.fullWidthBarItem}>Enterprise</div>
+                        <div className={styles.fullWidthBarItem}>Grid + Charts</div>
 
-                        <div className={styles.fullWidthBarRight}>
-                            
-                        </div>
+                        <div className={styles.fullWidthBarRight}></div>
                     </div>
                 </div>
             )}
@@ -88,44 +79,42 @@ export const LicensePricing = () => {
                             <h1 className={styles.pricingHeading}>Pricing</h1>
 
                             <p className="text-regular">
-                            Need bulk pricing? We can provide quotes, give bulk pricing, contact us at
-                                <InfoEmailLink
-                                    emailSubject="AG Grid Developer license query"
-                                    trackingType="headerLink"
-                                >
+                                Need bulk pricing? We can provide quotes, give bulk pricing, contact us at
+                                <InfoEmailLink emailSubject="AG Grid Developer license query" trackingType="headerLink">
                                     info@ag-grid.com
                                 </InfoEmailLink>{' '}
                                 for info.
                             </p>
 
-                            {/* <p className={styles.salesEmail}>
-                                <InfoEmailLink isButton withIcon className="button-tertiary" trackingType="headerButton">
-                                    info@ag-grid.com
-                                </InfoEmailLink>
-                            </p> */}
-
-<div className={styles.switchContainer}>
-    <div className={styles.gradient}></div>
-<ToggleBackground className={styles.toggleBackground}/>
-<hr className={styles.horizontalRule}/>
-<div className={styles.toggleWrapper}>
-      <input
-        type="checkbox"
-        id="toggle"
-        className={styles.toggleCheckbox}
-        checked={isChecked}
-        onChange={handleToggle}
-      />
-      <label htmlFor="toggle" className={styles.toggleContainer}>
-        <div className={styles.gridToggle}><GridActive className={styles.gridActive}/><GridInactive className={styles.gridInactive}/>Grid</div>
-        <div className={styles.chartsToggle}><ChartsActive className={styles.chartsActive}/><ChartsInactive className={styles.chartsInactive}/>Charts <div className={styles.newTag}>New</div></div>
-      </label>
-    </div>
-    </div>
-
+                            <div className={styles.switchContainer}>
+                                <div className={styles.gradient}></div>
+                                <ToggleBackground className={styles.toggleBackground} />
+                                <hr className={styles.horizontalRule} />
+                                <div className={styles.toggleWrapper}>
+                                    <input
+                                        type="checkbox"
+                                        id="toggle"
+                                        className={styles.toggleCheckbox}
+                                        checked={isChecked}
+                                        onChange={handleToggle}
+                                    />
+                                    <label htmlFor="toggle" className={styles.toggleContainer}>
+                                        <div className={styles.gridToggle}>
+                                            <GridActive className={styles.gridActive} />
+                                            <GridInactive className={styles.gridInactive} />
+                                            Grid
+                                        </div>
+                                        <div className={styles.chartsToggle}>
+                                            <ChartsActive className={styles.chartsActive} />
+                                            <ChartsInactive className={styles.chartsInactive} />
+                                            Charts <div className={styles.newTag}>New</div>
+                                        </div>
+                                    </label>
+                                </div>
+                            </div>
                         </div>
                         <div className={styles.licensesOuter}>
-                        <Licenses isChecked={isChecked} />
+                            <Licenses isChecked={isChecked} />
                         </div>
                         <ComparisonTable isChecked={isChecked} />
 
@@ -133,7 +122,8 @@ export const LicensePricing = () => {
                             <a href="#video-explainer" className={styles.thumbnail}>
                                 <img
                                     src="https://img.youtube.com/vi/xacx_attYuo/hqdefault.jpg"
-                                    alt="AG Grid license explained video" />
+                                    alt="AG Grid license explained video"
+                                />
                             </a>
 
                             <div>
