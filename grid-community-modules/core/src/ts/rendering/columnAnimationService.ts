f64918d6d839a41ad5ace395c4a9372b30760934
Stephen Cooper <scooperdev@gmail.com>
2024-02-09 14:58:41 +0000

    AG-10636 Less flushing, use queues

--- grid-community-modules/core/src/ts/rendering/columnAnimationService.ts
+++ grid-community-modules/core/src/ts/rendering/columnAnimationService.ts
@@ -43,17 +43,7 @@ export class ColumnAnimationService extends BeanStub {
 
     public finish(): void {
         if (!this.active) { return; }
-        this.executeNextVMTurn(() => {
-            // React components are rendered asynchronously so they call executeNextVMTurn after finish has been called
-            // This flushes any remaining executeNextVMTurn calls asap
-            this.flush();
-        });
-        this.executeLaterVMTurn(() => {
-            this.active = false;
-            // Ensure anything remaining is also executed now that active is false preventing any further executeNextVMTurn / executeLaterVMTurn calls
-            this.flush();
-        });
-        this.flush();
+        this.flush(() => { this.active = false });
     }
 
     public executeNextVMTurn(func: Function): void {
@@ -87,23 +77,25 @@ export class ColumnAnimationService extends BeanStub {
         });
     }
 
-    private flush(): void {
-        const nowFuncs = this.executeNextFuncs;
-        this.executeNextFuncs = [];
-
-        const waitFuncs = this.executeLaterFuncs;
-        this.executeLaterFuncs = [];
-
-
-        if(nowFuncs.length > 0){
-            this.getFrameworkOverrides().wrapIncoming(() => {
-                window.setTimeout(() => nowFuncs.forEach(func => func()), 0);
-            });    
+    private flush(callback: () => void): void {
+        if (this.executeNextFuncs.length === 0 && this.executeLaterFuncs.length === 0) { 
+            callback();
+            return; 
         }
-        if(waitFuncs.length > 0){
-            this.getFrameworkOverrides().wrapIncoming(() => {         
-                window.setTimeout(() => waitFuncs.forEach(func => func()), 200);
-            });
+
+        const runFuncs = (queue: Function[]) => {
+            while (queue.length) {
+                const func = queue.pop();
+                if (func) { func(); }
+            }
         }
+
+        this.getFrameworkOverrides().wrapIncoming(() => {
+            window.setTimeout(() => runFuncs(this.executeNextFuncs), 0);
+            window.setTimeout(() => {
+                runFuncs(this.executeLaterFuncs);
+                callback();
+            }, 200);
+        });
     }
 }
