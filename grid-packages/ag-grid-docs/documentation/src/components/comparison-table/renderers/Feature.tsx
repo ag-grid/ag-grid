12ccafc6e37fbb2f2b5082f9d2c3b84d27cd43ec
Mark Durrant <mark.j.durrant@gmail.com>
2024-02-14 12:55:34 +0000

    AG-10469 - Add support section to comparison table

--- grid-packages/ag-grid-docs/documentation/src/components/comparison-table/renderers/Feature.tsx
+++ grid-packages/ag-grid-docs/documentation/src/components/comparison-table/renderers/Feature.tsx
@@ -1,11 +1,15 @@
+import classNames from 'classnames';
 import React from 'react';
 import styles from '@design-system/modules/ComparisonTableRenderers.module.scss';
 import { Icon } from '../../Icon';
 
 export function Feature({ value }: { value: boolean | string }) {
-    if (value) {
-      return <div className={styles.feature}><span className={styles.tick}><Icon name="tick"/></span></div> 
-    } else {
-      return <div className={styles.feature}><span className={styles.dash}>—</span></div> 
-    }
+    const isEnabled = value === true || value.value === true;
+
+    return <div className={styles.feature}>
+      { isEnabled && <span className={styles.tick}><Icon name="tick"/></span> }
+      { !isEnabled && <span className={styles.dash}>—</span> }
+
+      { value.detail && <span className={styles.detail}>{value.detail}</span> }
+    </div> 
 }
