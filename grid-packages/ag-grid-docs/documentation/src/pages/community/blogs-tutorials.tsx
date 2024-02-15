c3b281315dcde72e86ca00f8c49c961788570e38
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-02-15 15:46:57 +0000

    AG-8783: Add content

--- grid-packages/ag-grid-docs/documentation/src/pages/community/blogs-tutorials.tsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/community/blogs-tutorials.tsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import styles from '@design-system/modules/GridDocs.module.scss';
-import aboutUsStyles from '@design-system/modules/about.module.scss';
 import classnames from 'classnames';
 import SEO from '../components/SEO';
+import 'ag-grid-community/styles/ag-grid.css'; // Core CSS
+import 'ag-grid-community/styles/ag-theme-quartz.css'; // Theme
+import { AgGridReact } from 'ag-grid-react';
+import { Icon } from '../../components/Icon';
+import content from './community-content.json';
 
 const CommunityPage = () => {
+    const colDefs = [
+        {
+            field: "title"
+        },
+        {
+            field: "description"
+        },
+        {
+            field: "link",
+            cellRenderer: (params) => {
+                return (params.value &&
+                    <a target="_blank" href={params.value}>
+                        link
+                    </a>
+                );
+            },
+            maxWidth: 90
+        },
+        {
+            field: "author"
+        },
+        {
+            field: "type",
+            maxWidth: 100
+        },
+        {
+            field: "published",
+            sort: 'desc'
+        },
+    ];
+
     return (
         <div id="doc-page-wrapper" className={styles['doc-page-wrapper']}>
             <div id="doc-content" className={classnames("doc-content", styles['doc-page'])}>
@@ -22,7 +57,13 @@ const CommunityPage = () => {
                     A collection of written content from our community
                 </span>
                 <div className={styles.pageSections} style={{ marginTop: '25px' }}>
-
+                    <div className="ag-theme-quartz-dark" style={{ height: '1000px' }}>
+                        <AgGridReact
+                            rowData={content}
+                            columnDefs={colDefs}
+                            pagination={true}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
--- grid-packages/ag-grid-docs/documentation/src/pages/community/showcase.jsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/community/showcase.jsx
@@ -37,7 +37,7 @@ const CommunityPage = () => {
             },
             maxWidth: 150,
         },
-        { field: 'stars', sort: 'desc' },
+        { field: 'stars', sort: 'desc', filter: 'agNumberColumnFilter', },
         { field: 'type' },
     ];
 
