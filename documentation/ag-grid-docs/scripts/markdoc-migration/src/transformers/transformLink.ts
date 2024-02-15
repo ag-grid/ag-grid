092c5327fd985e3744bb7f89d3cb02304e3ef078
Tak Tran <contact@tutaktran.com>
2024-02-15 16:44:40 +0000

    AG-10122 - Transform stray links
    Can exist in block level components eg, `videoSection`

--- documentation/ag-grid-docs/scripts/markdoc-migration/src/transformers/transformLink.ts
+++ documentation/ag-grid-docs/scripts/markdoc-migration/src/transformers/transformLink.ts
@@ -1,18 +1,27 @@
+import { link } from 'mdast-builder';
 import { visit } from 'unist-util-visit';
+
+import { JSX_TEXT_TYPE } from '../constants';
+import { getAttributeValue } from '../utils/getAttributeValue';
+import { getRelativeUrl } from '../utils/getRelativeUrl';
 
 /**
  * Transform links to be relative
  */
 export function transformLink(ast: any) {
-    const matcher = { type: 'link' };
+    visit(ast, { type: 'link' }, function (node) {
+        node.url = getRelativeUrl(node.url);
+    });
 
-    visit(ast, matcher, function (node) {
-        const { url } = node;
+    visit(ast, { type: JSX_TEXT_TYPE, name: 'a' }, function (node) {
+        const { attributes, children } = node;
 
-        if (url.startsWith('/')) {
-            node.url = `.${url}`;
-        } else if (url.startsWith('../')) {
-            node.url = node.url.slice(1);
-        }
+        const hrefValue = getAttributeValue({ attributes: attributes!, name: 'href' });
+        const url = getRelativeUrl(hrefValue);
+        const replacement = link(url, undefined, children);
+
+        node.type = replacement.type;
+        node.url = replacement.url;
+        node.title = replacement.title;
     });
 }
