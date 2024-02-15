6436c32e299fb120e922ba7422e6129ac17a0691
Tim Kendrick <tim.kendrick@ag-grid.com>
2024-02-15 11:25:30 +0000

    AG-9286 Enable hierarchical pivot charts (#7612)
    * AG-9286 Hide non-pivot-enabled charts in pivot chart type panel
    
    * AG-10683 Show correct chart menu types for current environment
    
    * AG-9286 Handle row grouping in hierarchical charts
    
    * AG-9286 Enable hierarchical pivot charts

--- grid-enterprise-modules/charts/src/charts/chartComp/chartProxies/hierarchical/hierarchicalChartProxy.ts
+++ grid-enterprise-modules/charts/src/charts/chartComp/chartProxies/hierarchical/hierarchicalChartProxy.ts
@@ -1,6 +1,7 @@
 import { AgCharts, AgHierarchyChartOptions, AgHierarchySeriesOptions } from 'ag-charts-community';
 import { ChartProxy, ChartProxyParams, UpdateParams } from '../chartProxy';
-import { CATEGORY_LABEL_KEY, createCategoryHierarchy } from './hierarchicalChartUtils';
+import { CATEGORY_LABEL_KEY, createAutoGroupHierarchy, createCategoryHierarchy } from './hierarchicalChartUtils';
+import { GROUP_AUTO_COLUMN_ID } from '@ag-grid-community/core';
 
 export abstract class HierarchicalChartProxy extends ChartProxy {
     protected constructor(protected readonly chartProxyParams: ChartProxyParams) {
@@ -20,7 +21,17 @@ export abstract class HierarchicalChartProxy extends ChartProxy {
     protected abstract getSeries(params: UpdateParams, labelKey: string): AgHierarchySeriesOptions[];
 
     protected getData(params: UpdateParams): any[] {
-        const categoryKeys = params.categories.map(({ id }) => id);
-        return createCategoryHierarchy(params.data, categoryKeys);
+        const { categories, data, grouping: isGrouped } = params;
+        if (isGrouped) {
+            return createAutoGroupHierarchy(data, getRowAutoGroupLabels);
+        } else {
+            const categoryKeys = categories.map(({ id }) => id);
+            return createCategoryHierarchy(data, categoryKeys);
+        }
     }
 }
+
+function getRowAutoGroupLabels(item: object): string[] | null {
+    return (item as { [GROUP_AUTO_COLUMN_ID]?: { labels: string[] } })[GROUP_AUTO_COLUMN_ID]?.labels ?? null;
+}
+
