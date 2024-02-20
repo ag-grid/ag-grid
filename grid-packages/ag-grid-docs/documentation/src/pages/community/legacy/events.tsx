828c4d4fa37f6f78cd5cf74a0e59fe12b11fa4b1
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-02-20 12:34:18 +0000

    AG-8783: Fix broken import

--- grid-packages/ag-grid-docs/documentation/src/pages/community/legacy/events.tsx
+++ grid-packages/ag-grid-docs/documentation/src/pages/community/legacy/events.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '@design-system/modules/GridDocs.module.scss';
 import classnames from 'classnames';
-import SEO from '../../../components/SEOSEO';
+import SEO from '../../../components/SEO';
 
 const PastEvents = () => {
     return (
