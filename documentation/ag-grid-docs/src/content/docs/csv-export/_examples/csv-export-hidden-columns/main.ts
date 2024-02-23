fb3bdb4c0bd727451c32cb68aa3ae04184930f18
Tak Tran <contact@tutaktran.com>
2024-02-23 12:58:29 +0000

    AG-10122 - Add missing docs from migration

---
+++ documentation/ag-grid-docs/src/content/docs/csv-export/_examples/csv-export-hidden-columns/main.ts
@@ -0,0 +1,62 @@
+import { GridApi, createGrid, GridOptions } from '@ag-grid-community/core';
+import { getData } from "./data";
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { CsvExportModule } from '@ag-grid-community/csv-export';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { ModuleRegistry } from "@ag-grid-community/core";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, CsvExportModule, MenuModule]);
+
+
+let gridApi: GridApi;
+
+const gridOptions: GridOptions = {
+  defaultColDef: {
+    editable: true,
+    minWidth: 100,
+    flex: 1,
+  },
+
+  suppressExcelExport: true,
+  popupParent: document.body,
+
+  columnDefs: [
+    { field: 'athlete' },
+    { field: 'country' },
+    { field: 'sport' },
+    { field: 'gold', hide: true },
+    { field: 'silver', hide: true },
+    { field: 'bronze', hide: true },
+    { field: 'total' },
+  ],
+
+  rowData: getData()
+}
+
+function getBoolean(id: string) {
+  var field: any = document.querySelector('#' + id)
+
+  return !!field.checked
+}
+
+function getParams() {
+  return {
+    allColumns: getBoolean('allColumns'),
+  }
+}
+
+function onBtnExport() {
+  gridApi!.exportDataAsCsv(getParams())
+}
+
+function onBtnUpdate() {
+  (document.querySelector('#csvResult') as any).value = gridApi!.getDataAsCsv(
+    getParams()
+  )
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector<HTMLElement>('#myGrid')!
+  gridApi = createGrid(gridDiv, gridOptions);
+})
---
+++ documentation/ag-grid-docs/src/content/docs/side-bar/_examples/only-filters/main.ts
@@ -0,0 +1,47 @@
+import { GridApi, createGrid, GridOptions } from '@ag-grid-community/core';
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { SetFilterModule } from '@ag-grid-enterprise/set-filter';
+import { ModuleRegistry } from "@ag-grid-community/core";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, ColumnsToolPanelModule, FiltersToolPanelModule, SetFilterModule]);
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+  columnDefs: [
+    { field: 'athlete', filter: 'agTextColumnFilter', minWidth: 200 },
+    { field: 'age' },
+    { field: 'country', minWidth: 180 },
+    { field: 'year' },
+    { field: 'date', minWidth: 150 },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+  ],
+  defaultColDef: {
+    flex: 1,
+    minWidth: 100,
+    // allow every column to be aggregated
+    enableValue: true,
+    // allow every column to be grouped
+    enableRowGroup: true,
+    // allow every column to be pivoted
+    enablePivot: true,
+    filter: true,
+  },
+  sideBar: 'filters',
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector<HTMLElement>('#myGrid')!
+  gridApi = createGrid(gridDiv, gridOptions);
+
+  fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+    .then(response => response.json())
+    .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data))
+})
