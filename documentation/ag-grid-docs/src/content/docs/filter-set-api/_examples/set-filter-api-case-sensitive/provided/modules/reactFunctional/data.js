17f8d1d802e3cfd3a73749d170f66e26c8e6bad8
Tak Tran <contact@tutaktran.com>
2024-02-23 12:34:19 +0000

    AG-10122 - Add missing filter-set-api page

---
+++ documentation/ag-grid-docs/src/content/docs/filter-set-api/_examples/set-filter-api-case-sensitive/provided/modules/reactFunctional/data.js
@@ -0,0 +1,67 @@
+function getData() {
+    return [
+        {
+            colour: 'Black',
+        },
+        {
+            colour: 'BLACK',
+        },
+        {
+            colour: 'black',
+        },
+        {
+            colour: 'Red',
+        },
+        {
+            colour: 'RED',
+        },
+        {
+            colour: 'red',
+        },
+        {
+            colour: 'Orange',
+        },
+        {
+            colour: 'ORANGE',
+        },
+        {
+            colour: 'orange',
+        },
+        {
+            colour: 'White',
+        },
+        {
+            colour: 'WHITE',
+        },
+        {
+            colour: 'white',
+        },
+        {
+            colour: 'Yellow',
+        },
+        {
+            colour: 'YELLOW',
+        },
+        {
+            colour: 'yellow',
+        },
+        {
+            colour: 'Green',
+        },
+        {
+            colour: 'GREEN',
+        },
+        {
+            colour: 'green',
+        },
+        {
+            colour: 'Purple',
+        },
+        {
+            colour: 'PURPLE',
+        },
+        {
+            colour: 'purple',
+        },
+    ];
+}
