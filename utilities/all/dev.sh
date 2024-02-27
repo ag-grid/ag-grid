67e906bd904cb1a55334123dd19426ab0ead64bb
seanlandsman <sean@thelandsmans.com>
2024-02-27 13:45:56 +0000

    Add react support

--- utilities/all/dev.sh
+++ utilities/all/dev.sh
@@ -58,7 +58,7 @@ if ! ignoreProject $MODIFIED; then
     nx run-many -p @ag-grid-community/client-side-row-model,@ag-grid-community/core,@ag-grid-community/csv-export,@ag-grid-community/infinite-row-model,@ag-grid-enterprise/advanced-filter@ag-grid-enterprise/charts,@ag-grid-enterprise/charts-enterprise,@ag-grid-enterprise/clipboard,@ag-grid-enterprise/column-tool-panel,@ag-grid-enterprise/core,@ag-grid-enterprise/excel-export,@ag-grid-enterprise/filter-tool-panel,@ag-grid-enterprise/master-detail,@ag-grid-enterprise/menu,@ag-grid-enterprise/multi-filter,@ag-grid-enterprise/range-selection,@ag-grid-enterprise/rich-select,@ag-grid-enterprise/row-grouping,@ag-grid-enterprise/server-side-row-model,@ag-grid-enterprise/set-filter,@ag-grid-enterprise/side-bar,@ag-grid-enterprise/sparklines,@ag-grid-enterprise/status-bar,@ag-grid-enterprise/viewport-row-model -t build:types -c watch
     nx build @ag-grid-community/core -t build:package,build:umd,generate-doc-references -c watch
   elif exampleRunnerFile $MODIFIED; then
-    nx generate-examples --batch
+    nx generate-examples
   elif [[ "${VALID_FILE_COUNT}" -gt 0 ]] ; then
     echo "[changed]${VALID_FILES}"
     nx run-many -p ${MODIFIED} -t build
