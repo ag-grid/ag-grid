32428be0df894c59a508ba351f6174ecc6dfa6a7
Stephen Cooper <scooperdev@gmail.com>
2024-02-27 14:27:55 +0000

    Batch generate examples in watch

--- plugins/ag-grid-task-autogen/src/generate-example-files.ts
+++ plugins/ag-grid-task-autogen/src/generate-example-files.ts
@@ -14,6 +14,7 @@ export function createTask(parentProject: string, srcRelativeInputPath: string):
                 '{projectRoot}/**',
                 { dependentTasksOutputFiles: '**/*', transitive: false },
                 { externalDependencies: ['npm:typescript'] },
+                '{workspaceRoot}/documentation/ag-grid-docs/public/example-runner/**',
             ],
             outputs: ['{options.outputPath}'],
             cache: true,
