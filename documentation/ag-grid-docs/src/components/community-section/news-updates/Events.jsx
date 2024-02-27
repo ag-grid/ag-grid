08e7be9074ef8aa9916674cb5d6602c775d792be
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-02-27 16:07:05 +0000

    Improve events logic

--- documentation/ag-grid-docs/src/components/community-section/news-updates/Events.jsx
+++ documentation/ag-grid-docs/src/components/community-section/news-updates/Events.jsx
@@ -1,32 +1,39 @@
-import React, { useState, useRef, useEffect } from 'react';
 import styles from '@design-system/modules/community-section/news-updates/Events.module.scss';
-import events from '../../../content/community/news-updates/events.json';
-import eventImages from'../../../content/community/news-updates/events-images.json';
+import React, { useEffect, useRef, useState } from 'react';
 
+import eventImages from '../../../content/community/news-updates/events-images.json';
+import events from '../../../content/community/news-updates/events.json';
+
+const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; 
+    }
+    return array;
+};
 
 const ScrollingGallery = () => {
     const scrollRef = useRef(null);
     useEffect(() => {
-      const scrollContent = () => {
-        if (!scrollRef.current) return;
-        const { scrollLeft, clientWidth, scrollWidth } = scrollRef.current;
-        const isAtEnd = scrollLeft + clientWidth >= scrollWidth;
-        scrollRef.current.scrollTo({
-          // Reset to start if at the end, otherwise increment scroll position
-          left: isAtEnd ? 0 : scrollLeft + 1,
-          behavior: 'auto',
-        });
-      };
-    
-      const interval = setInterval(scrollContent, 20); // Adjust interval for speed
-      return () => clearInterval(interval);
+        const scrollContent = () => {
+            if (!scrollRef.current) return;
+            const { scrollLeft, clientWidth, scrollWidth } = scrollRef.current;
+            const isAtEnd = scrollLeft + clientWidth >= scrollWidth;
+            scrollRef.current.scrollTo({
+                left: isAtEnd ? 0 : scrollLeft + 1,
+                behavior: 'auto',
+            });
+        };
+
+        const interval = setInterval(scrollContent, 20);
+        return () => clearInterval(interval);
     }, []);
 
     return (
         <div className={styles.blurContainer}>
             <div className={styles.scrollingGalleryContainer} ref={scrollRef}>
-                {eventImages.map((image, index) => (
-                    <img          
+                {shuffleArray([...eventImages]).map((image, index) => (
+                    <img
                         key={index}
                         src={`/community/events/${image.src}`}
                         alt={`Gallery item ${index}`}
@@ -35,8 +42,8 @@ const ScrollingGallery = () => {
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 const Events = () => {
     const [selectedYear, setSelectedYear] = useState(() => {
@@ -44,38 +51,36 @@ const Events = () => {
         return keys[keys.length - 1];
     });
 
-    const handleYearFilter = (year) => {
-        setSelectedYear(year)
-    }
-
     return (
         <div className={styles.container}>
             <ScrollingGallery />
             <div className={styles.eventDetailsContainer}>
                 <div className={styles.yearFiltersContainer}>
-                    {Object.keys(events).toReversed().map((year, index) => (
-                        <button 
-                            key={index}
-                            className={`${styles.yearFilter} ${year === selectedYear ? styles.active : ''}`} 
-                            onClick={() => handleYearFilter(year)}
-                        >
-                            {year}
-                        </button>
-                    ))}
+                    {Object.keys(events)
+                        .toReversed()
+                        .map((year, index) => (
+                            <button
+                                key={index}
+                                className={`${styles.yearFilter} ${year === selectedYear ? styles.active : ''}`}
+                                onClick={() => setSelectedYear(year)}
+                            >
+                                {year}
+                            </button>
+                        ))}
                 </div>
-                <div className={styles.eventTilesContainer} >
+                <div className={styles.eventTilesContainer}>
                     {events[selectedYear].map((event, index) => (
                         <div key={index} className={styles.eventTile}>
                             <span className={styles.title}>{event.title}</span>
                             <span className={styles.location}>{event.location}</span>
                             <hr></hr>
                             <span className={styles.description}>{event.description}</span>
-                    </div>
+                        </div>
                     ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Events;
