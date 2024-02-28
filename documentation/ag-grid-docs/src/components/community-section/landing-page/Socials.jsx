8d01a15f750ed0fa810184883c6cb8ad635245a7
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-02-28 16:37:08 +0000

    Extract support & socials to external json

--- documentation/ag-grid-docs/src/components/community-section/landing-page/Socials.jsx
+++ documentation/ag-grid-docs/src/components/community-section/landing-page/Socials.jsx
@@ -1,63 +1,15 @@
 import styles from '@design-system/modules/community-section/landing-page/Socials.module.scss';
 import React, { useEffect, useRef, useState } from 'react';
+import socialChannels from '../../../content/community/socials.json';
 
 const Socials = () => {
-    // Define an array of objects for the images and their corresponding text
-    const images = [
-        {
-            src: '/community/socials/x-white.svg',
-            text: 'X (Twitter)',
-            desc: 'Found a bug? Raise a GitHub issue and one of our team will take a look',
-            link: {
-                "url": "https://twitter.com/aggrid",
-                "text": "Follow"
-            }
-        },
-        {
-            src: '/community/socials/linkedin.svg',
-            text: 'LinkedIn',
-            desc: 'Found a bug? Raise a GitHub issue and one of our team will take a look',
-            link: {
-                "url": "https://www.linkedin.com/company/ag-grid/",
-                "text": "Connect"
-            }
-        },
-        {
-            src: '/community/socials/youtube.svg',
-            text: 'YouTube',
-            desc: 'Enterprise user? Use Zendesk to request support from our dedicated in-house team',
-            link: {
-                "url": "https://youtube.com/c/ag-grid",
-                "text": "Subscribe"
-            }
-        },
-        {
-            src: '/community/socials/blog.svg',
-            text: 'Blog',
-            desc: 'Havess a question? Browse the AG Grid & AG Charts tags to leverage our community to help answer your questions',
-            link: {
-                "url": "https://blog.ag-grid.com",
-                "text": "Browse"
-            }
-        },
-        {
-            src: '/community/socials/mailchimp.svg',
-            text: 'Newsletter',
-            desc: 'Enterprise user? Use Zendesk to request support from our dedicated in-house team',
-            link: {
-                "url": "https://blog.ag-grid.com/newsletter",
-                "text": "Join"
-            }
-        },
-    ];
-
     return (
         <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                {images.map((image, index) => (
-                    <a target={'_blank'} href={image.link.url}>
-                        <div key={index} className={styles.imageItem}>
-                            <img src={image.src} alt={`Image ${index + 1}`} />
+            <div className={styles.socialChannelContainer}>
+                {socialChannels.map((channel, index) => (
+                    <a target={'_blank'} href={channel.link.url}>
+                        <div key={index} className={styles.socialChannelItem}>
+                            <img src={channel.src} alt={`Image ${index + 1}`} />
                         </div>
                     </a>
                 ))}
--- documentation/ag-grid-docs/src/components/community-section/landing-page/Support.jsx
+++ documentation/ag-grid-docs/src/components/community-section/landing-page/Support.jsx
@@ -1,28 +1,8 @@
 import styles from '@design-system/modules/community-section/landing-page/Support.module.scss';
 import React, { useEffect, useRef, useState } from 'react';
+import supportSites from '../../../content/community/support.json';
 
 const Support = () => {
-    // Define an array of objects for the supportSites and their corresponding text
-    const supportSites = [
-        {
-            src: '/community/support/stack-overflow.svg',
-            text: 'Stack Overflow',
-            desc: 'Have a question? Browse the AG Grid & AG Charts tags or create a new thread',
-            link: "https://stackoverflow.com/questions/tagged/ag-grid"
-        },
-        {
-            src: '/community/support/github-white.svg',
-            text: 'GitHub',
-            desc: 'Found a bug? Raise a GitHub issue and one of our team will take a look',
-            link: "https://github.com/ag-grid/ag-grid",
-        },
-        {
-            src: '/community/support/zendesk.svg',
-            text: 'Zendesk',
-            desc: 'Enterprise user? Use Zendesk to raise a ticket with our dedicated in-house team',
-            link: "https://ag-grid.zendesk.com/hc/en-us",
-        },
-    ];
 
     return (
         <div className={styles.supportChannelContainer}>
