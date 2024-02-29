e44e4fb263c294eaf7a5ce5365d0fac4c9294719
Stephen Cooper <scooperdev@gmail.com>
2024-02-29 12:11:58 +0000

    Convert fakeServer to typescript

--- documentation/ag-grid-docs/src/content/docs/server-side-model-grouping/_examples/row-grouping/fakeServer.js
+++ documentation/ag-grid-docs/src/content/docs/server-side-model-grouping/_examples/row-grouping/fakeServer.js
@@ -1,5 +1,3 @@
-import alasql from "alasql";
-
 // This fake server uses http://alasql.org/ to mimic how a real server
 // might generate sql queries from the Server-Side Row Model request.
 // To keep things simple it does the bare minimum to support the example.
@@ -52,7 +50,6 @@ export function FakeServer(allData) {
     function whereSql(request) {
         var rowGroups = request.rowGroupCols;
         var groupKeys = request.groupKeys;
-        var filterModel = request.filterModel;
         var whereParts = [];
 
         if (groupKeys) {
@@ -63,87 +60,11 @@ export function FakeServer(allData) {
             });
         }
 
-        if (filterModel) {
-            Object.keys(filterModel).forEach(function(key) {
-                var item = filterModel[key];
-
-                switch (item.filterType) {
-                    case 'text':
-                        whereParts.push(createFilterSql(textFilterMapper, key, item));
-                        break;
-                    case 'number':
-                        whereParts.push(createFilterSql(numberFilterMapper, key, item));
-                        break;
-                    default:
-                        console.log('unknown filter type: ' + item.filterType);
-                        break;
-                }
-            });
-        }
-
         if (whereParts.length > 0) {
             return ' WHERE ' + whereParts.join(' AND ');
         }
 
         return '';
-    }
-
-    function createFilterSql(mapper, key, item) {
-        if (item.operator) {
-            const conditions = item.conditions.map(condition => mapper(key, condition));
-
-            return '(' + conditions.join(' ' + item.operator + ' ') + ')';
-        }
-
-        return mapper(key, item);
-    }
-
-    function textFilterMapper(key, item) {
-        switch (item.type) {
-            case 'equals':
-                return key + " = '" + item.filter + "'";
-            case 'notEqual':
-                return key + " != '" + item.filter + "'";
-            case 'contains':
-                return key + " LIKE '%" + item.filter + "%'";
-            case 'notContains':
-                return key + " NOT LIKE '%" + item.filter + "%'";
-            case 'startsWith':
-                return key + " LIKE '" + item.filter + "%'";
-            case 'endsWith':
-                return key + " LIKE '%" + item.filter + "'";
-            case 'blank':
-                return key + " IS NULL or " + key + " = ''";
-            case 'notBlank':
-                return key + " IS NOT NULL and " + key + " != ''";
-            default:
-                console.log('unknown text filter type: ' + item.type);
-        }
-    }
-
-    function numberFilterMapper(key, item) {
-        switch (item.type) {
-            case 'equals':
-                return key + ' = ' + item.filter;
-            case 'notEqual':
-                return key + ' != ' + item.filter;
-            case 'greaterThan':
-                return key + ' > ' + item.filter;
-            case 'greaterThanOrEqual':
-                return key + ' >= ' + item.filter;
-            case 'lessThan':
-                return key + ' < ' + item.filter;
-            case 'lessThanOrEqual':
-                return key + ' <= ' + item.filter;
-            case 'inRange':
-                return '(' + key + ' >= ' + item.filter + ' and ' + key + ' <= ' + item.filterTo + ')';
-            case 'blank':
-                return key + " IS NULL";
-            case 'notBlank':
-                return key + " IS NOT NULL";
-            default:
-                console.log('unknown number filter type: ' + item.type);
-        }
     }
 
     function groupBySql(request) {
@@ -174,7 +95,6 @@ export function FakeServer(allData) {
     function limitSql(request) {
         if (request.endRow == undefined || request.startRow == undefined) { return ''; }
         var blockSize = request.endRow - request.startRow;
-
         return ' LIMIT ' + blockSize + ' OFFSET ' + request.startRow;
     }
 
