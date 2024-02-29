a6cae6e625e86e4363bb2763eebd4cf9df0da09c
seanlandsman <sean@thelandsmans.com>
2024-02-29 11:57:40 +0000

    Vanilla js

--- documentation/ag-grid-docs/src/utils/gridLibraryPaths.ts
+++ documentation/ag-grid-docs/src/utils/gridLibraryPaths.ts
@@ -6,7 +6,7 @@ export const getGridScriptPath = (sitePrefix?: string) => {
         return PUBLISHED_UMD_URLS['ag-grid-community'];
     }
     const sitePrefixUrl = sitePrefix ? sitePrefix : '';
-    return pathJoin(sitePrefixUrl, '/dev/ag-grid-community/dist/umd/ag-grid-community.js');
+    return pathJoin(sitePrefixUrl, '/files/ag-grid-community/dist/ag-grid-community.js');
 };
 
 export const getGridEnterpriseScriptPath = (sitePrefix?: string) => {
@@ -14,7 +14,7 @@ export const getGridEnterpriseScriptPath = (sitePrefix?: string) => {
         return PUBLISHED_UMD_URLS['ag-grid-enterprise'];
     }
     const sitePrefixUrl = sitePrefix ? sitePrefix : '';
-    return pathJoin(sitePrefixUrl, '/dev/ag-grid-enterprise/dist/umd/ag-grid-enterprise.js');
+    return pathJoin(sitePrefixUrl, '/files/ag-grid-enterprise/dist/ag-grid-enterprise.js');
 };
 
 export const getCacheBustingUrl = (url: string, timestamp: number) => `${url}?t=${timestamp}`;
