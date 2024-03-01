5b596405448556e96957c8e898b0bf5079122c91
Stephen Cooper <scooperdev@gmail.com>
2024-03-01 15:18:23 +0000

    Add json end point for all examples

---
+++ documentation/ag-grid-docs/src/pages/debug/all-examples.json.ts
@@ -0,0 +1,36 @@
+import { getIsImplemented } from '@constants';
+import { getDocsExamplePages } from '@features/docs/utils/pageData';
+import { getCollection } from 'astro:content';
+
+export async function getAllExamples() {
+    const pages = await getCollection('docs');
+    const examples = await getDocsExamplePages({
+        pages,
+    });
+
+    return examples.filter(({ params }) => {
+        return getIsImplemented(params);
+    });
+}
+
+export async function GET() {
+   
+
+    const examples = await getAllExamples();
+
+    const cleanerExamples = examples.map((example) => {
+        return {
+            pageName: example.params.pageName,
+            exampleName: example.params.exampleName,
+            importType: example.params.importType,
+            internalFramework: example.params.internalFramework,
+        };
+    });
+
+    return new Response(JSON.stringify(cleanerExamples), {
+        status: 200,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
