728b7ab196b6b26ee6c68161b86be3bebd0f0feb
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-03-04 12:37:12 +0000

    Update support

--- documentation/ag-grid-docs/src/components/community-section/landing-page/Support.jsx
+++ documentation/ag-grid-docs/src/components/community-section/landing-page/Support.jsx
@@ -1,19 +1,22 @@
 import styles from '@design-system/modules/community-section/landing-page/Support.module.scss';
 import React, { useEffect, useRef, useState } from 'react';
+
 import supportSites from '../../../content/community/support.json';
 
 const Support = () => {
-
     return (
         <div className={styles.supportChannelContainer}>
             {supportSites.map((site, index) => (
-                <a target='_blank' className={styles.link} href={site.link}>
+                <a target="_blank" className={styles.link} href={site.link}>
                     <div key={index} className={styles.supportChannel}>
                         <div className={styles.header}>
                             <img src={site.src} alt={`Image ${index + 1}`} />
                             <span className={styles.text}>{site.text}</span>
                         </div>
                         <span className={styles.desc}>{site.desc}</span>
+                        <div className={styles.cta}>
+                            <a>{site?.cta}</a>
+                        </div>
                     </div>
                 </a>
             ))}
