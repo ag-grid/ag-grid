e1ed6c3983ffdcb539c5123597ae8fad9a76fbb2
Tim Kendrick <tim.kendrick@ag-grid.com>
2024-03-04 20:25:04 +0000

    AG-8529 Unlink `navigator.enabled` / `zoom.enabled` integrated charts options panels

--- enterprise-modules/charts/src/charts/chartComp/menu/format/navigator/navigatorPanel.ts
+++ enterprise-modules/charts/src/charts/chartComp/menu/format/navigator/navigatorPanel.ts
@@ -1,12 +1,8 @@
 import {
-    AgGroupComponent,
     AgGroupComponentParams,
     Autowired,
-    ChartOptionsChanged,
     Component,
-    Events,
     PostConstruct,
-    RefSelector
 } from "@ag-grid-community/core";
 import { ChartTranslationService } from "../../../services/chartTranslationService";
 import { FormatPanelOptions } from "../formatPanel";
@@ -25,12 +21,9 @@ export class NavigatorPanel extends Component {
         </div>`;
 
     @Autowired('chartTranslationService') private readonly chartTranslationService: ChartTranslationService;
-    
-    @RefSelector('navigatorGroup') private readonly navigatorGroup: AgGroupComponent;
 
     private readonly chartMenuUtils: ChartMenuUtils;
     private readonly isExpandedOnInit: boolean;
-    private isToggling: boolean = false;
 
     constructor({ chartOptionsService, isExpandedOnInit = false }: FormatPanelOptions) {
         super();
@@ -60,30 +53,12 @@ export class NavigatorPanel extends Component {
         const navigatorMaxSliderParams = this.chartMenuUtils.getDefaultSliderParams("navigator.max", "rangeEnd", 1);
         navigatorMaxSliderParams.step = 0.05;
 
-        // Disable the zoom setting whenever the navigator setting is enabled
-        navigatorGroupParams.onEnableChange = ((onEnableChange) => (enabled: boolean) => {
-            if (!onEnableChange) return;
-            this.isToggling = true;
-            if (enabled) this.chartMenuUtils.setValue('zoom.enabled', false);
-            onEnableChange(enabled);
-            this.isToggling = false;
-        })(navigatorGroupParams.onEnableChange);
-
         this.setTemplate(NavigatorPanel.TEMPLATE, {
             navigatorGroup: navigatorGroupParams,
             navigatorHeightSlider: navigatorHeightSliderParams,
             navigatorMiniChartCheckbox: navigatorMiniChartCheckboxParams,
             navigatorMinSlider: navigatorMinSliderParams,
             navigatorMaxSlider: navigatorMaxSliderParams,
-        });
-
-        // Ensure up-to-date panel enabled state whenever the chart options are changed from elsewhere
-        // (this is necessary because enabling the zoom setting causes the panel to become disabled)
-        this.addManagedListener(this.eventService, Events.EVENT_CHART_OPTIONS_CHANGED, (event: ChartOptionsChanged) => {
-            if (this.isToggling) return; // Ignore internal changes to the panel enabled state
-            const enabled = this.chartMenuUtils.getValue<boolean>('navigator.enabled');
-            const panelIsEnabled = this.navigatorGroup.isEnabled();
-            if (panelIsEnabled !== enabled) this.navigatorGroup.setEnabled(enabled);
         });
     }
 }
--- enterprise-modules/charts/src/charts/chartComp/menu/format/zoom/zoomPanel.ts
+++ enterprise-modules/charts/src/charts/chartComp/menu/format/zoom/zoomPanel.ts
@@ -1,12 +1,8 @@
 import {
-    AgEventListener,
-    AgGroupComponent,
     AgGroupComponentParams,
     AgInputNumberField,
     Autowired,
-    ChartOptionsChanged,
     Component,
-    Events,
     PostConstruct,
     RefSelector,
 } from '@ag-grid-community/core';
@@ -26,12 +22,10 @@ export class ZoomPanel extends Component {
 
     @Autowired('chartTranslationService') private readonly chartTranslationService: ChartTranslationService;
 
-    @RefSelector('zoomGroup') private readonly zoomGroup: AgGroupComponent;
     @RefSelector('zoomScrollingStepInput') private readonly zoomScrollingStepInput: AgInputNumberField;
 
     private readonly chartMenuUtils: ChartMenuUtils;
     private readonly isExpandedOnInit: boolean;
-    private isToggling: boolean = false;
 
     constructor({ chartOptionsService, isExpandedOnInit = false }: FormatPanelOptions) {
         super();
@@ -71,15 +65,6 @@ export class ZoomPanel extends Component {
             'selectingZoom'
         );
 
-        // Disable the navigator setting whenever the zoom setting is enabled
-        zoomGroupParams.onEnableChange = ((onEnableChange) => (enabled: boolean) => {
-            if (!onEnableChange) return;
-            this.isToggling = true;
-            if (enabled) this.chartMenuUtils.setValue('navigator.enabled', false);
-            onEnableChange(enabled);
-            this.isToggling = false;
-        })(zoomGroupParams.onEnableChange);
-
         // Enable/disable the scrolling step input according to whether the scrolling checkbox is checked
         zoomScrollingCheckboxParams.onValueChange = ((onValueChange) => (value: any) => {
             if (!onValueChange) return;
@@ -97,14 +82,5 @@ export class ZoomPanel extends Component {
 
         // Set the initial state of the scrolling step input according to whether the scrolling checkbox is checked
         this.zoomScrollingStepInput.setDisabled(!zoomScrollingCheckboxParams.value);
-
-        // Ensure up-to-date panel enabled state whenever the chart options are changed from elsewhere
-        // (this is necessary because enabling the navigator setting causes the panel to become disabled)
-        this.addManagedListener(this.eventService, Events.EVENT_CHART_OPTIONS_CHANGED, (event: ChartOptionsChanged) => {
-            if (this.isToggling) return; // Ignore internal changes to the panel enabled state
-            const enabled = this.chartMenuUtils.getValue<boolean>('zoom.enabled');
-            const panelIsEnabled = this.zoomGroup.isEnabled();
-            if (panelIsEnabled !== enabled) this.zoomGroup.setEnabled(enabled);
-        });
     }
 }
