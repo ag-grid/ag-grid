36c8c8996d81e7657427e2a018024a7679290efd
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-03-04 12:38:24 +0000

    Change to grid layout

--- documentation/ag-grid-docs/src/components/community-section/news-updates/FeaturedNews.jsx
+++ documentation/ag-grid-docs/src/components/community-section/news-updates/FeaturedNews.jsx
@@ -5,33 +5,37 @@ import featuredNews from '../../../content/community/news-updates/featured-news.
 
 const FeaturedNews = () => {
     return (
-        <div className={styles.container}>
-            <div className={styles.leftColumn}>
-                <div className={styles.card}>
-                    <img src={featuredNews.major_article.image} alt="Image Description" />
-                    <div className={styles.content}>
-                        <h2>{featuredNews.major_article.title}</h2>
-                        <span className={styles.leftColumnDescription}>{featuredNews.major_article.description}</span>
-                            <a target="_blank" href={featuredNews.major_article.link}>
-                                Learn More
-                            </a>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.rightColumn}>
-                {featuredNews.minor_articles.map((article, index) => (
-                    <div key={index} className={`${styles.card} ${styles.horizontal}`}>
-                        <img src={article.image} className={styles.minorArticleImage} alt="Image Description" />
+        <div class={styles.gridContainer}>
+            <div class={styles.leftColumn}>
+                <a href={featuredNews.major_article.link} target="_blank" className={styles.linkWrapper}>
+                    <div className={styles.card}>
+                        <img src={featuredNews.major_article.image} alt="Image Description" />
                         <div className={styles.content}>
-                            <h2 className={styles.rightColumnTitle}>{article.title}</h2>
-                            <p className={styles.rightColumnDescription}>{article.description}</p>
-                            <a target="_blank" href={article.link} className={styles.rightColumnLink}>
-                                Learn More
-                            </a>
+                            <h2>{featuredNews.major_article.title}</h2>
+                            <span className={styles.leftColumnDescription}>{featuredNews.major_article.description}</span>
+                                <a target="_blank" href={featuredNews.major_article.link}>
+                                    Learn More
+                                </a>
                         </div>
                     </div>
-                ))}
+                </a>
             </div>
+            {featuredNews.minor_articles.map((article, index) => (
+                <a href={article.link} target="_blank" className={styles.linkWrapper}>
+                    <div class={styles.rightColumn}>
+                        <div key={index} className={`${styles.card} ${styles.horizontal}`}>
+                            <img src={article.image} className={styles.minorArticleImage} alt="Image Description" />
+                            <div className={styles.content}>
+                                <h2 className={styles.rightColumnTitle}>{article.title}</h2>
+                                <p className={styles.rightColumnDescription}>{article.description}</p>
+                                <a target="_blank" href={article.link} className={styles.rightColumnLink}>
+                                    Learn More
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            ))}
         </div>
     );
 };
