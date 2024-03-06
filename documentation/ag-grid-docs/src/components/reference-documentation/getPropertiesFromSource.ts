ef60e6e4e811d8ad73ca569c532f3e0df339cc4e
Tak Tran <contact@tutaktran.com>
2024-03-06 15:53:12 +0000

    RTI-1947 - Transform apiDocumentation headings for side menu

---
+++ documentation/ag-grid-docs/src/components/reference-documentation/getPropertiesFromSource.ts
@@ -0,0 +1,58 @@
+import { getIsDev } from '@utils/env';
+import { getJsonFile } from '@utils/pages';
+import { getEntry } from 'astro:content';
+
+/**
+ * NOTE: Keep this as a separate file, so it is not imported by client
+ * side code
+ */
+export const getPropertiesFromSource = async ({
+    source,
+    sources: sourcesProp,
+}: {
+    source: string;
+    sources: string[];
+}) => {
+    const sources = source ? [source] : sourcesProp;
+    const propertiesFromFilesPromises = sources.map(async (s: string) => {
+        // NOTE: Need to remove `.json` for getEntry
+        const fileName = s.replace('.json', '');
+        const fileEntry = await getEntry('api-documentation', fileName);
+        if (!fileEntry) {
+            const message = `ApiDocumentation source not found: src/content/api-documentation/${fileName}.json`;
+            if (getIsDev()) {
+                console.error(message);
+            } else {
+                throw new Error(message);
+            }
+        } else {
+            return fileEntry.data;
+        }
+    });
+    const propertiesFromFiles = (await Promise.all(propertiesFromFilesPromises)).filter(Boolean);
+
+    const propertyConfigs = propertiesFromFiles
+        .map((p) => {
+            const config = p['_config_'];
+            if (!config) {
+                console.warn(`ApiDocumentation: _config_ property missing from source ${sources.join()}.`);
+            }
+            return config;
+        })
+        .filter(Boolean);
+    const codeConfigEntries = propertyConfigs
+        .map((config) => config.codeSrc)
+        .map((fileName) => {
+            const referenceFileName = `reference/${fileName}`;
+            const file = getJsonFile(referenceFileName);
+            return [fileName, file];
+        });
+    const codeConfigs = Object.fromEntries(codeConfigEntries);
+
+    return {
+        sources,
+        propertiesFromFiles,
+        propertyConfigs,
+        codeConfigs,
+    };
+};
