c74f4819521f2c401ad5d0770e2240988d0477fb
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-03-06 16:24:11 +0000

    Modify events for homepage

--- documentation/ag-grid-docs/src/components/community-section/news-updates/Events.jsx
+++ documentation/ag-grid-docs/src/components/community-section/news-updates/Events.jsx
@@ -2,12 +2,12 @@ import styles from '@design-system/modules/community-section/news-updates/Events
 import React, { useEffect, useRef, useState } from 'react';
 
 import eventImages from '../../../content/community/news-updates/events-images.json';
-import events from '../../../content/community/news-updates/events.json';
+import events from '../../../content/community/news-updates/events-new.json';
 
 const shuffleArray = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]]; 
+        [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
 };
@@ -45,31 +45,62 @@ const ScrollingGallery = () => {
     );
 };
 
-const Events = () => {
-    const [selectedYear, setSelectedYear] = useState(() => {
-        const keys = Object.keys(events);
-        return keys[keys.length - 1];
+const separateEventsByDate = (events) => {
+    const upcomingEvents = [];
+    const pastEvents = [];
+
+    events.forEach((event) => {
+        const startDate = new Date(event.startDate);
+        if (startDate >= new Date()) {
+            upcomingEvents.push(event);
+        } else {
+            pastEvents.push(event);
+        }
     });
+
+    return { upcomingEvents, pastEvents };
+};
+
+function extractUniqueYears(events) {
+    const years = new Set();
+    const currYear = new Date().getFullYear();
+    events.forEach((event) => {
+        const year = event.startDate.substring(0, 4);
+        years.add(year);
+    });
+    return Array.from(years);
+}
+
+const Events = () => {
+    const { upcomingEvents } = separateEventsByDate(events);
+    const [currEvents, setCurrEvents] = useState(upcomingEvents)
+    const [selectedYear, setSelectedYear] = useState(new Date().getFullYear());
+
+    const filterYears = (year) => {
+        setSelectedYear(year);
+        setCurrEvents(events.filter(event => {
+            const eventYear = new Date(event.startDate).getFullYear();
+            return eventYear == year;
+        }));
+    }
 
     return (
         <div className={styles.container}>
             <div className={styles.eventDetailsContainer}>
                 <div className={styles.yearFiltersContainer}>
-                    {Object.keys(events)
-                        .toReversed()
-                        .map((year, index) => (
-                            <button
-                                key={index}
-                                className={`${styles.yearFilter} ${year === selectedYear ? styles.active : ''}`}
-                                onClick={() => setSelectedYear(year)}
-                            >
-                                {year}
-                            </button>
-                        ))}
+                    {extractUniqueYears(events).map((year, index) => (
+                        <button
+                            key={index}
+                            className={`${styles.yearFilter} ${year == selectedYear ? styles.active : ''}`}
+                            onClick={() => filterYears(year)}
+                        >
+                            {year}
+                        </button>
+                    ))}
                 </div>
                 <ScrollingGallery />
                 <div className={styles.eventTilesContainer}>
-                    {events[selectedYear].map((event, index) => (
+                    {currEvents.map((event, index) => (
                         <div key={index} className={styles.eventTile}>
                             <span className={styles.title}>{event.title}</span>
                             <span className={styles.location}>{event.location}</span>
