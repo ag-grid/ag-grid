d101347948b86bf4f8c3af47b2eb8cb0126d90c4
Stephen Cooper <scooperdev@gmail.com>
2024-03-06 14:49:35 +0000

    Remove vanilla / typescript provided examples

--- documentation/ag-grid-docs/src/content/docs/deep-dive/_examples/testing-example/main.ts
+++ documentation/ag-grid-docs/src/content/docs/deep-dive/_examples/testing-example/main.ts
@@ -1,59 +1,125 @@
-import { createGrid, GridApi, GridOptions, CellValueChangedEvent, SelectionChangedEvent, ValueFormatterParams } from '@ag-grid-community/core';
-import { CountryFlagCellRenderer } from './CountryFlagCellRenderer';
+import { createGrid, ColDef, GridApi, GridOptions, CellValueChangedEvent, SelectionChangedEvent, ValueFormatterParams, ICellRendererComp, ICellRendererParams } from '@ag-grid-community/core';
+import '@ag-grid-community/styles/ag-grid.css';
+import "@ag-grid-community/styles/ag-theme-quartz.css";
+import { ModuleRegistry } from '@ag-grid-community/core';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { ModuleRegistry } from "@ag-grid-community/core";
+ModuleRegistry.registerModules([ ClientSideRowModelModule ]);
 
-ModuleRegistry.registerModules([ClientSideRowModelModule]);
-
-
+// Grid API: Access to Grid API methods
 let gridApi: GridApi;
-
-const currencyFormatter = (params: ValueFormatterParams) => {
-    return '£' + params.value.toLocaleString();
-}
 
 const dateFormatter = (params: ValueFormatterParams) => {
     return new Date(params.value).toLocaleDateString('en-us', { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' });
 }
 
+class CompanyLogoRenderer implements ICellRendererComp {
+    eGui!: HTMLSpanElement;
+
+    // Optional: Params for rendering. The same params that are passed to the cellRenderer function.
+    init(params: ICellRendererParams) {
+        let companyLogo: HTMLImageElement = document.createElement('img');
+        companyLogo.src = `https://www.ag-grid.com/example-assets/space-company-logos/${params.value.toLowerCase()}.png`
+        companyLogo.setAttribute('style', 'display: block; width: 25px; height: auto; max-height: 50%; margin-right: 12px; filter: brightness(1.1)');
+        
+        let companyName: HTMLParagraphElement = document.createElement('p');
+        companyName.textContent = params.value;
+        companyName.setAttribute('style', 'text-overflow: ellipsis; overflow: hidden; white-space: nowrap;');
+
+        this.eGui = document.createElement('span');
+        this.eGui.setAttribute('style', 'display: flex; height: 100%; width: 100%; align-items: center')
+        this.eGui.appendChild(companyLogo)
+        this.eGui.appendChild(companyName)
+    }
+
+    // Required: Return the DOM element of the component, this is what the grid puts into the cell
+    getGui() { 
+        return this.eGui;
+    }
+
+    // Required: Get the cell to refresh. 
+    refresh(params: ICellRendererParams): boolean {
+        return false
+    }
+}
+
+class MissionResultRenderer implements ICellRendererComp {
+    eGui!: HTMLSpanElement;
+
+    // Optional: Params for rendering. The same params that are passed to the cellRenderer function.
+    init(params: ICellRendererParams) {
+        let icon: HTMLImageElement = document.createElement('img');
+        icon.src = `https://www.ag-grid.com/example-assets/icons/${params.value ? 'tick-in-circle' : 'cross-in-circle'}.png`
+        icon.setAttribute('style', 'width: auto; height: auto;');
+
+        this.eGui = document.createElement('span');
+        this.eGui.setAttribute('style', 'display: flex; justify-content: center; height: 100%; align-items: center')
+        this.eGui.appendChild(icon)
+    }
+
+    // Required: Return the DOM element of the component, this is what the grid puts into the cell
+    getGui() { 
+        return this.eGui;
+    }
+
+    // Required: Get the cell to refresh. 
+    refresh(params: ICellRendererParams): boolean {
+        return false
+    }
+}
+
+
+// Row Data Interface
+interface IRow {
+  mission: string;
+  company: string;
+  location: string;
+  date: string;
+  time: string;
+  rocket: string;
+  price: number;
+  successful: boolean;
+}
+
 const gridOptions: GridOptions = {
     // Data to be displayed
-    rowData: [
-        { company: "RVSN USSR", country: "Kazakhstan", date: "1957-10-04", mission: "Sputnik-1", price: 9550000, successful: true },
-        { company: "RVSN USSR", country: "Kazakhstan", date: "1957-11-03", mission: "Sputnik-2", price: 8990000, successful: true },
-        { company: "US Navy", country: "USA", date: "1957-12-06", mission: "Vanguard TV3", price: 6860000, successful: false }
-    ],
+    rowData: [] as IRow[],
     // Columns to be displayed (Should match rowData properties)
     columnDefs: [
         {
-            field: "mission",
-            filter: true,
+            field: "mission", 
+            width: 150,
             checkboxSelection: true
-        },
-        {
-            field: "country",
-            cellRenderer: CountryFlagCellRenderer
-        },
-        {
-            field: "successful"
-        },
-        {
+          },
+          {
+            field: "company", 
+            width: 130,
+            cellRenderer: CompanyLogoRenderer 
+          },
+          {
+            field: "location",
+            width: 225
+          },
+          {
             field: "date",
             valueFormatter: dateFormatter
-        },
-        {
+          },
+          {
             field: "price",
-            valueFormatter: currencyFormatter
-        },
-        {
-            field: "company"
-        }
-    ],
+            width: 130,
+            valueFormatter: (params: ValueFormatterParams) => { return '£' + params.value.toLocaleString(); } 
+          },
+          {
+            field: "successful", 
+            width: 120,
+            cellRenderer: MissionResultRenderer 
+          },
+          { field: "rocket" },
+    ] as ColDef[],
     // Configurations applied to all columns
     defaultColDef: {
         filter: true,
         editable: true
-    },
+    } as ColDef,
     // Grid Options & Callbacks
     pagination: true,
     rowSelection: 'multiple',
@@ -65,10 +131,10 @@ const gridOptions: GridOptions = {
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    var gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
-    gridApi = createGrid(gridDiv, gridOptions);
-    fetch('https://www.ag-grid.com/example-assets/space-mission-data.json')
-        .then(response => response.json())
-        .then((data: any) => gridApi.setGridOption('rowData', data))
-})
+// Create Grid: Create new grid within the #myGrid div, using the Grid Options object
+gridApi = createGrid(document.querySelector<HTMLElement>('#myGrid')!, gridOptions);
+
+// Fetch Remote Data
+fetch('https://www.ag-grid.com/example-assets/space-mission-data.json')
+    .then(response => response.json())
+    .then((data: any) => gridApi.setGridOption('rowData', data))
