f907b95d51d2a3f2458dce4e748a441304bce63e
Stephen Cooper <scooperdev@gmail.com>
2024-03-07 10:50:35 +0000

    remove styles from main.ts examples

--- documentation/ag-grid-docs/src/content/docs/aligned-grids/_examples/aligned-grids/main.ts
+++ documentation/ag-grid-docs/src/content/docs/aligned-grids/_examples/aligned-grids/main.ts
@@ -1,5 +1,3 @@
-import '@ag-grid-community/styles/ag-grid.css';
-import "@ag-grid-community/styles/ag-theme-quartz.css";
 import { ColDef, ColGroupDef, createGrid, GridOptions } from '@ag-grid-community/core';
 import { ModuleRegistry } from '@ag-grid-community/core';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
