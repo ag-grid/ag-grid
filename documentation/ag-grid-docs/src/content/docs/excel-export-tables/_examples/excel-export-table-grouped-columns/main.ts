f535c2fcde4682671dc11d19d88eddecc891edd1
Salmen Hichri <salmen.hichri@gmail.com>
2024-03-08 15:52:30 +0000

    AG-8484 Added doc page for Excel Tables export feature

---
+++ documentation/ag-grid-docs/src/content/docs/excel-export-tables/_examples/excel-export-table-grouped-columns/main.ts
@@ -0,0 +1,58 @@
+import { GridApi, createGrid, GridOptions } from '@ag-grid-community/core';
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { CsvExportModule } from '@ag-grid-community/csv-export';
+import { ExcelExportModule } from '@ag-grid-enterprise/excel-export';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { ModuleRegistry } from "@ag-grid-community/core";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, CsvExportModule, ExcelExportModule, MenuModule]);
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+  columnDefs: [
+    {
+      headerName: 'Athlete',
+      children: [
+        { field: 'athlete' },
+        { field: 'age' },
+        { field: 'country' },
+      ],
+    },
+    {
+      headerName: 'Medals',
+      children: [
+          { field: 'gold' },
+          { field: 'silver' },
+          { field: 'bronze' },
+      ],
+    },
+    { field: 'total' },
+  ],
+  defaultColDef: {
+    filter: true,
+    minWidth: 100,
+    flex: 1,
+  },
+}
+
+function onBtExport() {
+  gridApi!.exportDataAsExcel({
+    tableSetup: {
+      name: 'Olympic Medals',
+    }
+  });
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+  var gridDiv = document.querySelector<HTMLElement>('#myGrid')!
+  gridApi = createGrid(gridDiv, gridOptions);
+
+  fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
+    .then(response => response.json())
+    .then(function (data) {
+      gridApi!.setGridOption('rowData', data)
+    })
+})
