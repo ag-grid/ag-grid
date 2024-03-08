b6318f73ea55cc61eb5cb63754e096acf5252a73
Stephen Cooper <scooperdev@gmail.com>
2024-03-08 09:46:46 +0000

    Remove legacy/grid-packages/ag-grid-dev Moved to ag-grid-testing

--- legacy/grid-packages/ag-grid-dev/jest.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-  ],
-  
-};
