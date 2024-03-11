6d051177d1f0190388990a4de9039421baf2296b
Stephen Cooper <scooperdev@gmail.com>
2024-03-11 14:16:00 +0000

    Fix vue3 examples

--- documentation/ag-grid-docs/src/content/docs/grouping-unbalanced-groups/_examples/balanced-groups/provided/modules/vue3/main.js
+++ documentation/ag-grid-docs/src/content/docs/grouping-unbalanced-groups/_examples/balanced-groups/provided/modules/vue3/main.js
@@ -4,7 +4,7 @@ import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-mod
 import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
 import '@ag-grid-community/styles/ag-grid.css';
 import "@ag-grid-community/styles/ag-theme-quartz.css";
-
+import { getData } from './data.js';
 import { ModuleRegistry } from '@ag-grid-community/core';
 ModuleRegistry.registerModules([ClientSideRowModelModule, RowGroupingModule]);
 
