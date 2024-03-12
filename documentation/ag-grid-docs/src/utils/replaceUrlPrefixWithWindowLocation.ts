21c478cc0ff23dd129c69fb95d3e142d58965a62
Stephen Cooper <scooperdev@gmail.com>
2024-03-12 11:10:19 +0000

    strip out vite module

--- documentation/ag-grid-docs/src/utils/replaceUrlPrefixWithWindowLocation.ts
+++ documentation/ag-grid-docs/src/utils/replaceUrlPrefixWithWindowLocation.ts
@@ -1,6 +1,11 @@
 import { SITE_URL } from '@constants';
 
-export function replaceUrlPrefixWithWindowLocation(text: string) {
+function replaceUrlPrefixWithWindowLocation(text: string) {
     const windowUrl = window.location.origin;
     return text.replaceAll(SITE_URL, windowUrl);
 }
+export const cleanIndexHtml = (htmlFile: string) => {
+    return replaceUrlPrefixWithWindowLocation(htmlFile)
+    .replace('<script type="module" src="/@vite/client"></script>', '');
+}
+
