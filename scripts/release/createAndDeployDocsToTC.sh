639e5891660389fa7366708950071faffaf074b4
seanlandsman <sean@thelandsmans.com>
2024-03-12 16:36:20 +0000

    Fix integrated charts stale issue

--- scripts/release/createAndDeployDocsToTC.sh
+++ scripts/release/createAndDeployDocsToTC.sh
@@ -2,6 +2,8 @@
 
 ZIP_PREFIX=`date +%Y%m%d`
 
+echo "Deploying Docs to Build Server"
+
 if [ ! -d "documentation/ag-grid-docs/dist" ];
 then
   echo "documentation/ag-grid-docs/dist does NOT EXIST. Exiting with error."
