b21a4d42555014a26bf7099fa6f6e353cfb14d62
Tak Tran <contact@tutaktran.com>
2024-03-12 14:06:06 +0000

    AG-3390 - Allow `getIsDev` to work during build
    During build, `import.meta.env` does not exist, so need to use `process.env`

--- documentation/ag-grid-docs/src/utils/env.ts
+++ documentation/ag-grid-docs/src/utils/env.ts
@@ -1,5 +1,5 @@
 import { PRODUCTION_SITE_URL, SITE_URL, STAGING_SITE_URL } from '../constants';
 
-export const getIsDev = () => import.meta.env?.DEV;
+export const getIsDev = () => process.env.NODE_ENV === 'development' || import.meta.env?.DEV;
 export const getIsStaging = () => SITE_URL === STAGING_SITE_URL;
 export const getIsProduction = () => SITE_URL === PRODUCTION_SITE_URL;
