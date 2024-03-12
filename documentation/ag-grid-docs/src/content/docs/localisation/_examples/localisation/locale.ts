395c2f7a6cf95f8181227acf55e19d588da89407
Stephen Cooper <scooperdev@gmail.com>
2024-03-12 12:34:12 +0000

    simplify example

--- documentation/ag-grid-docs/src/content/docs/localisation/_examples/localisation/locale.ts
+++ documentation/ag-grid-docs/src/content/docs/localisation/_examples/localisation/locale.ts
@@ -540,3 +540,10 @@ export const AG_GRID_LOCALE_EN: Record<string,string> = {
     november: 'November',
     december: 'December',
 }
+
+export const AG_GRID_LOCALE_ZZZ: Record<string, string> = {};
+// Create a dummy locale based on english but prefix everything with zzz
+Object.keys(AG_GRID_LOCALE_EN).forEach(function(key) {
+    if (key === 'thousandSeparator' || key === 'decimalSeparator') { return; }
+    AG_GRID_LOCALE_ZZZ[key] = 'zzz-' + AG_GRID_LOCALE_EN[key];
+});
