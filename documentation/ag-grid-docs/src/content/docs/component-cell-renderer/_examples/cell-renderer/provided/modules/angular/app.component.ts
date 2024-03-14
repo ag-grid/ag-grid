e557191c537dae3bd1f1733626964c19cdffc174
Stephen Cooper <scooperdev@gmail.com>
2024-03-14 11:10:57 +0000

    tidy example imports

--- documentation/ag-grid-docs/src/content/docs/component-cell-renderer/_examples/cell-renderer/provided/modules/angular/app.component.ts
+++ documentation/ag-grid-docs/src/content/docs/component-cell-renderer/_examples/cell-renderer/provided/modules/angular/app.component.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import '@ag-grid-community/styles/ag-grid.css';
-import '@ag-grid-community/styles/ag-theme-quartz.css';
-import './styles.css';
+import { NgFor } from '@angular/common';
+import { AgGridAngular } from '@ag-grid-community/angular';
 import {
     ColDef,
     GridApi,
     ICellRenderer,
     ICellRendererParams,
-    IRowNode,
+    IRowNode
 } from '@ag-grid-community/core';
-import { AgGridAngular } from '@ag-grid-community/angular';
+import '@ag-grid-community/styles/ag-grid.css';
+import '@ag-grid-community/styles/ag-theme-quartz.css';
+import './styles.css';
 
-import { ModuleRegistry } from '@ag-grid-community/core';
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { NgFor } from '@angular/common';
+import { ModuleRegistry } from '@ag-grid-community/core';
 
 ModuleRegistry.registerModules([ClientSideRowModelModule]);
 
