4ddc56d0a59fb253a1bded9002a1bd986b6e255c
seanlandsman <sean@thelandsmans.com>
2024-03-15 13:58:33 +0000

    Updates for archive prep

--- scripts/release/createDocsArchiveBundle.sh
+++ scripts/release/createDocsArchiveBundle.sh
@@ -10,27 +10,13 @@ fi
 RAW_VERSION=$1
 VERSION=""${RAW_VERSION//./}""
 
-echo "Starting Archive Docs Bundle Process"
-cd grid-packages/ag-grid-docs
-npm run clean
-
-echo "Gatsby Archive Package"
-cd documentation
-npm run clean
-GATSBY_HOST=www.ag-grid.com GATSBY_ROOT_DIRECTORY="/archive/${RAW_VERSION}" npm run package
-cd ..
-
-echo "Building Docs Archive"
-npx gulp release-archive
-cd ../../
-
 ARCHIVE_FILENAME="archive_`date +%Y%m%d`_$RAW_VERSION.tar"
 rm $ARCHIVE_FILENAME
 
 echo "Adding robots.txt and github version info"
 node scripts/release/patchDocs.js
 
-cd grid-packages/ag-grid-docs/dist
+cd documentation/ag-grid-docs/dist
 tar -cvf ../../../$ARCHIVE_FILENAME .
 cd ../../../
 
