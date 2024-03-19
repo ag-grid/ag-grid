0ce5afceab58fec8452500ade997423fd9a52c89
seanlandsman <sean@thelandsmans.com>
2024-03-19 16:58:47 +0000

    Add charts int util scripts

---
+++ scripts/deleteChartsDeps.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+for directory in 'community-modules' 'enterprise-modules' 'packages';
+do
+  for moduleDirectory in `ls $directory`;
+  do
+    if [ -d "$directory/$moduleDirectory/node_modules" ]; then
+      cd "$directory/$moduleDirectory/node_modules"
+      rm -rf ag-charts-community ag-charts-enterprise ag-charts-angular ag-charts-react ag-charts-vue ag-charts-vue3
+      cd -
+    fi
+  done
+done
+
+if [ -d "packages/ag-grid-docs/node_modules" ]; then
+  cd "packages/ag-grid-docs/node_modules"
+  rm -rf ag-charts-community ag-charts-enterprise ag-charts-angular ag-charts-react ag-charts-vue ag-charts-vue3
+fi
