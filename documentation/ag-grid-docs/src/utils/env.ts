8a13a356aadf4f9dcaedba1fa0a15428e0de5a7b
seanlandsman <sean@thelandsmans.com>
2024-03-19 22:40:22 +0000

    RTI-2051 fix homepage for archives

--- documentation/ag-grid-docs/src/utils/env.ts
+++ documentation/ag-grid-docs/src/utils/env.ts
@@ -3,4 +3,4 @@ import {PRODUCTION_SITE_URL, SITE_BASE_URL, SITE_URL, STAGING_SITE_URL} from '..
 export const getIsDev = () => process.env.NODE_ENV === 'development' || import.meta.env?.DEV;
 export const getIsStaging = () => SITE_URL === STAGING_SITE_URL;
 export const getIsProduction = () => SITE_URL === PRODUCTION_SITE_URL;
-export const getIsArchive = () => SITE_URL === PRODUCTION_SITE_URL && SITE_BASE_URL.contains("archive");
+export const getIsArchive = () => SITE_URL === PRODUCTION_SITE_URL && SITE_BASE_URL.includes("archive");
