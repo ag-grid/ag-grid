211f0ad94039a8048a875ebb0c728bedf33c080a
Kyler Phillips <kylerphillips@hotmail.co.uk>
2024-03-20 12:19:57 +0000

    AG-11017 Footer style and markup improvements

--- documentation/ag-grid-docs/src/components/footer/Footer.tsx
+++ documentation/ag-grid-docs/src/components/footer/Footer.tsx
@@ -1,10 +1,11 @@
 import type { FooterItem } from '@ag-grid-types';
+import { Icon } from '@components/icon/Icon';
 import { SITE_BASE_URL } from '@constants';
 import styles from '@design-system/modules/Footer.module.scss';
 import { urlWithBaseUrl } from '@utils/urlWithBaseUrl';
 import classNames from 'classnames';
 
-import { Icon } from '@components/icon/Icon';
+import LogoMarkTransparentSVG from '../../images/inline-svgs/ag-grid-logomark.svg?react';
 
 interface FooterProps {
     path: string;
@@ -28,22 +29,58 @@ const MenuColumns = ({ footerItems }: { footerItems: FooterItem[] }) =>
         </div>
     ));
 
-export const Footer = ({ path, footerItems }: FooterProps) => (
-    <footer className={styles.footer}>
-        <div className={classNames(styles.footerColumns, 'layout-grid')}>
-            <MenuColumns footerItems={footerItems} />
-        </div>
+export const Footer = ({ path, footerItems }: FooterProps) => {
+    // Filter items with icons
+    const iconItems = footerItems.flatMap(({ title, links }) =>
+        links
+            .filter(({ iconName }) => iconName)
+            .map(({ name, url, newTab, iconName }) => ({
+                title,
+                name,
+                url,
+                newTab,
+                iconName,
+            }))
+    );
 
-        <div className={classNames(styles.legal, 'layout-grid')}>
-            <p className="text-sm">AG Grid Ltd registered in the United Kingdom. Company&nbsp;No.&nbsp;07318192.</p>
-            <p className="text-sm">&copy; AG Grid Ltd. 2015-{new Date().getFullYear()}</p>
-        </div>
-
-        {/* Only show customer logo trademark info on homepage */}
-        {(path === SITE_BASE_URL || path === undefined) && (
-            <div className={classNames(styles.trademarks, 'layout-grid')}>
-                <p className="text-sm">The Microsoft logo is a trademark of the Microsoft group of companies.</p>
+    return (
+        <footer className={styles.footer}>
+            <div className={classNames(styles.footerColumns, 'layout-grid')}>
+                {/* Section with title, subtitle, and icons */}
+                <div className={styles.menuColumn}>
+                    <LogoMarkTransparentSVG className={styles.gridIcon} />
+                    <h2 className={styles.headerTitle}>AG Grid</h2>
+                    <p className={styles.subtitle}>&copy; AG Grid Ltd. 2015-{new Date().getFullYear()}</p>
+                    <ul className={`${styles.listStyleNone} ${styles.footerIcons}`}>
+                        {iconItems.map(({ title, name, url, newTab, iconName }) => (
+                            <li className={styles.footerIcons} key={`${title}_${name}`}>
+                                <a
+                                    href={urlWithBaseUrl(url)}
+                                    {...(newTab ? { target: '_blank', rel: 'noreferrer' } : {})}
+                                >
+                                    {iconName && <Icon name={iconName} />}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <div className={styles.placeholderSquares}>
+                        <div className={styles.square}></div>
+                        <div className={styles.square}></div>
+                        <div className={styles.square}></div>
+                    </div>
+                </div>
+                <MenuColumns footerItems={footerItems} />
             </div>
-        )}
-    </footer>
-);
+
+            <div className={classNames(styles.legal, 'layout-grid')}>
+                <p className="text-sm">AG Grid Ltd registered in the United Kingdom. Company&nbsp;No.&nbsp;07318192.</p>
+            </div>
+
+            {(path === SITE_BASE_URL || path === undefined) && (
+                <div className={classNames(styles.trademarks, 'layout-grid')}>
+                    <p className="text-sm">The Microsoft logo is a trademark of the Microsoft group of companies.</p>
+                </div>
+            )}
+        </footer>
+    );
+};
