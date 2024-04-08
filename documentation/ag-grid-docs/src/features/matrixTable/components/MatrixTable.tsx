adb9a0c2f7b9cdafbbad93da663cc4f30ec569f2
Tak Tran <contact@tutaktran.com>
2024-04-08 14:32:16 +0100

    AG-11264 - Remove EnterpriseIcon (moved to shared repo)

--- documentation/ag-grid-docs/src/features/matrixTable/components/MatrixTable.tsx
+++ documentation/ag-grid-docs/src/features/matrixTable/components/MatrixTable.tsx
@@ -1,5 +1,5 @@
 import { type Framework } from '@ag-grid-types';
-import { EnterpriseIcon } from '@components/icon/EnterpriseIcon';
+import { EnterpriseIcon } from '@ag-website-shared/components/icon/EnterpriseIcon';
 import styles from '@design-system/modules/MatrixTable.module.scss';
 import { transformMarkdoc } from '@utils/markdoc/transformMarkdoc';
 import classnames from 'classnames';
