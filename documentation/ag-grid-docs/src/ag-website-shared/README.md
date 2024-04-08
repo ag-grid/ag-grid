8338408532e8d4531bbce8f07be9df9da8956596
Tak Tran <contact@tutaktran.com>
2024-04-08 17:00:34 +0100

    git subrepo pull documentation/ag-grid-docs/src/ag-website-shared
    subrepo:
      subdir:   "documentation/ag-grid-docs/src/ag-website-shared"
      merged:   "bde055706b"
    upstream:
      origin:   "git@github.com:ag-grid/ag-website-shared.git"
      branch:   "latest"
      commit:   "bde055706b"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

--- documentation/ag-grid-docs/src/ag-website-shared/README.md
+++ documentation/ag-grid-docs/src/ag-website-shared/README.md
@@ -2,3 +2,88 @@
 
 Shared code for all AG Grid websites ([AG Grid](https://github.com/ag-grid/ag-grid) and [AG Charts](https://github.com/ag-grid/ag-charts)).
 
+## Usage
+
+This repository is included in other repositories by using [git-subrepo](https://github.com/ingydotnet/git-subrepo). Once it is included, the people using the container repository does not need to know about `git-subrepo`, unless they need to push/pull updates from this repository.
+
+### Prerequisites
+
+1. To modify or update this repository from the container repository, install [git-subrepo](https://github.com/ingydotnet/git-subrepo) on your local machine. You do not need `git-subrepo` if you just need to push/pull directly to this repository
+    1. `git clone https://github.com/ingydotnet/git-subrepo /path/to/git-subrepo`
+    1. Add `source /path/to/git-subrepo/.rc` to your terminal rc file eg, `~/.zshrc` (see docs for others)
+    1. Update `bash` if required: `brew install bash`
+    1. Restart your terminal
+
+### Installation
+
+To install this shared repository into another container repository, run the following commands (NOTE: This only needs to be done once on the container repository).
+
+1. In the container respository figure out the destination folder for this shared repository to go into (`[dest-folder]`), then run
+
+    ```
+    git subrepo clone git@github.com:ag-grid/ag-website-shared.git [dest-folder] -b latest --method rebase
+
+    # Grid
+    git subrepo clone git@github.com:ag-grid/ag-website-shared.git documentation/ag-grid-docs/src/ag-website-shared -b latest --method rebase
+
+    # Charts
+    git subrepo clone git@github.com:ag-grid/ag-website-shared.git packages/ag-charts-website/src/ag-website-shared -b latest --method rebase
+    ```
+
+1. In the website folder of the container repository, add the `@ag-website-shared/*` import alias to point to the `src` folder of this repository eg, if this repository is included in the `src/ag-website-shared` folder:
+
+    ```
+    // tsconfig.json
+    {
+      "compilerOptions": {
+         "baseUrl": ".",
+         "paths": {
+           "@ag-website-shared/*": ["src/ag-website-shared/src/*"],
+         }
+      }
+    }
+    ```
+
+    This allows all imports prefixed with `@ag-website-shared` to refer to files in this repository, whether it is imported in the container repository or this repository.
+
+### Pull changes from this shared repository
+
+In the container repository:
+
+```
+git subrepo pull [dest-folder]
+
+# Grid
+git subrepo pull documentation/ag-grid-docs/src/ag-website-shared
+
+# Charts
+git subrepo pull packages/ag-charts-website/src/ag-website-shared
+```
+
+### Backport changes from container repository to this shared repository
+
+To backport changes from the container repository back to this shared repository:
+
+1. Make commits to the container respository as normal.
+
+    It doesn't matter if there are changes not related to the shared repository in the same commit, as they will be filtered out. However, it is good practice to make shared repository changes in separate commits.
+
+1. Push changes from the container repository back to the shared repository:
+
+    ```
+    git subrepo push [dest-folder]
+
+    # Grid
+    git subrepo push documentation/ag-grid-docs/src/ag-website-shared
+
+    # Charts
+    git subrepo push packages/ag-charts-website/src/ag-website-shared
+    ```
+
+    If there are changes already on the shared repository, you will get the following error:
+
+    ```
+    git-subrepo: There are new changes upstream, you need to pull first.
+    ```
+
+    In which case, you should do a `git subrepo pull ...` first
--- documentation/ag-grid-docs/src/ag-website-shared/src/components/search/SearchModal.jsx
+++ documentation/ag-grid-docs/src/ag-website-shared/src/components/search/SearchModal.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState, useMemo } from 'react';
-import algoliasearch from 'algoliasearch/lite';
-import { InstantSearch, useHits, useInstantSearch, useSearchBox } from 'react-instantsearch';
-import SearchBox from './SearchBox';
-import Hits from './SearchHits';
-import Controls from './SearchControls';
 import styles from '@design-system/modules/SearchModal.module.scss';
+import algoliasearch from 'algoliasearch/lite';
+import React, { useEffect, useMemo, useState } from 'react';
+import { InstantSearch, useHits, useInstantSearch, useSearchBox } from 'react-instantsearch';
+
+import SearchBox from './SearchBox';
+import Controls from './SearchControls';
+import Hits from './SearchHits';
 
 const envPrefix = import.meta.env.PUBLIC_ALGOLIA_INDEX_PREFIX;
 const algoliaClient = algoliasearch(import.meta.env.PUBLIC_ALGOLIA_APP_ID, import.meta.env.PUBLIC_ALGOLIA_SEARCH_KEY);
@@ -36,7 +37,7 @@ const searchClient = {
     },
     future: {
         preserveSharedStateOnUnmount: true,
-    }
+    },
 };
 
 const Modal = ({ isOpen, closeModal, children }) => {
@@ -55,7 +56,7 @@ const Modal = ({ isOpen, closeModal, children }) => {
             htmlElement.style.paddingRight = `${scrollbarWidth}px`;
             return;
         }
-    
+
         htmlElement.style.overflow = '';
         htmlElement.style.paddingRight = '';
     }, [isOpen]);
@@ -70,14 +71,14 @@ const Modal = ({ isOpen, closeModal, children }) => {
 
     return (
         <div
-            className={`${styles.backdrop} ${(isOpen ? styles.backdropEnter : styles.backdropExit)}`}
+            className={`${styles.backdrop} ${isOpen ? styles.backdropEnter : styles.backdropExit}`}
             onAnimationEnd={onAnimationEnd}
-            onKeyDown={evt => evt.key === 'Tab' && evt.preventDefault()}
+            onKeyDown={(evt) => evt.key === 'Tab' && evt.preventDefault()}
             onClick={closeModal}
         >
             <div
                 className={styles.container}
-                onClick={evt => evt.stopPropagation()}
+                onClick={(evt) => evt.stopPropagation()}
                 role="presentation"
                 aria-modal="true"
             >
@@ -85,7 +86,7 @@ const Modal = ({ isOpen, closeModal, children }) => {
             </div>
         </div>
     );
-}
+};
 
 export default ({ currentFramework, closeModal, isOpen }) => {
     const index = `${envPrefix}_${currentFramework}`;
@@ -97,7 +98,7 @@ export default ({ currentFramework, closeModal, isOpen }) => {
             </InstantSearch>
         </Modal>
     );
-}
+};
 
 const SearchComponent = ({ closeModal }) => {
     const { status } = useInstantSearch();
@@ -126,7 +127,7 @@ const SearchComponent = ({ closeModal }) => {
         if (hitEl) {
             hitEl.scrollIntoView({ behavior: 'smooth', block });
         }
-    }
+    };
 
     const onKeyDown = (evt) => {
         if (hits.length === 0) {
@@ -134,18 +135,18 @@ const SearchComponent = ({ closeModal }) => {
         }
 
         if (evt.key === 'Enter') {
-            window.location = (flattenedHits[selectedHit].path);
+            window.location = flattenedHits[selectedHit].path;
             // close modal, if link was same page we don't want to keep the modal open.
             closeModal();
             return;
         }
 
         const newIdx = {
-            'PageUp': 0,
-            'PageDown': hits.length - 1,
-            'ArrowUp': (selectedHit - 1 + hits.length) % hits.length,
-            'ArrowDown': (selectedHit + 1) % hits.length,
-            'Tab': (selectedHit + (evt.shiftKey ? -1 : 1) + hits.length) % hits.length,
+            PageUp: 0,
+            PageDown: hits.length - 1,
+            ArrowUp: (selectedHit - 1 + hits.length) % hits.length,
+            ArrowDown: (selectedHit + 1) % hits.length,
+            Tab: (selectedHit + (evt.shiftKey ? -1 : 1) + hits.length) % hits.length,
         }[evt.key];
 
         if (newIdx === undefined) {
@@ -153,13 +154,13 @@ const SearchComponent = ({ closeModal }) => {
         }
         evt.preventDefault();
         selectHit(newIdx);
-    }
+    };
 
     return (
         <div onKeyDown={onKeyDown}>
             <SearchBox selectedHit={selectedHit} />
-            {
-                status === 'idle' && !!query.length && <>
+            {status === 'idle' && !!query.length && (
+                <>
                     <Hits
                         structuredHits={structuredHits}
                         selectedHit={selectedHit}
@@ -169,18 +170,18 @@ const SearchComponent = ({ closeModal }) => {
                     />
                     <Controls />
                 </>
-            }
+            )}
         </div>
     );
-}
+};
 
 const flattenStructure = (structuredHits) => {
     const result = [];
     structuredHits.forEach(({ children }) => {
-        children.forEach(child => result.push(child));
+        children.forEach((child) => result.push(child));
     });
     return result;
-}
+};
 
 /**
  * This function takes an array of crumbs and returns an array of crumbs which are the highest level shared crumbs.
@@ -193,16 +194,16 @@ const getAllCrumbs = (crumbArray) => {
         result.push(nextCrumb);
 
         // remove all crumbs which share the same path
-        allCrumbs = allCrumbs.filter(crumb => !crumb.breadcrumb.startsWith(nextCrumb.breadcrumb));
+        allCrumbs = allCrumbs.filter((crumb) => !crumb.breadcrumb.startsWith(nextCrumb.breadcrumb));
     }
     return result;
-}
+};
 
 const getNextBreadCrumb = (crumbArray) => {
     const firstBreadCrumb = crumbArray[0].breadcrumb.split(' > ');
     const firstCrumb = firstBreadCrumb[0];
 
-    const allSiblings = crumbArray.filter(crumb => crumb.breadcrumb.startsWith(firstCrumb));
+    const allSiblings = crumbArray.filter((crumb) => crumb.breadcrumb.startsWith(firstCrumb));
     // only crumb, return it alone as a full breadcrumb
     if (allSiblings.length === 1) {
         return { breadcrumb: crumbArray[0].breadcrumb, children: allSiblings };
@@ -220,4 +221,4 @@ const getNextBreadCrumb = (crumbArray) => {
     }
     // all crumbs are the same, return the full crumb
     return { breadcrumb: crumbArray[0].breadcrumb, children: allSiblings };
-}
+};
