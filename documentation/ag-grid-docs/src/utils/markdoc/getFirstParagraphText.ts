74802d42ee574126edaaf3634d2bc12d8253cd17
James Swinton-Bland <james.swinton-bland@ag-grid.com>
2024-04-11 10:31:57 +0100

    AG-11282 Fix incorrect descriptions on pages with a VideoSection component

--- documentation/ag-grid-docs/src/utils/markdoc/getFirstParagraphText.ts
+++ documentation/ag-grid-docs/src/utils/markdoc/getFirstParagraphText.ts
@@ -3,18 +3,30 @@ import Markdoc from '@markdoc/markdoc';
 export function getFirstParagraphText(markdocContent: string) {
     const root = Markdoc.parse(markdocContent);
 
-    const firstParagraph = root.children.find((node) => {
-        return node.type === 'paragraph';
-    });
+    function findFirstParagraph(node) {
+        if (node.type === 'paragraph') {
+            return node;
+        }
+
+        // Handle VideoSection
+        for (let child of node.children || []) {
+            const found: boolean = findFirstParagraph(child);
+            if (found) {
+                return found;
+            }
+        }
+
+        return null;
+    }
+
+    const firstParagraph = findFirstParagraph(root);
 
     if (!firstParagraph) {
         return;
     }
 
     return firstParagraph.children[0]?.children
-        ?.map((node) => {
-            return node.attributes?.content;
-        })
+        ?.map((node) => node.attributes?.content)
         .filter(Boolean)
         .join(' ');
 }
