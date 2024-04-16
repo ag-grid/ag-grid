b85dc06023e1e3cc136a10d5f096e9cb71e803d3
Tak Tran <contact@tutaktran.com>
2024-04-16 09:58:43 +0100

    git subrepo pull external/ag-website-shared
    subrepo:
      subdir:   "external/ag-website-shared"
      merged:   "59068b5995"
    upstream:
      origin:   "git@github.com:ag-grid/ag-website-shared.git"
      branch:   "latest"
      commit:   "6b6cf06c14"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

---
+++ external/ag-website-shared/scripts/subrepo/src/lib/terminal-colors.ts
@@ -0,0 +1,11 @@
+export const TERMINAL_COLORS = {
+    reset: '\x1b[0m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+    grey: '\x1b[90m',
+};
