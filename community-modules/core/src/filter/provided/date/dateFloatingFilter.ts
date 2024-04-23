16eb0f98c294fc973739b1c711649eed2e8770a2
Peter Reynolds <peterjrreynolds@gmail.com>
2024-04-23 16:16:56 +0100

    AG-5855 - fix vue custom date comps

--- community-modules/core/src/filter/provided/date/dateFloatingFilter.ts
+++ community-modules/core/src/filter/provided/date/dateFloatingFilter.ts
@@ -122,8 +122,9 @@ export class DateFloatingFilter extends SimpleFloatingFilter {
     }
 
     private createDateComponent(): void {
-        this.dateComp = new DateCompWrapper(this.getContext(), this.userComponentFactory, this.getDateComponentParams(), this.eDateWrapper);
-        this.dateComp.setInputAriaLabel(this.getAriaLabel(this.params));
+        this.dateComp = new DateCompWrapper(this.getContext(), this.userComponentFactory, this.getDateComponentParams(), this.eDateWrapper, dateComp => {
+            dateComp.setInputAriaLabel(this.getAriaLabel(this.params));
+        });
 
         this.addDestroyFunc(() => this.dateComp.destroy());
     }
