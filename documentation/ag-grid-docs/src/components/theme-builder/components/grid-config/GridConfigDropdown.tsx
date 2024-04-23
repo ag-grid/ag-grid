a14b6b50c204baeaa821d117c14f2862339ce238
Kyler Phillips <kylerphillips@hotmail.co.uk>
2024-04-23 16:16:11 +0100

    AG-9300 Fix svg stroke markup

--- documentation/ag-grid-docs/src/components/theme-builder/components/grid-config/GridConfigDropdown.tsx
+++ documentation/ag-grid-docs/src/components/theme-builder/components/grid-config/GridConfigDropdown.tsx
@@ -59,7 +59,7 @@ const configIcon = (
             stroke="currentColor"
             strokeWidth="2"
             strokeLinecap="round"
-            stroke-linejoin="round"
+            strokeLinejoin="round"
         />
         <path
             d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z"
