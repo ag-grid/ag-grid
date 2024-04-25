10bfe65738e24a0098756638e7debc1e810c50bd
Stephen Cooper <scooperdev@gmail.com>
2024-04-25 15:12:49 +0100

    RTI-2087 Fix React Row Rendering

--- packages/ag-grid-react/src/reactUi/rows/rowContainerComp.tsx
+++ packages/ag-grid-react/src/reactUi/rows/rowContainerComp.tsx
@@ -60,8 +60,8 @@ const RowContainerComp = (params: {name: RowContainerName}) => {
             const updateRowCtrlsOrdered = (useFlushSync: boolean) => {
                 const next = getNextValueIfDifferent(prevRowCtrlsRef.current, rowCtrlsRef.current, domOrderRef.current)!;
                 if (next !== prevRowCtrlsRef.current) {
+                    prevRowCtrlsRef.current = next;
                     agFlushSync(useFlushSync, () => setRowCtrlsOrdered(next));
-                    //prevRowCtrlsRef.current = next;
                 }
             }
 
@@ -73,7 +73,6 @@ const RowContainerComp = (params: {name: RowContainerName}) => {
                 },
                 setRowCtrls: ({ rowCtrls, useFlushSync }: { rowCtrls: RowCtrl[], useFlushSync?: boolean }) => {
                     const useFlush = !!useFlushSync && rowCtrlsRef.current.length > 0 && rowCtrls.length > 0;
-                    prevRowCtrlsRef.current = rowCtrlsRef.current;
                     // Keep a record of the rowCtrls in case we need to reset the Dom order.
                     rowCtrlsRef.current = rowCtrls;
                     updateRowCtrlsOrdered(useFlush);
