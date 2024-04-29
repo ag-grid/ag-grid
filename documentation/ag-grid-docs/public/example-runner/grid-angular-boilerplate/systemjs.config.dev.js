7a4409c3c2876f36edc885fcb3faa93c41003405
Stephen Cooper <scooperdev@gmail.com>
2024-04-29 16:00:12 +0100

    Run Angular on v17

--- documentation/ag-grid-docs/public/example-runner/grid-angular-boilerplate/systemjs.config.dev.js
+++ documentation/ag-grid-docs/public/example-runner/grid-angular-boilerplate/systemjs.config.dev.js
@@ -1,5 +1,5 @@
 (function (global) {
-    var ANGULAR_VERSION = "14.2.6";
+    var ANGULAR_VERSION = "^17";
     window.ENABLE_PROD_MODE = false;
 
     System.config({
@@ -7,13 +7,13 @@
         transpiler: "ts",
         typescriptOptions: {
             // Copy of compiler options in standard tsconfig.json
-            target: "es2015",
+            target: "es2020",
             module: "system", //gets rid of console warning
             moduleResolution: "node",
             sourceMap: true,
             emitDecoratorMetadata: true,
             experimentalDecorators: true,
-            lib: ["es2015", "dom"],
+            lib: ["es2020", "dom"],
             noImplicitAny: true,
             suppressImplicitAnyIndexErrors: true
         },
@@ -31,18 +31,19 @@
         // map tells the System loader where to look for things
         map:
             {
-                '@angular/compiler': 'npm:@angular/compiler@' + ANGULAR_VERSION + '/fesm2015/compiler.mjs',
-                '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@' + ANGULAR_VERSION + '/fesm2015/platform-browser-dynamic.mjs',
-                '@angular/core': 'npm:@angular/core@' + ANGULAR_VERSION + '/fesm2015/core.mjs',
-                '@angular/common': 'npm:@angular/common@' + ANGULAR_VERSION + '/fesm2015/common.mjs',
-                '@angular/common/http': 'npm:@angular/common@' + ANGULAR_VERSION + '/fesm2015/http.mjs',
+                '@angular/compiler': 'npm:@angular/compiler@' + ANGULAR_VERSION + '/fesm2022/compiler.mjs',
+                '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@' + ANGULAR_VERSION + '/fesm2022/platform-browser-dynamic.mjs',
+                '@angular/core': 'npm:@angular/core@' + ANGULAR_VERSION + '/fesm2022/core.mjs',
+                '@angular/core/primitives/signals': 'npm:@angular/core@' + ANGULAR_VERSION + '/fesm2022/primitives/signals.mjs',
+                '@angular/common': 'npm:@angular/common@' + ANGULAR_VERSION + '/fesm2022/common.mjs',
+                '@angular/common/http': 'npm:@angular/common@' + ANGULAR_VERSION + '/fesm2022/http.mjs',
 
-                '@angular/platform-browser': 'npm:@angular/platform-browser@' + ANGULAR_VERSION + '/fesm2015/platform-browser.mjs',
-                '@angular/platform-browser/animations': 'npm:@angular/platform-browser@' + ANGULAR_VERSION + '/fesm2015/animations.mjs',
+                '@angular/platform-browser': 'npm:@angular/platform-browser@' + ANGULAR_VERSION + '/fesm2022/platform-browser.mjs',
+                '@angular/platform-browser/animations': 'npm:@angular/platform-browser@' + ANGULAR_VERSION + '/fesm2022/animations.mjs',
 
-                '@angular/forms': 'npm:@angular/forms@' + ANGULAR_VERSION + '/fesm2015/forms.mjs',
-                '@angular/animations': 'npm:@angular/animations@' + ANGULAR_VERSION + '/fesm2015/animations.mjs',
-                '@angular/animations/browser': 'npm:@angular/animations@' + ANGULAR_VERSION + '/fesm2015/browser.mjs',
+                '@angular/forms': 'npm:@angular/forms@' + ANGULAR_VERSION + '/fesm2022/forms.mjs',
+                '@angular/animations': 'npm:@angular/animations@' + ANGULAR_VERSION + '/fesm2022/animations.mjs',
+                '@angular/animations/browser': 'npm:@angular/animations@' + ANGULAR_VERSION + '/fesm2022/browser.mjs',
 
                 'rxjs': "npm:rxjs@7.8.1/dist/bundles/rxjs.umd.min.js",
                 'rxjs/operators': "npm:rxjs@7.8.1/dist/bundles/rxjs.umd.min.js",
@@ -52,7 +53,7 @@
 
                 ts: "npm:plugin-typescript@8.0.0/lib/plugin.js",
                 tslib: "npm:tslib@2.3.1/tslib.js",
-                typescript: "npm:typescript@4.3.5/lib/typescript.min.js",
+                typescript: "npm:typescript@4.4/lib/typescript.min.js",
 
                 // our app is within the app folder, appLocation comes from index.html
                 app: appLocation,
@@ -66,7 +67,7 @@
                 defaultExtension: "ts",
             },
             "@ag-grid-community/angular": {
-                main: "./fesm2015/ag-grid-community-angular.mjs",
+                main: "./fesm2020/ag-grid-community-angular.mjs",
                 defaultExtension: "mjs"
             },
             '@ag-grid-community/core': {
@@ -208,7 +209,7 @@
                 defaultExtension: 'js'
             },
             'ag-grid-angular': {
-                main: './fesm2015/ag-grid-angular.mjs',
+                main: './fesm2020/ag-grid-angular.mjs',
                 defaultExtension: 'mjs',
             },
             'ag-charts-community': {
