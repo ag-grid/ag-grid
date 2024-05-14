fa9be0cd762e03174c47694bcdadc8b3f3c53abd
Abi Glazier <abiglazier@gmail.com>
2024-05-14 15:53:54 +0100

    AG-11152 update example icons

--- documentation/ag-grid-docs/src/content/docs/component-cell-renderer/_examples/cell-renderer-summary/priceRenderer_vue.js
+++ documentation/ag-grid-docs/src/content/docs/component-cell-renderer/_examples/cell-renderer-summary/priceRenderer_vue.js
@@ -5,7 +5,7 @@ export default {
   data: function () {
       return {
           arr: [],
-          src: 'https://www.ag-grid.com/example-assets/icons/pound-coin-color-icon.png',
+          src: 'https://www.ag-grid.com/example-assets/icons/pound-coin-color-icon.svg',
           priceMultiplier: 1,
       };
   },
