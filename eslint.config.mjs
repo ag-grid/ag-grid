8256524958f85088a70a6b303bcc77fdfa808584
Tak Tran <contact@tutaktran.com>
2024-05-15 12:40:27 +0100

    AG-11597 - Add linting for ag-grid-docs

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+    { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+    pluginJs.configs.recommended,
+    ...tseslint.configs.recommended
+];
