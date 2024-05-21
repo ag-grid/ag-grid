a563ffad8a1710e61b2cddd3f83be50b793129ea
Kyler Phillips <kylerphillips@hotmail.co.uk>
2024-05-21 15:37:43 +0100

    AG-11613 Update delete inventory button

--- documentation/ag-grid-docs/src/components/showcase/examples/ecommerce-inventory/buttonCellRenderer.ts
+++ documentation/ag-grid-docs/src/components/showcase/examples/ecommerce-inventory/buttonCellRenderer.ts
@@ -7,18 +7,31 @@ ButtonCellRenderer.prototype.init = function (params) {
 
     // Create container div
     this.eGui = document.createElement('div');
+    this.eGui.classList.add('button-cell')
 
     // Create "Remove" button
     this.removeButton = document.createElement('button');
-    this.removeButton.innerHTML = 'Remove';
+    this.removeButton.innerHTML = '';
     this.removeButton.addEventListener('click', this.onRemoveClick.bind(this));
     this.eGui.appendChild(this.removeButton);
+
+    //create delete image
+    var imgElement = document.createElement('img');
+    imgElement.src = '/example/inventory/delete.svg';
+    imgElement.classList.add('deleteIcon'); // Add class to the image
+    imgElement.alt = 'Delete Icon'; // Add alt text for accessibility
+   
+    this.removeButton.insertBefore(imgElement, this.removeButton.firstChild); // Insert image before text
+    this.removeButton.classList.add('delete-button')
+   
 
     // Create "Stop selling" button
     this.stopSellingButton = document.createElement('button');
     this.stopSellingButton.innerHTML = 'Stop selling';
     this.stopSellingButton.addEventListener('click', this.onStopSellingClick.bind(this));
     this.eGui.appendChild(this.stopSellingButton);
+    this.stopSellingButton.classList.add('button-secondary')
+    this.stopSellingButton.classList.add('button-stop-selling')
 };
 
 // Mandatory method: getGui()
@@ -35,7 +48,7 @@ ButtonCellRenderer.prototype.onRemoveClick = function () {
     this.params.api.applyTransaction({ remove: [rowData] });
 
     // Show pop-up window
-    alert('Inventory deleted');
+    // alert('Inventory deleted');
 
     // Implement your button click logic here
 };
