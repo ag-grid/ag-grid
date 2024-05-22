e15bec66a3e6c2068dc23f7171d18f9c6d879e5b
Stephen Cooper <scooperdev@gmail.com>
2024-05-22 17:26:08 +0100

    remove unused refs

--- community-modules/core/src/gridBodyComp/gridBodyComp.ts
+++ community-modules/core/src/gridBodyComp/gridBodyComp.ts
@@ -15,39 +15,39 @@ import { RowContainerName } from './rowContainer/rowContainerCtrl';
 const GRID_BODY_TEMPLATE =
     /* html */
     `<div class="ag-root ag-unselectable" role="treegrid">
-        <ag-header-root data-ref="gridHeader"></ag-header-root>
+        <ag-header-root></ag-header-root>
         <div class="ag-floating-top" data-ref="eTop" role="presentation">
-            <ag-row-container data-ref="topLeftContainer" name="${RowContainerName.TOP_LEFT}"></ag-row-container>
-            <ag-row-container data-ref="topCenterContainer" name="${RowContainerName.TOP_CENTER}"></ag-row-container>
-            <ag-row-container data-ref="topRightContainer" name="${RowContainerName.TOP_RIGHT}"></ag-row-container>
-            <ag-row-container data-ref="topFullWidthContainer" name="${RowContainerName.TOP_FULL_WIDTH}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.TOP_LEFT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.TOP_CENTER}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.TOP_RIGHT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.TOP_FULL_WIDTH}"></ag-row-container>
         </div>
         <div class="ag-body" data-ref="eBody" role="presentation">
             <div class="ag-body-viewport" data-ref="eBodyViewport" role="presentation">
-                <ag-row-container data-ref="leftContainer" name="${RowContainerName.LEFT}"></ag-row-container>
-                <ag-row-container data-ref="centerContainer" name="${RowContainerName.CENTER}"></ag-row-container>
-                <ag-row-container data-ref="rightContainer" name="${RowContainerName.RIGHT}"></ag-row-container>
-                <ag-row-container data-ref="fullWidthContainer" name="${RowContainerName.FULL_WIDTH}"></ag-row-container>
+                <ag-row-container name="${RowContainerName.LEFT}"></ag-row-container>
+                <ag-row-container name="${RowContainerName.CENTER}"></ag-row-container>
+                <ag-row-container name="${RowContainerName.RIGHT}"></ag-row-container>
+                <ag-row-container name="${RowContainerName.FULL_WIDTH}"></ag-row-container>
             </div>
             <ag-fake-vertical-scroll></ag-fake-vertical-scroll>
         </div>
         <div class="ag-sticky-top" data-ref="eStickyTop" role="presentation">
-            <ag-row-container data-ref="stickyTopLeftContainer" name="${RowContainerName.STICKY_TOP_LEFT}"></ag-row-container>
-            <ag-row-container data-ref="stickyTopCenterContainer" name="${RowContainerName.STICKY_TOP_CENTER}"></ag-row-container>
-            <ag-row-container data-ref="stickyTopRightContainer" name="${RowContainerName.STICKY_TOP_RIGHT}"></ag-row-container>
-            <ag-row-container data-ref="stickyTopFullWidthContainer" name="${RowContainerName.STICKY_TOP_FULL_WIDTH}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_TOP_LEFT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_TOP_CENTER}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_TOP_RIGHT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_TOP_FULL_WIDTH}"></ag-row-container>
         </div>
         <div class="ag-sticky-bottom" data-ref="eStickyBottom" role="presentation">
-            <ag-row-container data-ref="stickyBottomLeftContainer" name="${RowContainerName.STICKY_BOTTOM_LEFT}"></ag-row-container>
-            <ag-row-container data-ref="stickyBottomCenterContainer" name="${RowContainerName.STICKY_BOTTOM_CENTER}"></ag-row-container>
-            <ag-row-container data-ref="stickyBottomRightContainer" name="${RowContainerName.STICKY_BOTTOM_RIGHT}"></ag-row-container>
-            <ag-row-container data-ref="stickyBottomFullWidthContainer" name="${RowContainerName.STICKY_BOTTOM_FULL_WIDTH}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_BOTTOM_LEFT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_BOTTOM_CENTER}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_BOTTOM_RIGHT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.STICKY_BOTTOM_FULL_WIDTH}"></ag-row-container>
         </div>
         <div class="ag-floating-bottom" data-ref="eBottom" role="presentation">
-            <ag-row-container data-ref="bottomLeftContainer" name="${RowContainerName.BOTTOM_LEFT}"></ag-row-container>
-            <ag-row-container data-ref="bottomCenterContainer" name="${RowContainerName.BOTTOM_CENTER}"></ag-row-container>
-            <ag-row-container data-ref="bottomRightContainer" name="${RowContainerName.BOTTOM_RIGHT}"></ag-row-container>
-            <ag-row-container data-ref="bottomFullWidthContainer" name="${RowContainerName.BOTTOM_FULL_WIDTH}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.BOTTOM_LEFT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.BOTTOM_CENTER}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.BOTTOM_RIGHT}"></ag-row-container>
+            <ag-row-container name="${RowContainerName.BOTTOM_FULL_WIDTH}"></ag-row-container>
         </div>
         <ag-fake-horizontal-scroll></ag-fake-horizontal-scroll>
         <ag-overlay-wrapper></ag-overlay-wrapper>
