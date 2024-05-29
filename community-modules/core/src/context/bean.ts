3f890316dbf44f3ad9760b64def06f2c8fc186c9
Peter Reynolds <peterjrreynolds@gmail.com>
2024-05-29 10:11:04 +0100

    generic context

--- community-modules/core/src/context/bean.ts
+++ community-modules/core/src/context/bean.ts
@@ -1,13 +1,4 @@
-import type { BeanCollection } from './context';
+import type { BeanCollection, BeanName } from './context';
+import type { GenericBean } from './genericBean';
 
-// to be renamed `Bean` once the annotation is removed
-export interface BaseBean {
-    /** AG Grid internal - do not call */
-    wireBeans?(beans: BeanCollection): void;
-
-    /** AG Grid internal - do not call */
-    postConstruct?(): void;
-
-    /** AG Grid internal - do not call */
-    destroy?(): void;
-}
+export interface Bean extends GenericBean<BeanName, BeanCollection> {}
