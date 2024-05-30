9ef2de4e5eaeb3bf4e3c3f7e0478b10844cbf15b
Stephen Cooper <scooperdev@gmail.com>
2024-05-30 16:43:27 +0100

    AG-11690 Maintain consistent order of core beans registration (#8030)
    * Maintain consistent order of core beans registration
    
    * rename file
    
    * Add missing core bean collection types
    
    * remove clashing interfaces for now
    
    * remove clashing interfaces for now

---
+++ community-modules/core/src/context/gridBeanComparator.ts
@@ -0,0 +1,109 @@
+import type { BeanCollection, BeanName } from './context';
+import type { GenericBean } from './genericBean';
+
+/**
+ * We know that there is a risk in a change of behaviour if beans are registered in a different order due to the way
+ * that this means that their event listeners will be registered in a different order. If they fire in a different order
+ * there is a risk that behaviour will be inconsistent.
+ * As core beans are going to become optional and could be registered in unpredictable orders via feature modules,
+ * we need to provide a consistent order for them to be registered in.
+ *
+ * We have not included beans from modules as they will be registered after the core beans in the order they are provided.
+ */
+const orderedCoreBeans: BeanName[] = [
+    // core beans only
+    'rowPositionUtils',
+    'cellPositionUtils',
+    'headerPositionUtils',
+    'paginationAutoPageSizeService',
+    'gridApi',
+    'userComponentRegistry',
+    'agComponentUtils',
+    'componentMetadataProvider',
+    'resizeObserverService',
+    'userComponentFactory',
+    'rowContainerHeightService',
+    'horizontalResizeService',
+    'localeService',
+    'validationService',
+    'pinnedRowModel',
+    'dragService',
+    'visibleColsService',
+    'eventService',
+    'gos',
+    'popupService',
+    'selectionService',
+    'filterManager',
+    'columnModel',
+    'headerNavigationService',
+    'paginationProxy',
+    'rowRenderer',
+    'expressionService',
+    'columnFactory',
+    'alignedGridsService',
+    'navigationService',
+    'valueCache',
+    'valueService',
+    'loggerFactory',
+    'autoWidthCalculator',
+    'filterMenuFactory',
+    'dragAndDropService',
+    'focusService',
+    'mouseEventService',
+    'environment',
+    'cellNavigationService',
+    'stylingService',
+    'scrollVisibleService',
+    'sortController',
+    'columnHoverService',
+    'columnAnimationService',
+    'selectableService',
+    'autoColService',
+    'changeDetectionService',
+    'animationFrameService',
+    'undoRedoService',
+    'agStackComponentsRegistry',
+    'columnDefFactory',
+    'rowCssClassCalculator',
+    'rowNodeBlockLoader',
+    'rowNodeSorter',
+    'ctrlsService',
+    'pinnedWidthService',
+    'rowNodeEventThrottle',
+    'ctrlsFactory',
+    'dataTypeService',
+    'quickFilterService',
+    'syncService',
+    'overlayService',
+    'stateService',
+    'expansionService',
+    'apiEventService',
+    'ariaAnnouncementService',
+    'menuService',
+    'columnApplyStateService',
+    'columnEventDispatcher',
+    'columnMoveService',
+    'columnAutosizeService',
+    'columnGetStateService',
+    'columnGroupStateService',
+    'columnSizeService',
+    'funcColsService',
+    'columnNameService',
+    'columnViewportService',
+    'pivotResultColsService',
+    'showRowGroupColsService',
+];
+
+const beanNamePosition: { [key in BeanName]?: number } = Object.fromEntries(
+    orderedCoreBeans.map((beanName, index) => [beanName, index])
+);
+
+export function gridBeanComparator(
+    bean1: GenericBean<BeanName, BeanCollection>,
+    bean2: GenericBean<BeanName, BeanCollection>
+): number {
+    // if the beans are not in the ordered list, just ensure they are after the ordered beans and stable to provided order
+    const index1 = (bean1.beanName ? beanNamePosition[bean1.beanName] : undefined) ?? Number.MAX_SAFE_INTEGER;
+    const index2 = (bean2.beanName ? beanNamePosition[bean2.beanName] : undefined) ?? Number.MAX_SAFE_INTEGER;
+    return index1 - index2;
+}
