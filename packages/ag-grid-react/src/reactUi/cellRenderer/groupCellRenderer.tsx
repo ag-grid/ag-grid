66e0715e2b518655517791f3cf6350fa973f5fd1
Stephen Cooper <scooperdev@gmail.com>
2024-06-04 11:02:01 +0100

    Make react comp work without group ctrl

--- packages/ag-grid-react/src/reactUi/cellRenderer/groupCellRenderer.tsx
+++ packages/ag-grid-react/src/reactUi/cellRenderer/groupCellRenderer.tsx
@@ -73,16 +73,19 @@ const GroupCellRenderer = forwardRef((props: GroupCellRendererParams, ref) => {
             setCheckboxVisible: (visible) => setCheckboxCssClasses((prev) => prev.setClass('ag-invisible', !visible)),
         };
 
-        ctrlRef.current = ctrlsFactory.getInstance('groupCellRendererCtrl') as GroupCellRendererCtrl;
-        ctrlRef.current.init(
-            compProxy,
-            eGui.current,
-            eCheckboxRef.current!,
-            eExpandedRef.current!,
-            eContractedRef.current!,
-            GroupCellRenderer,
-            props
-        );
+        const groupCellRendererCtrl = ctrlsFactory.getInstance('groupCellRendererCtrl') as GroupCellRendererCtrl;
+        if (groupCellRendererCtrl) {
+            ctrlRef.current = context.createBean(groupCellRendererCtrl);
+            ctrlRef.current.init(
+                compProxy,
+                eGui.current,
+                eCheckboxRef.current!,
+                eExpandedRef.current!,
+                eContractedRef.current!,
+                GroupCellRenderer,
+                props
+            );
+        }
     }, []);
 
     const className = useMemo(() => `ag-cell-wrapper ${cssClasses.toString()}`, [cssClasses]);
