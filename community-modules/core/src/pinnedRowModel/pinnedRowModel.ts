629abe21d0a8b564522f6e9613552adbedd9b357
Elias Malik <elias.malik@ag-grid.com>
2024-06-04 15:06:05 +0100

    use `getCallback` instead of `get` to take advantage of wrapping

--- community-modules/core/src/pinnedRowModel/pinnedRowModel.ts
+++ community-modules/core/src/pinnedRowModel/pinnedRowModel.ts
@@ -114,8 +114,7 @@ export class PinnedRowModel extends BeanStub implements NamedBean {
     private createNodesFromData(allData: any[] | undefined, isTop: boolean): RowNode[] {
         const rowNodes: RowNode[] = [];
         if (allData) {
-            const getRowId = this.gos.get('getRowId');
-            const goContext = this.gos.get('context');
+            const getRowId = this.gos.getCallback('getRowId');
             const idPrefix = isTop ? RowNode.ID_PREFIX_TOP_PINNED : RowNode.ID_PREFIX_BOTTOM_PINNED;
 
             let nextRowTop = 0;
@@ -123,9 +122,7 @@ export class PinnedRowModel extends BeanStub implements NamedBean {
                 const rowNode = new RowNode(this.beans);
                 rowNode.data = dataItem;
 
-                rowNode.id =
-                    getRowId?.({ data: dataItem, level: 0, api: this.beans.gridApi, context: goContext }) ??
-                    idPrefix + index;
+                rowNode.id = getRowId?.({ data: dataItem, level: 0 }) ?? idPrefix + index;
 
                 rowNode.rowPinned = isTop ? 'top' : 'bottom';
                 rowNode.setRowTop(nextRowTop);
