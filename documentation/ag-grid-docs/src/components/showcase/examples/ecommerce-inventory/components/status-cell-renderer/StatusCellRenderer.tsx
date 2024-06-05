aa9930ee0629f999082dda56d34356f27aebf4cb
Kyler Phillips <kylerphillips@hotmail.co.uk>
2024-06-05 12:45:42 +0100

    AG-11613 Add status color tags to inventory

--- documentation/ag-grid-docs/src/components/showcase/examples/ecommerce-inventory/components/status-cell-renderer/StatusCellRenderer.tsx
+++ documentation/ag-grid-docs/src/components/showcase/examples/ecommerce-inventory/components/status-cell-renderer/StatusCellRenderer.tsx
@@ -6,11 +6,32 @@ import styles from './StatusCellRenderer.module.css';
 
 export const StatusCellRenderer: FunctionComponent<CustomCellRendererProps> = ({ data }) => {
     const status = data.status;
-    const activeClass = status === 'active' ? styles.activeTag : '';
+    let containerClass: string = '';
+    let circleClass: string = '';
+
+    // Assign different classes based on the status
+    switch (status) {
+        case 'Out of Stock':
+            containerClass = styles.outOfStockTag;
+            circleClass = styles.outOfStockCircle;
+            break;
+        case 'Active':
+            containerClass = styles.activeTag;
+            circleClass = styles.activeCircle;
+            break;
+        case 'Paused':
+            containerClass = styles.pausedTag;
+            circleClass = styles.pausedCircle;
+            break;
+        default:
+            containerClass = styles.defaultTag; // Optional: for statuses not listed
+            circleClass = styles.defaultCircle;
+    }
 
     return (
-        <div className={`${styles.statusCell} ${activeClass}`}>
-            <div>{status}</div>
+        <div className={`${styles.tag} ${containerClass}`}>
+            <div className={`${styles.circle} ${circleClass}`}></div>
+            <span>{status}</span>
         </div>
     );
 };
