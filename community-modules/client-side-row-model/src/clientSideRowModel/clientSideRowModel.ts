e1c3697e51cd30bc75676b8a1cb870374b11dd8e
Andrew Glazier <andrew.glazier@ag-grid.com>
2024-06-10 12:34:20 +0100

    AG-11838 - Fix CSRM total row count

--- community-modules/client-side-row-model/src/clientSideRowModel/clientSideRowModel.ts
+++ community-modules/client-side-row-model/src/clientSideRowModel/clientSideRowModel.ts
@@ -497,8 +497,10 @@ export class ClientSideRowModel extends BeanStub implements IClientSideRowModel,
             return 1;
         }
 
+        const totalFooterInc = this.rootNode.sibling ? 1 : 0;
+
         const filteredChildren = this.rootNode.childrenAfterAggFilter;
-        return filteredChildren ? filteredChildren.length : 0;
+        return (filteredChildren ? filteredChildren.length : 0) + totalFooterInc;
     }
 
     public getTopLevelRowDisplayedIndex(topLevelIndex: number): number {
