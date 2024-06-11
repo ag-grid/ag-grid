2a4fd0bc3f8d29e5e7ae52b1953625c7e8be7080
seanlandsman <sean@thelandsmans.com>
2024-06-11 14:26:22 +0100

    git subrepo pull external/ag-shared
    subrepo:
      subdir:   "external/ag-shared"
      merged:   "301baa5b37"
    upstream:
      origin:   "https://github.com/ag-grid/ag-shared.git"
      branch:   "latest"
      commit:   "301baa5b37"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

---
+++ external/ag-shared/src/interfaces.ts
@@ -0,0 +1,11 @@
+export type AgChartThemeName =
+    | 'ag-default'
+    | 'ag-default-dark'
+    | 'ag-sheets'
+    | 'ag-sheets-dark'
+    | 'ag-polychroma'
+    | 'ag-polychroma-dark'
+    | 'ag-vivid'
+    | 'ag-vivid-dark'
+    | 'ag-material'
+    | 'ag-material-dark';
