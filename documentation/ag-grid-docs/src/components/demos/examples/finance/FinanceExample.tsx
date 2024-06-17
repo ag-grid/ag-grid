ac82f2b695bbcc3f533534f7b69f4655ed5d1449
rmc-software <rmc.software.consultancy@gmail.com>
2024-06-17 17:31:33 +0100

    AG-11613 - initial finance demo example restructure

--- documentation/ag-grid-docs/src/components/demos/examples/finance/FinanceExample.tsx
+++ documentation/ag-grid-docs/src/components/demos/examples/finance/FinanceExample.tsx
@@ -1,235 +1,141 @@
-import {
-    type ColDef,
-    type GetRowIdFunc,
-    type GetRowIdParams,
-    type TooltipRendererParams,
-    type TooltipRendererResult,
-} from '@ag-grid-community/core';
+import { type ColDef, type GetRowIdFunc, type GetRowIdParams, type ValueFormatterFunc } from '@ag-grid-community/core';
 import { AgGridReact } from '@ag-grid-community/react';
-import classNames from 'classnames';
-import { type FunctionComponent, useCallback, useRef, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 import styles from './FinanceExample.module.css';
-import { TickerCellRenderer } from './cell-renderers/ticker-cell-renderer/TickerCellRenderer';
-import { generatePortfolio } from './data';
-import { toCurrency, toTime } from './formatters';
-import { type PortfolioItem } from './types';
-import { useDataGenerator } from './useDataGenerator';
-import { currencyFormatter, percentageFormatter } from './utils/valueFormatters';
-import {
-    calculate52wChange,
-    costCalculator,
-    pnlCalculator,
-    pnlPercentCalculator,
-    valueCalculator,
-} from './utils/valueGetters';
+import { getData } from './data';
 
 interface Props {
     gridTheme?: string;
     isDarkMode?: boolean;
 }
 
-const FinanceExample: FunctionComponent<Props> = ({ gridTheme = 'ag-theme-quartz', isDarkMode }) => {
-    const timelineTooltipRenderer = ({ xValue, yValue }: TooltipRendererParams): TooltipRendererResult => {
-        return {
-            title: toTime({ value: xValue }),
-            content: toCurrency({ value: yValue }),
-            color: '#94b2d0',
-            backgroundColor: '#07161b',
-        };
-    };
-
-    const gridRef = useRef<AgGridReact>(null);
-    const [rowData, setRowData] = useState<PortfolioItem[]>(generatePortfolio());
-    const [colDefs] = useState<ColDef[]>([
-        {
-            headerName: 'Symbol',
-            field: 'ticker',
-            cellDataType: 'text',
-            pinned: 'left',
-            width: 150,
-            cellRenderer: TickerCellRenderer,
-        },
-        {
-            headerName: 'P/L',
-            type: 'rightAligned',
-            valueGetter: pnlCalculator,
-            cellDataType: 'number',
-            cellClass: (params) => {
-                return classNames({
-                    'cell-green': params.value > 0,
-                    'cell-red': params.value <= 0,
-                });
-            },
-            valueFormatter: currencyFormatter,
-            width: 140,
-            pivot: true,
-            aggFunc: 'sum',
-            suppressHeaderFilterButton: true,
-            suppressHeaderMenuButton: true,
-        },
-        {
-            headerName: 'Change',
-            field: 'timeline',
-            cellRenderer: 'agSparklineCellRenderer',
-            cellRendererParams: {
-                sparklineOptions: {
-                    type: 'column',
-                    xKey: 'time',
-                    yKey: 'value',
-                    padding: {
-                        top: 2,
-                        bottom: 10,
-                    },
-                    paddingInner: 0.5,
-                    paddingOuter: 0.5,
-                    fill: '#65819c',
-                    line: {
-                        stroke: 'rgb(185,173,77)',
-                    },
-                    highlightStyle: {
-                        fill: '#D7E4F2',
-                        strokeWidth: 0,
-                    },
-                    tooltip: {
-                        renderer: timelineTooltipRenderer,
-                    },
-                    axis: {
-                        stroke: 'rgba(0,0,0,0.2)', // sets the axis line stroke
-                        strokeWidth: 1, // sets the axis line strokeWidth
-                    },
-                    crosshairs: {
-                        xLine: {
-                            enabled: true,
-                            lineDash: 'dash',
-                            stroke: '#999',
-                        },
-                        yLine: {
-                            enabled: true,
-                            lineDash: 'dash',
-                            stroke: '#999',
-                        },
-                    },
-                },
-            },
-            flex: 1,
-            suppressHeaderFilterButton: true,
-            sortable: false,
-        },
-        {
-            headerName: 'Last',
-            type: 'rightAligned',
-            cellDataType: 'number',
-            field: 'currentPrice',
-            valueFormatter: currencyFormatter,
-            width: 120,
-            aggFunc: 'avg',
-            suppressHeaderFilterButton: true,
-        },
-
-        {
-            headerName: 'P/L %',
-            type: 'rightAligned',
-            valueGetter: pnlPercentCalculator,
-            valueFormatter: percentageFormatter,
-            width: 100,
-            aggFunc: 'avg',
-            suppressHeaderFilterButton: true,
-        },
-        {
-            headerName: 'Instrument',
-            field: 'instrument',
-            cellDataType: 'text',
-            rowGroup: true,
-            hide: true,
-            width: 150,
-            suppressHeaderFilterButton: true,
-        },
-        {
-            headerName: 'CCY',
-            field: 'ccy',
-            cellDataType: 'text',
-            enableRowGroup: true,
-            width: 100,
-            filter: 'agSetColumnFilter',
-            suppressHeaderMenuButton: true,
-        },
-        {
-            headerName: 'Purchase Date',
-            field: 'buyDate',
-            cellDataType: 'dateString',
-            width: 180,
-            filter: 'agDateColumnFilter',
-            suppressHeaderMenuButton: true,
-        },
-        {
-            headerName: 'Quantity',
-            type: 'rightAligned',
-            field: 'quantity',
-            cellDataType: 'number',
-            width: 120,
-            suppressHeaderFilterButton: true,
-        },
-        {
-            headerName: 'Cost Price',
-            type: 'rightAligned',
-            field: 'buyPrice',
-            cellDataType: 'number',
-            valueFormatter: currencyFormatter,
-            width: 120,
-            aggFunc: 'avg',
-            suppressHeaderFilterButton: true,
-        },
-        {
-            headerName: 'Total Cost',
-            type: 'rightAligned',
-            cellDataType: 'number',
-            valueGetter: costCalculator,
-            valueFormatter: currencyFormatter,
-            width: 150,
-            aggFunc: 'sum',
-            filter: 'agNumberColumnFilter',
-        },
-        {
-            headerName: 'Total Value',
-            type: 'rightAligned',
-            cellDataType: 'number',
-            valueGetter: valueCalculator,
-            valueFormatter: currencyFormatter,
-            width: 150,
-            aggFunc: 'sum',
-            suppressHeaderFilterButton: true,
-            cellRenderer: 'agAnimateShowChangeCellRenderer',
-        },
-        {
-            headerName: '52w Change %',
-            type: 'rightAligned',
-            valueGetter: calculate52wChange,
-            valueFormatter: percentageFormatter,
-            width: 150,
-            cellClass: (params) => {
-                return classNames({
-                    'cell-green': params.value > 0,
-                    'cell-red': params.value <= 0,
-                });
-            },
-            aggFunc: 'avg',
-            suppressHeaderFilterButton: true,
-            suppressHeaderContextMenu: true,
-        },
-    ]);
-    const getRowId = useCallback<GetRowIdFunc>((params: GetRowIdParams) => {
-        return params.data.ticker;
-    }, []);
-    const generator = useDataGenerator({
-        gridRef,
-        rowData,
-        setRowData,
+const numberFormatter: ValueFormatterFunc = (params) => {
+    const formatter = new Intl.NumberFormat('en-US', {
+        style: 'decimal',
+        maximumFractionDigits: 2,
     });
-    const onGridReady = useCallback(() => {
-        generator.start();
+    return params.value == null ? '' : formatter.format(params.value);
+};
+
+const FinanceExample: React.FC<Props> = ({ gridTheme = 'ag-theme-quartz', isDarkMode = false }) => {
+    const [rowData, setRowData] = useState(getData);
+    const gridRef = useRef<AgGridReact>(null);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setRowData((rowData) =>
+                rowData.map((item) =>
+                    Math.random() < 0.1
+                        ? {
+                              ...item,
+                              price:
+                                  item.price +
+                                  item.price * ((Math.random() * 4 + 1) / 100) * (Math.random() > 0.5 ? 1 : -1),
+                          }
+                        : item
+                )
+            );
+        }, 1000);
+
+        return () => clearInterval(intervalId);
     }, []);
-    const themeClass = isDarkMode ? `${gridTheme}-dark` : gridTheme;
+
+    const colDefs = useMemo<ColDef[]>(
+        () => [
+            {
+                field: 'ticker',
+                cellDataType: 'text',
+                maxWidth: 140,
+            },
+            {
+                field: 'name',
+                cellDataType: 'text',
+                hide: true,
+            },
+            {
+                field: 'instrument',
+                cellDataType: 'text',
+                rowGroup: true,
+                hide: true,
+            },
+            {
+                headerName: 'P&L',
+                cellDataType: 'number',
+                type: 'rightAligned',
+                cellRenderer: 'agAnimateShowChangeCellRenderer',
+                valueGetter: (params) =>
+                    params.data && params.data.quantity * (params.data.price / params.data.purchasePrice),
+                valueFormatter: numberFormatter,
+                aggFunc: 'sum',
+            },
+            {
+                headerName: 'Total Value',
+                type: 'rightAligned',
+                cellDataType: 'number',
+                valueGetter: (params) => params.data && params.data.quantity * params.data.price,
+                cellRenderer: 'agAnimateShowChangeCellRenderer',
+                valueFormatter: numberFormatter,
+                aggFunc: 'sum',
+            },
+            {
+                field: 'quantity',
+                cellDataType: 'number',
+                maxWidth: 140,
+                type: 'rightAligned',
+                valueFormatter: numberFormatter,
+            },
+            {
+                field: 'purchasePrice',
+                cellDataType: 'number',
+                maxWidth: 140,
+                type: 'rightAligned',
+                valueFormatter: numberFormatter,
+            },
+            {
+                field: 'purchaseDate',
+                cellDataType: 'dateString',
+                type: 'rightAligned',
+            },
+            {
+                headerName: 'Last 24hrs',
+                field: 'last24',
+                maxWidth: 500,
+                cellRenderer: 'agSparklineCellRenderer',
+            },
+        ],
+        []
+    );
+
+    const defaultColDef: ColDef = useMemo(
+        () => ({
+            flex: 1,
+            minWidth: 140,
+            maxWidth: 180,
+            filter: true,
+            floatingFilter: true,
+            enableRowGroup: true,
+            enableValue: true,
+        }),
+        []
+    );
+
+    const getRowId = useCallback<GetRowIdFunc>((params: GetRowIdParams) => params.data.ticker, []);
+
+    const statusBar = useMemo(
+        () => ({
+            statusPanels: [
+                { statusPanel: 'agTotalAndFilteredRowCountComponent' },
+                { statusPanel: 'agTotalRowCountComponent' },
+                { statusPanel: 'agFilteredRowCountComponent' },
+                { statusPanel: 'agSelectedRowCountComponent' },
+                { statusPanel: 'agAggregationComponent' },
+            ],
+        }),
+        []
+    );
+
+    const themeClass = `${gridTheme}${isDarkMode ? '-dark' : ''}`;
 
     return (
         <div className={styles.wrapper}>
@@ -237,29 +143,17 @@ const FinanceExample: FunctionComponent<Props> = ({ gridTheme = 'ag-theme-quartz
                 <div className={`${themeClass} ${styles.grid}`}>
                     <AgGridReact
                         ref={gridRef}
-                        rowData={rowData}
-                        onGridReady={onGridReady}
                         getRowId={getRowId}
+                        rowData={rowData}
                         columnDefs={colDefs}
-                        defaultColDef={{
-                            resizable: true,
-                        }}
+                        defaultColDef={defaultColDef}
                         enableRangeSelection={true}
                         enableCharts={true}
-                        rowSelection="multiple"
-                        suppressAggFuncInHeader={true}
-                        statusBar={{
-                            statusPanels: [
-                                { statusPanel: 'agTotalAndFilteredRowCountComponent' },
-                                { statusPanel: 'agTotalRowCountComponent' },
-                                { statusPanel: 'agFilteredRowCountComponent' },
-                                { statusPanel: 'agSelectedRowCountComponent' },
-                                { statusPanel: 'agAggregationComponent' },
-                            ],
-                        }}
-                        groupDisplayType="groupRows"
-                        groupDefaultExpanded={1}
+                        rowSelection={'multiple'}
                         rowGroupPanelShow={'always'}
+                        suppressAggFuncInHeader={true}
+                        groupDefaultExpanded={-1}
+                        statusBar={statusBar}
                     />
                 </div>
             </div>
