9a9bc984465190aefc337f74ca57cdd8b6b4f791
Salvatore Previti <salvatore.previti@ag-grid.com>
2024-06-24 15:32:28 +0100

    AG-5497 [Overlays] the priority of suppressLoadingOverlay should be lower than setting loading=true (#8126)
    * change the logic of the deprecated suppressLoadingOverlay property to have less priority than loading=true, as per ticket

--- community-modules/core/src/rendering/overlays/overlayService.ts
+++ community-modules/core/src/rendering/overlays/overlayService.ts
@@ -4,12 +4,14 @@ import type { NamedBean } from '../../context/bean';
 import { BeanStub } from '../../context/beanStub';
 import type { BeanCollection } from '../../context/context';
 import type { GridOptions } from '../../entities/gridOptions';
-import type { WithoutGridCommon } from '../../interfaces/iCommon';
 import type { IRowModel } from '../../interfaces/iRowModel';
-import { _warnOnce } from '../../utils/function';
-import type { ILoadingOverlayParams } from './loadingOverlayComponent';
-import type { INoRowsOverlayParams } from './noRowsOverlayComponent';
 import type { OverlayWrapperComponent } from './overlayWrapperComponent';
+
+const enum OverlayServiceState {
+    Hidden = 0,
+    Loading = 1,
+    NoRows = 2,
+}
 
 export class OverlayService extends BeanStub implements NamedBean {
     beanName = 'overlayService' as const;
@@ -18,6 +20,9 @@ export class OverlayService extends BeanStub implements NamedBean {
     private rowModel: IRowModel;
     private columnModel: ColumnModel;
 
+    private state: OverlayServiceState = OverlayServiceState.Hidden;
+    private showInitialOverlay: boolean = true;
+
     public wireBeans(beans: BeanCollection): void {
         this.userComponentFactory = beans.userComponentFactory;
         this.rowModel = beans.rowModel;
@@ -25,15 +30,15 @@ export class OverlayService extends BeanStub implements NamedBean {
     }
 
     private overlayWrapperComp: OverlayWrapperComponent;
-    private manuallyDisplayed: boolean = false;
 
     public postConstruct(): void {
         const updateOverlayVisibility = () => this.updateOverlayVisibility();
 
         this.addManagedEventListeners({
-            newColumnsLoaded: () => this.onNewColumnsLoaded(),
+            newColumnsLoaded: updateOverlayVisibility,
             rowDataUpdated: updateOverlayVisibility,
         });
+
         this.addManagedPropertyListener('loading', updateOverlayVisibility);
     }
 
@@ -43,31 +48,83 @@ export class OverlayService extends BeanStub implements NamedBean {
     }
 
     public showLoadingOverlay(): void {
-        if (this.gos.get('suppressLoadingOverlay')) {
-            return;
-        }
-        if (this.gos.get('loading') === false) {
+        this.showInitialOverlay = false;
+
+        const loading = this.gos.get('loading');
+        if (!loading && (loading !== undefined || this.gos.get('suppressLoadingOverlay'))) {
             return;
         }
 
-        const params: WithoutGridCommon<ILoadingOverlayParams> = {};
-
-        const compDetails = this.userComponentFactory.getLoadingOverlayCompDetails(params);
-        this.showOverlay(compDetails, 'ag-overlay-loading-wrapper', 'loadingOverlayComponentParams');
+        this.doShowLoadingOverlay();
     }
 
     public showNoRowsOverlay(): void {
-        if (this.gos.get('suppressNoRowsOverlay')) {
+        this.showInitialOverlay = false;
+
+        if (this.gos.get('loading') || this.gos.get('suppressNoRowsOverlay')) {
             return;
         }
-        if (this.gos.get('loading') && !this.gos.get('suppressLoadingOverlay')) {
-            return; // loading property is true, we cannot show the no-rows overlay
+
+        this.doShowNoRowsOverlay();
+    }
+
+    public hideOverlay(): void {
+        this.showInitialOverlay = false;
+
+        if (this.gos.get('loading')) {
+            return;
         }
 
-        const params: WithoutGridCommon<INoRowsOverlayParams> = {};
+        this.doHideOverlay();
+    }
 
-        const compDetails = this.userComponentFactory.getNoRowsOverlayCompDetails(params);
-        this.showOverlay(compDetails, 'ag-overlay-no-rows-wrapper', 'noRowsOverlayComponentParams');
+    private updateOverlayVisibility(): void {
+        let loading = this.gos.get('loading');
+
+        if (this.showInitialOverlay && loading === undefined && !this.gos.get('suppressLoadingOverlay')) {
+            loading =
+                !this.gos.get('columnDefs') ||
+                !this.columnModel.isReady() ||
+                (!this.gos.get('rowData') && this.gos.isRowModelType('clientSide'));
+        }
+
+        if (loading) {
+            if (this.state !== OverlayServiceState.Loading) {
+                this.doShowLoadingOverlay();
+            }
+        } else {
+            this.showInitialOverlay = false;
+            if (this.rowModel.isEmpty() && !this.gos.get('suppressNoRowsOverlay')) {
+                if (this.state !== OverlayServiceState.NoRows) {
+                    this.doShowNoRowsOverlay();
+                }
+            } else if (this.state !== OverlayServiceState.Hidden) {
+                this.doHideOverlay();
+            }
+        }
+    }
+
+    private doShowLoadingOverlay(): void {
+        this.state = OverlayServiceState.Loading;
+        this.showOverlay(
+            this.userComponentFactory.getLoadingOverlayCompDetails({}),
+            'ag-overlay-loading-wrapper',
+            'loadingOverlayComponentParams'
+        );
+    }
+
+    private doShowNoRowsOverlay(): void {
+        this.state = OverlayServiceState.NoRows;
+        this.showOverlay(
+            this.userComponentFactory.getNoRowsOverlayCompDetails({}),
+            'ag-overlay-no-rows-wrapper',
+            'noRowsOverlayComponentParams'
+        );
+    }
+
+    private doHideOverlay(): void {
+        this.state = OverlayServiceState.Hidden;
+        this.overlayWrapperComp.hideOverlay();
     }
 
     private showOverlay(compDetails: UserCompDetails, wrapperCssClass: string, gridOption: keyof GridOptions): void {
@@ -84,47 +141,6 @@ export class OverlayService extends BeanStub implements NamedBean {
             });
         });
 
-        this.manuallyDisplayed = this.columnModel.isReady() && !this.rowModel.isEmpty();
         this.overlayWrapperComp.showOverlay(promise, wrapperCssClass, listenerDestroyFunc);
-    }
-
-    public hideOverlay(): void {
-        if (this.gos.get('loading') && !this.gos.get('suppressLoadingOverlay')) {
-            return; // loading property is true, we cannot hide the overlay
-        }
-        this.manuallyDisplayed = false;
-        this.overlayWrapperComp.hideOverlay();
-    }
-
-    private updateOverlayVisibility(): void {
-        let loadingVisible = false;
-
-        const loading = this.gos.get('loading');
-        if (!this.gos.get('suppressLoadingOverlay')) {
-            if (loading) {
-                loadingVisible = true;
-            } else if (loading === undefined) {
-                loadingVisible =
-                    !this.gos.get('columnDefs') || (this.gos.isRowModelType('clientSide') && !this.gos.get('rowData'));
-            }
-        }
-
-        if (loadingVisible) {
-            this.showLoadingOverlay();
-        } else if (this.rowModel.isEmpty() && !this.gos.get('suppressNoRowsOverlay')) {
-            this.showNoRowsOverlay();
-        } else {
-            this.hideOverlay();
-        }
-    }
-
-    private onNewColumnsLoaded(): void {
-        // hide overlay if columns and rows exist, this can happen if columns are loaded after data.
-        // this problem exists before of the race condition between the services (column controller in this case)
-        // and the view (grid panel). if the model beans were all initialised first, and then the view beans second,
-        // this race condition would not happen.
-        if (this.columnModel.isReady() && !this.rowModel.isEmpty() && !this.manuallyDisplayed) {
-            this.updateOverlayVisibility();
-        }
     }
 }
