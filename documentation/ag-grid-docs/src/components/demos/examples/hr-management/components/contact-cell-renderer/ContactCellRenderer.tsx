6f71dfa6b9c029e907ba8c8deb23708a829d432e
Mark Durrant <mark.j.durrant@gmail.com>
2024-06-25 12:42:01 +0100

    AG-11617 - Update mailtoo links with employee names for HR demo

--- documentation/ag-grid-docs/src/components/demos/examples/hr-management/components/contact-cell-renderer/ContactCellRenderer.tsx
+++ documentation/ag-grid-docs/src/components/demos/examples/hr-management/components/contact-cell-renderer/ContactCellRenderer.tsx
@@ -4,24 +4,28 @@ import { type FunctionComponent } from 'react';
 
 import styles from './ContactCellRenderer.module.css';
 
-export const ContactCellRenderer: FunctionComponent<CustomCellRendererProps> = ({ value }) => (
-    <div className={styles.contactCell}>
-        <div className={styles.iconContainer}>
-            <button className="button-secondary">
-                <a
-                    href={`https://www.linkedin.com/company/ag-grid/`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className={styles.iconLink}
-                >
-                    <img className={styles.icon} src={getResourceUrl(`/example/hr/linkedin.svg`)} alt="linkedin" />
-                </a>
-            </button>
-            <button className="button-secondary">
-                <a href={`mailto:${value}@company.com`} className={styles.iconLink}>
-                    <img className={styles.icon} src={getResourceUrl(`/example/hr/email.svg`)} alt="email" />
-                </a>
-            </button>
+export const ContactCellRenderer: FunctionComponent<CustomCellRendererProps> = ({ data }) => {
+    const emailName = data.orgHierarchy.at(-1).toLowerCase().replace(' ', '.');
+
+    return (
+        <div className={styles.contactCell}>
+            <div className={styles.iconContainer}>
+                <button className="button-secondary">
+                    <a
+                        href={`https://www.linkedin.com/company/ag-grid/`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className={styles.iconLink}
+                    >
+                        <img className={styles.icon} src={getResourceUrl(`/example/hr/linkedin.svg`)} alt="linkedin" />
+                    </a>
+                </button>
+                <button className="button-secondary">
+                    <a href={`mailto:${emailName}@company.com`} className={styles.iconLink}>
+                        <img className={styles.icon} src={getResourceUrl(`/example/hr/email.svg`)} alt="email" />
+                    </a>
+                </button>
+            </div>
         </div>
-    </div>
-);
+    );
+};
