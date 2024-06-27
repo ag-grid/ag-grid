af5bf3f3e731870eb3de34880c8141e159aa8718
Salvatore Previti <salvatore.previti@ag-grid.com>
2024-06-27 16:38:21 +0100

    fix norows regression with infinite row model, fix tests

---
+++ testing/behavioural/src/overlays/overlays-infinite-scrolling.test.tsx
@@ -0,0 +1,70 @@
+import type { GridOptions } from '@ag-grid-community/core';
+import { ModuleRegistry, createGrid } from '@ag-grid-community/core';
+import { InfiniteRowModelModule } from '@ag-grid-community/infinite-row-model';
+
+describe('ag-grid overlays infinite scrolling state', () => {
+    const columnDefs = [{ field: 'athlete' }, { field: 'sport' }, { field: 'age' }];
+    let consoleWarnSpy: jest.SpyInstance;
+
+    function createMyGrid(gridOptions: GridOptions) {
+        return createGrid(document.getElementById('myGrid')!, gridOptions);
+    }
+
+    function resetGrids() {
+        document.body.innerHTML = '<div id="myGrid"></div>';
+    }
+
+    function hasLoadingOverlay() {
+        return !!document.querySelector('.ag-overlay-loading-center');
+    }
+
+    function hasNoRowsOverlay() {
+        return !!document.querySelector('.ag-overlay-no-rows-center');
+    }
+
+    beforeAll(() => {
+        ModuleRegistry.register(InfiniteRowModelModule);
+    });
+
+    beforeEach(() => {
+        consoleWarnSpy = jest.spyOn(console, 'warn').mockImplementation(() => {});
+
+        resetGrids();
+    });
+
+    afterEach(() => {
+        consoleWarnSpy.mockRestore();
+    });
+
+    test('does not shows no-rows when using InfiniteRowModelModule', () => {
+        const pendingGetRows: (() => void)[] = [];
+
+        createMyGrid({
+            columnDefs,
+            rowModelType: 'infinite',
+            datasource: { getRows: (params) => pendingGetRows.push(() => params.successCallback([], 0)) },
+        });
+
+        expect(hasLoadingOverlay()).toBe(false);
+        expect(hasNoRowsOverlay()).toBe(false);
+
+        for (const pending of pendingGetRows) {
+            pending();
+        }
+
+        expect(hasLoadingOverlay()).toBe(false);
+        expect(hasNoRowsOverlay()).toBe(false);
+    });
+
+    test('it does show loading if forced', () => {
+        createMyGrid({
+            columnDefs,
+            rowModelType: 'infinite',
+            datasource: { getRows: () => {} },
+            loading: true,
+        });
+
+        expect(hasLoadingOverlay()).toBe(true);
+        expect(hasNoRowsOverlay()).toBe(false);
+    });
+});
