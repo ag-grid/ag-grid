c4c6fe98a9c147751b9c05a34d19f66e593d8972
Pete Reynolds <40919976+peterjrreynolds@users.noreply.github.com>
2024-06-28 13:29:24 +0100

    RTI-2125 - fix multi filter focus (#8189)

--- documentation/ag-grid-docs/src/content/docs/filter-multi/_examples/custom-filter/provided/modules/reactFunctionalTs/YearFilter.tsx
+++ documentation/ag-grid-docs/src/content/docs/filter-multi/_examples/custom-filter/provided/modules/reactFunctionalTs/YearFilter.tsx
@@ -1,20 +1,37 @@
-import { IDoesFilterPassParams } from '@ag-grid-community/core';
+import { IAfterGuiAttachedParams, IDoesFilterPassParams } from '@ag-grid-community/core';
 import { CustomFilterProps, useGridFilter } from '@ag-grid-community/react';
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 export default ({ model, onModelChange }: CustomFilterProps) => {
+    const refInput = useRef<HTMLInputElement>(null);
+
     const doesFilterPass = useCallback((params: IDoesFilterPassParams) => {
         return params.data.year >= 2004;
     }, []);
 
+    const afterGuiAttached = useCallback((params?: IAfterGuiAttachedParams) => {
+        if (!params || !params.suppressFocus) {
+            refInput.current?.focus();
+        }
+    }, []);
+
     useGridFilter({
         doesFilterPass,
+        afterGuiAttached,
     });
 
     return (
         <div className="year-filter">
             <label>
-                <input type="radio" name="year" value="All" checked={!model} onChange={() => onModelChange(null)} /> All
+                <input
+                    ref={refInput}
+                    type="radio"
+                    name="year"
+                    value="All"
+                    checked={!model}
+                    onChange={() => onModelChange(null)}
+                />{' '}
+                All
             </label>
             <label>
                 <input type="radio" name="year" value="2010" checked={!!model} onChange={() => onModelChange(true)} />{' '}
