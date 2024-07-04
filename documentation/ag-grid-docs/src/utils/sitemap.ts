2b210d617c79f67cd37bed526815ebfa4689901a
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-07-04 17:23:10 +0100

    AG-12107 Remove redirect pages from sitemap

--- documentation/ag-grid-docs/src/utils/sitemap.ts
+++ documentation/ag-grid-docs/src/utils/sitemap.ts
@@ -12,8 +12,21 @@ const isDebugPage = (page: string) => {
     return page.includes('/debug/');
 };
 
+/*
+ * Documentation redirect pages
+ */
+const isRedirectPage = (page: string) => {
+    return (
+        page.endsWith('/documentation/') ||
+        page.endsWith('/react-data-grid/') ||
+        page.endsWith('/angular-data-grid/') ||
+        page.endsWith('/javascript-data-grid/') ||
+        page.endsWith('/vue-data-grid/')
+    );
+};
+
 const filterIgnoredPages = (page: string) => {
-    return !isExamplePage(page) && !isDebugPage(page);
+    return !isExamplePage(page) && !isDebugPage(page) && !isRedirectPage(page);
 };
 
 export function getSitemapConfig() {
