cf7656e8eb64b69de8f4b68c39f927f7d7cd1d0c
Tak Tran <contact@tutaktran.com>
2024-07-08 12:24:55 +0100

    git subrepo pull external/ag-website-shared
    subrepo:
      subdir:   "external/ag-website-shared"
      merged:   "0fa4b70060"
    upstream:
      origin:   "git@github.com:ag-grid/ag-website-shared.git"
      branch:   "latest"
      commit:   "0fa4b70060"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

---
+++ external/ag-website-shared/src/markdoc/tags/imageCaption.ts
@@ -0,0 +1,37 @@
+import { type Render, component } from '@astrojs/markdoc/config';
+import type { Config, Schema } from '@markdoc/markdoc';
+
+export const imageCaption: Schema<Config, Render> = {
+    render: component('../../external/ag-website-shared/src/components/imageCaption/ImageCaption.astro'),
+    attributes: {
+        /**
+         * Docs page name in `src/content/[pageName]
+         *
+         * If not provided, will default to the location of the markdoc file
+         */
+        pageName: { type: String },
+        /**
+         * Relative path within markdoc page folder
+         */
+        imagePath: { type: String, required: true },
+        alt: { type: String, required: true },
+        centered: { type: Boolean },
+        constrained: { type: Boolean },
+        descriptionTop: { type: Boolean },
+        width: { type: String },
+        height: { type: String },
+        minWidth: { type: String },
+        maxWidth: { type: String },
+        /**
+         * Enable dark mode CSS filter for image
+         *
+         * Alternatively, add `-dark` suffixed image in `imagePath` to add
+         * dark mode image manually
+         */
+        enableDarkModeFilter: { type: Boolean },
+        /**
+         * Autoplay gif
+         */
+        autoPlay: { type: Boolean },
+    },
+};
