c74898b50e1ae6779d52f4fa3d9f4633f1ddb350
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-07-26 15:30:50 +0100

    add benchmarks

---
+++ documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/benchmarks/Benchmarks.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './Benchmarks.module.scss';
+
+const Benchmarks: React.FC = () => {
+    return (
+        <div className={styles.container}>
+            <div>TODO</div>
+        </div>
+    );
+};
+
+export default Benchmarks;
