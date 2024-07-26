948980255ceda1a2cc5d5d2875cded07c0a68a81
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-07-26 15:30:59 +0100

    update layout

--- documentation/ag-grid-docs/src/components/framework-landing-pages/react/ReactLandingPage.tsx
+++ documentation/ag-grid-docs/src/components/framework-landing-pages/react/ReactLandingPage.tsx
@@ -1,32 +1,56 @@
+import { useState } from 'react';
+
 import styles from './ReactLandingPage.module.scss';
+import Benchmarks from './sections/benchmarks/Benchmarks';
 import Customers from './sections/customers/Customers';
 import Examples from './sections/examples/Examples';
+import Faqs from './sections/faqs/Faqs';
 import FeaturesSection from './sections/features/FeaturesSection';
 import HeroSection from './sections/hero/HeroSection';
 import Showcase from './sections/showcase/Showcase';
 import Divider from './utils/Divider';
 
 export const ReactLandingPage = () => {
+    let isOdd = false;
+    const getIsOdd = () => {
+        isOdd = !isOdd;
+        return !isOdd;
+    };
+
     return (
         <div className={styles.container}>
-            <div className={styles.sectionContent}>
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
                 <HeroSection />
             </div>
-            <Divider odd={false} />
-            <div className={styles.sectionOdd}>
+            <Divider odd={getIsOdd()} />
+            <div className={isOdd ? styles.sectionOdd : styles.sectionOdd}>
                 <div className={styles.headingContainer}>
                     <h2 className={styles.tag}>Why Choose AG Grid?</h2>
                     <h3 className={styles.heading}>Amazingly Fast, Fully Customisable and Unbeatable Features</h3>
                     <h4 className={styles.subHeading}>
                         Get started in minutes and access 1000s of features without compromising on performance.
-                        Customise the grid with your own styles and components or upgrade to enterprise to take your
-                        application to the next level.
+                        Customise your React Table with your own styles and components or upgrade to enterprise to use
+                        our advanced features.
                     </h4>
                 </div>
                 <FeaturesSection />
             </div>
-            <Divider odd={true} />
-            <div className={styles.sectionContent}>
+            {/* <Divider odd={getIsOdd()} />
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
+                <div className={styles.headingContainer}>
+                    <h2 className={styles.tag}>What Can AG Grid Do?</h2>
+                    <h3 className={styles.heading}>
+                        Every Core Feature, For Free. Advanced Features, Found Nowhere Else.
+                    </h3>
+                    <h4 className={styles.subHeading}>
+                        100s of core features, all available for free, such as Sorting, Filtering, Grouping, and more.
+                        Upgrade to Enterprise for Integrated Charting, Pivoting,
+                    </h4>
+                </div>
+                <FeaturesSection />
+            </div> */}
+            <Divider odd={getIsOdd()} />
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
                 <div className={styles.headingContainer}>
                     <h2 className={styles.tag}>Where is AG Grid Used?</h2>
                     <h3 className={styles.heading}>Used in Every Industry, for All Types of Data</h3>
@@ -37,60 +61,64 @@ export const ReactLandingPage = () => {
                 </div>
                 <Showcase />
             </div>
-            <Divider odd={false} />
-            <div className={styles.sectionOdd}>
+            <Divider odd={getIsOdd()} />
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
                 <div className={styles.headingContainer}>
                     <h2 className={styles.tag}>Who Uses AG Grid?</h2>
                     <h3 className={styles.heading}>
-                        Loved By Developers<br></br>Trusted By The Worlds Biggest Enterprises
+                        Loved By Developers<br></br>Trusted By The Worlds Largest Enterprises
                     </h3>
                     <h4 className={styles.subHeading}>
-                        AG Grid is used by over 90% of the fortune 500 with 1,000,000 npm downloads per week and over
-                        12,000 Stars on Github.
+                        Over <b>90% of the Fortune 500</b> build React Tables using AG Grid, with{' '}
+                        <span className={styles.highlight}>
+                            <a href="https://www.npmjs.com/package/ag-grid-community" target="_blank">
+                                1,000,000+
+                            </a>
+                        </span>{' '}
+                        npm downloads per week and over{' '}
+                        <span className={styles.highlight}>
+                            <a href="https://github.com/ag-grid/ag-grid/tree/latest" target="_blank">
+                                12,000
+                            </a>
+                        </span>{' '}
+                        Stars on Github.
                     </h4>
                 </div>
                 <Customers />
             </div>
-            <Divider odd={true} />
-            <div className={styles.sectionContent}>
+            <Divider odd={getIsOdd()} />
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
                 <div className={styles.headingContainer}>
                     <h2 className={styles.tag}>How Do I Get Started?</h2>
                     <h3 className={styles.heading}>Get Started with Common Templates</h3>
                     <h4 className={styles.subHeading}>
-                        The samples below show how to use of AG Grids most popular & powerful features.
+                        Sample apps to help you build your React Table with AG Grid in minutes.
                     </h4>
                 </div>
                 <Examples />
             </div>
-            <Divider odd={false} />
-            <div>
-                <h3>Benchmarks</h3>
-                <p>
-                    A simple chart showing benchmarking of load/scroll times with a huge grid, compared to our
-                    competitors
-                </p>
-                <h4 className={styles.subHeading}>
-                    <ul>
-                        <li>Speed comparison w/ MUI</li>
-                        <li>Feature comparison w/ MUI</li>
-                    </ul>
-                </h4>
+            <Divider odd={getIsOdd()} />
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
+                <div className={styles.headingContainer}>
+                    <h2 className={styles.tag}>How Does AG Grid Compare to Competition?</h2>
+                    <h3 className={styles.heading}>Unbeatable Performance, Unmatched Feature Set</h3>
+                    <h4 className={styles.subHeading}>
+                        AG Grid is the fastest React Table available with features that aren't found anywhere else
+                    </h4>
+                </div>
+                <Benchmarks />
             </div>
+            <Divider odd={getIsOdd()} />
             {/* FAQs - Community vs. Enterprise,  etc... */}
-            <div>
-                <h3>FAQs</h3>
-                <p>simple list of common questions</p>
-                <h4 className={styles.subHeading}>
-                    <ul>
-                        <li>Community Vs. Enterprise</li>
-                        <li>Is AG Grid free</li>
-                        <li>AG Charts compatibility</li>
-                        <li>Can AG Grid be customised</li>
-                        <li>Why use AG Grid over MUI</li>
-                        <li>Why use AG Grid over React Table</li>
-                        <li>More TBC</li>
-                    </ul>
-                </h4>
+            <div className={isOdd ? styles.sectionOdd : styles.sectionContent}>
+                <div className={styles.headingContainer}>
+                    <h2 className={styles.tag}>How Does AG Grid Compare to Competition?</h2>
+                    <h3 className={styles.heading}>Frequently Asked Questions</h3>
+                    <h4 className={styles.subHeading}>
+                        Answers to some common questions when building React Tables with AG Grid
+                    </h4>
+                </div>
+                <Faqs />
             </div>
         </div>
     );
