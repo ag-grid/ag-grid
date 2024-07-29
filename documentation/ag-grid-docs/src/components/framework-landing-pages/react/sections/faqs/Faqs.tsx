c7e1b7654bcd5b4ee75bfe385444bc2de0a5989a
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-07-29 15:04:54 +0100

    restyle section

--- documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/faqs/Faqs.tsx
+++ documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/faqs/Faqs.tsx
@@ -1,3 +1,4 @@
+import { Icon } from '@ag-website-shared/components/icon/Icon';
 import React, { useState } from 'react';
 
 import styles from './Faqs.module.scss';
@@ -5,38 +6,51 @@ import faqs from './faqs.json';
 
 const Faqs: React.FC = () => {
     const [activeIndex, setActiveIndex] = useState(-1);
+
     const handleToggle = (index: number) => {
         setActiveIndex(activeIndex === index ? -1 : index);
     };
 
+    const midIndex = Math.ceil(faqs.length / 2);
+    const firstColumnFaqs = faqs.slice(0, midIndex);
+    const secondColumnFaqs = faqs.slice(midIndex);
+
     return (
         <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <img
-                    src="/landing-pages/misc/faqs-illustration.png"
-                    alt="React Table FAQs Illustration"
-                    className={styles.image}
-                />
-            </div>
-            <div className={styles.listContainer}>
-                {faqs.map((faq, index) => (
-                    <div
-                        key={index}
-                        className={`${styles.faqItem} ${activeIndex === index ? styles.active : ''}`}
-                        onClick={() => handleToggle(index)}
-                    >
-                        <div className={styles.questionContainer}>
-                            <div className={styles.questionIndex}>0{index + 1}</div>
-                            <div className={styles.divider}></div>
-                            <div className={styles.detailsContainer}>
-                                <div className={styles.questionText}>{faq.question}</div>
-                                <div className={styles.answersContainer}>
-                                    <div className={styles.faqAnswer}>{faq.answer}</div>
-                                </div>
+            <div className={styles.column}>
+                {firstColumnFaqs.map((faq, index) => (
+                    <React.Fragment key={index}>
+                        <div className={styles.questionContainer} onClick={() => handleToggle(index)}>
+                            <div className={styles.titleContainer}>
+                                <span className={styles.question}>{faq.question}</span>
+                                <Icon
+                                    svgClasses={styles.expandIcon}
+                                    name={activeIndex === index ? 'chevronDown' : 'chevronRight'}
+                                />
                             </div>
-                            <div className={styles.questionIcon}>{activeIndex === index ? '-' : '+'}</div>
+                            {activeIndex === index && <div className={styles.answerContainer}>{faq.answer}</div>}
                         </div>
-                    </div>
+                        <hr />
+                    </React.Fragment>
+                ))}
+            </div>
+            <div className={styles.column}>
+                {secondColumnFaqs.map((faq, index) => (
+                    <React.Fragment key={index + midIndex}>
+                        <div className={styles.questionContainer} onClick={() => handleToggle(index + midIndex)}>
+                            <div className={styles.titleContainer}>
+                                <span className={styles.question}>{faq.question}</span>
+                                <Icon
+                                    svgClasses={styles.expandIcon}
+                                    name={activeIndex === index + midIndex ? 'chevronDown' : 'chevronRight'}
+                                />
+                            </div>
+                            {activeIndex === index + midIndex && (
+                                <div className={styles.answerContainer}>{faq.answer}</div>
+                            )}
+                        </div>
+                        <hr />
+                    </React.Fragment>
                 ))}
             </div>
         </div>
