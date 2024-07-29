e1b61b53d513600ad94d19e12141ce4df3dddbea
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-07-29 14:30:00 +0100

    Refactor component

--- documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/showcase/Showcase.tsx
+++ documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/showcase/Showcase.tsx
@@ -3,14 +3,14 @@ import { useDarkmode } from '@utils/hooks/useDarkmode';
 import React from 'react';
 
 import styles from './Showcase.module.scss';
+import showcaseDetails from './showcase.json';
 
 const Showcase: React.FC = () => {
     const [darkMode] = useDarkmode();
-
-    const [expanded, setExpanded] = React.useState(0);
+    const [expanded, setExpanded] = React.useState<number>(0);
 
     const handleExpand = (index: number) => {
-        setExpanded(index);
+        setExpanded(index === expanded ? 0 : index);
     };
 
     return (
@@ -18,151 +18,58 @@ const Showcase: React.FC = () => {
             <div className={styles.columnContainer}>
                 <div className={styles.column}>
                     <div className={styles.buttonContainer}>
-                        <div className={styles.button} onClick={() => handleExpand(0)}>
-                            <div className={styles.buttonTitleContainer}>
-                                <span className={styles.buttonTitle}>Finance</span>{' '}
-                                <Icon svgClasses={styles.buttonIcon} name={expanded === 0 ? 'minimize' : 'maximize'} />
-                            </div>
-                            {expanded === 0 && (
-                                <div className={styles.buttonDetailContainer}>
-                                    <span className={styles.buttonDetail}>
-                                        Analyse complex financial data, perform calculations and visualise the data in
-                                        AG Grid, with standalone charts from AG Charts.
-                                    </span>
-                                    <div className={styles.buttonFooter}>
-                                        <a>Preview</a>
-                                        <a>Source Code</a>
-                                    </div>
+                        {showcaseDetails.map((item, index) => (
+                            <div key={index} className={styles.button} onClick={() => handleExpand(index)}>
+                                <div className={styles.buttonTitleContainer}>
+                                    <span className={styles.buttonTitle}>{item.title}</span>
+                                    <Icon
+                                        svgClasses={styles.buttonIcon}
+                                        name={expanded === index ? 'chevronDown' : 'chevronRight'}
+                                    />
                                 </div>
-                            )}
-                        </div>
-
-                        <div className={styles.button} onClick={() => handleExpand(1)}>
-                            <div className={styles.buttonTitleContainer}>
-                                <span className={styles.buttonTitle}>ML/AI</span>{' '}
-                                <Icon svgClasses={styles.buttonIcon} name={expanded === 1 ? 'minimize' : 'maximize'} />
-                            </div>
-                            {expanded === 1 && (
-                                <div className={styles.buttonDetailContainer}>
-                                    <span className={styles.buttonDetail}>Details about ML/AI - MLFlow</span>
-                                    <div className={styles.buttonFooter}>
-                                        <a>Preview</a>
-                                        <a>Source Code</a>
+                                {expanded === index && (
+                                    <div className={styles.buttonDetailContainer}>
+                                        <span className={styles.buttonDetail}>{item.detail}</span>
+                                        <div className={styles.buttonFooter}>
+                                            {item.links.map((link, linkIndex) => (
+                                                <a key={linkIndex}>{link}</a>
+                                            ))}
+                                        </div>
                                     </div>
-                                </div>
-                            )}
-                        </div>
-
-                        <div className={styles.button} onClick={() => handleExpand(2)}>
-                            <div className={styles.buttonTitleContainer}>
-                                <span className={styles.buttonTitle}>Developer Platforms</span>{' '}
-                                <Icon svgClasses={styles.buttonIcon} name={expanded === 2 ? 'minimize' : 'maximize'} />
+                                )}
                             </div>
-                            {expanded === 2 && (
-                                <div className={styles.buttonDetailContainer}>
-                                    <span className={styles.buttonDetail}>
-                                        Open-source developer platform and workflow engine who use AG Grid as part of
-                                        their drag & drop UI builder.
-                                    </span>
-                                    <div className={styles.buttonFooter}>
-                                        <a>Preview</a>
-                                        <a>Source Code</a>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-
-                        <div className={styles.button} onClick={() => handleExpand(3)}>
-                            <div className={styles.buttonTitleContainer}>
-                                <span className={styles.buttonTitle}>Data Modelling</span>{' '}
-                                <Icon svgClasses={styles.buttonIcon} name={expanded === 3 ? 'minimize' : 'maximize'} />
-                            </div>
-                            {expanded === 3 && (
-                                <div className={styles.buttonDetailContainer}>
-                                    <span className={styles.buttonDetail}>
-                                        Planning, scheduling, and sequencing tools for modern space missions. AG Grid is
-                                        used throughout to help visualise mission data.
-                                    </span>
-                                    <div className={styles.buttonFooter}>
-                                        <a>Preview</a>
-                                        <a>Source Code</a>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
-
-                        <div className={styles.button} onClick={() => handleExpand(4)}>
-                            <div className={styles.buttonTitleContainer}>
-                                <span className={styles.buttonTitle}>Dashboards</span>{' '}
-                                <Icon svgClasses={styles.buttonIcon} name={expanded === 4 ? 'minimize' : 'maximize'} />
-                            </div>
-                            {expanded === 4 && (
-                                <div className={styles.buttonDetailContainer}>
-                                    <span className={styles.buttonDetail}>
-                                        An open source React library for building dashboards, with AG Grid enterprise
-                                        support for building React tables with advanced features.
-                                    </span>
-                                    <div className={styles.buttonFooter}>
-                                        <a className={styles.secondaryCta}>Preview</a>
-                                        <a className={styles.primaryCta}>Source Code</a>
-                                    </div>
-                                </div>
-                            )}
-                        </div>
+                        ))}
                     </div>
                 </div>
                 <div className={styles.column}>
                     <div className={styles.imageContainer}>
-                        {expanded === 0 && (
-                            <>
-                                <div className={styles.imageTitle}>open.bb</div>
-                                <img
-                                    src="/landing-pages/showcase/openbb.png"
-                                    alt="OpenBB Terminal Pro Landing Page with React Table"
-                                    className={styles.stackedImage}
-                                />
-                            </>
-                        )}
-                        {expanded === 1 && (
-                            <>
-                                <div className={styles.imageTitle}>mlflow.org</div>
-                                <img
-                                    src="/landing-pages/showcase/mlflow.png"
-                                    alt="MLFlow Landing Page with React Table"
-                                    className={styles.stackedImage}
-                                />
-                            </>
-                        )}
-                        {expanded === 2 && (
-                            <>
-                                <div className={styles.imageTitle}>windmill.dev</div>
-                                <img
-                                    src={`/landing-pages/showcase/${darkMode ? 'windmill-light.png' : 'windmill.png'}`}
-                                    alt="Windmill Landing Page with React Table"
-                                    className={styles.stackedImage}
-                                />
-                            </>
-                        )}
-                        {expanded === 3 && (
-                            <>
-                                <div className={styles.imageTitle}>nasa.ammos.io</div>
-                                <img
-                                    src={`/landing-pages/showcase/${darkMode ? 'nasa-aerie-light.png' : 'nasa-aerie.png'}`}
-                                    alt="NASA Aerie Admin Landing Page with React Table"
-                                    className={styles.stackedImage}
-                                />
-                            </>
-                        )}
-                        {expanded === 4 && (
-                            <>
-                                <div className={styles.imageTitle}>marmelab.com/react-admin</div>
-                                <img
-                                    src="/landing-pages/showcase/react-admin.png"
-                                    alt="React Admin Landing Page with React Table"
-                                    className={styles.stackedImage}
-                                />
-                            </>
-                        )}
+                        {showcaseDetails.map((item, index) => {
+                            if (expanded === index) {
+                                return (
+                                    <React.Fragment key={index}>
+                                        <a>
+                                            <div
+                                                className={styles.imageTitle}
+                                                style={{
+                                                    display: 'flex',
+                                                    justifyContent: 'center',
+                                                    alignItems: 'center',
+                                                }}
+                                            >
+                                                <span style={{ flex: 1, textAlign: 'center' }}>{item.preview}</span>
+                                                <Icon svgClasses={styles.newTabIcon} name={'newTab'} />
+                                            </div>
+                                            <img
+                                                src={darkMode && item.imageLight ? item.imageLight : item.image}
+                                                alt={item.alt}
+                                                className={styles.stackedImage}
+                                            />
+                                        </a>
+                                    </React.Fragment>
+                                );
+                            }
+                            return null;
+                        })}
                     </div>
                 </div>
             </div>
