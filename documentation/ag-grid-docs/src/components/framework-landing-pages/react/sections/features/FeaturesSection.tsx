b5cc8d773e30011fc1f8c108e6aca65229a596f6
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-07-29 16:29:14 +0100

    refactor

--- documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/features/FeaturesSection.tsx
+++ documentation/ag-grid-docs/src/components/framework-landing-pages/react/sections/features/FeaturesSection.tsx
@@ -8,54 +8,35 @@ import CustomFeatures from './tabs/customfeatures/CustomFeatures';
 
 const FeaturesSection: React.FC = () => {
     const [activeTab, setActiveTab] = React.useState(0);
+    const tabs = [
+        { title: 'Build', component: <BasicFeatures /> },
+        { title: 'Customise', component: <CustomFeatures /> },
+        { title: 'Expand', component: <AdvancedFeatures /> },
+    ];
 
     const handleTabClick = (index: number) => {
-        if (index > 2) {
-            setActiveTab(2);
-        } else if (index < 0) {
+        if (index >= tabs.length) {
+            setActiveTab(tabs.length - 1);
+        } else if (index <= 0) {
             setActiveTab(0);
         } else setActiveTab(index);
-    };
-
-    const renderTabContent = () => {
-        switch (activeTab) {
-            case 0:
-                return <BasicFeatures />;
-            case 1:
-                return <CustomFeatures />;
-            case 2:
-                return <AdvancedFeatures />;
-            default:
-                return null;
-        }
     };
 
     return (
         <>
             <div className={styles.container}>
                 <div className={styles.tabContainer}>
-                    <button
-                        className={activeTab === 0 ? styles.activeTab : styles.tab}
-                        onClick={() => handleTabClick(0)}
-                    >
-                        Build
-                    </button>
-                    <div className={styles.divider}></div>
-                    <button
-                        className={activeTab === 1 ? styles.activeTab : styles.tab}
-                        onClick={() => handleTabClick(1)}
-                    >
-                        Customise
-                    </button>
-                    <div className={styles.divider}></div>
-                    <button
-                        className={activeTab === 2 ? styles.activeTab : styles.tab}
-                        onClick={() => handleTabClick(2)}
-                    >
-                        Expand
-                    </button>
+                    {tabs.map((tab, index) => (
+                        <button
+                            key={index}
+                            className={activeTab === index ? styles.activeTab : styles.tab}
+                            onClick={() => handleTabClick(index)}
+                        >
+                            {tab.title}
+                        </button>
+                    ))}
                 </div>
-                <div className={styles.contentContainer}>{renderTabContent()}</div>
+                <div className={styles.contentContainer}>{tabs[activeTab]?.component}</div>
                 <div className={styles.buttonContainer}>
                     <Icon
                         onClick={() => handleTabClick(activeTab - 1)}
