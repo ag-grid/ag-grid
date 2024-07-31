7dba191d61e6557fd86bd99a1f950cae6ca89f70
Tak Tran <contact@tutaktran.com>
2024-07-31 14:53:26 +0100

    AG-11543 - Remove `leftColumnWidth` config
    No longer needed with api table styles

--- documentation/ag-grid-docs/src/components/reference-documentation/types.d.ts
+++ documentation/ag-grid-docs/src/components/reference-documentation/types.d.ts
@@ -173,9 +173,6 @@ export interface Config {
     /** Suppress the missing property check. Needed for events as they are dynamic and so do not appear in src code */
     suppressMissingPropCheck?: true;
 
-    /** The width of the left column in characters. Names will wrap if there are any longer names. */
-    maxLeftColumnWidth: number;
-
     /** A regular expression limiting the names that should appear */
     namePattern: string;
 
