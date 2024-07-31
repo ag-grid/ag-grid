6f0c5409c4c3437cc045e718058ba68a3a1a19b7
Tak Tran <contact@tutaktran.com>
2024-07-31 17:02:47 +0100

    AG-11543 - Remove deprecated interface helpers

--- documentation/ag-grid-docs/src/components/reference-documentation/utils/interface-helpers.ts
+++ documentation/ag-grid-docs/src/components/reference-documentation/utils/interface-helpers.ts
@@ -3,27 +3,6 @@ import { SHOW_DEBUG_LOGS } from '@constants';
 import type { Config, DocEntryMap, InterfaceEntry, InterfaceHierarchyOverrides, PropertyType } from '../types';
 import { extractInterfaces } from './documentation-helpers';
 
-/**
- * @deprecated
- */
-export const getInterfacesToWriteLegacy = (name, definition, config) => {
-    let interfacesToWrite = [];
-    if (typeof definition === 'string') {
-        // Extract all the words to enable support for Union types
-        interfacesToWrite = extractInterfaces(definition, config.lookups.interfaces, applyInterfaceInclusions(config));
-    } else if (
-        (typeof definition == 'object' && !Array.isArray(definition)) ||
-        (typeof name == 'string' && Array.isArray(definition))
-    ) {
-        interfacesToWrite.push({
-            name,
-            interfaceType: { type: definition, meta: {} },
-        });
-    }
-
-    return interfacesToWrite;
-};
-
 export const getInterfacesToWrite = ({
     name,
     definition,
@@ -93,49 +72,6 @@ export const formatJson = (value: string) =>
 
 export function isCallSig(gridProp: InterfaceEntry): boolean | undefined {
     return gridProp && gridProp.meta && gridProp.meta.isCallSignature;
-}
-
-/**
- * Property type is the small blue text that tells you the type of the given property
- * @deprecated
- */
-export function getPropertyTypeLegacy(type: string | PropertyType, config: Config) {
-    let propertyType = '';
-    if (type) {
-        if (typeof type == 'string') {
-            propertyType = type;
-        } else if (typeof type == 'object') {
-            if (type.arguments || type.parameters) {
-                if (isGridOptionEvent(config.gridOpProp) || config.isEvent) {
-                    // If an event show the event type instead of Function
-                    propertyType = Object.values(type.arguments)[0];
-                } else {
-                    propertyType = `Function`;
-                }
-            } else if (type.returnType) {
-                if (typeof type.returnType == 'object') {
-                    propertyType = 'object';
-                } else if (typeof type.returnType == 'string') {
-                    const inter = config.lookups.interfaces[type.returnType];
-                    if (inter && inter.meta && inter.meta.isCallSignature) {
-                        propertyType = `Function`;
-                    } else {
-                        propertyType = type.returnType;
-                    }
-                }
-            } else {
-                propertyType = 'void';
-            }
-        }
-    }
-    // We hide generics from this part of the display for simplicity
-    // Could be done with a Regex...
-    propertyType = propertyType?.replace(
-        /<(TData|TValue|TContext|any)?(, )?(TData|TValue|TContext|any)?(, )?(TData|TValue|TContext|any)?>/g,
-        ''
-    );
-
-    return propertyType;
 }
 
 /**
