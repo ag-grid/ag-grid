ff43b1851debabb3dcec996c53f3fd8cb35de7ea
Elias Malik <elias.malik@ag-grid.com>
2024-07-31 12:47:57 +0100

    add legacy range selection docs

---
+++ documentation/ag-grid-docs/src/content/docs/range-selection-fill-handle-legacy/_examples/read-only-edit/main.ts
@@ -0,0 +1,71 @@
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { CellEditRequestEvent, GetRowIdParams, GridApi, GridOptions, createGrid } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, RangeSelectionModule]);
+
+let gridApi: GridApi<IOlympicDataWithId>;
+
+const gridOptions: GridOptions<IOlympicDataWithId> = {
+    columnDefs: [
+        { field: 'athlete', minWidth: 160 },
+        { field: 'age' },
+        { field: 'country', minWidth: 140 },
+        { field: 'year' },
+        { field: 'date', minWidth: 140 },
+        { field: 'sport', minWidth: 160 },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' },
+        { field: 'total' },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+        editable: true,
+        cellDataType: false,
+    },
+    getRowId: (params: GetRowIdParams) => String(params.data.id),
+    enableRangeSelection: true,
+    enableFillHandle: true,
+    readOnlyEdit: true,
+    onCellEditRequest: onCellEditRequest,
+};
+
+let rowImmutableStore: any[];
+
+function onCellEditRequest(event: CellEditRequestEvent) {
+    const data = event.data;
+    const field = event.colDef.field;
+    const newValue = event.newValue;
+
+    const oldItem = rowImmutableStore.find((row) => row.id === data.id);
+
+    if (!oldItem || !field) {
+        return;
+    }
+
+    const newItem = { ...oldItem };
+
+    newItem[field] = newValue;
+
+    console.log('onCellEditRequest, updating ' + field + ' to ' + newValue);
+
+    rowImmutableStore = rowImmutableStore.map((oldItem) => (oldItem.id == newItem.id ? newItem : oldItem));
+    gridApi!.setGridOption('rowData', rowImmutableStore);
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', () => {
+    const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then((data: any[]) => {
+            data.forEach((item, index) => (item.id = index));
+            rowImmutableStore = data;
+            gridApi!.setGridOption('rowData', rowImmutableStore);
+        });
+});
---
+++ documentation/ag-grid-docs/src/content/docs/range-selection-fill-handle-legacy/_examples/skipping-columns/main.ts
@@ -0,0 +1,52 @@
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { FillOperationParams, GridApi, GridOptions, createGrid } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, RangeSelectionModule]);
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    columnDefs: [
+        { field: 'athlete', minWidth: 150 },
+        { field: 'age', maxWidth: 90 },
+        { field: 'country', minWidth: 150 },
+        { field: 'year', maxWidth: 90 },
+        { field: 'date', minWidth: 150 },
+        { field: 'sport', minWidth: 150 },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+        editable: true,
+        cellDataType: false,
+    },
+    enableRangeSelection: true,
+    enableFillHandle: true,
+    suppressClearOnFillReduction: true,
+    fillOperation: (params: FillOperationParams) => {
+        if (params.column.getColId() === 'country') {
+            return params.currentCellValue;
+        }
+
+        return params.values[params.values.length - 1];
+    },
+};
+
+function createRowData(data: any[]) {
+    var rowData = data.slice(0, 100);
+    return rowData;
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then(function (data) {
+            gridApi!.setGridOption('rowData', createRowData(data));
+        });
+});
