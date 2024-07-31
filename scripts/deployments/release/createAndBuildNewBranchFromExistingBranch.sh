1ea9c233605e4b40ca6e87b13e15b8e87077ef3d
seanlandsman <sean@thelandsmans.com>
2024-07-31 11:26:01 +0100

    AG-12459 New Archive Job on TC

--- scripts/deployments/release/createAndBuildNewBranchFromExistingBranch.sh
+++ scripts/deployments/release/createAndBuildNewBranchFromExistingBranch.sh
@@ -27,7 +27,7 @@ echo "########### Creating and switching to new branch $NEW_BRANCH ###########"
 echo "########################################################################"
 echo "#################### Updating LicenseManager ###########################"
 if [ -f "$GEN_KEY_DEFAULT_LOCATION" ]; then
-    node scripts/release/updateLicenseManager.js `node "$GEN_KEY_DEFAULT_LOCATION" release`
+    node updateLicenseManager.js `node "$GEN_KEY_DEFAULT_LOCATION" release`
 else
     echo "ERROR: $GEN_KEY_DEFAULT_LOCATION does not exist."
     exit 1
