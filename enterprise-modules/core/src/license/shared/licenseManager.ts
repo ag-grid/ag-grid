a92c7b93f05788434959f62cdc817bebe1d5a7b3
seanlandsman <sean@thelandsmans.com>
2024-08-01 09:21:27 +0100

    Revert incorrect update

--- enterprise-modules/core/src/license/shared/licenseManager.ts
+++ enterprise-modules/core/src/license/shared/licenseManager.ts
@@ -14,7 +14,7 @@ export interface ILicenseManager {
 }
 
 export class LicenseManager {
-    private static RELEASE_INFORMATION: string = 'MTcyMjQzMTIyMTE2Nw==';
+    private static RELEASE_INFORMATION: string = 'MTcxOTQzMTYzMDEwNw==';
     private static licenseKey: string;
     private static chartsLicenseManager?: ILicenseManager;
     private watermarkMessage: string | undefined = undefined;
