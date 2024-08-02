fc3db047f3d440afa4c5cd84bc755410079d23ce
Elias Malik <elias.malik@ag-grid.com>
2024-08-02 11:20:36 +0100

    add example for cell selection with pinned columns

---
+++ documentation/ag-grid-docs/src/content/docs/cell-selection/_examples/range-selection-pinned-areas/main.ts
@@ -0,0 +1,42 @@
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { type GridApi, type GridOptions, createGrid } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { ClipboardModule } from '@ag-grid-enterprise/clipboard';
+import { MenuModule } from '@ag-grid-enterprise/menu';
+import { RangeSelectionModule } from '@ag-grid-enterprise/range-selection';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, ClipboardModule, MenuModule, RangeSelectionModule]);
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    columnDefs: [
+        { field: 'athlete', minWidth: 150, pinned: 'left' },
+        { field: 'age', maxWidth: 90 },
+        { field: 'country', minWidth: 150 },
+        { field: 'year', maxWidth: 90 },
+        { field: 'date', minWidth: 150 },
+        { field: 'sport', minWidth: 150 },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' },
+        { field: 'total', pinned: 'right' },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+    },
+    selectionOptions: {
+        mode: 'cell',
+    },
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
+        .then((response) => response.json())
+        .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data));
+});
