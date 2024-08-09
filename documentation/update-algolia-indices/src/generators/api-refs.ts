a316cd82907337d96b1c406fdd7ac94a188c9197
Elias <elias0789@gmail.com>
2024-08-09 04:41:00 -0700

    AG-12399 Support nested properties in API reference parser (#8412)
    * improve types
    
    * fix types
    
    * wip
    
    * wip
    
    * remove logging
    
    * last few fixes
    
    * ensure objectID is unique
    
    * prevent setting empty `nested: {}` on every reference, only where it's requested

--- documentation/update-algolia-indices/src/generators/api-refs.ts
+++ documentation/update-algolia-indices/src/generators/api-refs.ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
 
+import type { AlgoliaRecord } from '../types/algolia';
 import { API_REFERENCE_DIR, API_SOURCE_DIR } from '../utils/constants';
 
 export interface APIPageData {
@@ -12,7 +13,7 @@ export interface APIPageData {
  * Finds all the API pages to be indexed
  */
 export const getApiPageData = (): APIPageData[] => {
-    const result = [];
+    const result: APIPageData[] = [];
 
     const pageNames = fs.readdirSync(API_SOURCE_DIR, { withFileTypes: true });
     pageNames.forEach((page) => {
@@ -39,38 +40,61 @@ export const getApiPageData = (): APIPageData[] => {
     return result;
 };
 
+interface Link {
+    name: string;
+    url: string;
+}
+interface APIRefMeta {
+    meta?: { displayName: string; page: Link };
+}
+type APIProperties = Record<string, { description: string; more: Link; addNestedFields?: boolean }>;
+type APIRefSection = APIProperties & APIRefMeta;
+interface APIPropertyRefSource {
+    meta: {
+        all: string;
+        comment: string;
+        tags: string[];
+    };
+    type: {
+        returnType: string;
+        nested?: Record<string, APIPropertyRefSource['meta']>;
+    };
+}
+
+const IGNORE_FIELDS = new Set(['enable', 'enabled', 'mode']);
+
 /**
  * Parse the API files to retrieve the index data
  */
 export const parseApiPageData = (details: APIPageData): AlgoliaRecord[] => {
-    const records: AlgoliaRecord = [];
+    const records: AlgoliaRecord[] = [];
 
     const { propertiesFileUrl, breadcrumbSuffix, pagePath } = details;
 
-    const file = fs.readFileSync(propertiesFileUrl, null);
+    const file = fs.readFileSync(propertiesFileUrl, 'utf8');
     const { _config_, ...sections } = JSON.parse(file);
     if (!_config_) return []; // if no config, wrong type of file.
     const { codeSrc } = _config_;
     if (!codeSrc) return []; // if no src, wrong type of file.
 
-    const referenceFile = fs.readFileSync(`${API_REFERENCE_DIR}/${codeSrc}`, null);
-    const apiPropertiesSourceFile = JSON.parse(referenceFile);
+    const referenceFile = fs.readFileSync(`${API_REFERENCE_DIR}/${codeSrc}`, 'utf8');
+    const apiPropertiesSourceFile = JSON.parse(referenceFile) as Record<string, APIPropertyRefSource>;
 
     let position = 0;
     // load the defined sections for the API docs
-    Object.entries(sections).forEach(([sectionKey, section]) => {
+    Object.entries(sections as APIRefSection[]).forEach(([sectionKey, section]) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const { meta, ...properties } = section;
-        const sectionDisplayName = meta?.displayName ?? sectionKey; // Formatted section name
 
         Object.entries(properties).forEach(([propertyKey, property]) => {
-            const { description, more } = property; // more can include a link to a page with more info
+            const { description, addNestedFields = false } = property; // more can include a link to a page with more info
 
             const data = apiPropertiesSourceFile[propertyKey];
 
             const breadcrumb = `API > ${breadcrumbSuffix}`;
             const path = `${pagePath}#reference-${sectionKey}-${propertyKey}`;
             const text = description ?? data.meta.comment;
-            const normalizedText = text.replace(/\[([^\]]+)\][^\)]+\)/g, '$1');
+            const normalizedText = text.replace(/\[([^\]]+)\][^)]+\)/g, '$1');
 
             records.push({
                 source: 'api',
@@ -83,6 +107,24 @@ export const parseApiPageData = (details: APIPageData): AlgoliaRecord[] => {
                 path: path,
                 rank: position++,
             });
+
+            if (addNestedFields) {
+                for (const key in data.type.nested ?? {}) {
+                    if (IGNORE_FIELDS.has(key)) {
+                        continue;
+                    }
+                    records.push({
+                        source: 'api',
+                        objectID: `${path}:${key}`,
+                        title: breadcrumbSuffix,
+                        heading: key,
+                        text: data.type.nested?.[key].comment ?? '',
+                        breadcrumb,
+                        path,
+                        rank: position++,
+                    });
+                }
+            }
         });
     });
 
