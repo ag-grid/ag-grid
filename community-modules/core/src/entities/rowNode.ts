e6737e88396a053768672e5e0d60b4f6c8749c2a
Stephen Cooper <scooperdev@gmail.com>
2024-08-12 18:04:43 +0100

    Fix merge changes

--- community-modules/core/src/entities/rowNode.ts
+++ community-modules/core/src/entities/rowNode.ts
@@ -20,7 +20,7 @@ import type { IServerSideRowModel } from '../interfaces/iServerSideRowModel';
 import { LocalEventService } from '../localEventService';
 import { FrameworkEventListenerService } from '../misc/frameworkEventListenerService';
 import { _debounce, _errorOnce, _warnOnce } from '../utils/function';
-import { _exists, _missing, _missingOrEmpty } from '../utils/generic';
+import { _exists, _missing } from '../utils/generic';
 import type { AgColumn } from './agColumn';
 
 /**
@@ -449,27 +449,6 @@ export class RowNode<TData = any> implements IEventEmitter<RowNodeEventType>, IR
         return pixel >= this.rowTop && pixel < this.rowTop + this.rowHeight;
     }
 
-    public setFirstChild(firstChild: boolean): void {
-        if (this.firstChild !== firstChild) {
-            this.firstChild = firstChild;
-            this.dispatchRowEvent('firstChildChanged');
-        }
-    }
-
-    public setLastChild(lastChild: boolean): void {
-        if (this.lastChild !== lastChild) {
-            this.lastChild = lastChild;
-            this.dispatchRowEvent('lastChildChanged');
-        }
-    }
-
-    public setChildIndex(childIndex: number): void {
-        if (this.childIndex !== childIndex) {
-            this.childIndex = childIndex;
-            this.dispatchRowEvent('childIndexChanged');
-        }
-    }
-
     public setRowTop(rowTop: number | null): void {
         this.oldRowTop = this.rowTop;
 
@@ -490,40 +469,12 @@ export class RowNode<TData = any> implements IEventEmitter<RowNodeEventType>, IR
         this.setRowIndex(null);
     }
 
-    private setDisplayed(displayed: boolean): void {
-        if (this.displayed !== displayed) {
-            this.displayed = displayed;
-            this.dispatchRowEvent('displayedChanged');
-        }
-    }
-
-    public setDragging(dragging: boolean): void {
-        if (this.dragging !== dragging) {
-            this.dragging = dragging;
-            this.dispatchRowEvent('draggingChanged');
-        }
-    }
-
-    public setHighlighted(highlighted: RowHighlightPosition | null): void {
-        if (this.highlighted !== highlighted) {
-            this.highlighted = highlighted;
-            this.dispatchRowEvent('rowHighlightChanged');
-        }
-    }
-
     public setHovered(hovered: boolean): void {
         this.hovered = hovered;
     }
 
     public isHovered(): boolean {
         return this.hovered;
-    }
-
-    public setAllChildrenCount(allChildrenCount: number | null): void {
-        if (this.allChildrenCount !== allChildrenCount) {
-            this.allChildrenCount = allChildrenCount;
-            this.dispatchRowEvent('allChildrenCountChanged');
-        }
     }
 
     public setMaster(master: boolean): void {
@@ -654,20 +605,6 @@ export class RowNode<TData = any> implements IEventEmitter<RowNodeEventType>, IR
 
         const rowModel = this.beans.rowModel as IClientSideRowModel | IServerSideRowModel;
         rowModel.onRowHeightChangedDebounced?.();
-    }
-
-    public setRowIndex(rowIndex: number | null): void {
-        if (this.rowIndex !== rowIndex) {
-            this.rowIndex = rowIndex;
-            this.dispatchRowEvent('rowIndexChanged');
-        }
-    }
-
-    public setUiLevel(uiLevel: number): void {
-        if (this.uiLevel !== uiLevel) {
-            this.uiLevel = uiLevel;
-            this.dispatchRowEvent('uiLevelChanged');
-        }
     }
 
     /**
@@ -1182,49 +1119,66 @@ export class RowNode<TData = any> implements IEventEmitter<RowNodeEventType>, IR
         this.sibling = undefined as any;
     }
 
-    // Generic setters
-    private updateIfDifferent<T extends keyof RowNode>(key: T, value: RowNode[T], eventName: RowNodeEventType): void {
-        if (this[key] === value) {
-            return;
-        }
-        (this as RowNode)[key] = value;
-
-        this.dispatchRowEvent(eventName);
-    }
-
     public setFirstChild(firstChild: boolean): void {
-        this.updateIfDifferent('firstChild', firstChild, 'firstChildChanged');
+        if (this.firstChild !== firstChild) {
+            this.firstChild = firstChild;
+            this.dispatchRowEvent('firstChildChanged');
+        }
     }
 
     public setLastChild(lastChild: boolean): void {
-        this.updateIfDifferent('lastChild', lastChild, 'lastChildChanged');
+        if (this.lastChild !== lastChild) {
+            this.lastChild = lastChild;
+            this.dispatchRowEvent('lastChildChanged');
+        }
     }
 
     public setChildIndex(childIndex: number): void {
-        this.updateIfDifferent('childIndex', childIndex, 'childIndexChanged');
+        if (this.childIndex !== childIndex) {
+            this.childIndex = childIndex;
+            this.dispatchRowEvent('childIndexChanged');
+        }
     }
 
     private setDisplayed(displayed: boolean): void {
-        this.updateIfDifferent('displayed', displayed, 'displayedChanged');
+        if (this.displayed !== displayed) {
+            this.displayed = displayed;
+            this.dispatchRowEvent('displayedChanged');
+        }
     }
 
     public setDragging(dragging: boolean): void {
-        this.updateIfDifferent('dragging', dragging, 'draggingChanged');
+        if (this.dragging !== dragging) {
+            this.dragging = dragging;
+            this.dispatchRowEvent('draggingChanged');
+        }
     }
 
     public setHighlighted(highlighted: RowHighlightPosition | null): void {
-        this.updateIfDifferent('highlighted', highlighted, 'rowHighlightChanged');
+        if (this.highlighted !== highlighted) {
+            this.highlighted = highlighted;
+            this.dispatchRowEvent('rowHighlightChanged');
+        }
     }
 
     public setAllChildrenCount(allChildrenCount: number | null): void {
-        this.updateIfDifferent('allChildrenCount', allChildrenCount, 'allChildrenCountChanged');
+        if (this.allChildrenCount !== allChildrenCount) {
+            this.allChildrenCount = allChildrenCount;
+            this.dispatchRowEvent('allChildrenCountChanged');
+        }
     }
 
     public setRowIndex(rowIndex: number | null): void {
-        this.updateIfDifferent('rowIndex', rowIndex, 'rowIndexChanged');
+        if (this.rowIndex !== rowIndex) {
+            this.rowIndex = rowIndex;
+            this.dispatchRowEvent('rowIndexChanged');
+        }
     }
 
     public setUiLevel(uiLevel: number): void {
-        this.updateIfDifferent('uiLevel', uiLevel, 'uiLevelChanged');
+        if (this.uiLevel !== uiLevel) {
+            this.uiLevel = uiLevel;
+            this.dispatchRowEvent('uiLevelChanged');
+        }
     }
 }
