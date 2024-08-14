052f8f0931d76c4b31bd965b291458efb9597b09
Peter Reynolds <peterjrreynolds@gmail.com>
2024-08-14 14:28:47 +0100

    AG-12585 - fix rich select async value not in list

--- enterprise-modules/core/src/widgets/agRichSelectList.ts
+++ enterprise-modules/core/src/widgets/agRichSelectList.ts
@@ -109,12 +109,12 @@ export class AgRichSelectList<TValue, TEventType extends string = AgRichSelectLi
         });
     }
 
-    public selectValue(value?: TValue[] | TValue): void {
+    public selectValue(value?: TValue[] | TValue): boolean {
         if (!this.currentList) {
             if (this.eLoading) {
                 this.appendChild(this.eLoading);
             }
-            return;
+            return false;
         }
 
         if (this.eLoading?.offsetParent) {
@@ -122,13 +122,14 @@ export class AgRichSelectList<TValue, TEventType extends string = AgRichSelectLi
         }
 
         if (value == null) {
-            return;
+            return false;
         }
 
         const selectedPositions = this.getIndicesForValues(value);
-        const len = selectedPositions.length;
 
-        if (len > 0) {
+        const refresh = selectedPositions.length > 0;
+
+        if (refresh) {
             // make sure the virtual list has been sized correctly
             this.refresh();
             this.ensureIndexVisible(selectedPositions[0]);
@@ -138,6 +139,8 @@ export class AgRichSelectList<TValue, TEventType extends string = AgRichSelectLi
         }
 
         this.selectListItems(Array.isArray(value) ? value : [value]);
+
+        return refresh;
     }
 
     private selectListItems(values: TValue[], append = false): void {
