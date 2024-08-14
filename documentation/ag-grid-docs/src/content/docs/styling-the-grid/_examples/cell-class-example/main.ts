deb5010a27cafa9d9474f62e3faa2a4fb84d6eea
James Swinton-Bland <james_swinton@hotmail.co.uk>
2024-08-14 13:59:17 +0100

    Initial Content

---
+++ documentation/ag-grid-docs/src/content/docs/styling-the-grid/_examples/cell-class-example/main.ts
@@ -0,0 +1,55 @@
+import { createGrid, GridApi, GridOptions, CellValueChangedEvent } from '@ag-grid-community/core';
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ModuleRegistry } from "@ag-grid-community/core";
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+let gridApi: GridApi;
+
+const gridOptions: GridOptions = {
+    // Data to be displayed
+    rowData: [
+        { company: "RVSN USSR", country: "Kazakhstan", date: "1957-10-04", mission: "Sputnik-1", price: 9550000, successful: true },
+        { company: "RVSN USSR", country: "Kazakhstan", date: "1957-11-03", mission: "Sputnik-2", price: 8990000, successful: true },
+        { company: "US Navy", country: "USA", date: "1957-12-06", mission: "Vanguard TV3", price: 6860000, successful: false }
+    ],
+    // Columns to be displayed (Should match rowData properties)
+    columnDefs: [
+        {
+            field: "mission",
+            resizable: false,
+            cellClass: 'mission-cell'
+        },
+        {
+            field: "country"
+        },
+        {
+            field: "successful"
+        },
+        {
+            field: "date"
+        },
+        {
+            field: "price"
+        },
+        {
+            field: "company"
+        }
+    ],
+    // Configurations applied to all columns
+    defaultColDef: {
+        filter: true,
+        editable: true,
+    },
+    // Grid Options & Callbacks
+    pagination: true,
+    rowClass: 'row'
+}
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+    fetch('https://www.ag-grid.com/example-assets/space-mission-data.json')
+        .then(response => response.json())
+        .then((data: any) => gridApi.setGridOption('rowData', data))
+})
