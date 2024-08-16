3c57914c27e066af005323b3cbcd509fc210749b
Elias Malik <elias.malik@ag-grid.com>
2024-08-16 15:55:04 +0100

    update docs

--- documentation/ag-grid-docs/src/content/docs/row-selection-single-row/_examples/checkbox-selection/main.ts
+++ documentation/ag-grid-docs/src/content/docs/row-selection-single-row/_examples/checkbox-selection/main.ts
@@ -1,5 +1,5 @@
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { type GridApi, type GridOptions, type IRowNode, createGrid } from '@ag-grid-community/core';
+import { GridApi, GridOptions, IRowNode, createGrid } from '@ag-grid-community/core';
 import { ModuleRegistry } from '@ag-grid-community/core';
 import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
 import { MenuModule } from '@ag-grid-enterprise/menu';
