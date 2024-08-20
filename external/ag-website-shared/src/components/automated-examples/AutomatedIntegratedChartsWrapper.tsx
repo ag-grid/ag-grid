668f3b1ebb478907b7d3e8ed331343b05f60b399
Tak Tran <contact@tutaktran.com>
2024-08-20 12:08:26 +0100

    git subrepo pull external/ag-website-shared
    subrepo:
      subdir:   "external/ag-website-shared"
      merged:   "6533caaf8a"
    upstream:
      origin:   "git@github.com:ag-grid/ag-website-shared.git"
      branch:   "latest"
      commit:   "6533caaf8a"
    git-subrepo:
      version:  "0.4.6"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "73a0129"

---
+++ external/ag-website-shared/src/components/automated-examples/AutomatedIntegratedChartsWrapper.tsx
@@ -0,0 +1,53 @@
+import { createAutomatedExampleManager } from '@ag-website-shared/components/automated-examples/lib/createAutomatedExampleManager';
+import styles from '@pages-styles/homepage.module.scss';
+import classNames from 'classnames';
+import { type FunctionComponent, type ReactNode, useEffect, useMemo, useState } from 'react';
+
+import { AutomatedIntegratedCharts } from './AutomatedIntegratedCharts';
+import type { LogLevel } from './lib/scriptDebugger';
+
+interface Props {
+    children?: ReactNode;
+}
+
+export const AutomatedIntegratedChartsWrapper: FunctionComponent<Props> = ({ children }) => {
+    const [isCI, setIsCI] = useState(false);
+    const [runOnce, setRunOnce] = useState(false);
+    const automatedExampleManager = useMemo(
+        () =>
+            createAutomatedExampleManager({
+                debugCanvasClassname: styles.automatedExampleDebugCanvas,
+                debugPanelClassname: styles.automatedExampleDebugPanel,
+            }),
+        []
+    );
+
+    useEffect(() => {
+        const searchParams = new URLSearchParams(window.location.search);
+        const debugValue = searchParams.get('debug');
+        const debugLogLevel = searchParams.get('debugLogLevel') as LogLevel;
+        setIsCI(searchParams.get('isCI') === 'true');
+        setRunOnce(searchParams.get('runOnce') === 'true');
+
+        automatedExampleManager.setDebugEnabled(Boolean(debugValue));
+        automatedExampleManager.setDebugLogLevel(debugLogLevel);
+        automatedExampleManager.setDebugInitialDraw(debugValue === 'draw');
+    }, []);
+
+    return (
+        <section className={styles.automatedIntegratedChartsOuter}>
+            <div className={classNames('layout-max-width-small', styles.homepageExample)}>
+                <div className={styles.automatedIntegratedCharts}>
+                    <AutomatedIntegratedCharts
+                        automatedExampleManager={automatedExampleManager}
+                        useStaticData={isCI}
+                        runOnce={runOnce}
+                        visibilityThreshold={0.8}
+                    >
+                        {children}
+                    </AutomatedIntegratedCharts>
+                </div>
+            </div>
+        </section>
+    );
+};
