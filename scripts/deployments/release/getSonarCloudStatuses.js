761a80e73644688a4f773351f405ca25a8310e26
seanlandsman <sean@thelandsmans.com>
2024-08-22 11:54:55 +0100

    AG-12633 Automate releasing jira release

--- scripts/deployments/release/getSonarCloudStatuses.js
+++ scripts/deployments/release/getSonarCloudStatuses.js
@@ -7,38 +7,41 @@ if (!sonarCloudToken) {
 }
 
 const getQualityGateStatuses = (projectIds) => {
-    return Promise.all(projectIds.map(async projectId => {
-            const response = await fetch(`https://sonarcloud.io/api/qualitygates/project_status?projectKey=${projectId}`, {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${sonarCloudToken}`,
-                    Accept: 'application/json',
-                },
-            });
+    return Promise.all(
+        projectIds.map(async (projectId) => {
+            const response = await fetch(
+                `https://sonarcloud.io/api/qualitygates/project_status?projectKey=${projectId}`,
+                {
+                    method: 'GET',
+                    headers: {
+                        Authorization: `Bearer ${sonarCloudToken}`,
+                        Accept: 'application/json',
+                    },
+                }
+            );
 
             const result = await response.json();
             return {
                 projectId: projectId,
-                status: result.projectStatus.status
-            }
-        }
-    ))
-}
+                status: result.projectStatus.status,
+            };
+        })
+    );
+};
 
 (async function () {
     const projectStatuses = await getQualityGateStatuses([
         'ag-grid-community',
         'ag-grid-enterprise',
         'ag-grid-charts-enterprise',
-        'ag-charts-community'
+        'ag-charts-community',
     ]);
 
-    if(!projectStatuses.every(projectStatus => projectStatus.status === 'OK')) {
-        console.error("One or more projects are in an error state on SonarCloud");
+    if (!projectStatuses.every((projectStatus) => projectStatus.status === 'OK')) {
+        console.error('One or more projects are in an error state on SonarCloud');
         console.error(projectStatuses);
         process.exit(1);
     } else {
-        console.log("All SonarCloud projects passing quality gates.");
+        console.log('All SonarCloud projects passing quality gates.');
     }
 })();
-
