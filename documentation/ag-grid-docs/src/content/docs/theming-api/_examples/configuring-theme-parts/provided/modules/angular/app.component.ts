9ca20f5e505c56c22daef86a1dfab80bb66807cc
Bernie Sumption <bernie@berniecode.com>
2024-08-27 07:50:40 +0200

    Angular example

---
+++ documentation/ag-grid-docs/src/content/docs/theming-api/_examples/configuring-theme-parts/provided/modules/angular/app.component.ts
@@ -0,0 +1,122 @@
+import { AgGridAngular } from '@ag-grid-community/angular';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import type { ColDef } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import {
+    colorSchemeDarkBlue,
+    colorSchemeDarkNeutral,
+    colorSchemeDarkWarm,
+    colorSchemeLightCold,
+    colorSchemeLightNeutral,
+    colorSchemeLightWarm,
+    iconSetAlpine,
+    iconSetMaterial,
+    iconSetQuartzBold,
+    iconSetQuartzLight,
+    iconSetQuartzRegular,
+    themeBalham,
+    themeMaterial,
+    themeQuartz,
+} from '@ag-grid-community/theming';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { SideBarModule } from '@ag-grid-enterprise/side-bar';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+ModuleRegistry.registerModules([
+    ClientSideRowModelModule,
+    SideBarModule,
+    ColumnsToolPanelModule,
+    FiltersToolPanelModule,
+]);
+
+@Component({
+    standalone: true,
+    imports: [AgGridAngular, FormsModule, CommonModule],
+    selector: 'my-app',
+    template: `
+        <div style="height: 100%; display: flex; flex-direction: column">
+            <p style="flex: 0 1 0%">
+                Theme:
+                <select style="margin-right: 16px" [(ngModel)]="baseTheme">
+                    <option *ngFor="let baseTheme of baseThemes" [ngValue]="baseTheme">{{ baseTheme.id }}</option>
+                </select>
+                Icons:
+                <select style="margin-right: 16px" [(ngModel)]="iconSet">
+                    <option *ngFor="let iconSet of iconSets" [ngValue]="iconSet">
+                        {{ iconSet ? iconSet.variant : '(unchanged)' }}
+                    </option>
+                </select>
+                Color scheme:
+                <select style="margin-right: 16px" [(ngModel)]="colorScheme">
+                    <option *ngFor="let colorScheme of colorSchemes" [ngValue]="colorScheme">
+                        {{ colorScheme ? colorScheme.variant : '(unchanged)' }}
+                    </option>
+                </select>
+            </p>
+            <div style="flex: 1 1 0%">
+                <ag-grid-angular
+                    style="width: 100%; height: 100%;"
+                    [columnDefs]="columnDefs"
+                    [defaultColDef]="defaultColDef"
+                    [rowData]="rowData"
+                    [theme]="theme"
+                    sideBar
+                />
+            </div>
+        </div>
+    `,
+})
+export class AppComponent {
+    colorSchemeId = '';
+    iconSetId = '';
+
+    baseThemes = [themeQuartz, themeBalham, themeMaterial];
+    baseTheme = themeQuartz;
+
+    colorSchemes = [
+        null,
+        colorSchemeLightNeutral,
+        colorSchemeLightCold,
+        colorSchemeLightWarm,
+        colorSchemeDarkNeutral,
+        colorSchemeDarkWarm,
+        colorSchemeDarkBlue,
+    ];
+    colorScheme = null;
+
+    iconSets = [null, iconSetQuartzLight, iconSetQuartzRegular, iconSetQuartzBold, iconSetAlpine, iconSetMaterial];
+    iconSet = null;
+
+    get theme() {
+        let theme = this.baseTheme;
+        if (this.iconSet) {
+            theme = theme.usePart(this.iconSet);
+        }
+        if (this.colorScheme) {
+            theme = theme.usePart(this.colorScheme);
+        }
+        return theme;
+    }
+
+    columnDefs: ColDef[] = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
+
+    defaultColDef: ColDef = {
+        editable: true,
+        flex: 1,
+        minWidth: 100,
+        filter: true,
+    };
+
+    rowData: any[] = (() => {
+        const rowData: any[] = [];
+        for (let i = 0; i < 10; i++) {
+            rowData.push({ make: 'Toyota', model: 'Celica', price: 35000 + i * 1000 });
+            rowData.push({ make: 'Ford', model: 'Mondeo', price: 32000 + i * 1000 });
+            rowData.push({ make: 'Porsche', model: 'Boxster', price: 72000 + i * 1000 });
+        }
+        return rowData;
+    })();
+}
