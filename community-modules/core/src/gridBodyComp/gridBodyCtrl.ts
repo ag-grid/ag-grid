e9fc259a10b3f832394843667aef1bff945f7635
Bernie Sumption <bernie@berniecode.com>
2024-08-29 15:13:25 +0100

    AG-11630 code review fixes

--- community-modules/core/src/gridBodyComp/gridBodyCtrl.ts
+++ community-modules/core/src/gridBodyComp/gridBodyCtrl.ts
@@ -253,11 +253,11 @@ export class GridBodyCtrl extends BeanStub {
 
     private updateScrollingClasses(): void {
         this.eGridBody.classList.toggle(
-            'ag-body-vertical-content-overflows',
+            'ag-body-vertical-content-overflow',
             this.scrollVisibleService.isVerticalScrollShowing()
         );
         this.eGridBody.classList.toggle(
-            'ag-body-horizontal-content-overflows',
+            'ag-body-horizontal-content-overflow',
             this.scrollVisibleService.isHorizontalScrollShowing()
         );
     }
