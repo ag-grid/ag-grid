6bd12af298ecda0519b8e60498ba61d0f1f90d7c
Abi Glazier <abiglazier@gmail.com>
2024-09-06 17:29:01 +0100

    AG-12624 pivoting documentation restructure

---
+++ documentation/ag-grid-docs/src/content/docs/pivoting-group-columns/_examples/pivot-panel/main.ts
@@ -0,0 +1,53 @@
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { GridApi, GridOptions, createGrid } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, RowGroupingModule]);
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    columnDefs: [
+        { field: 'country', rowGroup: true, enableRowGroup: true },
+        { field: 'sport', pivot: true, enablePivot: true },
+        { field: 'year', pivot: true, enablePivot: true },
+        { field: 'gold', aggFunc: 'sum' },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 130,
+    },
+    autoGroupColumnDef: {
+        minWidth: 200,
+    },
+    pivotMode: true,
+    pivotPanelShow: 'onlyWhenPivoting',
+
+    sideBar: {
+        defaultToolPanel: 'columns',
+        toolPanels: [
+            {
+                id: 'columns',
+                labelDefault: 'Columns',
+                labelKey: 'columns',
+                iconKey: 'columns',
+                toolPanel: 'agColumnsToolPanel',
+                toolPanelParams: {
+                    suppressPivots: true,
+                    suppressPivotMode: true,
+                },
+            },
+        ],
+    },
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    var gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data));
+});
