0d95b8e6c8b5054c0889b84188c5a6a912815b46
Andrew Glazier <andrew.glazier@ag-grid.com>
2024-09-10 08:14:52 +0100

    AG-12624 - Tree data docs iteration

---
+++ documentation/ag-grid-docs/src/content/docs/tree-data-group-column/_examples/custom-component/provided/modules/reactFunctional/index.jsx
@@ -0,0 +1,102 @@
+'use strict';
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { AgGridReact } from '@ag-grid-community/react';
+import '@ag-grid-community/styles/ag-grid.css';
+import '@ag-grid-community/styles/ag-theme-quartz.css';
+import { RowGroupingModule } from '@ag-grid-enterprise/row-grouping';
+import React, { StrictMode, useCallback, useMemo, useRef, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import CustomGroupCellRenderer from './customGroupCellRenderer.jsx';
+import { getData } from './data';
+import './styles.css';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, RowGroupingModule]);
+
+const GridExample = () => {
+    const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+    const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+    const [columnDefs, setColumnDefs] = useState([
+        { field: 'created' },
+        { field: 'modified' },
+        {
+            field: 'size',
+            aggFunc: 'sum',
+            valueFormatter: (params) => {
+                const sizeInKb = params.value / 1024;
+
+                if (sizeInKb > 1024) {
+                    return `${+(sizeInKb / 1024).toFixed(2)} MB`;
+                } else {
+                    return `${+sizeInKb.toFixed(2)} KB`;
+                }
+            },
+        },
+    ]);
+    const getDataPath = useCallback((data) => data.path, []);
+    const autoGroupColumnDef = useMemo(() => {
+        return {
+            cellRenderer: CustomGroupCellRenderer,
+        };
+    }, []);
+    const defaultColDef = useMemo(() => {
+        return {
+            flex: 1,
+            minWidth: 120,
+        };
+    }, []);
+
+    const onCellDoubleClicked = useCallback((params) => {
+        if (params.colDef.showRowGroup) {
+            params.node.setExpanded(!params.node.expanded);
+        }
+    }, []);
+
+    const onCellKeyDown = useCallback((params) => {
+        if (!('colDef' in params)) {
+            return;
+        }
+        if (!(params.event instanceof KeyboardEvent)) {
+            return;
+        }
+        if (params.event.code !== 'Enter') {
+            return;
+        }
+        if (params.colDef.showRowGroup) {
+            params.node.setExpanded(!params.node.expanded);
+        }
+    }, []);
+
+    return (
+        <div style={containerStyle}>
+            <div
+                style={gridStyle}
+                className={
+                    /** DARK MODE START **/ document.documentElement.dataset.defaultTheme ||
+                    'ag-theme-quartz' /** DARK MODE END **/
+                }
+            >
+                <AgGridReact
+                    treeData
+                    getDataPath={getDataPath}
+                    rowData={getData()}
+                    columnDefs={columnDefs}
+                    autoGroupColumnDef={autoGroupColumnDef}
+                    defaultColDef={defaultColDef}
+                    groupDefaultExpanded={1}
+                    onCellDoubleClicked={onCellDoubleClicked}
+                    onCellKeyDown={onCellKeyDown}
+                />
+            </div>
+        </div>
+    );
+};
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <GridExample />
+    </StrictMode>
+);
