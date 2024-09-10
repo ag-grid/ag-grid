0d1b2e11dbaa4f7551be1b0d84e59c68ad2d707f
Abi Glazier <abiglazier@gmail.com>
2024-09-10 14:58:03 +0100

    AG-12624 typescript

--- documentation/ag-grid-docs/src/content/docs/tree-data-filtering/_examples/group-agg-filtering/provided/modules/reactFunctionalTs/index.tsx
+++ documentation/ag-grid-docs/src/content/docs/tree-data-filtering/_examples/group-agg-filtering/provided/modules/reactFunctionalTs/index.tsx
@@ -1,7 +1,15 @@
 'use strict';
 
 import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
-import { ColDef, GridReadyEvent, ModuleRegistry } from '@ag-grid-community/core';
+import {
+    ColDef,
+    ColGroupDef,
+    GetDataPath,
+    GridApi,
+    GridOptions,
+    GridReadyEvent,
+    ModuleRegistry,
+} from '@ag-grid-community/core';
 import { AgGridReact } from '@ag-grid-community/react';
 import '@ag-grid-community/styles/ag-grid.css';
 import '@ag-grid-community/styles/ag-theme-quartz.css';
