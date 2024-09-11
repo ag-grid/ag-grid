bf3273092c8a405d14e64ff6ccbe359371b17171
Andrew Glazier <andrew.glazier@ag-grid.com>
2024-09-11 13:49:17 +0100

    AG-12599 - Minor docs change pivot

--- documentation/ag-grid-docs/src/content/docs/pivoting-column-groups/_examples/strict-column-order/main.ts
+++ documentation/ag-grid-docs/src/content/docs/pivoting-column-groups/_examples/strict-column-order/main.ts
@@ -9,11 +9,11 @@ let gridApi: GridApi;
 
 const rowData = [
     { agg: 1, pivotValue: 6 },
-    { agg: 1, pivotValue: 5 },
-    { agg: 1, pivotValue: 4 },
+    { agg: 1, pivotValue: 1 },
     { agg: 1, pivotValue: 3 },
     { agg: 1, pivotValue: 2 },
-    { agg: 1, pivotValue: 1 },
+    { agg: 1, pivotValue: 5 },
+    { agg: 1, pivotValue: 4 },
 ];
 let count = 0;
 
