1a8043ce4ae6381c7ecd5fbceed3299d9d7e3f94
Bernie Sumption <bernie@berniecode.com>
2024-09-12 11:30:38 +0100

    AG-10798 Theming API createTheme example

---
+++ documentation/ag-grid-docs/src/content/docs/theming-api/_examples/creating-themes/provided/modules/angular/app.component.ts
@@ -0,0 +1,54 @@
+import { AgGridAngular } from '@ag-grid-community/angular';
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import type { ColDef } from '@ag-grid-community/core';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { createTheme, iconSetMaterial } from '@ag-grid-community/theming';
+import { Component } from '@angular/core';
+
+ModuleRegistry.registerModules([ClientSideRowModelModule]);
+
+const myCustomTheme = createTheme().with(iconSetMaterial).withParams({
+    accentColor: 'red',
+    foregroundColor: '#660000',
+    iconSize: 18,
+});
+
+@Component({
+    standalone: true,
+    imports: [AgGridAngular],
+    selector: 'my-app',
+    template: `
+        <ag-grid-angular
+            style="height: 100%;"
+            [columnDefs]="columnDefs"
+            [defaultColDef]="defaultColDef"
+            [rowData]="rowData"
+            [theme]="theme"
+            [selection]="selection"
+        />
+    `,
+})
+export class AppComponent {
+    theme = myCustomTheme;
+
+    selection = { mode: 'multiRow', checkboxes: true };
+
+    columnDefs: ColDef[] = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
+
+    defaultColDef: ColDef = {
+        editable: true,
+        flex: 1,
+        minWidth: 100,
+        filter: true,
+    };
+
+    rowData: any[] = (() => {
+        const rowData: any[] = [];
+        for (let i = 0; i < 10; i++) {
+            rowData.push({ make: 'Toyota', model: 'Celica', price: 35000 + i * 1000 });
+            rowData.push({ make: 'Ford', model: 'Mondeo', price: 32000 + i * 1000 });
+            rowData.push({ make: 'Porsche', model: 'Boxster', price: 72000 + i * 1000 });
+        }
+        return rowData;
+    })();
+}
