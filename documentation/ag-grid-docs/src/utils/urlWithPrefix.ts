2aace8f0eaeb0f7f56356f8db486b6faa64d4244
Tak Tran <contact@tutaktran.com>
2024-09-16 13:04:57 +0100

    AG-12729 - Make trailing slash the default for urlWithPrefix

--- documentation/ag-grid-docs/src/utils/urlWithPrefix.ts
+++ documentation/ag-grid-docs/src/utils/urlWithPrefix.ts
@@ -4,11 +4,15 @@ import { getFrameworkPath } from '@features/docs/utils/urlPaths';
 import { getIsDev } from '@utils/env';
 import { pathJoin } from '@utils/pathJoin';
 
+const isExternalLink = (url: string) => {
+    return url.startsWith('http') || url.startsWith('mailto');
+};
+
 export const urlWithPrefix = ({
     url = '',
     framework,
     siteBaseUrl = SITE_BASE_URL,
-    trailingSlash,
+    trailingSlash = true,
 }: {
     url: string;
     framework?: Framework;
@@ -16,12 +20,13 @@ export const urlWithPrefix = ({
     trailingSlash?: boolean;
 }): string => {
     let path = url;
+    const urlHasTrailingSlash = url.endsWith('/');
     if (url.startsWith('./')) {
         const frameworkPath = getFrameworkPath(framework!);
         path = pathJoin('/', siteBaseUrl, frameworkPath, url.slice('./'.length));
     } else if (url.startsWith('/')) {
         path = pathJoin('/', siteBaseUrl, url);
-    } else if (!url.startsWith('#') && !url.startsWith('http') && !url.startsWith('mailto')) {
+    } else if (!url.startsWith('#') && !isExternalLink(url)) {
         const errorMessage = `Invalid url: ${url} - use './' for framework urls, '/' for root urls, '#' for anchor links, and http/mailto for external urls`;
         if (getIsDev()) {
             // eslint-disable-next-line no-console
@@ -31,7 +36,7 @@ export const urlWithPrefix = ({
         }
     }
 
-    if (trailingSlash && !path.includes('#')) {
+    if ((trailingSlash || urlHasTrailingSlash) && !path.includes('#') && !isExternalLink(url)) {
         path = path + '/';
     }
 
