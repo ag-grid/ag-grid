99f5670a09477dae3b7d3047577e21be82312a45
Andrew Glazier <andrew.glazier@ag-grid.com>
2024-09-16 14:47:58 +0100

    RTI-2302 - Fix vue and angular example

---
+++ documentation/ag-grid-docs/src/content/docs/pivoting-column-groups/_examples/strict-column-order/data.ts
@@ -0,0 +1,10 @@
+export function getData() {
+    return [
+        { agg: 1, pivotValue: 6 },
+        { agg: 1, pivotValue: 1 },
+        { agg: 1, pivotValue: 3 },
+        { agg: 1, pivotValue: 2 },
+        { agg: 1, pivotValue: 5 },
+        { agg: 1, pivotValue: 4 },
+    ];
+}
