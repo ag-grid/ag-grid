5efe7ff8093bc455cb7159a8386ece638d1d9c94
Stephen Cooper <scooperdev@gmail.com>
2024-09-16 09:46:50 +0100

    draft types

--- community-modules/core/src/validation/consoleReporting/consoleMappings.ts
+++ community-modules/core/src/validation/consoleReporting/consoleMappings.ts
@@ -1,13 +1,33 @@
-export type ConsoleID = number;
-
 export type ConsoleMessage = string | ((...args: string[]) => string);
 // a map from error codes to console messages
-const consoleMappings: Record<ConsoleID, ConsoleMessage> = {
+const consoleMappings = {
     1: 'rowData must be an array!',
-};
+    3: 'rowData must be an!',
+    22: (test: string) => `rowData must be an array! ${test}`,
+    2: (rowId: string, data: string) => `rowData must be an array! ${rowId} ${data}`,
+} as const;
 
-export function getConsoleMessage(consoleID: ConsoleID, ...args: any[]): string {
-    const msgOrFunc = consoleMappings[consoleID];
+// type of number keys from consoleMappings
+// 1 | 2
+// const i: ConsoleID = 13;
+export type ConsoleID = keyof typeof consoleMappings;
+export type ConsoleMessages = typeof consoleMappings;
+
+type ss = ConsoleMessages[2];
+type p = Parameters<ConsoleMessages[22]>;
+
+export type ConsoleMessageParams<TId extends ConsoleID> = ConsoleMessages[TId] extends (...args: infer P) => string
+    ? P
+    : never;
+const i: ConsoleID = 1;
+
+const mmd: ConsoleMessageParams<2> = ['a', 'b'];
+
+export function getConsoleMessage<TId extends ConsoleID, TParams extends ConsoleMessageParams<TId>>(
+    consoleID: TId,
+    ...args: TParams
+): string {
+    const msgOrFunc: ConsoleMessages[TId] = consoleMappings[consoleID];
 
     if (!msgOrFunc) {
         console.error(`Console Message for ID ${consoleID} not found`);
@@ -18,5 +38,5 @@ export function getConsoleMessage(consoleID: ConsoleID, ...args: any[]): string 
         return msgOrFunc;
     }
 
-    return msgOrFunc(...args);
+    return (msgOrFunc as any)(args);
 }
