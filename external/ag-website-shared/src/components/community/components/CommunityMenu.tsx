bcc785e7144e4217578c597d68ecd0d597c2955a
Tak Tran <contact@tutaktran.com>
2024-09-16 15:27:53 +0100

    AG-12729 - Add trailing slash to community menu

--- external/ag-website-shared/src/components/community/components/CommunityMenu.tsx
+++ external/ag-website-shared/src/components/community/components/CommunityMenu.tsx
@@ -1,20 +1,21 @@
+import { addTrailingSlash } from '@ag-website-shared/utils/addTrailingSlash';
 import { pathJoin } from '@utils/pathJoin';
 import { urlWithBaseUrl } from '@utils/urlWithBaseUrl';
 
 import styles from './CommunityMenu.module.scss';
 
-const getPageFromPath = (path) => {
+const getPageFromPath = (path: string) => {
     const prefix = urlWithBaseUrl('/community');
     return path.replace(`${prefix}`, '');
 };
 
-const CommunityMenu = ({ path, menu }) => {
+const CommunityMenu = ({ path, menu }: { path: string; menu: string }) => {
     const curPage = getPageFromPath(path);
 
     return (
         <ul className={styles.communityMenu}>
-            {menu.map((item) => {
-                const link = urlWithBaseUrl(pathJoin('/community', item.path));
+            {menu.map((item: string) => {
+                const link = addTrailingSlash(urlWithBaseUrl(pathJoin('/community', item.path)));
                 const isCurrentPage = curPage.replaceAll('/', '') === item.path.replaceAll('/', '');
 
                 return (
