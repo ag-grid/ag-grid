ce352e2a5c87374dd4f1e4ff21723db3ca27bb16
Bernie Sumption <bernie@berniecode.com>
2024-09-16 16:49:59 +0100

    RTI-2267 Theming API docs improvements

---
+++ documentation/ag-grid-docs/src/content/docs/theming-api/_examples/built-in-themes/provided/modules/reactFunctional/index.jsx
@@ -0,0 +1,82 @@
+'use strict';
+
+import { ClientSideRowModelModule } from '@ag-grid-community/client-side-row-model';
+import { ModuleRegistry } from '@ag-grid-community/core';
+import { AgGridReact } from '@ag-grid-community/react';
+import { themeAlpine, themeBalham, themeQuartz } from '@ag-grid-community/theming';
+import { ColumnsToolPanelModule } from '@ag-grid-enterprise/column-tool-panel';
+import { FiltersToolPanelModule } from '@ag-grid-enterprise/filter-tool-panel';
+import { SideBarModule } from '@ag-grid-enterprise/side-bar';
+import React, { StrictMode, useMemo, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+ModuleRegistry.registerModules([
+    ClientSideRowModelModule,
+    SideBarModule,
+    ColumnsToolPanelModule,
+    FiltersToolPanelModule,
+]);
+
+const themes = [themeQuartz, themeBalham, themeAlpine];
+
+const GridExample = () => {
+    const [theme, setBaseTheme] = useState(themes[0]);
+
+    return (
+        <div style={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <p style={{ flex: 0 }}>
+                Theme: <PartSelector options={themes} value={theme} setValue={setBaseTheme} />
+            </p>
+            <div style={{ flex: 1 }}>
+                <AgGridReact
+                    theme={theme}
+                    loadThemeGoogleFonts
+                    columnDefs={columnDefs}
+                    rowData={rowData}
+                    defaultColDef={defaultColDef}
+                    sideBar
+                />
+            </div>
+        </div>
+    );
+};
+
+const PartSelector = ({ options, value, setValue }) => (
+    <select
+        onChange={(e) => setValue(options.find((t) => t?.id === e.currentTarget.value) || null)}
+        style={{ marginRight: 16 }}
+        value={value?.id}
+    >
+        {options.map((option, i) => (
+            <option key={i} value={option?.id}>
+                {option?.variant || option?.id || '(unchanged)'}
+            </option>
+        ))}
+    </select>
+);
+
+const rowData = (() => {
+    const rowData = [];
+    for (let i = 0; i < 10; i++) {
+        rowData.push({ make: 'Toyota', model: 'Celica', price: 35000 + i * 1000 });
+        rowData.push({ make: 'Ford', model: 'Mondeo', price: 32000 + i * 1000 });
+        rowData.push({ make: 'Porsche', model: 'Boxster', price: 72000 + i * 1000 });
+    }
+    return rowData;
+})();
+
+const columnDefs = [{ field: 'make' }, { field: 'model' }, { field: 'price' }];
+
+const defaultColDef = {
+    editable: true,
+    flex: 1,
+    minWidth: 100,
+    filter: true,
+};
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <StrictMode>
+        <GridExample />
+    </StrictMode>
+);
