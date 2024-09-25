641a5c931e5f4e0ddfc7b789c522c134f6ecda42
Bernie Sumption <bernie@berniecode.com>
2024-09-25 18:05:41 +0100

    AG-10827 Theming API color documentation

--- documentation/ag-grid-docs/src/features/example-runner/components/ExampleIFrame.tsx
+++ documentation/ag-grid-docs/src/features/example-runner/components/ExampleIFrame.tsx
@@ -11,9 +11,16 @@ interface Props {
     isHidden?: boolean;
     url?: string;
     loadingIFrameId: string;
+    suppressDarkMode?: boolean;
 }
 
-export const ExampleIFrame: FunctionComponent<Props> = ({ title, isHidden, url, loadingIFrameId }) => {
+export const ExampleIFrame: FunctionComponent<Props> = ({
+    title,
+    isHidden,
+    url,
+    loadingIFrameId,
+    suppressDarkMode,
+}) => {
     const [isIntersecting, setIsIntersecting] = useState(false);
     const iFrameRef = useRef<HTMLIFrameElement>(null);
     const [darkMode] = useDarkmode();
@@ -60,11 +67,15 @@ export const ExampleIFrame: FunctionComponent<Props> = ({ title, isHidden, url, 
         if (!iFrameRef.current) {
             return;
         }
-        applyExampleDarkMode(iFrameRef.current.contentDocument!, darkMode);
+        if (!suppressDarkMode) {
+            applyExampleDarkMode(iFrameRef.current.contentDocument!, darkMode);
+        }
     }, [darkMode]);
 
     const handleOnLoad = useCallback(() => {
-        applyExampleDarkMode(iFrameRef.current.contentDocument, darkMode);
+        if (!suppressDarkMode) {
+            applyExampleDarkMode(iFrameRef.current.contentDocument, darkMode);
+        }
     }, [darkMode]);
 
     return (
