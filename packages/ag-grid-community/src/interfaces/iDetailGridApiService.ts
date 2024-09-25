a6a221b59f87becb4e867d2f521ba6f42b524206
Salvatore Previti <salvatore.previti@ag-grid.com>
2024-09-25 17:30:19 +0100

    AG-12975 move masterDetail logic from RowNode+CSRM to MasterDetail module

--- packages/ag-grid-community/src/interfaces/iDetailGridApiService.ts
+++ packages/ag-grid-community/src/interfaces/iDetailGridApiService.ts
@@ -1,6 +1,10 @@
 import type { DetailGridInfo } from '../api/gridApi';
+import type { RowNode } from '../entities/rowNode';
 
 export interface IDetailGridApiService {
+    /** Called by CSRM to initialize a node as master */
+    setMasterForRow(rowNode: RowNode, data: any, shouldSetExpanded: boolean): void;
+
     addDetailGridInfo(id: string, gridInfo: DetailGridInfo): void;
 
     removeDetailGridInfo(id: string): void;
