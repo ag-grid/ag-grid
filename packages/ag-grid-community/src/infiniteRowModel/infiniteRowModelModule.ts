0de167e9a01da23d0af3d974a060d6185f8f7454
Pete Reynolds <40919976+peterjrreynolds@users.noreply.github.com>
2024-10-02 16:48:11 +0100

    AG-12847 - fix multiple beans with same name in different modules (#8876)
    * AG-12847 - fix multiple beans with same name in different modules
    
    * AG-12847 - tweak module registry updates

--- packages/ag-grid-community/src/infiniteRowModel/infiniteRowModelModule.ts
+++ packages/ag-grid-community/src/infiniteRowModel/infiniteRowModelModule.ts
@@ -1,7 +1,7 @@
 import type { _InfiniteRowModelGridApi } from '../api/gridApi';
 import { RowModelHelperService } from '../api/rowModelHelperService';
 import { SsrmInfiniteSharedApiModule } from '../api/sharedApiModule';
-import { CommunityFeaturesModule } from '../gridCoreModule';
+import { CommunityFeaturesModule } from '../communityFeaturesModule';
 import { defineCommunityModule } from '../interfaces/iModule';
 import { ModuleNames } from '../modules/moduleNames';
 import { RowNodeBlockModule } from '../rowNodeCache/rowNodeBlockModule';
@@ -9,12 +9,13 @@ import { InfiniteRowModel } from './infiniteRowModel';
 import { getInfiniteRowCount, purgeInfiniteCache, refreshInfiniteCache } from './infiniteRowModelApi';
 
 export const InfiniteRowModelCoreModule = defineCommunityModule('InfiniteRowModelCoreModule', {
-    rowModel: 'infinite',
+    rowModels: ['infinite'],
     beans: [InfiniteRowModel],
     dependsOn: [RowNodeBlockModule],
 });
 
 export const InfiniteRowModelApiModule = defineCommunityModule<_InfiniteRowModelGridApi>('InfiniteRowModelApiModule', {
+    rowModels: ['infinite'],
     beans: [RowModelHelperService],
     apiFunctions: {
         refreshInfiniteCache,
@@ -25,5 +26,6 @@ export const InfiniteRowModelApiModule = defineCommunityModule<_InfiniteRowModel
 });
 
 export const InfiniteRowModelModule = defineCommunityModule(ModuleNames.InfiniteRowModelModule, {
+    rowModels: ['infinite'],
     dependsOn: [InfiniteRowModelCoreModule, InfiniteRowModelApiModule, CommunityFeaturesModule],
 });
