a3ffac5802d4cc5ab39312b3a16a1ce9b286407d
Salvatore Previti <salvatore.previti@ag-grid.com>
2024-10-02 12:05:47 +0100

    fix tree data module

--- packages/ag-grid-enterprise/src/treeData/treeDataModule.ts
+++ packages/ag-grid-enterprise/src/treeData/treeDataModule.ts
@@ -1,10 +1,14 @@
-import { ClientSideRowModelModule } from 'ag-grid-community';
+import { ClientSideRowModelModule, ModuleNames } from 'ag-grid-community';
 
 import { defineEnterpriseModule } from '../moduleUtils';
 import { ClientSideChildrenTreeNodeManager } from './clientSideChildrenTreeNodeManager';
 import { ClientSidePathTreeNodeManager } from './clientSidePathTreeNodeManager';
 
-export const TreeDataModule = defineEnterpriseModule('TreeDataModule', {
+export const TreeDataCoreModule = defineEnterpriseModule('TreeDataModule', {
     beans: [ClientSidePathTreeNodeManager, ClientSideChildrenTreeNodeManager],
     dependsOn: [ClientSideRowModelModule],
+});
+
+export const TreeDataModule = defineEnterpriseModule(ModuleNames.TreeDataModule, {
+    dependsOn: [TreeDataCoreModule],
 });
