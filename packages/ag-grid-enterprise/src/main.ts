05d809b00bfc05e20d69909cc5b606b5ff26f3bc
Peter Reynolds <peterjrreynolds@gmail.com>
2024-10-07 09:37:57 +0100

    AG-12847 - update enterprise exports

--- packages/ag-grid-enterprise/src/main.ts
+++ packages/ag-grid-enterprise/src/main.ts
@@ -1,35 +1,10 @@
 export { GridLicenseManager as LicenseManager } from './license/gridLicenseManager';
-export { ILicenseManager } from './license/shared/licenseManager';
-
-// widgets shared across enterprise modules
-export { AgGroupComponent, AgGroupComponentSelector, AgGroupComponentParams } from './widgets/agGroupComponent';
-export { AgRichSelect } from './widgets/agRichSelect';
-export { PillDragComp } from './widgets/pillDragComp';
-export { PillDropZonePanel, PillDropZonePanelParams } from './widgets/pillDropZonePanel';
-export { AgDialog } from './widgets/agDialog';
-export { AgPanel } from './widgets/agPanel';
-export { VirtualList } from './widgets/virtualList';
-export { VirtualListModel } from './widgets/iVirtualList';
-
-export { AgMenuItemComponent, MenuItemActivatedEvent, CloseMenuEvent } from './widgets/agMenuItemComponent';
-export { AgMenuList } from './widgets/agMenuList';
-export { AgMenuPanel } from './widgets/agMenuPanel';
-export { AgMenuItemRenderer } from './widgets/agMenuItemRenderer';
-
-export { VirtualListDragItem, VirtualListDragParams } from './features/iVirtualListDragFeature';
-export { VirtualListDragFeature } from './features/virtualListDragFeature';
-
-export { TabbedItem } from './widgets/iTabbedLayout';
-export { TabbedLayout } from './widgets/tabbedLayout';
-
-export { GroupCellRenderer } from './rendering/groupCellRenderer';
-export { GroupCellRendererCtrl } from './rendering/groupCellRendererCtrl';
 
 export { getMultipleSheetsAsExcel, exportMultipleSheetsAsExcel } from './excelExport/excelCreator';
 
-export { MultiFilter } from './multiFilter/multiFilter';
+export type { MultiFilter } from './multiFilter/multiFilter';
 
-export { SetFilter } from './setFilter/setFilter';
+export type { SetFilter } from './setFilter/setFilter';
 
 export { EnterpriseCoreModule } from './agGridEnterpriseModule';
 export { AdvancedFilterModule } from './advancedFilter/advancedFilterModule';
