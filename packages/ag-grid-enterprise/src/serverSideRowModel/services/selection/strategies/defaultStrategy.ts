978417edecce8fac00bddb6705bcf202b0e7ec66
Andrew Glazier <andrew.glazier@ag-grid.com>
2024-10-08 14:44:42 +0100

    AG-7440 - Update types for messages without params

--- packages/ag-grid-enterprise/src/serverSideRowModel/services/selection/strategies/defaultStrategy.ts
+++ packages/ag-grid-enterprise/src/serverSideRowModel/services/selection/strategies/defaultStrategy.ts
@@ -59,13 +59,13 @@ export class DefaultStrategy extends BeanStub implements ISelectionStrategy {
 
         if (typeof state !== 'object') {
             // The provided selection state should be an object
-            _logError(115, {});
+            _logError(115);
             return;
         }
 
         if (!('selectAll' in state)) {
             //'Invalid selection state. The state must conform to `IServerSideSelectionState`.'
-            _logError(116, {});
+            _logError(116);
             return;
         }
 
@@ -73,7 +73,7 @@ export class DefaultStrategy extends BeanStub implements ISelectionStrategy {
             newState.selectAll = state.selectAll;
         } else {
             //selectAll must be of boolean type.
-            _logError(117, {});
+            _logError(117);
             return;
         }
 
