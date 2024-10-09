3f9b5f3fd0ffbb7645c7e77d37cb8bfde057c3b1
Tak Tran <contact@tutaktran.com>
2024-10-09 16:55:11 +0100

    AG-12915 - Redirect to archive version of website if version of the error is different to the deployed website grid version
    From the next major release v33.0.0 onwards, an error will redirect to the production website, and if it isn't the same version as production, it will redirect to the archive version of the website.

---
+++ documentation/ag-grid-docs/src/utils/getErrorRedirectBaseUrl.ts
@@ -0,0 +1,14 @@
+import { GRID_ARCHIVE_BASE_URL } from '@constants';
+
+interface Params {
+    errorVersion: string;
+    pageVersion: string;
+}
+
+export function getErrorRedirectBaseUrl({ errorVersion, pageVersion }: Params) {
+    if (errorVersion === pageVersion) {
+        return;
+    }
+
+    return `${GRID_ARCHIVE_BASE_URL}/${errorVersion}`;
+}
