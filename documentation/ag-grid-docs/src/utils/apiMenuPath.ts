4da5c727e806c53b03e501b2ea7164bf409bf15f
Mark Durrant <mark.j.durrant@gmail.com>
2024-10-10 15:35:27 +0100

    AG-12236 - Code cleanup

--- documentation/ag-grid-docs/src/utils/apiMenuPath.ts
+++ documentation/ag-grid-docs/src/utils/apiMenuPath.ts
@@ -1,20 +1,22 @@
-import { type MenuData } from '@ag-grid-types';
-
 function findSectionPaths(sections) {
+    // TODO: Understand why this is necessary
+    if (!Array.isArray(sections)) return;
+
     return sections.flatMap((section) => {
-        return section.items ? findSectionPaths(section.items) : section.path;
+        return section.children ? findSectionPaths(section.children) : section.path;
     });
 }
 
-export function getApiPaths(menuData: MenuData): string[] {
-    return findSectionPaths(menuData.api.sections);
+export function getApiPaths(menuData: any): string[] {
+    return findSectionPaths(menuData.sections);
 }
 
-export function isApiMenuPath({ pageName, menuData }: { pageName: string; menuData: MenuData }) {
+export function isApiMenuPath({ pageName, menuData }: { pageName: string; menuData: any }) {
     if (!pageName) {
         return false;
     }
 
     const apiPaths = getApiPaths(menuData);
+
     return apiPaths.includes(pageName);
 }
