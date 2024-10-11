c383a9322abd1263e500471b3af48bbfe2658cae
Tak Tran <contact@tutaktran.com>
2024-10-11 16:06:08 +0100

    AG-12915 - Redirect to archive version of website if version of the error is different to the deployed website grid version (#8961)
    * AG-12915 - Redirect to archive version of website if version of the error is different to the deployed website grid version
    
    From the next major release v33.0.0 onwards, an error will redirect to the production website, and if it isn't the same version as production, it will redirect to the archive version of the website.
    
    * AG-12915 - Add archive warning with message on error page

---
+++ documentation/ag-grid-docs/src/utils/getErrorRedirectBaseUrl.test.ts
@@ -0,0 +1,10 @@
+import { getErrorRedirectBaseUrl } from './getErrorRedirectBaseUrl';
+
+describe.each([
+    { errorVersion: '32.2.0', pageVersion: '32.2.0', output: undefined },
+    { errorVersion: '32.2.0', pageVersion: '33.0.0', output: 'https://ag-grid.com/archive/32.2.0' },
+])('getErrorRedirectBaseUrl', ({ errorVersion, pageVersion, output }) => {
+    it(`errorVersion: ${errorVersion} and pageVersion: ${pageVersion}, outputs ${output}`, () => {
+        expect(getErrorRedirectBaseUrl({ errorVersion, pageVersion })).toEqual(output);
+    });
+});
