254ac3281ff47b9ee9ca8e809e0e7e2c5637be8e
Mark Durrant <mark.j.durrant@gmail.com>
2024-10-14 16:28:36 +0100

    Ag 3390 algolia nav json bug (#8987) (#8993)
    * AG-3390 - WIP update algolia to use new menu json
    
    * AG-3390 - Fix Algolia docs nav json issues

--- documentation/update-algolia-indices/src/utils/constants.ts
+++ documentation/update-algolia-indices/src/utils/constants.ts
@@ -4,7 +4,8 @@ export const API_SOURCE_DIR = `${WORKING_DIR}/src/content/api-documentation`;
 export const DIST_DIR = `${WORKING_DIR}/dist`;
 export const API_REFERENCE_DIR = `${DIST_DIR}/files/reference`;
 
-export const MENU_FILE_PATH = `${WORKING_DIR}/src/content/menu/data.json`;
+export const MENU_FILE_PATH = `${WORKING_DIR}/src/content/docs-nav/docsNav.json`;
+export const API_FILE_PATH = `${WORKING_DIR}/src/content/api-nav/apiNav.json`;
 
 export const SUPPORTED_FRAMEWORKS = ['react', 'angular', 'vue', 'javascript'] as const;
 
