1b7eae76b8cb501bbfcc6679a17a1577fb576b8f
Tak Tran <contact@tutaktran.com>
2024-10-15 14:51:00 +0100

    AG-13081 - Add typescript eslint plugin for `@typescript-eslint/no-for-in-array` linting rule

--- packages/ag-grid-community/eslint.config.mjs
+++ packages/ag-grid-community/eslint.config.mjs
@@ -3,6 +3,14 @@ import rootESLint from '../../eslint.config.mjs';
 export default [
     ...rootESLint,
     {
+        languageOptions: {
+            parserOptions: {
+                // NOTE: Loading tsconfig.json references does not work, so
+                // loading explicitly here
+                project: ['./tsconfig.lib.json', './tsconfig.spec.json'],
+                tsconfigRootDir: import.meta.dirname,
+            },
+        },
         rules: {
             'no-fallthrough': 'warn',
             'no-case-declarations': 'warn',
@@ -11,19 +19,21 @@ export default [
             'no-useless-escape': 'warn',
             'prefer-spread': 'warn',
             'no-irregular-whitespace': 'warn',
-            '@typescript-eslint/ban-types': 'warn',
-            '@typescript-eslint/no-unused-vars': 'warn',
             'prefer-const': 'warn',
             'prefer-rest-params': 'warn',
+            '@typescript-eslint/ban-types': 'warn',
+            '@typescript-eslint/no-unused-vars': 'warn',
             '@typescript-eslint/no-var-requires': 'warn',
             '@typescript-eslint/prefer-as-const': 'warn',
             '@typescript-eslint/ban-ts-comment': 'warn',
             '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
             '@typescript-eslint/no-unnecessary-type-constraint': 'warn',
             '@typescript-eslint/no-this-alias': 'off',
+            // TODO
+            // '@typescript-eslint/no-for-in-array': 'error',
         },
     },
     {
-        ignores: ['webpack.config.js'],
+        ignores: ['webpack.config.js', 'jest.*.js', 'eslint.config.mjs'],
     },
 ];
