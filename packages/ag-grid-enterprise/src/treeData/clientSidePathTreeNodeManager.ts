cc77e7fc7cbe506dddfb4ca0135fa418f0deabd7
Salvatore Previti <salvatore.previti@ag-grid.com>
2024-10-15 20:10:06 +0100

    minor

--- packages/ag-grid-enterprise/src/treeData/clientSidePathTreeNodeManager.ts
+++ packages/ag-grid-enterprise/src/treeData/clientSidePathTreeNodeManager.ts
@@ -1,5 +1,5 @@
-import { _warnOnce } from 'ag-grid-community';
-import type { ChangedPath, NamedBean, RowNode, RowNodeTransaction } from 'ag-grid-community';
+import { _warn } from 'ag-grid-community';
+import type { ChangedPath, GetDataPath, NamedBean, RowNode, RowNodeTransaction } from 'ag-grid-community';
 
 import { AbstractClientSideTreeNodeManager } from './abstractClientSideTreeNodeManager';
 import type { TreeNode } from './treeNode';
@@ -19,7 +19,7 @@ export class ClientSidePathTreeNodeManager<TData>
 
         super.loadNewRowData(rowData);
 
-        this.addOrUpdateRows(rootNode.allLeafChildren!, false);
+        this.addOrUpdateRows(this.gos.get('getDataPath'), rootNode.allLeafChildren!, false);
 
         this.treeCommit();
     }
@@ -31,12 +31,13 @@ export class ClientSidePathTreeNodeManager<TData>
     ): void {
         this.treeRoot.setRow(this.rootNode);
 
+        const getDataPath = this.gos.get('getDataPath');
         for (const { remove, update, add } of transactions) {
             // the order of [add, remove, update] is the same as in ClientSideNodeManager.
             // Order is important when a record with the same id is added and removed in the same transaction.
             this.removeRows(remove as RowNode[] | null);
-            this.addOrUpdateRows(update as RowNode[] | null, true);
-            this.addOrUpdateRows(add as RowNode[] | null, false);
+            this.addOrUpdateRows(getDataPath, update as RowNode[] | null, true);
+            this.addOrUpdateRows(getDataPath, add as RowNode[] | null, false);
         }
 
         if (rowNodesOrderChanged) {
@@ -66,14 +67,14 @@ export class ClientSidePathTreeNodeManager<TData>
     }
 
     /** Transactional add/update */
-    private addOrUpdateRows(rows: RowNode[] | null, update: boolean): void {
-        const getDataPath = this.gos.get('getDataPath');
+    private addOrUpdateRows(getDataPath: GetDataPath | undefined, rows: RowNode[] | null, update: boolean): void {
         for (let i = 0, len = rows?.length ?? 0; i < len; ++i) {
             const row = rows![i];
 
-            const path = getDataPath?.(row.data) || EMPTY_ARRAY;
+            const data = row.data;
+            const path = getDataPath?.(data) || EMPTY_ARRAY;
             if (!path.length) {
-                _warnOnce(`getDataPath() should not return an empty path`, [row.data]);
+                _warn(185, { data });
             }
 
             // Gets the last node of a path. Inserts filler nodes where needed.
