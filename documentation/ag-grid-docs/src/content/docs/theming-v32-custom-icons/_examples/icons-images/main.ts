987f7483d07a562098ebc4bd7a3fca050c976918
Bernie Sumption <bernie@berniecode.com>
2024-10-17 15:58:36 +0100

    AG-10811 Remove v32 theme docs from menu

---
+++ documentation/ag-grid-docs/src/content/docs/theming-v32-custom-icons/_examples/icons-images/main.ts
@@ -0,0 +1,60 @@
+import type { ColDef, GridApi, GridOptions } from 'ag-grid-community';
+import { ClientSideRowModelModule, ModuleRegistry, createGrid } from 'ag-grid-community';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-quartz.css';
+import {
+    ColumnsToolPanelModule,
+    FiltersToolPanelModule,
+    GridChartsModule,
+    MenuModule,
+    RowGroupingModule,
+    SetFilterModule,
+} from 'ag-grid-enterprise';
+
+ModuleRegistry.registerModules([
+    ClientSideRowModelModule,
+    ColumnsToolPanelModule,
+    FiltersToolPanelModule,
+    GridChartsModule,
+    MenuModule,
+    RowGroupingModule,
+    SetFilterModule,
+]);
+
+const columnDefs: ColDef[] = [
+    { field: 'athlete', minWidth: 170 },
+    { field: 'age' },
+    { field: 'country' },
+    { field: 'year' },
+    { field: 'date' },
+    { field: 'sport' },
+    { field: 'gold' },
+    { field: 'silver' },
+    { field: 'bronze' },
+    { field: 'total' },
+];
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    theme: 'ag-theme-quartz',
+    rowData: null,
+    columnDefs: columnDefs,
+    defaultColDef: {
+        editable: true,
+        filter: true,
+    },
+    sideBar: true,
+    enableCharts: true,
+    cellSelection: true,
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data));
+});
