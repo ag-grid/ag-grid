1c462426319ecaa7917c3683325cf44a8d980930
Stephen Cooper <scooperdev@gmail.com>
2024-10-17 11:41:59 +0100

    Fix tests

--- packages/ag-grid-community/src/entities/rowNode.ts
+++ packages/ag-grid-community/src/entities/rowNode.ts
@@ -677,10 +677,10 @@ export class RowNode<TData = any> implements IEventEmitter<RowNodeEventType>, IR
                 this.dispatchCellChangedEvent(column, value, oldValue);
             };
 
-            for (const key of Object.keys(oldAggData)) {
+            for (const key of Object.keys(oldAggData ?? {})) {
                 eventFunc(key); // raise for old keys
             }
-            for (const key of Object.keys(newAggData)) {
+            for (const key of Object.keys(newAggData ?? {})) {
                 if (!oldAggData || !(key in oldAggData)) {
                     eventFunc(key); // new key, event not yet raised
                 }
