9f0d1bfbb6e3b239751ce5d1111934f6bcd84ab7
Elias <elias0789@gmail.com>
2024-10-21 14:49:20 +0100

    RTI-2371 Fix checkbox selection reactivity (#9065)

--- packages/ag-grid-community/src/selection/selectAllFeature.ts
+++ packages/ag-grid-community/src/selection/selectAllFeature.ts
@@ -2,7 +2,7 @@ import { isColumnSelectionCol } from '../columns/columnUtils';
 import { BeanStub } from '../context/beanStub';
 import type { BeanCollection } from '../context/context';
 import type { AgColumn } from '../entities/agColumn';
-import type { GridOptions, RowSelectionOptions, SelectAllMode } from '../entities/gridOptions';
+import type { SelectAllMode } from '../entities/gridOptions';
 import type { SelectionEventSourceType } from '../events';
 import {
     _getActiveDomElement,
@@ -29,7 +29,6 @@ export class SelectAllFeature extends BeanStub {
 
     private cbSelectAllVisible = false;
     private processingEventFromCheckbox = false;
-    private selectionOptions: RowSelectionOptions | undefined;
     private column: AgColumn;
     private headerCellCtrl: HeaderCellCtrl;
 
@@ -38,17 +37,6 @@ export class SelectAllFeature extends BeanStub {
     constructor(column: AgColumn) {
         super();
         this.column = column;
-    }
-
-    public postConstruct() {
-        const setRowSelectionOptions = (rowSelection: GridOptions['rowSelection']) => {
-            if (rowSelection && typeof rowSelection !== 'string') {
-                this.selectionOptions = rowSelection;
-            }
-        };
-
-        setRowSelectionOptions(this.gos.get('rowSelection'));
-        this.addManagedPropertyListener('rowSelection', (e) => setRowSelectionOptions(e.currentValue));
     }
 
     public onSpaceKeyDown(e: KeyboardEvent): void {
@@ -210,8 +198,8 @@ export class SelectAllFeature extends BeanStub {
      * or `headerCheckboxSelection` is enabled in the legacy API.
      */
     private isCheckboxSelection(): boolean {
-        const so = this.selectionOptions;
-        const newHeaderCheckbox = so && _getHeaderCheckbox(so) && isColumnSelectionCol(this.column);
+        const so = this.gos.get('rowSelection');
+        const newHeaderCheckbox = typeof so === 'object' && _getHeaderCheckbox(so) && isColumnSelectionCol(this.column);
         const headerCheckboxSelection = this.column.getColDef().headerCheckboxSelection;
 
         let result = false;
@@ -236,8 +224,8 @@ export class SelectAllFeature extends BeanStub {
     }
 
     private getSelectAllMode(): SelectAllMode {
-        const so = this.selectionOptions;
-        if (so) {
+        const so = this.gos.get('rowSelection');
+        if (typeof so === 'object') {
             return (so.mode === 'multiRow' && so.selectAll) || 'all';
         }
         const { headerCheckboxSelectionCurrentPageOnly, headerCheckboxSelectionFilteredOnly } = this.column.getColDef();
