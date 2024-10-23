e8b2b5e2eb6f324592c05910a0bdc592c6992792
seanlandsman <sean@thelandsmans.com>
2024-10-23 11:31:49 +0100

    Fix example issues caught by smoke tests

--- documentation/ag-grid-docs/src/content/docs/component-loading-cell-renderer/_examples/custom-cell-loading-renderer/main.ts
+++ documentation/ag-grid-docs/src/content/docs/component-loading-cell-renderer/_examples/custom-cell-loading-renderer/main.ts
@@ -1,11 +1,11 @@
 import type { GridApi, GridOptions, IServerSideDatasource, IServerSideGetRowsRequest } from 'ag-grid-community';
 import { createGrid } from 'ag-grid-community';
 import { ModuleRegistry } from 'ag-grid-community';
-import { ServerSideRowModelModule } from 'ag-grid-enterprise';
+import { ServerSideRowModelModule, RowGroupingModule } from 'ag-grid-enterprise';
 
 import { CustomLoadingCellRenderer } from './customLoadingCellRenderer_typescript';
 
-ModuleRegistry.registerModules([ServerSideRowModelModule]);
+ModuleRegistry.registerModules([ServerSideRowModelModule, RowGroupingModule]);
 
 let gridApi: GridApi<IOlympicData>;
 
