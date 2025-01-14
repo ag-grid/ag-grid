a1d67f801bf909da1cbe409cfe350b05b43a7362
Steph Meslin-Weber <steph@tangency.co.uk>
2025-01-14 12:18:23 +0000

    AG-12904 - remove copy-pasta comment

--- documentation/ag-grid-docs/src/content/docs/change-cell-renderers/_examples/customising-flashing/random.ts
+++ documentation/ag-grid-docs/src/content/docs/change-cell-renderers/_examples/customising-flashing/random.ts
@@ -1,4 +1,3 @@
 // The random data is generated through a seeded random generator for predictable randomness for reproducibility. Do not use this in production.
-// might generate sql queries from the Server-Side Row Model request.
 // To keep things simple it patches the Math.random function to use the seeded random generator from 'seedrandom'.
 Math.seedrandom('AG Grid Random Seed');
