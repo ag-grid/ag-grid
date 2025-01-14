192f07458998601355b69f1711b8c7d0a5379acd
Elias Malik <elias.malik@ag-grid.com>
2025-01-14 16:03:17 +0000

    update test

--- testing/behavioural/src/selection/selection-column.test.ts
+++ testing/behavioural/src/selection/selection-column.test.ts
@@ -1,6 +1,6 @@
 import type { MockInstance } from 'vitest';
 
-import type { AgEvent, GridApi, GridOptions } from 'ag-grid-community';
+import type { GridApi, GridOptions } from 'ag-grid-community';
 import { ClientSideRowModelModule } from 'ag-grid-community';
 
 import { GridRows, TestGridsManager } from '../test-utils';
@@ -78,7 +78,7 @@ describe('Row Selection Grid Options', () => {
         `);
     });
 
-    test('Selection column is updated when `selectionColDef` changes', () => {
+    test('Selection column width and pinning is updated when `selectionColDef` changes', () => {
         const api = createGrid({
             columnDefs,
             rowData,
@@ -86,30 +86,20 @@ describe('Row Selection Grid Options', () => {
             selectionColumnDef: {},
         });
 
-        expect(api.getColumnState()[0]).toEqual(
-            expect.objectContaining({
-                pinned: null,
-                width: 50,
-            })
-        );
+        gridMgr;
+
+        expect(api.isPinning()).toBe(false);
 
         const col = api.getColumn('ag-Grid-SelectionColumn');
-
-        const events: AgEvent<'columnStateUpdated'>[] = [];
-        col?.addEventListener('columnStateUpdated', (e) => events.push(e));
 
         api.setGridOption('selectionColumnDef', {
             pinned: 'right',
             width: 200,
         });
 
-        expect(api.getColumnState()[0]).toEqual(
-            expect.objectContaining({
-                pinned: 'right',
-                width: 200,
-            })
-        );
-
-        expect(events.map((e) => (e as any).key).sort()).toEqual(['pinned', 'width']);
+        expect(api.isPinning()).toBe(true);
+        expect(api.isPinningRight()).toBe(true);
+        expect(col?.isPinnedRight()).toBe(true);
+        expect(col?.getActualWidth()).toBe(200);
     });
 });
