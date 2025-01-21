e3dc5801834528e9a0b4985771ea7e9455b8cd50
Mark Durrant <mark.j.durrant@gmail.com>
2025-01-21 17:01:58 +0000

    AG-13867 - Refactor expanding section component from React to .astro component

--- external/ag-website-shared/src/markdoc/tags/expandingSection.ts
+++ external/ag-website-shared/src/markdoc/tags/expandingSection.ts
@@ -3,7 +3,7 @@ import type { Render } from '@astrojs/markdoc/config';
 import type { Config, Schema } from '@markdoc/markdoc';
 
 export const expandingSection: Schema<Config, Render> = {
-    render: component('../../external/ag-website-shared/src/components/expanding-section/ExpandingSection'),
+    render: component('../../external/ag-website-shared/src/components/expanding-section/ExpandingSection.astro'),
     attributes: {
         headerText: { type: String },
         isOpen: { type: Boolean },
