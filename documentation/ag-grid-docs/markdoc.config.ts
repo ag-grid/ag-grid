5182fbd1208e7b4a40ca689da6a3e5509587e7ea
Tak Tran <contact@tutaktran.com>
2025-01-22 12:41:33 +0000

    AG-13911 - First pass of trial form component

--- documentation/ag-grid-docs/markdoc.config.ts
+++ documentation/ag-grid-docs/markdoc.config.ts
@@ -15,6 +15,7 @@ import { note } from '@ag-website-shared/markdoc/tags/note';
 import { oneTrustCookies } from '@ag-website-shared/markdoc/tags/oneTrustCookies';
 import { openInCTA } from '@ag-website-shared/markdoc/tags/openInCTA';
 import { tabItem, tabs } from '@ag-website-shared/markdoc/tags/tabs';
+import { trialLicenceForm } from '@ag-website-shared/markdoc/tags/trialLicenceForm';
 import { video } from '@ag-website-shared/markdoc/tags/video';
 import { videoSection } from '@ag-website-shared/markdoc/tags/videoSection';
 import { warning } from '@ag-website-shared/markdoc/tags/warning';
@@ -74,6 +75,7 @@ export default defineMarkdocConfig({
         expandingSection,
         documentationArchiveSection: getDocumentationArchiveSection('grid'),
         changelogSection: getChangelogSection('grid'),
+        trialLicenceForm,
         enterpriseIcon,
         video,
         licenseSetup: {
