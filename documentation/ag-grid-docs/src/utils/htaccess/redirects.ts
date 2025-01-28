939c82e91df983920183467f3e5a9a03ed8041f3
James Swinton-Bland <james_swinton@hotmail.co.uk>
2025-01-28 09:40:56 +0000

    Initial pass of content changes

--- documentation/ag-grid-docs/src/utils/htaccess/redirects.ts
+++ documentation/ag-grid-docs/src/utils/htaccess/redirects.ts
@@ -2702,6 +2702,11 @@ export const SITE_301_REDIRECTS: Redirect[] = [
     { from: '/javascript-data-grid/whats-new', to: '/whats-new' },
     { from: '/vue-data-grid/framework-data-flow', to: '/vue-data-grid/getting-started/' },
 
+    { from: '/react-data-grid/licensing/', to: '/react-data-grid/community-vs-enterprise/' },
+    { from: '/vue-data-grid/licensing/', to: '/vue-data-grid/community-vs-enterprise/' },
+    { from: '/angular-data-grid/licensing/', to: '/angular-data-grid/community-vs-enterprise/' },
+    { from: '/javascript-data-grid/licensing/', to: '/javascript-data-grid/community-vs-enterprise/' },
+
     ...pageForAllFrameworks('range-selection', 'cell-selection'),
     ...pageForAllFrameworks('range-selection-handle', 'cell-selection-handle'),
     ...pageForAllFrameworks('range-selection-fill-handle', 'cell-selection-fill-handle'),
