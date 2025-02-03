5de58e7a32bd8a08da782c4f34737b304fbd5a55
Peter Reynolds <peterjrreynolds@gmail.com>
2025-02-03 17:27:41 +0000

    AG-119 - renderer updates

--- packages/ag-grid-community/src/search/searchCellRenderer.ts
+++ packages/ag-grid-community/src/search/searchCellRenderer.ts
@@ -21,10 +21,10 @@ export class SearchCellRenderer extends Component implements ICellRenderer {
         const displayValue = valueFormatted ?? value ?? '';
         const valueToSearch = _escapeString(displayValue, true)?.toLocaleUpperCase() ?? '';
         const eGui = this.getGui();
-        _clearElement(eGui);
         if (_missing(searchText)) {
+            _clearElement(eGui);
             const eSpan = document.createElement('span');
-            eSpan.textContent = _escapeString(displayValue);
+            eSpan.textContent = _escapeString(displayValue, true);
             eGui.appendChild(eSpan);
             return true;
         }
@@ -32,31 +32,48 @@ export class SearchCellRenderer extends Component implements ICellRenderer {
         let lastIndex = 0;
         let currentMatchNum = 0;
         const searchTextLength = searchText.length;
+        const parts: { value: string; match?: boolean; activeMatch?: boolean }[] = [];
         while (true) {
             const index = valueToSearch.indexOf(searchText, lastIndex === 0 ? 0 : lastIndex + 1);
             if (index != -1) {
                 currentMatchNum++;
                 if (index > lastIndex) {
-                    const eSpan = document.createElement('span');
-                    eSpan.textContent = _escapeString(displayValue.slice(lastIndex, index));
-                    eGui.appendChild(eSpan);
+                    parts.push({ value: displayValue.slice(lastIndex, index) });
                 }
                 const endIndex = index + searchTextLength;
-                const eSpan = document.createElement('span');
-                eSpan.textContent = _escapeString(displayValue.slice(index, endIndex));
-                eSpan.classList.add('ag-search-match');
-                if (currentMatchNum === activeMatchNum) {
-                    eSpan.classList.add('ag-search-active-match');
-                }
-                eGui.appendChild(eSpan);
+                parts.push({
+                    value: displayValue.slice(index, endIndex),
+                    match: true,
+                    activeMatch: currentMatchNum === activeMatchNum,
+                });
                 lastIndex = endIndex;
             } else {
                 if (lastIndex < displayValue.length) {
-                    const eSpan = document.createElement('span');
-                    eSpan.textContent = _escapeString(displayValue.slice(lastIndex));
-                    eGui.appendChild(eSpan);
+                    parts.push({
+                        value: displayValue.slice(lastIndex),
+                    });
                 }
                 break;
+            }
+        }
+
+        const numParts = parts.length;
+        const eChildren = eGui.children;
+        for (let i = 0; i < numParts; i++) {
+            const { value: partValue, match, activeMatch } = parts[i];
+            let eSpan = eChildren.item(i);
+            if (!eSpan) {
+                eSpan = document.createElement('span');
+                eGui.appendChild(eSpan);
+            }
+            eSpan.textContent = _escapeString(partValue, true);
+            eSpan.classList.toggle('ag-search-match', !!match);
+            eSpan.classList.toggle('ag-search-active-match', !!activeMatch);
+        }
+        const numEChildren = eChildren.length;
+        if (numEChildren > numParts) {
+            for (let i = numParts; i < numEChildren; i++) {
+                eGui.removeChild(eChildren.item(i)!);
             }
         }
 
