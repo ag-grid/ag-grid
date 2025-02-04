f34210f7b0138869eea8fc318f8ec1d00707e029
Guilherme Lopes <guilherme@gwm-solutions.com>
2025-02-04 07:41:08 -0600

    AG-227 - removed cellSelection dependency

--- packages/ag-grid-enterprise/src/rowHeaderColumn/rowHeaderColumnModule.ts
+++ packages/ag-grid-enterprise/src/rowHeaderColumn/rowHeaderColumnModule.ts
@@ -2,7 +2,6 @@ import type { _ModuleWithoutApi } from 'ag-grid-community';
 import { CellStyleModule } from 'ag-grid-community';
 
 import { EnterpriseCoreModule } from '../agGridEnterpriseModule';
-import { CellSelectionModule } from '../rangeSelection/rangeSelectionModule';
 import { VERSION } from '../version';
 import { RowHeaderColService } from './rowHeaderColService';
 import { rowHeaderColumnCSS } from './rowHeaderColumn.css-GENERATED';
@@ -15,6 +14,6 @@ export const RowHeaderColumnModule: _ModuleWithoutApi = {
     moduleName: 'RowHeaderColumn',
     version: VERSION,
     beans: [RowHeaderColService],
-    dependsOn: [EnterpriseCoreModule, CellSelectionModule, CellStyleModule],
+    dependsOn: [EnterpriseCoreModule, CellStyleModule],
     css: [rowHeaderColumnCSS],
 };
