427b0d596a14b9d044888c069a6d9b96bb7b21cd
Kyler Phillips <kylerphillips@hotmail.co.uk>
2025-02-05 16:29:19 +0000

    AG-14015 Add links

--- external/ag-website-shared/src/components/features-section/DocsFeaturesSection.tsx
+++ external/ag-website-shared/src/components/features-section/DocsFeaturesSection.tsx
@@ -1,3 +1,5 @@
+import { gridUrlWithPrefix } from '@ag-website-shared/utils/gridUrlWithPrefix';
+import { useFrameworkFromStore } from '@utils/hooks/useFrameworkFromStore';
 import type { FunctionComponent } from 'react';
 
 import styles from './features.module.scss';
@@ -7,11 +9,12 @@ interface Props {
 }
 
 function Section({ index, feature }) {
+    const framework = useFrameworkFromStore();
     return (
-        <div key={index} className={styles.card}>
+        <a key={index} className={styles.card} href={gridUrlWithPrefix({ framework, url: feature.link })}>
             <h4 className={styles.title}>{feature.title}</h4>
             <p className={styles.description}>{feature.description}</p>
-        </div>
+        </a>
     );
 }
 
@@ -24,18 +27,22 @@ const FeaturesSection: FunctionComponent<Props> = ({ type }) => {
         {
             title: 'Cell Rendering',
             description: 'Custom cell rendering - use your own components inside cells.',
+            link: './component-cell-renderer',
         },
         {
             title: 'Themes and Styling',
             description: 'Pre-built themes and CSS customization.',
+            link: './theming',
         },
         {
             title: 'Accessibility Support',
             description: 'ARIA support and keyboard navigation.',
+            link: './accessibility',
         },
         {
             title: 'Performance Optimisations',
             description: 'Column and row virtualization enabled by default.',
+            link: './dom-virtualisation',
         },
         {
             title: 'Major Frameworks',
@@ -47,42 +54,52 @@ const FeaturesSection: FunctionComponent<Props> = ({ type }) => {
         {
             title: 'Server-Side Row Model',
             description: 'Efficiently handle large datasets by loading data on-demand from the server.',
+            link: './server-side-model',
         },
         {
             title: 'Excel Export',
             description: 'Advanced export options, including styles and formulas.',
+            link: './excel-export',
         },
         {
             title: 'Pivot Tables & Aggregations',
             description: 'Built-in pivot and aggregation capabilities for complex data analysis.',
+            link: './pivoting',
         },
         {
             title: 'Range Selection',
             description: 'Select and manipulate ranges of data.',
+            link: './cell-selection',
         },
         {
             title: 'Integrated Charts',
             description: 'Create and customize charts directly within the grid.',
+            link: './integrated-charts',
         },
         {
             title: 'Master/Detail View',
             description: 'Nested grid views for hierarchical data representation.',
+            link: './master-detail',
         },
         {
             title: 'Row Grouping & Multi-Column Sorting',
             description: 'Advanced grouping and sorting options for better data organization.',
+            link: './grouping',
         },
         {
             title: 'Clipboard Operations',
             description: 'Enhanced copy and paste functionality, including support for Excel-like behaviour.',
+            link: './clipboard',
         },
         {
             title: 'Tool Panels',
             description: 'Built-in configuration panels for columns and filters, or create your own custom panels.',
+            link: './tool-panel',
         },
         {
             title: 'Custom Context Menu & Sidebars',
             description: 'Additional UI elements for enhanced user interaction and customization.',
+            link: './context-menu',
         },
     ];
 
