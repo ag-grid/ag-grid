c394923bce35997de09501afdaaa40b111264d50
Kyler Phillips <kylerphillips@hotmail.co.uk>
2025-02-05 15:25:07 +0000

    AG-14015 Update rest of casing

--- external/ag-website-shared/src/markdoc/tags/featuresSection.ts
+++ external/ag-website-shared/src/markdoc/tags/featuresSection.ts
@@ -2,7 +2,7 @@ import { component } from '@astrojs/markdoc/config';
 import type { Render } from '@astrojs/markdoc/config';
 import type { Config, Schema } from '@markdoc/markdoc';
 
-export const FeaturesSection: Schema<Config, Render> = {
+export const featuresSection: Schema<Config, Render> = {
     render: component('../../external/ag-website-shared/src/components/features-section/DocsFeaturesSection'),
     attributes: {
         type: { type: String, required: true },
