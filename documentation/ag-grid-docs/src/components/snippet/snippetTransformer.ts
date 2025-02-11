e423669d6e20860b25c97db408547f193e228b3b
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-02-11 10:59:47 +0000

    improve useMemo detection by checking if the value is a js literal

--- documentation/ag-grid-docs/src/components/snippet/snippetTransformer.ts
+++ documentation/ag-grid-docs/src/components/snippet/snippetTransformer.ts
@@ -204,7 +204,7 @@ class ReactTransformer extends SnippetTransformer {
             return `const [${propName}, ${setterPropName}] = useState(${decreaseIndent(value)});`;
         }
 
-        if (isUseMemoProp(propName)) {
+        if (isUseMemoProp(propName) && !isJsLiteralValue(value)) {
             return `const ${propName} = useMemo(() => { \n\treturn ${value};\n}, []);`;
         }
 
@@ -244,6 +244,26 @@ class ReactTransformer extends SnippetTransformer {
         return ''; // react comments are added in-place
     }
 }
+
+const isJsLiteralValue = (value: string) => {
+    value = value.trim();
+    if (value === 'null' || value === 'undefined') {
+        return true; // null or undefined
+    }
+    if (value === 'true' || value === 'false') {
+        return true; // A boolean literal
+    }
+    if (value.startsWith('"') && value.endsWith('"')) {
+        return true; // A string literal
+    }
+    if (value.startsWith("'") && value.endsWith("'")) {
+        return true; // A string literal
+    }
+    if (!isNaN(value as any) || value === 'NaN') {
+        return true; // A number
+    }
+    return false;
+};
 
 // This function associates comments with the correct node as comments returned in a separate array by 'esprima'
 const addCommentsToTree = (tree) => {
@@ -304,6 +324,7 @@ const isUseMemoProp = (propName) =>
         'sideBar',
         'statusBar',
         'autoSizeStrategy',
+        'rowNumbers',
     ].includes(propName);
 
 const isUseCallbackProp = (propName) => ['getDataPath'].includes(propName);
