ee885ffd46f38e77307a78bea92c3b2b110a8868
Elias Malik <elias.malik@ag-grid.com>
2025-02-11 11:35:16 +0000

    AG-10592 add selection state to event object

--- packages/ag-grid-community/src/events.ts
+++ packages/ag-grid-community/src/events.ts
@@ -15,6 +15,7 @@ import type { BuildEventTypeMap } from './interfaces/iEventEmitter';
 import type { IFilterComp } from './interfaces/iFilter';
 import type { FindMatch } from './interfaces/iFind';
 import type { IRowNode, RowPinnedType } from './interfaces/iRowNode';
+import type { IServerSideGroupSelectionState, IServerSideSelectionState } from './interfaces/iServerSideSelection';
 import type { RowNodeTransaction } from './interfaces/rowNodeTransaction';
 import type { ServerSideTransactionResult } from './interfaces/serverSideTransaction';
 
@@ -333,6 +334,7 @@ export type SelectionEventSourceType =
 export interface SelectionChangedEvent<TData = any, TContext = any>
     extends AgGlobalEvent<'selectionChanged', TData, TContext> {
     source: SelectionEventSourceType;
+    state: string[] | IServerSideSelectionState | IServerSideGroupSelectionState | null;
 }
 
 export type FilterChangedEventSourceType = 'api' | 'quickFilter' | 'columnFilter' | 'advancedFilter';
