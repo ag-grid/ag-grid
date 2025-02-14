05605542d5e4d8593301d5b557a8d456c4e832c5
Tak Tran <contact@tutaktran.com>
2025-02-14 14:57:58 +0000

    AG-14096 - Update with integrated charts medium breakpoint

--- external/ag-website-shared/src/components/automated-examples/lib/isMobile.ts
+++ external/ag-website-shared/src/components/automated-examples/lib/isMobile.ts
@@ -1,5 +1,12 @@
 import breakpoints from '@design-system/breakpoint.module.scss';
 
-const AUTOMATED_EXAMPLE_MEDIUM_WIDTH = parseInt(breakpoints['automated-row-grouping-medium'], 10);
+const AUTOMATED_ROW_GROUPING_MEDIUM_WIDTH = parseInt(breakpoints['automated-row-grouping-medium'], 10);
+const AUTOMATED_INTEGRATED_CHARTS_MEDIUM_WIDTH = parseInt(breakpoints['automated-integrated-charts-medium'], 10);
 
-export const isMobile = () => window.innerWidth <= AUTOMATED_EXAMPLE_MEDIUM_WIDTH;
+type ExampleType = 'rowGrouping' | 'integratedCharts';
+
+export const isMobile = (type: ExampleType) => {
+    const maxWidth =
+        type === 'rowGrouping' ? AUTOMATED_ROW_GROUPING_MEDIUM_WIDTH : AUTOMATED_INTEGRATED_CHARTS_MEDIUM_WIDTH;
+    return window.innerWidth <= maxWidth;
+};
