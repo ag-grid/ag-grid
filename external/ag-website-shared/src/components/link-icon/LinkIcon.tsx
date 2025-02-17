daa9d72f8c637c9c719d74231417f437dc52dca5
Mark Durrant <mark.j.durrant@gmail.com>
2025-02-17 17:08:25 +0000

    AG-11159 - WIP header anchor links copy to clipboard

--- external/ag-website-shared/src/components/link-icon/LinkIcon.tsx
+++ external/ag-website-shared/src/components/link-icon/LinkIcon.tsx
@@ -5,8 +5,21 @@ import type { AllHTMLAttributes } from 'react';
 import styles from './LinkIcon.module.scss';
 
 export function LinkIcon({ className, ...props }: AllHTMLAttributes<HTMLAnchorElement> & { children?: never }) {
+    const onclick = (event) => {
+        event.preventDefault();
+
+        const href = event.target.href;
+
+        navigator.clipboard.writeText(href);
+    };
+
     return (
-        <a aria-label="Heading link" {...props} className={classnames(styles.docsHeaderIcon, className)}>
+        <a
+            aria-label="Heading link"
+            {...props}
+            className={classnames(styles.docsHeaderIcon, className)}
+            onClick={onclick}
+        >
             <Icon name="link" />
         </a>
     );
