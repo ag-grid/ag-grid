d2b72d01b7d7f7135032f49772b3d12488b8e47b
Stephen Cooper <scooperdev@gmail.com>
2025-02-17 17:29:49 +0000

    Update one slice exception not loading from 0

--- documentation/ag-grid-docs/src/content/docs/row-sorting/_examples/custom-sorting/main.ts
+++ documentation/ag-grid-docs/src/content/docs/row-sorting/_examples/custom-sorting/main.ts
@@ -57,5 +57,5 @@ document.addEventListener('DOMContentLoaded', function () {
 
     fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
         .then((response) => response.json())
-        .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data.slice(5, 10)));
+        .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data.slice(0, 10)));
 });
