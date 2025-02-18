c183aeff2795bdba26a44e8ceb1795f9c785938c
Stephen Cooper <scooperdev@gmail.com>
2025-02-18 20:21:34 +0000

    remove and correct types

--- documentation/ag-grid-docs/src/content/docs/deep-dive/_examples/cell-renderer-example/main.ts
+++ documentation/ag-grid-docs/src/content/docs/deep-dive/_examples/cell-renderer-example/main.ts
@@ -1,5 +1,4 @@
 import type {
-    ColDef,
     GridApi,
     GridOptions,
     ICellRendererComp,
@@ -58,9 +57,9 @@ interface IRow {
     successful: boolean;
 }
 
-const gridOptions: GridOptions = {
+const gridOptions: GridOptions<IRow> = {
     // Data to be displayed
-    rowData: [] as IRow[],
+    rowData: [],
     // Columns to be displayed (Should match rowData properties)
     columnDefs: [
         {
@@ -83,11 +82,11 @@ const gridOptions: GridOptions = {
         },
         { field: 'successful' },
         { field: 'rocket' },
-    ] as ColDef[],
+    ],
     // Configurations applied to all columns
     defaultColDef: {
         filter: true,
-    } as ColDef,
+    },
     // Grid Options & Callbacks
     pagination: true,
 };
