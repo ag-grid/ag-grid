4ccd5ed9a8124c1ffca401accc83e7ebb48b54e2
Kyler Phillips <kylerphillips@hotmail.co.uk>
2025-02-26 11:01:51 +0000

    AG-14256 Remove email from plausible tracking

--- external/ag-website-shared/src/components/trial-licence-form/TrialLicenceForm.tsx
+++ external/ag-website-shared/src/components/trial-licence-form/TrialLicenceForm.tsx
@@ -159,7 +159,6 @@ function useTrialForm() {
                 } else {
                     setFormState('success');
                     trackTrialLicenseFormSuccess({
-                        email,
                         page: currentPage,
                     });
                 }
