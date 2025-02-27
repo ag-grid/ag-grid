8aea0c435e2f06ca581dbdaaf045d105078ad0ec
Mark Durrant <mark.j.durrant@gmail.com>
2025-02-27 10:24:42 +0000

    AG-11159 - Add tooltips to docs header link icons

--- external/ag-website-shared/src/components/link-icon/LinkIcon.tsx
+++ external/ag-website-shared/src/components/link-icon/LinkIcon.tsx
@@ -1,25 +1,47 @@
 import { Icon } from '@ag-website-shared/components/icon/Icon';
 import classnames from 'classnames';
-import type { AllHTMLAttributes } from 'react';
+import { type AllHTMLAttributes, useEffect, useRef, useState } from 'react';
 
 import styles from './LinkIcon.module.scss';
 
 export function LinkIcon({ className, ...props }: AllHTMLAttributes<HTMLAnchorElement> & { children?: never }) {
+    const [linkCopied, setLinkCopied] = useState(false);
+    const timeoutRef = useRef(null);
+
     const onclick = (event) => {
         event.preventDefault();
 
         const href = event.target.href;
 
         navigator.clipboard.writeText(href);
+
+        setLinkCopied(true);
+
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+
+        timeoutRef.current = setTimeout(() => {
+            setLinkCopied(false);
+        }, 2000);
     };
+
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) {
+                clearTimeout(timeoutRef.current);
+            }
+        };
+    }, []);
 
     return (
         <a
             aria-label="Heading link"
             {...props}
-            className={classnames(styles.docsHeaderIcon, className)}
+            className={classnames(styles.docsHeaderIcon, { [styles.active]: linkCopied }, className)}
             onClick={onclick}
         >
+            <span className={styles.tooltip}>{linkCopied ? 'Link copied' : 'Copy link'}</span>
             <Icon name="link" />
         </a>
     );
