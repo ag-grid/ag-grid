1d790453b96600fd071314d73ea945cbc03ddd79
Mark Durrant <mark.j.durrant@gmail.com>
2025-03-11 10:40:28 +0000

    AG-13628 - Refactor to use `majorTable` component

--- external/ag-website-shared/src/markdoc/tags/migrationGuides.ts
+++
@@ -1,7 +0,0 @@
-import { component } from '@astrojs/markdoc/config';
-import type { Render } from '@astrojs/markdoc/config';
-import type { Config, Schema } from '@markdoc/markdoc';
-
-export const migrationGuides: Schema<Config, Render> = {
-    render: component('../../external/ag-website-shared/src/components/migration-guides/MigrationGuides.astro'),
-};
