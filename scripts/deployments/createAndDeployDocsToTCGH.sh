b2b8d28ca55b11d49bbd6bb053369eacfa966f0d
seanlandsman <sean@thelandsmans.com>
2025-03-18 13:34:24 +0000

    AG-13989 Migrate CI to GH Actions

--- scripts/deployments/createAndDeployDocsToTCGH.sh
+++ scripts/deployments/createAndDeployDocsToTCGH.sh
@@ -20,7 +20,7 @@ zip -qr ../../../$FILENAME *
 
 cd ../../../
 
-scp -i $SSH_KEY_LOCATION StrictHostKeyChecking=no $FILENAME $SSH_USER@$SSH_HOST:$WWW_ROOT_DIR
+scp -i $SSH_KEY_LOCATION -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null $FILENAME $SSH_USER@$SSH_HOST:$WWW_ROOT_DIR
 #echo "Backing up branch builds"
 #ssh -i ./staging.pem $SSH_HOST "chmod +x $WWW_ROOT_DIR/switchReleaseRemote.sh"
 #cp -R /var/www/html/branch-builds /var/www/
