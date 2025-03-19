145d2ebf488588aef79aba34247621ba55e7eedd
Tak Tran <contact@tutaktran.com>
2025-03-19 10:58:20 +0000

    AG-14059 - Allow simple objects/arrays to be logged

--- external/ag-website-shared/src/components/example-runner/components/ExampleLogger.tsx
+++ external/ag-website-shared/src/components/example-runner/components/ExampleLogger.tsx
@@ -2,11 +2,19 @@ import { type FunctionComponent, useEffect, useLayoutEffect, useRef, useState } 
 
 import styles from './ExampleLogger.module.scss';
 
+type LogObject = {
+    __consoleLogObject: true;
+    isLoggable: boolean;
+    argType: string;
+    safeString: string;
+};
+type LogData = string | number | undefined | null | LogObject;
+
 interface Log {
     type: 'console-log';
     pageName?: string;
     exampleName: string;
-    data: any[];
+    data: LogData[];
 }
 
 interface Props {
@@ -14,12 +22,35 @@ interface Props {
     bufferSize?: number;
 }
 
+const SEE_DEV_CONSOLE_MESSAGE = '[Object] - see developer console';
 const IGNORED_MESSAGES = ['Angular is running in development mode.'];
 
 function containsIgnoredMessage(log: Log) {
     return log.data.some((message) =>
         IGNORED_MESSAGES.some((ignoredMessage) => typeof message === 'string' && message.includes(ignoredMessage))
     );
+}
+
+function getLoggableData(data: LogData[]) {
+    const isLoggable = data.every((logItem: LogData) => {
+        const consoleLogObject = logItem as LogObject;
+        return consoleLogObject?.__consoleLogObject ? consoleLogObject.isLoggable : true;
+    });
+
+    return isLoggable
+        ? data.map((logItem: LogData) => {
+              const consoleLogObject = logItem as LogObject;
+              if (logItem && consoleLogObject.__consoleLogObject) {
+                  return consoleLogObject.safeString;
+              } else if (logItem === null) {
+                  return 'null';
+              } else if (logItem === undefined) {
+                  return 'undefined';
+              } else {
+                  return logItem;
+              }
+          })
+        : [SEE_DEV_CONSOLE_MESSAGE];
 }
 
 export const ExampleLogger: FunctionComponent<Props> = ({ exampleName, bufferSize = 20 }) => {
@@ -33,7 +64,11 @@ export const ExampleLogger: FunctionComponent<Props> = ({ exampleName, bufferSiz
                 setLogs((prevLogs) => {
                     const bufferedLogs = prevLogs.length >= bufferSize ? prevLogs.slice(1) : prevLogs;
 
-                    return [...bufferedLogs, log];
+                    const newLog = {
+                        ...log,
+                        data: getLoggableData(log.data),
+                    };
+                    return [...bufferedLogs, newLog];
                 });
             }
         };
@@ -55,7 +90,7 @@ export const ExampleLogger: FunctionComponent<Props> = ({ exampleName, bufferSiz
             {logs.length === 0 && <div className={styles.noLogs}>Console logs from the example shown here...</div>}
             {logs.length > 0 && <div className={styles.consoleHeader}>Console</div>}
             {logs.map((log, i) => (
-                <div key={i}>{log.data.join(', ')}</div>
+                <div key={i}>{log.data.join(' ')}</div>
             ))}
         </pre>
     );
--- plugins/ag-grid-generate-example-files/src/executors/generate/generator/utils/getConsoleLogSnippet.ts
+++ plugins/ag-grid-generate-example-files/src/executors/generate/generator/utils/getConsoleLogSnippet.ts
@@ -11,31 +11,85 @@ export const CONSOLE_LOG_END = '/** CONSOLE LOG END **/';
  */
 export const getConsoleLogSnippet = ({ pageName, exampleName }: Params) =>
     `${CONSOLE_LOG_START}
+const PRIMITIVE_TYPES = ["string", "number", "boolean", "undefined", "null", "NaN", "symbol"];
+const OBJECT_PROPERTIES_LIMIT = 3;
+
+function getType(value) {
+    if (value === null) return "null";
+    if (Number.isNaN(value)) return "NaN";
+    if (Array.isArray(value)) return "array";
+    if (value instanceof Date) return "date";
+    if (value instanceof RegExp) return "regexp";
+    if (value instanceof Map) return "map";
+    if (value instanceof Set) return "set";
+    if (value instanceof WeakMap) return "weakmap";
+    if (value instanceof WeakSet) return "weakset";
+    if (value instanceof Promise) return "promise";
+    if (value instanceof Error) return "error";
+    if (typeof value === "object") return "object";
+    return typeof value;
+}
+
 function safeStringify(obj, space = 2) {
     const seen = new WeakSet();
-    return JSON.stringify(
+    const isLoggableArray = getType(obj) === "array" && obj.every(isLoggableType);
+    const getObjectValue = (value) => {
+        if (seen.has(value)) {
+            return '[Circular]';
+        } else if (value === undefined) {
+            return 'undefined';
+        }
+        seen.add(value);
+
+        // Include custom class names if available
+        if (value.constructor && value.constructor.name && getType(value) !== 'object') {
+            return \`\${value.constructor.name}Class { ... }\`;
+        }
+
+        return value;
+    };
+    return isLoggableArray ? JSON.stringify(obj) : JSON.stringify(
         obj,
         (_, value) => {
-            if (typeof value === 'object' && value !== null) {
-                if (seen.has(value)) {
-                    return '[Circular]';
-                }
-                seen.add(value);
-
-                // Include custom class names if available
-                if (value.constructor && value.constructor.name && value.constructor.name !== 'Object') {
-                    return \`\${value.constructor.name}Class { ... }\`;
-                }
+            const valueType = getType(value);
+            let newValue = value;
+            if (valueType === 'object') {
+                newValue = getObjectValue();
+            } else if (valueType === 'array') {
+                newValue = value.map((item) => {
+                    return getType(item === 'object') ? getObjectValue(item) : item;
+                });
             }
-            return value;
+
+            return newValue;
         },
         space
     );
 }
 
-function safeLogData(data) {
-    if (typeof data === 'string' || typeof data === 'number') return data;
-    return safeStringify(data);
+function isPrimitiveType(value) {
+    return PRIMITIVE_TYPES.includes(getType(value));
+}
+
+function isLoggableType(value) {
+    const valueType = getType(value);
+
+    return isPrimitiveType(value)
+        || (valueType === "array" && value.every(isPrimitiveType))
+        || (valueType === "object" && Object.values(value).every(isPrimitiveType) && Object.keys(value).length <= OBJECT_PROPERTIES_LIMIT);
+}
+
+function getConsoleValue(value) {
+    return isPrimitiveType(value) ? value : {
+        __consoleLogObject: true,
+        isLoggable: isLoggableType(value),
+        argType: getType(value),
+        safeString: safeStringify(value)
+    }
+}
+
+function getConsoleLogData(args) {
+    return args.map(getConsoleValue);
 }
 
 const originalConsoleLog = console.log;
@@ -45,7 +99,7 @@ console.log = (...args) => {
             type: 'console-log',
             pageName: '${pageName}',
             exampleName: '${exampleName}',
-            data: args.map(safeLogData),
+            data: getConsoleLogData(args),
         });
     } catch {
        // Posting is best-effort and shouldn't block normal console logging.
