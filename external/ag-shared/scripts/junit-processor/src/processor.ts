2d8dec22a387c3c73762e90859f2d6426d0d4397
seanlandsman <sean@thelandsmans.com>
2025-03-20 18:24:28 +0000

    Test

--- external/ag-shared/scripts/junit-processor/src/processor.ts
+++ external/ag-shared/scripts/junit-processor/src/processor.ts
@@ -78,7 +78,7 @@ export class TestSuites {
     }
 
     toJson(onlyFailures: boolean) {
-        return {
+        const result = {
             _declaration: {
                 _attributes: {
                     version: '1.0',
@@ -121,6 +121,28 @@ export class TestSuites {
                     })),
             },
         };
+
+        // if onlyFailures is set it could be there aren't - we ensure that the expected
+        // testsuites >testsuite > testcase hierarchy is maintained and some useful context added
+        if(result.testsuites.testsuite.length === 0) {
+            result.testsuites.testsuite.push(<any>{
+                _attributes: {
+                    name: `${result.testsuites._attributes.name}: (${result.testsuites._attributes.tests} tests run and passed)`,
+                    failures: result.testsuites._attributes.failures,
+                    tests: result.testsuites._attributes.tests,
+                    time: result.testsuites._attributes.time
+                },
+                testcase: [{
+                    _attributes: {
+                        classname: result.testsuites._attributes.name,
+                        name: result.testsuites._attributes.name,
+                        time: 0
+                    }
+                }]
+            })
+        }
+
+        return result;
     }
 
     writeJunitReport(outputPath: string, onlyFailures = false) {
