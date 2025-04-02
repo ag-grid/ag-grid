a121e356d01eb8658d07dc294ac99548358fd578
Elias Malik <elias.malik@ag-grid.com>
2025-04-02 11:01:38 +0100

    wip

---
+++ documentation/ag-grid-docs/src/content/docs/row-pinning/_examples/styling-pinned-rows/main.ts
@@ -0,0 +1,36 @@
+import type { ColDef, GridApi, GridOptions } from 'ag-grid-community';
+import { ClientSideRowModelModule, ModuleRegistry, ValidationModule, createGrid } from 'ag-grid-community';
+import { ContextMenuModule, ManualPinnedRowModule } from 'ag-grid-enterprise';
+
+ModuleRegistry.registerModules([
+    ClientSideRowModelModule,
+    ContextMenuModule,
+    ManualPinnedRowModule,
+    ValidationModule /* Development Only */,
+]);
+
+const columnDefs: ColDef[] = [{ field: 'athlete' }, { field: 'country' }, { field: 'sport' }];
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    defaultColDef: {
+        flex: 1,
+    },
+    columnDefs: columnDefs,
+    rowData: null,
+    enableRowPinning: true,
+    isRowPinned: (rowNode) => {
+        return rowNode.data?.country == 'Norway' ? 'top' : rowNode.data?.country === 'Canada' ? 'bottom' : null;
+    },
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/small-olympic-winners.json')
+        .then((response) => response.json())
+        .then((data: IOlympicData[]) => gridApi!.setGridOption('rowData', data));
+});
