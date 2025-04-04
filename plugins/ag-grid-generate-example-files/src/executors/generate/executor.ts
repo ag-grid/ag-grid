95e84f693b966efe002bdcc64f782da2761bcd6d
Tak Tran <contact@tutaktran.com>
2025-04-04 13:51:10 +0100

    AG-14529 - Fix console logger for provided examples
    Some provided examples don't have a console log in the `main.ts` but does in the provided example, so need to check in the provided examples for console log too.

--- plugins/ag-grid-generate-example-files/src/executors/generate/executor.ts
+++ plugins/ag-grid-generate-example-files/src/executors/generate/executor.ts
@@ -193,6 +193,7 @@ export async function generateFiles(options: ExecutorOptions, gridOptionsTypes: 
             entryFile,
             ...Object.values(otherScriptFiles),
             ...Object.values(componentScriptFiles),
+            ...(provideFrameworkFiles ? Object.values(provideFrameworkFiles) : []),
         ].some((file: string) => {
             return getHasExampleConsoleLog({ contents: file });
         });
@@ -275,7 +276,10 @@ export async function generateFiles(options: ExecutorOptions, gridOptionsTypes: 
     }
 }
 
-function getProvidedFrameworkFiles(internalFramework: string, frameworkProvidedExamples: any) {
+function getProvidedFrameworkFiles(
+    internalFramework: string,
+    frameworkProvidedExamples: any
+): Record<string, any> | undefined {
     const providedFramework = internalFramework === 'reactFunctional' ? 'reactFunctionalTs' : internalFramework;
     const provideFrameworkFiles = frameworkProvidedExamples[providedFramework]
         ? { ...frameworkProvidedExamples[providedFramework] }
@@ -377,11 +381,13 @@ async function processProvidedFiles(
                 code
             );
         }
-
-        provideFrameworkFiles[writeToFileName] = transformEntryFile({
-            entryFile: provideFrameworkFiles[writeToFileName],
-        });
     }
+
+    // Transform entry file
+    provideFrameworkFiles[entryFileName] = transformEntryFile({
+        entryFile: provideFrameworkFiles[entryFileName],
+    });
+
     return scriptFiles;
 }
 
