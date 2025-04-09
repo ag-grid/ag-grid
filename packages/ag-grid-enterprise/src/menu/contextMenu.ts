56b6a60313638d53fd4dcf95ebe3da1785de5876
Elias Malik <elias.malik@ag-grid.com>
2025-04-09 15:38:25 +0100

    comment

--- packages/ag-grid-enterprise/src/menu/contextMenu.ts
+++ packages/ag-grid-enterprise/src/menu/contextMenu.ts
@@ -103,6 +103,8 @@ export class ContextMenuService extends BeanStub implements NamedBean, IContextM
                 const isGrandTotalRow = node.level === -1 && node.footer;
                 const isGrandTotalRowFixed = grandTotalRow === 'pinnedBottom' || grandTotalRow === 'pinnedTop';
 
+                // We do not allow pinning of group total rows. As such, only show pinning related menu options for
+                // grand total rows that are not fixed in place, and normal rows that are not group total rows.
                 if ((isGrandTotalRow && !isGrandTotalRowFixed) || (!isGrandTotalRow && !isGroupTotalRow)) {
                     const pinnable = isRowPinnable?.(node) ?? true;
                     if (pinnable) {
