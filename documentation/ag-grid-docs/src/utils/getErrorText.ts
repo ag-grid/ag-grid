9629015cd3d01fb60ce04ca926b6b84a1275d54f
Tak Tran <contact@tutaktran.com>
2025-04-11 10:48:09 +0100

    AG-3390 - Merge b33.2.2

--- documentation/ag-grid-docs/src/utils/getErrorText.ts
+++ documentation/ag-grid-docs/src/utils/getErrorText.ts
@@ -21,6 +21,11 @@ function cleanParams(params: Params) {
                 cleanParam = false;
             }
 
+            // Ensure false is correctly handled as a boolean
+            if (cleanParam === 'false') {
+                cleanParam = false;
+            }
+
             return [key, cleanParam];
         })
     );
