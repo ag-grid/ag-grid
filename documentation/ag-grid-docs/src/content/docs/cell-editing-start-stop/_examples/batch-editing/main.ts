9dfb4b2a0e1d4ccae15c81526efd376dd84febfd
Steph Meslin-Weber <steph@tangency.co.uk>
2025-04-22 10:50:59 +0100

    AG-9780 - new example

---
+++ documentation/ag-grid-docs/src/content/docs/cell-editing-start-stop/_examples/batch-editing/main.ts
@@ -0,0 +1,116 @@
+import type {
+    CellEditingStartedEvent,
+    CellEditingStoppedEvent,
+    GridApi,
+    GridOptions,
+    RowEditingStartedEvent,
+    RowEditingStoppedEvent,
+    RowPinnedType,
+} from 'ag-grid-community';
+import {
+    ClientSideRowModelModule,
+    ModuleRegistry,
+    NumberEditorModule,
+    PinnedRowModule,
+    TextEditorModule,
+    ValidationModule,
+    createGrid,
+} from 'ag-grid-community';
+
+import { getData } from './data';
+
+ModuleRegistry.registerModules([
+    NumberEditorModule,
+    TextEditorModule,
+    PinnedRowModule,
+    ClientSideRowModelModule,
+    ValidationModule /* Development Only */,
+]);
+
+let gridApi: GridApi;
+
+const gridOptions: GridOptions = {
+    columnDefs: [
+        { field: 'firstName' },
+        { field: 'lastName' },
+        { field: 'gender' },
+        { field: 'age' },
+        { field: 'mood' },
+        { field: 'country' },
+        { field: 'address', minWidth: 550 },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 110,
+        editable: true,
+    },
+    rowData: getData(),
+    pinnedTopRowData: getPinnedTopData(),
+    pinnedBottomRowData: getPinnedBottomData(),
+    onRowEditingStarted: (event: RowEditingStartedEvent) => {
+        console.log('never called - not doing row editing');
+    },
+    onRowEditingStopped: (event: RowEditingStoppedEvent) => {
+        console.log('never called - not doing row editing');
+    },
+    onCellEditingStarted: (event: CellEditingStartedEvent) => {
+        console.log('cellEditingStarted');
+    },
+    onCellEditingStopped: (event: CellEditingStoppedEvent) => {
+        console.log('cellEditingStopped');
+    },
+    experimentalEditingModeV2: {
+        strategy: 'cellEditMode',
+    },
+    onCellValueChanged: (event) => {
+        console.log('Cell value changed', event);
+    },
+};
+
+function getPinnedTopData() {
+    return [
+        {
+            firstName: '##',
+            lastName: '##',
+            gender: '##',
+            address: '##',
+            mood: '##',
+            country: '##',
+        },
+    ];
+}
+
+function getPinnedBottomData() {
+    return [
+        {
+            firstName: '##',
+            lastName: '##',
+            gender: '##',
+            address: '##',
+            mood: '##',
+            country: '##',
+        },
+    ];
+}
+
+function updateStrategy(strategy: string) {
+    gridApi!.updateGridOptions({
+        experimentalEditingModeV2: {
+            strategy: strategy as any,
+        },
+    });
+}
+
+function updateTrigger(trigger: string) {
+    gridApi!.updateGridOptions({
+        experimentalEditingModeV2: {
+            trigger: trigger as any,
+        },
+    });
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+});
--- packages/ag-grid-community/src/entities/gridOptions.ts
+++ packages/ag-grid-community/src/entities/gridOptions.ts
@@ -2483,7 +2483,7 @@ export interface GridOptions<TData = any> {
      * @default false
      */
     experimentalEditingModeV2?: {
-        strategy: EditStrategyType;
+        strategy?: EditStrategyType;
         trigger?: EditTriggerType;
         params?: any;
     };
