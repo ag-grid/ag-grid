6f5a5bd0b8706ee31bd11185dc2e1744908b8a1d
Steph Meslin-Weber <steph@tangency.co.uk>
2025-04-22 12:38:42 +0100

    AG-9780 - refactor Record to Map

--- packages/ag-grid-community/src/editing/model/rowEditingModel.ts
+++ packages/ag-grid-community/src/editing/model/rowEditingModel.ts
@@ -1,63 +1,55 @@
-import type { NamedBean } from '../../context/bean';
-import { BeanStub } from '../../context/beanStub';
-import type { CellPosition } from '../../interfaces/iCellPosition';
 import { CellEditingModel } from './cellEditingModel';
 
 export class RowEditingModel {
     public rowId: string;
-    public editorCount = 0;
-    public editorModels: Record<string, CellEditingModel> = {};
+    public editorModels: Map<string, CellEditingModel> = new Map();
 
     constructor(rowId: string) {
         this.rowId = rowId;
     }
 
-    public getEditModel(columnId: string): CellEditingModel {
+    public getEditModel(columnId: string): CellEditingModel | undefined {
         console.warn('RowEditingModel: getEditModel', columnId);
-        return this.editorModels[columnId];
+        return this.editorModels.get(columnId);
     }
 
     public createEditModel(columnId: string): boolean {
         console.warn('RowEditingModel: createEditModel', columnId);
 
-        const cellModel = this.editorModels[columnId];
-
-        if (!cellModel) {
-            this.editorModels[columnId] = new CellEditingModel(this.rowId, columnId);
-            this.editorCount++;
+        if (!this.editorModels.has(columnId)) {
+            this.editorModels.set(columnId, new CellEditingModel(this.rowId, columnId));
             return true;
         }
         return false;
     }
 
     public getEditModelCount(): number {
-        return this.editorCount;
+        return this.editorModels.size;
     }
 
     public removeEditModel(columnId: string): boolean {
         console.warn('RowEditingModel: removeEditModel', columnId);
-        if (!this.editorModels[columnId]) {
+        if (!this.editorModels.has(columnId)) {
             return false;
         }
 
-        delete this.editorModels[columnId];
-        this.editorCount--;
+        this.editorModels.delete(columnId);
         return true;
     }
 
     public getEditModels(): CellEditingModel[] {
-        return Object.values(this.editorModels);
+        return Array.from(this.editorModels.values());
     }
 
     public isEditing(colId?: string): boolean {
         if (colId) {
-            return !!this.editorModels[colId];
+            return this.editorModels.has(colId);
         }
-        return this.editorCount > 0;
+        return this.getEditModelCount() > 0;
     }
 
     destroy() {
         console.warn('RowEditingModel: destroy', this.rowId);
-        Object.keys(this.editorModels).forEach((key) => this.removeEditModel(key));
+        this.editorModels.clear();
     }
 }
