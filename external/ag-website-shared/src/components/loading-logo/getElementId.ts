3f0899fbd7187fede698625dd22bfd05c761126e
Tak Tran <contact@tutaktran.com>
2025-05-02 16:09:47 +0100

    AG-14371 - Don't animate loading logo when it isn't visible
    * Create new `ag-grid-logomark-not-loading.svg` that does not animate by default
    * Move loading logo logic into `LoadingLogo` component
    * Move files into shared folder

--- external/ag-website-shared/src/components/loading-logo/getElementId.ts
+++ external/ag-website-shared/src/components/loading-logo/getElementId.ts
@@ -1,7 +1,3 @@
-export function getLoadingLogoId({ pageName, exampleName }: { pageName: string; exampleName: string }) {
-    return `loading-${pageName}-${exampleName}`;
-}
-
 export function getLoadingIFrameId({ pageName, exampleName }: { pageName: string; exampleName: string }) {
     return `loading-frame-${pageName}-${exampleName}`;
 }
