bfe28c54a7744a19e36ae602445138828e4890cd
Tak Tran <contact@tutaktran.com>
2025-05-15 15:44:02 +0100

    AG-14845 - Use replacement types for event and class instance

--- plugins/ag-grid-generate-example-files/src/executors/generate/generator/utils/getConsoleLogSnippet.ts
+++ plugins/ag-grid-generate-example-files/src/executors/generate/generator/utils/getConsoleLogSnippet.ts
@@ -16,6 +16,7 @@ export const CONSOLE_LOG_END = '/** CONSOLE LOG END **/';
  *
  *   * Circular references are replaced with `[Circular]`
  *   * Class instances are replaced with `ClassNameClass {}`
+ *   * Event instances are replaced with `EventName {}`
  *   * `undefined`, `NaN`, `Infinity`, and `-Infinity` are replaced with `[TYPE:undefined]`, etc.
  *     This is deserialised on the parent window to the correct value.
  *
@@ -25,8 +26,17 @@ export const getConsoleLogSnippet = ({ pageName, exampleName, logError }: Params
 
 function patchConsoleLog() {
     const PRIMITIVE_TYPES = ["string", "number", "boolean", "undefined", "null", "nan", "symbol"];
-    const REPLACEMENT_TYPES = ["undefined", "nan", "infinity", "negativeInfinity"];
-    const getReplacementTypeValue = (typeValue) => \`[TYPE:\${typeValue}]\`;
+    const REPLACEMENT_TYPES = ["undefined", "nan", "infinity", "negativeInfinity", 'classInstance', 'event'];
+    const getReplacementValue = (value) => {
+        const valueType = getType(value);
+        if (valueType === "classInstance") {
+            return value.constructor.name + "Class {}";
+        } else if (valueType === "event") {
+            return value.constructor.name + " { type: " + value.type + " }";
+        }
+
+        return \`[TYPE:\${valueType}]\`;
+    };
 
     function getType(value) {
         if (value === null) return "null";
@@ -38,6 +48,7 @@ function patchConsoleLog() {
         if (value instanceof RegExp) return "regexp";
         if (value instanceof Map) return "map";
         if (value instanceof Set) return "set";
+        if (value instanceof Event) return "event";
         if (value instanceof WeakMap) return "weakmap";
         if (value instanceof WeakSet) return "weakset";
         if (value instanceof Promise) return "promise";
@@ -58,10 +69,6 @@ function patchConsoleLog() {
                     return "[Circular]";
                 }
                 seen.add(value);
-
-                if (getType(value) === "classInstance") {
-                    return value.constructor.name + "Class {}";
-                }
             }
             return value;
         }, space);
@@ -71,17 +78,18 @@ function patchConsoleLog() {
         const valueType = getType(value);
         
         if (replacementTypes.includes(valueType)) {
-            return getReplacementTypeValue(valueType);
+            return getReplacementValue(value);
         } else if (valueType === 'array') {
             return value.map(item => updateWithReplacements(item, replacementTypes));
         } else if (valueType === 'object') {
             const obj = { ...value };
             for (const key in value) {
-                const objValueType = getType(value[key]);
+                const objValue = value[key];
+                const objValueType = getType(objValue);
                 if (replacementTypes.includes(objValueType)) {
-                    obj[key] = getReplacementTypeValue(objValueType);
+                    obj[key] = getReplacementValue(objValue);
                 } else {
-                    obj[key] = updateWithReplacements(value[key], replacementTypes);
+                    obj[key] = updateWithReplacements(objValue, replacementTypes);
                 }
             }
             return obj;
