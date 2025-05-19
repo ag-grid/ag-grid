cbaf229b2c480935b2f2727a0d61cc9867f43697
Elias Malik <elias.malik@ag-grid.com>
2025-05-19 16:14:18 +0100

    tidy

--- packages/ag-grid-react/src/reactUi/header/headerCellComp.tsx
+++ packages/ag-grid-react/src/reactUi/header/headerCellComp.tsx
@@ -39,6 +39,14 @@ const HeaderCellComp = ({ ctrl }: { ctrl: HeaderCellCtrl }) => {
             return;
         }
 
+        const refreshSelectAllGui = () => {
+            const selectAllGui = ctrl.getSelectAllGui();
+            if (selectAllGui) {
+                eResize.current?.insertAdjacentElement('afterend', selectAllGui);
+                compBean.current!.addDestroyFunc(() => selectAllGui.remove());
+            }
+        };
+
         const compProxy: IHeaderCellComp = {
             setWidth: (width: string) => {
                 if (eGui.current) {
@@ -54,26 +62,13 @@ const HeaderCellComp = ({ ctrl }: { ctrl: HeaderCellCtrl }) => {
             },
             setUserCompDetails: (compDetails: UserCompDetails) => setUserCompDetails(compDetails),
             getUserCompInstance: () => userCompRef.current || undefined,
-            refreshSelectAllGui: () => {
-                const selectAllGui = ctrl.getSelectAllGui();
-                if (selectAllGui) {
-                    eResize.current?.insertAdjacentElement('afterend', selectAllGui);
-                    compBean.current!.addDestroyFunc(() => selectAllGui.remove());
-                }
-            },
-            removeSelectAllGui: () => {
-                const selectAllGui = ctrl.getSelectAllGui();
-                selectAllGui?.remove();
-            },
+            refreshSelectAllGui,
+            removeSelectAllGui: () => ctrl.getSelectAllGui()?.remove(),
         };
 
         ctrl.setComp(compProxy, eRef, eResize.current!, eHeaderCompWrapper.current!, compBean.current);
 
-        const selectAllGui = ctrl.getSelectAllGui();
-        if (selectAllGui) {
-            eResize.current?.insertAdjacentElement('afterend', selectAllGui);
-            compBean.current!.addDestroyFunc(() => selectAllGui.remove());
-        }
+        refreshSelectAllGui();
     }, []);
 
     // js comps
