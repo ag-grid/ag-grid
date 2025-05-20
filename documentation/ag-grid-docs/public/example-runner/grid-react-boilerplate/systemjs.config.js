89d5aca1bcde2067a109df1732445d26fa12076d
Stephen Cooper <scooperdev@gmail.com>
2025-05-20 11:08:43 +0100

    AG-14155 update config

--- documentation/ag-grid-docs/public/example-runner/grid-react-boilerplate/systemjs.config.js
+++ documentation/ag-grid-docs/public/example-runner/grid-react-boilerplate/systemjs.config.js
@@ -1,10 +1,12 @@
 (function (global) {
     process = { env: { NODE_ENV: 'development' } };
 
-    // Valid values: 18 / 19
-    const REACT_VERSION = 19;
+    // Valid versions (18/19)
+    const config = { version: 19, isProd: true };
+
+    const filePart = config.isProd ? 'production.min' : 'development';
     const reactConfig =
-        REACT_VERSION == 18
+        config.version == 18
             ? {
                   map: {
                       react: 'npm:react@18.2.0',
@@ -13,19 +15,19 @@
                   },
                   packages: {
                       react: {
-                          main: './umd/react.production.min.js',
+                          main: `./umd/react.${filePart}.js`,
                       },
                       'react-dom': {
-                          main: './umd/react-dom.production.min.js',
+                          main: `./umd/react-dom.${filePart}.js`,
                       },
                   },
               }
             : {
                   map: {
-                      react: 'npm:react@19.1.0/cjs/react.production.min.js',
-                      'react-dom': 'npm:react-dom@19.1.0/cjs/react-dom.production.min.js',
-                      'react-dom/client': 'npm:react-dom@19.1.0/cjs/react-dom-client.production.min.js',
-                      scheduler: 'npm:scheduler@0.26.0/cjs/scheduler.production.min.js',
+                      react: `npm:react@19.1.0/cjs/react.${filePart}.js`,
+                      'react-dom': `npm:react-dom@19.1.0/cjs/react-dom.${filePart}.js`,
+                      'react-dom/client': `npm:react-dom@19.1.0/cjs/react-dom-client.${filePart}.js`,
+                      scheduler: `npm:scheduler@0.26.0/cjs/scheduler.${filePart}.js`,
                   },
                   packages: {
                       react: {
