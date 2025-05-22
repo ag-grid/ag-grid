60d07e69116e4024b148c8387ea44a9f68b0f077
Tak Tran <contact@tutaktran.com>
2025-05-22 16:41:49 +0100

    AG-10101 - Remove unused page nav helper functions

--- documentation/ag-grid-docs/src/components/pages-navigation/utils/pageNavigationHelpers.ts
+++ documentation/ag-grid-docs/src/components/pages-navigation/utils/pageNavigationHelpers.ts
@@ -1,5 +1,4 @@
-import type { MenuItem } from '@ag-grid-types';
-import type { Framework, MenuSection } from '@ag-grid-types';
+import type { Framework } from '@ag-grid-types';
 import { getExamplePageUrl } from '@components/docs/utils/urlPaths';
 
 export function toElementId(str: string) {
@@ -8,57 +7,4 @@ export function toElementId(str: string) {
 
 export function getLinkUrl({ framework, path, url }: { framework: Framework; path?: string; url?: string }) {
     return url ? url : getExamplePageUrl({ framework, path: path! });
-}
-
-const createGetTopLevelMenuItem = (activeMenuItemPath: string) => {
-    const findPath = ({ path, items }: MenuItem) => {
-        return path === activeMenuItemPath || items?.some(findPath);
-    };
-    return findPath;
-};
-
-export function findActiveTopLevelMenuItem({
-    menuSections,
-    activeMenuItemPath,
-}: {
-    menuSections: MenuSection[];
-    activeMenuItemPath: string;
-}): MenuItem | undefined {
-    return menuSections.reduce((foundMenuItem: MenuItem | undefined, section: MenuSection) => {
-        const { items } = section;
-        if (!items) {
-            return;
-        }
-        const getTopLevelMenuItem = createGetTopLevelMenuItem(activeMenuItemPath);
-        return items.find(getTopLevelMenuItem) || foundMenuItem;
-    }, undefined);
-}
-
-export function findActiveMenuItem({
-    menuSections,
-    activeMenuItemPath,
-}: {
-    menuSections: MenuSection[];
-    activeMenuItemPath: string;
-}) {
-    const getMenuItemReducer = (foundMenuItem: MenuItem | undefined, menuItem: MenuItem): MenuItem | undefined => {
-        const { path, items } = menuItem;
-        if (path === activeMenuItemPath) {
-            return menuItem;
-        }
-        const childMenuItem = items?.reduce(getMenuItemReducer, undefined);
-
-        return childMenuItem ? childMenuItem : foundMenuItem;
-    };
-    const getMenuSectionReducer = (
-        foundMenuItem: MenuItem | undefined,
-        menuSection: MenuSection
-    ): MenuItem | undefined => {
-        const { items } = menuSection;
-        const childMenuItem = items?.reduce(getMenuItemReducer, undefined);
-
-        return childMenuItem ? childMenuItem : foundMenuItem;
-    };
-
-    return menuSections.reduce<MenuItem | undefined>(getMenuSectionReducer, undefined);
 }
