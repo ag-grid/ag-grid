e506408ca68c33b198c5816b115d63f93d9b1b8a
Victor Musienko <victor.m@noibu.com>
2025-05-31 22:01:27 +0100

    - remove redundant export

--- packages/ag-grid-community/src/main.ts
+++ packages/ag-grid-community/src/main.ts
@@ -651,7 +651,6 @@ export {
 export { _getClientSideRowModel, _getServerSideRowModel } from './api/rowModelApiUtils';
 export { AgEventType, AgPublicEventType, _GET_ALL_EVENTS, _PUBLIC_EVENTS } from './eventTypes';
 export { _PUBLIC_EVENT_HANDLERS_MAP } from './publicEventHandlersMap';
-export { GRID_OPTIONS_VALIDATORS } from './validation/rules/gridOptionsValidations';
 export type { FocusService } from './focusService';
 export type { GridOptionsService, PropertyValueChangedEvent } from './gridOptionsService';
 export { PropertyChangedEvent } from './gridOptionsService';
