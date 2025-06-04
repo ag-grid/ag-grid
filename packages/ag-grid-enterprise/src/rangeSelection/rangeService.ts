24841d351360061ae9c5ea86c70755ab61760f8d
Steph Meslin-Weber <steph@tangency.co.uk>
2025-06-04 12:00:08 +0100

    AG-9780 - fix range selection across pending cells

--- packages/ag-grid-enterprise/src/rangeSelection/rangeService.ts
+++ packages/ag-grid-enterprise/src/rangeSelection/rangeService.ts
@@ -996,7 +996,7 @@ export class RangeService extends BeanStub implements NamedBean, IRangeService {
             return;
         }
 
-        const editing = this.beans.editSvc?.isEditing(cellCtrl?.rowNode, cellCtrl?.column);
+        const editing = this.beans.editSvc?.isEditing(cellCtrl?.rowNode, cellCtrl?.column, false, true);
 
         if (editing) {
             this.dragSvc.cancelDrag(eventTarget as HTMLElement);
