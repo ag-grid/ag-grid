2142af78fb27fc7019406909176c51572cb474aa
AgGitDeployment <deployments.team@ag-grid.com>
2025-06-04 09:18:20 +0100

    Release 33.3.2 Prep

--- packages/ag-grid-community/src/baseUrl.ts
+++ packages/ag-grid-community/src/baseUrl.ts
@@ -1,2 +1,2 @@
 // DO NOT UPDATE MANUALLY: Generated from script during build time
-export const BASE_URL = 'https://www.ag-grid.com/archive/33.3.2';
+export const BASE_URL = 'https://www.ag-grid.com';
