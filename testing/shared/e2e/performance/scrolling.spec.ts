fd94b530f390aa83eedb35fc0ecd73b520e6cd9c
sdwvit <victor.musienko@ag-grid.com>
2025-06-04 16:53:28 +0100

    - update test

--- testing/shared/e2e/performance/scrolling.spec.ts
+++ testing/shared/e2e/performance/scrolling.spec.ts
@@ -7,28 +7,40 @@ const version = process.env.FW_VERSION ?? 'unknown';
 const titleCaseFw = fw.charAt(0).toUpperCase() + fw.substring(1);
 const variantTitle = process.env.FW_VARIANT ? `(${process.env.FW_VARIANT})` : '';
 
+test.describe.configure({ timeout: 120_000 });
+const ITERATIONS_FOR_AVERAGE = 10;
+const SCROLLS_PER_ITERATION = 5;
+
 test.describe(`${titleCaseFw} ${variantTitle} ${version}`, () => {
-    test('load performance-test, set data, scroll a few times, measure score', async ({ page }) => {
-        const { consoleMsgs } = await gotoAndGetComms(page, '/javascript-data-grid/performance-test');
+    test(`Load performance-test, set data, scroll ${SCROLLS_PER_ITERATION} times by 5000px, find slowest, repeat ${ITERATIONS_FOR_AVERAGE} times`, async ({
+        page,
+    }) => {
+        const { consoleMsgs } = await gotoAndGetComms(page, '/examples/performance-test/lots-of-cells/typescript');
+        const result = [];
+        for (let i = 0; i < ITERATIONS_FOR_AVERAGE; i++) {
+            await page.getByText('Set Data').click();
+            await waitFor(() => consoleMsgs.filter((msg) => msg.text.includes('rowDataUpdated')).length >= 2);
+            const noise = (await page.evaluate(() => performance.getEntriesByType('long-animation-frame'))).length;
 
-        const frame = page.frameLocator('#loading-frame-performance-test-lots-of-cells');
+            const grid = page.getByTestId('myGrid');
+            const gridRect = await grid.boundingBox();
 
-        await frame
-            .owner()
-            .page()
-            .evaluate(() => {
-                window.addEventListener('gridReady', (...args) => console.log(...args));
-            });
-        await frame.getByText('Set Data').click({ force: true });
-        await waitFor(() => consoleMsgs.filter((msg) => msg.text.includes('rowDataUpdated')).length >= 2);
-        for (let i = 0; i < 5; i++) {
-            await frame.getByText('Scroll').click({ force: true });
-            await waitFor(() => consoleMsgs.filter((msg) => msg.text.includes('bodyScrollEnd')).length >= i + 1);
+            await page.mouse.move(gridRect.x + gridRect.width / 2, gridRect.y + gridRect.height / 2);
+
+            for (let j = 0; j < SCROLLS_PER_ITERATION; j++) {
+                await page.mouse.wheel(0, 5000);
+                await waitFor(50);
+            }
+
+            const eventPerf = (await page.evaluate(() => performance.getEntriesByType('long-animation-frame')))
+                .slice(noise)
+                .sort((a, b) => a.duration - b.duration)
+                .pop();
+            result.push(eventPerf);
+            await page.reload({ waitUntil: 'networkidle' });
         }
+        const avg = result.reduce((acc, entry) => acc + entry.duration, 0) / result.length;
 
-        const predicate = (msg) => msg.text.includes('Time taken:');
-        await waitFor(() => consoleMsgs.filter(predicate).length >= 3);
-        const msgs = (await waitFor(() => consoleMsgs.filter(predicate))) as typeof consoleMsgs;
-        expect(msgs.every(({ args: [_, timing] }) => timing < 2000)).toBeTruthy();
+        expect(avg).toBeLessThan(1000);
     });
 });
--- testing/shared/playwright.utils.ts
+++ testing/shared/playwright.utils.ts
@@ -2,11 +2,11 @@ import type { Page } from '@playwright/test';
 
 export async function getBrowserCommunications(page: Page) {
     const consoleMsgs: { args: any[]; text: string; type: string }[] = [];
-    page.on('console', async (msg) => {
+    page.on('console', (msg) => {
         consoleMsgs.push({
             type: msg.type(),
             text: msg.text(),
-            args: await Promise.all(msg.args().map((arg) => arg.jsonValue())),
+            args: msg.args().map((arg) => arg.jsonValue),
         });
     });
     return {
@@ -17,21 +17,25 @@ export async function getBrowserCommunications(page: Page) {
     };
 }
 
-export const waitFor = async <T>(getter: () => T, page?: Page, smart = false) => {
+export const waitFor = async <T>(getterOrTimeout: (() => T) | number, page?: Page, smart = false) => {
+    if (typeof getterOrTimeout === 'number') {
+        return new Promise<void>((resolve) => setTimeout(resolve, getterOrTimeout));
+    }
+
     if (page) {
-        await page.waitForFunction(getter);
+        await page.waitForFunction(getterOrTimeout);
         if (smart) {
-            return page.evaluateHandle(getter);
+            return page.evaluateHandle(getterOrTimeout);
         }
-        return page.evaluate(getter);
+        return page.evaluate(getterOrTimeout);
     }
     return new Promise<T>((resolve, reject) => {
         const timeout = setTimeout(() => {
             clearInterval(interval);
-            reject(new Error('waitFor timed out doing getter: ' + getter.toString()));
+            reject(new Error('waitFor timed out doing getter: ' + getterOrTimeout.toString()));
         }, 5000);
         const interval = setInterval(async () => {
-            const res = await getter();
+            const res = await getterOrTimeout();
             if (res) {
                 clearInterval(interval);
                 resolve(res);
@@ -43,9 +47,6 @@ export const waitFor = async <T>(getter: () => T, page?: Page, smart = false) =>
 
 export const gotoAndGetComms = async (page: Page, url: string) => {
     const msgs = await getBrowserCommunications(page);
-    await page.goto(url);
-    await page.waitForLoadState('domcontentloaded');
-    await page.waitForLoadState('load');
-    await page.waitForLoadState('networkidle');
+    await page.goto(url, { waitUntil: 'networkidle' });
     return msgs;
 };
