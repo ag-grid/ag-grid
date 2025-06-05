4e456f68b2b0ff1c67b29acd0f929974a7893b8d
Tak Tran <contact@tutaktran.com>
2025-06-05 16:47:17 +0100

    AG-15064 - Remove markdoc migration script
    No longer needed. Only for gatsby migration

--- documentation/ag-grid-docs/scripts/markdoc-migration/src/utils/getJsxString.ts
+++
@@ -1,9 +0,0 @@
-import { mdxJsxToMarkdown } from 'mdast-util-mdx-jsx';
-import { toMarkdown } from 'mdast-util-to-markdown';
-import type { Nodes } from 'mdast-util-to-markdown/lib';
-
-export function getJsxString(node: Nodes) {
-    return toMarkdown(node, {
-        extensions: [mdxJsxToMarkdown()],
-    });
-}
