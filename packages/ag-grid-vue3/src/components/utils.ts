f0d895f8c153b5c93bf786830dccc244d10c36f9
Josh Hobson <118217725+AlpineJosh@users.noreply.github.com>
2025-06-12 16:07:53 +0100

    Fixing Vue props

--- packages/ag-grid-vue3/src/components/utils.ts
+++ packages/ag-grid-vue3/src/components/utils.ts
@@ -603,6 +603,10 @@ export interface Props<TData> {
          * @agModule `AdvancedFilterModule`
          */
     advancedFilterParent?: HTMLElement | null | undefined,
+    /** Customise the parameters passed to the Advanced Filter.
+         * @agModule `AdvancedFilterModule`
+         */
+    advancedFilterParams?: IAdvancedFilterParams | undefined,
     /** Customise the parameters passed to the Advanced Filter Builder.
          * @agModule `AdvancedFilterModule`
          */
