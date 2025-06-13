43a04b1bd5d87fe7437b483a8d6d4f60226203e0
Steph Meslin-Weber <steph@tangency.co.uk>
2025-06-13 12:02:17 +0100

    AG-9780 - update module sizes

--- testing/module-size/moduleDefinitions.ts
+++ testing/module-size/moduleDefinitions.ts
@@ -51,7 +51,7 @@ export const AllGridCommunityModules: Record<`${CommunityModuleName}Module`, num
 };
 export const AllEnterpriseModules: Record<`${EnterpriseModuleName}Module`, number> = {
     AdvancedFilterModule: 200,
-    AllEnterpriseModule: 1438.2,
+    AllEnterpriseModule: 1444.04,
     CellSelectionModule: 53,
     ClipboardModule: 46.04,
     ColumnMenuModule: 153.19,
