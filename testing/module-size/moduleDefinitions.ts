8497655669663a1e426361bae999e22ed286f1a0
Andrew Glazier <andrew.glazier@ag-grid.com>
2025-06-16 10:43:07 +0100

    AG-14949 - Fix bundle size

--- testing/module-size/moduleDefinitions.ts
+++ testing/module-size/moduleDefinitions.ts
@@ -6,7 +6,7 @@ import {
 
 export const AllGridCommunityModules: Record<`${CommunityModuleName}Module`, number> = {
     AlignedGridsModule: 3.06,
-    AllCommunityModule: 454.53,
+    AllCommunityModule: 459.66,
     CellApiModule: 0.28,
     CellStyleModule: 2.24,
     CheckboxEditorModule: 40.86,
