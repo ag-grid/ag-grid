6ef58df488326d09a08856ce7e34fc8d5765ad27
Peter Reynolds <peterjrreynolds@gmail.com>
2025-06-17 15:10:31 +0100

    AG-14921 - more docs updates

---
+++ documentation/ag-grid-docs/src/content/docs/tool-panel-filters-new/_examples/state/main.ts
@@ -0,0 +1,89 @@
+import type { GridApi, GridOptions, NewFiltersToolPanelState } from 'ag-grid-community';
+import {
+    ClientSideRowModelModule,
+    DateFilterModule,
+    GridStateModule,
+    ModuleRegistry,
+    NumberFilterModule,
+    TextFilterModule,
+    ValidationModule,
+    createGrid,
+} from 'ag-grid-community';
+import { ColumnMenuModule, ContextMenuModule, NewFiltersToolPanelModule, SetFilterModule } from 'ag-grid-enterprise';
+
+ModuleRegistry.registerModules([
+    NumberFilterModule,
+    DateFilterModule,
+    ClientSideRowModelModule,
+    NewFiltersToolPanelModule,
+    ColumnMenuModule,
+    ContextMenuModule,
+    SetFilterModule,
+    TextFilterModule,
+    GridStateModule,
+    ...(process.env.NODE_ENV !== 'production' ? [ValidationModule] : []),
+]);
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    columnDefs: [
+        { field: 'athlete' },
+        { field: 'age' },
+        { field: 'country' },
+        { field: 'date', minWidth: 180 },
+        { field: 'total' },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+        filter: 'agSelectableColumnFilter',
+    },
+    sideBar: 'filters-new',
+    enableFilterHandlers: true,
+    suppressSetFilterByDefault: true,
+    initialState: {
+        sideBar: {
+            visible: true,
+            position: 'right',
+            openToolPanel: 'filters-new',
+            toolPanels: {
+                'filters-new': {
+                    filters: [
+                        {
+                            colId: 'country',
+                            expanded: true,
+                        },
+                        {
+                            colId: 'age',
+                        },
+                    ],
+                } as NewFiltersToolPanelState,
+            },
+        },
+    },
+};
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then((data: IOlympicData[]) =>
+            gridApi!.setGridOption(
+                'rowData',
+                data.map((rowData) => {
+                    const dateParts = rowData.date.split('/');
+                    const [year, month, day] = dateParts.reverse().map((e) => parseInt(e, 10));
+                    const paddedDateTimeStrings = [month, day].map((e) => e.toString().padStart(2, '0'));
+                    const date = `${year}-${paddedDateTimeStrings[0]}-${paddedDateTimeStrings[1]}`;
+                    return {
+                        ...rowData,
+                        date,
+                    };
+                })
+            )
+        );
+});
