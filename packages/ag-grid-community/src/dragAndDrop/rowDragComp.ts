97b30c51af44642c7ab6740229713b9a9473dd72
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-06-18 14:13:10 +0100

    AG-15132-hide-drag-handle-only-when-managed-tree-data-row-dragging

--- packages/ag-grid-community/src/dragAndDrop/rowDragComp.ts
+++ packages/ag-grid-community/src/dragAndDrop/rowDragComp.ts
@@ -170,18 +170,19 @@ class VisibilityStrategy extends BeanStub {
         super();
     }
 
-    protected setDisplayedOrVisible(neverDisplayed: boolean): void {
+    protected setDisplayedOrVisible(neverDisplayed: boolean, alwaysHidden: boolean = false): void {
         const displayedOptions = { skipAriaHidden: true };
         if (neverDisplayed) {
             this.parent.setDisplayed(false, displayedOptions);
         } else {
-            let shown: boolean = true;
+            let shown: boolean = !alwaysHidden;
             let isShownSometimes: boolean = false;
 
             const { column, rowNode, parent } = this;
             if (column) {
-                shown = column.isRowDrag(rowNode) || parent.isCustomGui();
-                isShownSometimes = typeof column.getColDef().rowDrag === 'function';
+                const rowDrag = column.getColDef().rowDrag;
+                isShownSometimes = typeof rowDrag === 'function';
+                shown = (alwaysHidden ? !!rowDrag : column.isRowDrag(rowNode)) || parent.isCustomGui();
             }
 
             // if shown sometimes, them some rows can have drag handle while other don't,
@@ -189,10 +190,10 @@ class VisibilityStrategy extends BeanStub {
             // keeps the empty space, whereas setDisplayed looses the space)
             if (isShownSometimes) {
                 parent.setDisplayed(true, displayedOptions);
-                parent.setVisible(shown, displayedOptions);
+                parent.setVisible(shown && !alwaysHidden, displayedOptions);
             } else {
                 parent.setDisplayed(shown, displayedOptions);
-                parent.setVisible(true, displayedOptions);
+                parent.setVisible(!alwaysHidden, displayedOptions);
             }
         }
     }
@@ -262,6 +263,6 @@ class ManagedVisibilityStrategy extends VisibilityStrategy {
         const hasExternalDropZones = dragAndDrop!.hasExternalDropZones();
         const neverDisplayed = (shouldPreventRowMove && !hasExternalDropZones) || suppressRowDrag;
 
-        this.setDisplayedOrVisible(neverDisplayed);
+        this.setDisplayedOrVisible(neverDisplayed, this.rowNode.footer);
     }
 }
