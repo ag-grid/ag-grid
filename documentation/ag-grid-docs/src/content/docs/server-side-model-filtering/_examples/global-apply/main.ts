27465bb27d117f9d3d83b5890b532e50bacce36e
Peter Reynolds <peterjrreynolds@gmail.com>
2025-06-19 15:11:10 +0100

    AG-15181 - ssrm new filters tool panel docs

---
+++ documentation/ag-grid-docs/src/content/docs/server-side-model-filtering/_examples/global-apply/main.ts
@@ -0,0 +1,97 @@
+import type { FilterWrapperParams, GridApi, GridOptions, IServerSideDatasource } from 'ag-grid-community';
+import { ModuleRegistry, NumberFilterModule, TextFilterModule, ValidationModule, createGrid } from 'ag-grid-community';
+import { ColumnMenuModule, ContextMenuModule, ServerSideRowModelModule } from 'ag-grid-enterprise';
+
+import { FakeServer } from './fakeServer';
+
+ModuleRegistry.registerModules([
+    ColumnMenuModule,
+    ContextMenuModule,
+    ServerSideRowModelModule,
+    TextFilterModule,
+    NumberFilterModule,
+    ...(process.env.NODE_ENV !== 'production' ? [ValidationModule] : []),
+]);
+
+let gridApi: GridApi<IOlympicData>;
+const gridOptions: GridOptions<IOlympicData> = {
+    columnDefs: [
+        {
+            field: 'athlete',
+            cellDataType: 'text',
+            minWidth: 220,
+        },
+        { field: 'year', cellDataType: 'number' },
+        { field: 'gold', cellDataType: 'number' },
+        { field: 'silver', cellDataType: 'number' },
+        { field: 'bronze', cellDataType: 'number' },
+    ],
+    defaultColDef: {
+        flex: 1,
+        minWidth: 100,
+        filter: true,
+        filterParams: {
+            buttons: ['apply'], // set all filters to use buttons
+        } as FilterWrapperParams,
+        suppressHeaderMenuButton: true,
+        suppressHeaderContextMenu: true,
+    },
+    rowModelType: 'serverSide',
+    enableFilterHandlers: true,
+    suppressSetFilterByDefault: true,
+    sideBar: {
+        toolPanels: [
+            {
+                id: 'filters-new',
+                labelDefault: 'Filters',
+                labelKey: 'filters',
+                iconKey: 'filter',
+                toolPanel: 'agNewFiltersToolPanel',
+                toolPanelParams: {
+                    buttons: ['reset', 'apply'],
+                },
+            },
+        ],
+        defaultToolPanel: 'filters-new',
+    },
+};
+
+function getServerSideDatasource(server: any): IServerSideDatasource {
+    return {
+        getRows: (params) => {
+            console.log('[Datasource] - rows requested by grid: ', params.request);
+
+            // get data for request from our fake server
+            const response = server.getData(params.request);
+
+            // simulating real server call with a 500ms delay
+            setTimeout(() => {
+                if (response.success) {
+                    // supply rows for requested block to grid
+                    params.success({ rowData: response.rows, rowCount: response.lastRow });
+                } else {
+                    params.fail();
+                }
+            }, 500);
+        },
+    };
+}
+
+// setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv = document.querySelector<HTMLElement>('#myGrid')!;
+    gridApi = createGrid(gridDiv, gridOptions);
+
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then(function (data) {
+            // setup the fake server with entire dataset
+            const fakeServer = new FakeServer(data);
+
+            // create datasource with a reference to the fake server
+            const datasource = getServerSideDatasource(fakeServer);
+
+            // register the datasource with the grid
+            gridApi!.setGridOption('serverSideDatasource', datasource);
+        });
+});
