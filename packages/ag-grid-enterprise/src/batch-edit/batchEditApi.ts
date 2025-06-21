1d6674b16ba82df468114a683bdc5946bafe10b4
Steph Meslin-Weber <steph@tangency.co.uk>
2025-06-21 16:13:14 +0100

    AG-15203 - TC90 - warn on unsupported models

--- packages/ag-grid-enterprise/src/batch-edit/batchEditApi.ts
+++ packages/ag-grid-enterprise/src/batch-edit/batchEditApi.ts
@@ -1,17 +1,33 @@
 import type { BeanCollection } from 'ag-grid-community';
+import { _isClientSideRowModel, _warn } from 'ag-grid-community';
 
-export function startBatchEdit(beans: BeanCollection): void {
-    beans.editSvc?.setBatchEditing(true);
+export function startBatchEdit({ editSvc, gos, rowModel }: BeanCollection): void {
+    if (!editSvc?.isBatchEditing()) {
+        if (!_isClientSideRowModel(gos, rowModel)) {
+            _warn(289, { rowModelType: gos.get('rowModelType') });
+            return;
+        }
+
+        editSvc?.setBatchEditing(true);
+    }
 }
 
-export function cancelBatchEdit(beans: BeanCollection): void {
-    beans.editSvc?.stopEditing(undefined, { cancel: true, source: 'api' });
-    beans.editSvc?.setBatchEditing(false);
+export function cancelBatchEdit({ editSvc }: BeanCollection): void {
+    if (!editSvc?.isBatchEditing()) {
+        return;
+    }
+
+    editSvc?.stopEditing(undefined, { cancel: true, source: 'api' });
+    editSvc?.setBatchEditing(false);
 }
 
-export function commitBatchEdit(beans: BeanCollection): void {
-    beans.editSvc?.stopEditing(undefined, { source: 'api' });
-    beans.editSvc?.setBatchEditing(false);
+export function commitBatchEdit({ editSvc }: BeanCollection): void {
+    if (!editSvc?.isBatchEditing()) {
+        return;
+    }
+
+    editSvc?.stopEditing(undefined, { source: 'api' });
+    editSvc?.setBatchEditing(false);
 }
 
 export function isBatchEditing(beans: BeanCollection): boolean {
