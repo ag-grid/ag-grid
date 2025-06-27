8406b22294fa424b969c521aba95ce74e5b8fa92
Stephen Cooper <scooperdev@gmail.com>
2025-06-27 12:08:56 +0100

    Setup Doc Tests

---
+++ testing/ag-grid-docs/playwright.config.ts
@@ -0,0 +1,96 @@
+import { defineConfig, devices } from '@playwright/test';
+
+// import path from 'node:path';
+
+// const PUBLIC_SITE_URL = process.env.PUBLIC_SITE_URL!;
+// const baseURL = process.env.PUBLIC_BASE_URL ? path.join(PUBLIC_SITE_URL, process.env.PUBLIC_BASE_URL) : PUBLIC_SITE_URL;
+
+// console.log(`Using base URL: ${baseURL}`);
+
+/**
+ * See https://playwright.dev/docs/test-configuration.
+ */
+export default defineConfig({
+    testDir: './e2e',
+    /* Run tests in files in parallel */
+    fullyParallel: true,
+    timeout: process.env.CI ? 30_000 : 10_000,
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 3,
+    /* Limit parallel tests on CI. */
+    workers: process.env.CI ? 6 : undefined,
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: [
+        ['line'],
+        [
+            'html',
+            {
+                open: process.env.CI ? 'never' : 'on-failure',
+                outputFolder: '../../reports/ag-grid-csp-e2e-html/',
+            },
+        ],
+        [
+            'playwright-ctrf-json-reporter',
+            {
+                outputDir: '../../reports/',
+                outputFile: 'ag-grid-csp-e2e.json',
+            },
+        ],
+    ],
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        // baseURL: 'https://grid-staging.ag-grid.com',
+        baseURL: process.env.CI ? 'https://grid-staging.ag-grid.com' : 'https://localhost:4610',
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'off', // process.env.CI ? 'off' : 'retain-on-first-failure',
+    },
+
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] },
+        },
+
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+
+        {
+            name: 'webkit',
+            use: { ...devices['Desktop Safari'] },
+        },
+
+        /* Test against mobile viewports. */
+        // {
+        //   name: 'Mobile Chrome',
+        //   use: { ...devices['Pixel 5'] },
+        // },
+        // {
+        //   name: 'Mobile Safari',
+        //   use: { ...devices['iPhone 12'] },
+        // },
+
+        /* Test against branded browsers. */
+        // {
+        //   name: 'Microsoft Edge',
+        //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
+        // },
+        // {
+        //   name: 'Google Chrome',
+        //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+        // },
+    ],
+
+    /* Run your local dev server before starting the tests */
+    // webServer: {
+    //   command: 'npm run start',
+    //   url: 'http://127.0.0.1:3000',
+    //   reuseExistingServer: !process.env.CI,
+    // },
+});
