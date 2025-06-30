9f442659c0561a36947ccf4e114502e13bf1ab85
Elias Malik <elias.malik@ag-grid.com>
2025-06-30 11:00:49 +0100

    AG-15300 add headerName to permitted selection col def props

--- packages/ag-grid-community/src/entities/gridOptions.ts
+++ packages/ag-grid-community/src/entities/gridOptions.ts
@@ -3035,6 +3035,7 @@ export type SelectionColumnDef = Pick<
     | 'headerClass'
     | 'headerComponent'
     | 'headerComponentParams'
+    | 'headerName'
     | 'mainMenuItems'
     | 'suppressHeaderContextMenu'
     | 'suppressHeaderMenuButton'
