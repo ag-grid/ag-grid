3e09205b6fb73fd3d9c7946b8ae8ae5afe944580
Stephen Cooper <scooperdev@gmail.com>
2025-07-01 23:29:19 +0100

    stop filtering tests

--- testing/ag-grid-docs/e2e/exampleTestRunner.ts
+++ testing/ag-grid-docs/e2e/exampleTestRunner.ts
@@ -37,15 +37,14 @@ export async function getFrameworkExamples(framework: InternalFramework) {
         examples = JSON.parse(readFileSync(filePath, 'utf-8'));
     }
 
-    return (examples as ExampleTestCase[])
-        .filter(
-            (e) =>
-                e.internalFramework === framework &&
-                !matchesExclusion(e) &&
-                // ag-grid.com still uses the old importType
-                ((e as any).importType === undefined || (e as any).importType === 'modules')
-        )
-        .splice(0, 5); // Limit to 10 examples per framework for testing purposes
+    return (examples as ExampleTestCase[]).filter(
+        (e) =>
+            e.internalFramework === framework &&
+            !matchesExclusion(e) &&
+            // ag-grid.com still uses the old importType
+            ((e as any).importType === undefined || (e as any).importType === 'modules')
+    );
+    // .splice(0, 5); // Limit to 10 examples per framework for testing purposes
 }
 
 export async function getSelectionOfFrameworkExamples(
