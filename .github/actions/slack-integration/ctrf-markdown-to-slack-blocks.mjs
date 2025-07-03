f08b655e7ee7e4f7da6a82bda29a28897a9b02f8
Victor Musienko <victor.m@noibu.com>
2025-07-03 11:23:12 +0100

    - use custom ctrf

--- .github/actions/slack-integration/ctrf-markdown-to-slack-blocks.mjs
+++ .github/actions/slack-integration/ctrf-markdown-to-slack-blocks.mjs
@@ -1,8 +1,12 @@
-const ctrfReport = process.env.CTRF_OUTPUT;
-
-if (!ctrfReport) {
+const ctrfSummary = process.env.CTRF_OUTPUT;
+const ctrfReport = process.env.CTRF_REPORT;
+if (!ctrfSummary) {
     console.log('CTRF_OUTPUT environment variable must be set.');
+}
+if (!ctrfReport) {
+    console.log('CTRF_REPORT environment variable must be set.');
 }
 
 console.log('Converting CTRF report to Slack blocks...');
-console.log(`CTRF report: ${ctrfReport}`);
+console.log(`CTRF summary:\n${ctrfSummary}`);
+console.log(`CTRF report:\n${ctrfReport}`);
