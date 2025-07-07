3c5e370880b9533de173bb70e1d4bdc1e28f60de
Tak Tran <contact@tutaktran.com>
2025-07-07 16:14:19 +0100

    AG-15239 - Allow trial license form to be switched out based on url search params

--- external/ag-website-shared/src/components/trial-licence-form/TrialLicenceForm.tsx
+++ external/ag-website-shared/src/components/trial-licence-form/TrialLicenceForm.tsx
@@ -1,16 +1,32 @@
 import type { FunctionComponent } from 'react';
+import { useEffect, useState } from 'react';
 
 import { TrialLicenceFormEmailOnly } from './TrialLicenceFormEmailOnly';
 import { TrialLicenceFormOriginal } from './TrialLicenceFormOriginal';
 
+type TrialLicenceFormType = 'emailOnly' | 'allFields' | 'original';
+
 interface Props {
-    type?: 'emailOnly' | 'allFields' | 'original';
+    type?: TrialLicenceFormType;
 }
 
+const trialLicenseTypes: readonly TrialLicenceFormType[] = ['emailOnly', 'allFields', 'original'] as const;
+
 export const TrialLicenceForm: FunctionComponent = ({ type = 'emailOnly' }: Props) => {
-    if (type === 'emailOnly') {
+    const [trialLicenseType, setTrialLicenseType] = useState<TrialLicenceFormType>(type);
+
+    useEffect(() => {
+        const url = new URL(window.location.toString());
+        const type = url.searchParams.get('trialLicense') as TrialLicenceFormType;
+
+        if (trialLicenseTypes.includes(type)) {
+            setTrialLicenseType(type);
+        }
+    }, []);
+
+    if (trialLicenseType === 'emailOnly') {
         return <TrialLicenceFormEmailOnly />;
-    } else if (type === 'original') {
+    } else if (trialLicenseType === 'original') {
         return <TrialLicenceFormOriginal />;
     }
 };
