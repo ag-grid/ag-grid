4a5fd5f13f83aa8781cbde9b3affe5f47c300a10
Tak Tran <contact@tutaktran.com>
2025-07-08 15:44:29 +0100

    AG-15239 - Remove useEffect to change the trial license
    Didn't work with emailOnly and allFields, probably because of the ZoomInfo script. Not needed anyway.

--- external/ag-website-shared/src/components/trial-licence-form/TrialLicenceForm.tsx
+++ external/ag-website-shared/src/components/trial-licence-form/TrialLicenceForm.tsx
@@ -1,6 +1,5 @@
 import { DEFAULT_TRIAL_LICENSE_FORM, type TrialLicenceFormType } from '@constants';
 import type { FunctionComponent } from 'react';
-import { useEffect, useState } from 'react';
 
 import { TrialLicenceFormAllFields } from './TrialLicenceFormAllFields';
 import { TrialLicenceFormEmailOnly } from './TrialLicenceFormEmailOnly';
@@ -10,23 +9,10 @@ interface Props {
     type?: TrialLicenceFormType;
 }
 
-const trialLicenseTypes: readonly TrialLicenceFormType[] = ['emailOnly', 'allFields', 'original'] as const;
-
 export const TrialLicenceForm: FunctionComponent = ({ type = DEFAULT_TRIAL_LICENSE_FORM }: Props) => {
-    const [trialLicenseType, setTrialLicenseType] = useState<TrialLicenceFormType>(type);
-
-    useEffect(() => {
-        const url = new URL(window.location.toString());
-        const type = url.searchParams.get('trialLicense') as TrialLicenceFormType;
-
-        if (trialLicenseTypes.includes(type)) {
-            setTrialLicenseType(type);
-        }
-    }, []);
-
-    if (trialLicenseType === 'emailOnly') {
+    if (type === 'emailOnly') {
         return <TrialLicenceFormEmailOnly />;
-    } else if (trialLicenseType === 'allFields') {
+    } else if (type === 'allFields') {
         return <TrialLicenceFormAllFields />;
     } else {
         return <TrialLicenceFormOriginal />;
