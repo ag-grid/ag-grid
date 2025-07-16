8987f1b7d3d551d0d51c70bc6ab79e712b0ae260
Stephen Cooper <scooperdev@gmail.com>
2025-07-16 15:52:55 +0100

    AG-15364 Avoid creating components if the grid is destroyed

--- packages/ag-grid-react/src/reactUi/gridComp.tsx
+++ packages/ag-grid-react/src/reactUi/gridComp.tsx
@@ -63,7 +63,7 @@ const GridComp = ({ context }: GridCompProps) => {
         focusInnerElementRef.current = gridCtrl.focusInnerElement.bind(gridCtrl);
 
         const compProxy: IGridComp = {
-            destroyGridUi: () => {}, // do nothing, as framework users destroy grid by removing the comp
+            destroyGridUi: () => { }, // do nothing, as framework users destroy grid by removing the comp
             setRtlClass,
             forceFocusOutOfContainer: (up?: boolean) => {
                 if (!up && paginationCompRef.current?.isDisplayed()) {
@@ -99,7 +99,7 @@ const GridComp = ({ context }: GridCompProps) => {
     useEffect(() => {
         const gridCtrl = gridCtrlRef.current;
         const eRootWrapper = eRootWrapperRef.current;
-        if (!tabGuardReady || !beans || !gridCtrl || !eGridBodyParent || !eRootWrapper) {
+        if (!tabGuardReady || !beans || !gridCtrl || !eGridBodyParent || !eRootWrapper || context.isDestroyed()) {
             return;
         }
 
