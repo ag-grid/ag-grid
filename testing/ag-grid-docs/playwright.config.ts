ba1a04ce6db01da0346532aafd47857ad557b5b2
Stephen Cooper <scooperdev@gmail.com>
2025-07-17 10:41:37 +0100

    try changing server code

--- testing/ag-grid-docs/playwright.config.ts
+++ testing/ag-grid-docs/playwright.config.ts
@@ -98,7 +98,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: 'npx nx dev',
+        command: 'nx dev',
         url: 'https://localhost:4610',
         reuseExistingServer: !process.env.CI,
         cwd: '../../',
