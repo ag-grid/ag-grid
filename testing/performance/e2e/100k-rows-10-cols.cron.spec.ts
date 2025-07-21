8465432ec74e529b805dfd1abb5f0e91f0c9c89f
Victor Musienko <victor.m@noibu.com>
2025-07-21 17:28:11 +0100

    wip

---
+++ testing/performance/e2e/100k-rows-10-cols.cron.spec.ts
@@ -0,0 +1,29 @@
+import type { TestCase } from '../benchmarking';
+import { getUrl } from '../benchmarking';
+import test from '../benchmarking';
+import { waitFor } from '../playwright.utils';
+
+const athleteCheck = () => document.body.textContent!.includes('Tony Smith');
+test(`Performance Test - `, {
+    timeout: 10 * 60_000,
+    minIterations: 5,
+    maxIterations: 10,
+    warmupIterations: 5,
+    testCases: [
+        {
+            name: 'example',
+            description: 'Scheduled: demo pages',
+            control: { version: 'local', url: getUrl({ url: 'https://localhost:4610/example/?rows=100000&cols=10' }) },
+            variant: {
+                version: 'staging',
+                url: getUrl({ url: 'https://grid-staging.ag-grid.com/example/?rows=100000&cols=10' }),
+            },
+            setupPreActions: async (page) => {
+                await waitFor(athleteCheck, page);
+            },
+            actions: async (page) => {
+                await waitFor(athleteCheck, page);
+            },
+        } as TestCase,
+    ],
+});
