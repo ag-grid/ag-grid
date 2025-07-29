be5dee27d44d736acabec81241128c4570fa7684
Stephen Cooper <scooperdev@gmail.com>
2025-07-29 15:26:55 +0100

    Fis tests

--- documentation/ag-grid-docs/src/content/docs/aggregation/_examples/aggregation-overview/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/aggregation/_examples/aggregation-overview/example.spec.ts
@@ -11,7 +11,9 @@ test.describe(pageExampleUrl, () => {
 
             const agIdFor = wrapAgTestIdFor((testId) => page.getByTestId(testId));
 
-            await expect(agIdFor.autoGroupCell('row-group-country-Canada')).toContainText('Canada (351)');
+            await expect(agIdFor.autoGroupCell('row-group-country-Canada')).toContainText('Canada (351)', {
+                useInnerText: true,
+            });
             await expect(agIdFor.cell('row-group-country-Canada', 'bronze')).toContainText('104');
             await expect(agIdFor.cell('row-group-country-Canada', 'silver')).toContainText('5');
             await expect(agIdFor.cell('row-group-country-Canada', 'gold')).toContainText('0.47863247863247865');
