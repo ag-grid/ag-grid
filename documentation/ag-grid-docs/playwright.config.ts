604a7c180f4eaf1295416e83685dfb0c69b9038b
Stephen Cooper <scooperdev@gmail.com>
2025-07-30 16:32:16 +0100

    remove unused import

--- documentation/ag-grid-docs/playwright.config.ts
+++ documentation/ag-grid-docs/playwright.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, devices } from '@playwright/test';
 
 const baseURL = process.env['PUBLIC_SITE_URL'] || 'https://localhost:4610';
 
+// eslint-disable-next-line no-console
 console.log(`Using base URL: ${baseURL}`);
 
 /**
--- documentation/ag-grid-docs/src/utils/grid/test-utils.ts
+++ documentation/ag-grid-docs/src/utils/grid/test-utils.ts
@@ -1,5 +1,5 @@
 import type { Page } from '@playwright/test';
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 type ExampleUrl = `${string}/${string}`;
 
