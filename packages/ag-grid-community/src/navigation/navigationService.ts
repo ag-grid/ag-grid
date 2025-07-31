6c90c734d112fa8382a42070287824b90665b5d5
Andrew Glazier <andrew.glazier@ag-grid.com>
2025-07-31 17:10:20 +0100

    AG-15664 - Fix col span focus

--- packages/ag-grid-community/src/navigation/navigationService.ts
+++ packages/ag-grid-community/src/navigation/navigationService.ts
@@ -216,7 +216,7 @@ export class NavigationService extends BeanStub implements NamedBean {
 
         const rowPixelDiff = up
             ? // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
-              currentRowNode?.rowHeight! - pixelsInOnePage - pagingPixelOffset
+            currentRowNode?.rowHeight! - pixelsInOnePage - pagingPixelOffset
             : pixelsInOnePage - pagingPixelOffset;
 
         // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
@@ -348,15 +348,19 @@ export class NavigationService extends BeanStub implements NamedBean {
 
     private onCtrlUpDownLeftRight(key: string, gridCell: CellPosition): void {
         const cellToFocus = this.beans.cellNavigation!.getNextCellToFocus(key, gridCell, true)!;
-        const { rowIndex, rowPinned } = cellToFocus;
-        const column = cellToFocus.column as AgColumn;
+        // in case we have col spanning we get the cellComp and use it to get the
+        // position. This was we always focus the first cell inside the spanning.
+        const normalisedPosition = this.getNormalisedPosition(cellToFocus);
+        const { rowIndex, rowPinned, column } = normalisedPosition ?? cellToFocus;
+        const col = column as AgColumn;
+
 
         this.navigateTo({
             scrollIndex: rowIndex,
             scrollType: null,
-            scrollColumn: column,
+            scrollColumn: col,
             focusIndex: rowIndex,
-            focusColumn: column,
+            focusColumn: col,
             rowPinned,
         });
     }
