6f4c7b78be80940db84ca1fd3294a481599dc8ee
Victor Musienko <victor.m@noibu.com>
2025-07-31 16:52:17 +0100

    Use existing api for checking if element is visible
    Signed-off-by: Victor Musienko <victor.m@noibu.com>

--- packages/ag-grid-enterprise/src/sideBar/agSideBar.ts
+++ packages/ag-grid-enterprise/src/sideBar/agSideBar.ts
@@ -19,6 +19,7 @@ import {
     _focusInto,
     _focusNextGridCoreContainer,
     _getActiveDomElement,
+    _isVisible,
     _removeFromParent,
     _setAriaControlsAndLabel,
     _warn,
@@ -120,7 +121,7 @@ class AgSideBar extends Component implements ISideBar {
 
         if (!nextEl) {
             nextEl = sideBarGui.querySelector('.ag-selected button') as HTMLElement;
-            nextEl = nextEl.closest('.ag-hidden') ? null : nextEl;
+            nextEl = _isVisible(nextEl) ? nextEl : null;
         }
 
         if (nextEl && nextEl !== e.target) {
