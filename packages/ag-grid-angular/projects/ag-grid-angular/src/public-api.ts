93fee8e49930a03b3f95ca06b07c2ab099df94f3
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-08-01 22:37:26 +0200

    Add exports for AgGridService and AgGridProvider in public API

--- packages/ag-grid-angular/projects/ag-grid-angular/src/public-api.ts
+++ packages/ag-grid-angular/projects/ag-grid-angular/src/public-api.ts
@@ -1,5 +1,7 @@
 export * from './lib/ag-grid-angular.component';
 export * from './lib/ag-grid-angular.module';
+export * from './lib/ag-grid-angular.service';
+export * from './lib/ag-grid-angular.provider';
 export * from './lib/interfaces';
 export * from './lib/angularFrameworkComponentWrapper';
 export * from './lib/angularFrameworkOverrides';
