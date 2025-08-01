0d9d6ae14c96cc163dc4ed353a15f4b9995fbad3
Mohamed Ben Makhlouf <med.b.makhlouf@gmail.com>
2025-08-01 23:13:54 +0200

    Refactor lazy loading of ag-Grid modules to use named imports for better clarity

--- packages/ag-grid-angular/projects/ag-grid-angular/src/lib/ag-grid-angular.service.ts
+++ packages/ag-grid-angular/projects/ag-grid-angular/src/lib/ag-grid-angular.service.ts
@@ -9,20 +9,20 @@ export class AgGridService {
 
     private async loadAgGridWithModules() {
         const config = this.config;
-        const agGridCommunity = await import('ag-grid-community');
+        const { ModuleRegistry, provideGlobalGridOptions } = await import('ag-grid-community');
         if (config.modules){
           const modules = await Promise.all(config.modules());
-          agGridCommunity.ModuleRegistry.registerModules(modules);
+          ModuleRegistry.registerModules(modules);
         }
         // If a global grid options function is provided, set it for ag-Grid Community
         if (config.options){
           const options = await config.options();
-          agGridCommunity.provideGlobalGridOptions(options);
+          provideGlobalGridOptions(options);
         }
         // If a license key is provided, set it for ag-Grid Enterprise
         if (config.licenseKey){
-          const agGridEnterprise = await import('ag-grid-enterprise');
-          agGridEnterprise.LicenseManager.setLicenseKey(config.licenseKey);
+          const { LicenseManager } = await import('ag-grid-enterprise');
+          LicenseManager.setLicenseKey(config.licenseKey);
         }
     }
 
