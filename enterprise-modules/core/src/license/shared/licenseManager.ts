9855bc623d7af7f6f00b423f6acc84a230998122
AgGitDeployment <deployments.team@ag-grid.com>
2025-08-06 19:28:39 +0100

    Release 32.3.9 Prep

--- enterprise-modules/core/src/license/shared/licenseManager.ts
+++ enterprise-modules/core/src/license/shared/licenseManager.ts
@@ -14,7 +14,7 @@ export interface ILicenseManager {
 }
 
 export class LicenseManager {
-    private static RELEASE_INFORMATION: string = 'MTczMTQzMDQwMzMxOQ==';
+    private static RELEASE_INFORMATION: string = 'MTc1NDUwNDcwNTI3NA==';
     private static licenseKey: string;
     private static chartsLicenseManager?: ILicenseManager;
     private watermarkMessage: string | undefined = undefined;
