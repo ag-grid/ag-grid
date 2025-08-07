a82b37a3050213cc07247c9dfe5bfdfbe5c205ff
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-08-07 13:43:27 +0100

    AG-4663-drag-not-allowed-icon

--- packages/ag-grid-community/src/entities/gridOptions.ts
+++ packages/ag-grid-community/src/entities/gridOptions.ts
@@ -2616,7 +2616,7 @@ export interface GridOptions<TData = any> {
     onRowDragCancel?(event: RowDragCancelEvent<TData>): void;
 
     /**
-     * Called by managed drag and drop when rows are dropped on another row.
+     * Called by drag and drop when rows are dragged over another row to conditionally prevent dropping the dragged row on the hovered row.
      * The user can cancel the drop by returning `false` or customize the operation by returning a `IsRowValidDropPositionResult`.
      * @agModule `RowDragModule`
      */
