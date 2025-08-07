e1bb77c9af82d3efb3c35cd6749e3b0d5a1c1ae6
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-08-07 16:31:23 +0100

    AG-4663-drag-not-allowed-icon

--- testing/module-size/moduleDefinitions.ts
+++ testing/module-size/moduleDefinitions.ts
@@ -6,7 +6,7 @@ import type {
 
 export const AllGridCommunityModules: Record<`${CommunityModuleName}Module`, number> = {
     AlignedGridsModule: 6.88,
-    AllCommunityModule: 470.87,
+    AllCommunityModule: 475.92,
     CellApiModule: 0.28,
     CellStyleModule: 2.24,
     CheckboxEditorModule: 60.34,
