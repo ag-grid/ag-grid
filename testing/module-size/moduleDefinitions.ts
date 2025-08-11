13388cccf52ba6932aa214ed4e6d2cbbf59812f9
Elias Malik <elias.malik@ag-grid.com>
2025-08-11 17:03:58 +0100

    bump module

--- testing/module-size/moduleDefinitions.ts
+++ testing/module-size/moduleDefinitions.ts
@@ -61,7 +61,7 @@ export const AllEnterpriseModules: Record<`${EnterpriseModuleName}Module`, numbe
     ExcelExportModule: 84,
     FiltersToolPanelModule: 133.66,
     FindModule: 14.51,
-    GridChartsModule: 67,
+    GridChartsModule: 69,
     IntegratedChartsModule: 397.02,
     GroupFilterModule: 111.6,
     MasterDetailModule: 82,
