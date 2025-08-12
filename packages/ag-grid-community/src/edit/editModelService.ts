40ac90f0c2e734f973fb5789e16889c7b2233e47
Steph Meslin-Weber <steph@tangency.co.uk>
2025-08-12 12:21:57 +0100

    Update packages/ag-grid-community/src/edit/editModelService.ts
    Co-authored-by: Pete Reynolds <40919976+peterjrreynolds@users.noreply.github.com>

--- packages/ag-grid-community/src/edit/editModelService.ts
+++ packages/ag-grid-community/src/edit/editModelService.ts
@@ -145,7 +145,7 @@ export class EditModelService extends BeanStub implements NamedBean, IEditModelS
                 // Ensure we copy the cell data to avoid reference issues
                 newEditRow.set(column, { ...cellData })
             );
-           map.set(rowNode, newEditRow);
+            map.set(rowNode, newEditRow);
         });
         return map;
     }
