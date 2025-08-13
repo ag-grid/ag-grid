0817166c33cd5f54b9b5f69a856e1692dd746bce
Kyler Phillips <kylerphillips@hotmail.co.uk>
2025-08-13 14:29:41 +0100

    AG-15679 Updates to hero

--- documentation/ag-grid-docs/src/components/campaigns-components/ReturnToSupportPage.tsx
+++ documentation/ag-grid-docs/src/components/campaigns-components/ReturnToSupportPage.tsx
@@ -13,7 +13,7 @@ interface ReturnToSupportPageProps {
 }
 
 const ReturnToSupportPage: React.FC<ReturnToSupportPageProps> = ({ versionsData, utm }) => {
-    const endDate = new Date('2025-08-31T23:59:59');
+    const endDate = new Date('2025-07-31T23:59:59');
 
     // Content states
     const [heroTitle, setHeroTitle] = useState('Welcome back to better support & new features');
@@ -72,7 +72,12 @@ const ReturnToSupportPage: React.FC<ReturnToSupportPageProps> = ({ versionsData,
                             <img
                                 src={urlWithBaseUrl('images/campaigns/hero-ag-grid.png')}
                                 alt="AG Grid Hero"
-                                className={styles.rtsHeroImage}
+                                className={`${styles.rtsHeroImage} ${styles.rtsHeroImageLight}`}
+                            />
+                            <img
+                                src={urlWithBaseUrl('images/campaigns/hero-ag-grid-dark.png')}
+                                alt="AG Grid Hero"
+                                className={`${styles.rtsHeroImage} ${styles.rtsHeroImageDark}`}
                             />
                         </div>
                     </div>
