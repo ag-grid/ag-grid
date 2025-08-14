234c0e09f7f5d6be28ce438b9d67fecd71518850
Stephen Cooper <scooperdev@gmail.com>
2025-08-14 17:14:41 +0100

    Fix formatting

--- packages/ag-grid-react/src/reactUi/header/headerGroupCellComp.tsx
+++ packages/ag-grid-react/src/reactUi/header/headerGroupCellComp.tsx
@@ -110,13 +110,7 @@ const HeaderGroupCellComp = ({ ctrl }: { ctrl: HeaderGroupCellCtrl }) => {
     const UserCompClass = userCompDetails?.componentClass;
 
     return (
-        <div
-            ref={setRef}
-            style={userStyles}
-            className={className}
-            role="columnheader"
-            aria-expanded={ariaExpanded}
-        >
+        <div ref={setRef} style={userStyles} className={className} role="columnheader" aria-expanded={ariaExpanded}>
             <div ref={eHeaderCompWrapper} className="ag-header-cell-comp-wrapper" role="presentation">
                 {reactUserComp ? (
                     userCompStateless ? (
