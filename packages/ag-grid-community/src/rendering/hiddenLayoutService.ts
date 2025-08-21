b7372d99cd56ebaf0e6ab8a71076ddb411e484b1
Stephen Cooper <scooperdev@gmail.com>
2025-08-21 09:37:40 +0100

    remove debug logging

--- packages/ag-grid-community/src/rendering/hiddenLayoutService.ts
+++ packages/ag-grid-community/src/rendering/hiddenLayoutService.ts
@@ -4,20 +4,17 @@ import type { _ModuleWithoutApi } from '../interfaces/iModule';
 import { VERSION } from '../version';
 import { hiddenLayoutCSS } from './hidden-layout.css-GENERATED';
 
+const HideClass = 'ag-delay-render';
+
 export class HiddenLayoutService extends BeanStub implements NamedBean {
     beanName = 'hiddenLayoutSvc' as const;
 
     private showCellsOnDelay: boolean = false;
 
     public hideColumns() {
-        console.info('AG Grid: Request Hiding columns');
-
-        // TODO do we need to wait for ctrlsSvc to be ready?
         this.beans.ctrlsSvc.whenReady(this, (p) => {
             this.showCellsOnDelay = true;
-            console.warn('AG Grid: Hiding columns');
-
-            p.gridBodyCtrl.eGridBody.classList.add('ag-delay-render');
+            p.gridBodyCtrl.eGridBody.classList.add(HideClass);
         });
     }
     public revealColumns() {
@@ -26,8 +23,7 @@ export class HiddenLayoutService extends BeanStub implements NamedBean {
         }
 
         if (this.showCellsOnDelay) {
-            console.warn('AG Grid: Revealing cells after they were hidden for layout purposes');
-            this.beans.ctrlsSvc.getGridBodyCtrl().eGridBody.classList.remove('ag-delay-render');
+            this.beans.ctrlsSvc.getGridBodyCtrl().eGridBody.classList.remove(HideClass);
             this.showCellsOnDelay = false;
         }
     }
