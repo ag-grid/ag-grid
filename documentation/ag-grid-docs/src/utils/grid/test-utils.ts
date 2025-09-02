d89d6591b0c1c1fbbded9ac0181a504d05cebe08
Steph Meslin-Weber <steph@tangency.co.uk>
2025-09-02 15:16:45 +0100

    AG-15797 - additional conditions to recreate de-virtualized column editors

--- documentation/ag-grid-docs/src/utils/grid/test-utils.ts
+++ documentation/ag-grid-docs/src/utils/grid/test-utils.ts
@@ -431,7 +431,7 @@ export async function repeat(
 export async function scrollGridRelative(
     method: 'wheel' | 'element',
     page: Page,
-    { x, y }: { x?: number; y?: number },
+    { x, y, xStep, yStep }: { x?: number; y?: number; xStep?: number; yStep?: number },
     waitForTimeout = 10
 ) {
     async function scrollElement() {
@@ -439,28 +439,75 @@ export async function scrollGridRelative(
         const horizontalView = page.locator('.ag-viewport.ag-center-cols-viewport');
 
         if (y !== undefined) {
-            await verticalView.evaluate((el, { y }) => (el.scrollTop += y), { y });
+            if (yStep !== undefined) {
+                let currentY = 0;
+                const isNegative = y < 0;
+                while (isNegative ? currentY > y : currentY < y) {
+                    await verticalView.evaluate(
+                        (el, { yStep }) => {
+                            el.scrollTop += yStep;
+                        },
+                        { yStep }
+                    );
+                    currentY += yStep;
+                }
+            } else {
+                await verticalView.evaluate((el, { y }) => (el.scrollTop += y), { y });
+            }
             await page.waitForTimeout(waitForTimeout);
         }
 
         if (x !== undefined) {
-            await horizontalView.evaluate((el, { x }) => (el.scrollLeft += x), { x });
+            if (xStep !== undefined) {
+                let currentX = 0;
+                const isNegative = x < 0;
+                while (isNegative ? currentX > x : currentX < x) {
+                    await horizontalView.evaluate(
+                        (el, { xStep }) => {
+                            el.scrollLeft += xStep;
+                        },
+                        { xStep }
+                    );
+                    currentX += xStep;
+                }
+            } else {
+                await horizontalView.evaluate((el, { x }) => (el.scrollLeft += x), { x });
+            }
             await page.waitForTimeout(waitForTimeout);
         }
     }
 
     async function scrollWheel() {
         if (y !== undefined) {
-            await page.mouse.wheel(0, y);
-            await page.waitForTimeout(waitForTimeout);
+            if (yStep !== undefined) {
+                let currentY = 0;
+                const isNegative = y < 0;
+                while (isNegative ? currentY > y : currentY < y) {
+                    await page.mouse.wheel(0, yStep);
+                    await page.waitForTimeout(waitForTimeout);
+                    currentY += yStep;
+                }
+            } else {
+                await page.mouse.wheel(0, y);
+                await page.waitForTimeout(waitForTimeout);
+            }
         }
 
         if (x !== undefined) {
-            await page.mouse.wheel(x, 0);
-            await page.waitForTimeout(waitForTimeout);
+            if (xStep !== undefined) {
+                let currentX = 0;
+                const isNegative = x < 0;
+                while (isNegative ? currentX > x : currentX < x) {
+                    await page.mouse.wheel(xStep, 0);
+                    await page.waitForTimeout(waitForTimeout);
+                    currentX += xStep;
+                }
+            } else {
+                await page.mouse.wheel(x, 0);
+                await page.waitForTimeout(waitForTimeout);
+            }
         }
     }
-
     const directionWord = [];
 
     if (x !== undefined && x !== 0) {
