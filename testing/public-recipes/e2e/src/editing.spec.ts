e169089293b03a4b07fc43917aba9bc5ba8cc005
Stephen Cooper <scooperdev@gmail.com>
2025-09-02 16:19:34 +0100

    Example updated to match new random start

--- testing/public-recipes/e2e/src/editing.spec.ts
+++ testing/public-recipes/e2e/src/editing.spec.ts
@@ -14,8 +14,8 @@ test.describe('Edit e2e testing examples', () => {
 
             await expect(agIdFor.overlay()).toBeHidden();
 
-            await expect(agIdFor.cell('0', 'color')).toContainText('Violet');
-            await expect(agIdFor.cell('0', 'value')).toContainText('£ 118');
+            await expect(agIdFor.cell('0', 'color')).toContainText('PowderBlue');
+            await expect(agIdFor.cell('0', 'value')).toContainText('£ 246');
 
             // Edit the color cell
             const cell = agIdFor.cell('0', 'color');
