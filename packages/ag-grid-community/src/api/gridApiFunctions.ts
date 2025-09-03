8ee47ad9f748d4610cb59ae9c9ca5fdd5b582346
sdwvit <victor.musienko@ag-grid.com>
2025-09-03 16:55:45 +0100

    Add resetRowHeights method to _RowModelSharedApi
     - Add _RowModelSharedApi import to gridApiFunctions.ts
     - Introduce resetRowHeights method in _RowModelSharedApi interface
     - Include module documentation comment for resetRowHeights

--- packages/ag-grid-community/src/api/gridApiFunctions.ts
+++ packages/ag-grid-community/src/api/gridApiFunctions.ts
@@ -1,5 +1,5 @@
 import type { ValidationModuleName } from '../interfaces/iModule';
-import type { _CsrmSsrmSharedGridApi, _FindApi } from './gridApi';
+import type { _CsrmSsrmSharedGridApi, _FindApi, _RowModelSharedApi } from './gridApi';
 import type {
     GridApi,
     _AdvancedFilterGridApi,
@@ -291,7 +291,10 @@ export const gridApiFunctionsMap: Record<keyof GridApi, ValidationModuleName> = 
         getCacheBlockState: 0,
         isLastRowIndexKnown: 0,
     }),
-
+    ...mod<_RowModelSharedApi>('RowModelSharedApi', {
+        onRowHeightChanged: 0,
+        resetRowHeights: 0,
+    }),
     ...mod<_ClientSideRowModelGridApi<any>>('ClientSideRowModelApi', {
         onGroupExpandedOrCollapsed: 0,
         refreshClientSideRowModel: 0,
