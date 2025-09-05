9bcada3b6fb73ca8cbd8c874c34407a7e0f17f48
Steph Meslin-Weber <steph@tangency.co.uk>
2025-09-05 11:35:39 +0100

    AG-15792 - baseline testcases

--- documentation/ag-grid-docs/src/content/docs/pivoting-result-columns/_examples/pivot-result-summary/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/pivoting-result-columns/_examples/pivot-result-summary/example.spec.ts
@@ -1,12 +1,206 @@
-import { expect, test } from '@utils/grid/test-utils';
-import type { Page } from 'playwright/test';
-
-import type { ICellEditorParams } from 'ag-grid-community';
+import { ensureGridReady, expect, test } from '@utils/grid/test-utils';
 
 test.agExample(import.meta, () => {
-    ['Enter', 'Escape', 'Tab'].forEach((key) => {
+    // Values captured versus 33.3.0
+    [
+        {
+            editFinishKey: 'Enter',
+            expected: [
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 7,
+                    },
+                ],
+                [
+                    'cellEditingStopped',
+                    {
+                        newValue: 1234,
+                        oldValue: 7,
+                        value: 7,
+                        valueChanged: false,
+                    },
+                ],
+            ],
+        },
+        {
+            editFinishKey: 'Escape',
+            expected: [
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 7,
+                    },
+                ],
+                [
+                    'cellEditingStopped',
+                    {
+                        newValue: undefined,
+                        oldValue: 7,
+                        value: 7,
+                        valueChanged: false,
+                    },
+                ],
+            ],
+        },
+        {
+            editFinishKey: 'Tab',
+            expected: [
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 7,
+                    },
+                ],
+                [
+                    'cellEditingStopped',
+                    {
+                        newValue: 1234,
+                        oldValue: 7,
+                        value: 7,
+                        valueChanged: false,
+                    },
+                ],
+                // 33.3.0 only, not applicable in 34.x.0
+                // [
+                //     'cellEditingStarted',
+                //     {
+                //         value: 1,
+                //     },
+                // ],
+            ],
+        },
+    ].forEach(({ editFinishKey, expected }) => {
         test.vanilla(
-            `Edit on cell with [${key}] when enableGroupEdits=true`,
+            `Keyboard-started Edit on cell with [${editFinishKey}] when enableGroupEdits=true`,
+            async ({ page, agIdFor, remoteGrid, request }) => {
+                const response = await request.get('/example-assets/small-olympic-winners.json', {
+                    ignoreHTTPSErrors: true,
+                });
+                const data = await response.json();
+
+                const remoteApi = remoteGrid(page, '1');
+
+                // Integration Testing with initial options
+                await remoteApi.recreateGrid({
+                    columnDefs: [
+                        { field: 'country', rowGroup: true },
+                        { field: 'year', pivot: true },
+                        {
+                            field: 'gold',
+                            aggFunc: 'sum',
+                        },
+                        { field: 'silver', aggFunc: 'sum' },
+                        { field: 'bronze', aggFunc: 'sum' },
+                    ],
+                    defaultColDef: {
+                        flex: 1,
+                        minWidth: 130,
+                        editable: true,
+                    },
+                    autoGroupColumnDef: {
+                        minWidth: 200,
+                        editable: true,
+                    },
+                    // singleClickEdit: true,
+                    enableGroupEdit: true,
+                    pivotMode: true,
+                    rowData: data,
+                });
+
+                await ensureGridReady(page);
+
+                await remoteApi.logEvent('cellEditingStarted', ['value']);
+                await remoteApi.logEvent('cellValueChanged', ['newValue', 'oldValue', 'source']);
+                await remoteApi.logEvent('cellEditingStopped', ['newValue', 'oldValue', 'value', 'valueChanged']);
+
+                const cell = agIdFor.cell('row-group-country-United States', 'pivot_year_2000_gold');
+                await expect(cell).toBeVisible();
+                await cell.press('1');
+                const editor = cell.locator('input');
+                await expect(editor).toBeVisible();
+                await page.keyboard.type('234');
+                await page.keyboard.press(editFinishKey);
+
+                const eventLog = await remoteGrid.waitForEventlog(250);
+
+                expect(eventLog).toMatchObject(expected);
+            }
+        );
+    });
+
+    // Values captured versus 33.3.0
+
+    [
+        {
+            editFinishKey: 'Enter',
+            expected: [
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 7,
+                    },
+                ],
+                [
+                    'cellEditingStopped',
+                    {
+                        newValue: 1234,
+                        oldValue: 7,
+                        value: 7,
+                        valueChanged: false,
+                    },
+                ],
+            ],
+        },
+        {
+            editFinishKey: 'Escape',
+            expected: [
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 7,
+                    },
+                ],
+                [
+                    'cellEditingStopped',
+                    {
+                        newValue: undefined,
+                        oldValue: 7,
+                        value: 7,
+                        valueChanged: false,
+                    },
+                ],
+            ],
+        },
+        {
+            editFinishKey: 'Tab',
+            expected: [
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 7,
+                    },
+                ],
+                [
+                    'cellEditingStopped',
+                    {
+                        newValue: 1234,
+                        oldValue: 7,
+                        value: 7,
+                        valueChanged: false,
+                    },
+                ],
+                [
+                    'cellEditingStarted',
+                    {
+                        value: 1,
+                    },
+                ],
+            ],
+        },
+    ].forEach(({ editFinishKey, expected }) => {
+        test.vanilla(
+            `DblClick-started Edit on cell with [${editFinishKey}] when enableGroupEdits=true`,
             async ({ page, agIdFor, remoteGrid, request }) => {
                 const response = await request.get('/example-assets/small-olympic-winners.json', {
                     ignoreHTTPSErrors: true,
@@ -46,86 +240,15 @@ test.agExample(import.meta, () => {
                 await remoteApi.logEvent('cellValueChanged', ['newValue', 'oldValue', 'source']);
                 await remoteApi.logEvent('cellEditingStopped', ['newValue', 'oldValue', 'value', 'valueChanged']);
 
-                const cell = agIdFor.cell('row-group-country-Australia', 'pivot_year_2000_gold');
+                const cell = agIdFor.cell('row-group-country-United States', 'pivot_year_2000_gold');
                 await cell.dblclick();
                 await page.keyboard.type('1234');
-                await page.keyboard.press('Enter');
+                await page.keyboard.press(editFinishKey);
 
                 const eventLog = await remoteGrid.waitForEventlog(250);
 
-                // Not really supported but unintentionally available in 34.0.0, so all keys have the same outcome
-                expect(eventLog.length).toBe(2);
-                expect(eventLog).toEqual([
-                    [
-                        'cellEditingStarted',
-                        {
-                            value: 3,
-                        },
-                    ],
-                    [
-                        'cellEditingStopped',
-                        {
-                            newValue: undefined,
-                            oldValue: undefined,
-                            value: 1234,
-                            valueChanged: true,
-                        },
-                    ],
-                ]);
+                expect(eventLog).toMatchObject(eventLog);
             }
         );
     });
 });
-
-const addCustomEditor = (page: Page) => {
-    page.addInitScript(() => {
-        (window as any).TestEditor = class TestEditor {
-            eInput?: HTMLInputElement;
-            params?: ICellEditorParams;
-
-            getGui(): HTMLElement {
-                return this.eInput!;
-            }
-
-            // @ts-ignore
-            init(params: ICellEditorParams) {
-                // (window as any).logEvent?.('init', []);
-                this.eInput = document.createElement('input');
-                this.eInput.type = 'text';
-                this.eInput.value = params.value;
-            }
-
-            getValue(): string | null {
-                (window as any).logEvent?.('getValue', []);
-                return this.eInput?.value ?? null;
-            }
-
-            isCancelAfterEnd(): boolean {
-                (window as any).logEvent?.('isCancelAfterEnd', []);
-                return (window as any).isCancelAfterEnd;
-            }
-
-            isCancelBeforeStart?(): boolean {
-                (window as any).logEvent?.('isCancelBeforeStart', []);
-                return (window as any).isCancelBeforeStart;
-            }
-
-            afterGuiAttached() {
-                this.eInput?.focus();
-            }
-        };
-    });
-};
-
-const setEditor = async (page: Page, field: string, editor: string) =>
-    await page.evaluate(`
-        window.getGridApi('1').updateGridOptions({
-            columnDefs: [
-                {
-                    field: '${field}',
-                    editable: true,
-                    cellEditor: window.${editor},
-                },
-            ],
-        })
-`);
