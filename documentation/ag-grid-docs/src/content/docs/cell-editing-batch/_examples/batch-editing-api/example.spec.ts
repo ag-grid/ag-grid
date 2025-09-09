bc2f892307573c9a9252bed8eee90c3c0e5cd173
Steph Meslin-Weber <steph@tangency.co.uk>
2025-09-09 09:01:26 +0100

    RTI-3054 - ensure API call succeeds

--- documentation/ag-grid-docs/src/content/docs/cell-editing-batch/_examples/batch-editing-api/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/cell-editing-batch/_examples/batch-editing-api/example.spec.ts
@@ -85,4 +85,88 @@ test.agExample(import.meta, () => {
         await expect(totalCell).toHaveText('105'); // verify the total cell has the new value
         await expect(totalCell).not.toHaveClass(/ag-cell-batch-edit/);
     });
+
+    test.eachFramework('GridApi + Styles: multiple batches', async ({ agIdFor, page, remoteGrid }) => {
+        const gridApi = remoteGrid(page);
+
+        await test.step('First Batch', async () => {
+            await gridApi.startBatchEdit();
+
+            const result = await gridApi.isBatchEditing();
+            expect(result).toBeTruthy();
+
+            const cell = agIdFor.cell('0', 'gold');
+            // initiate cell editing by double clicking the cell
+            await cell.dblclick();
+            const cellEditor = cell.locator('input');
+            await expect(cellEditor).toBeVisible();
+
+            await page.keyboard.type('100'); // type in a new value
+            await page.keyboard.press('Enter'); // press Enter to save the value
+
+            await expect(cellEditor).toHaveCount(0); // verify the cell editor is closed
+            await expect(cell).toHaveText('100'); // verify the cell has the new value
+            await expect(cell).toHaveClass(/ag-cell-batch-edit/);
+
+            const totalCell = agIdFor.cell('0', 'total');
+            await expect(totalCell).toHaveText('105'); // verify the total cell has the new value
+            await expect(totalCell).not.toHaveClass(/ag-cell-batch-edit/);
+
+            // now commit the batch
+            await gridApi.commitBatchEdit();
+
+            await page.waitForTimeout(100); // give the grid a moment to update
+
+            await expect(cellEditor).toHaveCount(0); // verify the cell editor is closed
+            await expect(cell).toHaveText('100'); // verify the cell has the new value
+            await expect(cell).not.toHaveClass(/ag-cell-batch-edit/);
+
+            await expect(totalCell).toHaveText('105'); // verify the total cell has the new value
+            await expect(totalCell).not.toHaveClass(/ag-cell-batch-edit/);
+        });
+
+        await test.step('2nd Batch', async () => {
+            await gridApi.setFocusedCell(1, 'gold');
+            await gridApi.startBatchEdit();
+
+            const result = await gridApi.isBatchEditing();
+            expect(result).toBeTruthy();
+
+            const cell = agIdFor.cell('1', 'gold');
+            // initiate cell editing by double clicking the cell
+            await cell.dblclick();
+            const cellEditor = cell.locator('input');
+            await expect(cellEditor).toBeVisible();
+
+            await page.keyboard.type('100'); // type in a new value
+            await page.keyboard.press('Enter'); // press Enter to save the value
+
+            await expect(cellEditor).toHaveCount(0); // verify the cell editor is closed
+            await expect(cell).toHaveText('100'); // verify the cell has the new value
+            await expect(cell).toHaveClass(/ag-cell-batch-edit/);
+
+            const totalCell = agIdFor.cell('0', 'total');
+            await expect(totalCell).toHaveText('105'); // verify the total cell has the new value
+            await expect(totalCell).not.toHaveClass(/ag-cell-batch-edit/);
+
+            // now commit the batch
+            await gridApi.commitBatchEdit();
+
+            await page.waitForTimeout(100); // give the grid a moment to update
+
+            const gridApiHandle = await page.evaluateHandle(() => (window as any).getGridApi('1'));
+
+            const editingCellCount = await test.step('remoteGridApi.getEditingCells()', async () =>
+                await page.evaluate((gridApi) => gridApi.getEditingCells().length, gridApiHandle));
+
+            expect(editingCellCount).toBe(0);
+
+            await expect(cellEditor).toHaveCount(0); // verify the cell editor is closed
+            await expect(cell).toHaveText('100'); // verify the cell has the new value
+            await expect(cell).not.toHaveClass(/ag-cell-batch-edit/);
+
+            await expect(totalCell).toHaveText('105'); // verify the total cell has the new value
+            await expect(totalCell).not.toHaveClass(/ag-cell-batch-edit/);
+        });
+    });
 });
