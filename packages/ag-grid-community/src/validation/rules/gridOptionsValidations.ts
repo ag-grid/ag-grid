495fde34fb5959affce9631f8833ee7b5a44a548
Elias Malik <elias.malik@ag-grid.com>
2025-09-10 11:35:27 +0100

    specify correct module dependency

--- packages/ag-grid-community/src/validation/rules/gridOptionsValidations.ts
+++ packages/ag-grid-community/src/validation/rules/gridOptionsValidations.ts
@@ -127,7 +127,7 @@ export const GRID_OPTIONS_MODULES: Partial<Record<keyof GridOptions, RequiredMod
     getRowClass: 'RowStyle',
     getRowStyle: 'RowStyle',
     groupTotalRow: 'SharedRowGrouping',
-    grandTotalRow: ['SharedRowGrouping', 'TreeData'],
+    grandTotalRow: 'ClientSideRowModelHierarchy',
     initialState: 'GridState',
     isExternalFilterPresent: 'ExternalFilter',
     isRowPinnable: 'PinnedRow',
