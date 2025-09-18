75c85e7b611de6002d971d6abee40662f7c621f1
Tak Tran <contact@tutaktran.com>
2025-09-18 16:02:30 +0100

    AG-15781 - Add trailing slash to avoid S3 redirect

--- external/ag-website-shared/src/components/contact-form/constants.ts
+++ external/ag-website-shared/src/components/contact-form/constants.ts
@@ -5,6 +5,7 @@ import { urlWithBaseUrl } from '@utils/urlWithBaseUrl';
 export type ResultType = 'success' | 'failure';
 
 export const RETURN_URLS: Record<ResultType, string> = {
-    success: pathJoin(SITE_URL, urlWithBaseUrl('/contact/success')),
-    failure: pathJoin(SITE_URL, urlWithBaseUrl('/contact/failure')),
+    // NOTE: Need to add trailing slash to avoid 302 redirect on S3
+    success: pathJoin(SITE_URL, urlWithBaseUrl('/contact/success')) + '/',
+    failure: pathJoin(SITE_URL, urlWithBaseUrl('/contact/failure')) + '/',
 };
