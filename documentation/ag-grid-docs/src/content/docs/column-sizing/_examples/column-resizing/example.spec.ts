f11db4d9c4d6f99fa10bb32506211c072e4b333d
Elias Malik <elias.malik@ag-grid.com>
2025-09-19 11:35:06 +0100

    add regression cases to test

--- documentation/ag-grid-docs/src/content/docs/column-sizing/_examples/column-resizing/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/column-sizing/_examples/column-resizing/example.spec.ts
@@ -1,22 +1,72 @@
 import { expect, test } from '@utils/grid/test-utils';
+import type { Locator } from 'playwright/test';
+
+async function getWidth(locator: Locator): Promise<number | undefined> {
+    return (await locator.boundingBox())?.width;
+}
 
 test.agExample(import.meta, () => {
-    test.eachFramework('Example', async ({ page, agIdFor }) => {
-        const cellBBs = await Promise.all(
-            [
-                agIdFor.headerCell('athlete'),
-                agIdFor.headerCell('age'),
-                agIdFor.headerCell('country'),
-                agIdFor.headerCell('year'),
-                agIdFor.headerCell('date'),
-            ].map((locator) => locator.boundingBox())
+    test.eachFramework('fitCellToContents', async ({ page, agIdFor }) => {
+        expect(await getWidth(agIdFor.headerCell('athlete'))).toEqual(150);
+        expect(await getWidth(agIdFor.headerCell('age'))).toEqual(142);
+        expect(await getWidth(agIdFor.headerCell('country'))).toEqual(137);
+        expect(await getWidth(agIdFor.headerCell('year'))).toEqual(86);
+        expect(await getWidth(agIdFor.headerCell('date'))).toEqual(130);
+
+        expect(await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') }))).toEqual(
+            645
         );
 
-        const headerRow = page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') });
-        const headerRowBB = await headerRow.boundingBox();
-
-        const totalColumnWidth = cellBBs.reduce((acc, bb) => acc + (bb?.width ?? 0), 0);
-
-        expect(headerRowBB?.width).toBeCloseTo(totalColumnWidth, 1);
+        // API call doesn't use defaultMaxWidth so we expect a larger column
+        await page.locator('button.resize-button').click();
+        expect(await getWidth(agIdFor.headerCell('athlete'))).toEqual(185);
+        expect(await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') }))).toEqual(
+            680
+        );
     });
+
+    test.eachFramework(
+        'fitCellToContents combinations of skipHeaders and scaleUpToFitGridWidth',
+        async ({ page, agIdFor }) => {
+            // `skipHeaders`
+            await page.locator('#toggle-ignore-headers').click(); // on
+            await page.locator('button.resize-button').click();
+
+            expect(await getWidth(agIdFor.headerCell('athlete'))).toEqual(185);
+            expect(await getWidth(agIdFor.headerCell('age'))).toEqual(69);
+            expect(await getWidth(agIdFor.headerCell('country'))).toEqual(137);
+            expect(await getWidth(agIdFor.headerCell('year'))).toEqual(86);
+            expect(await getWidth(agIdFor.headerCell('date'))).toEqual(130);
+            expect(
+                await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') }))
+            ).toEqual(607);
+
+            // `scaleUpToFitGridWidth`
+            await page.locator('#toggle-ignore-headers').click(); // off
+            await page.locator('#toggle-scale-up').click(); // on
+            await page.locator('button.resize-button').click();
+
+            expect(await getWidth(agIdFor.headerCell('athlete'))).toEqual(185);
+            expect(await getWidth(agIdFor.headerCell('age'))).toEqual(150);
+            expect(await getWidth(agIdFor.headerCell('country'))).toEqual(342);
+            expect(await getWidth(agIdFor.headerCell('year'))).toEqual(256);
+            expect(await getWidth(agIdFor.headerCell('date'))).toEqual(313);
+            expect(
+                await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') }))
+            ).toEqual(1246);
+
+            // `skipHeaders` and `scaleUpToFitGridWidth`
+            await page.locator('#toggle-ignore-headers').click(); // on
+            await page.locator('button.resize-button').click();
+
+            expect(await getWidth(agIdFor.headerCell('athlete'))).toEqual(185);
+            expect(await getWidth(agIdFor.headerCell('age'))).toEqual(150);
+            expect(await getWidth(agIdFor.headerCell('country'))).toEqual(342);
+            expect(await getWidth(agIdFor.headerCell('year'))).toEqual(256);
+            expect(await getWidth(agIdFor.headerCell('date'))).toEqual(313);
+            expect(
+                await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') }))
+            ).toEqual(1246);
+        }
+    );
 });
