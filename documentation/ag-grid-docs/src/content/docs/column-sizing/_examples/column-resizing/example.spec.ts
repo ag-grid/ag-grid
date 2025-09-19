22f083de5b595c659ed94e84a79944d301f72b94
Elias Malik <elias.malik@ag-grid.com>
2025-09-19 12:04:05 +0100

    extra tests for pinned + selection cols

--- documentation/ag-grid-docs/src/content/docs/column-sizing/_examples/column-resizing/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/column-sizing/_examples/column-resizing/example.spec.ts
@@ -69,4 +69,42 @@ test.agExample(import.meta, () => {
             ).toEqual(1246);
         }
     );
+
+    test.describe('Example modifications', () => {
+        test.use({ agModules: ['RowSelectionModule'] });
+
+        test.eachFramework('fitCellContents + pinned col + selection col', async ({ page, remoteGrid, agIdFor }) => {
+            const remoteApi = remoteGrid(page, '1');
+
+            await remoteApi.setGridOption('rowSelection', { mode: 'multiRow' });
+            await remoteApi.setGridOption('columnDefs', [
+                { field: 'athlete', width: 150, pinned: 'left' },
+                {
+                    field: 'age',
+                    headerName: 'Age of Athlete',
+                    width: 90,
+                },
+                { field: 'country', width: 120 },
+                { field: 'year', width: 90 },
+                { field: 'date', width: 110 },
+            ]);
+
+            await page.locator('#toggle-scale-up').click(); // on
+            await page.locator('button.resize-button').click();
+
+            expect(await getWidth(agIdFor.headerCell('athlete'))).toEqual(185);
+            expect(await getWidth(agIdFor.headerCell('age'))).toEqual(222);
+            expect(await getWidth(agIdFor.headerCell('country'))).toEqual(296);
+            expect(await getWidth(agIdFor.headerCell('year'))).toEqual(222);
+            expect(await getWidth(agIdFor.headerCell('date'))).toEqual(271);
+
+            expect(await getWidth(agIdFor.headerCell('ag-Grid-SelectionColumn'))).toEqual(50);
+
+            const pinnedWidth =
+                (await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('athlete') }))) ?? 0;
+            const mainWidth =
+                (await getWidth(page.locator('.ag-header-row').filter({ has: agIdFor.headerCell('age') }))) ?? 0;
+            expect(pinnedWidth + mainWidth).toEqual(1246);
+        });
+    });
 });
