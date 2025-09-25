d1119b27e8e5e8afb3b5a4ab8513fb781d0517b0
Stephen Cooper <scooperdev@gmail.com>
2025-09-25 23:34:27 +0100

    fix tests

--- documentation/ag-grid-docs/src/content/docs/master-detail-grids/_examples/template-callback-customisation/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/master-detail-grids/_examples/template-callback-customisation/example.spec.ts
@@ -1,7 +1,9 @@
 import { clickAllButtons, ensureGridReady, test } from '@utils/grid/test-utils';
 
 test.agExample(import.meta, () => {
-    test.eachFramework('Example', async ({ page }) => {
+    test.eachFramework('Example', async ({ agFramework, page }) => {
+        test.skip(agFramework.includes('react'), 'Example not for React.');
+
         // PLACEHOLDER - MINIMAL TEST TO ENSURE GRID LOADS WITHOUT ERRORS
         await ensureGridReady(page);
         await clickAllButtons(page);
