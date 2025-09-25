ac2ab819e169a28db73388c82b2f677f1349a51c
Stephen Cooper <scooperdev@gmail.com>
2025-09-25 23:20:27 +0100

    fix tests

--- documentation/ag-grid-docs/src/content/docs/theming-test/_examples/theming-api/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/theming-test/_examples/theming-api/example.spec.ts
@@ -1,7 +1,14 @@
 import { clickAllButtons, ensureGridReady, test } from '@utils/grid/test-utils';
 
 test.agExample(import.meta, () => {
-    test.eachFramework('Example', async ({ page }) => {
+    test.typescript('Example', async ({ page }) => {
+        // PLACEHOLDER - MINIMAL TEST TO ENSURE GRID LOADS WITHOUT ERRORS
+        await ensureGridReady(page);
+        await clickAllButtons(page);
+        // END PLACEHOLDER
+    });
+
+    test.vanilla('Example', async ({ page }) => {
         // PLACEHOLDER - MINIMAL TEST TO ENSURE GRID LOADS WITHOUT ERRORS
         await ensureGridReady(page);
         await clickAllButtons(page);
