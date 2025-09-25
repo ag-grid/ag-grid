f16afa7d9d38e4f407f88a7bab0e9a69a641b6ca
Stephen Cooper <scooperdev@gmail.com>
2025-09-25 09:11:28 +0100

    Add placeholders to every example

---
+++ documentation/ag-grid-docs/src/content/docs/integrated-charts-api-downloading-image/_examples/downloading-chart-image/example.spec.ts
@@ -0,0 +1,10 @@
+import { clickAllButtons, ensureGridReady, test } from '@utils/grid/test-utils';
+
+test.agExample(import.meta, () => {
+    test.eachFramework('Example', async ({ page }) => {
+        // PLACEHOLDER - MINIMAL TEST TO ENSURE GRID LOADS WITHOUT ERRORS
+        await ensureGridReady(page);
+        await clickAllButtons(page);
+        // END PLACEHOLDER
+    });
+});
