2c1d93936ca3b0c2712b8630d72bc4560a0a6229
Elias Malik <elias.malik@ag-grid.com>
2025-09-25 17:27:05 +0100

    missing module

--- documentation/ag-grid-docs/src/content/docs/cell-data-types/_examples/providing-custom-cell-data-types/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/cell-data-types/_examples/providing-custom-cell-data-types/example.spec.ts
@@ -11,6 +11,7 @@ test.agExample(import.meta, () => {
                 'SideBarModule',
                 'ColumnsToolPanelModule',
                 'FiltersToolPanelModule',
+                'NumberEditorModule',
             ],
         });
 
