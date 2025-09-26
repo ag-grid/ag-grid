5aae946a3cf6d5c76f4a9bd7d62e2774eb969131
Stephen Cooper <scooperdev@gmail.com>
2025-09-26 13:25:52 +0100

    put base url into action

--- documentation/ag-grid-docs/playwright.config.ts
+++ documentation/ag-grid-docs/playwright.config.ts
@@ -4,8 +4,8 @@ const PRE_34_VERSION = process.env.PRE_34_VERSION;
 
 const PREV_URL = PRE_34_VERSION && `https://www.ag-grid.com/archive/${PRE_34_VERSION}/`;
 const PROD_URL = process.env['PUBLIC_SITE_URL'];
-
-const baseURL = PREV_URL || PROD_URL || 'https://localhost:4610';
+const BASE_URL = process.env.BASE_URL;
+const baseURL = BASE_URL || PREV_URL || PROD_URL || 'https://localhost:4610';
 
 // eslint-disable-next-line no-console
 console.log(`Using base URL: ${baseURL}`);
