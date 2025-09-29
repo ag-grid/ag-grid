a654d2c4006bb28a4b9b71b6b700e2e3d1ee1965
Elias Malik <elias.malik@ag-grid.com>
2025-09-29 15:18:40 +0100

    remove pivoting from test

--- documentation/ag-grid-docs/src/content/docs/cell-data-types/_examples/providing-custom-cell-data-types/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/cell-data-types/_examples/providing-custom-cell-data-types/example.spec.ts
@@ -53,53 +53,31 @@ test.agExample(import.meta, () => {
                 useInnerText: true,
             });
 
-            // Enable pivot mode and pivot by Year
-            await agIdFor.pivotModeSelect().click();
-            agIdFor
-                .columnSelectListItemDragHandle('Date (Year) Column')
-                .dragTo(agIdFor.columnDropArea('toolbar', 'Column Labels'));
-
-            await expect(agIdFor.headerGroupCell(`pivotGroup_${vcolPrefix}-date-year_2000_0`)).toBeVisible();
-
-            // Then pivot by Year / Month
-            agIdFor
-                .columnSelectListItemDragHandle('Date (Month) Column')
-                .dragTo(agIdFor.columnDropArea('toolbar', 'Column Labels'));
-            const headerGroupCell = agIdFor.headerGroupCell(
-                `pivotGroup_${vcolPrefix}-date-year-${vcolPrefix}-date-month_2000_0`
+            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
+                3
             );
-            await expect(headerGroupCell).toBeVisible();
 
-            // Expand column group to verify
-            await headerGroupCell.locator('.ag-header-expand-icon-collapsed').click();
-            await expect(
-                agIdFor.headerGroupCell(`pivotGroup_${vcolPrefix}-date-year-${vcolPrefix}-date-month_2000-10_0`)
-            ).toBeVisible();
-
-            // Uncheck all columns
+            // Check virtual columns
             await agIdFor.columnSelectListItemCheckbox('Date (Year) Column').click();
             await agIdFor.columnSelectListItemCheckbox('Date (Month) Column').click();
-            await agIdFor.columnSelectListItemCheckbox('Date Column').click();
+            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
+                3
+            );
 
+            // Remove date columns from grouping one by one
+            await agIdFor.columnDropCellCancelButton('toolbar', 'Row Groups', 'Date').click();
+            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
+                2
+            );
+
+            await agIdFor.columnDropCellCancelButton('toolbar', 'Row Groups', 'Date (Month)').click();
+            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
+                1
+            );
+
+            await agIdFor.columnDropCellCancelButton('toolbar', 'Row Groups', 'Date (Year)').click();
             await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
                 0
-            );
-
-            // Recheck all columns
-            // ...in reverse order this time, checking Date should make Month/Year appear immediately, and the other checks are idempotent
-            await agIdFor.columnSelectListItemCheckbox('Date Column').click();
-            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
-                3
-            );
-
-            await agIdFor.columnSelectListItemCheckbox('Date (Month) Column').click();
-            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
-                3
-            );
-
-            await agIdFor.columnSelectListItemCheckbox('Date (Year) Column').click();
-            await expect(agIdFor.columnDropArea('toolbar', 'Row Groups').locator('.ag-column-drop-cell')).toHaveCount(
-                3
             );
         });
     });
