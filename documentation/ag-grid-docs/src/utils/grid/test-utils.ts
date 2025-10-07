075ec7d997e8e85d5170d1d0db222d3ca7e42d09
Stephen Cooper <scooperdev@gmail.com>
2025-10-07 13:54:44 +0100

    AG-15943 Browsers defined in CI task

--- documentation/ag-grid-docs/src/utils/grid/test-utils.ts
+++ documentation/ag-grid-docs/src/utils/grid/test-utils.ts
@@ -76,9 +76,6 @@ function getFilteredFrameworks(): readonly AgFramework[] {
 
 const FILTERED_FRAMEWORKS = getFilteredFrameworks();
 
-const additionalBrowser = ['webkit', 'firefox'];
-const frameworksWithAdditionalBrowser: AgFramework[] = ['reactFunctionalTs', 'typescript'];
-
 const licenseTexts = [
     '****************************************************************************************************************************',
     '************************************************ AG Grid Enterprise License ************************************************',
@@ -271,9 +268,6 @@ const frameworkTest =
             }
 
             // Would be nice if this logic could be done so that the test is not even created rather than skipped
-            if (additionalBrowser.includes(browserName) && !frameworksWithAdditionalBrowser.includes(agFramework)) {
-                test.skip(true, `Skipping ${agFramework} tests in ${browserName} to reduce duplication.`);
-            }
             if (process.env.FRAMEWORK && process.env.FRAMEWORK !== agFramework) {
                 test.skip(true, `Skipping ${agFramework} as not the selected framework ${process.env.FRAMEWORK}.`);
             }
