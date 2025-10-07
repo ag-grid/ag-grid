aaa851c79f818338f993859d5ecc3a445ffe5ad4
Mark Durrant <mark.j.durrant@gmail.com>
2025-10-07 12:16:48 +0100

    AG-15781 - Refactor customer logos to fix layout issues across different contexts

--- documentation/ag-grid-docs/src/components/customer-logos/CustomerLogos.tsx
+++ documentation/ag-grid-docs/src/components/customer-logos/CustomerLogos.tsx
@@ -1,11 +1,10 @@
-import classnames from 'classnames';
 import React from 'react';
 
 import styles from './CustomerLogos.module.scss';
 
 export const CustomerLogos: React.FC = () => {
     return (
-        <div className={classnames(styles.customerLogosOuter, 'layout-max-width-small')}>
+        <div className={styles.customerLogosOuter}>
             <div className={styles.customerLogos}></div>
         </div>
     );
