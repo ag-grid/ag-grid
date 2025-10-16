e696110d9018570c7bf1edb51ba5b8efd98fab61
Andrew Glazier <andrew.glazier@ag-grid.com>
2025-10-16 16:30:26 +0100

    AG-3953 - Module sizes

--- packages/ag-grid-enterprise/src/formula/formulaModule.ts
+++ packages/ag-grid-enterprise/src/formula/formulaModule.ts
@@ -1,9 +1,10 @@
 import type { _ModuleWithoutApi } from 'ag-grid-community';
 
-import { CellSelectionModule, RowNumbersModule } from '../main';
 import { VERSION } from '../version';
 import { formulaStylesCSS } from './formula-styles.css-GENERATED';
 import { FormulaService } from './formulaService';
+import { CellSelectionModule } from '../rangeSelection/rangeSelectionModule';
+import { RowNumbersModule } from '../rowNumbers/rowNumbersModule';
 
 /**
  * @feature FormulaModule
