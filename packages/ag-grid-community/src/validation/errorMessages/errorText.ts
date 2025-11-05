4c591c7bab31150bb6ccd1aa8a4c91dd88d0400d
Stephen Cooper <scooperdev@gmail.com>
2025-11-05 15:12:49 +0000

    Update jsdoc

--- packages/ag-grid-community/src/validation/errorMessages/errorText.ts
+++ packages/ag-grid-community/src/validation/errorMessages/errorText.ts
@@ -720,7 +720,7 @@ export const AG_GRID_ERRORS = {
     292: ({ colId }: { colId: string }) =>
         `The Multi Filter for column '${colId}' has buttons configured against the child filters. When 'enableFilterHandlers=true', buttons must instead be provided against the parent Multi Filter params. The child filter buttons will be ignored.` as const,
     293: () =>
-        `Setting \`filterListAsync\` to \`true\` in \`agRichSelectCellEditor\` is only relevant when both \`values()\` returns a Promise result and \`filterList\` is set to \`true\`.` as const,
+        `When using the \`agRichSelectCellEditor\` setting \`filterListAsync = true\` requires \`filterList = true\` and the \`values()\` callback must return a Promise of filtered values.` as const,
 };
 
 export type ErrorMap = typeof AG_GRID_ERRORS;
