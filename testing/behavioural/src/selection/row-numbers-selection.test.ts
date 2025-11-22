f4cac4147e42d091fe22cb8f82e9f0446f964494
Elias Malik <elias.malik@ag-grid.com>
2025-11-22 23:03:58 +0000

    add tests

---
+++ testing/behavioural/src/selection/row-numbers-selection.test.ts
@@ -0,0 +1,222 @@
+import { getByTestId } from '@testing-library/dom';
+import type { MockInstance } from 'vitest';
+
+import type { GridApi, GridOptions } from 'ag-grid-community';
+import { ClientSideRowModelModule, agTestIdFor, getGridElement, setupAgTestIds } from 'ag-grid-community';
+import { CellSelectionModule, RowNumbersModule } from 'ag-grid-enterprise';
+
+import { TestGridsManager, assertSelectedCellRanges, asyncSetTimeout, waitForEvent } from '../test-utils';
+import { GridActions } from './utils';
+
+describe('Row Numbers Cell Selection', () => {
+    let consoleErrorSpy: MockInstance;
+    let consoleWarnSpy: MockInstance;
+
+    const gridMgr = new TestGridsManager({
+        modules: [ClientSideRowModelModule, CellSelectionModule, RowNumbersModule],
+    });
+
+    async function createGrid(go: GridOptions): Promise<[GridApi, GridActions]> {
+        const api = gridMgr.createGrid('myGrid', go);
+        const actions = new GridActions(api);
+
+        await waitForEvent('firstDataRendered', api);
+        await asyncSetTimeout(0);
+
+        return [api, actions];
+    }
+
+    beforeAll(() => {
+        setupAgTestIds();
+    });
+
+    beforeEach(() => {
+        gridMgr.reset();
+
+        consoleErrorSpy = vitest.spyOn(console, 'error').mockImplementation(() => {});
+        consoleWarnSpy = vitest.spyOn(console, 'warn').mockImplementation(() => {});
+    });
+
+    afterEach(() => {
+        gridMgr.reset();
+
+        consoleErrorSpy.mockRestore();
+        consoleWarnSpy.mockRestore();
+    });
+
+    const columnDefs = [{ field: 'sport' }, { field: 'year' }, { field: 'amount' }, { field: 'day' }];
+    const rowData = [
+        { sport: 'football', year: 2021, amount: 43, day: 'monday' },
+        { sport: 'rugby', year: 2020, amount: 102, day: 'sunday' },
+        { sport: 'tennis', year: 2018, amount: 235, day: 'thursday' },
+        { sport: 'cricket', year: 2003, amount: 11, day: 'friday' },
+        { sport: 'golf', year: 2021, amount: 7, day: 'monday' },
+        { sport: 'swimming', year: 2020, amount: 93, day: 'tuesday' },
+        { sport: 'rowing', year: 2019, amount: 32, day: 'saturday' },
+    ];
+
+    test('click row number selects row cells, clears existing range', async () => {
+        const [api] = await createGrid({
+            columnDefs,
+            rowData,
+            cellSelection: true,
+            rowNumbers: true,
+        });
+
+        const gridDiv = getGridElement(api)! as HTMLElement;
+
+        const row1 = getByTestId(gridDiv, agTestIdFor.rowNumber('0'));
+        const row2 = getByTestId(gridDiv, agTestIdFor.rowNumber('1'));
+        const columns = api.getColumns()!.map((c) => c.getColId());
+
+        // Have to use touch instead of click because the grid only attaches touchstart in JSDOM
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 0, rowStartIndex: 0, columns }], api);
+
+        row2.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 1, rowStartIndex: 1, columns }], api);
+    });
+
+    test('CTRL-click row number selects row cells additively', async () => {
+        const [api] = await createGrid({
+            columnDefs,
+            rowData,
+            cellSelection: true,
+            rowNumbers: true,
+        });
+
+        const gridDiv = getGridElement(api)! as HTMLElement;
+
+        const row1 = getByTestId(gridDiv, agTestIdFor.rowNumber('0'));
+        const row2 = getByTestId(gridDiv, agTestIdFor.rowNumber('1'));
+        const columns = api.getColumns()!.map((c) => c.getColId());
+
+        // Have to use touch instead of click because the grid only attaches touchstart in JSDOM
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 0, rowStartIndex: 0, columns }], api);
+
+        row2.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, ctrlKey: true }));
+        assertSelectedCellRanges(
+            [
+                { rowEndIndex: 0, rowStartIndex: 0, columns },
+                { rowEndIndex: 1, rowStartIndex: 1, columns },
+            ],
+            api
+        );
+    });
+
+    test('CTRL-click row number deselects already-selected row cells', async () => {
+        const [api] = await createGrid({
+            columnDefs,
+            rowData,
+            cellSelection: true,
+            rowNumbers: true,
+        });
+
+        const gridDiv = getGridElement(api)! as HTMLElement;
+
+        const row1 = getByTestId(gridDiv, agTestIdFor.rowNumber('0'));
+        const columns = api.getColumns()!.map((c) => c.getColId());
+
+        // Have to use touch instead of click because the grid only attaches touchstart in JSDOM
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 0, rowStartIndex: 0, columns }], api);
+
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, ctrlKey: true }));
+        assertSelectedCellRanges([], api);
+    });
+
+    test('CTRL-click row number deselects, re-selecting does not merge with adjacent ranges', async () => {
+        const [api] = await createGrid({
+            columnDefs,
+            rowData,
+            cellSelection: true,
+            rowNumbers: true,
+        });
+
+        const gridDiv = getGridElement(api)! as HTMLElement;
+
+        const row1 = getByTestId(gridDiv, agTestIdFor.rowNumber('0'));
+        const row2 = getByTestId(gridDiv, agTestIdFor.rowNumber('1'));
+        const row3 = getByTestId(gridDiv, agTestIdFor.rowNumber('2'));
+        const columns = api.getColumns()!.map((c) => c.getColId());
+
+        // Have to use touch instead of click because the grid only attaches touchstart in JSDOM
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        row3.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, shiftKey: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 2, rowStartIndex: 0, columns }], api);
+
+        row2.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, ctrlKey: true }));
+        assertSelectedCellRanges(
+            [
+                { rowEndIndex: 0, rowStartIndex: 0, columns },
+                { rowEndIndex: 2, rowStartIndex: 2, columns },
+            ],
+            api
+        );
+
+        row2.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, ctrlKey: true }));
+        assertSelectedCellRanges(
+            [
+                { rowEndIndex: 0, rowStartIndex: 0, columns },
+                { rowEndIndex: 2, rowStartIndex: 2, columns },
+                { rowEndIndex: 1, rowStartIndex: 1, columns },
+            ],
+            api
+        );
+    });
+
+    test('SHIFT-click row number selects range of row cells', async () => {
+        const [api] = await createGrid({
+            columnDefs,
+            rowData,
+            cellSelection: true,
+            rowNumbers: true,
+        });
+
+        const gridDiv = getGridElement(api)! as HTMLElement;
+
+        const row1 = getByTestId(gridDiv, agTestIdFor.rowNumber('0'));
+        const row4 = getByTestId(gridDiv, agTestIdFor.rowNumber('3'));
+        const columns = api.getColumns()!.map((c) => c.getColId());
+
+        // Have to use touch instead of click because the grid only attaches touchstart in JSDOM
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 0, rowStartIndex: 0, columns }], api);
+
+        row4.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, shiftKey: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 3, rowStartIndex: 0, columns }], api);
+    });
+
+    test('CTRL-SHIFT-click row number selects range of row cells additively', async () => {
+        const [api] = await createGrid({
+            columnDefs,
+            rowData,
+            cellSelection: true,
+            rowNumbers: true,
+        });
+
+        const gridDiv = getGridElement(api)! as HTMLElement;
+
+        const row1 = getByTestId(gridDiv, agTestIdFor.rowNumber('0'));
+        const row2 = getByTestId(gridDiv, agTestIdFor.rowNumber('1'));
+        const row4 = getByTestId(gridDiv, agTestIdFor.rowNumber('3'));
+        const columns = api.getColumns()!.map((c) => c.getColId());
+
+        // Have to use touch instead of click because the grid only attaches touchstart in JSDOM
+        row1.dispatchEvent(new MouseEvent('touchstart', { bubbles: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 0, rowStartIndex: 0, columns }], api);
+
+        row2.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, ctrlKey: true }));
+        assertSelectedCellRanges([{ rowEndIndex: 0, rowStartIndex: 0, columns }], api);
+
+        row4.dispatchEvent(new MouseEvent('touchstart', { bubbles: true, ctrlKey: true, shiftKey: true }));
+        assertSelectedCellRanges(
+            [
+                { rowEndIndex: 0, rowStartIndex: 0, columns },
+                { rowEndIndex: 3, rowStartIndex: 1, columns },
+            ],
+            api
+        );
+    });
+});
