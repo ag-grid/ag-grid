6b87ef8cf286b1258cd6dbf29988865c8060bfa8
Victor Musienko <8777372+sdwvit@users.noreply.github.com>
2025-11-28 10:54:18 +0000

    Clarify behavior of group order preservation during sorting
     - Update documentation to specify that group order is not preserved if user resets sort direction
    
    Signed-off-by: Victor Musienko <8777372+sdwvit@users.noreply.github.com>

--- packages/ag-grid-community/src/entities/gridOptions.ts
+++ packages/ag-grid-community/src/entities/gridOptions.ts
@@ -1332,6 +1332,7 @@ export interface GridOptions<TData = any> {
     autoGroupColumnDef?: AutoGroupColumnDef<TData>;
     /**
      * When `true`, preserves the current group order when sorting on non-group columns.
+     * If a user explicitly resets the current group sort direction, then the current group column order is not preserved.
      * @default false
      * @agModule `RowGroupingModule`
      */
