2831f070b03d65ad5cb76d9707944d92f4269abe
Salvatore Previti <salvatore.previti@ag-grid.com>
2025-12-09 12:40:23 +0000

    RTI-3176 temporary fix for examples (#12717)

--- documentation/ag-grid-docs/src/content/docs/change-detection/_examples/change-detection-groups/main.ts
+++ documentation/ag-grid-docs/src/content/docs/change-detection/_examples/change-detection-groups/main.ts
@@ -37,7 +37,7 @@ const gridOptions: GridOptions = {
         {
             headerName: 'Total',
             type: 'totalColumn',
-            // we use getValue() instead of data.a so that it gets the aggregated values at the group level
+            aggFunc: 'sum',
             valueGetter: 'getValue("a") + getValue("b") + getValue("c") + getValue("d")',
         },
     ],
