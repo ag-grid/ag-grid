eeaa99c4ef5798184081aa3fc76a3f2275d13f30
Guilherme Lopes <guilherme@gwm-solutions.com>
2025-12-10 14:35:25 -0300

    AG-16430 - fixed module size

--- testing/module-size/moduleDefinitions.ts
+++ testing/module-size/moduleDefinitions.ts
@@ -51,7 +51,7 @@ export const AllGridCommunityModules: Record<`${CommunityModuleName}Module`, num
 };
 export const AllEnterpriseModules: Record<`${EnterpriseModuleName}Module`, number> = {
     AdvancedFilterModule: 223.75,
-    AllEnterpriseModule: 1593.7,
+    AllEnterpriseModule: 1601.05,
     AiToolkitModule: 19.41,
     BatchEditModule: 84.54,
     CellSelectionModule: 62.78,
