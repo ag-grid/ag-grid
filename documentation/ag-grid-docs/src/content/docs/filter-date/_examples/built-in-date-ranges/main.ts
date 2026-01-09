4928664fe9497485919ff12773faa20d8fff9aad
Victor Musienko <8777372+sdwvit@users.noreply.github.com>
2026-01-09 10:59:50 +0000

    Craft an example
    Signed-off-by: Victor Musienko <8777372+sdwvit@users.noreply.github.com>

---
+++ documentation/ag-grid-docs/src/content/docs/filter-date/_examples/built-in-date-ranges/main.ts
@@ -0,0 +1,79 @@
+import type { ColDef, GridApi, GridOptions } from 'ag-grid-community';
+import {
+    ClientSideRowModelModule,
+    DateFilterModule,
+    ModuleRegistry,
+    ValidationModule,
+    createGrid,
+} from 'ag-grid-community';
+
+ModuleRegistry.registerModules([
+    ClientSideRowModelModule,
+    DateFilterModule,
+    ...(process.env.NODE_ENV !== 'production' ? [ValidationModule] : []),
+]);
+
+const columnDefs: ColDef[] = [
+    { field: 'athlete' },
+    {
+        field: 'date',
+        filter: 'agDateColumnFilter',
+        filterParams: {
+            // Enable built-in named & relative date ranges
+            filterOptions: [
+                'empty',
+                'equals',
+                'yesterday',
+                'today',
+                'tomorrow',
+                'last7Days',
+                'lastWeek',
+                'thisWeek',
+                'nextWeek',
+                'last30Days',
+                'lastMonth',
+                'thisMonth',
+                'nextMonth',
+                'last90Days',
+                'lastQuarter',
+                'thisQuarter',
+                'nextQuarter',
+                'lastYear',
+                'thisYear',
+                'yearToDate',
+                'nextYear',
+                'last6Months',
+                'last12Months',
+                'last24Months',
+            ],
+        },
+    },
+];
+
+let gridApi: GridApi<IOlympicData>;
+
+const gridOptions: GridOptions<IOlympicData> = {
+    columnDefs: columnDefs,
+    defaultColDef: {
+        flex: 1,
+        minWidth: 150,
+        filter: true,
+    },
+};
+
+// Setup the grid after the page has finished loading
+document.addEventListener('DOMContentLoaded', function () {
+    const gridDiv: HTMLElement | null = document.querySelector('#myGrid');
+    gridApi = createGrid(gridDiv, gridOptions);
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+        .then((response) => response.json())
+        .then((data) =>
+            gridApi.setGridOption(
+                'rowData',
+                data.map((d: IOlympicData) => ({
+                    ...d,
+                    date: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000 * (Math.random() * 0.5 - Math.random())), // Random date [-6 month, +6 month]
+                }))
+            )
+        );
+});
