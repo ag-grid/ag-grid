25289d4550ae1ae291fc9ecb2ba485dbd87eee4c
Stephen Cooper <scooperdev@gmail.com>
2026-01-12 17:41:01 +0000

    Fix tests

--- packages/ag-grid-react/src/index.ts
+++ packages/ag-grid-react/src/index.ts
@@ -4,3 +4,4 @@ export * from './shared/customComp/interfaces';
 export * from './shared/customComp/util';
 export { CustomContext as CustomComponentContext } from './shared/customComp/customContext';
 export type { AgGridProviderProps } from './reactUi/agGridProvider';
+export { AgGridProvider } from './reactUi/agGridProvider';
