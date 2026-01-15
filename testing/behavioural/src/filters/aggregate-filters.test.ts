1d31028832d4b52c5d1a270b9bbe6e49ceb3f81b
Elias Malik <elias.malik@ag-grid.com>
2026-01-15 00:35:40 +0000

    add test case

--- testing/behavioural/src/filters/aggregate-filters.test.ts
+++ testing/behavioural/src/filters/aggregate-filters.test.ts
@@ -1,11 +1,20 @@
-import { ClientSideRowModelModule, NumberFilterModule, TextFilterModule, setupAgTestIds } from 'ag-grid-community';
-import { RowGroupingModule } from 'ag-grid-enterprise';
+import { getByTestId } from '@testing-library/dom';
+import userEvent from '@testing-library/user-event';
 
-import { GridRows, TestGridsManager } from '../test-utils';
+import {
+    ClientSideRowModelModule,
+    NumberFilterModule,
+    TextFilterModule,
+    agTestIdFor,
+    setupAgTestIds,
+} from 'ag-grid-community';
+import { PivotModule, RowGroupingModule } from 'ag-grid-enterprise';
+
+import { GridRows, TestGridsManager, getGridHTMLElement, waitForEvent } from '../test-utils';
 
 describe('Aggregate Filters', () => {
     const gridsManager = new TestGridsManager({
-        modules: [ClientSideRowModelModule, TextFilterModule, NumberFilterModule, RowGroupingModule],
+        modules: [ClientSideRowModelModule, TextFilterModule, NumberFilterModule, RowGroupingModule, PivotModule],
     });
 
     const rowData = [
@@ -68,6 +77,58 @@ describe('Aggregate Filters', () => {
             ROOT id:ROOT_NODE_ID
             └─┬ LEAF_GROUP collapsed id:"row-group-athlete-Michael Phelps" ag-Grid-AutoColumn:"Michael Phelps" gold:8
             · └── LEAF hidden id:0 athlete:"Michael Phelps" gold:8
+        `);
+    });
+
+    test('Grand total row updates displayed aggregation when number filter applied in pivot mode with no column lables', async () => {
+        const userSession = userEvent.setup();
+
+        const api = await gridsManager.createGridAndWait('grid2', {
+            columnDefs: [
+                { field: 'athlete', rowGroup: true, hide: true },
+                { field: 'gold', aggFunc: 'sum', filter: 'agNumberColumnFilter' },
+            ],
+            defaultColDef: {
+                flex: 1,
+                floatingFilter: true,
+                filterParams: {
+                    filterOptions: ['greaterThan'],
+                },
+            },
+            grandTotalRow: 'bottom',
+            pivotMode: true,
+            rowData,
+        });
+
+        const gridDiv = getGridHTMLElement(api)!;
+
+        await new GridRows(api, 'before filter').check(`
+            ROOT id:ROOT_NODE_ID gold:21
+            ├─┬ LEAF_GROUP collapsed id:"row-group-athlete-Michael Phelps" ag-Grid-AutoColumn:"Michael Phelps" gold:18
+            │ ├── LEAF hidden id:0 athlete:"Michael Phelps" gold:8
+            │ ├── LEAF hidden id:1 athlete:"Michael Phelps" gold:6
+            │ └── LEAF hidden id:2 athlete:"Michael Phelps" gold:4
+            ├─┬ LEAF_GROUP collapsed id:"row-group-athlete-Natalie Coughlin" ag-Grid-AutoColumn:"Natalie Coughlin" gold:3
+            │ ├── LEAF hidden id:3 athlete:"Natalie Coughlin" gold:1
+            │ ├── LEAF hidden id:4 athlete:"Natalie Coughlin" gold:2
+            │ └── LEAF hidden id:5 athlete:"Natalie Coughlin" gold:0
+            └─ footer id:rowGroupFooter_ROOT_NODE_ID ag-Grid-AutoColumn:"Total " gold:21
+        `);
+
+        const modelUpdated = waitForEvent('modelUpdated', api);
+        await userSession.type(
+            getByTestId(gridDiv, agTestIdFor.numberFilterInstanceInput({ source: 'floating-filter', colId: 'gold' })),
+            '5'
+        );
+        await modelUpdated;
+
+        await new GridRows(api, 'after filter').check(`
+            ROOT id:ROOT_NODE_ID gold:21
+            ├─┬ LEAF_GROUP collapsed id:"row-group-athlete-Michael Phelps" ag-Grid-AutoColumn:"Michael Phelps" gold:18
+            │ ├── LEAF hidden id:0 athlete:"Michael Phelps" gold:8
+            │ ├── LEAF hidden id:1 athlete:"Michael Phelps" gold:6
+            │ └── LEAF hidden id:2 athlete:"Michael Phelps" gold:4
+            └─ footer id:rowGroupFooter_ROOT_NODE_ID ag-Grid-AutoColumn:"Total " gold:18
         `);
     });
 });
