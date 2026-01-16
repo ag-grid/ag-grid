1cbc0a6e32fd4e9ae6b4e2ee61d01744913210d1
Tak Tran <contact@tutaktran.com>
2026-01-16 12:45:09 +0000

    AG-3390 - Cache the sitemap generated for future builds to use

--- documentation/ag-grid-docs/astro.config.mjs
+++ documentation/ag-grid-docs/astro.config.mjs
@@ -8,6 +8,7 @@ import { loadEnv } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
 
+import agCacheSitemap from '../../external/ag-website-shared/plugins/agCacheSitemap';
 import agLinkChecker from '../../external/ag-website-shared/plugins/agLinkChecker';
 import buildTime from './plugins/agBuildTime';
 import agHotModuleReload from './plugins/agHotModuleReload';
@@ -203,5 +204,9 @@ export default defineConfig({
             skip: CHECK_REDIRECTS !== 'true',
         }),
         agLinkChecker({ include: CHECK_LINKS === 'true' }),
+
+        agCacheSitemap({
+            cacheFolder: './.astro/cache/sitemap',
+        }),
     ],
 });
