5b8b33bede75b3be261463599d5e6a7b5093fa7e
Kyler Phillips <kylerphillips@hotmail.co.uk>
2026-01-19 16:30:58 +0000

    AG-16219 Clean up icons and references/css

--- documentation/ag-grid-docs/src/components/pages-navigation/components/SideNavigation.tsx
+++ documentation/ag-grid-docs/src/components/pages-navigation/components/SideNavigation.tsx
@@ -4,37 +4,12 @@ import { addNonBreakingSpaceBetweenLastWords } from '@utils/addNonBreakingSpaceB
 import type { MarkdownHeading } from 'astro';
 
 import styles from './SideNavigation.module.scss';
+import BackToTopIcon from '../../../images/back-to-top-icon.svg?react';
 
 interface Props {
     headings: MarkdownHeading[];
     delayedScrollSpy?: boolean;
 }
-
-const OnThisPageIcon = () => (
-    <svg
-        width="16"
-        height="16"
-        viewBox="0 0 16 16"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        xmlns="http://www.w3.org/2000/svg"
-        className={styles.level1Icon}
-    >
-        <path d="M2.44434 12.6665H13.5554" strokeLinecap="round" strokeLinejoin="round"></path>
-        <path d="M2.44434 3.3335H13.5554" strokeLinecap="round" strokeLinejoin="round"></path>
-        <path d="M2.44434 8H7.33323" strokeLinecap="round" strokeLinejoin="round"></path>
-    </svg>
-);
-
-const BackToTopIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24" className={styles.backToTopIcon}>
-        <path
-            fill="currentColor"
-            d="m11 7.825l-4.9 4.9q-.3.3-.7.288t-.7-.313q-.275-.3-.288-.7t.288-.7l6.6-6.6q.15-.15.325-.212T12 4.425t.375.063t.325.212l6.6 6.6q.275.275.275.688t-.275.712q-.3.3-.712.3t-.713-.3L13 7.825V19q0 .425-.288.713T12 20t-.712-.288T11 19z"
-        />
-    </svg>
-);
 
 export function SideNavigation({ headings, delayedScrollSpy }: Props) {
     const menuRef = useScrollSpy({ headings, delayedScrollSpy });
@@ -73,7 +48,7 @@ export function SideNavigation({ headings, delayedScrollSpy }: Props) {
                             navigate({ search: window.location.search, hash: 'top' });
                         }}
                     >
-                        <BackToTopIcon />
+                        <BackToTopIcon className={styles.backToTopIcon} />
                         Back to top
                     </a>
                 </div>
