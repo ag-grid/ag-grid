45b5a0b3d32c55c2ed9493b70cb2c215349d5916
Kyler Phillips <kylerphillips@hotmail.co.uk>
2026-01-19 15:07:30 +0000

    AG-16219 Add back to top icon at bottom of sidenav

--- documentation/ag-grid-docs/src/components/pages-navigation/components/SideNavigation.tsx
+++ documentation/ag-grid-docs/src/components/pages-navigation/components/SideNavigation.tsx
@@ -27,6 +27,21 @@ const OnThisPageIcon = () => (
     </svg>
 );
 
+const BackToTopIcon = () => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 24 24"
+        className={styles.backToTopIcon}
+    >
+        <path
+            fill="currentColor"
+            d="m11 7.825l-4.9 4.9q-.3.3-.7.288t-.7-.313q-.275-.3-.288-.7t.288-.7l6.6-6.6q.15-.15.325-.212T12 4.425t.375.063t.325.212l6.6 6.6q.275.275.275.688t-.275.712q-.3.3-.712.3t-.713-.3L13 7.825V19q0 .425-.288.713T12 20t-.712-.288T11 19z"
+        />
+    </svg>
+);
+
 export function SideNavigation({ headings, delayedScrollSpy }: Props) {
     const menuRef = useScrollSpy({ headings, delayedScrollSpy });
 
@@ -50,11 +65,23 @@ export function SideNavigation({ headings, delayedScrollSpy }: Props) {
                                 }}
                             >
                                 {depth === 1 && <OnThisPageIcon />}
-                                {depth === 1 ? 'On this page' : addNonBreakingSpaceBetweenLastWords(text)}
+                                {depth === 1 ? text : addNonBreakingSpaceBetweenLastWords(text)}
                             </a>
                         </li>
                     ))}
                 </ul>
+                <div className={styles.backToTop}>
+                    <a
+                        href="#"
+                        onClick={(event) => {
+                            event.preventDefault();
+                            window.scrollTo({ top: 0, behavior: 'smooth' });
+                        }}
+                    >
+                        <BackToTopIcon />
+                        Back to top
+                    </a>
+                </div>
             </div>
         </nav>
     );
