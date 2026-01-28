14aa18d18190603f03ff1a726adca952f21d76e7
sdwvit <victor.musienko@ag-grid.com>
2026-01-28 16:02:53 +0000

    pr comment

--- packages/ag-grid-enterprise/src/serverSideRowModel/stores/lazy/lazyCache.ts
+++ packages/ag-grid-enterprise/src/serverSideRowModel/stores/lazy/lazyCache.ts
@@ -1193,8 +1193,6 @@ export class LazyCache extends BeanStub {
          */
         if (isNewRowCountValid) {
             this.numberOfRows = newRowCount;
-        } else if (this.isLastRowKnown && (!remainingIdsToRemove.length || nodesToVerify.length > 0)) {
-            this.numberOfRows -= idsToRemove.length; // i suspect in this case this is equal to removednodes.length
         } else {
             this.numberOfRows -= removedNodes.length;
         }
