993b2aa50adad158f330582e6a40456d12cd92ba
Salvatore Previti <salvatore.previti@ag-grid.com>
2026-02-03 10:24:35 +0000

    AG-16600-row-node-get-data-value

--- packages/ag-grid-community/src/valueService/valueService.ts
+++ packages/ag-grid-community/src/valueService/valueService.ts
@@ -516,7 +516,7 @@ export class ValueService extends BeanStub implements NamedBean {
         // skipped value changes on group rows because we were not looking for them when calling setDataValue.
         // Now we do, so we need to block auto-creation to avoid unexpected data mutations to not change behavior.
         if (colDef.pivotValueColumn) {
-            return false; // Legacy behaviour - pivot groups do not auto-create data
+            return false; // Legacy behaviour - pivot groups do not auto-create data with pivot columns
         }
 
         return true;
