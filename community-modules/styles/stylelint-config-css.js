1b77117a66bd726c55fbf07d781ea707c0d806d0
Bernie Sumption <bernie@berniecode.com>
2026-02-10 11:23:43 +0000

    RTI-3242 fix false-positive lint errors

--- community-modules/styles/stylelint-config-css.js
+++ community-modules/styles/stylelint-config-css.js
@@ -1,6 +1,8 @@
 module.exports = {
     plugins: ['stylelint-csstree-validator'],
     rules: {
-        'csstree/validator': true,
+        'csstree/validator': {
+            ignoreProperties: ['container-type'],
+        },
     },
 };
