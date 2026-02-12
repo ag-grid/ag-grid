b36e753d2014f5d84d90688b92f4e5c860107df2
Stephen Cooper <scooperdev@gmail.com>
2026-02-12 11:26:19 +0000

    Update aggregation-overview spec to use stable test data

--- documentation/ag-grid-docs/src/content/docs/aggregation/_examples/aggregation-overview/example.spec.ts
+++ documentation/ag-grid-docs/src/content/docs/aggregation/_examples/aggregation-overview/example.spec.ts
@@ -2,11 +2,11 @@ import { expect, test } from '@utils/grid/test-utils';
 
 test.agExample(import.meta, () => {
     test.eachFramework('Example', async ({ agIdFor }) => {
-        await expect(agIdFor.autoGroupCell('row-group-country-Canada')).toContainText('Canada (351)', {
+        await expect(agIdFor.autoGroupCell('row-group-country-Netherlands')).toContainText('Netherlands (4)', {
             useInnerText: true,
         });
-        await expect(agIdFor.cell('row-group-country-Canada', 'bronze')).toContainText('104');
-        await expect(agIdFor.cell('row-group-country-Canada', 'silver')).toContainText('5');
-        await expect(agIdFor.cell('row-group-country-Canada', 'gold')).toContainText('0.47863247863247865');
+        await expect(agIdFor.cell('row-group-country-Netherlands', 'bronze')).toContainText('4');
+        await expect(agIdFor.cell('row-group-country-Netherlands', 'silver')).toContainText('0.75');
+        await expect(agIdFor.cell('row-group-country-Netherlands', 'gold')).toContainText('3');
     });
 });
