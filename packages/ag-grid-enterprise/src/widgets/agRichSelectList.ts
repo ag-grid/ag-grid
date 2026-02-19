3a27667e33f2a17369fbf16a55e2e24772a78527
Guilherme Lopes <guilherme@gwm-solutions.com>
2026-02-19 12:15:43 -0300

    more cleanup

--- packages/ag-grid-enterprise/src/widgets/agRichSelectList.ts
+++ packages/ag-grid-enterprise/src/widgets/agRichSelectList.ts
@@ -56,11 +56,13 @@ export class AgRichSelectList<TValue, TEventType extends string = AgRichSelectLi
 
     public override postConstruct(): void {
         super.postConstruct();
-        const i18n = this.getLocaleTextFunc();
-        this.loadingLabel = i18n('loadingOoo', 'Loading...');
-        this.noMatchesLabel = i18n('noMatches', 'No matches to show');
+        const translate = this.getLocaleTextFunc();
+        this.loadingLabel = translate('loadingOoo', 'Loading...');
+        this.noMatchesLabel = translate('noMatches', 'No matches to show');
+
         this.eLoadingIcon = _createIconNoSpan('richSelectLoading', this.beans, null);
         this.eStateCompLabel = _createElement({ tag: 'span', cls: 'ag-loading-text', children: this.loadingLabel });
+
         this.eStateComp = _createElement({
             tag: 'div',
             cls: 'ag-rich-select-loading',
@@ -73,6 +75,7 @@ export class AgRichSelectList<TValue, TEventType extends string = AgRichSelectLi
                 { tag: 'span', cls: 'ag-loading-text', children: [() => this.eStateCompLabel] },
             ],
         });
+
         this.appendChild(this.eStateComp);
 
         const { cellRowHeight, pickerAriaLabelKey, pickerAriaLabelValue } = this.params;
@@ -96,7 +99,6 @@ export class AgRichSelectList<TValue, TEventType extends string = AgRichSelectLi
 
         const listId = `${LIST_COMPONENT_NAME}-${this.getCompId()}`;
         eListAriaEl.setAttribute('id', listId);
-        const translate = this.getLocaleTextFunc();
         const ariaLabel = translate(pickerAriaLabelKey, pickerAriaLabelValue);
 
         _setAriaLabel(eListAriaEl, ariaLabel);
