0dc9be1e8d16189df72b13c61fb22838b05f2840
Hesam <sabaahang@gmail.com>
2026-02-23 15:34:41 +0000

    reorganized infinite scrolling page

---
+++ documentation/ag-grid-docs/src/content/docs/infinite-scrolling-selection/_examples/specify-selectable-rows/provided/reactFunctionalTs/index.tsx
@@ -0,0 +1,118 @@
+import React, { StrictMode, useCallback, useMemo, useState } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import type { ColDef, GridReadyEvent, IDatasource, IRowNode, RowSelectionOptions } from 'ag-grid-community';
+import { InfiniteRowModelModule, RowSelectionModule, ValidationModule } from 'ag-grid-community';
+import type { CustomCellRendererProps } from 'ag-grid-react';
+import { AgGridProvider, AgGridReact } from 'ag-grid-react';
+
+const modules = [
+    RowSelectionModule,
+    InfiniteRowModelModule,
+    ...(process.env.NODE_ENV !== 'production' ? [ValidationModule] : []),
+];
+
+const rowSelection: RowSelectionOptions = {
+    mode: 'multiRow',
+    hideDisabledCheckboxes: true,
+    headerCheckbox: false,
+    isRowSelectable: (rowNode: IRowNode) => {
+        return rowNode.data ? rowNode.data.country === 'United States' : false;
+    },
+};
+
+const GridExample = () => {
+    const containerStyle = useMemo(() => ({ width: '100%', height: '100%' }), []);
+    const gridStyle = useMemo(() => ({ height: '100%', width: '100%' }), []);
+
+    const [columnDefs, setColumnDefs] = useState<ColDef[]>([
+        // this row shows the row index, doesn't use any data from the row
+        {
+            headerName: 'ID',
+            maxWidth: 100,
+            // it is important to have node.id here, so that when the id changes (which happens
+            // when the row is loaded) then the cell is refreshed.
+            valueGetter: 'node.id',
+            cellRenderer: (props: CustomCellRendererProps) => {
+                if (props.value !== undefined) {
+                    return props.value;
+                } else {
+                    return <img src="https://www.ag-grid.com/example-assets/loading.gif" />;
+                }
+            },
+        },
+        { field: 'athlete', minWidth: 200 },
+        { field: 'age' },
+        { field: 'country', minWidth: 200 },
+        { field: 'year' },
+        { field: 'date', minWidth: 150 },
+        { field: 'sport', minWidth: 150 },
+        { field: 'gold' },
+        { field: 'silver' },
+        { field: 'bronze' },
+        { field: 'total' },
+    ]);
+    const defaultColDef = useMemo<ColDef>(() => {
+        return {
+            flex: 1,
+            minWidth: 100,
+            sortable: false,
+        };
+    }, []);
+
+    const onGridReady = useCallback((params: GridReadyEvent) => {
+        fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+            .then((resp) => resp.json())
+            .then((data) => {
+                const dataSource: IDatasource = {
+                    rowCount: undefined,
+                    getRows: (params) => {
+                        // console.log('asking for ' + params.startRow + ' to ' + params.endRow);
+                        // At this point in your code, you would call the server.
+                        // To make the demo look real, wait for 500ms before returning
+                        setTimeout(function () {
+                            // take a slice of the total rows
+                            const rowsThisPage = data.slice(params.startRow, params.endRow);
+                            // if on or after the last page, work out the last row.
+                            let lastRow = -1;
+                            if (data.length <= params.endRow) {
+                                lastRow = data.length;
+                            }
+                            // call the success callback
+                            params.successCallback(rowsThisPage, lastRow);
+                        }, 500);
+                    },
+                };
+                params.api.setGridOption('datasource', dataSource);
+            });
+    }, []);
+
+    return (
+        <AgGridProvider modules={modules}>
+            <div style={containerStyle}>
+                <div style={gridStyle}>
+                    <AgGridReact
+                        columnDefs={columnDefs}
+                        defaultColDef={defaultColDef}
+                        rowBuffer={0}
+                        rowSelection={rowSelection}
+                        rowModelType={'infinite'}
+                        cacheBlockSize={100}
+                        cacheOverflowSize={2}
+                        maxConcurrentDatasourceRequests={2}
+                        infiniteInitialRowCount={1}
+                        maxBlocksInCache={2}
+                        onGridReady={onGridReady}
+                    />
+                </div>
+            </div>
+        </AgGridProvider>
+    );
+};
+
+const root = createRoot(document.getElementById('root')!);
+root.render(
+    <StrictMode>
+        <GridExample />
+    </StrictMode>
+);
