8da2001eb6430636fcf350535876fe8e06632636
seanlandsman <sean@thelandsmans.com>
2026-03-05 11:09:35 +0000

    esbuild/css build changes

--- documentation/ag-grid-docs/src/components/theme-builder/api.ts
+++ documentation/ag-grid-docs/src/components/theme-builder/api.ts
@@ -1,2 +1,5 @@
 export * from '../../../../../packages/ag-grid-community/src/theming/private-theming-api';
+// not importing via relative path to avoid ts trying to import CSS  - doing it this way means we don't need a special
+// css plugin in astro
 export { _gridThemeLogger as gridThemeLogger } from 'ag-grid-community';
+
--- scripts/deployments/release/publishDistArtifactsToNpm.sh
+++ scripts/deployments/release/publishDistArtifactsToNpm.sh
@@ -25,16 +25,3 @@ publishModules()
 publishModules "dist/artifacts/contents/community-modules"
 publishModules "dist/artifacts/contents/packages"
 
-sonar-scanner \
--Dsonar.javascript.node.maxspace=8192 \
--Dsonar.organization=ag-grid \
--Dsonar.projectBaseDir=./packages/ \
--Dsonar.sources=./ag-studio,./ag-studio-angular/projects/ag-studio-angular,./ag-studio-react,./ag-studio-vue3 \
--Dsonar.typescript.tsconfigPaths=./ag-studio/tsconfig.lib.json,./ag-studio-angular/projects/ag-studio-angular/tsconfig.lib.prod.json,./ag-studio-react/tsconfig.json,./ag-studio-vue3/tsconfig.json \
--Dsonar.projectKey=ag-studio_next \
--Dsonar.branch.name=latest \
--Dsonar.host.url=https://sonarcloud.io \
--Dsonar.login=ff6a9075d4febbc270e1a1a5dbcdc54bfa426d51 \
--Dsonar.exclusions=**/__tests__/*,**/test.ts,**/tsconfig.typings.json,**/LICENSE.html,**/*.md,**/dist,**/node_modules,**/typings,**/lib,**/*spec.js \
--Dsonar.scanner.force-deprecated-java-version=true \
--Dsonar.coverage.exclusions=**/*
