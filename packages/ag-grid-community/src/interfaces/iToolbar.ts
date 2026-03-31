94bcfd56bfac8261a7b0a93778c272a078368c5d
sdwvit <victor.musienko@ag-grid.com>
2026-03-31 16:42:11 +0100

    Add toolbar types and CSS (AG-12557)
    iToolbar.ts: Toolbar, ToolbarItemDef, IToolbarItem, IToolbarItemComp
    agToolbar.css: flex layout with min-height and border

---
+++ packages/ag-grid-community/src/interfaces/iToolbar.ts
@@ -0,0 +1,31 @@
+import type { IComponent } from '../agStack/interfaces/iComponent';
+import type { AgGridCommon } from './iCommon';
+
+export type Toolbar = { items: ToolbarItemDef[] };
+
+export interface ToolbarItemDef {
+    toolbarItem?: any;
+    align?: 'left' | 'right';
+    key?: string;
+    toolbarItemParams?: any;
+}
+
+export interface IToolbarItemParams<TData = any, TContext = any> extends AgGridCommon<TData, TContext> {
+    key: string;
+}
+
+export interface IToolbarItem<TData = any, TContext = any> {
+    /**
+     * Called when the `toolbar` grid option is updated.
+     * If this method returns `true`,
+     * the grid assumes that the toolbar item has updated with the latest params,
+     * and takes no further action.
+     * If this method returns `false`, or is not implemented,
+     * the grid will destroy and recreate the toolbar item.
+     */
+    refresh?(params: IToolbarItemParams<TData, TContext>): boolean;
+}
+
+export interface IToolbarItemComp<TData = any, TContext = any>
+    extends IToolbarItem<TData, TContext>,
+        IComponent<IToolbarItemParams<TData, TContext>> {}
