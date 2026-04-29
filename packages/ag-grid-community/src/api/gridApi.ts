a7f255b9e64ae96ff31c7673ae86da158fbbd8af
Victor Musienko <8777372+sdwvit@users.noreply.github.com>
2026-04-29 11:50:19 +0100

    lint
    Signed-off-by: Victor Musienko <8777372+sdwvit@users.noreply.github.com>

--- packages/ag-grid-community/src/api/gridApi.ts
+++ packages/ag-grid-community/src/api/gridApi.ts
@@ -1986,8 +1986,7 @@ export interface _AiToolkitGridApi {
 }
 
 export interface GridApi<TData = any>
-    extends
-        _CoreGridApi<TData>,
+    extends _CoreGridApi<TData>,
         _StateGridApi,
         _RowSelectionGridApi<TData>,
         _RowGridApi<TData>,
