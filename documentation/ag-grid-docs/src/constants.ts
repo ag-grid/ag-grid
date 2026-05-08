6fbed31e6f1766bbe5755a4b2ac24049923069ec
Tak Tran <contact@tutaktran.com>
2026-05-08 10:48:57 +0100

    AG-3390 - Add DEBUG_SCRIPT_FILE_NAME constant
    Not used in grid at the moment, but could be used in future.
    From studio, see https://github.com/ag-grid/ag-studio/pull/792

--- documentation/ag-grid-docs/src/constants.ts
+++ documentation/ag-grid-docs/src/constants.ts
@@ -185,6 +185,7 @@ export const EXAMPLE_RANDOM_SEED = 'AG Grid Random Seed';
 export const TRIAL_LICENCE_FORM_URL = import.meta.env?.PUBLIC_TRIAL_LICENCE_FORM_URL;
 
 export const EXAMPLE_STYLE_FILE_NAME = 'ag-example-styles.css';
+export const DEBUG_SCRIPT_FILE_NAME = 'ag-grid-debug.js';
 
 export const PRODUCTION_CHANGELOG_JSON_URL = 'https://www.ag-grid.com/changelog/changelog.json';
 
