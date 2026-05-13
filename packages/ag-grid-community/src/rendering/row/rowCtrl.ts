5239db3bb442f6be6cd580a2358ecb090aebf4c0
Guilherme Lopes <guilherme@gwm-solutions.com>
2026-05-13 17:47:54 -0300

    added internal comment

--- packages/ag-grid-community/src/rendering/row/rowCtrl.ts
+++ packages/ag-grid-community/src/rendering/row/rowCtrl.ts
@@ -90,6 +90,7 @@ export interface RowGui {
     compBean: BeanStub;
 }
 
+/** @internal AG_GRID_INTERNAL - Not for public use. Can change / be removed at any time. */
 export interface PinnedCellGroupWidths {
     leftWidth: number;
     centerWidth: number;
