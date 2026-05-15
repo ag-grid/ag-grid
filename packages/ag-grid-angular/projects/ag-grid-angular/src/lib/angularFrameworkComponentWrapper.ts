3f53c093b5e4b83baa0e4e319087f39393002185
Stephen Cooper <scooperdev@gmail.com>
2026-05-15 16:32:06 +0100

    Remove  standalone: false

--- packages/ag-grid-angular/projects/ag-grid-angular/src/lib/angularFrameworkComponentWrapper.ts
+++ packages/ag-grid-angular/projects/ag-grid-angular/src/lib/angularFrameworkComponentWrapper.ts
@@ -13,7 +13,6 @@ import type { AgFrameworkComponent } from './interfaces';
 @Component({
     selector: 'ag-component-container',
     template: '',
-    standalone: false,
 })
 export class AgComponentContainer {
     public vcr = inject(ViewContainerRef);
