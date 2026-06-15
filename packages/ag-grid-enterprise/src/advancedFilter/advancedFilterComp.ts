53b35aed999b784285e625b906c3c35651b49e88
Bernie Sumption <bernie@berniecode.com>
2026-06-15 14:19:57 +0100

    Ensure button disabled status correct on init

--- packages/ag-grid-enterprise/src/advancedFilter/advancedFilterComp.ts
+++ packages/ag-grid-enterprise/src/advancedFilter/advancedFilterComp.ts
@@ -179,6 +179,7 @@ export class AdvancedFilterComp extends Component {
         });
 
         this.eButtons.updateButtons(buttons);
+        this.eButtons.updateValidity(!this.isApplyDisabled);
     }
 
     private updateModel(action: FilterAction): void {
