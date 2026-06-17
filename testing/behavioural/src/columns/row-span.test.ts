63835d2f2d59471ff62bfc3e95f9fdbe6273c17b
Stephen Cooper <scooperdev@gmail.com>
2026-06-17 11:10:55 +0100

    AG-17415 Enable calculatedColumns opt-in in row-span calc test

--- testing/behavioural/src/columns/row-span.test.ts
+++ testing/behavioural/src/columns/row-span.test.ts
@@ -654,6 +654,7 @@ describe('row spanning', () => {
                 columnDefs: [{ field: 'athlete', colId: 'athlete' }],
                 rowData: [{ athlete: 'A' }, { athlete: 'A' }, { athlete: 'B' }],
                 defaultColDef: { spanRows: true },
+                calculatedColumns: true,
             });
             await settle();
             await new GridRows(api, 'calc-add rows before').check(`
