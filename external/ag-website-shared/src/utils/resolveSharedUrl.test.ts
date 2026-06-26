b0ab24cbdd280c2b0887ed6c166c3ec7ecb7eee4
David Skewis <david.skewis@ag-grid.com>
2026-06-26 14:38:31 +0100

    formatting

--- external/ag-website-shared/src/utils/resolveSharedUrl.test.ts
+++ external/ag-website-shared/src/utils/resolveSharedUrl.test.ts
@@ -12,14 +12,14 @@ vi.mock('@ag-website-shared/utils/chartsUrlWithPrefix', () => ({
 
 describe('resolveSharedUrl', () => {
     test.each`
-        url                               | framework      | expected
-        ${'grid:./mcp-server/'}           | ${'react'}     | ${'GRID/react/mcp-server/'}
-        ${'grid:./filter-text/'}          | ${'angular'}   | ${'GRID/angular/filter-text/'}
-        ${'grid:./page/#section'}         | ${'react'}     | ${'GRID/react/page/#section'}
-        ${'charts:./bar-series/'}         | ${'react'}     | ${'CHARTS/react/bar-series/'}
-        ${'charts:./overview/'}           | ${'vue3'}      | ${'CHARTS/vue3/overview/'}
-        ${'./local-page/'}                | ${'react'}     | ${'./local-page/'}
-        ${'https://www.ag-grid.com/'}     | ${'react'}     | ${'https://www.ag-grid.com/'}
+        url                           | framework    | expected
+        ${'grid:./mcp-server/'}       | ${'react'}   | ${'GRID/react/mcp-server/'}
+        ${'grid:./filter-text/'}      | ${'angular'} | ${'GRID/angular/filter-text/'}
+        ${'grid:./page/#section'}     | ${'react'}   | ${'GRID/react/page/#section'}
+        ${'charts:./bar-series/'}     | ${'react'}   | ${'CHARTS/react/bar-series/'}
+        ${'charts:./overview/'}       | ${'vue3'}    | ${'CHARTS/vue3/overview/'}
+        ${'./local-page/'}            | ${'react'}   | ${'./local-page/'}
+        ${'https://www.ag-grid.com/'} | ${'react'}   | ${'https://www.ag-grid.com/'}
     `('$url (framework=$framework) -> $expected', ({ url, framework, expected }) => {
         expect(resolveSharedUrl({ url, framework })).toBe(expected);
     });
