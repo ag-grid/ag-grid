aa7491df632dc0607d167743ff0aeea81897ec90
AG-seanlandsman <286289162+AG-seanlandsman@users.noreply.github.com>
2026-07-15 11:56:15 +0100

    Update publishDistArtifactsToNpm.sh

--- scripts/deployments/release/publishDistArtifactsToNpm.sh
+++ scripts/deployments/release/publishDistArtifactsToNpm.sh
@@ -2,29 +2,26 @@
 
 TAG="${1:-latest}"
 
-#publishModules()
-#{
-#  local directory=$1
-#
-#  for moduleDirectory in `ls $directory`;
-#  do
-#    local modulePath="$directory/$moduleDirectory/package"
-#
-#    echo "PUBLISHING TO NPM: $modulePath"
-#    npm publish $modulePath --tag $TAG --provenance
-#
-#    if [ $? -ne 0 ]; then
-#        echo "Error publishing $modulePath"
-#        exit 1;
-#    else
-#        echo "$modulePath published to npm"
-#    fi
-#  done
-#}
-#
-#publishModules "dist/artifacts/contents/community-modules"
-#publishModules "dist/artifacts/contents/packages"
+publishModules()
+{
+  local directory=$1
 
-cd dist/artifacts/contents/packages/ag-stack/package
-echo "PUBLISHING TO NPM: ag-stack"
-npm publish --tag $TAG --provenance
+  for moduleDirectory in `ls $directory`;
+  do
+    local modulePath="$directory/$moduleDirectory/package"
+
+    echo "PUBLISHING TO NPM: $modulePath"
+    npm publish $modulePath --tag $TAG --provenance
+
+    if [ $? -ne 0 ]; then
+        echo "Error publishing $modulePath"
+        exit 1;
+    else
+        echo "$modulePath published to npm"
+    fi
+  done
+}
+
+publishModules "dist/artifacts/contents/community-modules"
+publishModules "dist/artifacts/contents/packages"
+
