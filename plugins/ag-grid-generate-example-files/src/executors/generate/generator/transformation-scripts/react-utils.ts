dcb3808ed8f3f1daa2198c327be6b4c496db6a80
Stephen Cooper <scooperdev@gmail.com>
2026-08-07 11:24:44 +0100

    Address PR feedback: anchor the checked rewrite to an attribute boundary

--- plugins/ag-grid-generate-example-files/src/executors/generate/generator/transformation-scripts/react-utils.ts
+++ plugins/ag-grid-generate-example-files/src/executors/generate/generator/transformation-scripts/react-utils.ts
@@ -44,7 +44,7 @@ export function convertTemplate(template: string) {
         .replace(/,\s+event([),])/g, '$1')
         .replace(/<input (.+?[^=])>/g, '<input $1 />')
         .replace(/<input (.*)value=/g, '<input $1defaultValue=')
-        .replace(/<input ([^>]*?)\bchecked\b(?:="")?/g, '<input $1defaultChecked')
+        .replace(/(<input [^>]*?\s)checked(?:="[^"]*")?(?=[\s/>])/g, '$1defaultChecked')
         .replace(/ class=/g, ' className=')
         .replace(/ for=/g, ' htmlFor=')
         .replace(/ <option (.*)selected=""/g, '<option $1selected={true}');
@@ -85,7 +85,7 @@ export function convertFunctionalTemplate(template: string) {
         .replace(/,\s+event([),])/g, '$1')
         .replace(/<input (.+?[^=])>/g, '<input $1 />')
         .replace(/<input (.*)value=/g, '<input $1defaultValue=')
-        .replace(/<input ([^>]*?)\bchecked\b(?:="")?/g, '<input $1defaultChecked')
+        .replace(/(<input [^>]*?\s)checked(?:="[^"]*")?(?=[\s/>])/g, '$1defaultChecked')
         .replace(/ class=/g, ' className=')
         .replace(/ for=/g, ' htmlFor=')
         .replace(/ <option (.*)selected=""/g, '<option $1selected={true}');
