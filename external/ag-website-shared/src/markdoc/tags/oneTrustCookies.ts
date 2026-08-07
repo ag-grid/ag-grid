0e2e30efb77ce2d259763da4fe7c1997ce970708
Mark Durrant <mark.j.durrant@gmail.com>
2026-08-07 11:01:22 +0100

    AG-18105 - Remove OneTrust content

--- external/ag-website-shared/src/markdoc/tags/oneTrustCookies.ts
+++
@@ -1,9 +0,0 @@
-import { component } from '@astrojs/markdoc/config';
-import type { Render } from '@astrojs/markdoc/config';
-import type { Config, Schema } from '@markdoc/markdoc';
-
-export const oneTrustCookies: Schema<Config, Render> = {
-    render: component(
-        '../../external/ag-website-shared/src/components/policies/components/OneTrustCookiesDetails.astro'
-    ),
-};
