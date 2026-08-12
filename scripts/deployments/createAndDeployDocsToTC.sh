cf048a14826e0a1e525c2dbadb0f95869732d0f8
seanlandsman <sean@thelandsmans.com>
2026-08-12 08:37:42 +0100

    Branch builds - add all products, refactor

--- scripts/deployments/createAndDeployDocsToTC.sh
+++ scripts/deployments/createAndDeployDocsToTC.sh
@@ -27,15 +27,9 @@ fi
 
 cd ../../../
 
-echo "Backing up branch builds"
-cp -R /var/www/html/branch-builds /var/www/
-
 echo "Cleaning current grid staging"
 rm -rf /var/www/html/*
 mv $FILENAME /var/www/html/
 
 echo "Unzipping new grid staging"
 unzip -q /var/www/html/$FILENAME -d /var/www/html/
-
-echo "Restoring branch builds"
-cp -R /var/www/branch-builds /var/www/html/
--- scripts/deployments/updateGridStagingRemote.sh
+++ scripts/deployments/updateGridStagingRemote.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Backing up branch builds"
-cp -R @WWW_ROOT_DIR@/html/branch-builds @WWW_ROOT_DIR@/
-
 echo "Cleaning current grid staging"
 rm -rf @WWW_ROOT_DIR@/html/*
 mv @FILENAME@ @WWW_ROOT_DIR@/html/
@@ -12,5 +9,3 @@ echo "Unzipping new grid staging"
 # (rm above leaves dotfiles untouched). Non-interactive deploy would otherwise skip it.
 unzip -qo @WWW_ROOT_DIR@/html/@FILENAME@ -d @WWW_ROOT_DIR@/html/
 
-echo "Restoring branch builds"
-cp -R @WWW_ROOT_DIR@/branch-builds @WWW_ROOT_DIR@/html/
