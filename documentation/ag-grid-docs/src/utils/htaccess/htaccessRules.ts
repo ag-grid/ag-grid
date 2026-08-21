8ef132197b36e76686dc607b465b223e928bf703
seanlandsman <sean@thelandsmans.com>
2026-08-21 14:37:05 +0100

    SE-91 Map every old blog URL to its new /blog/ address SE-86 Land the map as single-hop 301s SE-89 Update robots.txt for /blog/ SE-85 Set the canonicals and sitemap to the new /blog/ URLs

--- documentation/ag-grid-docs/src/utils/htaccess/htaccessRules.ts
+++ documentation/ag-grid-docs/src/utils/htaccess/htaccessRules.ts
@@ -274,7 +274,7 @@ ${SITE_SINGLE_HOP_REWRITES.map((r) => {
 
     # Generic dated permalink: the slug survived the WordPress -> Ghost move.
     RewriteCond %{HTTP_HOST} ^blog\\.ag-grid\\.com$ [NC]
-    RewriteRule ^/?(?:index\\.php/)?[0-9]{4}/[0-9]{2}/[0-9]{2}/(.+?)/?$ https://www.ag-grid.com/blog/$1/ [R=301,NC,L]
+    RewriteRule ^/?(?:index\\.php/)?[0-9]{4}/[0-9]{2}/[0-9]{2}/(.+?)(?:/feed)?/?$ https://www.ag-grid.com/blog/$1/ [R=301,NC,L]
 
     # WordPress categories became Ghost tags. "react" was itself renamed, so it is
     # mapped directly -- via /blog/tag/react/ it would take a second hop.
@@ -282,6 +282,8 @@ ${SITE_SINGLE_HOP_REWRITES.map((r) => {
     RewriteRule ^/?index\\.php/category/react/?$ https://www.ag-grid.com/blog/tag/react-data-grid/ [R=301,NC,L]
     RewriteCond %{HTTP_HOST} ^blog\\.ag-grid\\.com$ [NC]
     RewriteRule ^/?index\\.php/category/(.+?)/?$ https://www.ag-grid.com/blog/tag/$1/ [R=301,NC,L]
+    RewriteCond %{HTTP_HOST} ^blog\\.ag-grid\\.com$ [NC]
+    RewriteRule ^/?index\\.php/tag/react/?$ https://www.ag-grid.com/blog/tag/react-data-grid/ [R=301,NC,L]
     RewriteCond %{HTTP_HOST} ^blog\\.ag-grid\\.com$ [NC]
     RewriteRule ^/?index\\.php/(author|tag|page)/(.+?)/?$ https://www.ag-grid.com/blog/$1/$2/ [R=301,NC,L]
     RewriteCond %{HTTP_HOST} ^blog\\.ag-grid\\.com$ [NC]
