a5541152c69b0a283cfe605b530de6b3a1d88b4f
Bernie Sumption <bernie@berniecode.com>
2026-08-24 17:03:56 +0200

    AG-18149 Remove 1px minimum height for empty row container (#14941)

--- packages/ag-grid-angular/projects/ag-grid-angular/src/lib/tests/quick-filter.spec.ts
+++ packages/ag-grid-angular/projects/ag-grid-angular/src/lib/tests/quick-filter.spec.ts
@@ -88,7 +88,7 @@ describe('Quick Filter', () => {
         validateState({ gridRows: 17, displayedRows: 0, templateRows: 0 });
 
         // To have all the async functions run we flush our fakeAsync test environment. This empties the call stack
-        flush(25);
+        flush(40);
         // So now our component has its displayedRows property updated as the grid callback has been run
         // However, this has not been reflected in our template yet as change detection has not run.
         validateState({ gridRows: 17, displayedRows: 17, templateRows: 0 });
@@ -114,7 +114,7 @@ describe('Quick Filter', () => {
         validateState({ gridRows: 10, displayedRows: 17, templateRows: 17 });
 
         // We now flush out all the async callbacks
-        flush(25);
+        flush(40);
         // Our component event handler has now been run and updated its displayedRows value
         validateState({ gridRows: 10, displayedRows: 10, templateRows: 17 });
 
@@ -127,7 +127,7 @@ describe('Quick Filter', () => {
 
     it('should filter rows by quickFilterText', fakeAsync(() => {
         fixture.detectChanges();
-        flush(25);
+        flush(40);
         fixture.detectChanges();
 
         validateState({ gridRows: 17, displayedRows: 17, templateRows: 17 });
@@ -136,7 +136,7 @@ describe('Quick Filter', () => {
         quickFilterDE.nativeElement.dispatchEvent(new Event('input'));
 
         fixture.detectChanges();
-        flush(25);
+        flush(40);
         fixture.detectChanges();
 
         validateState({ gridRows: 10, displayedRows: 10, templateRows: 10 });
