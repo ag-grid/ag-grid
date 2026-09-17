52e512594441c02b6e596fe810d06d3f530fd40a
Stephen Cooper <scooperdev@gmail.com>
2026-09-17 15:39:58 +0100

    small doc updates

--- documentation/ag-grid-docs/src/content/docs/row-numbers/_examples/row-numbers-row-selection/main.ts
+++ documentation/ag-grid-docs/src/content/docs/row-numbers/_examples/row-numbers-row-selection/main.ts
@@ -36,8 +36,6 @@ const gridOptions: GridOptions<IOlympicData> = {
     rowSelection: {
         mode: 'multiRow',
         enableClickSelection: true,
-        checkboxes: false,
-        headerCheckbox: false,
     },
     rowNumbers: {
         suppressCellSelectionIntegration: true,
